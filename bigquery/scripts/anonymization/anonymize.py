248dc11ddef4a334e32db5c5fb83eefdbeabb314
Célia BARRANGER <c.barranger@groupeonepoint.com>
2020-11-03 09:28:21 +0100

    Add docstrings

--- bigquery/scripts/anonymization/anonymize.py
+++ bigquery/scripts/anonymization/anonymize.py
@@ -13,7 +13,9 @@ logger = logging.getLogger()
 
 
 def anonymize_validation_token_offerer():
-    # Table offerer : Génération d'un token de validation aléatoire pour les utilisateurs en ayant un
+    """
+    Table offerer : Génération d'un token de validation aléatoire pour les utilisateurs en ayant un
+    """
     update_validation_token = f"""UPDATE poc_scalingo.public_offerer 
     SET validationToken = (SUBSTR(TO_HEX(MD5(CAST(RAND() AS STRING))), 1, 27)) 
     WHERE validationToken is not null;"""
@@ -22,7 +24,9 @@ def anonymize_validation_token_offerer():
 
 
 def anonymize_apikey():
-    # Table Provider : Génération aléatoire d'une apiKey anonymisée (si la valeur est non nulle)
+    """
+    Table provider : Génération aléatoire d'une apiKey anonymisée (si la valeur est non nulle)
+    """
     update_apikey = f"""UPDATE poc_data_federated_query.provider 
     SET apiKey = (SUBSTR(TO_HEX(MD5(CAST(RAND() AS STRING))), 1,32)) 
     WHERE apiKey is not null;"""
@@ -31,7 +35,9 @@ def anonymize_apikey():
 
 
 def anonymize_firstname():
-    # Table user : Remplacement du prénom par firstName || id
+    """
+    Table user : Remplacement du prénom par firstName || id
+    """
     update_firstname = f"""UPDATE poc_data_federated_query.user 
     SET  firstName = 'firstName' || id 
     WHERE firstName IS NOT NULL;"""
@@ -40,7 +46,9 @@ def anonymize_firstname():
 
 
 def anonymize_lastname():
-    # Table user : Remplacement du nom de famille par lastName || id
+    """
+    Table user : Remplacement du nom de famille par lastName || id
+    """
     update_lastname = f"""UPDATE poc_data_federated_query.user 
     SET  lastName = 'lastName' || id 
     WHERE lastName IS NOT NULL;"""
@@ -49,7 +57,9 @@ def anonymize_lastname():
 
 
 def anonymize_dateofbirth():
-    # Table user : Attribution arbitraire de la date de naissance 01/01/2001 pour tous les utilisateurs
+    """
+    Table user : Attribution arbitraire de la date de naissance 01/01/2001 pour tous les utilisateurs
+    """
     update_dateofbirth = f"""UPDATE poc_data_federated_query.user 
     SET  dateOfBirth = '2001-01-01T00:00:00' 
     WHERE dateOfBirth IS NOT NULL;"""
@@ -58,7 +68,9 @@ def anonymize_dateofbirth():
 
 
 def anonymize_phonenumber():
-    # Table user : Attribution arbitraire du numéro de téléphone 0606060606 pour tous les utilisateurs
+    """
+    Table user : Attribution arbitraire du numéro de téléphone 0606060606 pour tous les utilisateurs
+    """
     update_phonenumber = f"""UPDATE poc_data_federated_query.user 
     SET  phoneNumber = '0606060606' 
     WHERE phoneNumber IS NOT NULL;"""
@@ -67,7 +79,9 @@ def anonymize_phonenumber():
 
 
 def anonymize_email():
-    # Table user : Remplacement de l'email par la concaténation de user@ et de son identifiant
+    """
+    Table user : Remplacement de l'email par la concaténation de user@ et de son identifiant
+    """
     update_email = f"""UPDATE poc_data_federated_query.user 
     SET  email = 'user@' || id 
     WHERE email IS NOT NULL;"""
@@ -76,7 +90,9 @@ def anonymize_email():
 
 
 def anonymize_publicname():
-    # Table user : Remplacement du nom d'utilisateur (publicName) par User || id,
+    """
+    Table user : Remplacement du nom d'utilisateur (publicName) par User || id
+    """
     update_publicname = f"""UPDATE poc_data_federated_query.user 
     SET  publicName = 'User' || id 
     WHERE publicName IS NOT NULL;"""
@@ -85,7 +101,9 @@ def anonymize_publicname():
 
 
 def anonymize_password():
-    # Table user : Remplacement du mot de passe par $PASSWORD || id
+    """
+    Table user : Remplacement du mot de passe par $PASSWORD || id
+    """
     update_password = f"""UPDATE poc_data_federated_query.user 
     SET  password = CAST('Password' || id AS BYTES) 
     WHERE password IS NOT NULL;"""
@@ -94,7 +112,9 @@ def anonymize_password():
 
 
 def anonymize_validation_token_user():
-    # Table user : Génération d'un token de validation aléatoire pour les rattachements en ayant un
+    """
+    Table user : Génération d'un token de validation aléatoire pour les rattachements en ayant un
+    """
     update_validation_token_user = f"""UPDATE poc_data_federated_query.user 
     SET validationToken = (SUBSTR(TO_HEX(MD5(CAST(RAND() AS STRING))), 1,27)) 
     WHERE validationToken is not null;"""
@@ -103,7 +123,9 @@ def anonymize_validation_token_user():
 
 
 def anonymize_reset_password_token():
-    # Génération d'un token de réinitialisation de mot de passe aléatoire pour les utilisateurs en ayant un
+    """
+    Génération d'un token de réinitialisation de mot de passe aléatoire pour les utilisateurs en ayant un
+    """
     update_reset_password_token = f"""UPDATE poc_data_federated_query.user 
     SET resetPasswordToken = (SUBSTR(TO_HEX(MD5(CAST(RAND() AS STRING))), 1,10)) 
     WHERE resetPasswordToken is not null;"""
@@ -112,7 +134,9 @@ def anonymize_reset_password_token():
 
 
 def anonymize_iban_bic():
-    # Table bank_information : Remplacement du BIC et IBAN par une séquence de même longueur générée aléatoirement
+    """
+    Table bank_information : Remplacement du BIC et IBAN par une séquence de même longueur générée aléatoirement
+    """
     update_iban_bic = f"""CREATE TEMPORARY FUNCTION generate_random_between(upper_limit FLOAT64, lower_limit FLOAT64) \
     RETURNS STRING 
     LANGUAGE js 
@@ -128,7 +152,9 @@ def anonymize_iban_bic():
 
 
 def anonymize_iban_payment():
-    # Table payment : Remplacement iban par une séquence de même longueur générée aléatoirement
+    """
+    Table payment : Remplacement iban par une séquence de même longueur générée aléatoirement
+    """
     update_iban_payment = f"""UPDATE poc_scalingo.public_payment  
     SET iban = 'FR7630001007941234567890185' 
     WHERE iban is not null;"""
@@ -137,7 +163,9 @@ def anonymize_iban_payment():
 
 
 def anonymize_bic_payment():
-    # Table payment : Remplacement du BIC par une séquence de même longueur générée aléatoirement
+    """
+    Table payment : Remplacement du BIC par une séquence de même longueur générée aléatoirement
+    """
     update_bic_payment = f"""UPDATE poc_scalingo.public_payment 
      SET bic = 'BDFEFR2L' WHERE bic is not null;"""
 
@@ -145,7 +173,9 @@ def anonymize_bic_payment():
 
 
 def anonymize_validation_token_user_offerer():
-    # Table user_offerer : Remplacement du BIC par une séquence de même longueur générée aléatoirement
+    """
+    Table user_offerer : Remplacement du BIC par une séquence de même longueur générée aléatoirement
+    """
     update_validation_token_user_offerer = f"""UPDATE poc_data_federated_query.user_offerer 
     SET validationToken = (SUBSTR(TO_HEX(MD5(CAST(RAND() AS STRING))), 1,10)) 
     WHERE validationToken is not null;"""
@@ -154,7 +184,9 @@ def anonymize_validation_token_user_offerer():
 
 
 def anonymize_token():
-    # Table booking : Génération d'une fausse contremarque à partir de l'identifiant du booking
+    """
+    Table booking : Génération d'une fausse contremarque à partir de l'identifiant du booking
+    """
     update_token = f"""UPDATE poc_data_federated_query.booking 
     SET token = UPPER(RIGHT(CAST(id AS STRING), 6)) 
     WHERE token is not null;"""
@@ -163,7 +195,9 @@ def anonymize_token():
 
 
 def anonymize_validation_token_venue():
-    # Table venue : Génération aléatoire d'un token de validation
+    """
+    Table venue : Génération aléatoire d'un token de validation
+    """
     update_validation_token_venue = f"""UPDATE poc_data_federated_query.venue 
     SET validationToken = (SUBSTR(TO_HEX(MD5(CAST(RAND() AS STRING))), 1,27)) 
     WHERE validationToken is not null;"""

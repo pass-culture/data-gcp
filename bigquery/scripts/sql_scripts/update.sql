5a4cecf6ec25469d9ce21cb5f4227da8dc887e47
FerdiHr <ferdi.harmel@gmail.com>
2020-11-06 10:48:19 +0100

    (PC-4976) change update field

--- bigquery/scripts/sql_scripts/update.sql
+++ bigquery/scripts/sql_scripts/update.sql
@@ -1,3 +1,3 @@
-UPDATE target_offer SET "dateModifiedAtLastProvider" = NOW() WHERE id IN (SELECT id
+UPDATE target_offer SET "lastupdate" = NOW() WHERE id IN (SELECT id
 FROM target_offer
 ORDER BY random() LIMIT 10000000); 

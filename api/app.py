07d8a27eb5fde75d72bfe7e1832eebe9407199e2
Romain Chaffal <romain.chaffal@passculture.app>
2020-11-12 15:17:18 +0100

    (PC-4886): check token before sending result

--- api/app.py
+++ api/app.py
@@ -1,6 +1,9 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+
+
+API_TOKEN = os.environ.get("API_TOKEN")
 
 app = Flask(__name__)
 
@@ -13,6 +16,16 @@ def hello_world():
 @app.route("/check")
 def check():
     return "OK"
+
+
+@app.route("/get_recommendations")
+def get_recommendations():
+    token = request.args.get("token", None)
+
+    if token != API_TOKEN:
+        return "Forbidden", 403
+
+    return "Ok", 200
 
 
 if __name__ == "__main__":
--- api/tests/api_test.py
+++ api/tests/api_test.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from api.app import app
 
 
@@ -19,3 +21,29 @@ def test_check():
     # Then
     assert response.status_code == 200
     assert response.data == b"OK"
+
+
+class GetRecommendations:
+    @patch("api.app.API_TOKEN", "good_token")
+    def test_get_recommendations_return_403_when_there_is_wrong_token(self):
+        # Given
+        api_token = "wrong_token"
+
+        # When
+        response = app.test_client().get(f"/get_recommendations?token={api_token}")
+
+        # Then
+        assert response.status_code == 403
+        assert response.data == b"Forbidden"
+
+    @patch("api.app.API_TOKEN", "good_token")
+    def test_get_recommendations_return_200_when_there_is_right_token(self):
+        # Given
+        api_token = "good_token"
+
+        # When
+        response = app.test_client().get(f"/get_recommendations?token={api_token}")
+
+        # Then
+        assert response.status_code == 200
+        assert response.data == b"Ok"

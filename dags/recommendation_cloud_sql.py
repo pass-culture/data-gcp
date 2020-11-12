2ebf7f073823dc303dc3b7d3ee55c54cb8b0bddf
Maxime Bourliatoux <bourliam@gmail.com>
2020-11-12 10:00:12 +0100

    (PC-5170) Apply linter

--- dags/recommendation_cloud_sql.py
+++ dags/recommendation_cloud_sql.py
@@ -3,49 +3,53 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.contrib.operators.gcp_sql_operator import CloudSqlQueryOperator, CloudSqlInstanceImportOperator
+from airflow.contrib.operators.gcp_sql_operator import (
+    CloudSqlQueryOperator,
+    CloudSqlInstanceImportOperator,
+)
 
 GCP_PROJECT_ID = "pass-culture-app-projet-test"
-BUCKET_PATH = 'gs://pass-culture-data'
-RECOMMENDATION_SQL_DUMP = 'dump_staging_for_recommendations_09_11_20.gz'
-RECOMMENDATION_SQL_INSTANCE = 'pcdata-poc-csql-recommendation'
-RECOMMENDATION_SQL_BASE = 'pcdata-poc-csql-recommendation'
-TYPE = 'postgres'
+BUCKET_PATH = "gs://pass-culture-data"
+RECOMMENDATION_SQL_DUMP = "dump_staging_for_recommendations_09_11_20.gz"
+RECOMMENDATION_SQL_INSTANCE = "pcdata-poc-csql-recommendation"
+RECOMMENDATION_SQL_BASE = "pcdata-poc-csql-recommendation"
+TYPE = "postgres"
 LOCATION = "europe-west1-d"
 
-RECOMMENDATION_SQL_USER = os.getenv('RECOMMENDATION_SQL_USER')
-RECOMMENDATION_SQL_PASSWORD = os.getenv('RECOMMENDATION_SQL_PASSWORD')
-RECOMMENDATION_SQL_PUBLIC_IP = os.getenv('RECOMMENDATION_SQL_PUBLIC_IP')
-RECOMMENDATION_SQL_PUBLIC_PORT = os.getenv('RECOMMENDATION_SQL_PUBLIC_PORT')
+RECOMMENDATION_SQL_USER = os.getenv("RECOMMENDATION_SQL_USER")
+RECOMMENDATION_SQL_PASSWORD = os.getenv("RECOMMENDATION_SQL_PASSWORD")
+RECOMMENDATION_SQL_PUBLIC_IP = os.getenv("RECOMMENDATION_SQL_PUBLIC_IP")
+RECOMMENDATION_SQL_PUBLIC_PORT = os.getenv("RECOMMENDATION_SQL_PUBLIC_PORT")
 
-TABLES = ['booking', 'offer', 'iris_venues', 'stock', 'mediation', 'venue', 'offerer']
-VIEWS = ['recommendable_offers']
+TABLES = ["booking", "offer", "iris_venues", "stock", "mediation", "venue", "offerer"]
+VIEWS = ["recommendable_offers"]
 
-os.environ['AIRFLOW_CONN_PROXY_POSTGRES_TCP'] = \
-    f"gcpcloudsql://{RECOMMENDATION_SQL_USER}:{RECOMMENDATION_SQL_PASSWORD}@{RECOMMENDATION_SQL_PUBLIC_IP}:{RECOMMENDATION_SQL_PUBLIC_PORT}/{RECOMMENDATION_SQL_BASE}?" \
-    f"database_type={TYPE}&" \
-    f"project_id={GCP_PROJECT_ID}&" \
-    f"location={LOCATION}&" \
-    f"instance={RECOMMENDATION_SQL_INSTANCE}&" \
-    f"use_proxy=True&" \
+os.environ["AIRFLOW_CONN_PROXY_POSTGRES_TCP"] = (
+    f"gcpcloudsql://{RECOMMENDATION_SQL_USER}:{RECOMMENDATION_SQL_PASSWORD}@{RECOMMENDATION_SQL_PUBLIC_IP}:{RECOMMENDATION_SQL_PUBLIC_PORT}/{RECOMMENDATION_SQL_BASE}?"
+    f"database_type={TYPE}&"
+    f"project_id={GCP_PROJECT_ID}&"
+    f"location={LOCATION}&"
+    f"instance={RECOMMENDATION_SQL_INSTANCE}&"
+    f"use_proxy=True&"
     f"sql_proxy_use_tcp=True"
+)
 
 default_args = {
-    'start_date': datetime(2020, 11, 10),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    'catchup': False,
+    "start_date": datetime(2020, 11, 10),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "catchup": False,
 }
 
 with DAG(
-    'recommendation_cloud_sql_v5',
+    "recommendation_cloud_sql_v5",
     default_args=default_args,
-    description='Restore postgres dumps to Cloud SQL',
-    schedule_interval='@daily',
-    dagrun_timeout=timedelta(minutes=90)
+    description="Restore postgres dumps to Cloud SQL",
+    schedule_interval="@daily",
+    dagrun_timeout=timedelta(minutes=90),
 ) as dag:
 
-    start = DummyOperator(task_id='start')
+    start = DummyOperator(task_id="start")
 
     drop_table_tasks = []
 
@@ -54,7 +58,7 @@ with DAG(
             gcp_cloudsql_conn_id="proxy_postgres_tcp",
             task_id=f"drop_table_public_{table}",
             sql=f"DROP TABLE IF EXISTS public.{table}",
-            autocommit=True
+            autocommit=True,
         )
         drop_table_tasks.append(task)
 
@@ -62,7 +66,7 @@ with DAG(
         "importContext": {
             "fileType": "sql",
             "uri": f"{BUCKET_PATH}/{RECOMMENDATION_SQL_DUMP}",
-            "database": RECOMMENDATION_SQL_BASE
+            "database": RECOMMENDATION_SQL_BASE,
         }
     }
 
@@ -70,7 +74,7 @@ with DAG(
         project_id=GCP_PROJECT_ID,
         body=import_body,
         instance=RECOMMENDATION_SQL_INSTANCE,
-        task_id='sql_restore_task'
+        task_id="sql_restore_task",
     )
 
     recreate_indexes_query = """
@@ -91,7 +95,7 @@ with DAG(
         gcp_cloudsql_conn_id="proxy_postgres_tcp",
         task_id="recreate_indexes",
         sql=recreate_indexes_query,
-        autocommit=True
+        autocommit=True,
     )
 
     refresh_materialized_view_tasks = []
@@ -101,10 +105,10 @@ with DAG(
             gcp_cloudsql_conn_id="proxy_postgres_tcp",
             task_id=f"refresh_materialized_view_{view}",
             sql=f"REFRESH MATERIALIZED VIEW CONCURRENTLY {view}",
-            autocommit=True
+            autocommit=True,
         )
         refresh_materialized_view_tasks.append(task)
 
-    end = DummyOperator(task_id='end')
+    end = DummyOperator(task_id="end")
 
     start >> drop_table_tasks >> sql_restore_task >> recreate_indexes_task >> refresh_materialized_view_tasks >> end

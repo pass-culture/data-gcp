cf51cabdd5535ef3fd9e08f339692445d85aef15
emnak <emnak@sicara.com>
2020-11-13 10:39:01 +0100

    [PC-5171] Update api tests

--- api/tests/api_test.py
+++ api/tests/api_test.py
@@ -3,16 +3,6 @@ from unittest.mock import patch
 from api.app import app
 
 
-def test_root():
-    # Given
-    # When
-    response = app.test_client().get("/")
-
-    # Then
-    assert response.status_code == 200
-    assert b"Recommandation" in response.data
-
-
 def test_check():
     # Given
     # When
@@ -23,27 +13,31 @@ def test_check():
     assert response.data == b"OK"
 
 
-class GetRecommendations:
-    @patch("api.app.API_TOKEN", "good_token")
-    def test_get_recommendations_return_403_when_there_is_wrong_token(self):
-        # Given
-        api_token = "wrong_token"
+@patch("api.app.API_TOKEN", "good_token")
+def test_recommendation_return_403_when_there_is_wrong_token():
+    # Given
+    api_token = "wrong_token"
+    user_id = 1
 
-        # When
-        response = app.test_client().get(f"/get_recommendations?token={api_token}")
+    # When
+    response = app.test_client().get(f"/recommendation/{user_id}?token={api_token}")
 
-        # Then
-        assert response.status_code == 403
-        assert response.data == b"Forbidden"
+    # Then
+    assert response.status_code == 403
+    assert response.data == b"Forbidden"
 
-    @patch("api.app.API_TOKEN", "good_token")
-    def test_get_recommendations_return_200_when_there_is_right_token(self):
-        # Given
-        api_token = "good_token"
 
-        # When
-        response = app.test_client().get(f"/get_recommendations?token={api_token}")
+@patch("api.app.API_TOKEN", "good_token")
+@patch("api.app.get_recommendations_for_user")
+def test_recommendation_return_recommended_offeres_when_there_is_right_token(get_recommendations_for_user_mock):
+    # Given
+    api_token = "good_token"
+    user_id = 1
+    get_recommendations_for_user_mock.return_value = [1, 2, 3]
 
-        # Then
-        assert response.status_code == 200
-        assert response.data == b"Ok"
+    # When
+    response = app.test_client().get(f"/recommendation/{user_id}?token={api_token}")
+
+    # Then
+    assert response.status_code == 200
+    assert response.data == b'{"recommended offers ":[1,2,3]}\n'

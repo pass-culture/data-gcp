f894393d5c9d8d369b4c13cab29e6b38a30a7bd7
emnak <46672169+emnak@users.noreply.github.com>
2020-11-17 18:35:46 +0100

    Pc 5216 test cloud composer (#35)
    * [PC-5216] Add dag tests
    
    * [PC-5216] Update dependencies
    
    * [PC-5216] Change airflow version

---
+++ dags/tests/dags_test.py
@@ -0,0 +1,53 @@
+import unittest
+
+from airflow.models import DagBag
+
+
+class TestDags(unittest.TestCase):
+
+    LOAD_SECOND_THRESHOLD = 2
+
+    def setUp(self):
+        self.dagbag = DagBag(include_examples=False)
+
+    def test_dag_import_no_error(self):
+        # Then
+        self.assertFalse(
+            len(self.dagbag.import_errors),
+            "There should be no DAG failures. Got: {}".format(
+                self.dagbag.import_errors
+            ),
+        )
+
+    def test_dag_import_is_fast(self):
+        # Given
+        stats = self.dagbag.dagbag_stats
+        slow_files = filter(lambda d: d.duration > self.LOAD_SECOND_THRESHOLD, stats)
+        res = ", ".join(map(lambda d: d.file[1:], slow_files))
+
+        # When / Then
+        self.assertEqual(
+            0,
+            len(list(slow_files)),
+            "The following files take more than {threshold}s to load: {res}".format(
+                threshold=self.LOAD_SECOND_THRESHOLD, res=res
+            ),
+        )
+
+    def test_restore_data_analytics_dag_is_loaded(self):
+        # When
+        dag = self.dagbag.get_dag(dag_id="restore_data_analytics_v1")
+
+        # Then
+        self.assertDictEqual(self.dagbag.import_errors, {})
+        self.assertIsNotNone(dag)
+        self.assertEqual(len(dag.tasks), 21)
+
+    def test_recommendation_cloud_sql_dag_is_loaded(self):
+        # When
+        dag = self.dagbag.get_dag(dag_id="recommendation_cloud_sql_v6")
+
+        # Then
+        self.assertDictEqual(self.dagbag.import_errors, {})
+        self.assertIsNotNone(dag)
+        self.assertEqual(len(dag.tasks), 13)

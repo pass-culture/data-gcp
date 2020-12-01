b2ab663866c219d43e96def52057971550ceced4
florianc-ds <40148089+florianc-ds@users.noreply.github.com>
2020-12-01 18:35:22 +0100

    (PC-5047) Propose a testing strategy for BigQuery testing (#39)
    * (PC-5047) Propose a testing strategy for bigquery testing
    
    * (PC-5047) Move bigquery utils functions to a specific file
    
    * (PC-5047) Move bigquery tests to analytics folder
    
    * (PC-5047) Ignore venv dir
    
    * (PC-5047) Clean with black

---
+++ analytics/tests/test_queries.py
@@ -0,0 +1,70 @@
+import pytest
+from google.cloud import bigquery
+
+from analytics.tests.queries_to_be_tested import (
+    define_simple_query_1,
+    define_simple_query_2,
+)
+from analytics.tests.config import TEST_DATASET, GCP_PROJECT
+from analytics.tests.data import (
+    SIMPLE_TABLE_1_INPUT,
+    SIMPLE_TABLE_1_EXPECTED,
+    SIMPLE_TABLE_2_EXPECTED,
+    SIMPLE_TABLE_2_INPUT,
+)
+from analytics.tests.utils import (
+    drop_dataset,
+    create_dataset,
+    drop_table,
+    create_data,
+    run_query,
+    retrieve_data,
+)
+from set_env import set_env_vars
+
+
+@pytest.fixture(scope="module", autouse=True)
+def prepare_bigquery():
+    set_env_vars()
+    pytest.bq_client = bigquery.Client()
+    drop_dataset(client=pytest.bq_client, dataset=TEST_DATASET)
+    create_dataset(client=pytest.bq_client, dataset=TEST_DATASET)
+    yield
+    drop_dataset(client=pytest.bq_client, dataset=TEST_DATASET)
+
+
+@pytest.fixture()
+def flush_dataset():
+    yield
+    dataset_id = f"{GCP_PROJECT}.{TEST_DATASET}"
+    tables = pytest.bq_client.list_tables(dataset_id)
+    for table in tables:
+        drop_table(client=pytest.bq_client, dataset=TEST_DATASET, table=table.table_id)
+
+
+@pytest.mark.parametrize(
+    ["table_name", "query", "input_data", "expected"],
+    [
+        (
+            "simple_table_1",
+            define_simple_query_1(dataset=TEST_DATASET),
+            SIMPLE_TABLE_1_INPUT,
+            SIMPLE_TABLE_1_EXPECTED,
+        ),
+        (
+            "simple_table_2",
+            define_simple_query_2(dataset=TEST_DATASET),
+            SIMPLE_TABLE_2_INPUT,
+            SIMPLE_TABLE_2_EXPECTED,
+        ),
+    ],
+)
+def test_create_queries(flush_dataset, table_name, query, input_data, expected):
+    create_data(client=pytest.bq_client, dataset=TEST_DATASET, data=input_data)
+    run_query(client=pytest.bq_client, query=query)
+    output = retrieve_data(
+        client=pytest.bq_client, dataset=TEST_DATASET, table=table_name
+    )
+    assert sorted(output, key=lambda d: d["id"]) == sorted(
+        expected, key=lambda d: d["id"]
+    )

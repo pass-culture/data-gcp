c10cc7dd910eef0f53e1a5ab5c9717a28098b86e
Jean <jeanr@sicara.com>
2020-12-08 11:57:12 +0100

    (PC-5650): fetch all

--- recommendation/api/tests/recommendation_test.py
+++ recommendation/api/tests/recommendation_test.py
@@ -67,7 +67,7 @@ def test_get_recommendation_for_user(setup_database: Tuple[Any, Any]):
     user_iris_id = 1
     number_of_recommendations = 10
     user_recommendation = get_recommendations_for_user(
-        user_id, user_iris_id, number_of_recommendations, connection
+        user_id, user_iris_id, connection
     )
 
     # Then
@@ -93,7 +93,7 @@ def test_get_recommendation_for_user_with_no_iris(setup_database: Tuple[Any, Any
     user_iris_id = None
     number_of_recommendations = 10
     user_recommendation = get_recommendations_for_user(
-        user_id, user_iris_id, number_of_recommendations, connection
+        user_id, user_iris_id, connection
     )
 
     # Then

9d6dd5d7a65428d1c25b3b86fc1a01dec401c3d3
Célia BARRANGER <c.barranger@groupeonepoint.com>
2020-12-09 18:54:18 +0100

    (PC-5606) update import table

--- orchestration/dags/dependencies/data_analytics/import_tables.py
+++ orchestration/dags/dependencies/data_analytics/import_tables.py
@@ -52,6 +52,72 @@ def define_import_query(table, region=GCP_REGION, cloudsql_database=CLOUDSQL_DAT
             CAST("id" AS varchar(255)), "paymentId", "date", CAST("status" AS varchar(255)), "detail"
         FROM public.payment_status
     """
+    cloudsql_queries[
+        "booking"
+    ] = """
+        SELECT
+            "isActive", CAST("id" AS varchar(255)), "dateCreated", "recommendationId", "stockId", "quantity", 
+            "token", "userId", "amount", "isCancelled", "isUsed", "dateUsed", "cancellationDate" ,"lastupdate"
+        FROM public.booking
+    """
+    cloudsql_queries[
+        "offer"
+    ] = """
+        SELECT
+            "idAtProviders", "dateModifiedAtLastProvider", CAST("id" AS varchar(255)), "dateCreated", "productId",
+            "venueId", "lastProviderId", "bookingEmail", "isActive", "type",	"name",	"description",
+            "conditions", "ageMin", "ageMax", "url", "mediaUrls", "durationMinutes", "isNational" ,
+            "extraData", "isDuo", "fieldsUpdated", "withdrawalDetails", "lastupdate"
+        FROM public.offer
+    """
+    cloudsql_queries[
+        "stock"
+    ] = """
+        SELECT
+            "idAtProviders", "dateModifiedAtLastProvider", CAST("id" AS varchar(255)), "dateModified", "price", "quantity",
+            "bookingLimitDatetime", "lastProviderId", "offerId", "isSoftDeleted", "beginningDatetime",
+            "dateCreated", "fieldsUpdated", "hasBeenMigrated", "lastupdate" 
+        FROM public.stock
+    """
+    cloudsql_queries[
+        "venue"
+    ] = """
+        SELECT
+            "thumbCount", "firstThumbDominantColor", "idAtProviders", "dateModifiedAtLastProvider", "address", "postalCode",
+            "city", CAST("id" AS varchar(255)) , "name", "siret", "departementCode", "latitude", "longitude", "managingOffererId", "bookingEmail",
+            "lastProviderId", "isVirtual", "comment", "validationToken", "publicName", "fieldsUpdated", "venueTypeId",
+            "venueLabelId", "dateCreated", "lastupdate"  
+        FROM public.venue
+    """
+    cloudsql_queries[
+        "offerer"
+    ] = """
+        SELECT
+            "isActive", "thumbCount", "firstThumbDominantColor", "idAtProviders", "dateModifiedAtLastProvider", "address",
+            "postalCode", "city", "validationToken", CAST("id" AS varchar(255)), "dateCreated", "name", "siren", "lastProviderId", "fieldsUpdated","lastupdate"  
+        FROM public.offerer
+    """
+    cloudsql_queries[
+        "provider"
+    ] = """
+        SELECT
+            "isActive", CAST("id" AS varchar(255)), "name", "localClass", "apiKey", "apiKeyGenerationDate", "enabledForPro", "requireProviderIdentifier","lastupdate"
+        FROM public.provider
+    """
+    cloudsql_queries[
+        "venue_type"
+    ] = """
+        SELECT
+            CAST("id" AS varchar(255)), "label", "lastupdate" 
+        FROM public.venue_type
+    """
+    cloudsql_queries[
+        "venue_label"
+    ] = """
+        SELECT
+            CAST("id" AS varchar(255)), "label", "lastupdate" 
+        FROM public.venue_label
+    """
 
     # Build specific federated queries
     queries = {}

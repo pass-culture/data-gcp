ca915c8060dcd04efbbbaf197c5a572615456cd7
celiabarranger <72383863+celiabarranger@users.noreply.github.com>
2020-12-10 16:42:54 +0100

    Pc 5606 metabase bigquery (#58)
    * (PC-5606) convert ID to string
    
    * (PC-5606) update comment
    
    * (PC-5606) update import table
    
    Co-authored-by: Célia BARRANGER <c.barranger@groupeonepoint.com>

--- orchestration/dags/dependencies/data_analytics/import_tables.py
+++ orchestration/dags/dependencies/data_analytics/import_tables.py
@@ -8,13 +8,13 @@ def define_import_query(table, region=GCP_REGION, cloudsql_database=CLOUDSQL_DAT
     In order to handle type incompatibility between postgresql and BigQuery (eg. UUID and custom types),
     we sometimes have to explicitly select and cast columns.
     """
-    # Define select-queries for tables that need a specific CAST
+    # Define select-queries for tables that need a specific CAST and ID for metabase
     cloudsql_queries = {}
     cloudsql_queries[
         "user"
     ] = """
         SELECT
-            "id", "validationToken", "email", "password", "publicName", "dateCreated", "departementCode",
+            CAST("id" AS varchar(255)), "validationToken", "email", "password", "publicName", "dateCreated", "departementCode",
             "canBookFreeOffers", "isAdmin", "resetPasswordToken", "resetPasswordTokenValidityLimit",
             "firstName", "lastName", "postalCode", "phoneNumber", "dateOfBirth", "needsToFillCulturalSurvey",
             CAST("culturalSurveyId" AS varchar(255)), "civility", "activity", "culturalSurveyFilledDate",
@@ -25,14 +25,14 @@ def define_import_query(table, region=GCP_REGION, cloudsql_database=CLOUDSQL_DAT
         "user_offerer"
     ] = """
         SELECT
-            "id", "userId", "offererId", CAST("rights" AS varchar(255)), "validationToken"
+            CAST("id" AS varchar(255)), "userId", "offererId", CAST("rights" AS varchar(255)), "validationToken"
         FROM public.user_offerer
     """
     cloudsql_queries[
         "bank_information"
     ] = """
         SELECT
-            "id", "offererId", "venueId", "iban", "bic", "applicationId", "dateModified",
+            CAST("id" AS varchar(255)), "offererId", "venueId", "iban", "bic", "applicationId", "dateModified",
             CAST("status" AS varchar(255))
         FROM public.bank_information
     """
@@ -40,7 +40,7 @@ def define_import_query(table, region=GCP_REGION, cloudsql_database=CLOUDSQL_DAT
         "payment"
     ] = """
         SELECT
-            "id", "author", "comment", "recipientName", "iban", "bic", "bookingId", "amount",
+            CAST("id" AS varchar(255)), "author", "comment", "recipientName", "iban", "bic", "bookingId", "amount",
             "reimbursementRule", CAST("transactionEndToEndId" AS varchar(255)), "recipientSiren",
             "reimbursementRate", "transactionLabel", "paymentMessageId"
         FROM public.payment
@@ -49,8 +49,74 @@ def define_import_query(table, region=GCP_REGION, cloudsql_database=CLOUDSQL_DAT
         "payment_status"
     ] = """
         SELECT
-            "id", "paymentId", "date", CAST("status" AS varchar(255)), "detail"
+            CAST("id" AS varchar(255)), "paymentId", "date", CAST("status" AS varchar(255)), "detail"
         FROM public.payment_status
+    """
+    cloudsql_queries[
+        "booking"
+    ] = """
+        SELECT
+            "isActive", CAST("id" AS varchar(255)), "dateCreated", "recommendationId", "stockId", "quantity", 
+            "token", "userId", "amount", "isCancelled", "isUsed", "dateUsed", "cancellationDate" ,"lastupdate"
+        FROM public.booking
+    """
+    cloudsql_queries[
+        "offer"
+    ] = """
+        SELECT
+            "idAtProviders", "dateModifiedAtLastProvider", CAST("id" AS varchar(255)), "dateCreated", "productId",
+            "venueId", "lastProviderId", "bookingEmail", "isActive", "type",	"name",	"description",
+            "conditions", "ageMin", "ageMax", "url", "mediaUrls", "durationMinutes", "isNational" ,
+            "extraData", "isDuo", "fieldsUpdated", "withdrawalDetails", "lastupdate"
+        FROM public.offer
+    """
+    cloudsql_queries[
+        "stock"
+    ] = """
+        SELECT
+            "idAtProviders", "dateModifiedAtLastProvider", CAST("id" AS varchar(255)), "dateModified", "price", "quantity",
+            "bookingLimitDatetime", "lastProviderId", "offerId", "isSoftDeleted", "beginningDatetime",
+            "dateCreated", "fieldsUpdated", "hasBeenMigrated", "lastupdate" 
+        FROM public.stock
+    """
+    cloudsql_queries[
+        "venue"
+    ] = """
+        SELECT
+            "thumbCount", "firstThumbDominantColor", "idAtProviders", "dateModifiedAtLastProvider", "address", "postalCode",
+            "city", CAST("id" AS varchar(255)) , "name", "siret", "departementCode", "latitude", "longitude", "managingOffererId", "bookingEmail",
+            "lastProviderId", "isVirtual", "comment", "validationToken", "publicName", "fieldsUpdated", "venueTypeId",
+            "venueLabelId", "dateCreated", "lastupdate"  
+        FROM public.venue
+    """
+    cloudsql_queries[
+        "offerer"
+    ] = """
+        SELECT
+            "isActive", "thumbCount", "firstThumbDominantColor", "idAtProviders", "dateModifiedAtLastProvider", "address",
+            "postalCode", "city", "validationToken", CAST("id" AS varchar(255)), "dateCreated", "name", "siren", "lastProviderId", "fieldsUpdated","lastupdate"  
+        FROM public.offerer
+    """
+    cloudsql_queries[
+        "provider"
+    ] = """
+        SELECT
+            "isActive", CAST("id" AS varchar(255)), "name", "localClass", "apiKey", "apiKeyGenerationDate", "enabledForPro", "requireProviderIdentifier","lastupdate"
+        FROM public.provider
+    """
+    cloudsql_queries[
+        "venue_type"
+    ] = """
+        SELECT
+            CAST("id" AS varchar(255)), "label", "lastupdate" 
+        FROM public.venue_type
+    """
+    cloudsql_queries[
+        "venue_label"
+    ] = """
+        SELECT
+            CAST("id" AS varchar(255)), "label", "lastupdate" 
+        FROM public.venue_label
     """
 
     # Build specific federated queries

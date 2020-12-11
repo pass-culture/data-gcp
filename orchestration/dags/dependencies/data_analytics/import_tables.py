e8270bac741a380b47cfbc95a6b578f1faa9ee4d
celiabarranger <72383863+celiabarranger@users.noreply.github.com>
2020-12-11 17:56:16 +0100

    convert all id to string (#61)
    Co-authored-by: Célia BARRANGER <c.barranger@groupeonepoint.com>

--- orchestration/dags/dependencies/data_analytics/import_tables.py
+++ orchestration/dags/dependencies/data_analytics/import_tables.py
@@ -25,14 +25,14 @@ def define_import_query(table, region=GCP_REGION, cloudsql_database=CLOUDSQL_DAT
         "user_offerer"
     ] = """
         SELECT
-            CAST("id" AS varchar(255)), "userId", "offererId", CAST("rights" AS varchar(255)), "validationToken"
+            CAST("id" AS varchar(255)), CAST("userId" AS varchar(255)), CAST("offererId" AS varchar(255)), CAST("rights" AS varchar(255)), "validationToken"
         FROM public.user_offerer
     """
     cloudsql_queries[
         "bank_information"
     ] = """
         SELECT
-            CAST("id" AS varchar(255)), "offererId", "venueId", "iban", "bic", "applicationId", "dateModified",
+            CAST("id" AS varchar(255)), CAST("offererId" AS varchar(255)), CAST("venueId" AS varchar(255)), "iban", "bic", CAST("applicationId" AS varchar(255)), "dateModified",
             CAST("status" AS varchar(255))
         FROM public.bank_information
     """
@@ -40,7 +40,7 @@ def define_import_query(table, region=GCP_REGION, cloudsql_database=CLOUDSQL_DAT
         "payment"
     ] = """
         SELECT
-            CAST("id" AS varchar(255)), "author", "comment", "recipientName", "iban", "bic", "bookingId", "amount",
+            CAST("id" AS varchar(255)), "author", "comment", "recipientName", "iban", "bic", CAST("bookingId" AS varchar(255)), "amount",
             "reimbursementRule", CAST("transactionEndToEndId" AS varchar(255)), "recipientSiren",
             "reimbursementRate", "transactionLabel", "paymentMessageId"
         FROM public.payment
@@ -49,23 +49,23 @@ def define_import_query(table, region=GCP_REGION, cloudsql_database=CLOUDSQL_DAT
         "payment_status"
     ] = """
         SELECT
-            CAST("id" AS varchar(255)), "paymentId", "date", CAST("status" AS varchar(255)), "detail"
+            CAST("id" AS varchar(255)), CAST("paymentId" AS varchar(255)), "date", CAST("status" AS varchar(255)), "detail"
         FROM public.payment_status
     """
     cloudsql_queries[
         "booking"
     ] = """
         SELECT
-            "isActive", CAST("id" AS varchar(255)), "dateCreated", "recommendationId", "stockId", "quantity", 
-            "token", "userId", "amount", "isCancelled", "isUsed", "dateUsed", "cancellationDate" ,"lastupdate"
+            "isActive", CAST("id" AS varchar(255)), "dateCreated", CAST("recommendationId" AS varchar(255)), CAST("stockId" AS varchar(255)), "quantity", 
+            "token", CAST("userId" AS varchar(255)), "amount", "isCancelled", "isUsed", "dateUsed", "cancellationDate" ,"lastupdate"
         FROM public.booking
     """
     cloudsql_queries[
         "offer"
     ] = """
         SELECT
-            "idAtProviders", "dateModifiedAtLastProvider", CAST("id" AS varchar(255)), "dateCreated", "productId",
-            "venueId", "lastProviderId", "bookingEmail", "isActive", "type",	"name",	"description",
+            CAST("idAtProviders" AS varchar(255)), "dateModifiedAtLastProvider", CAST("id" AS varchar(255)), "dateCreated", CAST("productId" AS varchar(255)),
+            CAST("venueId" AS varchar(255)), CAST("lastProviderId" AS varchar(255)), "bookingEmail", "isActive", "type", "name", "description",
             "conditions", "ageMin", "ageMax", "url", "mediaUrls", "durationMinutes", "isNational" ,
             "extraData", "isDuo", "fieldsUpdated", "withdrawalDetails", "lastupdate"
         FROM public.offer
@@ -74,8 +74,8 @@ def define_import_query(table, region=GCP_REGION, cloudsql_database=CLOUDSQL_DAT
         "stock"
     ] = """
         SELECT
-            "idAtProviders", "dateModifiedAtLastProvider", CAST("id" AS varchar(255)), "dateModified", "price", "quantity",
-            "bookingLimitDatetime", "lastProviderId", "offerId", "isSoftDeleted", "beginningDatetime",
+            CAST("idAtProviders" AS varchar(255)), "dateModifiedAtLastProvider", CAST("id" AS varchar(255)), "dateModified", "price", "quantity",
+            "bookingLimitDatetime", CAST("lastProviderId" AS varchar(255)), CAST("offerId" AS varchar(255)), "isSoftDeleted", "beginningDatetime",
             "dateCreated", "fieldsUpdated", "hasBeenMigrated", "lastupdate" 
         FROM public.stock
     """
@@ -84,17 +84,17 @@ def define_import_query(table, region=GCP_REGION, cloudsql_database=CLOUDSQL_DAT
     ] = """
         SELECT
             "thumbCount", "firstThumbDominantColor", "idAtProviders", "dateModifiedAtLastProvider", "address", "postalCode",
-            "city", CAST("id" AS varchar(255)) , "name", "siret", "departementCode", "latitude", "longitude", "managingOffererId", "bookingEmail",
-            "lastProviderId", "isVirtual", "comment", "validationToken", "publicName", "fieldsUpdated", "venueTypeId",
-            "venueLabelId", "dateCreated", "lastupdate"  
+            "city", CAST("id" AS varchar(255)) , "name", "siret", "departementCode", "latitude", "longitude", CAST("managingOffererId" AS varchar(255)), "bookingEmail",
+            CAST("lastProviderId" AS varchar(255)), "isVirtual", "comment", "validationToken", "publicName", "fieldsUpdated", CAST("venueTypeId" AS varchar(255)),
+            CAST("venueLabelId" AS varchar(255)), "dateCreated", "lastupdate"  
         FROM public.venue
     """
     cloudsql_queries[
         "offerer"
     ] = """
         SELECT
-            "isActive", "thumbCount", "firstThumbDominantColor", "idAtProviders", "dateModifiedAtLastProvider", "address",
-            "postalCode", "city", "validationToken", CAST("id" AS varchar(255)), "dateCreated", "name", "siren", "lastProviderId", "fieldsUpdated","lastupdate"  
+            "isActive", "thumbCount", "firstThumbDominantColor", CAST("idAtProviders" AS varchar(255)), "dateModifiedAtLastProvider", "address",
+            "postalCode", "city", "validationToken", CAST("id" AS varchar(255)), "dateCreated", "name", "siren", CAST("lastProviderId" AS varchar(255)), "fieldsUpdated","lastupdate"  
         FROM public.offerer
     """
     cloudsql_queries[
@@ -118,6 +118,13 @@ def define_import_query(table, region=GCP_REGION, cloudsql_database=CLOUDSQL_DAT
             CAST("id" AS varchar(255)), "label", "lastupdate" 
         FROM public.venue_label
     """
+    cloudsql_queries[
+        "favorite"
+    ] = """
+            SELECT
+                CAST("id" AS varchar(255)), CAST("userId" AS varchar(255)), CAST("offerId" AS varchar(255)), CAST("mediationId" AS varchar(255)), "lastupdate"
+            FROM public.favorite
+        """
 
     # Build specific federated queries
     queries = {}

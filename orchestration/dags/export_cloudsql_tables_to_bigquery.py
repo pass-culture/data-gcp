bf73be71a77106214f1bcc818eb3272e5d86ebf4
Jean <jeanr@sicara.com>
2020-12-16 16:12:03 +0100

    (PC-5812): orchestrate export to bigquery

--- orchestration/dags/export_cloudsql_tables_to_bigquery.py
+++ orchestration/dags/export_cloudsql_tables_to_bigquery.py
@@ -9,22 +9,31 @@ from dependencies.data_analytics.config import (
 )
 from dependencies.slack_alert import task_fail_slack_alert
 
+yesterday = (datetime.datetime.now() + datetime.timedelta(days=-1)).strftime(
+    "%Y-%m-%d"
+) + " 00:00:00"
 CONNECTION_ID = "europe-west1.cloud_SQL_pcdata-poc-csql-recommendation"
-AB_TESTING_TABLES = ["ab_testing_20201207"]
+TABLES = {
+    "ab_testing_20201207": {"query": None, "write_disposition": "WRITE_TRUNCATE"},
+    "past_recommended_offers": {
+        "query": f"SELECT * FROM public.past_recommended_offers where date <= '{yesterday}'",
+        "write_disposition": "WRITE_APPEND",
+    },
+}
 BIGQUERY_AB_TESTING_DATASET = "algo_reco_kpi_data"
 
 default_dag_args = {
     "on_failure_callback": task_fail_slack_alert,
-    "start_date": datetime.datetime(2020, 12, 7),
+    "start_date": datetime.datetime(2020, 12, 15),
     "retries": 1,
     "retry_delay": datetime.timedelta(minutes=5),
     "project_id": GCP_PROJECT_ID,
 }
 
 dag = DAG(
-    "export_ab_testing_tables_v4",
+    "export_cloudsql_tables_to_bigquery_v1",
     default_args=default_dag_args,
-    description="Export A/B testing tables from CloudSQL to BigQuery",
+    description="Export tables from CloudSQL to BigQuery",
     schedule_interval="@daily",
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=90),
@@ -33,11 +42,14 @@ dag = DAG(
 start = DummyOperator(task_id="start", dag=dag)
 
 export_table_tasks = []
-for table in AB_TESTING_TABLES:
+for table in TABLES:
+    query = TABLES[table]["query"]
+    if query is None:
+        query = f"SELECT * FROM public.{table}"
     task = BigQueryOperator(
         task_id=f"import_{table}",
-        sql=f"SELECT * FROM EXTERNAL_QUERY('{CONNECTION_ID}', 'SELECT * FROM public.{table}');",
-        write_disposition="WRITE_TRUNCATE",
+        sql=f'SELECT * FROM EXTERNAL_QUERY("{CONNECTION_ID}", "{query}");',
+        write_disposition=TABLES[table]["write_disposition"],
         use_legacy_sql=False,
         destination_dataset_table=f"{BIGQUERY_AB_TESTING_DATASET}.{table}",
         dag=dag,

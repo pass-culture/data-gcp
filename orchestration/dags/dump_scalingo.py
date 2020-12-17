2cbbc2504fb9a47722328287a725e75624a46901
FerdiHr <ferdi.harmel@gmail.com>
2020-12-17 15:37:42 +0100

    (PC-5262) csv could not be imported

--- orchestration/dags/dump_scalingo.py
+++ orchestration/dags/dump_scalingo.py
@@ -1,3 +1,5 @@
+import os
+import ast
 from datetime import datetime, timedelta
 
 import airflow
@@ -6,90 +8,84 @@ from airflow.contrib.hooks.ssh_hook import SSHHook
 from airflow.contrib.operators.postgres_to_gcs_operator import (
     PostgresToGoogleCloudStorageOperator,
 )
+from airflow.contrib.operators.gcp_sql_operator import (
+    CloudSqlQueryOperator,
+    CloudSqlInstanceImportOperator,
+)
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
+#  Global variables
 GCS_BUCKET = "dump_scalingo"
+GCP_PROJECT_ID = "pass-culture-app-projet-test"
+
 TABLES = [
-    "user",
-    "provider",
+    # "user",
+    # "provider",
     "offerer",
-    "bank_information",
-    "booking",
-    "payment",
-    "venue",
-    "user_offerer",
-    "offer",
-    "stock",
-    "favorite",
-    "venue_type",
-    "venue_label",
+    # "bank_information",
+    # "booking",
+    # "payment",
+    # "venue",
+    # "user_offerer",
+    # "offer",
+    # "stock",
+    # "favorite",
+    # "venue_type",
+    # "venue_label",
 ]
 
+TESTING = ast.literal_eval(os.environ.get("TESTING"))
+LOCAL_HOST = "localhost"
+LOCAL_PORT = 10025
 
+DATABASE = "test-restore"
+INSTANCE_DATABASE = "dump-prod-8-10-2020"
+
+# Starting DAG
 default_args = {
-    "start_date": datetime(2020, 12, 3),
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
 
 dag = DAG(
-    "dump_scalingo_v1",
+    "temp_dump_prod_from_scalingo_v2",
     default_args=default_args,
+    start_date=datetime(2020, 12, 8),
     description="Dump scalingo db to cloud storage in csv format",
     schedule_interval="@daily",
     dagrun_timeout=timedelta(minutes=180),
     catchup=False,
 )
 
-# ENV TESTING to transfer in env var with PC-5263
-TESTING = {
-    "port": 34469,
-    "host": "pass-culture-407.postgresql.dbs.scalingo.com",
-    "dbname": "pass_culture_407",
-    "user": "pass_culture_407",
-    "password": "szJm55of481Jfl2LS7W3",
-}
-LOCAL_HOST = "localhost"
-LOCAL_PORT = 10025
 
 start = DummyOperator(task_id="start", dag=dag)
 
 
-def open_tunnel():
+def create_tunnel():
     # Open SSH tunnel
     ssh_hook = SSHHook(
         ssh_conn_id="ssh_scalingo",
-        keepalive_interval=120,
+        keepalive_interval=1200,
     )
     tunnel = ssh_hook.get_tunnel(
         remote_port=TESTING["port"],
         remote_host=TESTING["host"],
         local_port=LOCAL_PORT,
     )
+    return tunnel
+
+
+def query_postgresql_from_tunnel(**kwargs):
+    tunnel = create_tunnel()
     tunnel.start()
 
-    return
-
-
-open_tunnel = PythonOperator(
-    task_id="test_tunnel_conn", python_callable=open_tunnel, dag=dag
-)
-start >> open_tunnel
-last_task = open_tunnel
-
-for table in TABLES:
-    sql_query = f"select * from {table};"
-
-    # File path and name.
-    now = datetime.now()
-    file_name = f"{table}/{now.year}_{now.month}_{now.day}_{table}.csv"
-
     export_table = PostgresToGoogleCloudStorageOperator(
-        task_id=f"dump_{table}",
-        sql=sql_query,
+        task_id=f"dump_{kwargs['table']}",
+        sql=kwargs["sql_query"],
         bucket=GCS_BUCKET,
-        filename=file_name,
+        schema_filename=None,
+        filename=kwargs["file_name"],
         postgres_conn_id="postgres_scalingo",
         google_cloud_storage_conn_id="google_cloud_default",
         gzip=False,
@@ -99,8 +95,63 @@ for table in TABLES:
         dag=dag,
     )
 
+    export_table.execute(context=kwargs)
+    tunnel.stop()
+    return
+
+
+last_task = start
+
+for table in TABLES:
+    sql_query = f"select * from {table};"
+
+    # File path and name.
+    now = datetime.now()
+    file_name = f"{table}/{now.year}_{now.month}_{now.day}_{table}.csv"
+
+    export_table = PythonOperator(
+        task_id=f"query_{table}",
+        python_callable=query_postgresql_from_tunnel,
+        op_kwargs={"table": table, "sql_query": sql_query, "file_name": file_name},
+        dag=dag,
+    )
+
     last_task >> export_table
     last_task = export_table
+
+
+for table in TABLES:
+    # File path and name.
+    now = datetime.now()
+    file_name = f"{table}/{now.year}_{now.month}_{now.day}_{table}.csv"
+
+    drop_table_task = CloudSqlQueryOperator(
+        gcp_cloudsql_conn_id="test_cloudsql",
+        task_id=f"drop_table_public_{table}",
+        sql=f"DELETE FROM public.{table};",
+        autocommit=True,
+    )
+
+    import_body = {
+        "importContext": {
+            "fileType": "CSV",
+            "csvImportOptions": {"table": f"public.{table}"},
+            "uri": f"gs://{GCS_BUCKET}/{file_name}",
+            "database": DATABASE,
+        }
+    }
+
+    sql_restore_task = CloudSqlInstanceImportOperator(
+        task_id=f"cloud_sql_restore_table_{table}",
+        project_id=GCP_PROJECT_ID,
+        body=import_body,
+        instance=INSTANCE_DATABASE,
+    )
+
+    last_task >> drop_table_task >> sql_restore_task
+    # last_task >> sql_restore_task
+    last_task = sql_restore_task
+
 
 end = DummyOperator(task_id="end", dag=dag)
 last_task >> end

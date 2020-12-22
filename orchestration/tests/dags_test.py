da128b8ba1df530ecc67e373d3dd18aa07104625
FerdiHr <ferdi.harmel@gmail.com>
2020-12-22 18:49:06 +0100

    (PC-5262) fix tasks number

--- orchestration/tests/dags_test.py
+++ orchestration/tests/dags_test.py
@@ -59,7 +59,7 @@ class TestDags(unittest.TestCase):
         # Then
         self.assertDictEqual(self.dagbag.import_errors, {})
         self.assertIsNotNone(dag)
-        self.assertEqual(len(dag.tasks), 94)
+        self.assertEqual(len(dag.tasks), 102)
 
     def test_create_ab_testing_table_dag_is_loaded(self):
         # When

572d3f9b1616d98c37aef4af5bd42a1003193c64
emnak <emnak@sicara.com>
2021-01-26 11:47:03 +0100

    [PC-6444] Use data_analytics db instead of applicative_database

--- recommendation/kpi/direct_transformations/diversification/ratio_recommendation_pct_over_offer_percentage.sql
+++ recommendation/kpi/direct_transformations/diversification/ratio_recommendation_pct_over_offer_percentage.sql
@@ -1,14 +1,14 @@
 WITH offers AS (
     SELECT
     type, COUNT(*) AS Number_of_offers, SUM(COUNT(*)) OVER () AS Total_number_of_offers, ROUND(COUNT(*) / SUM(COUNT(*)) OVER (),4) AS Percentage_of_offer_types
-    FROM `pass-culture-app-projet-test.applicative_database.offer`
+    FROM `pass-culture-app-projet-test.data_analytics.offer`
     GROUP BY type
     ORDER BY Number_of_offers DESC
 ),
 recommendations AS (
     SELECT
     type, COUNT(*) AS Number_of_recommended_offers, SUM(COUNT(*)) OVER () AS Total_number_of_recommended_offers, ROUND(COUNT(*) / SUM(COUNT(*)) OVER (),4) AS Percentage_of_recommended_offer_types
-    FROM `pass-culture-app-projet-test.applicative_database.offer` AS o
+    FROM `pass-culture-app-projet-test.data_analytics.offer` AS o
     JOIN `pass-culture-app-projet-test.algo_reco_kpi_data.past_recommended_offers` AS ro ON CAST(ro.offerid AS STRING)=o.id
     GROUP BY type
     ORDER BY Number_of_recommended_offers DESC

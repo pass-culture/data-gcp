f547cb99914241b51eca2b515da92eb2b26c19cd
Célia BARRANGER <c.barranger@groupeonepoint.com>
2021-01-27 14:18:02 +0100

    (PC-6068) update dags_test

--- orchestration/tests/dags_test.py
+++ orchestration/tests/dags_test.py
@@ -112,6 +112,7 @@ class TestDags(unittest.TestCase):
 
         # Then
         self.assertDictEqual(self.dagbag.import_errors, {})
+        self.assertIsNotNone(dag)
         self.assertEqual(len(dag.tasks), 39)
 
     def test_archive_database_dag_is_loaded(self):

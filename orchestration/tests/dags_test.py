3831e876f15d11926c74ce5a12e597c941c5d696
FerdiHr <ferdi.harmel@gmail.com>
2021-01-27 12:16:46 +0100

    (PC-6473) Update test len

--- orchestration/tests/dags_test.py
+++ orchestration/tests/dags_test.py
@@ -113,7 +113,7 @@ class TestDags(unittest.TestCase):
         # Then
         self.assertDictEqual(self.dagbag.import_errors, {})
         self.assertIsNotNone(dag)
-        self.assertEqual(len(dag.tasks), 39)
+        self.assertEqual(len(dag.tasks), 16)
 
     def test_archive_database_dag_is_loaded(self):
         # When

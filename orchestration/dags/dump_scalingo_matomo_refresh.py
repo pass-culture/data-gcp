70f747fa604d865939e87defe8fa54485414d15e
emnak <emnak@sicara.com>
2021-02-04 12:04:37 +0100

    [PC-6446] Update version and tests

--- orchestration/dags/dump_scalingo_matomo_refresh.py
+++ orchestration/dags/dump_scalingo_matomo_refresh.py
@@ -170,7 +170,7 @@ default_args = {
 }
 
 dag = DAG(
-    "dump_scalingo_matomo_refresh_v6",
+    "dump_scalingo_matomo_refresh_v7",
     default_args=default_args,
     description="Dump scalingo matomo new data to cloud storage in csv format and use it to refresh data in bigquery",
     schedule_interval="0 4 * * *",

528c6ee2960a12e428a8d63379773d02ffde55c5
Jean <jeanr@sicara.com>
2021-03-29 17:00:50 +0200

    (PC-7547): update tests

--- orchestration/tests/dags_test.py
+++ orchestration/tests/dags_test.py
@@ -89,7 +89,7 @@ class TestDags(unittest.TestCase):
         # Then
         self.assertDictEqual(self.dagbag.import_errors, {})
         self.assertIsNotNone(dag)
-        self.assertEqual(len(dag.tasks), 42)
+        self.assertEqual(len(dag.tasks), 43)
 
     def test_import_data_analytics_dag_is_loaded(self):
         # When

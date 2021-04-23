d3cb2cb894bbd6ad5982b255de3a7aa6ece2d9a8
Jean <jeanr@sicara.com>
2021-04-23 11:33:04 +0200

    (PC-8172): add qpi answers in cloudsql

--- orchestration/tests/dags_test.py
+++ orchestration/tests/dags_test.py
@@ -62,7 +62,7 @@ class TestDags(unittest.TestCase):
         # Then
         self.assertDictEqual(self.dagbag.import_errors, {})
         self.assertIsNotNone(dag)
-        self.assertEqual(len(dag.tasks), 58)
+        self.assertEqual(len(dag.tasks), 65)
 
     def test_create_ab_testing_table_dag_is_loaded(self):
         # When

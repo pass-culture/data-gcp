0c63be21445a4b1ca4ee69ee345a88a4c80933e7
Jean <jeanr@sicara.com>
2021-05-10 18:37:51 +0200

    (PC-8484): train model

---
+++ recommendation/api/scripts/metrics.py
@@ -0,0 +1,188 @@
+import numpy as np
+import tqdm
+from scipy.spatial.distance import cosine
+
+
+TYPE_LIST = [
+    "ThingType.LIVRE_EDITION",
+    "ThingType.INSTRUMENT",
+    "EventType.PRATIQUE_ARTISTIQUE",
+    "ThingType.AUDIOVISUEL",
+    "ThingType.MUSIQUE",
+    "EventType.SPECTACLE_VIVANT",
+    "ThingType.PRATIQUE_ARTISTIQUE_ABO",
+    "EventType.CINEMA",
+    "EventType.MUSIQUE",
+    "EventType.CONFERENCE_DEBAT_DEDICACE",
+    "EventType.MUSEES_PATRIMOINE",
+    "ThingType.MUSEES_PATRIMOINE_ABO",
+    "ThingType.PRESSE_ABO",
+    "ThingType.OEUVRE_ART",
+    "ThingType.LIVRE_AUDIO",
+    "EventType.JEUX",
+    "ThingType.CINEMA_ABO",
+    "ThingType.JEUX_VIDEO",
+    "ThingType.CINEMA_CARD",
+    "ThingType.MUSIQUE_ABO",
+    "EventType.ACTIVATION",
+    "ThingType.JEUX_VIDEO_ABO",
+    "ThingType.SPECTACLE_VIVANT_ABO",
+    "ThingType.ACTIVATION",
+]
+
+
+def get_unexpectedness(booked_type_list, recommended_type_list):
+    booked_type_vector_list = [
+        [int(booked_type == offer_type) for offer_type in TYPE_LIST]
+        for booked_type in booked_type_list
+    ]
+    recommended_type_vector_list = [
+        [int(recommended_type == offer_type) for offer_type in TYPE_LIST]
+        for recommended_type in recommended_type_list
+    ]
+
+    cosine_sum = 0
+    for booked_type_vector in booked_type_vector_list:
+        for recommended_type_vector in recommended_type_vector_list:
+            cosine_sum += cosine(booked_type_vector, recommended_type_vector)
+
+    return (1 / (len(booked_type_list) * len(recommended_type_list))) * cosine_sum
+
+
+def compute_metrics(k, pos_data_train, pos_data_test, match_model):
+    # map all offers to corresponding types
+    offer_type_dict = {}
+    unique_offer_types = (
+        pos_data_train.groupby(["item_id", "type"]).first().reset_index()
+    )
+    for item_id, item_type in zip(
+        unique_offer_types.item_id.values, unique_offer_types.type.values
+    ):
+        offer_type_dict[item_id] = item_type
+
+    # Only keep user - item iteractions in pos_data_test, which can be infered from model
+    cleaned_pos_data_test = pos_data_test.copy()
+    print(
+        f"Original number of positive feedbacks in test: {cleaned_pos_data_test.shape[0]}"
+    )
+    cleaned_pos_data_test = cleaned_pos_data_test.loc[
+        lambda df: df.user_id.apply(
+            lambda user_id: user_id in pos_data_train.user_id.values
+        )
+    ]
+    print(
+        f"Number of positive feedbacks in test after removing users not present in train: {cleaned_pos_data_test.shape[0]}"
+    )
+    cleaned_pos_data_test = cleaned_pos_data_test.loc[
+        lambda df: df.item_id.apply(
+            lambda item_id: item_id in pos_data_train.item_id.values
+        )
+    ]
+    print(
+        f"Number of positive feedbacks in test after removing offers not present in train: {cleaned_pos_data_test.shape[0]}"
+    )
+
+    # Get all offers the model may predict
+    all_item_ids = list(set(pos_data_train.item_id.values))
+    # Get all users in cleaned test data
+    all_test_user_ids = list(set(cleaned_pos_data_test.user_id.values))
+
+    hidden_items_number = 0
+    recommended_hidden_items_number = 0
+    prediction_number = 0
+    recommended_items = []
+
+    user_count = 0
+    unexpectedness = []
+    serendipity = []
+    new_types_ratio = []
+
+    for user_id in tqdm.tqdm(all_test_user_ids):
+        user_count += 1
+
+        pos_item_train = pos_data_train[pos_data_train["user_id"] == user_id]
+        pos_item_test = cleaned_pos_data_test[
+            cleaned_pos_data_test["user_id"] == user_id
+        ]
+
+        # remove items in train - they can not be in test set anyway
+        items_to_rank = np.setdiff1d(all_item_ids, pos_item_train["item_id"].values)
+        booked_offer_types = pos_item_train["type"].values
+
+        # check if any item of items_to_rank is in the test positive feedbacks for this user
+        expected = np.in1d(items_to_rank, pos_item_test["item_id"].values)
+
+        repeated_user_id = np.empty_like(items_to_rank)
+        repeated_user_id.fill(user_id)
+
+        predicted = match_model.predict(
+            [repeated_user_id, items_to_rank], batch_size=4096
+        )
+
+        scored_items = sorted(
+            [(item_id, score[0]) for item_id, score in zip(items_to_rank, predicted)],
+            key=lambda k: k[1],
+            reverse=True,
+        )[:k]
+        recommended_offer_types = [offer_type_dict[item[0]] for item in scored_items]
+
+        user_unexpectedness = get_unexpectedness(
+            booked_offer_types, recommended_offer_types
+        )
+        unexpectedness.append(user_unexpectedness)
+        new_types_ratio.append(
+            np.mean(
+                [
+                    int(recommended_offer_type not in booked_offer_types)
+                    for recommended_offer_type in recommended_offer_types
+                ]
+            )
+        )
+
+        if np.sum(expected) >= 1:
+            recommended_items.extend([item[0] for item in scored_items])
+            recommended_items = list(set(recommended_items))
+
+            hidden_items = pos_item_test["item_id"].values
+            recommended_hidden_items = [
+                item[0] for item in scored_items if item[0] in hidden_items
+            ]
+
+            hidden_items_number += len(hidden_items)
+            recommended_hidden_items_number += len(recommended_hidden_items)
+            prediction_number += 1
+
+            if hidden_items_number > 0:
+                user_serendipity = (
+                    (len(recommended_hidden_items) / len(hidden_items))
+                    * user_unexpectedness
+                    * 100
+                )
+                serendipity.append(user_serendipity)
+
+        metrics = {
+            f"recall@{k}": (recommended_hidden_items_number / hidden_items_number) * 100
+            if hidden_items_number > 0
+            else None,
+            f"precision@{k}": (
+                recommended_hidden_items_number / (prediction_number * k)
+            )
+            * 100,
+            f"maximal_precision@{k}": (
+                cleaned_pos_data_test.shape[0] / (prediction_number * k)
+            )
+            * 100,
+            f"coverage@{k}": (len(recommended_items) / len(all_item_ids)) * 100,
+            f"unexpectedness@{k}": np.mean(unexpectedness)
+            if len(unexpectedness) > 0
+            else None,
+            f"new_types_ratio@{k}": np.mean(new_types_ratio)
+            if len(new_types_ratio) > 0
+            else None,
+            f"serendipity@{k}": np.mean(serendipity) if len(serendipity) > 0 else None,
+        }
+
+        if user_count % 100 == 0:
+            print(metrics)
+
+    return metrics
---
+++ recommendation/api/scripts/train_and_evaluate_algo.py
@@ -0,0 +1,132 @@
+import json
+import random
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from metrics import compute_metrics
+from tf_model import TripletModel, MatchModel, identity_loss
+from matplotlib import pyplot as plt
+
+MODEL_DATA_PATH = "tf_bpr_string_input_7_months"
+START_DATE = "2020-10-10"
+END_DATE = "2021-05-10"
+
+n_epochs = 20
+batch_size = 32
+
+
+def save_dict_to_path(dictionnary, path):
+    with open(path, "w") as fp:
+        json.dump(dictionnary, fp)
+
+
+def get_bookings(start_date, end_date):
+    query = f"""
+        select user_id, 
+        (CASE WHEN offer.offer_type in ('ThingType.LIVRE_EDITION', 'EventType.CINEMA') THEN CONCAT('product-', offer.offer_product_id) ELSE CONCAT('offer-', offer.offer_id) END) AS offer_id, offer.offer_type as type,
+        count(*) as nb_bookings
+        from `passculture-data-prod.clean_prod.applicative_database_booking` booking
+        inner join `passculture-data-prod.clean_prod.applicative_database_stock` stock
+        on booking.stock_id = stock.stock_id
+        inner join `passculture-data-prod.clean_prod.applicative_database_offer` offer
+        on stock.offer_id = offer.offer_id 
+        where offer.offer_creation_date >= DATETIME '{start_date} 00:00:00'
+        and offer.offer_creation_date <= DATETIME '{end_date} 00:00:00'
+        group by user_id, offer_id, type
+    """
+    bookings = pd.read_gbq(query)
+    return bookings
+
+
+# DATA PROCESSING
+bookings = get_bookings(start_date=START_DATE, end_date=END_DATE).rename(
+    columns={"offer_id": "item_id", "nb_bookings": "rating"}
+)
+print("Bookings:")
+print(bookings.head())
+n_users = len(set(bookings.user_id.values))
+n_items = len(set(bookings.item_id.values))
+print(f"{n_users} users and {n_items} items")
+user_ids = bookings["user_id"].unique().tolist()
+item_ids = bookings["item_id"].unique().tolist()
+
+# SPLIT TRAIN AND TEST DATA
+df = bookings.sample(frac=1).reset_index(drop=True)
+lim_train = df.shape[0] * 80 / 100
+lim_eval = df.shape[0] * 90 / 100
+pos_data_train = df.loc[df.index < lim_train]
+pos_data_eval = df.loc[df.index < lim_eval]
+pos_data_eval = pos_data_eval.loc[pos_data_eval.index >= lim_train]
+pos_data_test = df[df.index >= lim_eval]
+pos_data_train.to_csv(f"{MODEL_DATA_PATH}/pos_data_train.csv", index=False)
+pos_data_test.to_csv(f"{MODEL_DATA_PATH}/pos_data_test.csv", index=False)
+
+triplet_model = TripletModel(user_ids, item_ids, latent_dim=64, l2_reg=1e-6)
+match_model = MatchModel(triplet_model.user_layer, triplet_model.item_layer)
+
+
+def sample_triplets(pos_data, item_ids):
+    """Sample negatives at random"""
+
+    user_ids = pos_data["user_id"].values
+    pos_item_ids = pos_data["item_id"].values
+    neg_item_ids = np.array(random.choices(item_ids, k=len(user_ids)), dtype=object)
+
+    return [user_ids, pos_item_ids, neg_item_ids]
+
+
+fake_y = np.array(["1"] * pos_data_train["user_id"].shape[0], dtype=object)
+evaluation_fake_train = np.array(
+    ["1"] * pos_data_eval["user_id"].shape[0], dtype=object
+)
+triplet_model.compile(loss=identity_loss, optimizer="adam")
+
+best_eval = 1
+
+train = []
+evaluation = []
+runned_epochs = 0
+for i in range(n_epochs):
+    # Sample new negatives to build different triplets at each epoch
+    triplet_inputs = sample_triplets(pos_data_train, item_ids)
+    evaluation_triplet_inputs = sample_triplets(pos_data_eval, item_ids)
+    # ds = tf.data.Dataset.from_tensor_slices(triplet_inputs)
+
+    # Fit the model incrementally by doing a single pass over the
+    # sampled triplets.
+    try:
+        print(f"Training epoch {i}")
+        train_result = triplet_model.fit(
+            x=triplet_inputs, y=fake_y, shuffle=True, batch_size=64, epochs=1
+        )
+        train.append(train_result.history["loss"][0])
+        print(train)
+        print(f"Evaluation epoch {i}")
+        eval_result = triplet_model.evaluate(
+            x=evaluation_triplet_inputs, y=evaluation_fake_train, batch_size=64
+        )
+        evaluation.append(eval_result)
+        print(evaluation)
+        runned_epochs += 1
+        if eval_result < best_eval:
+            tf.saved_model.save(match_model, f"{MODEL_DATA_PATH}/tf_bpr_{i}epochs")
+            best_eval = eval_result
+    except KeyboardInterrupt:
+        break
+
+# TRAINING CURVES
+plt.plot(list(range(runned_epochs)), train[:runned_epochs], label="Train Loss")
+plt.plot(
+    list(range(runned_epochs)), evaluation[:runned_epochs], label="Evaluation Loss"
+)
+plt.xlabel("Epoch")
+plt.ylabel("Losses")
+plt.legend()
+plt.savefig(f"{MODEL_DATA_PATH}/learning_curves.png")
+
+metrics = compute_metrics(10, pos_data_train, pos_data_test, match_model)
+
+print(metrics)
+save_dict_to_path(metrics, f"{MODEL_DATA_PATH}/metrics.json")

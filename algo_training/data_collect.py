a0445dd659f7d5ace426a927d6807e354a420037
FerdiHr <ferdi.harmel@gmail.com>
2021-05-11 09:16:30 +0200

    PC-8510 Create preprocessing script (#266)
    * (PC-8510) Create preprocessing script
    
    * (PC-8510) Clean end date for data collect
    
    * (PC-8510) Clean linter
    
    * (PC-8510) Clean typo
    
    * (PC-8510) Put scrîpts at root

---
+++ algo_training/data_collect.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+from datetime import datetime
+
+
+def get_bookings(start_date, end_date):
+    query = f"""
+        select user_id, 
+        (CASE WHEN offer.offer_type = 'ThingType.LIVRE_EDITION' THEN CONCAT('product-', offer.offer_product_id) ELSE CONCAT('offer-', offer.offer_id) END) AS offer_id,
+        count(*) as nb_bookings
+        from `passculture-data-prod.clean_prod.applicative_database_booking` booking
+        inner join `passculture-data-prod.clean_prod.applicative_database_stock` stock
+        on booking.stock_id = stock.stock_id
+        inner join `passculture-data-prod.clean_prod.applicative_database_offer` offer
+        on stock.offer_id = offer.offer_id 
+        where offer.offer_creation_date >= DATETIME '{start_date} 00:00:00'
+        and offer.offer_creation_date <= DATETIME '{end_date} 00:00:00'
+        group by user_id, offer_id
+    """
+    bookings = pd.read_gbq(query)
+    return bookings
+
+
+def main():
+    STORAGE_PATH = os.environ.get("STORAGE_PATH", "")
+    START_DATE = "2020-11-13"
+    now = datetime.now()
+    END_DATE = x.strftime("%Y-%m-%d")
+    bookings = get_bookings(start_date=START_DATE, end_date=END_DATE)
+    bookings.to_csv(f"{STORAGE_PATH}/raw_data.csv", delimiter=",")
+
+
+if __name__ == "__main__":
+    main()
---
+++ algo_training/preprocess.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+
+def preprocess(storage_path: str):
+    bookings = pd.read_csv(f"{storage_path}/raw_data.csv")
+    bookings.rename(
+        columns={"offer_id": "item_id", "nb_bookings": "rating"}, inplace=True
+    )
+    bookings.to_csv(f"{storage_path}/clean_data.csv", delimiter=",")
+
+
+def main():
+    STORAGE_PATH = os.environ.get("STORAGE_PATH", "")
+    preprocess(STORAGE_PATH)
+
+
+if __name__ == "__main__":
+    main()

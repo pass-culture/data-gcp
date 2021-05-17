ae95539b80e8c297cf6bf4869c3d5b43c7bb541d
Daniel Cuesta <daniel.cuesta@passculture.app>
2021-05-17 12:08:34 +0200

    (PC-8680) fix interval of days

--- recommendation/db/scripts/create_recommendable_offers.sql
+++ recommendation/db/scripts/create_recommendable_offers.sql
@@ -101,7 +101,7 @@ BEGIN
             FROM public.booking
             LEFT JOIN public.stock
             ON booking.stock_id = stock.stock_id
-            WHERE booking.booking_creation_date >= NOW() - INTERVAL '30 days'
+            WHERE booking.booking_creation_date >= NOW() - INTERVAL '7 days'
             AND NOT booking.booking_is_cancelled
             GROUP BY stock.offer_id
       ) booking_numbers

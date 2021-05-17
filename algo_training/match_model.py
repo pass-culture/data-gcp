ff12c614f18e8423f6d18b37f9d26210901ef6cb
FerdiHr <ferdi.harmel@gmail.com>
2021-05-17 17:46:29 +0200

    (PC-8366) Init dependencies

---
+++ algo_training/match_model.py
@@ -0,0 +1,28 @@
+import numpy as np
+from tensorflow.keras.models import Model
+
+
+class MatchModel(Model):
+    def __init__(self, user_layer, item_layer):
+        super().__init__(name="MatchModel")
+
+        # Reuse shared weights for those layers:
+        self.user_layer = user_layer
+        self.item_layer = item_layer
+
+        self.flatten = Flatten()
+        self.dot = Dot(axes=1, normalize=True)
+
+    def call(self, inputs):
+        user_input = inputs[0]
+        pos_item_input = inputs[1]
+
+        user_embedding = self.user_layer(user_input)
+        user_embedding = self.flatten(user_embedding)
+
+        pos_item_embedding = self.item_layer(pos_item_input)
+        pos_item_embedding = self.flatten(pos_item_embedding)
+
+        pos_similarity = self.dot([user_embedding, pos_item_embedding])
+
+        return pos_similarity

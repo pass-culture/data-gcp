fdca9f1c65bf646b8e9173652f2d2b209262b18c
FerdiHr <ferdi.harmel@gmail.com>
2021-05-19 10:44:34 +0200

    PC-8366 Train script (#279)
    * (PC-8366) Init dependencies
    
    * (PC-8366) Add train script
    
    * (PC-8366) Fix typo + linter
    
    * (PC-8366) Add requirements
    
    * (PC-8366) Fix typo requirements
    
    * (PC-8366) Add missing imports
    
    * (PC-8366) Add missing import
    
    * (PC-8366) Update preprocessing scripts
    
    * (PC-8366) Fix train typo
    
    * (PC-8366) Add pos eval
    
    * (PC-8366) Set up mlflow server
    
    * (PC-8366) Update fetching secrets
    
    * (PC-8366) Add autolog
    
    * (PC-8366) Add metrics logs
    
    * (PC-8366) Fix import & import type issues
    
    * (PC-8366) Save model after train
    
    * (PC-8366) Clean files
    
    * (PC-8366) Add log + clean imports

--- algo_training/data_collect.py
+++ algo_training/data_collect.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def get_bookings(start_date, end_date):
     query = f"""
         select user_id, 
-        (CASE WHEN offer.offer_type = 'ThingType.LIVRE_EDITION' THEN CONCAT('product-', offer.offer_product_id) ELSE CONCAT('offer-', offer.offer_id) END) AS offer_id,
+        (CASE WHEN offer.offer_type in ('ThingType.LIVRE_EDITION', 'EventType.CINEMA') THEN CONCAT('product-', offer.offer_product_id) ELSE CONCAT('offer-', offer.offer_id) END) AS offer_id, offer.offer_type as type,
         count(*) as nb_bookings
         from `passculture-data-prod.clean_prod.applicative_database_booking` booking
         inner join `passculture-data-prod.clean_prod.applicative_database_stock` stock
@@ -15,7 +15,7 @@ def get_bookings(start_date, end_date):
         on stock.offer_id = offer.offer_id 
         where offer.offer_creation_date >= DATETIME '{start_date} 00:00:00'
         and offer.offer_creation_date <= DATETIME '{end_date} 00:00:00'
-        group by user_id, offer_id
+        group by user_id, offer_id, type
     """
     bookings = pd.read_gbq(query)
     return bookings

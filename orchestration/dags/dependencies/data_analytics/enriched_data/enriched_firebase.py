6f637b586e7f36b58bfbde42d6b7dd9e43a79150
Maxime Bourliatoux <bourliam@gmail.com>
2021-06-04 10:07:14 +0200

    (PC-8774) Add table firebase_visits (#313)

--- orchestration/dags/dependencies/data_analytics/enriched_data/enriched_firebase.py
+++ orchestration/dags/dependencies/data_analytics/enriched_data/enriched_firebase.py
@@ -60,3 +60,76 @@ def aggregate_firebase_user_events(gcp_project, bigquery_raw_dataset):
         WHERE user_id IS NOT NULL
         GROUP BY user_id
     """
+
+
+def aggregate_firebase_visits(gcp_project, bigquery_raw_dataset):
+    return f"""
+    WITH base AS (
+        SELECT
+            event_name, event_timestamp, user_id, user_first_touch_timestamp,
+            device.category, device.mobile_brand_name, device.operating_system, device.operating_system_version,
+            traffic_source.name, traffic_source.medium, traffic_source.source,
+            (select event_params.value.int_value
+                from unnest(event_params) event_params
+                where event_params.key = 'ga_session_id'
+            ) as session_id,
+            (select event_params.value.int_value
+                from unnest(event_params) event_params
+                where event_params.key = 'ga_session_number'
+            ) as session_number,
+        FROM `{gcp_project}.{bigquery_raw_dataset}.events_*`
+    )
+    SELECT
+        session_id,
+        ANY_VALUE(session_number) AS session_number,
+        ANY_VALUE(category) AS category,
+        ANY_VALUE(mobile_brand_name) AS mobile_brand_name,
+        ANY_VALUE(operating_system) AS operating_system,
+        ANY_VALUE(operating_system_version) AS operating_system_version,
+        ANY_VALUE(user_id) AS user_id,
+        TIMESTAMP_SECONDS(CAST(CAST(MIN(user_first_touch_timestamp) as INT64)/1000000 as INT64)) AS user_first_touch_timestamp,
+        ANY_VALUE(name) AS name,
+        ANY_VALUE(medium) AS medium,
+        ANY_VALUE(source) AS source,
+        TIMESTAMP_SECONDS(CAST(CAST(MAX(event_timestamp) as INT64)/1000000 as INT64)) AS last_event_timestamp,
+        COUNTIF(event_name="ConsultOffer") AS nb_consult_offer,
+        COUNTIF(event_name="screen_view_bookingconfirmation") AS nb_booking_confirmation,
+    FROM base
+    GROUP BY session_id;
+    """
+
+
+def copy_table_to_analytics(gcp_project, bigquery_raw_dataset, execution_date):
+    return f"""
+    SELECT
+        event_name, user_pseudo_id, user_id, platform,
+        PARSE_DATE("%Y%m%d", event_date) AS event_date,
+        TIMESTAMP_SECONDS(CAST(CAST(event_timestamp as INT64)/1000000 as INT64)) AS event_timestamp,
+        TIMESTAMP_SECONDS(CAST(CAST(event_previous_timestamp as INT64)/1000000 as INT64)) AS event_previous_timestamp,
+        TIMESTAMP_SECONDS(CAST(CAST(event_timestamp as INT64)/1000000 as INT64)) AS user_first_touch_timestamp,
+        (select event_params.value.string_value
+            from unnest(event_params) event_params
+            where event_params.key = 'firebase_screen'
+        ) as firebase_screen,
+        (select event_params.value.string_value
+            from unnest(event_params) event_params
+            where event_params.key = 'firebase_previous_screen'
+        ) as firebase_previous_screen,
+        (select event_params.value.int_value
+            from unnest(event_params) event_params
+            where event_params.key = 'ga_session_number'
+        ) as session_number,
+        (select event_params.value.int_value
+            from unnest(event_params) event_params
+            where event_params.key = 'ga_session_id'
+        ) as session_id,
+        (select event_params.value.string_value
+            from unnest(event_params) event_params
+            where event_params.key = 'pageName'
+        ) as page_name,
+        (select CAST(event_params.value.double_value AS STRING)
+            from unnest(event_params) event_params
+            where event_params.key = 'offerId'
+        ) as offer_id,
+    FROM {gcp_project}.{bigquery_raw_dataset}.events_{execution_date}
+    """

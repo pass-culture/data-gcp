bb2fcdb040e341cbdbd165adf8e4b13a3d64c23b
FerdiHr <ferdi.harmel@gmail.com>
2021-06-10 12:22:17 +0200

    (PC-9037) Init DAG and dependencies

---
+++ orchestration/dags/dependencies/monitoring.py
@@ -0,0 +1,29 @@
+from dependencies.config import (
+    GCP_PROJECT,
+    BIGQUERY_RAW_DATASET,
+    BIGQUERY_CLEAN_DATASET,
+    BIGQUERY_ANALYTICS_DATASET,
+    ENV_SHORT_NAME,
+)
+
+FIREBASE_EVENTS_TABLE = "firebase_events"
+TABLE_AB_TESTING = 'ab_testing_202104_v0_v0bis'
+
+
+def get_request_click_through_reco_module(start_date, group_id):
+    print(f"group_id : {group_id}")
+    return f"""SELECT COUNT(*) FROM (
+            SELECT AS STRUCT event_timestamp, event_date, event_name, user_id, 
+                (select event_params.value.string_value
+                    from unnest(event_params) event_params
+                    where event_params.key = 'moduleName'
+                ) as module_name, 
+            FROM `{GCP_PROJECT}.{BIGQUERY_CLEAN_DATASET}.{FIREBASE_EVENTS_TABLE}_*` a 
+            LEFT JOIN `{GCP_PROJECT}.{BIGQUERY_RAW_DATASET}.{TABLE_AB_TESTING}` b on  a.user_id =  b.userid, 
+            UNNEST(event_params) as ep 
+            WHERE event_name = 'ConsultOffer' 
+            AND ep.key = 'moduleName' 
+            AND ep.value.string_value = 'Fais le plein de découvertes' 
+            AND b.groupid = '{group_id}'
+            AND TIMESTAMP_SECONDS(CAST(CAST(event_timestamp as INT64)/1000000 as INT64)) > TIMESTAMP_SECONDS(CAST(CAST({start_date} as INT64)/1000000 as INT64))
+        );"""
---
+++ orchestration/dags/monitoring.py
@@ -0,0 +1,137 @@
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+from airflow.contrib.operators.bigquery_get_data import BigQueryGetDataOperator
+from airflow.contrib.operators.gcs_to_bq import (
+    GoogleCloudStorageToBigQueryOperator,
+)
+
+from dependencies.slack_alert import task_fail_slack_alert
+from dependencies.bigquery_client import BigQueryClient
+from dependencies.monitoring import get_request_click_through_reco_module
+
+from dependencies.config import (
+    GCP_PROJECT,
+    DATA_GCS_BUCKET_NAME,
+    BIGQUERY_RAW_DATASET,
+    BIGQUERY_CLEAN_DATASET,
+    BIGQUERY_ANALYTICS_DATASET,
+    ENV_SHORT_NAME,
+)
+
+FIREBASE_EVENTS_TABLE = "firebase_events"
+MONITORING_TABLE = "monitoring_data"
+START_DATE = datetime(2021, 5, 20)
+
+
+groups = ["A", "B"]
+
+
+def get_last_data_timestamp(project_name, dataset, table, ti, **kwargs):
+    bigquery_query = f"SELECT event_timestamp FROM {project_name}.{dataset}.{table} ORDER BY event_timestamp DESC LIMIT 1;"
+    bigquery_client = BigQueryClient()
+    results = bigquery_client.query(bigquery_query)
+    result = int(results.values[0][0].to_pydatetime().timestamp())
+    print(f"result : {result} is type : {type(result)}")
+    ti.xcom_push(key="from_time", value=result)
+
+
+def compute_click_through_reco_module(ti, **kwargs):
+    start_date = ti.xcom_pull(key="from_time")
+    for group_id in groups:
+        bigquery_query = get_request_click_through_reco_module(start_date, group_id)
+        bigquery_client = BigQueryClient()
+        result = int(bigquery_client.query(bigquery_query).values[0][0])
+        ti.xcom_push(key=f"COUNT_CLICK_RECO_{group_id}", value=result)
+
+
+def insert_metric_bq(project_name, dataset, table, ti, **kwargs):
+    bigquery_query = f"INSERT {project_name}.{dataset}.{table} (compute_time, from_time, last_metric_time, metric_name, metric_value, algorithm_id, environment, group_id) VALUES"
+    last_metric_time = ti.xcom_pull(key="from_time")
+    for metric_id, _ in metrics_to_compute.items():
+        for group_id in groups:
+            metric_query = f"""
+            (   {datetime.now()}, 
+                TIMESTAMP_SECONDS(CAST(CAST({START_DATE} as INT64)/1000000 as INT64)), 
+                {last_metric_time}, 
+                '{metric_id}', 
+                '{ti.xcom_pull(key=f"{metric_id}_{group_id}")}',
+                'algo_v0',
+                '{ENV_SHORT_NAME}',
+                '{group_id}'
+            )"""
+            bigquery_query += metric_query
+            bigquery_query += ","
+    bigquery_client = BigQueryClient()
+    bigquery_query = f"{bigquery_query[:-1]};"
+    print(bigquery_query)
+    bigquery_client.query(bigquery_query)
+
+metrics_to_compute = {
+    "COUNT_CLICK_RECO": compute_click_through_reco_module,
+}
+
+
+default_args = {
+    "start_date": datetime(2021, 5, 26),
+    #"on_failure_callback": task_fail_slack_alert,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=2),
+}
+
+
+with DAG(
+    "compute_monitoring",
+    default_args=default_args,
+    description="Compute monitoring metrics",
+    schedule_interval="0 1 * * *",
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=180),
+) as dag:
+
+    start = DummyOperator(task_id="start")
+
+    init_dag = PythonOperator(
+        task_id="get_last_data_timestamp",
+        python_callable=get_last_data_timestamp,
+        op_kwargs={
+            "project_name": GCP_PROJECT,
+            "dataset": BIGQUERY_ANALYTICS_DATASET,
+            "table": FIREBASE_EVENTS_TABLE,
+        },
+        provide_context=True,
+    )
+
+    compute_metric_task = [
+        PythonOperator(
+            task_id=f"compute_{metric}",
+            python_callable=function_to_call,
+            # op_kwargs={
+            #     "project_name": GCP_PROJECT,
+            #     "dataset": BIGQUERY_ANALYTICS_DATASET,
+            #     "table": FIREBASE_EVENTS_TABLE,
+            # },
+            provide_context=True,
+        )
+        for metric, function_to_call in metrics_to_compute.items()
+    ]
+
+    insert_metric_bq = PythonOperator(
+        task_id=f"insert_metric_bigquery",
+        python_callable=insert_metric_bq,
+        op_kwargs={
+            "project_name": GCP_PROJECT,
+            "dataset": BIGQUERY_ANALYTICS_DATASET,
+            "table": MONITORING_TABLE,
+        },
+        provide_context=True,
+    )
+
+    end = DummyOperator(task_id="end")
+
+
+start >> init_dag >> compute_metric_task >> insert_metric_bq >> end

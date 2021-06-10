cefd4c07ff0938cd4768b201fd3a3c7f747306c1
FerdiHr <ferdi.harmel@gmail.com>
2021-06-10 14:17:55 +0200

    (PC-9359) Clean fetch secrets

--- orchestration/dags/dependencies/request_dms.py
+++ orchestration/dags/dependencies/request_dms.py
@@ -10,6 +10,13 @@ import pandas as pd
 from datetime import datetime
 from google.cloud import secretmanager
 
+from access_gcp_secrets import access_secret_data
+
+from config import (
+    GCP_PROJECT_ID,
+    DATA_GCS_BUCKET_NAME,
+)
+
 
 API_URL = "https://www.demarches-simplifiees.fr/api/v2/graphql"
 demarches_ids = ["44675", "44623", "29161"]
@@ -154,15 +161,10 @@ def run_query(query, dms_token):
 
 
 def get_secret_token():
-    client = secretmanager.SecretManagerServiceClient()
-    GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "passculture-data-ehp")
-    name = f"projects/{GCP_PROJECT_ID}/secrets/token_dms/versions/1"
-    response = client.access_secret_version(name=name)
-    return response.payload.data.decode("UTF-8")
+    return access_secret_data(GCP_PROJECT_ID, "token_dms")
 
 
 def save_result(df_applications):
-    DATA_GCS_BUCKET_NAME = os.environ.get("DATA_GCS_BUCKET_NAME")
     now = datetime.now()
     df_applications.to_csv(
         f"gs://{DATA_GCS_BUCKET_NAME}/dms_export/dms_{now.year}_{now.month}_{now.day}.csv",

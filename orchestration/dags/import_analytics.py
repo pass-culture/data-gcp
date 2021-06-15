2e333b5c2b6a86305f87e1214d12bf5c8c10aa0b
Daniel Cuesta <daniel.cuesta@passculture.app>
2021-06-15 17:31:52 +0200

    (PC-8971) rework import analytics task to Paris timezone

--- orchestration/dags/import_analytics.py
+++ orchestration/dags/import_analytics.py
@@ -32,7 +32,10 @@ from dependencies.data_analytics.enriched_data.user import (
 from dependencies.data_analytics.enriched_data.venue import (
     define_enriched_venue_data_full_query,
 )
-from dependencies.data_analytics.import_tables import define_import_query
+from dependencies.data_analytics.import_tables import (
+    define_import_query,
+    define_replace_query,
+)
 from dependencies.slack_alert import task_fail_slack_alert
 
 # Variables
@@ -51,14 +54,12 @@ data_applicative_tables = [
     "venue_type",
     "venue_label",
     "payment_status",
-    "iris_venues",
     "transaction",
     "local_provider_event",
     "beneficiary_import_status",
     "deposit",
     "beneficiary_import",
     "mediation",
-    "iris_france",
     "offer_criterion",
     "allocine_pivot",
     "venue_provider",
@@ -68,6 +69,49 @@ data_applicative_tables = [
     "feature",
     "criterion",
 ]
+
+date_to_convert = [
+    [
+        "user_creation_date",
+        "user_cultural_survey_filled_date",
+        "user_last_connection_date",
+    ],
+    "",
+    ["offerer_modified_at_last_provider_date", "offerer_creation_date"],
+    "dateModified",
+    ["booking_creation_date", "booking_used_date", "booking_cancellation_date"],
+    "",
+    ["venue_modified_at_last_provider", "venue_creation_date"],
+    "",
+    ["offer_modified_at_last_provider_date", "offer_creation_date"],
+    [
+        "stock_modified_at_last_provider_date",
+        "stock_modified_date",
+        "stock_booking_limit_date",
+        "stock_creation_date",
+    ],
+    "dateCreated",
+    "",
+    "",
+    "date",
+    "",
+    "date",
+    "date",
+    "dateCreated",
+    "",
+    "dateCreated",
+    "",
+    "",
+    "dateModifiedAtLastProvider",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]
+
+tables_to_convert = dict(zip(data_applicative_tables, date_to_convert))
 
 default_dag_args = {
     "start_date": datetime.datetime(2020, 12, 21),
@@ -108,7 +152,7 @@ import_tables_to_analytics_tasks = []
 for table in data_applicative_tables:
     task = BigQueryOperator(
         task_id=f"import_to_analytics_{table}",
-        sql=f"SELECT * FROM {BIGQUERY_CLEAN_DATASET}.{APPLICATIVE_PREFIX}{table}",
+        sql=f"SELECT * {define_replace_query(table,tables_to_convert)} FROM {BIGQUERY_CLEAN_DATASET}.{APPLICATIVE_PREFIX}{table}",
         write_disposition="WRITE_TRUNCATE",
         use_legacy_sql=False,
         destination_dataset_table=f"{BIGQUERY_ANALYTICS_DATASET}.{APPLICATIVE_PREFIX}{table}",
@@ -117,6 +161,39 @@ for table in data_applicative_tables:
     import_tables_to_analytics_tasks.append(task)
 
 end_import = DummyOperator(task_id="end_import", dag=dag)
+
+IRIS_DISTANCE = 100000
+
+link_iris_venues_task = BigQueryOperator(
+    task_id="link_iris_venues_task",
+    sql=f"""
+    WITH venues_to_link AS (
+        SELECT venue_id, venue_longitude, venue_latitude
+        FROM `{BIGQUERY_CLEAN_DATASET}.{APPLICATIVE_PREFIX}venue` as venue
+        JOIN  `{BIGQUERY_CLEAN_DATASET}.{APPLICATIVE_PREFIX}offerer` as offerer ON venue_managing_offerer_id=offerer_id
+        LEFT JOIN `{BIGQUERY_CLEAN_DATASET}.iris_venues` as iv on venue.venue_id = iv.venueId
+        WHERE iv.venueId is null
+        AND venue_is_virtual is false
+        AND venue_validation_token is null
+        AND offerer_validation_token is null
+    )
+    SELECT iris_france.id as irisId, venue_id as venueId FROM {BIGQUERY_CLEAN_DATASET}.iris_france, venues_to_link
+    WHERE ST_DISTANCE(centroid, ST_GEOGPOINT(venue_longitude, venue_latitude)) < {IRIS_DISTANCE}
+    """,
+    destination_dataset_table=f"{BIGQUERY_CLEAN_DATASET}.iris_venues",
+    write_disposition="WRITE_APPEND",
+    use_legacy_sql=False,
+    dag=dag,
+)
+
+copy_to_analytics_iris_venues = BigQueryOperator(
+    task_id=f"copy_to_analytics_iris_venues",
+    sql=f"SELECT * FROM {BIGQUERY_CLEAN_DATASET}.iris_venues",
+    write_disposition="WRITE_TRUNCATE",
+    use_legacy_sql=False,
+    destination_dataset_table=f"{BIGQUERY_ANALYTICS_DATASET}.iris_venues",
+    dag=dag,
+)
 
 create_enriched_offer_data_task = BigQueryOperator(
     task_id="create_enriched_offer_data",
@@ -211,4 +288,5 @@ create_enriched_data_tasks = [
 
 end = DummyOperator(task_id="end", dag=dag)
 
-start >> import_tables_to_clean_tasks >> end_import_table_to_clean >> import_tables_to_analytics_tasks >> end_import >> create_enriched_data_tasks >> end
+start >> import_tables_to_clean_tasks >> end_import_table_to_clean >> import_tables_to_analytics_tasks >> end_import
+end_import >> link_iris_venues_task >> copy_to_analytics_iris_venues >> create_enriched_data_tasks >> end

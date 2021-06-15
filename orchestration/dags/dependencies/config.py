f3f0eba5ffa7e002a734ca86bc5521263e244371
Daniel Cuesta <daniel.cuesta@passculture.app>
2021-06-15 11:47:49 +0200

    (PC-8971) remove timezone var from env. vars

--- orchestration/dags/dependencies/config.py
+++ orchestration/dags/dependencies/config.py
@@ -27,4 +27,3 @@ BIGQUERY_ANALYTICS_DATASET = os.environ.get(
 APPLICATIVE_PREFIX = "applicative_database_"
 MATOMO_PREFIX = "matomo_"
 SURVEY_PREFIX = "survey_"
-CONVERT_TO_PARIS_TIME = " at time zone \'utc\' at time zone \'Europe/Paris\'"

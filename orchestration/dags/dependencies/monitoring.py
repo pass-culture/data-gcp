ca3414d6d91ceaf72deaa73f8a302598d8180894
FerdiHr <ferdi.harmel@gmail.com>
2021-06-16 09:07:42 +0200

    (PC-9037) Update dependecies file

--- orchestration/dags/dependencies/monitoring.py
+++ orchestration/dags/dependencies/monitoring.py
@@ -7,8 +7,18 @@ from dependencies.config import (
 )
 
 FIREBASE_EVENTS_TABLE = "firebase_events"
+MONITORING_TABLE = "monitoring_data"
 TABLE_AB_TESTING = "ab_testing_202104_v0_v0bis"
 
+LAST_EVENT_TIME_KEY = "last_event_time_key"
+
+metrics_to_compute = {
+    "COUNT_CLICK_RECO": compute_click_through_reco_module,
+}
+
+
+def get_last_event_time_request():
+    return f"SELECT max(event_timestamp) FROM `{GCP_PROJECT}.{BIGQUERY_ANALYTICS_DATASET}.{FIREBASE_EVENTS_TABLE}`;"
 
 def get_request_click_through_reco_module(start_date, group_id):
     print(f"group_id : {group_id}")
@@ -27,3 +37,24 @@ def get_request_click_through_reco_module(start_date, group_id):
             AND b.groupid = '{group_id}'
             AND TIMESTAMP_SECONDS(CAST(CAST(event_timestamp as INT64)/1000000 as INT64)) > TIMESTAMP_SECONDS(CAST(CAST({start_date} as INT64)/1000000 as INT64))
         );"""
+
+
+def get_insert_metric_request(ti, start_date):
+    bigquery_query = f"""INSERT `{GCP_PROJECT}.{BIGQUERY_ANALYTICS_DATASET}.{MONITORING_TABLE}` (compute_time, from_time, last_metric_time, metric_name, metric_value, algorithm_id, environment, group_id) 
+    VALUES"""
+    last_metric_time = ti.xcom_pull(key=LAST_EVENT_TIME_KEY)
+    for metric_id, _ in metrics_to_compute.items():
+        for group_id in groups:
+            metric_query = f"""
+            (   '{datetime.now()}', 
+                '{start_date}', 
+                TIMESTAMP_SECONDS(CAST(CAST({last_metric_time} as INT64)/1000000 as INT64)), 
+                '{metric_id}', 
+                {float(ti.xcom_pull(key=f"{metric_id}_{group_id}"))},
+                'algo_v0',
+                '{ENV_SHORT_NAME}',
+                '{group_id}'
+            ),"""
+            bigquery_query += metric_query
+    bigquery_query = f"{bigquery_query[:-1]};"
+    return bigquery_query

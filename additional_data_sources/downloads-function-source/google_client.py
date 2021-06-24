92fe292a28ca2a01b4e033cc026101607777c099
Jean <jeanr@sicara.com>
2021-06-24 17:48:06 +0200

    (PC-9542): debug google client and update apple data daily

--- additional_data_sources/downloads-function-source/google_client.py
+++ additional_data_sources/downloads-function-source/google_client.py
@@ -1,12 +1,38 @@
+import os
+
 import pandas as pd
+import tempfile
+from werkzeug.utils import secure_filename
+
+from google.cloud import storage
 
 
 class GoogleClient:
     def __init__(self, report_bucket_name):
         self.report_bucket_name = report_bucket_name
 
-    def get_monthly_donloads(self, month="2021-05"):
+    def get_monthly_downloads(self, month="2021-05"):
         file_name = f"stats/installs/installs_app.passculture.webapp_{month.replace('-', '')}_app_version.csv"
-        path = f"gs://{self.report_bucket_name}/{file_name}"
-        df = pd.read_csv(path, encoding="utf-16")
-        return df["Install events"].sum()
+
+        temp_destination_file_name = self.download_file_from_gcs(
+            self.report_bucket_name, file_name, "report.csv"
+        )
+        df = pd.read_csv(temp_destination_file_name, encoding="utf-16")
+        download_stats = df["Daily Device Installs"].sum()
+        os.remove(temp_destination_file_name)
+        return download_stats
+
+    @staticmethod
+    def get_file_path(filename):
+        file_name = secure_filename(filename)
+        return os.path.join(tempfile.gettempdir(), file_name)
+
+    def download_file_from_gcs(
+        self, bucket_name, source_file_name, destination_file_name
+    ):
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(source_file_name)
+        temp_destination_file_name = self.get_file_path(destination_file_name)
+        blob.download_to_filename(temp_destination_file_name)
+        return temp_destination_file_name

e5e3e103aab315ce1e4c50568fbc3511852d598d
Maxime Bourliatoux <bourliam@gmail.com>
2021-07-16 15:40:39 +0200

    (PC-9660) Modifying analytics dag

--- orchestration/dags/import_analytics.py
+++ orchestration/dags/import_analytics.py
@@ -47,8 +47,10 @@ from dependencies.slack_alert import task_fail_slack_alert
 from dependencies.tag_offers import extract_tags
 
 
-def getting_service_account_token():
-    function_url = f"https://europe-west1-{GCP_PROJECT}.cloudfunctions.net/downloads_{ENV_SHORT_NAME}"
+def getting_service_account_token(function_name):
+    function_url = (
+        f"https://europe-west1-{GCP_PROJECT}.cloudfunctions.net/{function_name}"
+    )
     open_id_connect_token = id_token.fetch_id_token(Request(), function_url)
     return open_id_connect_token
 
@@ -262,9 +264,12 @@ create_enriched_booked_categories_data_v2_task = BigQueryOperator(
     dag=dag,
 )
 
-getting_service_account_token = PythonOperator(
-    task_id="getting_service_account_token",
+getting_downloads_service_account_token = PythonOperator(
+    task_id="getting_downloads_service_account_token",
     python_callable=getting_service_account_token,
+    op_kwargs={
+        "function_name": f"downloads_{ENV_SHORT_NAME}",
+    },
     dag=dag,
 )
 
@@ -275,7 +280,29 @@ import_downloads_data_to_bigquery = SimpleHttpOperator(
     endpoint=f"downloads_{ENV_SHORT_NAME}",
     headers={
         "Content-Type": "application/json",
-        "Authorization": "Bearer {{task_instance.xcom_pull(task_ids='getting_service_account_token', key='return_value')}}",
+        "Authorization": "Bearer {{task_instance.xcom_pull(task_ids='getting_downloads_service_account_token', key='return_value')}}",
+    },
+    log_response=True,
+    dag=dag,
+)
+
+getting_contentful_service_account_token = PythonOperator(
+    task_id="getting_contentful_service_account_token",
+    python_callable=getting_service_account_token,
+    op_kwargs={
+        "function_name": f"contentful_{ENV_SHORT_NAME}",
+    },
+    dag=dag,
+)
+
+import_contentful_data_to_bigquery = SimpleHttpOperator(
+    task_id="import_contentful_data_to_bigquery",
+    method="POST",
+    http_conn_id="http_gcp_cloud_function",
+    endpoint=f"contentful_{ENV_SHORT_NAME}",
+    headers={
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {{task_instance.xcom_pull(task_ids='getting_contentful_service_account_token', key='return_value')}}",
     },
     log_response=True,
     dag=dag,
@@ -293,16 +320,16 @@ create_enriched_app_downloads_stats = BigQueryOperator(
 create_offer_extracted_data = BigQueryOperator(
     task_id="create_offer_extracted_data",
     sql=f"""SELECT offer_id, offer_type,[""] as offer_tags, LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.author"), " ")) AS author,
-             LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.performer")," ")) AS performer,
-             LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.musicType"), " ")) AS musicType,
-             LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.musicSubtype"), " ")) AS musicSubtype,
-             LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.stageDirector"), " ")) AS stageDirector,
-             LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.theater"), " ")) AS theater,
-             LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.showType"), " ")) AS showType,
-             LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.showSubType"), " ")) AS showSubType,
-             LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.speaker"), " ")) AS speaker,
-             LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.rayon"), " ")) AS rayon
-          FROM `{GCP_PROJECT}.{BIGQUERY_ANALYTICS_DATASET}.applicative_database_offer`""",
+            LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.performer")," ")) AS performer,
+            LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.musicType"), " ")) AS musicType,
+            LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.musicSubtype"), " ")) AS musicSubtype,
+            LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.stageDirector"), " ")) AS stageDirector,
+            LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.theater"), " ")) AS theater,
+            LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.showType"), " ")) AS showType,
+            LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.showSubType"), " ")) AS showSubType,
+            LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.speaker"), " ")) AS speaker,
+            LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.rayon"), " ")) AS rayon
+        FROM `{GCP_PROJECT}.{BIGQUERY_ANALYTICS_DATASET}.applicative_database_offer`""",
     destination_dataset_table=f"{BIGQUERY_CLEAN_DATASET}.offer_extracted_data",
     write_disposition="WRITE_TRUNCATE",
     use_legacy_sql=False,
@@ -345,8 +372,14 @@ end = DummyOperator(task_id="end", dag=dag)
 )
 (
     extract_tags
-    >> getting_service_account_token
+    >> getting_downloads_service_account_token
     >> import_downloads_data_to_bigquery
     >> create_enriched_app_downloads_stats
+    >> end
+)
+(
+    extract_tags
+    >> getting_contentful_service_account_token
+    >> import_contentful_data_to_bigquery
     >> end
 )

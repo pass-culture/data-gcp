53edfbbe8c51678bbe924a058620d89dece039c0
dcuesta-pass <daniel.cuesta@passculture.app>
2021-07-27 17:32:24 +0200

    (PC-9692bis) fix dag start_date

--- orchestration/dags/extract_offer_tags.py
+++ orchestration/dags/extract_offer_tags.py
@@ -9,13 +9,13 @@ from dependencies.tag_offers import tag_offers
 from dependencies.config import GCP_PROJECT
 
 default_dag_args = {
-    "start_date": datetime.datetime(2021, 07, 27),
+    "start_date": datetime.datetime(2021, 7, 27),
     "retries": 1,
     "project_id": GCP_PROJECT,
-}
+    }
 
 dag = DAG(
-    "extracted_data_tag_offers",
+    "extract_offer_tags",
     default_args=default_dag_args,
     description="Tag offer based on description topic",
     on_failure_callback=None,

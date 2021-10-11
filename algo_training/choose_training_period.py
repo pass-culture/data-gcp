885b336c1fd36e099ed155868de20cf72291db33
dcuesta-pass <daniel.cuesta@passculture.app>
2021-10-11 14:49:47 +0200

    PC-10869 update choose_training_period

--- algo_training/choose_training_period.py
+++ algo_training/choose_training_period.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta
 def get_bookings(start_date, end_date):
     query = f"""
         select user_id,
-        (CASE WHEN offer.offer_type in ('ThingType.LIVRE_EDITION', 'EventType.CINEMA') THEN CONCAT('product-', offer.offer_product_id) ELSE CONCAT('offer-', offer.offer_id) END) AS offer_id, offer.offer_type as type,
-        count(*) as nb_bookings
+        (CASE WHEN offer.offer_subcategoryId in ('LIVRE', 'CINEMA') THEN CONCAT('product-', offer.offer_product_id) ELSE CONCAT('offer-', offer.offer_id) END) AS offer_id, offer.offer_type as type,
+        offer.offer_subcategoryId as subcategoryId, count(*) as nb_bookings
         from `passculture-data-prod.clean_prod.applicative_database_booking` booking
         inner join `passculture-data-prod.clean_prod.applicative_database_stock` stock
         on booking.stock_id = stock.stock_id
@@ -15,7 +15,7 @@ def get_bookings(start_date, end_date):
         on stock.offer_id = offer.offer_id
         where offer.offer_creation_date >= DATETIME '{start_date} 00:00:00'
         and offer.offer_creation_date <= DATETIME '{end_date} 00:00:00'
-        group by user_id, offer_id, type
+        group by user_id, offer_id, type, subcategoryId
     """
     bookings = pd.read_gbq(query)
     return bookings

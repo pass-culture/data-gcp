922ce46c5793584533baa7fd5f76340bc291691f
danslama2310 <85947191+danslama2310@users.noreply.github.com>
2021-11-08 11:52:07 +0100

    Join_on_code_geo_and_venue_id_in_geoshape_of_zrr (#490)
    * Join_on_code_geo_and_venue_id_in_geoshape_of_zrr
    
    * PC-11675-Remove-longitude_copy
    
    Co-authored-by: Dan SLAMA <d.slama@groupeonepoint.com>

--- orchestration/dags/dependencies/data_analytics/enriched_data/venue_locations.py
+++ orchestration/dags/dependencies/data_analytics/enriched_data/venue_locations.py
@@ -12,12 +12,14 @@ def define_table_venue_locations(dataset, table_prefix=""):
             commune_qp as qpv_communes,
             epci.epci_code,
             epci.epci_name,
+            ZRR.CODGEO,
             ZRR.LIBGEO,
             ZRR.ZRR_SIMP,
             ZRR.ZONAGE_ZRR,
-        FROM `{dataset}.QPV`, `{dataset}.{table_prefix}venue` venue, `{dataset}.epci` epci, `{dataset}.ZRR` ZRR
+        FROM `{dataset}.QPV`, `{dataset}.{table_prefix}venue` venue, `{dataset}.epci` epci, `{dataset}.ZRR` ZRR,`{dataset}.correspondance_code_insee` table_insee
         where ST_CONTAINS(geoshape, ST_GEOGPOINT(venue.venue_longitude,venue.venue_latitude)) is true 
               and venue.venue_latitude>-90 and venue.venue_latitude <90 and venue.venue_longitude >-90 
               and venue.venue_longitude <90 and ST_CONTAINS(epci.geo_shape, ST_GEOGPOINT(venue.venue_longitude,venue.venue_latitude)) is true
-              and ZRR.Code_Postal = venue.venue_postal_code;
+              and ST_CONTAINS(table_insee.geo_shape_insee, ST_GEOGPOINT(venue.venue_longitude,venue.venue_latitude)) is true
+              and cast(ZRR.CODGEO as string) = table_insee.insee_com;
     """

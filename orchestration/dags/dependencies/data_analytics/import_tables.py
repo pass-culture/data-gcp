d5eaae5a0b78adaca6b3820175259087de692015
adrienlpassculture <61141187+adrienlpassculture@users.noreply.github.com>
2021-11-18 15:05:21 +0100

    add booking_reimbursement_date and educational_booking_id (#507)
    * add booking_reimbursement_date and educational_booking_id
    
    * fix linter
    
    Co-authored-by: adrienlin <lin.adrien@gmail.com>

--- orchestration/dags/dependencies/data_analytics/import_tables.py
+++ orchestration/dags/dependencies/data_analytics/import_tables.py
@@ -77,7 +77,9 @@ def define_import_query(
             "isCancelled" as booking_is_cancelled, "isUsed" as booking_is_used, "dateUsed" as booking_used_date,
             "cancellationDate" as booking_cancellation_date,
             CAST("cancellationReason" AS VARCHAR) AS booking_cancellation_reason,
-            CAST("individualBookingId" AS varchar(255)) as individual_booking_id
+            CAST("individualBookingId" AS varchar(255)) as individual_booking_id,
+            CAST("educationalBookingId" AS varchar(255)) as educational_booking_id,
+            "reimbursementDate" AS booking_reimbursement_date
         FROM public.booking
     """
     cloudsql_queries[

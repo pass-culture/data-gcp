8ce57f7c5ce782ce3dfc29a98b72b57c6b2a15fd
dcuesta-pass <daniel.cuesta@passculture.app>
2021-12-06 11:15:26 +0100

    PC-11838 fix split_data.py arguments

--- algo_training/split_data.py
+++ algo_training/split_data.py
@@ -3,8 +3,8 @@ import pandas as pd
 from utils import STORAGE_PATH, MODEL_NAME
 
 
-def split_data(storage_path: str):
-    if MODEL_NAME == "v1":
+def split_data(storage_path: str,model_name: str):
+    if model_name == "v1":
         bookings = pd.read_csv(f"{storage_path}/clean_data.csv")
 
         df = bookings.sample(frac=1).reset_index(drop=True)
@@ -23,13 +23,14 @@ def split_data(storage_path: str):
         positive_data_test.to_csv(f"{storage_path}/positive_data_test.csv", index=False)
         positive_data_eval.to_csv(f"{storage_path}/positive_data_eval.csv", index=False)
 
-    if MODEL_NAME == "v2_deep_reco":
+    if model_name == "v2_deep_reco":
+        clicks_light=pd.read_csv(f"{storage_path}/clean_data.csv")
         clicks_train_light = clicks_light[clicks_light.train_set == True]
         clicks_test_light = clicks_light[clicks_light.train_set == False]
         clicks_train_light.to_csv(
-            f"{STORAGE_PATH}/positive_data_train.csv", index=False
+            f"{storage_path}/positive_data_train.csv", index=False
         )
-        clicks_test_light.to_csv(f"{STORAGE_PATH}/positive_data_test.csv", index=False)
+        clicks_test_light.to_csv(f"{storage_path}/positive_data_test.csv", index=False)
 
 
 if __name__ == "__main__":

cc85ac7fe837dd01f582171123412adc79929fdb
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2021-12-09 16:34:19 +0100

    [PC-9923] add offer is bookable in enriched offer (#532)
    * add offer is bookable in enriched offer
    
    * change dataset prefix
    
    * remove useless comma
    
    * remove prefix for enriched stock data
    
    * PC-9923 fix enriched_offer query
    
    * add enriched_stock in input test data
    
    * change enriched_stock name in input data
    
    * PC-9923 WIP
    
    * PC-9923 update config.py BIGQUERY_SCHEMAS
    
    Co-authored-by: dcuesta-pass <daniel.cuesta@passculture.app>

--- analytics/tests/data.py
+++ analytics/tests/data.py
@@ -197,6 +197,24 @@ ENRICHED_OFFER_DATA_INPUT = {
             "stock_fields_updated": "{}",
         },
     ],
+    "enriched_stock_data": [
+        {
+            "stock_id": "1",
+            "offer_id": "3",
+            "available_stock_information": "8",
+            "stock_booking_limit_date": "2019-11-23",
+            "stock_beginning_date": "2019-11-24",
+            "stock_price": "0",
+        },
+        {
+            "stock_id": "2",
+            "offer_id": "4",
+            "available_stock_information": "12",
+            "stock_booking_limit_date": None,
+            "stock_beginning_date": None,
+            "stock_price": "0",
+        },
+    ],
     "user": [
         {
             "user_id": "1",
@@ -320,6 +338,7 @@ ENRICHED_OFFER_DATA_EXPECTED = [
         "casting": "[]",
         "offer_is_educational": False,
         "offer_is_underage_selectable": True,
+        "offer_is_bookable": False,
     },
     {
         "offer_id": "4",
@@ -364,6 +383,7 @@ ENRICHED_OFFER_DATA_EXPECTED = [
         "casting": None,
         "offer_is_educational": False,
         "offer_is_underage_selectable": True,
+        "offer_is_bookable": True,
     },
 ]
 

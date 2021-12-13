fd1c3355bf39f74ea938ea45e150d7f6be501f23
Dan SLAMA <d.slama@groupeonepoint.com>
2021-12-13 15:31:32 +0100

    Update_function_for_table_location

--- orchestration/dags/dependencies/data_analytics/enriched_data/venue_locations.py
+++ orchestration/dags/dependencies/data_analytics/enriched_data/venue_locations.py
@@ -1,25 +1,70 @@
 def define_table_venue_locations(dataset, table_prefix=""):
     return f"""
-    SELECT 
-            venue.venue_id,
-            venue.venue_city,
-            venue.venue_postal_code,
-            venue.venue_department_code,
-            venue.venue_latitude,
-            venue.venue_longitude,
-            code_quartier as code_qpv,
-            noms_des_communes_concernees as qpv_name,
-            commune_qp as qpv_communes,
-            epci.epci_code,
-            epci.epci_name,
-            ZRR.CODGEO,
-            ZRR.LIBGEO,
-            ZRR.ZRR_SIMP,
-            ZRR.ZONAGE_ZRR,
-        FROM `{dataset}.QPV`, `{dataset}.{table_prefix}venue` venue, `{dataset}.epci` epci, `{dataset}.ZRR` ZRR,`{dataset}.correspondance_code_insee` table_insee
-        where ST_CONTAINS(geoshape, ST_GEOGPOINT(venue.venue_longitude,venue.venue_latitude)) is true 
-              and venue.venue_latitude>-90 and venue.venue_latitude <90 and venue.venue_longitude >-90 
-              and venue.venue_longitude <90 and ST_CONTAINS(epci.geo_shape, ST_GEOGPOINT(venue.venue_longitude,venue.venue_latitude)) is true
-              and ST_CONTAINS(table_insee.geo_shape_insee, ST_GEOGPOINT(venue.venue_longitude,venue.venue_latitude)) is true
-              and cast(ZRR.CODGEO as string) = table_insee.insee_com;
-    """
+     with venue_epci AS (
+            SELECT 
+            venue_id,
+            epci_code,
+            epci_name
+            FROM `{dataset}.{table_prefix}venue` venue
+            JOIN (SELECT 
+                        epci.epci_code,
+                        epci.epci_name, 
+                        geo_shape
+                        FROM `{dataset}.epci` epci) c on ST_CONTAINS(c.geo_shape, ST_GEOGPOINT(venue.venue_longitude,venue.venue_latitude))),
+            
+            venue_qpv AS (
+            SELECT 
+            venue_id,
+            code_qpv,
+            qpv_name,
+            qpv_communes
+            FROM `{dataset}.{table_prefix}venue` venue 
+             JOIN (SELECT code_quartier as code_qpv,
+                                    noms_des_communes_concernees as qpv_name,
+                                    commune_qp as qpv_communes,
+                                    geoshape
+                                FROM `{dataset}.QPV` )b ON ST_CONTAINS(b.geoshape, ST_GEOGPOINT(venue.venue_longitude,venue.venue_latitude))),
+            
+            venue_zrr AS (
+            SELECT 
+            venue_id,
+            CODGEO,
+            LIBGEO,
+            ZRR_SIMP,
+            ZONAGE_ZRR, 
+            Code_Postal, 
+            geo_shape_insee
+            FROM `{dataset}.{table_prefix}venue` venue 
+             JOIN (SELECT 
+                        ZRR.CODGEO,
+                        ZRR.LIBGEO,
+                        ZRR.ZRR_SIMP,
+                        ZRR.ZONAGE_ZRR, 
+                        ZRR.Code_Postal, 
+                        ZRR.geo_shape_insee
+                        FROM `{dataset}.ZRR` ZRR) d on ST_CONTAINS(d.geo_shape_insee, ST_GEOGPOINT(venue.venue_longitude,venue.venue_latitude)))
+            
+            
+        SELECT 
+                    venue.venue_id,
+                    venue.venue_city,
+                    venue.venue_postal_code,
+                    venue.venue_department_code,
+                    venue.venue_latitude,
+                    venue.venue_longitude, 
+                    venue_epci.epci_name,
+                    venue_epci.epci_code, 
+                    venue_qpv.code_qpv,
+                    venue_qpv.qpv_name,
+                    venue_qpv.qpv_communes,
+                    venue_zrr.CODGEO,
+                    venue_zrr.LIBGEO,
+                    venue_zrr.ZRR_SIMP,
+                    venue_zrr.ZONAGE_ZRR, 
+                    venue_zrr.Code_Postal
+                    FROM `{dataset}.{table_prefix}venue` venue
+        LEFT JOIN venue_epci ON venue.venue_id = venue_epci.venue_id
+        LEFT JOIN venue_qpv ON venue.venue_id = venue_qpv.venue_id
+        LEFT JOIN venue_zrr ON venue.venue_id = venue_zrr.venue_id
+        WHERE venue.venue_is_virtual is false;
+"""

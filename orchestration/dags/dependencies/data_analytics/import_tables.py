25e86d31bbde326bffb25473932a35c5260120ea
MatthieuRipollPassCulture <matthieu.ripoll+1@passculture.app>
2022-01-10 11:04:00 +0100

    add variables to benef fraud check table

--- orchestration/dags/dependencies/data_analytics/import_tables.py
+++ orchestration/dags/dependencies/data_analytics/import_tables.py
@@ -331,13 +331,17 @@ def define_import_query(
     cloudsql_queries[
         "beneficiary_fraud_check"
     ] = r"""
-            SELECT id, datecreated, user_id, type, thirdpartyid
+            SELECT id, datecreated, user_id, type, reason, reasonCodes, status, eligibility_type, thirdpartyid
                 ,regexp_replace(content, \'"(email|phone|lastName|birthDate|firstName|phoneNumber|reason_code|account_email|last_name|birth_date|first_name|phone_number)": "[^"]*",\' ,\'"\\1":"XXX",\', \'g\') as result_content
                 FROM (
                 SELECT CAST("id" AS varchar(255)) AS id
                     ,"dateCreated" AS datecreated
                     ,CAST("userId" AS varchar(255)) AS user_id
                     ,type AS type
+                    ,"reason" AS reason
+                    ,"reasonCodes"[1] AS reasonCodes
+                    ,"status" AS status
+                    ,"eligibilityType" AS eligibility_type
                     ,"thirdPartyId" AS thirdpartyid
                     ,CAST("resultContent" AS text) as content 
                     FROM public.beneficiary_fraud_check

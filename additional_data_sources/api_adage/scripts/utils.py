d16b9ab12332b06c3b307df295bf7167c8daa574
Dan SLAMA <d.slama@groupeonepoint.com>
2022-01-19 13:45:57 +0100

    Scraping_api_adage

---
+++ additional_data_sources/api_adage/scripts/utils.py
@@ -0,0 +1,8 @@
+import requests
+import pandas as pd
+import pandas_gbq as gbq
+from datetime import datetime
+
+
+API_KEY = "4e2a0208-cfa8-4294-bceb-e24a3277d842"
+ENDPOINT = 'https://omogen-api-pr.phm.education.gouv.fr/adage-api-test/v1'

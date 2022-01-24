6fccef1f93a70eb9f40526d84c14da28392be195
dcuesta-pass <daniel.cuesta@passculture.app>
2022-01-24 16:59:47 +0100

    PC-12799bis update and_clause on booking number

--- recommendation/api/not_eac/scoring.py
+++ recommendation/api/not_eac/scoring.py
@@ -21,7 +21,7 @@ def get_intermediate_recommendations_for_user(
     user_id: int, user_iris_id: int
 ) -> List[Dict[str, Any]]:
     if ENV_SHORT_NAME == "prod":
-        and_clause = "AND booking_number > 5"
+        and_clause = "AND booking_number > 10"
     else:
         and_clause = ""
     if not user_iris_id:

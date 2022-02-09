44fa4e3f2e1ac75148423729619b4ef9939a42cb
dcuesta-pass <daniel.cuesta@passculture.app>
2022-02-09 17:21:13 +0100

    WIP

--- additional_data_sources/dms-function-source/main.py
+++ additional_data_sources/dms-function-source/main.py
@@ -142,7 +142,11 @@ def mergeDictionary(dict_1, dict_2):
     dict_3 = {**dict_1, **dict_2}
     for key, value in dict_3.items():
         if key in dict_1 and key in dict_2:
-            dict_3[key] = [value, dict_1[key]]
+            if isinstance(dict_1[key], list):
+                list_value=[value]
+                dict_3[key]=list_value+ dict_1[key]
+            else:
+                dict_3[key] = [value, dict_1[key]]
     return dict_3
 
 def save_json(json_object, filename):

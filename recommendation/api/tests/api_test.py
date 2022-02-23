095fc19d9721c3e7127d9867288cb21116baefe5
dcuesta-pass <daniel.cuesta@passculture.app>
2022-02-23 11:10:33 +0100

    PC-12895 fix api_test

--- recommendation/api/tests/api_test.py
+++ recommendation/api/tests/api_test.py
@@ -76,4 +76,7 @@ def test_recommendation_return_recommended_offers_when_there_is_right_token(
 
     # Then
     assert response.status_code == 200
-    assert response.data == b'{"recommended_offers":[3,2,1]}\n'
+    assert (
+        response.data
+        == b'{"recommended_offers": [3, 2, 1],"AB_test": "A","reco_origin": "ColdStart"}\n'
+    )

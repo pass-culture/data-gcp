b79a945dde3868f1f2bdd699f23365740ef805f8
Dan SLAMA <d.slama@groupeonepoint.com>
2022-03-02 17:09:54 +0100

    Correction

--- analytics/tests/config.py
+++ analytics/tests/config.py
@@ -43,12 +43,12 @@ BIGQUERY_SCHEMAS = {
         "educational_institution_id": "STRING",
         "educational_institution_institution_id": "STRING",
     },
-    "user_suspension" :{
-        "id" : "STRING",
-        "userId" :"STRING",
-        "eventType":"STRING",
-        "eventDate":"DATETIME",
-        "actorUserId":"STRING",
+    "user_suspension": {
+        "id": "STRING",
+        "userId": "STRING",
+        "eventType": "STRING",
+        "eventDate": "DATETIME",
+        "actorUserId": "STRING",
         "reasonCode": "STRING",
     },
     "favorite": {

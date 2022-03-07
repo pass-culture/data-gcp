159b77587c71439bf17facaeb77b94314377b926
Dan SLAMA <d.slama@groupeonepoint.com>
2022-03-07 19:49:10 +0100

    Create_table

--- orchestration/dags/import_contentful_home.py
+++ orchestration/dags/import_contentful_home.py
@@ -5,8 +5,10 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from dependencies.slack_alert import task_fail_slack_alert
 from dependencies.import_contentful_file import (
     load_from_csv_contentful_file,
+    create_table_contentful_home,
 )
 from dependencies.config import (
     GCP_PROJECT,
@@ -20,7 +22,7 @@ default_dag_args = {
 }
 
 dag = DAG(
-    "import_contentful_home",
+    "create_contentful_home",
     default_args=default_dag_args,
     description="Import from storage contentful home",
     on_failure_callback=task_fail_slack_alert,
@@ -31,12 +33,18 @@ dag = DAG(
 
 start = DummyOperator(task_id="start", dag=dag)
 
-
-create_contentful_home = BigQueryOperator(
-    task_id="create_contentful_home",
-    python_callable=load_from_csv_contentful_file(),
+create_contentful_home_table = BigQueryOperator(
+    task_id="create_contentful_home_table",
+    sql=create_table_contentful_home(),
     use_legacy_sql=False,
-    destination_dataset_table=f"{GCP_PROJECT}.{BIGQUERY_ANALYTICS_DATASET}.contentful_home",
+    write_disposition="WRITE_TRUNCATE",
+    dag=dag,
+)
+
+create_contentful_home = PythonOperator(
+    task_id="create_contentful_home",
+    python_callable=load_from_csv_contentful_file,
+    use_legacy_sql=False,
     write_disposition="WRITE_TRUNCATE",
     dag=dag,
 )
@@ -45,4 +53,4 @@ create_contentful_home = BigQueryOperator(
 end = DummyOperator(task_id="end", dag=dag)
 
 
-(start >> create_contentful_home >> end)
+(start >> create_contentful_home_table >> create_contentful_home >> end)

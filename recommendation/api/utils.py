4a5526b6dbf79855483eb23a759e52d258641ec3
dcuesta-pass <daniel.cuesta@passculture.app>
2022-03-15 12:29:35 +0100

    [PC-13981] update URL initialisation

--- recommendation/api/utils.py
+++ recommendation/api/utils.py
@@ -41,7 +41,7 @@ query_string = dict(
 def create_pool():
     print("SQL_BASE_USER:", SQL_BASE_USER)
     return create_engine(
-        engine.url.URL(
+        engine.url.URL.create(
             drivername="postgresql+pg8000",
             username=SQL_BASE_USER,
             password=SQL_BASE_PASSWORD,

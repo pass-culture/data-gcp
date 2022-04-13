1fc1191d28616e57ddb5ca6effc103ad73997cb9
dcuesta-pass <daniel.cuesta@passculture.app>
2022-04-13 17:43:17 +0200

    [PC-14100] fix default value for ENV_SHORT_NAME

--- analytics/scripts/import_subcategories_model/export_subcategories_from_native_definition.py
+++ analytics/scripts/import_subcategories_model/export_subcategories_from_native_definition.py
@@ -4,7 +4,7 @@ from api.src.pcapi.core.categories.subcategories import *
 from api.src.pcapi.core.categories.subcategories import ALL_SUBCATEGORIES
 
 GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "default")
-ENV_SHORT_NAME = os.environ.get("ENV_SHORT_NAME", "ehp")
+ENV_SHORT_NAME = os.environ.get("ENV_SHORT_NAME", "")
 ALL_SUBCAT_DICT = []
 for subcats in ALL_SUBCATEGORIES:
     ALL_SUBCAT_DICT.append(subcats.__dict__)

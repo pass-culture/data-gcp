11582bb320c6af5b4503d0b53dce42f7c5f8a44b
Cyrille Delabre <cyrille.delabre@passculture.app>
2022-05-02 18:38:49 +0200

    Fix + add secrets

--- orchestration/dags/common/alerts.py
+++ orchestration/dags/common/alerts.py
@@ -3,7 +3,7 @@ from urllib.parse import quote
 from airflow import configuration
 from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
 
-import datetime
+from datetime import datetime
 from dependencies.access_gcp_secrets import access_secret_data
 from dependencies.config import GCP_PROJECT, ENV_SHORT_NAME
 
@@ -17,16 +17,19 @@ ENV_EMOJI = {
 
 JOB_TYPE = {
     "analytics": access_secret_data(
-        GCP_PROJECT, "slack-composer-analytics-webhook-token"
+        GCP_PROJECT,
+        "slack-composer-analytics-webhook-token",
+        version_id=2,
+        default=None,
     ),
     "prod": access_secret_data(
-        GCP_PROJECT, "slack-composer-prod-webhook-token", default=None
+        GCP_PROJECT, "slack-composer-prod-webhook-token", version_id=2, default=None
     ),
     "stg": access_secret_data(
-        GCP_PROJECT, "slack-composer-stg-webhook-token", default=None
+        GCP_PROJECT, "slack-composer-ehp-webhook-token", version_id=2, default=None
     ),
     "dev": access_secret_data(
-        GCP_PROJECT, "slack-composer-dev-webhook-token", default=None
+        GCP_PROJECT, "slack-composer-ehp-webhook-token", version_id=2, default=None
     ),
 }
 
@@ -40,6 +43,8 @@ def analytics_fail_slack_alert(context):
 
 
 def __task_fail_slack_alert(context, job_type):
+    run_id = context.get("templates_dict", {}).get("run_id", "")
+    status_str = "_(scheduled)_" if run_id.startswith("scheduled__") else "_(manual)_"
     webhook_token = JOB_TYPE.get(job_type)
 
     dag_url = "{base_url}/admin/airflow/graph?dag_id={dag_id}&execution_date={exec_date}".format(
@@ -55,9 +60,10 @@ def __task_fail_slack_alert(context, job_type):
     )
 
     slack_msg = f"""
-            {ENV_EMOJI[ENV_SHORT_NAME]}: Task <{task_url}|{task_name}> has failed!
-            *Dag*: <{dag_url}>|{dag_name}
-            *Execution Time*: {execution_date}
+            {ENV_EMOJI[ENV_SHORT_NAME]}: 
+            *Task* <{task_url}|{task_name}> has failed!
+            *Dag*: <{dag_url}|{dag_name}>
+            *Execution Time*: {execution_date} {status_str}
             """
 
     failed_alert = SlackWebhookOperator(

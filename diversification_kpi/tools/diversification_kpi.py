99ad6504f3a4f7e5210317bd59bcef3ba3241cf1
Colin ZACCOMER <c.zaccomer@groupeonepoint.com>
2022-05-04 15:57:25 +0200

    fix qpi value is NA

--- diversification_kpi/tools/diversification_kpi.py
+++ diversification_kpi/tools/diversification_kpi.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def fuse_columns_into_format(is_physical_good, is_digital_good, is_event):
@@ -118,9 +119,10 @@ def calculate_diversification_per_feature(df_clean, features):
             if not qpi_point_given:
                 subcat = booking["subcategory"]
                 if subcat in df_clean.columns:
-                    if not booking[subcat]:
-                        qpi_point_given = True
-                        qpi_diversification = 1
+                    if not pd.isna(booking[subcat]):
+                        if not booking[subcat]:
+                            qpi_point_given = True
+                            qpi_diversification = 1
             divers_per_feature["qpi_diversification"].append(qpi_diversification)
 
             delta_div = (

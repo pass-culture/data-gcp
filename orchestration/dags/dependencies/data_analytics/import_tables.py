e82efd9fc8c6292bdad5cb02426827e9506dc8e6
Lucille-Artgd <78435671+Lucille-Artgd@users.noreply.github.com>
2022-05-04 12:27:08 +0200

    create enriched_institution_data (#738)

--- orchestration/dags/dependencies/data_analytics/import_tables.py
+++ orchestration/dags/dependencies/data_analytics/import_tables.py
@@ -497,6 +497,12 @@ def define_import_query(
             SELECT
             CAST(id AS varchar(255)) AS educational_institution_id
             ,CAST("institutionId" AS varchar(255)) AS educational_institution_institution_id
+            ,"city" AS institution_city
+            ,"name" AS institution_name 
+            ,"postalCode" AS institution_postal_code
+            ,CASE 
+                    WHEN ("postalCode" LIKE \\'97%\\' OR "postalCode" LIKE \\'98%\\') THEN SUBSTRING("postalCode",1,3) 
+                    ELSE SUBSTRING("postalCode",1,2) END AS institution_departement_code
             FROM educational_institution
         """
 

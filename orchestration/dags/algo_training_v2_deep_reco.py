8f9935756de67ff6c6a8b8a4c68d0eead09797e5
Colin ZACCOMER <c.zaccomer@groupeonepoint.com>
2022-05-09 15:17:48 +0200

    PC-14851 Delete old model version after deploying a new one

--- orchestration/dags/algo_training_v2_deep_reco.py
+++ orchestration/dags/algo_training_v2_deep_reco.py
@@ -25,6 +25,7 @@ else:
 DATE = "{{ts_nodash}}"
 STORAGE_PATH = f"gs://{MLFLOW_BUCKET_NAME}/algo_training_v2_deep_reco_{ENV_SHORT_NAME}/algo_training_v2_deep_reco_{DATE}"
 MODEL_NAME = "v2_deep_reco"
+AI_MODEL_NAME = f"deep_reco_{ENV_SHORT_NAME}"
 SLACK_CONN_ID = "slack"
 SLACK_CONN_PASSWORD = access_secret_data(GCP_PROJECT_ID, "slack-conn-password")
 
@@ -213,6 +214,42 @@ with DAG(
         bash_command=DEPLOY_COMMAND,
     )
 
+    list_model_versions = BashOperator(
+        task_id="list_model_versions",
+        bash_command="gcloud ml-engine versions list "
+        "--model=tf_model_reco_{{ENV_SHORT_NAME}} "
+        "--region=europe-west1 "
+        "--project={{params.project}} "
+        "--sort-by=~NAME",
+        xcom_push=True,
+        params={
+            "model": AI_MODEL_NAME,
+            "project": GCP_PROJECT_ID,
+        },
+        dag=dag,
+    )
+
+    get_version_task = BashOperator(
+        task_id="get_version_task",
+        bash_command="echo '{{ ti.xcom_pull(task_ids='list_model_versions') }}' | awk '{print $1}'",
+        xcom_push=True,
+        dag=dag,
+    )
+
+    delete_last_version = BashOperator(
+        task_id="delete_last_version",
+        bash_command="gcloud ml-engine versions delete {{ ti.xcom_pull(task_ids='get_version_task') }} "
+        "--model {{params.model}} "
+        "--region=europe-west1 "
+        "--project={{params.project}}",
+        xcom_push=True,
+        params={
+            "model": AI_MODEL_NAME,
+            "project": GCP_PROJECT_ID,
+        },
+        dag=dag,
+    )
+
     SLACK_BLOCKS = [
         {
             "type": "section",
@@ -268,5 +305,8 @@ with DAG(
         >> postprocess
         >> gce_instance_stop
         >> deploy_model
+        >> list_model_versions
+        >> get_version_task
+        >> delete_last_version
         >> send_slack_notif_success
     )

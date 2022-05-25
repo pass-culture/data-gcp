cca850086e8dfcbaaccf8fa60ea56b2e285f2a8d
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2022-05-25 12:16:20 +0200

    Pc 14485 data brancher la table enriched educational booking sur collective booking (#699)
    * from educational to collective booking table
    
    * format files
    
    * fix orchestration tests
    
    * fix import analytics incremental test
    
    * get booking amount from collective stock price
    
    * PC-14163-correct-compute-monitoring-dag-error-on-NA (#679)
    
    Co-authored-by: Colin ZACCOMER <c.zaccomer@groupeonepoint.com>
    
    * [PC-14475] create enriched deposit data (#695)
    
    * create enriched deposit data
    
    * fix import function in import analytics
    
    * fix dags test
    
    * fix SQL errors
    
    * fix calculation
    
    * add withdrawal columns in offer table (#700)
    
    * add filter column in firebase events (#694)
    
    * Replace by sqll (#697)
    
    * Replace by sqll
    
    * Clean code
    
    * Delete description
    
    * retire la limite
    
    * Add time
    
    * Clean code
    
    * Modify
    
    * compute diversification by batch
    
    * fix dataset env
    
    * fix syntax
    
    * fix
    
    * final fix hopefully
    
    * execute dag weekly
    
    * format files
    
    Co-authored-by: Dan SLAMA <d.slama@groupeonepoint.com>
    Co-authored-by: Colin ZACCOMER <c.zaccomer@groupeonepoint.com>
    
    * [fix-slack-alert-emoji] fix emoji naming
    
    * [PC-14100bis-import-subcategories-model] fx import
    
    * [PC-14100bis] fix bash script
    
    * [PC-14100bis] rm subcat matching type refs
    
    * PC-14197 Add firebase pro data to firebase dag (#689)
    
    * PC-14197 Add firebase pro data to firebase dag
    
    * format files
    
    * change table name
    
    * update tests
    
    Co-authored-by: Colin ZACCOMER <c.zaccomer@groupeonepoint.com>
    
    * Pc 14197 add firebase pro data (#709)
    
    * PC-14197 Add firebase pro data to firebase dag
    
    * format files
    
    * change table name
    
    * update tests
    
    * PC-14197-Fix-import-firebase-pro
    
    Co-authored-by: Colin ZACCOMER <c.zaccomer@groupeonepoint.com>
    
    * [PC-13959] rollback import_analytics_incremental
    
    * [PC-13959] rollback import_tables
    
    * [PC-13959] fix formatting
    
    * [PC-13959] rm duplicates
    
    * [PC-13959] fix dag test task number
    
    * [PC-13959] update dag name and start date
    
    * [PC-13959] update dag version in dag_tests
    
    * [PC-14622] rm imprt beneficiary fraud result tbl
    
    * [PC-14622] rm wrong commit
    
    * [PC-14622] fix dag_test task number
    
    * [PC-14660] refacto to import to analytics
    
    * [fix-remove-unused-dummy-task] remove airflow task (#713)
    
    * [fix-remove-unused-dummy-task] remove airflow task
    
    * [fix-remove-unused-dummy-task] fix dag tests
    
    * [PC-9130] Refactor Tests v1 (#711)
    
    * from educational to collective booking table
    
    * format files
    
    * [PC-14475] create enriched deposit data (#695)
    
    * create enriched deposit data
    
    * fix import function in import analytics
    
    * fix dags test
    
    * fix SQL errors
    
    * fix calculation
    
    * [PC-14622] rm wrong commit
    
    * [fix-algo-monitoring-env-vars] (#715)
    
    * [fix-algo-monitoring-env-vars] WIP
    
    * [fix-algo-monitoring-env-vars] updt ab test table
    
    * [fix-algo-monitoring-env-vars] updt monitoring
    
    * [fix-algo-monitoring-env-vars] fix dag name
    
    * [fix-algo-monitoring-env-vars] fix dag start date
    
    Co-authored-by: ColinZaccomer <93599912+ColinZaccomer@users.noreply.github.com>
    Co-authored-by: Colin ZACCOMER <c.zaccomer@groupeonepoint.com>
    Co-authored-by: danslama2310 <85947191+danslama2310@users.noreply.github.com>
    Co-authored-by: Dan SLAMA <d.slama@groupeonepoint.com>
    Co-authored-by: dcuesta-pass <daniel.cuesta@passculture.app>
    Co-authored-by: dcuesta-pass <82374920+dcuesta-pass@users.noreply.github.com>
    Co-authored-by: cdelabre <103637148+cdelabre@users.noreply.github.com>

---
+++ orchestration/dags/dependencies/data_analytics/enriched_data/collective_booking.py
@@ -0,0 +1,62 @@
+def create_collective_booking_ranking_view(dataset, table_prefix=""):
+    return f"""
+        CREATE TEMP TABLE collective_booking_ranking_view AS (
+            SELECT
+                collective_booking.collective_booking_id,
+                rank() OVER (PARTITION BY collective_booking.collective_booking_educational_institution_id ORDER BY collective_booking.collective_booking_creation_date) AS collective_booking_rank
+            FROM {dataset}.{table_prefix}collective_booking AS collective_booking);
+        """
+
+
+def define_enriched_collective_booking_query(dataset, table_prefix=""):
+    return f"""
+        CREATE OR REPLACE TABLE {dataset}.enriched_collective_booking_data AS (
+SELECT
+    collective_booking.collective_booking_id
+    , collective_booking.booking_id
+    , collective_booking.collective_booking_collective_stock_id AS collective_stock_id
+    , collective_stock.collective_stock_stock_id AS stock_id
+    , collective_stock.collective_stock_collective_offer_id AS collective_offer_id
+    , collective_offer_offer_id AS offer_id
+    , collective_offer.collective_offer_name
+    , collective_offer.collective_offer_subcategory_id
+    , collective_offer.collective_offer_venue_id AS venue_id
+    , venue.venue_name
+    , venue.venue_department_code
+    , collective_booking.collective_booking_offerer_id AS offerer_id
+    , offerer.offerer_name
+    , collective_stock.collective_stock_price AS booking_amount
+    , collective_stock.collective_stock_number_of_tickets AS number_of_tickets
+    , collective_booking.collective_booking_educational_institution_id AS educational_institution_id
+    , collective_booking.collective_booking_educational_year_id AS educational_year_id
+    , collective_booking.collective_booking_educational_redactor_id AS educational_redactor_id
+    , eple.nom_etablissement
+    , eple.code_departement AS school_department_code
+    , eple.libelle_academie
+    , collective_booking.collective_booking_creation_date
+    , collective_booking.collective_booking_cancellation_date
+    , collective_booking.collective_booking_status
+    , collective_booking.collective_booking_cancellation_reason
+    , collective_booking.collective_booking_confirmation_date
+    , collective_booking.collective_booking_confirmation_limit_date
+    , collective_booking.collective_booking_used_date
+    , collective_booking.collective_booking_reimbursement_date
+    , collective_booking_ranking_view.collective_booking_rank
+FROM {dataset}.{table_prefix}collective_booking AS collective_booking
+INNER JOIN {dataset}.{table_prefix}collective_stock AS collective_stock  ON collective_stock.collective_stock_id  = collective_booking.collective_booking_collective_stock_id
+INNER JOIN {dataset}.{table_prefix}collective_offer AS collective_offer  ON collective_offer.collective_offer_id = collective_stock.collective_stock_collective_offer_id
+INNER JOIN {dataset}.{table_prefix}venue AS venue ON collective_booking.collective_booking_venue_id = venue.venue_id
+INNER JOIN {dataset}.{table_prefix}offerer AS offerer ON offerer.offerer_id = venue.venue_managing_offerer_id
+INNER JOIN {dataset}.{table_prefix}educational_institution AS educational_institution  ON educational_institution.educational_institution_id = collective_booking.collective_booking_educational_institution_id
+LEFT JOIN  {dataset}.eple AS eple  ON eple.id_etablissement  = educational_institution.educational_institution_institution_id
+LEFT JOIN collective_booking_ranking_view ON collective_booking_ranking_view.collective_booking_id = collective_booking.collective_booking_id
+
+ );
+    """
+
+
+def define_enriched_collective_booking_full_query(dataset, table_prefix=""):
+    return f"""
+        {create_collective_booking_ranking_view(dataset=dataset, table_prefix=table_prefix)}
+        {define_enriched_collective_booking_query(dataset=dataset, table_prefix=table_prefix)}
+    """

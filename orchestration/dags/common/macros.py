2f3f21ba74399acfca745b9c954f3594dda31e81
Cyrille Delabre <cyrille.delabre@passculture.app>
2022-06-06 15:20:14 +0200

    Overwrite only daily partition in re-run

--- orchestration/dags/common/macros.py
+++ orchestration/dags/common/macros.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from dependencies.config import (
     BIGQUERY_ANALYTICS_DATASET,
     BIGQUERY_CLEAN_DATASET,
@@ -6,10 +8,20 @@ from dependencies.config import (
     GCP_PROJECT,
 )
 
+
+def yyyymmdd():
+    return datetime.today().strftime("%Y%m%d")
+
+def today():
+    return datetime.today().strftime("%Y-%m-%d")
+
+
 default = {
     "bigquery_analytics_dataset": BIGQUERY_ANALYTICS_DATASET,
     "bigquery_clean_dataset": BIGQUERY_CLEAN_DATASET,
     "bigquery_raw_dataset": BIGQUERY_RAW_DATASET,
     "env_short_name": ENV_SHORT_NAME,
     "gcp_project": GCP_PROJECT,
+    "yyyymmdd": yyyymmdd,
+    "today": today
 }
--- orchestration/dags/dependencies/data_analytics/import_historical.py
+++ orchestration/dags/dependencies/data_analytics/import_historical.py
@@ -1,42 +1,40 @@
-from dependencies.config import BIGQUERY_CLEAN_DATASET
-
 SQL_PATH = "dependencies/data_analytics/sql/clean/applicative_database/"
 
 
 historical_data_applicative_tables = {
     "booking_history": {
         "sql": f"{SQL_PATH}/booking_history.sql",
-        "destination_dataset_table": f"{BIGQUERY_CLEAN_DATASET}.booking_history",
+        "destination_dataset_table": "{{ bigquery_clean_dataset }}.booking_history${{ yyyymmdd() }}",
         "time_partitioning": {"field": "partition_date"},
         "cluster_fields": ["partition_date"],
     },
     "collective_booking_history": {
         "sql": f"{SQL_PATH}/collective_booking_history.sql",
-        "destination_dataset_table": f"{BIGQUERY_CLEAN_DATASET}.collective_booking_history",
+        "destination_dataset_table": "{{ bigquery_clean_dataset }}.collective_booking_history${{ yyyymmdd() }}",
         "time_partitioning": {"field": "partition_date"},
         "cluster_fields": ["partition_date"],
     },
     "collective_offer_history": {
         "sql": f"{SQL_PATH}/collective_offer_history.sql",
-        "destination_dataset_table": f"{BIGQUERY_CLEAN_DATASET}.collective_offer_history",
+        "destination_dataset_table": "{{ bigquery_clean_dataset }}.collective_offer_history${{ yyyymmdd() }}",
         "time_partitioning": {"field": "partition_date"},
         "cluster_fields": ["partition_date"],
     },
     "collective_stock_history": {
         "sql": f"{SQL_PATH}/collective_stock_history.sql",
-        "destination_dataset_table": f"{BIGQUERY_CLEAN_DATASET}.collective_stock_history",
+        "destination_dataset_table": "{{ bigquery_clean_dataset }}.collective_stock_history${{ yyyymmdd() }}",
         "time_partitioning": {"field": "partition_date"},
         "cluster_fields": ["partition_date"],
     },
     "offer_history": {
         "sql": f"{SQL_PATH}/offer_history.sql",
-        "destination_dataset_table": f"{BIGQUERY_CLEAN_DATASET}.offer_history",
+        "destination_dataset_table": "{{ bigquery_clean_dataset }}.offer_history${{ yyyymmdd() }}",
         "time_partitioning": {"field": "partition_date"},
         "cluster_fields": ["partition_date"],
     },
     "stock_history": {
         "sql": f"{SQL_PATH}/stock_history.sql",
-        "destination_dataset_table": f"{BIGQUERY_CLEAN_DATASET}.stock_history",
+        "destination_dataset_table": "{{ bigquery_clean_dataset }}.stock_history${{ yyyymmdd() }}",
         "time_partitioning": {"field": "partition_date"},
         "cluster_fields": ["partition_date"],
     },

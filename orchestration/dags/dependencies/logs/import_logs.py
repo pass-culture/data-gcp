cbcd62814c37a18817acba1ed312949305a75cc6
Cyrille Delabre <cyrille.delabre@passculture.app>
2022-06-23 11:00:08 +0200

    Add adage logs

---
+++ orchestration/dags/dependencies/logs/import_logs.py
@@ -0,0 +1,11 @@
+SQL_PATH = f"dependencies/logs/sql/analytics"
+
+
+import_tables = {
+    "adage_logs": {
+        "sql": f"{SQL_PATH}/adage_logs.sql",
+        "destination_dataset_table": "{{ bigquery_analytics_dataset }}.adage_logs${{ yyyymmdd(yesterday()) }}",
+        "time_partitioning": {"field": "partition_date"},
+        "cluster_fields": ["partition_date"],
+    },
+}
---
+++ orchestration/dags/jobs/import/import_logs.py
@@ -0,0 +1,53 @@
+import datetime
+from airflow import DAG
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryExecuteQueryOperator,
+)
+from airflow.operators.dummy_operator import DummyOperator
+from common import macros
+from dependencies.logs.import_logs import (
+    import_tables,
+)
+from common.config import DAG_FOLDER, GCP_PROJECT
+from common.alerts import analytics_fail_slack_alert
+
+
+default_dag_args = {
+    "start_date": datetime.datetime(2022, 6, 20),
+    "retries": 1,
+    "retry_delay": datetime.timedelta(minutes=5),
+    "project_id": GCP_PROJECT,
+}
+
+dag = DAG(
+    "import_logs",
+    default_args=default_dag_args,
+    description="Import tables from log sink",
+    on_failure_callback=analytics_fail_slack_alert,
+    schedule_interval="00 01 * * *",
+    catchup=True,
+    dagrun_timeout=datetime.timedelta(minutes=120),
+    user_defined_macros=macros.default,
+    template_searchpath=DAG_FOLDER,
+)
+
+
+start_import_logs_task = DummyOperator(task_id="start_import_logs_task", dag=dag)
+import_logs_tasks = []
+for table, params in import_tables.items():
+    task = BigQueryExecuteQueryOperator(
+        task_id=f"logs_{table}",
+        sql=params["sql"],
+        write_disposition="WRITE_TRUNCATE",
+        use_legacy_sql=False,
+        destination_dataset_table=params["destination_dataset_table"],
+        time_partitioning=params.get("time_partitioning", None),
+        cluster_fields=params.get("cluster_fields", None),
+        dag=dag,
+    )
+    import_logs_tasks.append(task)
+
+end_import_logs_task = DummyOperator(task_id="end_import_logs_task", dag=dag)
+
+
+start_import_logs_task >> import_logs_tasks >> end_import_logs_task

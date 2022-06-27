74b5bc766c8a1467d8692dd4ff41176daf43077b
cdelabre <103637148+cdelabre@users.noreply.github.com>
2022-06-27 13:26:21 +0200

    [PC-15919] Collective logs (#850)

--- orchestration/dags/dependencies/logs/import_logs.py
+++ orchestration/dags/dependencies/logs/import_logs.py
@@ -8,4 +8,10 @@ import_tables = {
         "time_partitioning": {"field": "partition_date"},
         "cluster_fields": ["partition_date"],
     },
+    "collective_logs": {
+        "sql": f"{SQL_PATH}/collective_logs.sql",
+        "destination_dataset_table": "{{ bigquery_analytics_dataset }}.collective_logs${{ yyyymmdd(ds) }}",
+        "time_partitioning": {"field": "partition_date"},
+        "cluster_fields": ["partition_date"],
+    },
 }

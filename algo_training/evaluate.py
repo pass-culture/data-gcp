a920fea46640ccfd599e5de45ccd3d974faddedf
dcuesta-pass <daniel.cuesta@passculture.app>
2022-07-07 16:54:17 +0200

    [PC-15877] WIP

--- algo_training/evaluate.py
+++ algo_training/evaluate.py
@@ -1,13 +1,8 @@
-import gcsfs
 import numpy as np
 import pandas as pd
-import pickle
 import tensorflow as tf
 import mlflow.tensorflow
-from metrics import compute_metrics
 from models.v1.match_model import MatchModel
-from models.v2.deep_reco.deep_match_model import DeepMatchModel
-from models.v2.mf_reco.matrix_factorization_model import MFModel
 from utils import (
     get_secret,
     connect_remote_mlflow,
@@ -16,37 +11,48 @@ from utils import (
     MODEL_NAME,
     GCP_PROJECT_ID,
 )
+from metrics import compute_metrics, get_actual_and_predicted
 
 RECOMMENDATION_NUMBER = 40
 
 
 def evaluate(model, storage_path: str, model_name):
 
-    if model_name == "v2_mf_reco":
-        positive_data_test = pd.read_csv(
-            f"{storage_path}/positive_data_test.csv",
-            dtype={"user_id": str, "offer_id": str, "offer_subcategoryid": str},
-        )
-        positive_data_train = pd.read_csv(
-            f"{storage_path}/positive_data_train.csv",
-            dtype={"user_id": str, "offer_id": str, "offer_subcategoryid": str},
-        )
-    else:
-        positive_data_test = pd.read_csv(
-            f"{storage_path}/positive_data_test.csv",
-            dtype={"user_id": str, "item_id": str, "offer_subcategoryid": str},
-        )
     positive_data_train = pd.read_csv(
         f"{storage_path}/positive_data_train.csv",
         dtype={"user_id": str, "item_id": str, "offer_subcategoryid": str},
     )
-    metrics = compute_metrics(
-        RECOMMENDATION_NUMBER,
-        positive_data_train,
-        positive_data_test,
-        model_name,
-        model,
+
+    positive_data_test = pd.read_csv(
+        f"{storage_path}/positive_data_test.csv",
+        dtype={"user_id": str, "item_id": str, "offer_subcategoryid": str},
     )
+
+    positive_data_test_clean = positive_data_test[
+        positive_data_test.user_id.isin(positive_data_train.user_id)
+    ]
+
+    positive_data_test_clean = positive_data_test_clean[
+        positive_data_test_clean.item_id.isin(positive_data_train.item_id)
+    ]
+    positive_data_test_clean = positive_data_test_clean.drop_duplicates()
+
+    data_model_dict = {
+        "name": model_name,
+        "data": {"train": positive_data_train, "test": positive_data_test_clean},
+        "model": model,
+    }
+
+    data_model_dict_w_metrics = compute_metrics(
+        get_actual_and_predicted(data_model_dict), RECOMMENDATION_NUMBER
+    )
+
+    metrics = {
+        f"recall_at_{k}": data_model_dict_w_metrics["metrics"]["mark"],
+        f"precision_at_{k}": data_model_dict_w_metrics["metrics"]["mapk"],
+        f"coverage_at_{k}": data_model_dict_w_metrics["metrics"]["coverage"],
+    }
+
     connect_remote_mlflow(client_id, env=ENV_SHORT_NAME)
     mlflow.log_metrics(metrics)
     print("------- EVALUATE DONE -------")
@@ -78,27 +84,10 @@ if __name__ == "__main__":
     experiment_name = f"algo_training_{MODEL_NAME}"
     experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
     run_id = mlflow.list_run_infos(experiment_id)[0].run_id
-    if MODEL_NAME == "v1":
-        with mlflow.start_run(run_id=run_id):
-            loaded_model = tf.keras.models.load_model(
-                mlflow.get_artifact_uri("model"),
-                custom_objects={"MatchModel": MatchModel},
-                compile=False,
-            )
-            evaluate(loaded_model, STORAGE_PATH, MODEL_NAME)
-    elif MODEL_NAME == "v2_deep_reco":
-        with mlflow.start_run(run_id=run_id):
-            loaded_model = tf.keras.models.load_model(
-                mlflow.get_artifact_uri("model"),
-                custom_objects={"DeepMatchModel": DeepMatchModel},
-                compile=False,
-            )
-            evaluate(loaded_model, STORAGE_PATH, MODEL_NAME)
-    elif MODEL_NAME == "v2_mf_reco":
-        with mlflow.start_run(run_id=run_id):
-            loaded_model = tf.keras.models.load_model(
-                mlflow.get_artifact_uri("model"),
-                custom_objects={"MFModel": MFModel},
-                compile=False,
-            )
-            evaluate(loaded_model, STORAGE_PATH, MODEL_NAME)
+    with mlflow.start_run(run_id=run_id):
+        loaded_model = tf.keras.models.load_model(
+            mlflow.get_artifact_uri("model"),
+            custom_objects={"MatchModel": MatchModel},
+            compile=False,
+        )
+        evaluate(loaded_model, STORAGE_PATH, MODEL_NAME)

a6392cebb0610da4045c3fb20e18134a298bc4ef
Cyrille Delabre <cyrille.delabre@passculture.app>
2022-07-19 14:24:20 +0200

    [PC-16214] Add table to Analytics & Remove old table

--- additional_data_sources/contentful-function-source/main.py
+++ additional_data_sources/contentful-function-source/main.py
@@ -4,6 +4,7 @@ from google.cloud import bigquery
 from contentful_client import ContentfulClient
 from utils import (
     BIGQUERY_RAW_DATASET,
+    BIGQUERY_ANALYTICS_DATASET,
     GCP_PROJECT,
     ENV_SHORT_NAME,
 )
@@ -14,7 +15,7 @@ CONTENTFUL_RELATIONSHIPS_TABLE_NAME = "contentful_relationships"
 CONTENTFUL_TAGS_TABLE_NAME = "contentful_tags"
 
 
-def save_modules_to_bq(modules_df, table_name):
+def save_raw_modules_to_bq(modules_df, table_name):
     _now = datetime.today()
     yyyymmdd = _now.strftime("%Y%m%d")
     modules_df["execution_date"] = _now
@@ -33,6 +34,31 @@ def save_modules_to_bq(modules_df, table_name):
     job.result()
 
 
+def save_clean_modules_to_bq(table_name, id_key):
+    bigquery_client = bigquery.Client()
+
+    df = bigquery_client.query(
+        f"""
+        SELECT * except(row_number)
+        FROM (
+            SELECT
+            *,
+            ROW_NUMBER() OVER (PARTITION BY {id_key} ORDER BY execution_date DESC) as row_number
+            FROM `{GCP_PROJECT}.{BIGQUERY_RAW_DATASET}.{table_name}`
+        ) inn
+        WHERE row_number=1
+        """
+    ).to_dataframe()
+
+    job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE")
+    job = bigquery_client.load_table_from_dataframe(
+        df,
+        f"{GCP_PROJECT}.{BIGQUERY_ANALYTICS_DATASET}.{table_name}",
+        job_config=job_config,
+    )
+    job.result()
+
+
 def run(request):
     """The Cloud Function entrypoint.
     Args:
@@ -41,7 +67,12 @@ def run(request):
     contentful_envs = {"prod": "production", "stg": "testing", "dev": "testing"}
     contentful_client = ContentfulClient(env=contentful_envs[ENV_SHORT_NAME])
     modules, links, tags = contentful_client.get_all_playlists()
-    save_modules_to_bq(modules, CONTENTFUL_ENTRIES_TABLE_NAME)
-    save_modules_to_bq(links, CONTENTFUL_RELATIONSHIPS_TABLE_NAME)
-    save_modules_to_bq(tags, CONTENTFUL_TAGS_TABLE_NAME)
+    save_raw_modules_to_bq(modules.drop_duplicates(), CONTENTFUL_ENTRIES_TABLE_NAME)
+    save_raw_modules_to_bq(links.drop_duplicates(), CONTENTFUL_RELATIONSHIPS_TABLE_NAME)
+    save_raw_modules_to_bq(tags.drop_duplicates(), CONTENTFUL_TAGS_TABLE_NAME)
+
+    save_clean_modules_to_bq(CONTENTFUL_ENTRIES_TABLE_NAME, id_key="id")
+    save_clean_modules_to_bq(CONTENTFUL_RELATIONSHIPS_TABLE_NAME, id_key="parent,child")
+    save_clean_modules_to_bq(CONTENTFUL_TAGS_TABLE_NAME, id_key="tag_id,entry_id")
+
     return "Done"

1bd2cd411046f12a795d8053cb18df41448ad280
cdelabre <103637148+cdelabre@users.noreply.github.com>
2022-07-20 15:32:41 +0200

    [PC-16211] Integrate open_data.raw_supply_data (#881)

--- orchestration/dags/dependencies/open_data/export_open_data.py
+++ orchestration/dags/dependencies/open_data/export_open_data.py
@@ -14,4 +14,10 @@ aggregated_open_data_tables = {
         "sql": f"{SQL_PATH}/aggregated_monthly_used_booking.sql",
         "destination_dataset_table": "{{ bigquery_open_data_project }}.{{ bigquery_open_data_public_dataset }}.aggregated_monthly_used_booking",
     },
+    "raw_supply_data": {
+        "sql": f"{SQL_PATH}/raw_supply_data.sql",
+        "destination_dataset_table": "{{ bigquery_open_data_project }}.{{ bigquery_open_data_public_dataset }}.raw_supply_data${{ yyyymmdd(current_month(ds)) }}",
+        "time_partitioning": {"field": "calculation_month"},
+        "cluster_fields": ["calculation_month"],
+    },
 }

60fda8aa88752fb2701b75e9e7709cf79be86523
cdelabre <103637148+cdelabre@users.noreply.github.com>
2022-07-21 10:25:16 +0200

    [PC-16269] Improve Datasources for open data (#899)

--- orchestration/dags/dependencies/open_data/export_open_data.py
+++ orchestration/dags/dependencies/open_data/export_open_data.py
@@ -12,7 +12,9 @@ aggregated_open_data_tables = {
     },
     "aggregated_monthly_used_booking": {
         "sql": f"{SQL_PATH}/aggregated_monthly_used_booking.sql",
-        "destination_dataset_table": "{{ bigquery_open_data_project }}.{{ bigquery_open_data_public_dataset }}.aggregated_monthly_used_booking",
+        "destination_dataset_table": "{{ bigquery_open_data_project }}.{{ bigquery_open_data_public_dataset }}.aggregated_monthly_used_booking${{ yyyymmdd(current_month(ds)) }}",
+        "time_partitioning": {"field": "calculation_month"},
+        "cluster_fields": ["calculation_month"],
     },
     "raw_supply_data": {
         "sql": f"{SQL_PATH}/raw_supply_data.sql",

eedae341884e8532ad65293516bf30eacd70a613
cdelabre <103637148+cdelabre@users.noreply.github.com>
2022-07-21 11:33:09 +0200

    [PC-16269] Remove 15 17 from open data export (#902)

--- orchestration/dags/dependencies/open_data/export_open_data.py
+++ orchestration/dags/dependencies/open_data/export_open_data.py
@@ -4,7 +4,9 @@ SQL_PATH = f"dependencies/open_data/sql/open_data"
 aggregated_open_data_tables = {
     "aggregated_usage_and_intensity": {
         "sql": f"{SQL_PATH}/aggregated_usage_and_intensity.sql",
-        "destination_dataset_table": "{{ bigquery_open_data_project }}.{{ bigquery_open_data_public_dataset }}.aggregated_usage_and_intensity",
+        "destination_dataset_table": "{{ bigquery_open_data_project }}.{{ bigquery_open_data_public_dataset }}.aggregated_usage_and_intensity${{ yyyymmdd(current_month(ds)) }}",
+        "time_partitioning": {"field": "calculation_month"},
+        "cluster_fields": ["calculation_month"],
     },
     "aggregated_monthly_user_data": {
         "sql": f"{SQL_PATH}/aggregated_monthly_user_data.sql",
@@ -12,9 +14,7 @@ aggregated_open_data_tables = {
     },
     "aggregated_monthly_used_booking": {
         "sql": f"{SQL_PATH}/aggregated_monthly_used_booking.sql",
-        "destination_dataset_table": "{{ bigquery_open_data_project }}.{{ bigquery_open_data_public_dataset }}.aggregated_monthly_used_booking${{ yyyymmdd(current_month(ds)) }}",
-        "time_partitioning": {"field": "calculation_month"},
-        "cluster_fields": ["calculation_month"],
+        "destination_dataset_table": "{{ bigquery_open_data_project }}.{{ bigquery_open_data_public_dataset }}.aggregated_monthly_used_booking",
     },
     "raw_supply_data": {
         "sql": f"{SQL_PATH}/raw_supply_data.sql",

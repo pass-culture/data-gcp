400a791dd01fdc832a55cd13cecead83d6691a67
dcuesta-pass <daniel.cuesta@passculture.app>
2022-07-26 18:09:58 +0200

    [PC-15873-add-training-data-to-BQ] WIP

---
+++ orchestration/dags/dependencies/import_training_data/sql/clics.sql
@@ -0,0 +1,25 @@
+WITH events AS (
+SELECT
+user_id,
+offer_id,
+count(*) as clics_count,
+FROM `{{ bigquery_analytics_dataset }}`.`firebase_events`
+WHERE event_name = "ConsultOffer"
+AND event_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 4 MONTH)
+AND event_date < CURRENT_DATE()
+AND user_id is not null
+AND offer_id is not null
+AND offer_id != 'NaN'
+GROUP BY user_id, offer_id
+)
+SELECT
+user_id,
+ANY_VALUE(offer.offer_id) AS offer_id,
+ANY_VALUE(offer_subcategoryid) AS offer_subcategoryid,
+CASE WHEN offer.offer_subcategoryId in ('LIVRE_PAPIER','LIVRE_AUDIO_PHYSIQUE','SEANCE_CINE')
+THEN CONCAT('product-', offer.offer_product_id) ELSE CONCAT('offer-', offer.offer_id)
+END AS item_id,
+SUM(clics_count) as clics_count,
+FROM events
+JOIN `{{ bigquery_clean_dataset }}`.`applicative_database_offer` offer ON offer.offer_id = events.offer_id
+GROUP BY user_id, item_id
---
+++ orchestration/dags/jobs/import/import_training_data.py
@@ -0,0 +1,62 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryExecuteQueryOperator,
+)
+
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+import datetime
+from common.alerts import analytics_fail_slack_alert
+from common import macros
+from common.config import DAG_FOLDER
+
+IMPORT_TRAINING_SQL_PATH = f"dependencies/import_training_data/sql"
+from common.config import (
+    BIGQUERY_ANALYTICS_DATASET,
+    BIGQUERY_CLEAN_DATASET,
+    BIGQUERY_RAW_DATASET,
+    ENV_SHORT_NAME,
+    GCP_PROJECT,
+)
+
+
+default_dag_args = {
+    "start_date": datetime.datetime(2022, 7, 26),
+    "retries": 1,
+    "on_failure_callback": analytics_fail_slack_alert,
+    "retry_delay": datetime.timedelta(minutes=5),
+    "project_id": GCP_PROJECT,
+}
+
+dag = DAG(
+    "import_training_data",
+    default_args=default_dag_args,
+    description="Import data for training and build aggregated tables",
+    schedule_interval="0 10 * * 5",
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=120),
+    user_defined_macros=macros.default,
+    template_searchpath=DAG_FOLDER,
+)
+
+start = DummyOperator(task_id="start", dag=dag)
+training_data_tables = ["bookings", "clics", "favorites"]
+
+import_tables_to_clean_tasks = []
+for table in training_data_tables:
+    task = BigQueryExecuteQueryOperator(
+        task_id=f"import_to_clean_training_data_{table}",
+        sql=f"{IMPORT_TRAINING_SQL_PATH}/{table}.sql",
+        write_disposition="WRITE_TRUNCATE",
+        use_legacy_sql=False,
+        destination_dataset_table=f"{BIGQUERY_RAW_DATASET}.training_data_{table}",
+        dag=dag,
+    )
+    import_tables_to_clean_tasks.append(task)
+
+end_import_to_raw = DummyOperator(task_id="end_import_to_raw", dag=dag)
+
+end = DummyOperator(task_id="end", dag=dag)
+
+(start >> import_tables_to_clean_tasks >> end_import_to_raw >> end)

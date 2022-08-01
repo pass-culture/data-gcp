ba79aa9443d6d635d3f880864218dbded96adf6e
dcuesta-pass <daniel.cuesta@passculture.app>
2022-08-01 14:48:15 +0200

    [fix-auto-deploy-to-vertex-ai] wip

--- algo_training/deploy_model.py
+++ algo_training/deploy_model.py
@@ -1,9 +1,13 @@
 import os
 from google.cloud import aiplatform
+
 SERVING_CONTAINER = "europe-docker.pkg.dev/vertex-ai/prediction/tf2-cpu.2-5:latest"
+
+
 def upload_model_and_deploy_to_endpoint():
     parent_model_id = aiplatform.Model.list(
-        filter=f"display_name={MODEL_NAME}", location=REGION, project=PROJECT_NAME)[0].name
+        filter=f"display_name={MODEL_NAME}", location=REGION, project=PROJECT_NAME
+    )[0].name
 
     model = aiplatform.Model.upload(
         display_name=VERSION_NAME,
@@ -11,13 +15,12 @@ def upload_model_and_deploy_to_endpoint():
         artifact_uri=RECOMMENDATION_MODEL_DIR,
         serving_container_image_uri=SERVING_CONTAINER,
         parent_model=parent_model_id,
-        location='europe-west1',
+        location="europe-west1",
     )
 
-
     endpoint = aiplatform.Endpoint.list(
-        filter=f"display_name={END_POINT_NAME}", location=REGION, project=PROJECT_NAME)[0]
-
+        filter=f"display_name={END_POINT_NAME}", location=REGION, project=PROJECT_NAME
+    )[0]
 
     model.deploy(
         endpoint=endpoint,

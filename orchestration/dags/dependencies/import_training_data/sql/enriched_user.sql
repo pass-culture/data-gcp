560eececfe8c102273ac9fd376d691476e5d1d03
dcuesta-pass <daniel.cuesta@passculture.app>
2022-08-02 16:43:47 +0200

    [PC-15873] WIP enriched_user

--- orchestration/dags/dependencies/import_training_data/sql/enriched_user.sql
+++ orchestration/dags/dependencies/import_training_data/sql/enriched_user.sql
@@ -1,9 +1,32 @@
-def get_subcat_proportion(df_user_interaction): df_user_interaction_subcat = df_user_interaction [['offer_subcategoryId']]
-    df_user_interaction_subcat["subcat_count"] = 1 df_user_interaction_subcat = df_user_interaction_subcat.groupby(['offer_subcategoryId'], as_index = False) ['subcat_count'].sum() df_user_interaction_subcat ['percent'] = (
-    df_user_interaction_subcat ['subcat_count'] / df_user_interaction_subcat ['subcat_count'].sum()
-) * 100 return df_user_interaction_subcat.sort_values(['percent'], ascending = False)
-
-with subcat_proportion
-
-#need user_id, proportion(lire,cinema,musique,spectacle) , join enriched_user_data
-
+with base as(
+    SELECT
+        user_id,
+        offer_subcategoryid,
+        SUM(count) as sub_cat_count
+    FROM
+        `{{ bigquery_clean_dataset }}`.`training_data_aggregated_users`
+    group by
+        user_id,
+        offer_subcategoryid
+    limit
+        10
+), user_total as (
+    select
+        user_id,
+        sum(count) as total_count
+    FROM
+        `{{ bigquery_clean_dataset }}`.`training_data_aggregated_users`
+    group by
+        user_id
+    limit
+        10
+), user_subcat_prop as(
+    select
+        b.user_id,
+        b.offer_subcategoryid,
+        SAFE_DIVIDE(b.sub_cat_count, ut.total_count) as sub_cat_prop
+    from
+        base b
+        join user_total ut on b.user_id = ut.user_id
+) 
+select user_id,offer_subcategoryid,sub_cat_prop from user_subcat_prop

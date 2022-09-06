8dd030c65a80ca1cdc63685d01fa611b512cc037
dcuesta-pass <daniel.cuesta@passculture.app>
2022-09-06 14:40:21 +0200

    [PC-16784] WIP

--- algo_training/train_similar_offers.py
+++ algo_training/train_similar_offers.py
@@ -2,7 +2,7 @@ from annoy import AnnoyIndex
 import tensorflow as tf
 from utils import remove_dir, SIMILAR_OFFERS_DIR
 
-model_A = tf.keras.models.load_model(f"{SIMILAR_OFFERS_DIR}/model/tf_reco/model")
+model_A = tf.keras.models.load_model(f"{SIMILAR_OFFERS_DIR}/model/tf_reco")
 embedding_item_model = model_A.item_layer.layers[1].get_weights()
 item_list_model = model_A.item_layer.layers[0].get_vocabulary()
 
@@ -10,4 +10,4 @@ a = AnnoyIndex(64, "euclidean")
 for index, vector in enumerate(embedding_item_model[0].tolist()):
     a.add_item(index, vector)
 a.build(-1)
-a.save(f"{SIMILAR_OFFERS_DIR}/sim_offers.ann")
+a.save(f"{SIMILAR_OFFERS_DIR}/model/sim_offers.ann")

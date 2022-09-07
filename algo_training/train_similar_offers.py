73a0bc3b6d4a7961e22ba03a2a086f6683f5f4a5
dcuesta-pass <daniel.cuesta@passculture.app>
2022-09-07 14:22:14 +0200

    [PC-16784] WIP

--- algo_training/train_similar_offers.py
+++ algo_training/train_similar_offers.py
@@ -1,6 +1,6 @@
 from annoy import AnnoyIndex
 import tensorflow as tf
-from utils import  SIMILAR_OFFERS_DIR
+from utils import SIMILAR_OFFERS_DIR
 
 model_A = tf.keras.models.load_model(f"{SIMILAR_OFFERS_DIR}/model/tf_reco")
 embedding_item_model = model_A.item_layer.layers[1].get_weights()
@@ -10,5 +10,5 @@ a = AnnoyIndex(64, "euclidean")
 for index, vector in enumerate(embedding_item_model[0].tolist()):
     a.add_item(index, vector)
 a.build(-1)
-ann_path=f"{SIMILAR_OFFERS_DIR}/model/sim_offers.ann"
+ann_path = f"{SIMILAR_OFFERS_DIR}/model/sim_offers.ann"
 a.save(ann_path)

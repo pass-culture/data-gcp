ba67e8344a1a57f511f61d8379cbb3ce46755f8b
dcuesta-pass <daniel.cuesta@passculture.app>
2022-09-08 17:16:41 +0200

    [fix-qpi-import-to-analytics] rm wrong commit bis

--- recommendation/db/scripts/create_recommendable_offers_eac_16_17.sql
+++ recommendation/db/scripts/create_recommendable_offers_eac_16_17.sql
@@ -154,7 +154,7 @@ WITH NO DATA;
 
 
 /* Populating the materialized view. */
-  CREATE UNIQUE INDEX idx_offer_recommendable_16_17_id ON public.recommendable_offers_eac_16_17 USING btree (offer_id);
+  CREATE UNIQUE INDEX idx_offer_recommendable_16_17_id ON public.recommendable_offers_eac_16_17 USING btree (offer_id)
   CREATE INDEX idx_recommendable_offers_eac_16_17_venue_id ON public.recommendable_offers_eac_16_17 (venue_id);
   REFRESH MATERIALIZED VIEW recommendable_offers_eac_16_17;
 /* Takes about 80 secondes with the indexes.*/

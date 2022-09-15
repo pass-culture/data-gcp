c3ecba82be0300ec4782ec3f9735b94dab78f13b
cdelabre <103637148+cdelabre@users.noreply.github.com>
2022-09-15 15:52:04 +0200

    [PC-16760](bq) offerer_tag add fields to bq (#1014)

--- orchestration/dags/dependencies/import_analytics/import_tables.py
+++ orchestration/dags/dependencies/import_analytics/import_tables.py
@@ -829,6 +829,8 @@ def define_import_query(
              SELECT 
                 CAST("id" AS varchar(255)) AS offerer_tag_id
                 ,"name" AS offerer_tag_name
+                ,"label" AS offerer_tag_label
+                ,"description" AS offerer_tag_description
             FROM public.offerer_tag
             """
 

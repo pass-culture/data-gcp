1f6b8736f33a70767e47e7c636bc6a3ab72f6866
dcuesta-pass <daniel.cuesta@passculture.app>
2022-09-23 15:34:31 +0200

    [PC-17586] add monitoring queries

---
+++ orchestration/dags/dependencies/import_recommendation_cloudsql/monitor_tables.py
@@ -0,0 +1,7 @@
+SQL_PATH = f"dependencies/import_recommendation_cloudsql/monitoring_queries/"
+
+monitoring_tables = {
+    "monitor_recommendable_offers_data": {
+        "sql": f"{SQL_PATH}/monitor_recommendable_offers_data.sql",
+    }
+}

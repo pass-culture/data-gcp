37ecd562c84f34748cc4c9822e2a2ce5e0a2b1c0
cdelabre <cyrille.delabre@passculture.app>
2022-09-28 17:43:40 +0200

    [PC-17563] app-native-logs

---
+++ orchestration/dags/dependencies/logs/sql/analytics/app_native_logs.sql
@@ -0,0 +1,13 @@
+SELECT 
+  timestamp,
+  jsonPayload.user_id,
+  jsonPayload.technical_message_id,
+  jsonPayload.extra.choice_datetime,
+  jsonPayload.extra.device_id,
+  jsonPayload.extra.analyticssource,
+  jsonPayload.extra.consent.mandatory as cookies_consent_mandatory,
+  jsonPayload.extra.consent.accepted as cookies_consent_accepted,
+  jsonPayload.extra.consent.refused as cookies_consent_refused
+FROM `{{ bigquery_raw_dataset }}.stdout`
+WHERE 
+DATE(timestamp) = "{{ ds }}" AND jsonPayload.extra.analyticssource  = "app-native"

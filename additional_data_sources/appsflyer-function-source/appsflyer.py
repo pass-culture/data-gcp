5740a234e4954ff0fc47392882b65f9c36c5d209
cdelabre <cyrille.delabre@passculture.app>
2022-09-29 11:10:32 +0200

    [PC-17626] add appsflyer tables

---
+++ additional_data_sources/appsflyer-function-source/appsflyer.py
@@ -0,0 +1,151 @@
+from io import StringIO
+
+import requests
+import pandas as df
+from furl import furl
+
+
+class AppsFlyer:
+    DEFAULT_ENDPOINT = "https://hq.appsflyer.com"
+    RAW_DATA_REPORT_ADDITIONAL_FIELDS = ",".join(
+        [
+            "install_app_store",
+            "match_type",
+            "contributor1_match_type",
+            "contributor2_match_type",
+            "contributor3_match_type",
+            "device_category",
+            "gp_referrer",
+            "gp_click_time",
+            "gp_install_begin",
+        ]
+    )
+    UNINSTALL_REPORT_ADDITIONAL_FIELDS = ",".join(
+        [
+            "gp_referrer",
+            "gp_click_time",
+            "gp_install_begin",
+        ]
+    )
+
+    def __init__(self, api_token, app_id):
+        self.api_token = api_token
+        self.app_id = app_id
+
+    def __build_args(self, date_from, date_to, kwargs):
+        args = {
+            "api_token": self.api_token,
+            "from": date_from,
+            "to": date_to,
+            "timezone": kwargs.get("timezone", "UTC"),
+            "currency": kwargs.get("currency", "preferred"),
+        }
+
+        if "media_source" in kwargs:
+            args["media_source"] = kwargs.get("media_source")
+
+        if "event_name" in kwargs:
+            args["event_name"] = kwargs.get("event_name")
+
+        if "category" in kwargs:
+            args["category"] = kwargs.get("category")
+
+        if "additional_fields" in kwargs:
+            args["additional_fields"] = kwargs.get("additional_fields")
+
+        return args
+
+    def __to_df(self, resp):
+        if resp.status_code != requests.codes.ok:
+            raise Exception(resp.text)
+
+        return df.read_csv(StringIO(resp.text))
+
+    def partners_report(self, date_from, date_to, as_df=False, **kwargs):
+        f = furl(self.DEFAULT_ENDPOINT)
+        f.path = "/export/%s/partners_report/v5" % self.app_id
+        f.args = self.__build_args(date_from, date_to, kwargs)
+        resp = requests.get(f.url)
+
+        if as_df:
+            return self.__to_df(resp)
+
+        return resp
+
+    def partners_by_date_report(self, date_from, date_to, as_df=False, **kwargs):
+        f = furl(self.DEFAULT_ENDPOINT)
+        f.path = "/export/%s/partners_by_date_report/v5" % self.app_id
+        f.args = self.__build_args(date_from, date_to, kwargs)
+        resp = requests.get(f.url)
+
+        if as_df:
+            return self.__to_df(resp)
+
+        return resp
+
+    def daily_report(self, date_from, date_to, as_df=False, **kwargs):
+        f = furl(self.DEFAULT_ENDPOINT)
+        f.path = "/export/%s/daily_report/v5" % self.app_id
+        f.args = self.__build_args(date_from, date_to, kwargs)
+        resp = requests.get(f.url)
+
+        if as_df:
+            return self.__to_df(resp)
+
+        return resp
+
+    def geo_by_date_report(self, date_from, date_to, as_df=False, **kwargs):
+        f = furl(self.DEFAULT_ENDPOINT)
+        f.path = "/export/%s/geo_by_date_report/v5" % self.app_id
+        f.args = self.__build_args(date_from, date_to, kwargs)
+        resp = requests.get(f.url)
+
+        if as_df:
+            return self.__to_df(resp)
+
+        return resp
+
+    def installs_report(
+        self, date_from, date_to, as_df=False, additional_fields=None, **kwargs
+    ):
+        f = furl(self.DEFAULT_ENDPOINT)
+        if additional_fields is None:
+            kwargs["additional_fields"] = self.RAW_DATA_REPORT_ADDITIONAL_FIELDS
+        f.path = "/export/%s/installs_report/v5" % self.app_id
+        f.args = self.__build_args(date_from, date_to, kwargs)
+        resp = requests.get(f.url)
+
+        if as_df:
+            return self.__to_df(resp)
+
+        return resp
+
+    def in_app_events_report(
+        self, date_from, date_to, as_df=False, additional_fields=None, **kwargs
+    ):
+        f = furl(self.DEFAULT_ENDPOINT)
+        if additional_fields is None:
+            kwargs["additional_fields"] = self.RAW_DATA_REPORT_ADDITIONAL_FIELDS
+        f.path = "/export/%s/in_app_events_report/v5" % self.app_id
+        f.args = self.__build_args(date_from, date_to, kwargs)
+        resp = requests.get(f.url)
+
+        if as_df:
+            return self.__to_df(resp)
+
+        return resp
+
+    def uninstall_events_report(
+        self, date_from, date_to, as_df=False, additional_fields=None, **kwargs
+    ):
+        f = furl(self.DEFAULT_ENDPOINT)
+        if additional_fields is None:
+            kwargs["additional_fields"] = self.UNINSTALL_REPORT_ADDITIONAL_FIELDS
+        f.path = "/export/%s/uninstall_events_report/v5" % self.app_id
+        f.args = self.__build_args(date_from, date_to, kwargs)
+        resp = requests.get(f.url)
+
+        if as_df:
+            return self.__to_df(resp)
+
+        return resp
---
+++ additional_data_sources/appsflyer-function-source/main.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from datetime import date, timedelta
+from utils import IOS_APP_ID, ANDROID_APP_ID, TOKEN, save_to_bq
+from appsflyer import AppsFlyer
+from mapping import (
+    DAILY_REPORT,
+    DAILY_REPORT_TYPE,
+    INSTALLS_REPORT_MAPPING,
+    INSTALLS_REPORT,
+    APP_REPORT,
+    APP_REPORT_MAPPING,
+)
+
+APPS = {"ios": IOS_APP_ID, "android": ANDROID_APP_ID}
+
+
+class ImportAppsFlyer:
+    def __init__(self, _from, _to):
+        self._from = _from
+        self._to = _to
+        self.apis = {app: AppsFlyer(TOKEN, _id) for app, _id in APPS.items()}
+
+    def get_activity_report(self):
+        dfs = []
+        for app, api in self.apis.items():
+            df_installs = api.installs_report(self._from, self._to, True)
+            df_uninstalls = api.uninstall_events_report(self._from, self._to, True)
+            df_installs["app"] = app
+            df_uninstalls["app"] = app
+            dfs.append(df_installs)
+            dfs.append(df_uninstalls)
+        df = pd.concat(dfs, ignore_index=True)
+        df = df.replace(columns=INSTALLS_REPORT)
+        for k, v in INSTALLS_REPORT_MAPPING.items():
+            df[k] = df[k].astype(v)
+        return df[list(INSTALLS_REPORT.values())]
+
+    def get_daily_report(self):
+        dfs = []
+        for app, api in self.apis.items():
+            df = api.daily_report(self._from, self._to, True)
+            df["app"] = app
+            dfs.append(df)
+        df = pd.concat(dfs, ignore_index=True)
+        df = df.replace(columns=DAILY_REPORT)
+        for k, v in DAILY_REPORT_TYPE.items():
+            df[k] = df[k].astype(v)
+        return df[list(DAILY_REPORT.values())]
+
+    def get_in_app_events_report(self):
+        dfs = []
+        for app, api in self.apis.items():
+            df = api.in_app_events_report(self._from, self._to, True)
+            df["app"] = app
+            dfs.append(df)
+        df = df.replace(columns=APP_REPORT)
+        for k, v in APP_REPORT_MAPPING.items():
+            df[k] = df[k].astype(v)
+        return df[list(APP_REPORT.values())]
+
+
+def default_date():
+    return (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+
+
+def run(request):
+    try:
+        execution_date = request.get_json().get("execution_date", None)
+        if execution_date is not None:
+            execution_date = execution_date.strftime("%Y-%m-%d")
+        else:
+            execution_date = default_date()
+    except:
+        execution_date = default_date()
+
+    import_app = ImportAppsFlyer(execution_date, execution_date)
+    save_to_bq(
+        import_app.get_activity_report(), "appsflyer_activity_report", execution_date
+    )
+    save_to_bq(import_app.get_daily_report(), "appsflyer_daily_report", execution_date)
+    save_to_bq(
+        import_app.get_in_app_events_report(),
+        "appsflyer_in_app_event_report",
+        execution_date,
+    )
+
+    return "Success"

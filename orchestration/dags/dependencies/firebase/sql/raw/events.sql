337c682f92da65a4ec402b3badb46ee90e3023e4
cdelabre <cyrille.delabre@passculture.app>
2022-10-28 15:32:37 +0200

    [HOTFIX](firebase) Remove unwanted logs for pro

--- orchestration/dags/dependencies/firebase/sql/raw/events.sql
+++ orchestration/dags/dependencies/firebase/sql/raw/events.sql
@@ -6,3 +6,15 @@ FROM
     {% else %}
     `{{ params.gcp_project_native_env }}.{{ params.firebase_raw_dataset }}.events{{ params.prefix }}{{ yyyymmdd(add_days(ds, -1)) }}`
     {% endif %}
+WHERE
+    {% if params.table_type == 'pro' %}
+        device.web_info.hostname IN (
+            "{{ params.app_info_ids | join('", "') }}"
+        )
+    {% else %}
+        app_info.id IN (
+            "{{ params.app_info_ids | join('", "') }}"
+        )
+        OR app_info.id is NULL
+    {% endif %}
+    

2d04b437de53285817bd77e305a184b86a0137d8
dcuesta-pass <daniel.cuesta@passculture.app>
2022-10-31 11:24:17 +0100

    [PC-18310] Add numerical and national top_items

---
+++ orchestration/dags/dependencies/import_analytics/sql/analytics/top_items_not_geolocated.sql
@@ -0,0 +1,40 @@
+WITH top_items AS(
+    SELECT
+        inn.item_id,
+        inn.subcategory_id,
+        ROW_NUMBER() OVER (
+            PARTITION BY inn.subcategory_id
+            ORDER BY
+                is_numerical ASC,
+                booking_number DESC
+        ) AS rank
+    FROM
+        (
+            SELECT
+                ro.item_id,
+                ro.subcategory_id,
+                MAX(CAST(ro.url IS NOT NULL AS int)) AS is_numerical,
+                MAX(ro.booking_number) AS booking_number
+            FROM
+                `passculture-data-ehp.analytics_stg.recommendable_offers_data` ro
+            where
+                ro.booking_number > 0
+                and (
+                    is_national
+                    or ro.url IS NOT NULL
+                )
+            GROUP BY
+                ro.item_id,
+                ro.subcategory_id
+        ) inn
+),
+
+SELECT
+    ti.item_id,
+    ro.offer_id,
+    NULL as iris_id,
+    NULL as venue_id,
+    0 as venue_distance_to_iris
+FROM top_items ti
+INNER JOIN `passculture-data-ehp.analytics_stg.recommendable_offers_data` ro ON ti.item_id = ro.item_id
+WHERE rank < 1000

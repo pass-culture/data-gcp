eca02f441d5a38b1491f7b270def39feec8b812d
LucileRainteau <lucile.rainteau@passculture.app>
2022-11-09 16:30:30 +0100

    ingest diversification score in aggregated table

--- orchestration/dags/dependencies/import_analytics/sql/analytics/aggregated_daily_user_used_activity.sql
+++ orchestration/dags/dependencies/import_analytics/sql/analytics/aggregated_daily_user_used_activity.sql
@@ -21,8 +21,8 @@ user_active_dates AS (
         DATE_DIFF(CURRENT_DATE, deposit_creation_date, MONTH) AS seniority_months
     FROM
         `{{ bigquery_analytics_dataset }}.enriched_deposit_data`
-        JOIN days ON days.day BETWEEN `{{ bigquery_analytics_dataset }}.enriched_deposit_data`.deposit_creation_date
-        AND `{{ bigquery_analytics_dataset }}.enriched_deposit_data`.deposit_expiration_date
+        JOIN days ON days.day BETWEEN date(`{{ bigquery_analytics_dataset }}.enriched_deposit_data`.deposit_creation_date)
+        AND date(`{{ bigquery_analytics_dataset }}.enriched_deposit_data`.deposit_expiration_date)
 ),
 aggregated_daily_user_used_bookings_history_1 AS (
     SELECT
@@ -68,10 +68,36 @@ aggregated_daily_user_used_bookings_history_1 AS (
         10,
         11,
         12
+),
+-- Ajouter score de diversification par jour et score cumulé
+-- /!\ Score de diversification indisponible pour les résas sur les offres inexistantes dans la table enriched_offer_data
+diversification as (
+  SELECT 
+    divers.user_id
+    , divers.booking_id
+    , divers.booking_creation_date
+    , date(booking_used_date) as booking_used_date
+    , delta_diversification
+  FROM `{{ bigquery_analytics_dataset }}.diversification_booking` divers
+  LEFT JOIN `{{ bigquery_analytics_dataset }}.enriched_booking_data` book
+  ON divers.booking_id = book.booking_id
+), 
+
+diversification_used as (
+  -- Aggreger le score de diversification selon la date d'utilisation du booking
+  SELECT 
+    user_id
+    , booking_used_date
+    , count(booking_id) as nb_bookings
+    , sum(delta_diversification) AS delta_diversification
+  FROM diversification 
+  GROUP BY 
+    user_id
+    , booking_used_date
 )
 SELECT
     active_date,
-    user_id,
+    activity.user_id,
     user_department_code,
     user_region_name,
     user_age,
@@ -85,16 +111,21 @@ SELECT
     amount_spent,
     cnt_used_bookings,
     SUM(amount_spent) OVER (
-        PARTITION BY user_id,
+        PARTITION BY activity.user_id,
         deposit_id
         ORDER BY
             active_date ASC
     ) AS cumulative_amount_spent,
     SUM(cnt_used_bookings) OVER (
-        PARTITION BY user_id,
+        PARTITION BY activity.user_id,
         deposit_id
         ORDER BY
             active_date
-    ) AS cumulative_cnt_used_bookings,
+    ) AS cumulative_cnt_used_bookings
+    , coalesce(delta_diversification, 0) as delta_diversification
+    , coalesce(sum(delta_diversification) over(partition by activity.user_id order by active_date), 0) as delta_diversification_cumsum
 FROM
-    aggregated_daily_user_used_bookings_history_1
+    aggregated_daily_user_used_bookings_history_1 as activity
+LEFT JOIN diversification_used
+  ON activity.user_id = diversification_used.user_id
+  AND activity.active_date = diversification_used.booking_used_date

0794224afd009377521dc26b67d2c276aca8907d
LucileRainteau <lucile.rainteau@passculture.app>
2022-11-15 18:46:05 +0100

    PC-16112 Add diversification computation as SQL

--- orchestration/dags/dependencies/import_analytics/import_analytics.py
+++ orchestration/dags/dependencies/import_analytics/import_analytics.py
@@ -192,6 +192,16 @@ analytics_tables = {
             "aggregated_monthly_user_used_booking_activity",
         ],
     },
+    "diversification_booking_remastered": {
+        "sql": f"{ANALYTICS_SQL_PATH}/diversification_booking_remastered.sql",
+        "destination_dataset": "{{ bigquery_raw_dataset }}",
+        "destination_table": "diversification_booking_remastered",
+        "depends": [
+            "applicative_database_user",
+            "enriched_booking_data",
+            "enriched_offer_data",
+        ],
+    },
 }
 
 aggregated_tables = {

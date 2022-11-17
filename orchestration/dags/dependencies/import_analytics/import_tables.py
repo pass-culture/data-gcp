8bde5ad5f4f20d2a8631a44d250a894f018bb912
MatthieuRipollPassCulture <matthieu.ripoll+1@passculture.app>
2022-11-17 17:25:01 +0100

    add variables in collective offer

--- orchestration/dags/dependencies/import_analytics/import_tables.py
+++ orchestration/dags/dependencies/import_analytics/import_tables.py
@@ -173,7 +173,8 @@ def define_import_query(
     ] = """
         SELECT
             CAST("id" AS varchar(255)), CAST("userId" AS varchar(255)), CAST("offererId" AS varchar(255)), 
-            CAST(CAST("user_offerer"."validationToken" AS varchar(255)) IS NULL AS boolean) AS user_offerer_is_validated
+            CAST(CAST("user_offerer"."validationToken" AS varchar(255)) IS NULL AS boolean) AS user_offerer_is_validated,
+            "validationStatus" AS user_offerer_validation_status
         FROM public.user_offerer
     """
     cloudsql_queries[
@@ -781,6 +782,7 @@ def define_import_query(
                 ,CAST("lastValidationType" AS VARCHAR) AS collective_offer_last_validation_type
                 ,CAST("institutionId" AS varchar(255)) AS institution_id
                 ,BTRIM(array_to_string("interventionArea", \\',\\'), \\'{\\') AS intervention_area
+                ,CAST("templateId" AS varchar(255)) AS template_id
             FROM public.collective_offer
         """
 

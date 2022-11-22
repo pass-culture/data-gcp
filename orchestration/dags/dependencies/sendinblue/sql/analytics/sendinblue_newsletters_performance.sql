64dedf3e2711adfa04a0bab9ae4df023c9c160d2
LucileRainteau <lucile.rainteau@passculture.app>
2022-11-22 18:12:57 +0100

    WIP - Add query to create analytics table

--- orchestration/dags/dependencies/sendinblue/sql/analytics/sendinblue_newsletters_performance.sql
+++ orchestration/dags/dependencies/sendinblue/sql/analytics/sendinblue_newsletters_performance.sql
@@ -2,6 +2,28 @@
 -- & compute indicators.
 -- *** Missing utm 
 
-SELECT *
+SELECT 
+    campaign_id
+    , campaign_utm
+    , campaign_name
+    , campaign_sent_date
+    , share_link
+    , domain
+    , sum(audience_size) as audience_size
+    , sum(open_number) as open_number
+    , sum(unsubscriptions) as unsubscriptions
+    , date(update_date) as update_date
+    , count(distinct session_id) as session_number
 
-FROM `{{ bigquery_raw_dataset }}.sendinblue_newsletters`
+FROM `{{ bigquery_raw_dataset }}.sendinblue_newsletters` sendinblue
+LEFT JOIN `{{ bigquery_analytics_dataset }}.firebase_events` firebase
+ON sendinblue.campaign_utm = firebase.traffic_campaign
+
+WHERE traffic_campaign IS NOT NULL
+GROUP BY 
+    campaign_id
+    , campaign_utm
+    , campaign_name
+    , campaign_sent_date
+    , share_link
+    , domain

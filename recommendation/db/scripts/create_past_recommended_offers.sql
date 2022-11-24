450378202cf05f84dc740aca3376f40ba2535bf0
dcuesta-pass <daniel.cuesta@passculture.app>
2022-11-24 17:13:54 +0100

    [HOTFIX] rm temp cfg + fix mat view sql

--- recommendation/db/scripts/create_past_recommended_offers.sql
+++ recommendation/db/scripts/create_past_recommended_offers.sql
@@ -12,4 +12,6 @@ ADD COLUMN  model_name     varchar,
 ADD COLUMN model_version   varchar,
 ADD COLUMN call_id         varchar,
 ADD COLUMN reco_filters    json,
+ADD COLUMN lat varchar,
+ADD COLUMN long varchar,
 ADD COLUMN user_iris_id    varchar;

f3e858943947915113522da98205677b5b7c2104
cdelabre <103637148+cdelabre@users.noreply.github.com>
2022-11-30 18:51:36 +0100

    [PC-18952](reco) Remove deprecated code (#1180)

--- recommendation/kpi/pertinence/count_booking.sql
+++
@@ -1,4 +0,0 @@
-SELECT count(*) as total
-FROM `passculture-data-prod.analytics_prod.applicative_database_booking`
-WHERE booking_creation_date >= PARSE_DATETIME('%Y%m%d',@DS_START_DATE)    -- Dates à définir sur la dashboard
-AND booking_creation_date < PARSE_DATETIME('%Y%m%d',@DS_END_DATE)

d89ef5ff1b20c6340ede0922ebc4be59905e4698
cdelabre <cyrille.delabre@passculture.app>
2022-11-30 09:26:54 +0100

    [PC-18952](reco) Remove deprecated code

--- recommendation/kpi/pertinence/count_favorites.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    COUNT(*) AS favorite_offers_from_homepage
-FROM `passculture-data-prod.clean_prod.log_link_visit_action_preprocessed` AS llvap
-WHERE  llvap.idaction_event_action = 6957092                          -- 6957092 : AddFavorite_FromHomepage
-AND llvap.server_time >= PARSE_TIMESTAMP('%Y%m%d',@DS_START_DATE)     -- Dates à définir sur la dashboard
-AND llvap.server_time < PARSE_TIMESTAMP('%Y%m%d',@DS_END_DATE)        -- pour gérer la période d'AB testing

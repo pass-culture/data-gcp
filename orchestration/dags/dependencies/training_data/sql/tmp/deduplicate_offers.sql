5fdc27e39e55d19dbade46b9ea11fb3bf733ad6a
laurentm <laurentm@sicara.com>
2022-12-02 11:39:31 +0100

    Format SQL

--- orchestration/dags/dependencies/training_data/sql/tmp/deduplicate_offers.sql
+++ orchestration/dags/dependencies/training_data/sql/tmp/deduplicate_offers.sql
@@ -1,7 +1,10 @@
-SELECT * except (deduplicated_offer_name) FROM
-  (SELECT * FROM
-    (SELECT * except (item_id) from `{{ bigquery_analytics_dataset }}`.`enriched_offer_data`) t1
-  LEFT JOIN
-    (SELECT offer_name as deduplicated_offer_name, MIN(item_id) as item_id FROM `{{ bigquery_analytics_dataset }}`.`enriched_offer_data` group by offer_name) t2
-  ON
-    t1.offer_name=t2.deduplicated_offer_name) order by offer_name
+SELECT * EXCEPT (deduplicated_offer_name)
+FROM (SELECT *
+      FROM (SELECT * EXCEPT (item_id) FROM `{{ bigquery_analytics_dataset }}`.`enriched_offer_data`) t1
+               LEFT JOIN
+           (SELECT offer_name AS deduplicated_offer_name, MIN(item_id) AS item_id
+            FROM `{{ bigquery_analytics_dataset }}`.`enriched_offer_data`
+            GROUP BY offer_name) t2
+           ON
+               t1.offer_name = t2.deduplicated_offer_name)
+ORDER BY offer_name
--- orchestration/dags/dependencies/training_data/sql/tmp/training_data_deduplicated_bookings.sql
+++ orchestration/dags/dependencies/training_data/sql/tmp/training_data_deduplicated_bookings.sql
@@ -1,28 +1,29 @@
-SELECT
-    booking.user_id,
-    "BOOKING" as event_type,
-    booking_creation_date as event_date,
-    offer_item_ids.item_id as item_id,
-    offer.offer_subcategoryId as offer_subcategoryid,
-    subcategories.category_id as offer_categoryId,
-    enroffer.genres,
-    enroffer.rayon,
-    enroffer.type,
-    enroffer.venue_id,
-    enroffer.venue_name,
-    count(*) as count,
-from
+SELECT booking.user_id,
+       "BOOKING"                 AS event_type,
+       booking_creation_date     AS event_date,
+       offer_item_ids.item_id    AS item_id,
+       offer.offer_subcategoryid AS offer_subcategoryid,
+       subcategories.category_id AS offer_categoryid,
+       enroffer.genres,
+       enroffer.rayon,
+       enroffer.type,
+       enroffer.venue_id,
+       enroffer.venue_name,
+       COUNT(*) AS COUNT,
+FROM
     `{{ bigquery_clean_dataset }}`.`applicative_database_booking` booking
-    inner join `{{ bigquery_clean_dataset }}`.`applicative_database_stock` stock on booking.stock_id = stock.stock_id
-    inner join `{{ bigquery_clean_dataset }}`.`applicative_database_offer` offer on stock.offer_id = offer.offer_id
-    inner join `{{ bigquery_analytics_dataset }}`.`subcategories` subcategories on offer.offer_subcategoryId = subcategories.id
-    inner join `{{ bigquery_sandbox_dataset }}`.`deduplicated_enriched_offer_data` enroffer on enroffer.offer_id = offer.offer_id
-    inner join `{{ bigquery_analytics_dataset }}`.`offer_item_ids` offer_item_ids on offer_item_ids.offer_id = offer.offer_id
-where
-    booking.booking_creation_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 4 MONTH)
-    and booking.booking_creation_date <= CURRENT_DATE()
-    and user_id is not null
-group by
+    INNER JOIN `{{ bigquery_clean_dataset }}`.`applicative_database_stock` stock
+ON booking.stock_id = stock.stock_id
+    INNER JOIN `{{ bigquery_clean_dataset }}`.`applicative_database_offer` offer ON stock.offer_id = offer.offer_id
+    INNER JOIN `{{ bigquery_analytics_dataset }}`.`subcategories` subcategories ON offer.offer_subcategoryId = subcategories.id
+    INNER JOIN `{{ bigquery_sandbox_dataset }}`.`deduplicated_enriched_offer_data` enroffer ON enroffer.offer_id = offer.offer_id
+    INNER JOIN `{{ bigquery_analytics_dataset }}`.`offer_item_ids` offer_item_ids ON offer_item_ids.offer_id = offer.offer_id
+WHERE
+    booking.booking_creation_date >= DATE_SUB(CURRENT_DATE ()
+    , INTERVAL 4 MONTH)
+  AND booking.booking_creation_date <= CURRENT_DATE ()
+  AND user_id IS NOT NULL
+GROUP BY
     booking.user_id,
     item_id,
     event_type,

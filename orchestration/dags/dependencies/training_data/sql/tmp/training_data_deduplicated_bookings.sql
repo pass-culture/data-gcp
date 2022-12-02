4ca57bce8b5ffe1edf4e71b4c9f3622902b6217e
laurentm <laurentm@sicara.com>
2022-12-02 11:46:44 +0100

    Remove useless join

--- orchestration/dags/dependencies/training_data/sql/tmp/training_data_deduplicated_bookings.sql
+++ orchestration/dags/dependencies/training_data/sql/tmp/training_data_deduplicated_bookings.sql
@@ -1,7 +1,7 @@
 SELECT booking.user_id,
        "BOOKING"                 AS event_type,
        booking_creation_date     AS event_date,
-       offer_item_ids.item_id    AS item_id,
+       enroffer.item_id    AS item_id,
        offer.offer_subcategoryid AS offer_subcategoryid,
        subcategories.category_id AS offer_categoryid,
        enroffer.genres,
@@ -17,7 +17,6 @@ ON booking.stock_id = stock.stock_id
     INNER JOIN `{{ bigquery_clean_dataset }}`.`applicative_database_offer` offer ON stock.offer_id = offer.offer_id
     INNER JOIN `{{ bigquery_analytics_dataset }}`.`subcategories` subcategories ON offer.offer_subcategoryId = subcategories.id
     INNER JOIN `{{ bigquery_sandbox_dataset }}`.`deduplicated_enriched_offer_data` enroffer ON enroffer.offer_id = offer.offer_id
-    INNER JOIN `{{ bigquery_analytics_dataset }}`.`offer_item_ids` offer_item_ids ON offer_item_ids.offer_id = offer.offer_id
 WHERE
     booking.booking_creation_date >= DATE_SUB(CURRENT_DATE ()
     , INTERVAL 4 MONTH)

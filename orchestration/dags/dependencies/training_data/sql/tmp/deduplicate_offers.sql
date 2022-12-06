d63709b31ef992135a502f351216eba6ebd6d99b
laurentm <laurentm@sicara.com>
2022-12-06 09:07:11 +0100

    Refacto SQL Query

--- orchestration/dags/dependencies/training_data/sql/tmp/deduplicate_offers.sql
+++ orchestration/dags/dependencies/training_data/sql/tmp/deduplicate_offers.sql
@@ -1,17 +1,23 @@
-SELECT * EXCEPT (deduplicated_offer_name)
-FROM (SELECT *
-      FROM (SELECT * EXCEPT (item_id)
-            FROM `{{ bigquery_analytics_dataset }}`.`enriched_offer_data`
-            WHERE offer_subcategoryId IN {{ event_subcategories }}) t1
-               LEFT JOIN
-           (SELECT offer_name AS deduplicated_offer_name, MIN(item_id) AS item_id
-            FROM `{{ bigquery_analytics_dataset }}`.`enriched_offer_data`
-            WHERE offer_subcategoryId IN {{ event_subcategories }}
-            GROUP BY offer_name) t2
-           ON
-               t1.offer_name = t2.deduplicated_offer_name)
+WITH event_offers_table AS
+(
+  SELECT * except (item_id) from `{{ bigquery_analytics_dataset }}`.`enriched_offer_data` where offer_subcategoryId IN {{ params.event_subcategories }}
+),
+linked_offers_table AS
+(
+  SELECT offer_name as deduplicated_offer_name, MIN(item_id) as item_id FROM `{{ bigquery_analytics_dataset }}`.`enriched_offer_data` where offer_subcategoryId IN {{ params.event_subcategories }} group by offer_name
+),
+deduplicated_event_offers_table AS
+(
+  SELECT * FROM event_offers_table LEFT JOIN linked_offers_table
+  ON
+    event_offers_table.offer_name=linked_offers_table.deduplicated_offer_name
+),
+not_event_offers_table AS
+(
+  SELECT * except (item_id), item_id from `{{ bigquery_analytics_dataset }}`.`enriched_offer_data`
+  where offer_subcategoryId NOT IN {{ params.event_subcategories }}
+)
+SELECT * except (deduplicated_offer_name) FROM deduplicated_event_offers_table
 UNION ALL
-SELECT * EXCEPT (item_id), item_id
-FROM `{{ bigquery_analytics_dataset }}`.`enriched_offer_data`
-WHERE offer_subcategoryId NOT IN {{ event_subcategories }}
-ORDER BY offer_name
+SELECT * from not_event_offers_table
+order by offer_name

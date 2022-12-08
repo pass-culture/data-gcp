2a66c08459b74d2d874db8387c655a3089a079f4
Gabriel GUERIN <gabriel.guerin@sicara.com>
2022-12-08 16:14:56 +0100

    Use a Temporal user split in split_data.py

--- algo_training/split_data.py
+++ algo_training/split_data.py
@@ -4,16 +4,46 @@ from utils import STORAGE_PATH, MODEL_NAME, GCP_PROJECT_ID, ENV_SHORT_NAME
 
 
 def split_data(storage_path: str, model_name: str):
+    """We want to split the dataset into train, eval & test datasets but we want each dataset to contain
+    at least one information about every user & offer the model was trained with.
+
+    To do so:
+        - We sample the clean_data by 80% after grouping it by users to create the train set
+        - We split the remaining data in half to create test & eval sets
+        - We reassign the offers present in eval & test and not in train into the train set
+    """
     clean_data = pd.read_csv(f"{storage_path}/clean_data.csv")
 
-    df = clean_data.sample(frac=1).reset_index(drop=True)
-    lim_train = int(df.shape[0] * 80 / 100)
-    lim_eval = int(df.shape[0] * 90 / 100)
-    positive_data_train = df.loc[df.index < lim_train]
-    positive_data_eval = df.loc[df.index < lim_eval]
-    positive_data_eval = positive_data_eval.loc[positive_data_eval.index >= lim_train]
-    positive_data_test = df[df.index >= lim_eval]
+    # Sample 80% of the clean data based on users
+    positive_data_train = clean_data.groupby(["user_id"]).sample(frac=0.8)
 
+    # Split the remaining data in half
+    positive_data_not_in_train = clean_data.loc[
+        ~clean_data.index.isin(list(positive_data_train.index))
+    ]
+    positive_data_eval = positive_data_not_in_train.sample(frac=0.5)
+    positive_data_test = positive_data_not_in_train.loc[
+        ~positive_data_not_in_train.index.isin(list(positive_data_eval.index))
+    ]
+
+    # Reassign the non evaluable data into train
+    training_items = positive_data_train["item_id"].values
+
+    positive_data_train = pd.concat(
+        [
+            positive_data_train,
+            positive_data_eval[~positive_data_eval["item_id"].isin(training_items)],
+            positive_data_test[~positive_data_test["item_id"].isin(training_items)],
+        ]
+    )
+    positive_data_eval = positive_data_eval[
+        positive_data_eval["item_id"].isin(training_items)
+    ]
+    positive_data_test = positive_data_test[
+        positive_data_test["item_id"].isin(training_items)
+    ]
+
+    # Store the datasets
     positive_data_train.to_csv(f"{storage_path}/positive_data_train.csv", index=False)
     positive_data_eval.to_csv(f"{storage_path}/positive_data_eval.csv", index=False)
     positive_data_test.to_csv(f"{storage_path}/positive_data_test.csv", index=False)

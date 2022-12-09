19ff0a6df7a1777d6c1e2330f6bd7dc513626cca
LucileRainteau <114910642+LucileRainteau@users.noreply.github.com>
2022-12-09 17:58:53 +0100

    (PC-18242) data import sendinblue data - newsletters (#1156)
    * PC-18242-import-sendinblue-newsletter-data
    
    * PC-18242 Add dag to import sendinblue data
    
    * WIP - Add query to create analytics table
    
    * WIP
    
    * PC-18242 Change partion param
    
    * Adapt SQL

---
+++ orchestration/dags/dependencies/sendinblue/sql/analytics/sendinblue_newsletters_performance.sql
@@ -0,0 +1,39 @@
+-- Join with firebase_events to get the number of sessions
+-- & compute indicators.
+-- *** Missing utm 
+
+WITH sendinblue_newsletter as (
+    SELECT 
+        *
+        , row_number() over( partition by campaign_id order by update_date desc) as rank_update
+    FROM sendinblue_newsletters_histo
+    QUALIFY rank_update = 1
+)
+
+
+SELECT 
+    campaign_id
+    , campaign_utm
+    , campaign_name
+    , campaign_sent_date
+    , share_link
+    , domain
+    , sum(audience_size) as audience_size
+    , sum(open_number) as open_number
+    , sum(unsubscriptions) as unsubscriptions
+    , date(update_date) as update_date
+    , count(distinct session_id) as session_number
+
+FROM sendinblue_newsletter
+LEFT JOIN `{{ bigquery_analytics_dataset }}.firebase_events` firebase
+ON sendinblue.campaign_utm = firebase.traffic_campaign
+
+WHERE traffic_campaign IS NOT NULL
+GROUP BY 
+    campaign_id
+    , campaign_utm
+    , campaign_name
+    , campaign_sent_date
+    , share_link
+    , domain
+    , update_date

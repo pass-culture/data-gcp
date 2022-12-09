83f25acd08b922b37709b88ecf6953904f49d56a
LucileRainteau <lucile.rainteau@passculture.app>
2022-12-09 17:20:11 +0100

    (PC-18918) Create cloud function to automatically import cold data from GCS bucket

---
+++ additional_data_sources/cold-data-function-source/tables_config.py
@@ -0,0 +1,6 @@
+ref_tables = {
+    "macro_rayons": {
+        "file_type": "csv",
+        "schema": {"index": "INTEGER", "macro_rayon": "STRING", "rayon": "STRING"},
+    }
+}
---
+++ orchestration/dags/jobs/import/import_cold_data.py
@@ -0,0 +1,95 @@
+import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryExecuteQueryOperator,
+)
+
+from common import macros
+from common.utils import getting_service_account_token, depends_loop
+
+from common.config import (
+    GCP_PROJECT,
+    DAG_FOLDER,
+    ENV_SHORT_NAME,
+)
+from common.config import GCP_PROJECT, DAG_FOLDER
+from common.alerts import task_fail_slack_alert
+from dependencies.cold_data.import_cold_data import analytics_tables
+
+default_dag_args = {
+    "start_date": datetime.datetime(2020, 12, 21),
+    "retries": 1,
+    "on_failure_callback": task_fail_slack_alert,
+    "retry_delay": datetime.timedelta(minutes=5),
+    "project_id": GCP_PROJECT,
+}
+
+dag = DAG(
+    "import_cold_data",
+    default_args=default_dag_args,
+    description="Import cold data from GCS to BQ",
+    schedule_interval="00 01 * * *",
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=120),
+    user_defined_macros=macros.default,
+    template_searchpath=DAG_FOLDER,
+)
+
+start = DummyOperator(task_id="start", dag=dag)
+
+service_account_token = PythonOperator(
+    task_id="getting_cold_data_service_account_token",
+    python_callable=getting_service_account_token,
+    op_kwargs={
+        "function_name": f"cold_data_{ENV_SHORT_NAME}",
+    },
+    dag=dag,
+)
+
+import_cold_data_op = SimpleHttpOperator(
+    task_id=f"import_cold_data",
+    method="POST",
+    http_conn_id="http_gcp_cloud_function",
+    endpoint=f"cold_data_{ENV_SHORT_NAME}",
+    headers={
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {{task_instance.xcom_pull(task_ids='getting_cold_data_service_account_token', key='return_value')}}",
+    },
+    log_response=True,
+    dag=dag,
+)
+
+end_raw = DummyOperator(task_id="end_raw", dag=dag)
+
+analytics_table_jobs = {}
+for name, params in analytics_tables.items():
+
+    task = BigQueryExecuteQueryOperator(
+        task_id=f"{name}",
+        sql=params["sql"],
+        write_disposition="WRITE_TRUNCATE",
+        use_legacy_sql=False,
+        destination_dataset_table=params["destination_dataset_table"],
+        dag=dag,
+    )
+
+    analytics_table_jobs[name] = {
+        "operator": task,
+        "depends": params.get("depends", []),
+    }
+
+analytics_table_tasks = depends_loop(analytics_table_jobs, end_raw)
+
+end = DummyOperator(task_id="end", dag=dag)
+
+(
+    start
+    >> service_account_token
+    >> import_cold_data_op
+    >> end_raw
+    >> analytics_table_tasks
+    >> end
+)

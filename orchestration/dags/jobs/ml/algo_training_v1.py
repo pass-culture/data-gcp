3d8eeaaec40ba06b680c73c2da431a5cc2ef63fe
gabrielsicara <89379639+gabrielsicara@users.noreply.github.com>
2022-12-13 16:15:11 +0100

    Fix fetch-code-cmd (#1203)

--- orchestration/dags/jobs/ml/algo_training_v1.py
+++ orchestration/dags/jobs/ml/algo_training_v1.py
@@ -98,7 +98,7 @@ with DAG(
     if ENV_SHORT_NAME == "prod":
         branch = "production"
 
-    FETCH_CODE = f'"if cd data-gcp; then git checkout master && git pull && git checkout {branch} && git pull; else git clone git@github.com:pass-culture/data-gcp.git && cd data-gcp && git checkout {branch} && git pull; fi"'
+    FETCH_CODE = f'"if cd data-gcp; then git fetch origin {branch} && git checkout {branch} && git pull; else git clone git@github.com:pass-culture/data-gcp.git && cd data-gcp && git checkout {branch}; fi"'
 
     fetch_code = BashOperator(
         task_id="fetch_code",

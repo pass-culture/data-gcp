05f3e90faf15d423279835260908a066aa3f53b6
cdelabre <cyrille.delabre@passculture.app>
2022-12-13 11:36:38 +0100

    [PC-19177](backend) Refacto daily and weekly

--- orchestration/dags/dependencies/backend/create_tables.py
+++ orchestration/dags/dependencies/backend/create_tables.py
@@ -5,9 +5,11 @@ create_tables = {
         "sql": f"{BACKEND_SQL_PATH}/favorites_not_booked.sql",
         "destination_dataset_table": "{{ bigquery_backend_dataset }}.favorites_not_booked${{ yyyymmdd(today()) }}",
         "time_partitioning": {"field": "execution_date"},
+        "schedule_type": "weekly",
     },
     "users_that_landed_on_come_back_later": {
         "sql": f"{BACKEND_SQL_PATH}/users_that_landed_on_come_back_later.sql",
         "destination_dataset_table": "{{ bigquery_backend_dataset }}.users_that_landed_on_come_back_later",
+        "schedule_type": "daily",
     },
 }
--- orchestration/dags/jobs/export/export_backend_tables.py
+++ orchestration/dags/jobs/export/export_backend_tables.py
@@ -19,35 +19,43 @@ default_dag_args = {
     "project_id": GCP_PROJECT,
 }
 
-dag = DAG(
-    "export_backend_tables",
-    default_args=default_dag_args,
-    description="Export weekly tables for backend needs",
-    on_failure_callback=task_fail_slack_alert,
-    schedule_interval="00 03 * * 1" if ENV_SHORT_NAME == "prod" else "00 03 * * *",
-    catchup=False,
-    dagrun_timeout=datetime.timedelta(minutes=120),
-    user_defined_macros=macros.default,
-    template_searchpath=DAG_FOLDER,
-)
 
-start = DummyOperator(task_id="start", dag=dag)
-
-end = DummyOperator(task_id="end", dag=dag)
-
-export_table_tasks = []
-for table, params in create_tables.items():
-    task = BigQueryExecuteQueryOperator(
-        task_id=f"export_{table}",
-        sql=params["sql"],
-        write_disposition="WRITE_APPEND",
-        use_legacy_sql=False,
-        destination_dataset_table=params["destination_dataset_table"],
-        time_partitioning=params.get("time_partitioning", None),
-        cluster_fields=params.get("cluster_fields", None),
-        dag=dag,
+dag_schedule = {
+    "daily": "00 03 * * *",
+    "weekly": "00 03 * * 1" if ENV_SHORT_NAME == "prod" else "00 03 * * *",
+}
+for schedule_type, schedule_cron in dag_schedule.items():
+    dag_id = f"export_backend_tables_{schedule_type}"
+    dag = DAG(
+        dag_id,
+        default_args=default_dag_args,
+        description=f"Export {schedule_type} tables for backend needs",
+        on_failure_callback=task_fail_slack_alert,
+        schedule_interval=schedule_cron,
+        catchup=False,
+        dagrun_timeout=datetime.timedelta(minutes=120),
+        user_defined_macros=macros.default,
+        template_searchpath=DAG_FOLDER,
     )
-    export_table_tasks.append(task)
+    globals()[dag_id] = dag
 
+    start = DummyOperator(task_id="start", dag=dag)
 
-(start >> export_table_tasks >> end)
+    end = DummyOperator(task_id="end", dag=dag)
+
+    export_table_tasks = []
+    for table, params in create_tables.items():
+        if params["schedule_type"] == schedule_type:
+            task = BigQueryExecuteQueryOperator(
+                task_id=f"export_{table}",
+                sql=params["sql"],
+                write_disposition="WRITE_APPEND",
+                use_legacy_sql=False,
+                destination_dataset_table=params["destination_dataset_table"],
+                time_partitioning=params.get("time_partitioning", None),
+                cluster_fields=params.get("cluster_fields", None),
+                dag=dag,
+            )
+            export_table_tasks.append(task)
+
+    (start >> export_table_tasks >> end)

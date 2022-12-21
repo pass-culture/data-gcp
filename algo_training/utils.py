6234128afdda31c7b7bf998ad1c914be57d0ee25
cdelabre <103637148+cdelabre@users.noreply.github.com>
2022-12-21 19:20:06 +0100

    [PC-19390](mlflow) Store BQ table with artifacts (#1242)

--- algo_training/utils.py
+++ algo_training/utils.py
@@ -8,7 +8,9 @@ from google.oauth2 import id_token
 
 GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "passculture-data-ehp")
 STORAGE_PATH = os.environ.get("STORAGE_PATH", "")
-ENV_SHORT_NAME = os.environ.get("ENV_SHORT_NAME", "ehp")
+ENV_SHORT_NAME = os.environ.get("ENV_SHORT_NAME", "dev")
+BIGQUERY_CLEAN_DATASET = f"clean_{ENV_SHORT_NAME}"
+MODELS_RESULTS_TABLE_NAME = "mlflow_training_results"
 MLFLOW_EHP_URI = "https://mlflow-ehp.internal-passculture.app/"
 MLFLOW_PROD_URI = "https://mlflow.internal-passculture.app/"
 TRAIN_DIR = os.environ.get("TRAIN_DIR", "/home/airflow/train")

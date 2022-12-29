7cd46e9d72a2b51b738d964c20aad5085a246a65
LucileRainteau <114910642+LucileRainteau@users.noreply.github.com>
2022-12-29 14:27:14 +0100

    (PC-18918) Create cloud function to automatically import cold data (#1204)
    * (PC-18918) Create cloud function to automatically import cold data from GCS bucket
    
    * Updated config.yml
    
    * (PC-18918) Add eac budget cold data
    
    * Apply linter

--- additional_data_sources/cold-data-function-source/tables_config.py
+++ additional_data_sources/cold-data-function-source/tables_config.py
@@ -2,5 +2,14 @@ ref_tables = {
     "macro_rayons": {
         "file_type": "csv",
         "schema": {"index": "INTEGER", "macro_rayon": "STRING", "rayon": "STRING"},
-    }
+    },
+    "eac_cash_in": {
+        "file_type": "csv",
+        "schema": {
+            "ministry": "STRING",
+            "date_update": "DATE",
+            "scholar_year": "STRING",
+            "cash_in": "FLOAT",
+        },
+    },
 }

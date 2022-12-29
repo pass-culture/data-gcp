1c7e8fa830f26c7b41da3c3cecbc47aa64b94129
LucileRainteau <lucile.rainteau@passculture.app>
2022-12-29 16:20:23 +0100

    (PC-19439) Distinguer 2A/2B en corse + optimiser query

--- orchestration/dags/dependencies/import_analytics/sql/analytics/enriched_deposit_data.sql
+++ orchestration/dags/dependencies/import_analytics/sql/analytics/enriched_deposit_data.sql
@@ -106,53 +106,7 @@ user_suspension_history AS (
         ) AS rank
     FROM
         `{{ bigquery_analytics_dataset }}`.applicative_database_user_suspension
-),
-
-clean_applicative_database_user AS (
-    SELECT * EXCEPT(user_department_code), 
-        -- set 99 when user user_creation_date does not match opening phases.
-        -- this is due to Support changes in the past which migh lead to misunderstandings.
-        CASE 
-            -- if user_department is not in "pilote" (2019_02 / 2019_06) phase but has created an account before, set 99.
-            WHEN 
-                user_department_code not in ("29","34","67","93","973")
-                AND date(user_creation_date) < "2019-06-01"
-            THEN "99"
-            -- if user_department is not in "experimentation" (2019_06 / 2021_05) phase but has created an account before, set 99.
-            WHEN 
-                user_department_code not in ("29","34","67","93","973","22","25","35","56","58","71","08","84","94")
-                AND date(user_creation_date) < "2021-05-01"
-            THEN "99"
-            ELSE user_department_code 
-        END AS user_department_code 
-    FROM (
-    SELECT 
-        user_id,
-        user_creation_date,
-        -- keep user_postal_code by default.
-        COALESCE(
-        CASE
-            SUBSTRING(user_postal_code, 0, 2)
-            WHEN '97' THEN SUBSTRING(user_postal_code, 0, 3)
-            WHEN '98' THEN SUBSTRING(user_postal_code, 0, 3)
-            ELSE SUBSTRING(user_postal_code, 0, 2)
-          END, 
-          user_department_code
-        ) AS user_department_code,
-        user_postal_code,
-        user_activity,
-        user_civility,
-        user_school_type,
-        user_is_active,
-        user_age,
-        user_role,
-        user_birth_date,
-        user_cultural_survey_filled_date
-    FROM  `{{ bigquery_analytics_dataset }}`.applicative_database_user
-    -- only BENEFICIARY
-    WHERE user_role IN ('UNDERAGE_BENEFICIARY', 'BENEFICIARY')
-    ) inn
-)
+) 
 
 SELECT
     deposit.id AS deposit_id,
@@ -189,7 +143,7 @@ SELECT
     user.user_birth_date
 FROM
     `{{ bigquery_analytics_dataset }}`.applicative_database_deposit AS deposit
-    JOIN clean_applicative_database_user AS user ON user.user_id = deposit.userId
+    JOIN `{{ bigquery_clean_dataset }}`.user_beneficiary AS user ON user.user_id = deposit.userId
     JOIN ranked_deposit_asc ON ranked_deposit_asc.deposit_id = deposit.id
     JOIN ranked_deposit_desc ON ranked_deposit_desc.deposit_id = deposit.id
     LEFT JOIN `{{ bigquery_analytics_dataset }}`.region_department ON user.user_department_code = region_department.num_dep

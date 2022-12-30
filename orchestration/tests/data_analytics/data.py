a70e51708a517da610ec568e729ee5f1d49f3e71
LucileRainteau <lucile.rainteau@passculture.app>
2022-12-30 15:39:41 +0100

    Update data.py

--- orchestration/tests/data_analytics/data.py
+++ orchestration/tests/data_analytics/data.py
@@ -812,6 +812,11 @@ ENRICHED_STOCK_DATA_EXPECTED = [
 # Enriched_user_data => user 1 is beneficiary and its department has to exist in region_department, has one used booking
 # on digital goods. This booking is corresponding to an offer, with an offered, stock and venue
 ENRICHED_USER_DATA_INPUT = {
+    "applicative_database_user": [
+        {
+            "user_id": "1",
+        }
+    ],
     "user_beneficiary": [
         {
             "user_id": "1",

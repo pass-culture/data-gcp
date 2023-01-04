a1efa15cb266bd4fb3b8299de9ef58ea2ddd1bdd
dcuesta-pass <daniel.cuesta@passculture.app>
2023-01-04 16:51:58 +0100

    [PC-19242bis] reorder sleep time

--- record_linkage/main.py
+++ record_linkage/main.py
@@ -184,9 +184,10 @@ def main(
                         repeat(batch_number),
                         range(batch_number),
                     )
+                time.sleep(10)
                 for future in futures:
                     df_matched_list.append(future)
-                time.sleep(60)
+
         df_offers_matched_list.append(pd.concat(df_matched_list))
 
     df_offers_linked_full = pd.concat(df_offers_matched_list)

ff51a741a290910b8ca16b58d74ff4bab79e5ff2
LucileRainteau <lucile.rainteau@passculture.app>
2023-01-12 11:52:35 +0100

    (PC-18912) Simplify depends_loop with a dependencies dict

--- orchestration/dags/common/utils.py
+++ orchestration/dags/common/utils.py
@@ -1,7 +1,8 @@
 from google.auth.transport.requests import Request
 from google.oauth2 import id_token
 from airflow.sensors.external_task import ExternalTaskSensor
-from itertools import zip_longest
+import base64
+import hashlib
 
 from common.config import (
     GCP_PROJECT_ID,
@@ -20,9 +21,17 @@ def getting_service_account_token(function_name):
     return open_id_connect_token
 
 
-def waiting_operator(dag, dag_id):
+def waiting_operator(dag, dag_id, task_id):
+
+    hasher = hashlib.sha1(task_id.encode("utf-8"))
+    hashed_dag_id = (
+        str(base64.urlsafe_b64encode(hasher.digest()[:10]))
+        .replace("=", "")
+        .replace("'", "")
+    )
+
     return ExternalTaskSensor(
-        task_id=f"wait_for_{dag_id}",
+        task_id=f"wait_for_{dag_id}_{hashed_dag_id}",
         external_dag_id=dag_id,
         external_task_id="end",
         check_existence=True,
@@ -37,39 +46,49 @@ def waiting_operator(dag, dag_id):
 def depends_loop(jobs: dict, default_upstream_operator, dag):
     default_downstream_operators = []
     has_downstream_dependencies = []
+
+    table_dependencies = [
+        {
+            "dependency_type": "table",
+            "task_id": table,
+            "task": jobs_def["operator"],
+            "depends_on": dependency,
+        }
+        for table, jobs_def in jobs.items()
+        for dependency in jobs_def.get("depends", [])
+    ]
+
+    dag_dependencies = [
+        {
+            "dependency_type": "dag",
+            "task_id": table,
+            "task": jobs_def["operator"],
+            "depends_on": dependency,
+        }
+        for table, jobs_def in jobs.items()
+        for dependency in jobs_def.get("dag_depends", [])
+    ]
+
+    dependencies = table_dependencies + dag_dependencies
+
     for _, jobs_def in jobs.items():
 
         operator = jobs_def["operator"]
-        dag_dependencies = jobs_def["dag_depends"]
-        dependencies = jobs_def["depends"]
         default_downstream_operators.append(operator)
 
-        if len(dependencies) == 0 and len(dag_dependencies) == 0:
-            operator.set_upstream(default_upstream_operator)
-
-        if len(dependencies) > 0 and len(dag_dependencies) == 0:
-            for d in dependencies:
-                depend_job = jobs[d]["operator"]
+        for dependency in dependencies:
+            if dependency["dependency_type"] == "dag":
+                depend_job = waiting_operator(
+                    dag, dependency["depends_on"], task_id=dependency["task_id"]
+                )
                 has_downstream_dependencies.append(depend_job)
-                operator.set_upstream(depend_job)
+                dependency["task"].set_upstream(depend_job)
+                depend_job.set_upstream(default_upstream_operator)
 
-        if len(dependencies) == 0 and len(dag_dependencies) > 0:
-            for d in dag_dependencies:
-                dag_depend_job = waiting_operator(dag, d)
-                has_downstream_dependencies.append(dag_depend_job)
-                operator.set_upstream(dag_depend_job)
-                dag_depend_job.set_upstream(default_upstream_operator)
-
-        if len(dependencies) > 0 and len(dag_dependencies) > 0:
-            for dag_d, d in zip_longest(dag_dependencies, dependencies, fillvalue=None):
-                if d is not None:
-                    depend_job = jobs[d].get("operator", None)
-                if dag_d is not None:
-                    dag_depend_job = waiting_operator(dag, dag_d)
-                has_downstream_dependencies.append(dag_depend_job)
+            if dependency["dependency_type"] == "table":
+                depend_job = jobs[dependency["depends_on"]]["operator"]
                 has_downstream_dependencies.append(depend_job)
-                dag_depend_job.set_upstream(depend_job)
-                operator.set_upstream(dag_depend_job)
+                dependency["task"].set_upstream(depend_job)
 
     return [
         x for x in default_downstream_operators if x not in has_downstream_dependencies

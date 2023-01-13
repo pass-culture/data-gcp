d3b7719a3a545e1f37ba918d5a5da5560626d0be
cdelabre <cyrille.delabre@passculture.app>
2023-01-13 16:26:16 +0100

    [HOTFIX](import) Update applicative_database_action_history

--- orchestration/dags/dependencies/import_analytics/sql/raw/action_history.sql
+++ orchestration/dags/dependencies/import_analytics/sql/raw/action_history.sql
@@ -6,15 +6,15 @@ SELECT
     , "actionDate" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS action_date
     , CAST(ah."authorUserId" AS VARCHAR(255)) AS author_user_id
     , CASE
-        WHEN ah."actionType" = 'COMMENT' THEN CAST(u."email" AS VARCHAR(255))
-        WHEN ah."actionType" = 'USER_OFFERER_VALIDATED' THEN CAST(u."email" AS VARCHAR(255))
-        WHEN ah."actionType" = 'USER_OFFERER_PENDING' THEN CAST(u."email" AS VARCHAR(255))
-        WHEN ah."actionType" = 'USER_OFFERER_REJECTED' THEN CAST(u."email" AS VARCHAR(255))
-        WHEN ah."actionType" = 'OFFERER_VALIDATED' THEN CAST(u."email" AS VARCHAR(255))
-        WHEN ah."actionType" = 'OFFERER_PENDING' THEN CAST(u."email" AS VARCHAR(255))
-        WHEN ah."actionType" = 'OFFERER_REJECTED' THEN CAST(u."email" AS VARCHAR(255))
-        WHEN ah."actionType" = 'OFFERER_SUSPENDED' THEN CAST(u."email" AS VARCHAR(255))
-        WHEN ah."actionType" = 'OFFERER_UNSUSPENDED' THEN CAST(u."email" AS VARCHAR(255))
+        WHEN ah."actionType" = \'COMMENT\' THEN CAST(u."email" AS VARCHAR(255))
+        WHEN ah."actionType" = \'USER_OFFERER_VALIDATED\' THEN CAST(u."email" AS VARCHAR(255))
+        WHEN ah."actionType" = \'USER_OFFERER_PENDING\' THEN CAST(u."email" AS VARCHAR(255))
+        WHEN ah."actionType" = \'USER_OFFERER_REJECTED\' THEN CAST(u."email" AS VARCHAR(255))
+        WHEN ah."actionType" = \'OFFERER_VALIDATED\' THEN CAST(u."email" AS VARCHAR(255))
+        WHEN ah."actionType" = \'OFFERER_PENDING\' THEN CAST(u."email" AS VARCHAR(255))
+        WHEN ah."actionType" = \'OFFERER_REJECTED\' THEN CAST(u."email" AS VARCHAR(255))
+        WHEN ah."actionType" = \'OFFERER_SUSPENDED\' THEN CAST(u."email" AS VARCHAR(255))
+        WHEN ah."actionType" = \'OFFERER_UNSUSPENDED\' THEN CAST(u."email" AS VARCHAR(255))
     END AS author_email
     , CAST(ah."userId" AS VARCHAR(255)) AS user_id
     , CAST(ah."offererId" AS VARCHAR(255)) AS offerer_id

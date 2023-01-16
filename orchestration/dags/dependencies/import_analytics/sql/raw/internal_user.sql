467d00651296dcff6d6f477632902c40e2f63d02
cdelabre <cyrille.delabre@passculture.app>
2023-01-16 19:11:07 +0100

    fix import fmt

--- orchestration/dags/dependencies/import_analytics/sql/raw/internal_user.sql
+++ orchestration/dags/dependencies/import_analytics/sql/raw/internal_user.sql
@@ -2,4 +2,4 @@ SELECT
     CAST("id" AS varchar(255)) AS user_id,
     CAST("email" AS VARCHAR(255)) as email
 FROM public.user
-WHERE "email" like '%@passculture.app' and "isActive"
+WHERE "email" like \'%@passculture.app\' and "isActive"

b8b1b3a5b3d444d8bd16abb7e0ccb6234156a3a5
Gabriel GUERIN <gabriel.guerin@sicara.com>
2023-01-19 15:20:47 +0100

    Set up feature engineering

--- orchestration/dags/dependencies/training_data/sql/two_towers/recommendation_user_features.sql
+++ orchestration/dags/dependencies/training_data/sql/two_towers/recommendation_user_features.sql
@@ -1,4 +1,21 @@
 SELECT
-  *
+  firebase_agg.user_id,
+  firebase_agg.user_engagement,
+  firebase_agg.consult_offer,
+  firebase_agg.click_book_offer,
+  stats_reco.booking_cnt,
+  ROUND(stats_reco.user_theoretical_remaining_credit, 0) AS user_theoretical_remaining_credit,
+  stats_reco.has_added_offer_to_favorites,
+  STRING_AGG(DISTINCT firebase.query, " ") AS user_queries
+
 FROM
-  `{{ bigquery_analytics_dataset }}`.firebase_aggregated_users
+  `{{ bigquery_analytics_dataset }}`.firebase_aggregated_users firebase_agg
+  JOIN `{{ bigquery_analytics_dataset }}`.aggregated_user_stats_reco stats_reco ON stats_reco.user_id = firebase_agg.user_id
+  JOIN `{{ bigquery_analytics_dataset }}`.firebase_events firebase ON firebase.user_id = firebase_agg.user_id
+
+GROUP BY
+  firebase_agg.user_id,
+  stats_reco.booking_cnt,
+  stats_reco.user_theoretical_remaining_credit,
+  stats_reco.consult_offer,
+  stats_reco.has_added_offer_to_favorites

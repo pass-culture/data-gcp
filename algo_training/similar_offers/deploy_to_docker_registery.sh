09ddbfcaa78351da21c88618069784e7446f0360
cdelabre <cyrille.delabre@passculture.app>
2023-01-23 17:02:57 +0100

    [PC-20034] Set up default algo

--- algo_training/similar_offers/deploy_to_docker_registery.sh
+++ algo_training/similar_offers/deploy_to_docker_registery.sh
@@ -4,7 +4,7 @@ ENV_SHORT_NAME=$2
 API_DOCKER_IMAGE=$3
 
 echo "${API_DOCKER_IMAGE}"
-cp -rf ${TRAIN_DIR}/${ENV_SHORT_NAME}/ ./model/
+cp -rf ${TRAIN_DIR}/${ENV_SHORT_NAME}/model ./model/
 
 gcloud auth configure-docker 
 docker build . -t $API_DOCKER_IMAGE

23927d74997f31d9f4bc0ba7e7e351008470d8a5
cdelabre <103637148+cdelabre@users.noreply.github.com>
2023-01-23 13:34:00 +0100

    [PC-19545](composer) Add gce prefix for iap tunnel (#1333)

--- orchestration/dags/common/operators/gce.py
+++ orchestration/dags/common/operators/gce.py
@@ -4,7 +4,13 @@ from airflow.providers.google.cloud.hooks.compute_ssh import ComputeEngineSSHHoo
 from airflow.exceptions import AirflowException
 from airflow.operators.bash import BashOperator
 from airflow.configuration import conf
-from common.config import GCE_ZONE, GCP_PROJECT_ID, SSH_USER, ENV_SHORT_NAME
+from common.config import (
+    GCE_ZONE,
+    GCP_PROJECT_ID,
+    GCE_BASE_PREFIX,
+    SSH_USER,
+    ENV_SHORT_NAME,
+)
 from common.hooks.gce import GCEHook, GPUImage, CPUImage
 import typing as t
 from base64 import b64encode
@@ -24,7 +30,7 @@ class StartGCEOperator(BaseOperator):
         **kwargs,
     ):
         super(StartGCEOperator, self).__init__(*args, **kwargs)
-        self.instance_name = instance_name
+        self.instance_name = f"{GCE_BASE_PREFIX}-{instance_name}"
         self.instance_type = instance_type
         self.preemptible = preemptible
         self.accelerator_types = accelerator_types
@@ -56,7 +62,7 @@ class StopGCEOperator(BaseOperator):
     ):
 
         super(StopGCEOperator, self).__init__(*args, **kwargs)
-        self.instance_name = instance_name
+        self.instance_name = f"{GCE_BASE_PREFIX}-{instance_name}"
 
     def execute(self, context):
         hook = GCEHook()
@@ -76,7 +82,7 @@ class BaseSSHGCEOperator(BaseOperator):
         **kwargs,
     ):
 
-        self.instance_name = instance_name
+        self.instance_name = f"{GCE_BASE_PREFIX}-{instance_name}"
         self.command = command
         self.environment = environment
         super(BaseSSHGCEOperator, self).__init__(*args, **kwargs)
@@ -179,7 +185,7 @@ class GCloudSSHGCEOperator(BashOperator):
         *args,
         **kwargs,
     ):
-        self.instance_name = instance_name
+        self.instance_name = f"{GCE_BASE_PREFIX}-{instance_name}"
         self.command = command
         default_command = "\n".join(
             [

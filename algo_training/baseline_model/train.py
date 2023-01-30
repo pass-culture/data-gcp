71a4ce48bfa3745a241024d0547a10bba8ba2a46
JulieP <juliep@sicara.com>
2023-01-30 15:59:45 +0100

    fix: apply read from multiple uris to baseline

--- algo_training/baseline_model/train.py
+++ algo_training/baseline_model/train.py
@@ -1,11 +1,7 @@
-import json
-
 import mlflow
-import typer
-
 import tensorflow as tf
+import typer
 from loguru import logger
-import pandas as pd
 
 from baseline_model.models.baseline_model import BaselineModel
 from baseline_model.models.match_model import MatchModel
@@ -17,6 +13,7 @@ from utils.constants import (
     TRAIN_DIR,
     MLFLOW_RUN_ID_FILENAME,
 )
+from utils.data_collect_queries import read_from_gcs
 from utils.mlflow_tools import connect_remote_mlflow, get_mlflow_experiment
 from utils.secrets_utils import get_secret
 
@@ -60,12 +57,12 @@ def train(
 
     # Load data
     logger.info("Loading & processing datasets")
-    train_data = pd.read_csv(f"{STORAGE_PATH}/{training_table_name}.csv",)[
-        ["user_id", "item_id"]
-    ].astype(str)
-    validation_data = pd.read_csv(f"{STORAGE_PATH}/{validation_table_name}.csv",)[
-        ["user_id", "item_id"]
-    ].astype(str)
+    train_data = read_from_gcs(
+        storage_path=STORAGE_PATH, table_name=training_table_name
+    )[["user_id", "item_id"]].astype(str)
+    validation_data = read_from_gcs(
+        storage_path=STORAGE_PATH, table_name=validation_table_name
+    )[["user_id", "item_id"]].astype(str)
 
     train_user_ids = train_data["user_id"].unique()
     train_item_ids = train_data["item_id"].unique()

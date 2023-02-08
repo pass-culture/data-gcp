904c4da54d946a6af02b1f1383f50087bce6a1a2
Jules-Arbelot <115459312+Jules-Arbelot@users.noreply.github.com>
2023-02-08 10:49:22 +0100

    [PC-20328](analytics) corrections annee et jointure (#1377)

--- orchestration/dags/dependencies/import_analytics/sql/analytics/user_penetration.sql
+++ orchestration/dags/dependencies/import_analytics/sql/analytics/user_penetration.sql
@@ -9,7 +9,7 @@ with population_dpt as (
       sum(population) as population
     FROM `{{ bigquery_analytics_dataset }}.population_age_and_department_france_details` pop
     LEFT JOIN `{{ bigquery_analytics_dataset }}.region_department` dep	on dep.num_dep = pop.department_code
-    WHERE pop.current_year in (2020, 2021, 2022) and cast(age as int) BETWEEN 15 AND 25
+    WHERE pop.current_year in (2020, 2021, 2022, 2023) and cast(age as int) BETWEEN 15 AND 25
     GROUP BY 1,2,3,4,5,6
   ),
 
@@ -17,11 +17,12 @@ user_booking AS (
   SELECT
     aa.active_month,
     aa.user_department_code as department_code,
-    DATE(DATE_TRUNC(ud.user_birth_date , MONTH)) as born_date,      
+    DATE(DATE_TRUNC(ud.user_birth_date , MONTH)) as born_date,
+    user_age + MOD(date_diff(active_month, DATE_TRUNC(ud.user_birth_date , MONTH), month),12)/12 decimal_age,      
     COUNT(distinct ud.user_id) as total_users,
     FROM  `{{ bigquery_analytics_dataset }}.aggregated_monthly_user_used_booking_activity` aa
     INNER JOIN `{{ bigquery_analytics_dataset }}.enriched_user_data` ud on ud.user_id = aa.user_id
-  GROUP BY 1,2,3
+  GROUP BY 1,2,3,4
 )
 
 
@@ -32,13 +33,13 @@ SELECT
   pop.department_code,
   ub.total_users,
   CASE
-   WHEN decimal_age >= 15 AND decimal_age < 18 THEN "15_17" 
-   WHEN decimal_age >= 18 AND decimal_age < 20 THEN "18_19" 
+   WHEN pop.decimal_age >= 15 AND pop.decimal_age < 18 THEN "15_17" 
+   WHEN pop.decimal_age >= 18 AND pop.decimal_age < 20 THEN "18_19" 
   ELSE "20_25"
   END AS age_range,
   population
 FROM population_dpt pop
 LEFT JOIN user_booking ub on 
     pop.active_month = ub.active_month
-    AND pop.born_date = ub.born_date
+    AND pop.decimal_age = ub.decimal_age
     AND pop.department_code = ub.department_code

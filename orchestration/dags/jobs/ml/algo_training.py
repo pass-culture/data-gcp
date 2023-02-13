ad3682ef35659837cb845480cd7df17f9b3daef8
cdelabre <cyrille.delabre@passculture.app>
2023-02-13 13:48:16 +0100

    (ml) Reduce event_day_number size for training

--- orchestration/dags/jobs/ml/algo_training.py
+++ orchestration/dags/jobs/ml/algo_training.py
@@ -48,8 +48,8 @@ dag_config = {
 train_params = {
     "batch_size": 4096,
     "embedding_size": 64,
-    "train_set_size": 0.8,
-    "event_day_number": 120 if ENV_SHORT_NAME == "prod" else 20,
+    "train_set_size": 0.9 if ENV_SHORT_NAME == "prod" else 0.8,
+    "event_day_number": 90 if ENV_SHORT_NAME == "prod" else 20,
 }
 gce_params = {
     "instance_name": f"algo-training-{ENV_SHORT_NAME}",

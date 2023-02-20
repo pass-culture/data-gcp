73f991533ecec2cfaada7565ad74d4cc29f10e95
Lucille <lucille@macairdelucille.home>
2023-02-20 14:36:10 +0100

    PC-20506 fix error

--- orchestration/dags/dependencies/cold_data/sql/analytics/institutional_partners.sql
+++ orchestration/dags/dependencies/cold_data/sql/analytics/institutional_partners.sql
@@ -1,9 +1,2 @@
-SELECT 
-    institutional_offerer_id
-    ,zendesk_id
-    ,institutional_offerer_name
-    ,city
-    ,region_name
-    ,institutional_offerer_type
-    ,related_team_pass
+SELECT *
 FROM `{{ bigquery_raw_dataset }}.institutional_partners`

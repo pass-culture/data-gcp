fa2876c60c685c8cb1718b4e66b1ac841d38f1ff
LucileRainteau <lucile.rainteau@passculture.app>
2023-02-20 11:43:58 +0100

    (FIX)(Sendinblue) Remove domains in data import

--- additional_data_sources/sendinblue-function-source/main.py
+++ additional_data_sources/sendinblue-function-source/main.py
@@ -1,6 +1,13 @@
 from sendinblue import SendinblueNewsletters
 from datetime import datetime, timezone, timedelta
-from utils import GCP_PROJECT, BIGQUERY_RAW_DATASET, ENV_SHORT_NAME, access_secret_data
+from utils import (
+    GCP_PROJECT,
+    BIGQUERY_RAW_DATASET,
+    ENV_SHORT_NAME,
+    access_secret_data,
+    histo_schema,
+)
+
 
 API_KEY = access_secret_data(
     GCP_PROJECT, f"sendinblue-api-key-{ENV_SHORT_NAME}", version_id=1
@@ -13,7 +20,7 @@ today = datetime.now(tz=timezone.utc)
 
 
 def run(request):
-    # Statistics for email campaigns Sendinblue (by domain)
+    # Statistics for email campaigns Sendinblue
 
     sendinblue_request = SendinblueNewsletters(
         gcp_project=GCP_PROJECT,
@@ -25,7 +32,7 @@ def run(request):
     )
 
     sendinblue_request.create_instance_email_campaigns_api()
-    df = sendinblue_request.get_data_by_domain()
-    sendinblue_request.save_to_historical(df)
+    df = sendinblue_request.get_data()
+    sendinblue_request.save_to_historical(df, histo_schema)
 
     return "success"

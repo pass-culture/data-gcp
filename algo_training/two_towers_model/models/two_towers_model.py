5f886e8a58500e36e19e59442640ffc48a911415
dcuesta-pass <daniel.cuesta@passculture.app>
2023-03-01 11:39:30 +0100

    [PC-20796] WIP add dev logging

--- algo_training/two_towers_model/models/two_towers_model.py
+++ algo_training/two_towers_model/models/two_towers_model.py
@@ -105,6 +105,10 @@ class SingleTowerModel(tf.keras.models.Model):
 
         self._embedding_layers = {}
         for layer_name, layer_class in self.input_embedding_layers.items():
+            print(f"layer_name: {layer_name}")
+            print(f"layer_class: {layer_class}")
+            print(f"vocab size: {self.data[layer_name].nunique()}")
+            print(f"Vocab preview: {self.data[layer_name].unique()[:2]}")
             self._embedding_layers[layer_name] = layer_class.build_sequential_layer(
                 vocabulary=self.data[layer_name].unique()
             )

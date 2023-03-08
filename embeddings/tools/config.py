f64003e9cb92897ecaf2b622f388b9acec6f6fdc
dcuesta-pass <daniel.cuesta@passculture.app>
2023-03-08 15:13:06 +0100

    [PC-20771] WIP fix config.py

--- embeddings/tools/config.py
+++ embeddings/tools/config.py
@@ -2,6 +2,6 @@ import os
 
 GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "passculture-data-ehp")
 STORAGE_PATH = os.environ.get("STORAGE_PATH", "")
-CONFIGS_PATH = os.environ.get("STORAGE_PATH", "configs")
+CONFIGS_PATH = os.environ.get("CONFIGS_PATH", "configs")
 ENV_SHORT_NAME = os.environ.get("ENV_SHORT_NAME", "ehp")
 MAX_OFFER_PER_BATCH = 50000

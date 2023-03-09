c36441b2b023def39a85e6e23f351d9476a1bc3d
LucileRainteau <lucile.rainteau@passculture.app>
2023-03-09 14:04:24 +0100

    (PC-20991) remove transactionId from cashflow table

--- orchestration/dags/dependencies/import_analytics/sql/raw/cashflow.sql
+++ orchestration/dags/dependencies/import_analytics/sql/raw/cashflow.sql
@@ -1,10 +1,9 @@
 SELECT
     CAST("id" AS varchar(255))
     , "creationDate" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' as creationDate
-    ,"status"
+    , "status"
     , CAST("bankAccountId" AS varchar(255))
     , CAST("batchId" AS varchar(255))
     , "amount"
-    , CAST("transactionId" AS varchar(255))
-    ,CAST("reimbursementPointId" AS varchar(255)) AS reimbursement_point_id
+    , CAST("reimbursementPointId" AS varchar(255)) AS reimbursement_point_id
 FROM public.cashflow

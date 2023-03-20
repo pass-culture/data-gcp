589d42706b822074a72071ff2c5f45ed220606bd
dcuesta-pass <82374920+dcuesta-pass@users.noreply.github.com>
2023-03-20 16:07:55 +0100

    PC-20771-extract-and-import-offers-metadata-emb-to-BQ (#1452)
    * [PC-20771] Extract and import offers emb to BQ
    
    * [PC-20771] WIP
    
    * [PC-20771] WIP
    
    * [PC-20771] WIP
    
    * [PC-20771] WIP
    
    * [PC-20771] WIP : fix requirements
    
    * [PC-20771] WIP fix requirements bis
    
    * [PC-20771] WIP : fix preprocess import
    
    * [PC-20771] WIP fix config.py
    
    * [PC-20771] WIP: fix arg passing
    
    * [PC-20771] WIP: fix python args
    
    * [PC-20771] WIP: add space btw args
    
    * [PC-20771] WIP fix config file
    
    * [PC-20771] WIP: fix feature_name refs
    
    * [PC-20771] WIP: fix requirements
    
    * [PC-20771] WIP fix requirements bis
    
    * [PC-20771] WIP fix main args
    
    * [PC-20771] Fix main default args
    
    * [PC-20771] WIP fix imports
    
    * [PC-20771] WIP fix main args
    
    * [PC-20771] WIP fix emb extraction
    
    * [PC-20771] Fix emb output fmt
    
    * [PC-20771] WIP: fix urls refs
    
    * [PC-20771] Fix req and parrallelisation
    
    * [PC-20771] fix imports
    
    * [PC-20771] Fix export to BQ
    
    * [PC-20771] Fix emb output format for export
    
    * [PC-20771] Fix export to BQ : list format
    
    * [PC-20771] Fix config file
    
    * [PC-20771] Fix storage already embedded data
    
    * [PC-20771] WIP fix sql and output format
    
    * [PC-20771] Fix batch handling
    
    * [PC-20771] Fix sql query
    
    * [PC-20771] clean dataset names
    
    * [PC-20771] clean main
    
    * [PC-20771] Clean var naming
    
    * [PC-20771] Fix format + rm useless file
    
    * [PC-20771] rm legacy folder
    
    * [PC-20771] Update dataset + format json
    
    * [PC-20771] Update dataset + format json
    
    * [PC-20771] update DAG + linter
    
    * [PC-20771] update DAG + linter

---
+++ orchestration/dags/jobs/ml/embedding_extraction_offers.py
@@ -0,0 +1,110 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Param
+from common.operators.gce import (
+    StartGCEOperator,
+    StopGCEOperator,
+    CloneRepositoryGCEOperator,
+    GCloudSSHGCEOperator,
+)
+from common.operators.biquery import bigquery_job_task
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from dependencies.ml.embeddings.import_offers import params
+from common import macros
+from common.alerts import task_fail_slack_alert
+from common.config import GCP_PROJECT_ID, ENV_SHORT_NAME, DAG_FOLDER
+from common.utils import get_airflow_schedule
+
+DEFAULT_REGION = "europe-west1"
+GCE_INSTANCE = f"extract-offers-embeddings-{ENV_SHORT_NAME}"
+BASE_DIR = f"data-gcp/embeddings"
+
+default_args = {
+    "start_date": datetime(2023, 3, 6),
+    "on_failure_callback": task_fail_slack_alert,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=2),
+}
+
+with DAG(
+    "embeddings_extraction_offers",
+    default_args=default_args,
+    description="Extact offer metadata embeddings",
+    schedule_interval=get_airflow_schedule("0 0 * * 0"),
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=180),
+    user_defined_macros=macros.default,
+    template_searchpath=DAG_FOLDER,
+    params={
+        "branch": Param(
+            default="production" if ENV_SHORT_NAME == "prod" else "master",
+            type="string",
+        ),
+        "instance_type": Param(
+            default="n1-standard-2" if ENV_SHORT_NAME == "dev" else "n1-standard-32",
+            type="string",
+        ),
+        "config_file_name": Param(
+            default="default-config-offer",
+            type="string",
+        ),
+    },
+) as dag:
+    data_collect_task = bigquery_job_task(
+        dag, "import_offer_batch", params, extra_params={}
+    )
+
+    gce_instance_start = StartGCEOperator(
+        task_id="gce_start_task",
+        instance_name=GCE_INSTANCE,
+        instance_type="{{ params.instance_type }}",
+    )
+
+    fetch_code = CloneRepositoryGCEOperator(
+        task_id="fetch_code",
+        instance_name=GCE_INSTANCE,
+        python_version="3.10",
+        command="{{ params.branch }}",
+    )
+
+    install_dependencies = GCloudSSHGCEOperator(
+        task_id="install_dependencies",
+        instance_name=GCE_INSTANCE,
+        base_dir=BASE_DIR,
+        command="""pip install -r requirements.txt --user""",
+    )
+
+    preprocess = GCloudSSHGCEOperator(
+        task_id="preprocess",
+        instance_name=GCE_INSTANCE,
+        base_dir=BASE_DIR,
+        command="PYTHONPATH=. python preprocess.py "
+        f"--gcp-project {GCP_PROJECT_ID} "
+        f"--env-short-name {ENV_SHORT_NAME} "
+        "--config-file-name {{ params.config_file_name }} ",
+    )
+
+    extract_embedding = GCloudSSHGCEOperator(
+        task_id="extract_embedding",
+        instance_name=GCE_INSTANCE,
+        base_dir=BASE_DIR,
+        command="PYTHONPATH=. python main.py "
+        f"--gcp-project {GCP_PROJECT_ID} "
+        f"--env-short-name {ENV_SHORT_NAME} "
+        "--config-file-name {{ params.config_file_name }} ",
+    )
+
+    gce_instance_stop = StopGCEOperator(
+        task_id="gce_stop_task", instance_name=GCE_INSTANCE
+    )
+
+    (
+        data_collect_task
+        >> gce_instance_start
+        >> fetch_code
+        >> install_dependencies
+        >> preprocess
+        >> extract_embedding
+        >> gce_instance_stop
+    )

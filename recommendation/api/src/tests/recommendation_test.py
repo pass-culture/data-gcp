2c3fd99613ef8c02699a22ba80a2da65a0ae1926
dcuesta-pass <daniel.cuesta@passculture.app>
2023-03-24 11:02:08 +0100

    [PC-20987] Update recommendation_test

--- recommendation/api/src/tests/recommendation_test.py
+++ recommendation/api/src/tests/recommendation_test.py
@@ -427,42 +427,36 @@ class RecommendationTest:
             ), f"{input_reco.json} should contain params"
 
     @pytest.mark.parametrize(
-        ["user_id", "geoloc", "sub_cat", "params_in", "use_case"],
+        ["user_id", "geoloc", "use_case"],
         [
+            (
+                "115",
+                {"longitude": 2.331289, "latitude": 48.830719},
+                "15_geoloc",
+            ),
+            (
+                "116",
+                {"longitude": 2.331289, "latitude": 48.830719},
+                "1617_geoloc",
+            ),
             (
                 "118",
                 {"longitude": 2.331289, "latitude": 48.830719},
-                ["EVENEMENT_CINE"],
-                {
-                    "offerTypeList": [
-                        {"key": "MOVIE", "value": "BOOLYWOOD"},
-                    ]
-                },
-                "18_geoloc_EVENEMENT_CINE_MOVIE",
-            ),
-            (
-                "118",
-                {"longitude": None, "latitude": None},
-                ["LIVRE_PAPIER"],
-                {
-                    "offerTypeList": [
-                        {"key": "BOOK", "value": "Histoire"},
-                    ]
-                },
-                "18_geoloc_LIVRE_PAPIER_MOVIE",
+                "18_geoloc",
             ),
         ],
     )
+    @patch("pcreco.core.recommendation.Recommendation.Algo.get_scored_offers")
     @patch("pcreco.core.utils.cold_start.get_cold_start_categories")
     @patch("pcreco.core.utils.cold_start.get_cold_start_status")
     def test_recommendation_playlist_cold_start_model(
         self,
         cold_start_status_mock: Mock,
         cold_start_categories_mock: Mock,
+        get_scored_offers_CS_algo_mock: Mock,
         setup_database: Any,
         user_id,
         geoloc,
-        subcategories,
         use_case,
     ):
         with patch("pcreco.utils.db.db_connection.__get_session") as connection_mock:
@@ -473,35 +467,22 @@ class RecommendationTest:
             user = User(user_id, longitude, latitude)
             cold_start_status_mock.return_value = True
             cold_start_categories_mock.return_value = ["SEANCE_CINE"]
-            input_reco = PlaylistParamsIn(
-                {"subcategories": subcategories, "modelEndpoint": "cold_start_model"}
-            )
+            # for this test the model is fixed as the active one
+            input_reco = PlaylistParamsIn({"modelEndpoint": "cold_start_b"})
 
             scoring = Recommendation(user, params_in=input_reco)
+            recommendable_offers = scoring.scoring.recommendable_offers
+            mock_predictions = [random.random()] * len(recommendable_offers)
+            get_scored_offers_CS_algo_mock.return_value = [
+                {**recommendation, "score": mock_predictions[i]}
+                for i, recommendation in enumerate(recommendable_offers)
+            ]
+            user_recommendations = scoring.get_scoring()
 
-            recommended_offers = scoring.scoring.get_scored_offers()
-            recommendation_sgn = [reco["subcategory_id"] for reco in recommended_offers]
-
-            if latitude is not None and longitude is not None:
-
-                assert (
-                    len(recommended_offers) > 0
-                ), f"{use_case}: playlist should not be empty"
-                assert set(recommendation_sgn) == set(
-                    subcategories
-                ), f"{use_case}: recommended subcategories are expected"
-            elif input_reco.is_event == True:
-                assert (
-                    len(recommended_offers) == 0
-                ), f"{use_case}: playlist should be empty"
-            else:
-                assert (
-                    len(recommended_offers) > 0
-                ), f"{use_case}: playlist should not be empty"
-
+            assert input_reco.has_conditions == True
             assert (
-                input_reco.has_conditions == True
-            ), f"{input_reco.json} should contain params"
+                len(user_recommendations) > 0
+            ), f"{use_case}: user_recommendations list is non empty"
 
     @pytest.mark.parametrize(
         ["user_id", "geoloc", "sub_cat", "params_in", "use_case"],

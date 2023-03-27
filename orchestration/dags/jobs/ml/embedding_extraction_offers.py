1d1d1ff54c96d2001b668d9b22a2d9ee3b2a6be8
dcuesta-pass <daniel.cuesta@passculture.app>
2023-03-27 09:42:10 +0200

    [FIX] rollback branch config

--- orchestration/dags/jobs/ml/embedding_extraction_offers.py
+++ orchestration/dags/jobs/ml/embedding_extraction_offers.py
@@ -38,7 +38,7 @@ with DAG(
     template_searchpath=DAG_FOLDER,
     params={
         "branch": Param(
-            default="production" if ENV_SHORT_NAME == "prod" else "FIX-embeddings-extraction",
+            default="production" if ENV_SHORT_NAME == "prod" else "master",
             type="string",
         ),
         "instance_type": Param(

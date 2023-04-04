f0ab81c25c208a4d9dd97c0e7897f528579fc1f9
MatthieuRipollPassCulture <matthieu.ripoll+1@passculture.app>
2023-04-04 17:29:19 +0200

    fix orchestration tests v2

--- orchestration/tests/data_analytics/data.py
+++ orchestration/tests/data_analytics/data.py
@@ -790,13 +790,12 @@ ENRICHED_USER_DATA_INPUT = {
     ],
     "user_suspension": [
         {
-            "id": "1",
+            "action_history_id": "1",
             "user_id": "1",
             "action_history_reason": None,
-            "eventType": "UNSUSPENDED",
-            "eventDate": datetime.now().replace(microsecond=0),
-            "actorUserId": "1388409",
-            "reasonCode": None,
+            "action_type": "UNSUSPENDED",
+            "action_date": datetime.now().replace(microsecond=0),
+            "author_user_id": "1388409",
             "rank": 1,
         }
     ],

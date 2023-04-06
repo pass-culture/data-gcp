35a6a70d8b0202b01ecfa2d74f92312adb6d10da
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2023-04-06 16:03:48 +0200

    [PC-21056](analytics) adapt enriched table to multi price model (#1454)

--- orchestration/dags/dependencies/import_analytics/import_clean.py
+++ orchestration/dags/dependencies/import_analytics/import_clean.py
@@ -50,6 +50,11 @@ clean_tables = {
         "destination_dataset": "{{ bigquery_clean_dataset }}",
         "destination_table": "available_stock_information",
     },
+    "cleaned_stock": {
+        "sql": f"{CLEAN_SQL_PATH}/cleaned_stock.sql",
+        "destination_dataset": "{{ bigquery_clean_dataset }}",
+        "destination_table": "cleaned_stock",
+    },
 }
 
 
--- orchestration/dags/dependencies/import_analytics/sql/analytics/enriched_stock_data.sql
+++ orchestration/dags/dependencies/import_analytics/sql/analytics/enriched_stock_data.sql
@@ -6,7 +6,7 @@ SELECT
     stock_id,
     humanize_id(stock_id) AS humanized_id
 FROM
-    `{{ bigquery_analytics_dataset }}`.applicative_database_stock
+    `{{ bigquery_clean_dataset }}`.cleaned_stock
 WHERE
     stock_id is not NULL
 )
@@ -26,9 +26,12 @@ SELECT
     stock_booking_information.booking_quantity,
     stock_booking_information.bookings_cancelled AS booking_cancelled,
     stock_booking_information.bookings_paid AS booking_paid,
-    stock.stock_price
+    stock.stock_price,
+    stock.price_category_id,
+    stock.price_category_label_id,
+    stock.price_category_label
 FROM
-    `{{ bigquery_analytics_dataset }}`.applicative_database_stock AS stock
+    `{{ bigquery_clean_dataset }}`.cleaned_stock AS stock
     LEFT JOIN `{{ bigquery_analytics_dataset }}`.applicative_database_offer AS offer ON stock.offer_id = offer.offer_id
     LEFT JOIN `{{ bigquery_analytics_dataset }}`.applicative_database_venue AS venue ON venue.venue_id = offer.venue_id
     LEFT JOIN `{{ bigquery_analytics_dataset }}`.stock_booking_information ON stock.stock_id = stock_booking_information.stock_id

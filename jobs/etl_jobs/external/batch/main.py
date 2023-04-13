7498d74df0b33ca55c02b297761ffc41f7934adb
LucileRainteau <lucile.rainteau@passculture.app>
2023-04-13 15:17:08 +0200

    (PC-18869) batch import - push code for test purposes

---
+++ jobs/etl_jobs/external/batch/main.py
@@ -0,0 +1,94 @@
+import requests
+import typer
+import pandas as pd
+from google.cloud import bigquery
+
+from utils import access_secret_data, bigquery_load_job
+from batch_client import BatchClient
+
+
+def main(
+    gcp_project_id: str = typer.Option(
+        GCP_PROJECT_ID, help="Google Cloud Platform project ID"
+    ),
+    env_short_name: str = typer.Option(
+        ENV_SHORT_NAME, help="Environment short name : dev|stg|prod"
+    ),
+    operating_system: str = typer.Option(
+        ENV_SHORT_NAME, help="Operating system of users : ios|android"
+    ),
+) -> None:
+
+    if operating_system == "android":
+        API_KEY = access_secret_data(
+            gcp_project_id, f"batch-android-api-key-{env_short_name}", version_id=1
+        )
+        REST_API_KEY = access_secret_data(
+            gcp_project_id, f"batch-android-rest-api-key-{env_short_name}", version_id=1
+        )
+    elif operating_system == "ios":
+        API_KEY = access_secret_data(
+            gcp_project_id, f"batch-ios-api-key-{env_short_name}", version_id=1
+        )
+        REST_API_KEY = access_secret_data(
+            gcp_project_id, f"batch-ios-rest-api-key-{env_short_name}", version_id=1
+        )
+    else:
+        raise RuntimeError(
+            "You need to provide an operating system supported by Batch: ios|android."
+        )
+
+    batch_client = BatchClient(API_KEY, REST_API_KEY)
+    bigquery_client = bigquery.Client()
+
+    # Campaigns
+    metadata = batch_client.get_campaigns_metadata()
+    metadata.to_gbq(
+        destination_table="{{bigquery_raw_dataset}}.batch_campaigns_ref",
+        if_exists="append",
+    )
+
+    campaigns_stats_df = batch_client.get_campaigns_stats()
+    ab_testing_df = batch_client.get_ab_testing_details(campaigns_stats_df)
+    stats = batch_client.get_campaigns_stats_detailed(campaigns_stats_df, ab_testing_df)
+    stats = stats.assign(operating_system=operating_system)
+    stats.to_gbq(
+        destination_table="{{bigquery_raw_dataset}}.batch_campaigns_stats",
+        if_exists="append",
+    )
+
+    # Transactional
+    transactional_group_ids = [
+        "Cancel_booking",
+        "Offer_link",
+        "Soon_expiring_bookings",
+        "Today_stock",
+        "Favorites_not_booked",
+    ]
+    transac_dfs = []
+
+    for group_id in transactional_group_ids:
+        df = batch_client.get_transactional_stats(group_id, start_date, end_date)
+        transac_dfs.append(df)
+
+    transac_df = pd.concat(transac_dfs).assign(operating_system=operating_system)
+
+    bigquery_load_job(
+        df=transac_df,
+        partition_date=end_date,
+        partitioning_field="update_date",
+        gcp_project_id=gcp_project_id,
+        dataset=bq_raw_dataset,
+        table_name="batch_transac",
+        schema={
+            "date": "DATE",
+            "sent": "INTEGER",
+            "direct_open": "INTEGER",
+            "influenced_open": "INTEGER",
+            "reengaged": "INTEGER",
+            "errors": "INTEGER",
+            "group_id": "STRING",
+            "update_date": "DATE",
+            "operating_system": "STRING",
+        },
+    )
---
+++ orchestration/dags/jobs/import/import_batch.py
@@ -0,0 +1,89 @@
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.models import Param
+from common.operators.gce import (
+    StartGCEOperator,
+    StopGCEOperator,
+    CloneRepositoryGCEOperator,
+    GCloudSSHGCEOperator,
+    SSHGCEOperator,
+)
+from common.alerts import task_fail_slack_alert
+from common.config import GCP_PROJECT_ID, ENV_SHORT_NAME
+from common.utils import get_airflow_schedule
+
+GCE_INSTANCE = f"import-batch-{ENV_SHORT_NAME}"
+BASE_PATH = "data-gcp/jobs/etl_jobs/external/batch"
+
+default_args = {
+    "start_date": datetime(2022, 4, 13),
+    "on_failure_callback": task_fail_slack_alert,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=2),
+}
+
+
+with DAG(
+    "import_batch",
+    default_args=default_args,
+    description="Import batch push notifications statistics",
+    schedule_interval=get_airflow_schedule("0 0 * * *"),  # import every day at 00:00
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=300),
+    params={
+        "branch": Param(
+            default="production" if ENV_SHORT_NAME == "prod" else "master",
+            type="string",
+        )
+    },
+) as dag:
+
+    start = DummyOperator(task_id="start")
+
+    gce_instance_start = StartGCEOperator(
+        instance_name=GCE_INSTANCE, task_id="gce_start_task"
+    )
+
+    fetch_code = CloneRepositoryGCEOperator(
+        task_id="fetch_code",
+        instance_name=GCE_INSTANCE,
+        command="{{ params.branch }}",
+        python_version="3.10",
+    )
+
+    install_dependencies = SSHGCEOperator(
+        task_id="install_dependencies",
+        instance_name=GCE_INSTANCE,
+        base_dir=BASE_PATH,
+        command="pip install -r requirements.txt --user",
+        dag=dag,
+    )
+
+    ios_job = GCloudSSHGCEOperator(
+        task_id=f"import_ios",
+        instance_name=GCE_INSTANCE,
+        base_dir=BASE_PATH,
+        command=f"""
+        python main.py --operating_system=ios --gcp_project_id={GCP_PROJECT_ID} --env_short_name={ENV_SHORT_NAME}        
+        """,
+    )
+
+    android_job = GCloudSSHGCEOperator(
+        task_id=f"import_android",
+        instance_name=GCE_INSTANCE,
+        base_dir=BASE_PATH,
+        command=f"""
+        python main.py --operating_system=android --gcp_project_id={GCP_PROJECT_ID} --env_short_name={ENV_SHORT_NAME}        
+        """,
+    )
+
+    gce_instance_stop = StopGCEOperator(
+        instance_name=GCE_INSTANCE, task_id="gce_stop_task"
+    )
+
+    (start >> gce_instance_start >> fetch_code >> install_dependencies)
+    (install_dependencies >> ios_job >> gce_instance_stop)
+    (install_dependencies >> android_job >> gce_instance_stop)

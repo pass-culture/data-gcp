91ea0ac2d5aec65a3a7665cb20d81d94d68f63f5
cdelabre <103637148+cdelabre@users.noreply.github.com>
2023-04-17 11:06:24 +0200

    [HOTFIX] Fix EAC Students (#1520)

--- orchestration/dags/dependencies/propilote/sql/tmp/taux_participation_eac_jeunes/propilote_tmp_participation_eac_jeunes.sql
+++ orchestration/dags/dependencies/propilote/sql/tmp/taux_participation_eac_jeunes/propilote_tmp_participation_eac_jeunes.sql
@@ -1,3 +1,11 @@
+WITH last_day AS (
+SELECT 
+    DATE_TRUNC(date,MONTH) AS date,
+    MAX(date) AS last_date 
+FROM `{{ bigquery_analytics_dataset }}.adage_involved_student`
+GROUP BY 1
+)
+
 SELECT
     DATE_TRUNC(date, MONTH) AS mois
     , "{{ params.group_type }}" as dimension_name
@@ -11,6 +19,9 @@ SELECT
     , SUM(involved_students) AS numerator -- students_involved_in_eac_offer
     , SUM(total_involved_students) AS denominator -- students_eligible
 FROM `{{ bigquery_analytics_dataset }}.adage_involved_student` as involved
+-- take only last day for each month.
+JOIN last_day ON last_day.last_date = involved.date AND DATE_TRUNC(involved.date,MONTH) = last_day.date 
+
 LEFT JOIN `{{ bigquery_analytics_dataset }}.region_department` as rd
     ON involved.department_code = rd.num_dep
 WHERE

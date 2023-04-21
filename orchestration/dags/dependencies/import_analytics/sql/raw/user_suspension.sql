60c56d45c6927097738f49ebf580196958d828f5
LucileRainteau <lucile.rainteau@passculture.app>
2023-04-21 10:10:38 +0200

    (FIX) Remove user_suspension import

--- orchestration/dags/dependencies/import_analytics/sql/raw/user_suspension.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    CAST("id" AS varchar(255))
-    , CAST("userId" AS varchar(255))
-    , "eventType"
-    , "eventDate" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' as eventDate
-    , cast("actorUserId" AS VARCHAR(255))
-    , "reasonCode"
-FROM public.user_suspension

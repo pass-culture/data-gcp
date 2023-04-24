40fe13eb2c20e9d1e537913d89268d082aae8ff8
dcuesta-pass <daniel.cuesta@passculture.app>
2023-04-24 13:53:47 +0200

    [FIX] Fix multiprocessing indexes

--- jobs/ml_jobs/embeddings/tools/embedding_extraction.py
+++ jobs/ml_jobs/embeddings/tools/embedding_extraction.py
@@ -9,7 +9,7 @@ from PIL import Image
 from sentence_transformers import SentenceTransformer
 from tools.logging_tools import log_duration
 from tqdm import tqdm
-
+import shutil 
 
 def extract_embedding(
     df_data,
@@ -61,7 +61,7 @@ def encode_img_from_urls(model, urls):
             offer_wo_img += 1
     print(f"{(offer_wo_img*100)/len(urls)}% offers dont have image")
     print("Removing image on local disk...")
-    os.remove(f"./img")
+    shutil.rmtree(f"./img")
     return offer_img_embs
 
 
@@ -73,25 +73,28 @@ def download_img_multiprocess(urls):
     print(
         f"Starting process... with {batch_number} CPUs, subset length: {subset_length} "
     )
-    with concurrent.futures.ProcessPoolExecutor(max_process) as executor:
-        futures = executor.map(
+    with concurrent.futures.ProcessPoolExecutor(batch_number) as executor:
+        futures=executor.map(
             _download_img_from_url_list,
             repeat(urls),
             repeat(subset_length),
+            repeat(batch_number),
             range(batch_number),
         )
     print("Multiprocessing done")
     return
 
 
-def _download_img_from_url_list(urls, subset_length, batch_id):
+def _download_img_from_url_list(urls, subset_length, batch_number, batch_id):
     try:
         temp_urls = urls[batch_id * subset_length : (batch_id + 1) * subset_length]
-        index = batch_id if batch_id == 0 else (batch_id * subset_length) + 1
+        index = batch_id if batch_id == 0 else (batch_id * subset_length)
+        if batch_id == (batch_number - 1):
+            temp_urls = urls[batch_id * subset_length :]
         for url in temp_urls:
             STORAGE_PATH_IMG = f"./img/{index}"
             __download_img_from_url(url, STORAGE_PATH_IMG)
-            index = +1
+            index += 1
         return
     except:
         return
@@ -100,5 +103,6 @@ def _download_img_from_url_list(urls, subset_length, batch_id):
 def __download_img_from_url(url, storage_path):
     try:
         urllib.request.urlretrieve(url, f"{storage_path}.jpeg")
+        return
     except:
         return None

a3a0a0eceda56dc446ee6724c56500c77e402d20
cdelabre <cyrille.delabre@passculture.app>
2023-05-11 15:08:19 +0200

    (analytics) Fix dpt codes in clean stage

---
+++ orchestration/dags/dependencies/import_analytics/sql/clean/venue.sql
@@ -0,0 +1,13 @@
+
+SELECT
+    * except(venue_department_code),
+    COALESCE(CASE
+        WHEN SUBSTRING(venue_postal_code, 0, 2) = '97' THEN SUBSTRING(venue_postal_code, 0, 3)
+        WHEN SUBSTRING(venue_postal_code, 0, 2) = '98' THEN SUBSTRING(venue_postal_code, 0, 3)
+        WHEN SUBSTRING(venue_postal_code, 0, 3) in ('200', '201', '209') THEN '2A'
+        WHEN SUBSTRING(venue_postal_code, 0, 3) in ('202', '206') THEN '2B'
+        ELSE SUBSTRING(venue_postal_code, 0, 2)
+        END, 
+        venue_department_code
+    ) AS venue_department_code
+FROM `{{ bigquery_raw_dataset }}`.applicative_database_venue

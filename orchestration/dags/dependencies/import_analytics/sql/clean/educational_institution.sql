18c9c895be8f4a8281199d5c1e849861f812cdc7
cdelabre <cyrille.delabre@passculture.app>
2023-05-11 15:40:22 +0200

    (analytics) Fix dpt codes in clean stage

--- orchestration/dags/dependencies/import_analytics/sql/clean/educational_institution.sql
+++ orchestration/dags/dependencies/import_analytics/sql/clean/educational_institution.sql
@@ -2,11 +2,11 @@
 SELECT
     * except(institution_departement_code),
     COALESCE(CASE
-        WHEN SUBSTRING(institution_departement_code, 0, 2) = '97' THEN SUBSTRING(institution_departement_code, 0, 3)
-        WHEN SUBSTRING(institution_departement_code, 0, 2) = '98' THEN SUBSTRING(institution_departement_code, 0, 3)
-        WHEN SUBSTRING(institution_departement_code, 0, 3) in ('200', '201', '209') THEN '2A'
-        WHEN SUBSTRING(institution_departement_code, 0, 3) in ('202', '206') THEN '2B'
-        ELSE SUBSTRING(institution_departement_code, 0, 2)
+        WHEN SUBSTRING(institution_postal_code, 0, 2) = '97' THEN SUBSTRING(institution_postal_code, 0, 3)
+        WHEN SUBSTRING(institution_postal_code, 0, 2) = '98' THEN SUBSTRING(institution_postal_code, 0, 3)
+        WHEN SUBSTRING(institution_postal_code, 0, 3) in ('200', '201', '209') THEN '2A'
+        WHEN SUBSTRING(institution_postal_code, 0, 3) in ('202', '206') THEN '2B'
+        ELSE SUBSTRING(institution_postal_code, 0, 2)
         END, 
         institution_departement_code
     ) AS institution_departement_code

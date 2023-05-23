53c1e1ddd729bf63e9c22fe94c316a2ca4fd1b57
LucileRainteau <lucile.rainteau@passculture.app>
2023-05-23 12:22:39 +0200

    (FIX)(Firebase) fix partitioning for firebase_bookings

--- orchestration/dags/dependencies/firebase/sql/analytics/firebase_bookings.sql
+++ orchestration/dags/dependencies/firebase/sql/analytics/firebase_bookings.sql
@@ -7,3 +7,8 @@ SELECT
     , event_timestamp as booking_timestamp
 FROM `{{ bigquery_analytics_dataset }}.firebase_events`
 WHERE event_name = "BookingConfirmation"
+  {% if params.dag_type == 'intraday' %}
+  AND event_date = DATE('{{ ds }}')        
+  {% else %}
+  AND event_date = DATE('{{ add_days(ds, -1) }}')
+  {% endif %}

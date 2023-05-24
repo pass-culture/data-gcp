fffc1c7d5634ba61140d0a69767aee512a697325
LucileRainteau <lucile.rainteau@passculture.app>
2023-05-24 16:31:47 +0200

    set master branch in ehp

--- orchestration/dags/jobs/import/import_dms_subscriptions.py
+++ orchestration/dags/jobs/import/import_dms_subscriptions.py
@@ -58,7 +58,7 @@ with DAG(
     dagrun_timeout=timedelta(minutes=300),
     params={
         "branch": Param(
-            default="production" if ENV_SHORT_NAME == "prod" else "dms-pro-fix-partition",
+            default="production" if ENV_SHORT_NAME == "prod" else "master",
             type="string",
         ),
         "updated_since_jeunes": Param(

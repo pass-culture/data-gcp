d9ca53dcf9101e1ade8602ac5b6eb3e18c8b6658
cdelabre <103637148+cdelabre@users.noreply.github.com>
2023-06-13 11:13:21 +0200

    (reco) Similar Offers V2 (#1622)

--- apps/recommendation/api/src/pcreco/utils/health_check_queries.py
+++ apps/recommendation/api/src/pcreco/utils/health_check_queries.py
@@ -14,6 +14,18 @@ def does_materialized_view_exist(connection: Any, materialized_view_name: str) -
     return connection.execute(query).scalar()
 
 
+def get_available_materialized_view(materialized_view_name: str) -> bool:
+    connection = get_session()
+    for suffix in ["_mv", "_mv_tmp", "_mv_old"]:
+        table_name = f"{materialized_view_name}{suffix}"
+        result = connection.execute(
+            f"""SELECT EXISTS(SELECT FROM pg_matviews WHERE matviewname = '{table_name}');"""
+        ).scalar()
+        if result:
+            return table_name
+    return materialized_view_name
+
+
 def get_materialized_view_status(materialized_view_name: str) -> dict:
     connection = get_session()
 

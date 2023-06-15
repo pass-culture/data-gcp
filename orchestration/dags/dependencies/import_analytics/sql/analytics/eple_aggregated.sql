c89f9bd9eecf02d9b049269dc204a0cd8388c7db
Jules-Arbelot <115459312+Jules-Arbelot@users.noreply.github.com>
2023-06-15 14:45:20 +0200

    Update eple_aggregated.sql

--- orchestration/dags/dependencies/import_analytics/sql/analytics/eple_aggregated.sql
+++ orchestration/dags/dependencies/import_analytics/sql/analytics/eple_aggregated.sql
@@ -21,8 +21,8 @@ eple_bookings AS (
 SELECT 
     eple_infos.institution_id
     ,eple_infos.scholar_year
-    ,SUM(CASE WHEN collective_booking_status != 'CANCELLED' THEN booking_amount ELSE NULL END) AS montant_depense_theorique
-    ,SUM(CASE WHEN collective_booking_status IN ('USED','REIMBURSED') THEN booking_amount ELSE NULL END) AS montant_depense_reel
+    ,SUM(CASE WHEN collective_booking_status != 'CANCELLED' THEN booking_amount ELSE NULL END) AS theoric_amount_spent
+    ,SUM(CASE WHEN collective_booking_status IN ('USED','REIMBURSED') THEN booking_amount ELSE NULL END) AS real_amount_spent
 FROM eple_infos
 JOIN `{{ bigquery_analytics_dataset }}.enriched_collective_booking_data` ecbd ON ecbd.educational_institution_id = eple_infos.institution_id AND ecbd.scholar_year = eple_infos.scholar_year
 GROUP BY 1, 2)
@@ -39,11 +39,11 @@ GROUP BY 1,2)
 ,nb_eleves_educonnectes_per_eple AS (
 SELECT  
      TRIM(json_extract(result_content, '$.school_uai'), '"') AS school 
-    , COUNT(DISTINCT edd.user_id) AS nb_jeunes_credited
-    , COUNT(DISTINCT CASE WHEN DATE_DIFF(current_date, deposit_creation_date, DAY) <= 365 THEN edd.user_id ELSE NULL END) AS nb_jeunes_credited_last_12_months
+    , COUNT(DISTINCT edd.user_id) AS educonnect_inscriptions
+    , COUNT(DISTINCT CASE WHEN DATE_DIFF(current_date, deposit_creation_date, DAY) <= 365 THEN edd.user_id ELSE NULL END) AS last_12_months_inscriptions
     , AVG(COALESCE(deposit_theoretical_amount_spent,0)) AS avg_spent_per_user
     , SAFE_DIVIDE(SUM(deposit_theoretical_amount_spent), SUM(deposit_amount)) AS pct_spent
-    , COUNT(DISTINCT ebd.user_id) AS nb_jeunes_credit_utilise
+    , COUNT(DISTINCT ebd.user_id) AS nb_credit_used_students
 FROM `{{ bigquery_clean_dataset }}.applicative_database_beneficiary_fraud_check` bfc
 JOIN `{{ bigquery_analytics_dataset }}.enriched_deposit_data` edd ON edd.user_id = bfc.user_id
 LEFT JOIN `{{ bigquery_analytics_dataset }}.enriched_booking_data` ebd ON ebd.user_id = edd.user_id AND not booking_is_cancelled
@@ -64,19 +64,18 @@ SELECT
     ,eple_infos.institution_type
     ,eple_infos.scholar_year
     ,institution_current_deposit_amount
-    ,montant_depense_theorique
-    ,SAFE_DIVIDE(montant_depense_theorique, institution_current_deposit_amount) AS part_credit_depense_theorique
-    ,montant_depense_reel
-    ,SAFE_DIVIDE(montant_depense_reel, institution_current_deposit_amount) AS part_credit_depense_reel
-    ,nb_students AS nb_total_eleves
-    ,nb_jeunes_credited AS nb_inscrits_educonnect
-    ,nb_jeunes_credited_last_12_months AS dont_inscrits_last_12_months
-    ,nb_jeunes_credit_utilise
+    ,theoric_amount_spent
+    ,SAFE_DIVIDE(theoric_amount_spent, institution_current_deposit_amount) AS pct_credit_theoric_amount_spent
+    ,real_amount_spent
+    ,SAFE_DIVIDE(real_amount_spent, institution_current_deposit_amount) AS pct_credit_real_amount_spent
+    ,nb_students AS total_students
+    ,educonnect_inscriptions
+    ,last_12_months_inscriptions
+    ,nb_credit_used_students
     ,avg_spent_per_user
     ,pct_spent AS pct_spent_per_user
-    ,SAFE_DIVIDE(nb_jeunes_credited_last_12_months, nb_students) AS part_eleves_beneficiaires
+    ,SAFE_DIVIDE(last_12_months_inscriptions, nb_students) AS pct_beneficiary_students
 FROM eple_infos
 LEFT JOIN eple_bookings ON eple_bookings.institution_id = eple_infos.institution_id AND eple_infos.scholar_year = eple_bookings.scholar_year
 LEFT JOIN total_nb_of_students ON eple_infos.institution_id = total_nb_of_students.institution_id
 LEFT JOIN nb_eleves_educonnectes_per_eple ON eple_infos.institution_external_id = nb_eleves_educonnectes_per_eple.school
-ORDER BY montant_depense_theorique DESC, nb_students DESC

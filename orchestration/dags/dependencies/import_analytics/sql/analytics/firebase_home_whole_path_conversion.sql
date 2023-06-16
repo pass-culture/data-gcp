9f33c57a27dac0548126521a472f1912d38d6f50
cdelabre <103637148+cdelabre@users.noreply.github.com>
2023-06-16 09:57:09 +0200

    (analytics) firebase_home fix SQL typo (#1634)

--- orchestration/dags/dependencies/import_analytics/sql/analytics/firebase_home_whole_path_conversion.sql
+++ orchestration/dags/dependencies/import_analytics/sql/analytics/firebase_home_whole_path_conversion.sql
@@ -15,7 +15,7 @@ SELECT
     , COUNT( CASE WHEN consult_offer_timestamp IS NOT NULL THEN 1 ELSE NULL END) AS nb_consult_offer
     , COUNT(DISTINCT CASE WHEN booking_timestamp IS NOT NULL THEN CONCAT(session_id, user_pseudo_id) ELSE NULL END) AS nb_sesh_booking
     , COUNT( CASE WHEN booking_timestamp IS NOT NULL THEN 1 ELSE NULL END) AS nb_bookings
-    , COUNT( CASE WHEN diversification_booking.booking_id IS NOT NULL THEN 1 ELSE NULL END) AS nb_bookings_non_cancelled
+    , COUNT( CASE WHEN booking_id IS NOT NULL THEN 1 ELSE NULL END) AS nb_bookings_non_cancelled
     , SUM(delta_diversification) AS total_diversification
 FROM `{{ bigquery_analytics_dataset }}.firebase_home_funnel_conversion`
 LEFT JOIN `{{ bigquery_clean_dataset }}.applicative_database_user` USING(user_id)

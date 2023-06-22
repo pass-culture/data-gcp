671d24f33d673ce84bfd7f5574a2a1a183803e53
cdelabre <cyrille.delabre@passculture.app>
2023-06-22 15:53:40 +0200

    (analytics) add offerer_tag_* to history

--- orchestration/dags/dependencies/import_analytics/import_historical.py
+++ orchestration/dags/dependencies/import_analytics/import_historical.py
@@ -47,6 +47,27 @@ historical_clean_applicative_database = {
         "time_partitioning": {"field": "partition_date"},
         "clustering_fields": {"fields": ["partition_date"]},
     },
+    "offerer_tag_history": {
+        "sql": f"{CLEAN_APPLICATIVE_DATABASE_SQL_PATH}/offerer_tag_history.sql",
+        "destination_dataset": "{{ bigquery_clean_dataset }}",
+        "destination_table": "applicative_database_offerer_tag_history${{ yyyymmdd(yesterday()) }}",
+        "time_partitioning": {"field": "partition_date"},
+        "clustering_fields": {"fields": ["partition_date"]},
+    },
+    "offerer_tag_mapping_history": {
+        "sql": f"{CLEAN_APPLICATIVE_DATABASE_SQL_PATH}/offerer_tag_mapping_history.sql",
+        "destination_dataset": "{{ bigquery_clean_dataset }}",
+        "destination_table": "applicative_database_offerer_tag_mapping_history${{ yyyymmdd(yesterday()) }}",
+        "time_partitioning": {"field": "partition_date"},
+        "clustering_fields": {"fields": ["partition_date"]},
+    },
+    "offerer_tag_category_mapping_history": {
+        "sql": f"{CLEAN_APPLICATIVE_DATABASE_SQL_PATH}/offerer_tag_category_mapping_history.sql",
+        "destination_dataset": "{{ bigquery_clean_dataset }}",
+        "destination_table": "applicative_database_offerer_tag_category_mapping_history${{ yyyymmdd(yesterday()) }}",
+        "time_partitioning": {"field": "partition_date"},
+        "clustering_fields": {"fields": ["partition_date"]},
+    },
     "stock_history": {
         "sql": f"{CLEAN_APPLICATIVE_DATABASE_SQL_PATH}/stock_history.sql",
         "destination_dataset": "{{ bigquery_clean_dataset }}",

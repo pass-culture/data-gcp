bc3eb63207b5e62775ee26b69dc2505de6b698c8
cdelabre <103637148+cdelabre@users.noreply.github.com>
2023-07-03 08:42:51 +0200

    (ml) Faiss CPU (#1642)

--- jobs/ml_jobs/algo_training/similar_offers/app/main.py
+++ jobs/ml_jobs/algo_training/similar_offers/app/main.py
@@ -4,6 +4,7 @@ from loguru import logger
 import tensorflow as tf
 
 import numpy as np
+import pandas as pd
 import faiss
 
 app = Flask(__name__)
@@ -16,73 +17,93 @@ def load_model():
     embedding_item_model = tf_reco.item_layer.layers[1].get_weights()
     model_weights = embedding_item_model[0]
     distance = len(model_weights[0])
-    index = faiss.IndexFlatL2(distance)
+
+    quantizer = faiss.IndexFlatL2(distance)
+    index = faiss.IndexIVFPQ(quantizer, distance, 16, 8, 4)
+    index.train(model_weights)
     index.add(model_weights)
     return index, model_weights, offer_item_model
 
 
-def compute_distance_subset(index, xq, subset):
-    n, _ = xq.shape
-    _, k = subset.shape
-    distances = np.empty((n, k), dtype=np.float32)
-    index.compute_distance_subset(
-        n, faiss.swig_ptr(xq), k, faiss.swig_ptr(distances), faiss.swig_ptr(subset)
-    )
-    return distances
-
-
 class FaissModel:
-    def __init__(self, faiss_index, model_weights, offer_list):
+    def __init__(self, faiss_index, model_weights, item_list):
         self.faiss_index = faiss_index
         self.item_dict = {}
-        self.offer_list = offer_list
-        for idx, (x, y) in enumerate(zip(offer_list, model_weights)):
+        self.categories = {}
+        self.item_list = item_list
+        for idx, (x, y) in enumerate(zip(item_list, model_weights)):
             self.item_dict[x] = {"embeddings": y, "idx": idx}
+        self.set_up_item_indexes()
 
-    def get_offer_emb(self, offer_id):
-        embs = self.item_dict.get(offer_id, None)
+    def set_up_item_indexes(self):
+        df = pd.read_parquet("./metadata/item_metadata.parquet")
+        for _, row in df.iterrows():
+            idx = self.get_item_idx(row["item_id"])
+            if idx is not None:
+                self.categories.setdefault(row["category"], []).append(idx)
+
+    def get_idx_from_categories(self, categories):
+        result = []
+        for cat in categories:
+            result.extend(self.categories[cat])
+        return result
+
+    def get_offer_emb(self, item_id):
+        embs = self.item_dict.get(item_id, None)
         if embs is not None:
             return np.array([embs["embeddings"]])
         else:
             return None
 
-    def get_offer_idx(self, offer_id):
-        embs = self.item_dict.get(offer_id, None)
+    def get_item_idx(self, item_id):
+        embs = self.item_dict.get(item_id, None)
         if embs is not None:
             return embs["idx"]
         else:
             return None
 
-    def selected_to_idx(self, selected_offers):
+    def selected_to_idx(self, selected_items):
         arr = []
-        for offer_id in selected_offers:
-            idx = self.get_offer_idx(offer_id)
+        for item_id in selected_items:
+            idx = self.get_item_idx(item_id)
             if idx is not None:
                 arr.append(idx)
-        return np.array([arr])
+        return np.array(arr)
 
-    def distances_to_offer(self, nn_idx, n=10):
-        if len(nn_idx) > n:
-            nn_idx = nn_idx[:n]
-        return [self.offer_list[index] for (index, _) in nn_idx]
+    def distances_to_offer(self, distances, indexes, n=10):
+        if len(indexes) > n:
+            distances = distances[:n]
+            indexes = indexes[:n]
+        out_preds, out_dist = [], []
+        for (distance, index) in zip(distances, indexes):
+            if index > 0:
+                out_preds.append(self.item_list[index])
+                out_dist.append(distance)
 
-    def compute_distance(self, offer_id, selected_offers, n=10):
-        offer_emb = self.get_offer_emb(offer_id)
-        subset_offer_indexes = self.selected_to_idx(selected_offers)
-        if offer_emb is not None and subset_offer_indexes is not None:
-            distances = compute_distance_subset(
-                self.faiss_index, offer_emb, subset_offer_indexes
-            ).tolist()[0]
-            nn_idx = sorted(
-                zip(subset_offer_indexes.tolist()[0], distances), key=lambda tup: tup[1]
+        return {"predictions": out_preds}
+
+    def compute_distance(self, item_id, selected_idx=None, n=10):
+        offer_emb = self.get_offer_emb(item_id)
+        params = None
+        if selected_idx is not None:
+            params = faiss.SearchParametersIVF(sel=faiss.IDSelectorBatch(selected_idx))
+
+        if offer_emb is not None:
+            results = faiss_index.search(
+                offer_emb,
+                k=n,
+                params=params,
             )
+            return self.distances_to_offer(
+                distances=results[0][0], indexes=results[1][0], n=n
+            )
+        return {"predictions": []}
 
-            return self.distances_to_offer(nn_idx, n)
-        return []
 
+logger.info("Load app...")
 
-faiss_index, model_weights, offer_list = load_model()
-faiss_model = FaissModel(faiss_index, model_weights, offer_list)
+faiss_index, model_weights, item_list = load_model()
+faiss_model = FaissModel(faiss_index, model_weights, item_list)
 
 logger.info("Loaded model")
 
@@ -99,19 +120,23 @@ def predict():
 
     req_json = request.get_json()
     input_json = req_json["instances"]
-    offer_id = input_json[0]["offer_id"]
-    selected_offers = input_json[0].get("selected_offers", None)
+    item_id = input_json[0]["offer_id"]
+    selected_categories = input_json[0].get("selected_categories", [])
+    selected_idx = faiss_model.get_idx_from_categories(selected_categories)
+    selected_offers = input_json[0].get("selected_offers", [])
+    if len(selected_idx) == 0:
+        selected_idx = None
+    if len(selected_offers) > 0:
+        selected_idx = faiss_model.selected_to_idx(selected_offers)
     try:
         n = int(input_json[0].get("size", 10))
     except:
         n = 10
     try:
-        if selected_offers is None:
-            # TODO
-            selected_offers = offer_list
-        sim_offers = faiss_model.compute_distance(offer_id, selected_offers, n=n)
-        logger.info(f"out {len(sim_offers)}")
-        return jsonify({"predictions": sim_offers})
+        sim_offers = faiss_model.compute_distance(
+            item_id, selected_idx=selected_idx, n=n
+        )
+        return jsonify(sim_offers)
     except Exception as e:
         logger.info(e)
         logger.info("error")

2ba347e1f50554a3d09b19ad6c9e8b002c3ff97f
cdelabre <cyrille.delabre@passculture.app>
2023-07-05 11:00:36 +0200

    small fixes

--- jobs/ml_jobs/algo_training/similar_offers/deploy_to_docker_registery.sh
+++ jobs/ml_jobs/algo_training/similar_offers/deploy_to_docker_registery.sh
@@ -3,7 +3,7 @@ API_DOCKER_IMAGE=$1
 
 echo "${API_DOCKER_IMAGE}"
 
-gcloud auth configure-docker 
+yes | gcloud auth configure-docker 
 docker build . -t $API_DOCKER_IMAGE
 docker push $API_DOCKER_IMAGE
 
--- orchestration/dags/jobs/ml/algo_training.py
+++ orchestration/dags/jobs/ml/algo_training.py
@@ -219,8 +219,8 @@ with DAG(
         command="python main.py "
         "--experiment-name similar_offers_{{ params.input_type }}"
         + f"_v2.1_{ENV_SHORT_NAME} "
-        "--model-name v2.1"
-        f"--source-experiment-name {dag_config['EXPERIMENT_NAME']}",
+        "--model-name v2.1 "
+        f"--source-experiment-name {dag_config['EXPERIMENT_NAME']} ",
         dag=dag,
     )
 

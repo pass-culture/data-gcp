4a60ece7a8700b0c8478b2169c0202242e7bfd76
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2023-07-10 15:48:43 +0200

    [PC-23210] create churned actors listing for marketing purposes (#1675)
    * create churned actors listing for marketing purposes
    
    * alias for clarity

--- orchestration/dags/dependencies/backend/create_tables.py
+++ orchestration/dags/dependencies/backend/create_tables.py
@@ -9,4 +9,20 @@ create_tables = {
         "schedule_type": "weekly",
         "write_disposition": "WRITE_APPEND",
     },
+    "marketing_pro_email_churned_40_days_ago": {
+        "sql": f"{BACKEND_SQL_PATH}/marketing_pro_email_churned_40_days_ago.sql",
+        "destination_dataset": "{{ bigquery_backend_dataset }}",
+        "destination_table": "marketing_pro_email_churned_40_days_ago{{ yyyymmdd(today()) }}",
+        "time_partitioning": {"field": "execution_date"},
+        "schedule_type": "daily",
+        "write_disposition": "WRITE_APPEND",
+    },
+    "marketing_pro_email_last_booking_40_days_ago": {
+        "sql": f"{BACKEND_SQL_PATH}/marketing_pro_email_last_booking_40_days_ago.sql",
+        "destination_dataset": "{{ bigquery_backend_dataset }}",
+        "destination_table": "marketing_pro_email_last_booking_40_days_ago{{ yyyymmdd(today()) }}",
+        "time_partitioning": {"field": "execution_date"},
+        "schedule_type": "daily",
+        "write_disposition": "WRITE_APPEND",
+    },
 }

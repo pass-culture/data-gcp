27ea721c7395aa09d5a0114108e9354d39057efb
cdelabre <cyrille.delabre@passculture.app>
2023-07-18 16:25:11 +0200

    Update similar offer model definition

--- apps/recommendation/api/src/pcreco/core/model_selection/similar_offer.py
+++ apps/recommendation/api/src/pcreco/core/model_selection/similar_offer.py
@@ -2,28 +2,74 @@ from pcreco.utils.env_vars import ENV_SHORT_NAME
 
 import pcreco.core.scorer.recommendable_offer as offer_scorer
 from pcreco.core.scorer.similar_offer import (
-    SimilarOfferEndpoint,
     DummyEndpoint,
-    SimilarOfferV2Endpoint,
+    SimilarOfferEndpoint,
 )
 from pcreco.core.model_selection.model_configuration import ModelConfiguration
 
 SIMILAR_OFFER_ENDPOINTS = {
-    # Deprecated
     "default": ModelConfiguration(
-        name="default",
+        name="item_v2",
         description="""
-        Default model:
-        Takes the top {retrieval_limit} offers nearest the user iris (SQL)
-        Takes the top {ranking_limit} most similar ones (training based on clicks) (NN)
+        Item model:
+        Takes most similar ones (training based on clicks) (NN)
+        Sort top 500 most similar, by distance range and similarity score (SQL)
         """,
-        scorer=offer_scorer.OfferIrisRetrieval,
-        scorer_order_columns="booking_number",
-        scorer_order_ascending=False,
+        scorer=offer_scorer.SimilarOfferItemRanker,
+        scorer_order_columns="order",
+        scorer_order_ascending=True,
         endpoint=SimilarOfferEndpoint(f"similar_offers_default_{ENV_SHORT_NAME}"),
-        retrieval_order_query="booking_number DESC",
-        retrieval_limit=10_000,
-        ranking_order_query="booking_number DESC",
+        retrieval_order_query=None,
+        retrieval_limit=500,
+        ranking_order_query="item_score DESC",
+        ranking_limit=20,
+    ),
+    "version_b": ModelConfiguration(
+        name="item_v2_version_b",
+        description="""
+        Item model:
+        Takes most similar ones (training based on clicks) (NN)
+        Sort top 500 most similar, by distance range and similarity score (SQL)
+        """,
+        scorer=offer_scorer.SimilarOfferItemRanker,
+        scorer_order_columns="order",
+        scorer_order_ascending=True,
+        endpoint=SimilarOfferEndpoint(f"similar_offers_version_b_{ENV_SHORT_NAME}"),
+        retrieval_order_query=None,
+        retrieval_limit=500,
+        ranking_order_query="item_score DESC",
+        ranking_limit=20,
+    ),
+    "version_c": ModelConfiguration(
+        name="version_c",
+        description="""
+        Item model:
+        Takes most similar ones
+        Sort top 500 most similar, by distance range and similarity score (SQL)
+        """,
+        scorer=offer_scorer.SimilarOfferItemRanker,
+        scorer_order_columns="order",
+        scorer_order_ascending=True,
+        endpoint=SimilarOfferEndpoint(f"similar_offers_version_c_{ENV_SHORT_NAME}"),
+        retrieval_order_query=None,
+        retrieval_limit=500,
+        ranking_order_query="user_km_distance ASC, item_score DESC",
+        ranking_limit=20,
+    ),
+    "cold_start": ModelConfiguration(
+        name="cold_start",
+        description="""
+        Item model:
+        Takes most similar ones
+        Sort top 500 most similar, by distance range and similarity score (SQL)
+        """,
+        scorer=offer_scorer.SimilarOfferItemRanker,
+        scorer_order_columns="order",
+        scorer_order_ascending=True,
+        endpoint=SimilarOfferEndpoint(f"similar_offers_cold_start_{ENV_SHORT_NAME}"),
+        retrieval_order_query=None,
+        retrieval_limit=500,
+        ranking_order_query="item_score DESC",
         ranking_limit=20,
     ),
     "random": ModelConfiguration(
@@ -41,72 +87,6 @@ SIMILAR_OFFER_ENDPOINTS = {
         retrieval_order_query="booking_number DESC",
         retrieval_limit=1000,
         ranking_order_query="booking_number DESC",
-        ranking_limit=20,
-    ),
-    # Deprecated
-    "item": ModelConfiguration(
-        name="item",
-        description="""
-        Item model:
-        Takes the top {retrieval_limit} items (SQL)  
-        Takes most similar ones (training based on clicks) (NN)
-        Sort top 500 most similar, by distance range and similarity score (SQL)
-        """,
-        scorer=offer_scorer.ItemRetrievalRanker,
-        scorer_order_columns="order",
-        scorer_order_ascending=True,
-        endpoint=SimilarOfferEndpoint(f"similar_offers_default_{ENV_SHORT_NAME}"),
-        retrieval_order_query="RANDOM() ASC",
-        retrieval_limit=30_000,
-        ranking_order_query="item_score DESC",
-        ranking_limit=20,
-    ),
-    "item_v2": ModelConfiguration(
-        name="item_v2",
-        description="""
-        Item model:
-        Takes most similar ones (training based on clicks) (NN)
-        Sort top 500 most similar, by distance range and similarity score (SQL)
-        """,
-        scorer=offer_scorer.SimilarOfferItemRanker,
-        scorer_order_columns="order",
-        scorer_order_ascending=True,
-        endpoint=SimilarOfferV2Endpoint(f"similar_offers_default_{ENV_SHORT_NAME}"),
-        retrieval_order_query=None,
-        retrieval_limit=500,
-        ranking_order_query="item_score DESC",
-        ranking_limit=20,
-    ),
-    "item_v2_version_b": ModelConfiguration(
-        name="item_v2_version_b",
-        description="""
-        Item model:
-        Takes most similar ones (training based on clicks) (NN)
-        Sort top 500 most similar, by distance range and similarity score (SQL)
-        """,
-        scorer=offer_scorer.SimilarOfferItemRanker,
-        scorer_order_columns="order",
-        scorer_order_ascending=True,
-        endpoint=SimilarOfferV2Endpoint(f"similar_offers_version_b_{ENV_SHORT_NAME}"),
-        retrieval_order_query=None,
-        retrieval_limit=500,
-        ranking_order_query="item_score DESC",
-        ranking_limit=20,
-    ),
-    "cold_start": ModelConfiguration(
-        name="cold_start",
-        description="""
-        Item model:
-        Takes most similar ones
-        Sort top 500 most similar, by distance range and similarity score (SQL)
-        """,
-        scorer=offer_scorer.SimilarOfferItemRanker,
-        scorer_order_columns="order",
-        scorer_order_ascending=True,
-        endpoint=SimilarOfferV2Endpoint(f"similar_offers_version_c_{ENV_SHORT_NAME}"),
-        retrieval_order_query=None,
-        retrieval_limit=500,
-        ranking_order_query="item_score DESC",
         ranking_limit=20,
     ),
 }

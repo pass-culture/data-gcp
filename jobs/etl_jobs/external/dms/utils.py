ce1f20be3538094787a6d34dccc112d619446307
dcuesta-pass <daniel.cuesta@passculture.app>
2023-07-20 16:24:08 +0200

    [Update-DMS-Token]

--- jobs/etl_jobs/external/dms/utils.py
+++ jobs/etl_jobs/external/dms/utils.py
@@ -6,7 +6,7 @@ demarches_jeunes = [47380, 47480]
 demarches_pro = [50362, 55475, 57081, 57189, 61589, 62703, 65028]
 
 
-def access_secret_data(project_id, secret_id, version_id=2, default=None):
+def access_secret_data(project_id, secret_id, version_id="latest", default=None):
     try:
         client = secretmanager.SecretManagerServiceClient()
         name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"

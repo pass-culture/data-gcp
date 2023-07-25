ffcf4113a6e0139d49890e9d9350a4b3c159f66b
LucileRainteau <lucile.rainteau@passculture.app>
2023-07-25 15:44:39 +0200

    Delete model complexity

--- apps/recommendation_v2/api/src/core/model_engine/similar_offer.py
+++ apps/recommendation_v2/api/src/core/model_engine/similar_offer.py
@@ -1,66 +1,69 @@
 from sqlalchemy import text
-import json
 from schemas.user import User
 from schemas.offer import Offer
 from schemas.playlist_params import PlaylistParamsRequest
 from models.past_recommended_offers import PastSimilarOffers
-from core.model_selection.model_configuration import ModelConfiguration
 
-# from pcreco.utils.db.db_connection import get_session
-from core.model_engine import ModelEngine
-from core.model_selection import (
-    select_sim_model_params,
-)
 from sqlalchemy.orm import Session
 
-# from pcreco.utils.env_vars import log_duration
 from typing import List
 import datetime
 import time
 import pytz
 
 
-class SimilarOffer(ModelEngine):
+class SimilarOffer:
     def __init__(self, user: User, offer: Offer, params_in: PlaylistParamsRequest):
         self.offer = offer
-        super().__init__(user=user, params_in=params_in)
-
-    def get_scorer(self):
-        # init input
-        self.model_params.endpoint.init_input(self.offer)
-        return self.model_params.scorer(
-            user=self.user,
-            params_in=self.params_in,
-            model_params=self.model_params,
-            model_endpoint=self.model_params.endpoint,
-        )
-
-    def get_model_configuration(
-        self, user: User, params_in: PlaylistParamsRequest
-    ) -> ModelConfiguration:
-        model_params = select_sim_model_params(params_in.model_endpoint)
-        self.reco_origin = "default"
-        return model_params
+        self.user = user
+        self.params_in = params_in
 
     def get_scoring(self) -> List[str]:
-        if self.offer.item_id is None:
-            return []
-        return super().get_scoring()
+        # if self.offer.item_id is None:
+        #     return []
+        user_recommendation = {
+            # FOR TEST ONLY
+            "isbn-1": {
+                "id": 1,
+                "item_id": "isbn-1",
+                "venue_id": "11",
+                "user_distance": 10,
+                "booking_number": 3,
+                "category": "A",
+                "subcategory_id": "EVENEMENT_CINE",
+                "search_group_name": "CINEMA",
+                "is_geolocated": False,
+                "random": 1,
+            },
+            "movie-3": {
+                "id": 3,
+                "item_id": "movie-3",
+                "venue_id": "33",
+                "user_distance": 20,
+                "booking_number": 10,
+                "category": "C",
+                "subcategory_id": "EVENEMENT_CINE",
+                "search_group_name": "CINEMA",
+                "is_geolocated": False,
+                "random": 3,
+            },
+        }
+        return user_recommendation
 
     def save_recommendation(self, db: Session, recommendations) -> None:
         if len(recommendations) > 0:
             start = time.time()
             date = datetime.datetime.now(pytz.utc)
-            for offer_id in recommendations:
+            for item, offer_infos in recommendations.items():
                 reco_offer = PastSimilarOffers(
                     call_id=self.user.call_id,
-                    user_id=self.user.id,
-                    origin_offer_id=self.offer.id,
-                    offer_id=offer_id,
+                    user_id=self.user.user_id,
+                    origin_offer_id=self.offer.offer_id,
+                    offer_id=offer_infos["id"],
                     date=date,
-                    group_id=self.model_params.name,
-                    model_name=self.scorer.model_endpoint.model_display_name,
-                    model_version=self.scorer.model_endpoint.model_version,
+                    group_id="group_id",
+                    model_name="model_name",
+                    model_version="model_version",
                     reco_filters=self.params_in,
                     venue_iris_id=self.offer.iris_id,
                 )
--- apps/recommendation_v2/api/src/core/scorer/__init__.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class ModelEndpoint(ABC):
-    @abstractmethod
-    def model_score(self, item_input, size):
-        pass

ce024814188b3c2f999fb0e19d0db5b27a0316d4
dcuesta-pass <82374920+dcuesta-pass@users.noreply.github.com>
2023-07-31 11:12:27 +0200

    [Update-emb-extraction-add-dim-reduction-step] (#1694)

---
+++ jobs/ml_jobs/embeddings/tools/dimension_reduction.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import umap
+import json
+from loguru import logger
+
+
+def convert_str_emb_to_float(emb_list, emb_size=124):
+    float_emb = []
+    for str_emb in emb_list:
+        try:
+            emb = json.loads(str_emb)
+        except:
+            emb = [0] * emb_size
+        float_emb.append(np.array(emb))
+    return float_emb
+
+
+def reduce_embedding_dimension(
+    data,
+    emb_size,
+    dimension,
+):
+
+    reduced_emb_df = {}
+    logger.info(f"Initial emb dimension: {emb_size}")
+    float_emb = convert_str_emb_to_float(
+        emb_list=data,
+        emb_size=emb_size,
+    )
+
+    logger.info(f"float_emb: {float_emb[0][:3]} ...")
+    logger.info(f"reduction to {dimension} dimensions...")
+    transformer = umap.UMAP(
+        n_components=dimension,
+        random_state=42,
+        n_neighbors=10,
+        transform_seed=42,
+        verbose=False,
+    ).fit(float_emb)
+    emb_reduced = transformer.embedding_.astype(np.float32)
+    logger.info(f"float_emb: {emb_reduced[0][0][:3]} ...")
+    return emb_reduced.tolist()

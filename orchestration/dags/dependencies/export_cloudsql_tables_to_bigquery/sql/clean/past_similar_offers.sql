90449600b0960d52f4cec6f7b272e8f1e65474db
cdelabre <cyrille.delabre@passculture.app>
2023-08-04 11:11:37 +0200

    (fix) past-similar-offers typo

--- orchestration/dags/dependencies/export_cloudsql_tables_to_bigquery/sql/clean/past_similar_offers.sql
+++ orchestration/dags/dependencies/export_cloudsql_tables_to_bigquery/sql/clean/past_similar_offers.sql
@@ -9,7 +9,7 @@ WITH export_table AS (
         , model_name
         , model_version
         , venue_iris_id
-        , i.centroid as enue_iris_centroid
+        , i.centroid as venue_iris_centroid
         , ROW_NUMBER() OVER (PARTITION BY call_id, date(date), user_id, origin_offer_id ORDER BY pso.id ) as item_rank
     FROM `{{ bigquery_raw_dataset }}.past_similar_offers` pso
     LEFT JOIN `{{ bigquery_anlytics_dataset }}.iris_france` i on i.id = pso.venue_iris_id

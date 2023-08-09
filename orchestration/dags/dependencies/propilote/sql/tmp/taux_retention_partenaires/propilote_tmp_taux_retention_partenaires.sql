c950a45df1131cd8cd030b5f0fa20c0f2a6fa617
Lucille <lucille@macairdelucille.home>
2023-08-09 16:35:28 +0200

    PC-23712 fix mistakes in taux_retention_partenaires

--- orchestration/dags/dependencies/propilote/sql/tmp/taux_retention_partenaires/propilote_tmp_taux_retention_partenaires.sql
+++ orchestration/dags/dependencies/propilote/sql/tmp/taux_retention_partenaires/propilote_tmp_taux_retention_partenaires.sql
@@ -3,43 +3,50 @@ WITH dates AS (
         DISTINCT DATE_TRUNC(deposit_creation_date, MONTH) AS month
     FROM `{{ bigquery_analytics_dataset }}.enriched_deposit_data`
     WHERE deposit_creation_date >= '2023-01-01'
-),
+)
 
 ,active AS (SELECT
     DATE_TRUNC(retention_partner_history.day, MONTH) as month
-    ,"{{ params.group_type }}" as dimension_name
+        ,"{{ params.group_type }}" as dimension_name
         , {% if params.group_type == 'NAT' %}
             'NAT'
         {% else %}
-            rd.{{ params.group_type_name }}
+            {{ params.group_type_name }}
         {% endif %} as dimension_value
     ,COUNT(DISTINCT retention_partner_history.partner_id) AS nb_active_partners
 FROM dates
 LEFT JOIN {{ bigquery_analytics_dataset }}.retention_partner_history ON dates.month >= DATE_TRUNC(retention_partner_history.day, MONTH)
 LEFT JOIN {{ bigquery_analytics_dataset }}.enriched_cultural_partner_data ON retention_partner_history.partner_id = enriched_cultural_partner_data.partner_id
+LEFT JOIN {{ bigquery_analytics_dataset }}.region_department ON enriched_cultural_partner_data.partner_department_code = region_department.num_dep
 WHERE DATE_DIFF(CURRENT_DATE,last_bookable_date,MONTH) < 12
 AND was_registered_last_year IS TRUE
-GROUP BY 1)
+GROUP BY 1,2,3)
 
 ,all_partners AS (SELECT
-    "{{ params.group_type }}" as dimension_name
+    DATE_TRUNC(partner_creation_date, MONTH) as month
+    ,"{{ params.group_type }}" as dimension_name
         , {% if params.group_type == 'NAT' %}
             'NAT'
         {% else %}
-            rd.{{ params.group_type_name }}
+            {{ params.group_type_name }}
         {% endif %} as dimension_value
-    COUNT(partner_id) AS nb_total_partners
-FROM {{ bigquery_analytics_dataset }}.enriched_cultural_partner_data
+    ,COUNT(partner_id) AS nb_total_partners
+FROM dates
+LEFT JOIN {{ bigquery_analytics_dataset }}.enriched_cultural_partner_data ON dates.month >= DATE_TRUNC(partner_creation_date, MONTH)
+LEFT JOIN {{ bigquery_analytics_dataset }}.region_department ON enriched_cultural_partner_data.partner_department_code = region_department.num_dep
 WHERE was_registered_last_year IS TRUE
-AND total_offers_created > 0)
+AND total_offers_created > 0
+GROUP BY 1,2,3)
 
 SELECT
     active.month AS mois
-    ,dimension_name
-    ,dimension_value
+    ,active.dimension_name
+    ,active.dimension_value
     , null as user_type
     , "taux_activation_partenaires" as indicator
     ,nb_total_partners AS denominator
     ,nb_active_partners AS numerator
-FROM active
-CROSS JOIN all_partners
+FROM all_partners
+JOIN active
+    ON active.dimension_name = all_partners.dimension_name
+    AND active.month = all_partners.month

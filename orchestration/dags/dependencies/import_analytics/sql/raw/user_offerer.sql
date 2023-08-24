dfc668effed0a88ab41f044442a4699c7338bf7d
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2023-08-24 16:06:07 +0200

    (import) add date column in user offerer table (#1774)

--- orchestration/dags/dependencies/import_analytics/sql/raw/user_offerer.sql
+++ orchestration/dags/dependencies/import_analytics/sql/raw/user_offerer.sql
@@ -3,4 +3,5 @@ SELECT
     , CAST("userId" AS varchar(255)) as userId
     , CAST("offererId" AS varchar(255)) as offererId
     , CAST("validationStatus" AS varchar(255))  AS user_offerer_validation_status
+	, "dateCreated" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' as user_offerer_creation_date
 FROM public.user_offerer

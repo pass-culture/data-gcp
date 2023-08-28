dd013145fafba278e4f7cba7572b0e1af4bf1dc7
LucileRainteau <lucile.rainteau@passculture.app>
2023-08-28 10:55:36 +0200

    add deepcopy

--- apps/huggy/api/src/core/model_selection/model_configuration.py
+++ apps/huggy/api/src/core/model_selection/model_configuration.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import List
+import copy
 
 import core.scorer.offer as offer_scorer
 from core.endpoint.retrieval_endpoint import RetrievalEndpoint
@@ -76,26 +77,26 @@ class ModelFork:
     def get_user_status(self, user: User):
         """Get model status based on User interactions"""
         if not user.found:
-            return self.cold_start_model, "unknown"
+            return copy.deepcopy(self.cold_start_model), "unknown"
 
         if self.favorites_count is not None:
             if user.favorites_count >= self.favorites_count:
-                return self.warm_start_model, "algo"
+                return copy.deepcopy(self.warm_start_model), "algo"
 
         if self.bookings_count is not None:
             if user.bookings_count >= self.bookings_count:
-                return self.warm_start_model, "algo"
+                return copy.deepcopy(self.warm_start_model), "algo"
 
         if self.clicks_count is not None:
             if user.clicks_count >= self.clicks_count:
-                return self.warm_start_model, "algo"
-        return self.cold_start_model, "cold_start"
+                return copy.deepcopy(self.warm_start_model), "algo"
+        return copy.deepcopy(self.cold_start_model), "cold_start"
 
     def get_offer_status(self, offer: Offer):
         """Get model status based on Offer interactions"""
         if not offer.found:
-            return self.cold_start_model, "unknown"
+            return copy.deepcopy(self.cold_start_model), "unknown"
         if self.bookings_count is not None:
-            if offer.booking_number >= self.bookings_count:
-                return self.warm_start_model, "algo"
-        return self.cold_start_model, "cold_start"
+            if offer.bookings_count >= self.bookings_count:
+                return copy.deepcopy(self.warm_start_model), "algo"
+        return copy.deepcopy(self.cold_start_model), "cold_start"

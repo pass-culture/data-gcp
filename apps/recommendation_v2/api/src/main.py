f40cc16fcd1fadea7ccf0500352ac4872bd11b70
LucileRainteau <114910642+LucileRainteau@users.noreply.github.com>
2023-08-30 14:42:00 +0200

    (Refacto API) Create GET & POST methods for similar_offers route (#1798)

--- apps/recommendation_v2/api/src/main.py
+++ apps/recommendation_v2/api/src/main.py
@@ -3,9 +3,11 @@ from fastapi.logger import logger
 from sqlalchemy.orm import Session
 import uuid
 
-from huggy.schemas.user import UserInput
-from huggy.schemas.offer import OfferInput
-from huggy.schemas.playlist_params import PlaylistParams
+from huggy.schemas.playlist_params import (
+    PostSimilarOfferPlaylistParams,
+    GetSimilarOfferPlaylistParams,
+    RecommendationPlaylistParams,
+)
 
 from huggy.core.model_engine.similar_offer import SimilarOffer
 from huggy.core.model_engine.recommendation import Recommendation
@@ -48,21 +50,20 @@ def check():
     return "OK"
 
 
-@app.post("/similar_offers")
+@app.post("/similar_offers/{offer_id}", dependencies=[Depends(setup_trace)])
 def similar_offers(
-    offer: OfferInput,
-    user: UserInput,
-    playlist_params: PlaylistParams,
+    offer_id: str,
+    playlist_params: PostSimilarOfferPlaylistParams,
+    latitude: float = None,  # venue_latitude
+    longitude: float = None,  # venue_longitude
     db: Session = Depends(get_db),
 ):
 
     call_id = str(uuid.uuid4())
 
-    user = get_user_profile(db, user.user_id, call_id, user.latitude, user.longitude)
+    user = get_user_profile(db, playlist_params.user_id, call_id, latitude, longitude)
 
-    offer = get_offer_characteristics(
-        db, offer.offer_id, offer.latitude, offer.longitude
-    )
+    offer = get_offer_characteristics(db, offer_id, latitude, longitude)
 
     scoring = SimilarOffer(user, offer, playlist_params)
 
@@ -96,14 +97,67 @@ def similar_offers(
     return offer_recommendations
 
 
-@app.post("/playlist_recommendation", dependencies=[Depends(setup_trace)])
-def playlist_recommendation(
-    user: UserInput, playlist_params: PlaylistParams, db: Session = Depends(get_db)
+@app.get("/similar_offers/{offer_id}", dependencies=[Depends(setup_trace)])
+def similar_offers(
+    offer_id: str,
+    playlist_params: GetSimilarOfferPlaylistParams = Depends(),
+    latitude: float = None,  # venue_latitude
+    longitude: float = None,  # venue_longitude
+    db: Session = Depends(get_db),
 ):
 
     call_id = str(uuid.uuid4())
 
-    user = get_user_profile(db, user.user_id, call_id, user.latitude, user.longitude)
+    user = get_user_profile(
+        db, playlist_params.user_id, call_id, latitude, longitude
+    )  # corriger pour avoir la latitude / longitude de l'user (et non de la venue)
+
+    offer = get_offer_characteristics(db, offer_id, latitude, longitude)
+
+    scoring = SimilarOffer(user, offer, playlist_params)
+
+    offer_recommendations = scoring.get_scoring(db)
+
+    log_extra_data = {
+        "user_id": user.user_id,
+        "offer_id": offer.offer_id,
+        "iris_id": user.iris_id,
+        "call_id": call_id,
+        "reco_origin": scoring.reco_origin,
+        # 'filters': playlist_params,
+        "retrieval_model_name": scoring.scorer.retrieval_endpoints[
+            0
+        ].model_display_name,
+        "retrieval_model_version": scoring.scorer.retrieval_endpoints[0].model_version,
+        "retrieval_endpoint_name": scoring.scorer.retrieval_endpoints[0].endpoint_name,
+        "ranking_model_name": scoring.scorer.ranking_endpoint.model_display_name,
+        "ranking_model_version": scoring.scorer.ranking_endpoint.model_version,
+        "ranking_endpoint_name": scoring.scorer.ranking_endpoint.endpoint_name,
+        "recommended_offers": offer_recommendations,
+    }
+
+    custom_logger.info(
+        f"Get similar offer of offer_id {offer.offer_id} for user {user.user_id}",
+        extra=log_extra_data,
+    )
+
+    scoring.save_recommendation(db, offer_recommendations)
+
+    return offer_recommendations
+
+
+@app.post("/playlist_recommendation/{user_id}", dependencies=[Depends(setup_trace)])
+def playlist_recommendation(
+    user_id: str,
+    playlist_params: RecommendationPlaylistParams,
+    latitude: float = None,
+    longitude: float = None,
+    db: Session = Depends(get_db),
+):
+
+    call_id = str(uuid.uuid4())
+
+    user = get_user_profile(db, user_id, call_id, latitude, longitude)
 
     scoring = Recommendation(user, params_in=playlist_params)
 

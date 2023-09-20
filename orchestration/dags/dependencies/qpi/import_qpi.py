d6307cac268ec45047a7793f93a296ef8dbe1244
LucileRainteau <114910642+LucileRainteau@users.noreply.github.com>
2023-09-20 17:59:09 +0200

    (PC-22572) data mettre les differentes versions des tables qpi en environnement clean (#1830)

--- orchestration/dags/dependencies/qpi/import_qpi.py
+++ orchestration/dags/dependencies/qpi/import_qpi.py
@@ -28,43 +28,51 @@ RAW_TABLES = {
 
 
 CLEAN_TABLES = {
-    "qpi_answers_v4": {
-        "sql": f"{SQL_CLEAN_PATH}/qpi_answers_v4.sql",
+    "qpi_answers_historical_clean": {
+        "sql": f"{SQL_CLEAN_PATH}/qpi_answers_historical_clean.sql",
         "write_disposition": "WRITE_APPEND",
         "destination_dataset": "{{ bigquery_clean_dataset }}",
-        "destination_table": "qpi_answers_v4",
+        "destination_table": "qpi_answers_historical_clean",
+        "trigger_rule": "none_failed_or_skipped",
+        "load_table": False,
+    },
+    "qpi_answers_v4_clean": {
+        "sql": f"{SQL_CLEAN_PATH}/qpi_answers_v4_clean.sql",
+        "write_disposition": "WRITE_TRUNCATE",
+        "destination_dataset": "{{ bigquery_clean_dataset }}",
+        "destination_table": "qpi_answers_v4_clean",
+        "time_partitioning": {"field": "submitted_at"},
+        "clustering_fields": {"fields": ["submitted_at"]},
         "trigger_rule": "none_failed_or_skipped",
     },
-    "temp_qpi_answers_v4": {
-        "sql": f"{SQL_CLEAN_PATH}/temp_qpi_answers_v4.sql",
-        "write_disposition": "WRITE_APPEND",
+    "qpi_answers_projection": {
+        "sql": f"{SQL_CLEAN_PATH}/qpi_answers_projection.sql",
+        "write_disposition": "WRITE_TRUNCATE",
         "destination_dataset": "{{ bigquery_clean_dataset }}",
-        "destination_table": "temp_qpi_answers_v4",
+        "destination_table": "qpi_answers_projection",
+        "time_partitioning": {"field": "submitted_at"},
+        "clustering_fields": {"fields": ["submitted_at"]},
         "trigger_rule": "none_failed_or_skipped",
     },
 }
 
 ANALYTICS_TABLES = {
-    "enriched_qpi_answers_v4": {
-        "sql": f"{SQL_ANALYTICS_PATH}/enriched_qpi_answers_v4.sql",
-        "write_disposition": "WRITE_TRUNCATE",
-        "destination_dataset": "{{ bigquery_analytics_dataset }}",
-        "destination_table": "enriched_qpi_answers_v4",
-        "trigger_rule": "none_failed_or_skipped",
-    },
     "enriched_qpi_answers_projection": {
         "sql": f"{SQL_ANALYTICS_PATH}/enriched_qpi_answers_projection.sql",
         "write_disposition": "WRITE_TRUNCATE",
         "destination_dataset": "{{ bigquery_analytics_dataset }}",
         "destination_table": "enriched_qpi_answers_projection",
         "trigger_rule": "none_failed_or_skipped",
+        "time_partitioning": {"field": "submitted_at"},
+        "clustering_fields": {"fields": ["submitted_at"]},
     },
-    "enriched_aggregated_qpi_answers": {
-        "sql": f"{SQL_ANALYTICS_PATH}/enriched_aggregated_qpi_answers.sql",
+    "enriched_qpi_answers": {
+        "sql": f"{SQL_ANALYTICS_PATH}/enriched_qpi_answers.sql",
         "write_disposition": "WRITE_TRUNCATE",
         "destination_dataset": "{{ bigquery_analytics_dataset }}",
-        "destination_table": "enriched_aggregated_qpi_answers",
+        "destination_table": "enriched_qpi_answers",
         "trigger_rule": "none_failed_or_skipped",
-        "depends": ["enriched_qpi_answers_v4"],
+        "time_partitioning": {"field": "submitted_at"},
+        "clustering_fields": {"fields": ["submitted_at"]},
     },
 }

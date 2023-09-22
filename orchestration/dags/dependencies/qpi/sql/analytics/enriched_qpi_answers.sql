b4f67fff027e17e1cf82346924f418e6ed0d9297
cdelabre <cyrille.delabre@passculture.app>
2023-09-22 17:34:27 +0200

    [HOTFIX](qpi) Remove duplicates for QPIs answers

--- orchestration/dags/dependencies/qpi/sql/analytics/enriched_qpi_answers.sql
+++ orchestration/dags/dependencies/qpi/sql/analytics/enriched_qpi_answers.sql
@@ -7,17 +7,23 @@ WITH qpi_v4 as (
 FROM `{{ bigquery_clean_dataset }}.qpi_answers_v4_clean` uqpi
 join `{{ bigquery_analytics_dataset }}.subcategories` subcat
 ON subcat.id=uqpi.subcategories
-)
-SELECT 
-  user_id
+),
+
+all AS (
+  SELECT 
+    user_id
+      , submitted_at
+      , category_id
+      , subcategory_id as subcategories
+  FROM `{{ bigquery_clean_dataset }}.qpi_answers_historical_clean`
+  UNION ALL
+  SELECT 
+    user_id
     , submitted_at
     , category_id
-    , subcategory_id as subcategories
-FROM `{{ bigquery_clean_dataset }}.qpi_answers_historical_clean`
-UNION ALL
-SELECT 
-  user_id
-  , submitted_at
-  , category_id
-  , subcategories
-FROM qpi_v4
+    , subcategories
+  FROM qpi_v4
+)
+
+SELECT * FROM all 
+QUALIFY ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY submitted_at DESC ) = 1

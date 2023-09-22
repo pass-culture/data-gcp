81bacf188918ecf6bdcf06ea03477411fc0e5312
LucileRainteau <114910642+LucileRainteau@users.noreply.github.com>
2023-09-22 17:45:37 +0200

    (PC-24380)(analytics) create a base for enriched_provider_data table
    * (analytics) create a base for enriched_provider_data table
    
    * (PC-24380) Rename enriched_provider in enriched_venue_provider

---
+++ orchestration/dags/dependencies/import_analytics/sql/analytics/enriched_venue_provider_data.sql
@@ -0,0 +1,34 @@
+SELECT 
+  provider.provider_id
+  , provider.provider_name
+  , provider.is_active as is_active
+  , venue_provider.venue_id as venue_id
+  , venue.venue_name
+  , venue_department_code
+  , venue_creation_date
+  , venue_is_permanent
+  , venue_label
+  , last_sync_date
+  , count(offer_id_at_providers) as offer_sync_cnt
+  , min(offer_creation_date) AS first_offer_sync_date
+FROM `{{ bigquery_clean_dataset }}.applicative_database_provider` provider
+JOIN `{{ bigquery_clean_dataset }}.applicative_database_venue_provider` venue_provider
+USING(provider_id)
+LEFT JOIN `{{ bigquery_clean_dataset }}.applicative_database_venue` venue
+USING(venue_id)
+LEFT JOIN `{{ bigquery_clean_dataset }}.applicative_database_venue_label` label
+USING(venue_label_id)
+LEFT JOIN `{{ bigquery_clean_dataset }}.applicative_database_offer` offer
+ON venue.venue_id = offer.venue_id
+AND offer_id_at_providers is not null
+GROUP BY 
+  provider.provider_id
+  , provider.provider_name 
+  , provider.is_active
+  , venue_provider.venue_id
+  , venue.venue_name
+  , venue_department_code
+  , venue_creation_date
+  , venue_is_permanent
+  , venue_label
+  , last_sync_date

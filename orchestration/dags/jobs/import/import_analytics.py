a85944b9b93537cd9420d696eb3c8939c07b00c5
LucileRainteau <lucile.rainteau@passculture.app>
2023-10-03 18:24:47 +0200

    Remove unnecessary links in import analytics

--- orchestration/dags/jobs/import/import_analytics.py
+++ orchestration/dags/jobs/import/import_analytics.py
@@ -248,4 +248,4 @@ end = DummyOperator(task_id="end", dag=dag)
     >> end_historical_analytics_table_tasks
 )
 (end_import >> start_analytics_table_tasks)
-(analytics_table_tasks)
+(analytics_table_tasks >> end_analytics_table_tasks >> end)

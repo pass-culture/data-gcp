59e778d7313791ea4e06b4a89bf3fc7937f48b5f
« MathildeVey » <Mathilde.vey@passculture.app>
2023-10-04 14:14:01 +0200

    correct few mistakes

--- orchestration/dags/dependencies/import_analytics/sql/raw/venue_bank_account_link.sql
+++ orchestration/dags/dependencies/import_analytics/sql/raw/venue_bank_account_link.sql
@@ -1,6 +1,6 @@
 SELECT 
     CAST("id" AS VARCHAR(255)) AS bank_account_link_id
-    ,CAST("Venueid" AS VARCHAR(255)) AS venue_id 
+    ,CAST("VenueId" AS VARCHAR(255)) AS venue_id 
     ,CAST("BankAccountId" AS VARCHAR(255)) AS bank_account_id
     ,CAST(TRIM(BOTH \'[") \' FROM SPLIT_PART("timespan" :: text, \',\',1)) AS timestamp) AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS bank_account_link_beginning_date
     ,CAST(NULLIF(TRIM(BOTH \'[") \' FROM SPLIT_PART("timespan" :: text, \',\',2)),\'\') AS timestamp) AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS bank_account_link_ending_date

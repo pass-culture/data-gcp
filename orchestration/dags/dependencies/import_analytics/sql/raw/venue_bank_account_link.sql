9f0a8000cf81737f84a43f1b4beb4629fab5183d
« MathildeVey » <Mathilde.vey@passculture.app>
2023-10-04 10:49:04 +0200

    add 3 tables : about bank infos and ems cinemas

---
+++ orchestration/dags/dependencies/import_analytics/sql/raw/venue_bank_account_link.sql
@@ -0,0 +1,7 @@
+SELECT 
+    CAST("id" AS VARCHAR(255)) AS bank_account_link_id
+    ,CAST("Venueid" AS VARCHAR(255)) AS venue_id 
+    ,CAST("BankAccountId" AS VARCHAR(255)) AS bank_account_id
+    ,CAST(TRIM(BOTH \'[") \' FROM SPLIT_PART("timespan" :: text, \',\',1)) AS timestamp) AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS bank_account_link_beginning_date
+    ,CAST(NULLIF(TRIM(BOTH \'[") \' FROM SPLIT_PART("timespan" :: text, \',\',2)),\'\') AS timestamp) AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS bank_account_link_ending_date
+FROM public.venue_bank_account_link

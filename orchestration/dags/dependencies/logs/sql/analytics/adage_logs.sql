e27956efca927a364d1a965c7719d069f497a088
valentin <142802218+valoumiaou@users.noreply.github.com>
2023-10-14 09:29:02 +0200

    remove duplicate column name (#1931)

--- orchestration/dags/dependencies/logs/sql/analytics/adage_logs.sql
+++ orchestration/dags/dependencies/logs/sql/analytics/adage_logs.sql
@@ -35,7 +35,6 @@ SELECT
     ARRAY_TO_STRING(ARRAY(SELECT CAST(value AS STRING) FROM UNNEST(jsonPayload.extra.filtervalues.domains) AS value), ',') as artistic_domain_filter,
     ARRAY_TO_STRING(ARRAY(SELECT CAST(value AS STRING) FROM UNNEST(jsonPayload.extra.filtervalues.students) AS value), ',') as student_filter,
     ARRAY_TO_STRING(jsonPayload.extra.filtervalues.categories, ',') as category_filter,
-    jsonPayload.extra.header_link_name as header_link_name,
     jsonPayload.extra.suggestiontype as suggestion_type,
     jsonPayload.extra.suggestionvalue as suggestion_value,
 

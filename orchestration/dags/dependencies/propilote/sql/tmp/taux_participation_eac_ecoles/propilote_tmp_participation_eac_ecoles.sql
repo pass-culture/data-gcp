eb83a170ab725d622dc0fb9220a1843c3b742f9a
Jules-Arbelot <jules.arbelot@passculture.app>
2023-10-25 18:22:37 +0200

    minor changes

--- orchestration/dags/dependencies/propilote/sql/tmp/taux_participation_eac_ecoles/propilote_tmp_participation_eac_ecoles.sql
+++ orchestration/dags/dependencies/propilote/sql/tmp/taux_participation_eac_ecoles/propilote_tmp_participation_eac_ecoles.sql
@@ -1,15 +1,20 @@
-WITH last_day AS (
+WITH last_year_beginning_date as (
+SELECT 
+    educational_year_beginning_date as last_year_start_date
+FROM `{{ bigquery_analytics_dataset }}.applicative_database_educational_year` 
+WHERE educational_year_beginning_date <= DATE_SUB(current_date(), interval 1 year) AND educational_year_expiration_date > DATE_SUB(current_date(), interval 1 year)
+)
+
+, last_day AS (
 SELECT 
     DATE_TRUNC(date,MONTH) AS date,
-    MAX(date) AS last_date 
+    MAX(date) AS last_date,
+    MAX(adage_id) AS last_adage_id
 FROM `{{ bigquery_analytics_dataset }}.adage_involved_student`
 WHERE 
-    -- On prend l'année scolaire en cours 
-    (educational_year_beginning_date <= current_date() AND educational_year_expiration_date > current_date()) 
-    OR 
-    -- Et l'année scolaire précédente pour la comparaison 
-    (educational_year_beginning_date <= DATE_SUB(current_date(), interval 1 year) AND educational_year_expiration_date > DATE_SUB(current_date(), interval 1 year)) 
-GROUP BY 1
+    date <= current_date 
+AND 
+    date > (select last_year_start_date from last_year_beginning_date)GROUP BY 1
 )
 
 SELECT
@@ -26,11 +31,9 @@ SELECT
     , SUM(total_institutions) AS denominator -- total_institutions
 FROM `{{ bigquery_analytics_dataset }}.adage_involved_institution` as involved
 -- take only last day for each month.
-JOIN last_day ON last_day.last_date = involved.date AND DATE_TRUNC(involved.date,MONTH) = last_day.date 
+JOIN last_day ON last_day.last_date = involved.date AND DATE_TRUNC(involved.date,MONTH) = last_day.date AND last_day.last_adage_id = involved.adage_id
 LEFT JOIN `{{ bigquery_analytics_dataset }}.region_department` as rd
     ON involved.department_code = rd.num_dep
--- on selectionne uniquement l'année scolaire qui correspond à la date de calcul
-JOIN `{{ bigquery_analytics_dataset }}.applicative_database_educational_year` as ey ON ey.scholar_year = involved.scholar_year AND last_day.last_date >= ey.educational_year_beginning_date AND last_day.last_date < ey.educational_year_expiration_date
 WHERE
 {% if params.group_type == 'NAT' %}
     department_code = '-1'

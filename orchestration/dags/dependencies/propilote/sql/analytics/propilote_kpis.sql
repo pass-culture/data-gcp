dc2ec19e06216f49fcb6afdadaf6dc7a98b8f0eb
Jules-Arbelot <jules.arbelot@passculture.app>
2023-10-25 15:04:32 +0200

    participation_eac_ecoles from adage_involved_institution + take current and last scholar year for all indicators

--- orchestration/dags/dependencies/propilote/sql/analytics/propilote_kpis.sql
+++ orchestration/dags/dependencies/propilote/sql/analytics/propilote_kpis.sql
@@ -2,6 +2,7 @@
     {% for granularity in  ['region', 'departement', 'academie','all'] %}
         SELECT
             cast(month as date) as month
+            , "TRUE" AS is_current_year
             , cast(dimension_name as STRING) as dimension_name			
             , cast(dimension_value as STRING) as dimension_value
             , cast(user_type as STRING) as user_type
@@ -13,6 +14,23 @@
     {% if not loop.last %}
         UNION ALL 
     {% endif %}
+        SELECT
+            -- Changer ici prendre month + 12 month
+            cast(month as date) as month
+            , "FALSE" AS is_current_year
+            , cast(dimension_name as STRING) as dimension_name			
+            , cast(dimension_value as STRING) as dimension_value
+            , cast(user_type as STRING) as user_type
+            , cast(indicator as STRING) as indicator
+            , cast(numerator as INTEGER) as numerator
+            , cast(denominator as INTEGER) as denominator
+        FROM
+        -- Changer ici prendre yyyy - 1 
+            `{{ bigquery_tmp_dataset }}.{{ yyyymmdd(ds) }}_{{ kpi_details.table_name }}_{{ granularity }}`
+    {% if not loop.last %}
+        UNION ALL 
+    {% endif %}
+    
     {% endfor %}
 
 {% if not loop.last %}

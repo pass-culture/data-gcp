4996239d48f8b68f3ea0dbb64252d97374d2a68c
cdelabre <cyrille.delabre@passculture.app>
2023-10-26 09:57:25 +0200

    update SQL for propilote

--- orchestration/dags/dependencies/propilote/sql/analytics/propilote_kpis.sql
+++ orchestration/dags/dependencies/propilote/sql/analytics/propilote_kpis.sql
@@ -2,7 +2,7 @@
     {% for granularity in  ['region', 'departement', 'academie','all'] %}
         SELECT
             cast(month as date) as month
-            , "TRUE" AS is_current_year
+            , True AS is_current_year
             , cast(dimension_name as STRING) as dimension_name			
             , cast(dimension_value as STRING) as dimension_value
             , cast(user_type as STRING) as user_type
@@ -11,13 +11,13 @@
             , cast(denominator as INTEGER) as denominator
         FROM
             `{{ bigquery_tmp_dataset }}.{{ yyyymmdd(ds) }}_{{ kpi_details.table_name }}_{{ granularity }}`
-    {% if not loop.last %}
+    
         UNION ALL 
-    {% endif %}
+
         SELECT
-            -- Changer ici prendre month + 12 month
-            cast(month as date) as month
-            , "FALSE" AS is_current_year
+            
+            date(DATE_TRUNC(DATE_ADD(cast(month as date), interval 1 year), MONTH)) as month
+            , False AS is_current_year
             , cast(dimension_name as STRING) as dimension_name			
             , cast(dimension_value as STRING) as dimension_value
             , cast(user_type as STRING) as user_type
@@ -25,8 +25,8 @@
             , cast(numerator as INTEGER) as numerator
             , cast(denominator as INTEGER) as denominator
         FROM
-        -- Changer ici prendre yyyy - 1 
             `{{ bigquery_tmp_dataset }}.{{ yyyymmdd(ds) }}_{{ kpi_details.table_name }}_{{ granularity }}`
+        WHERE DATE_TRUNC(DATE_SUB(CURRENT_DATE, interval 1 year), MONTH) = DATE_TRUNC(cast(month as date), MONTH)
     {% if not loop.last %}
         UNION ALL 
     {% endif %}

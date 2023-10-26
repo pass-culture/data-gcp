27d6ca96379068fd4ad9cdd5cf9e68202087cab3
Jules-Arbelot <115459312+Jules-Arbelot@users.noreply.github.com>
2023-10-26 10:40:17 +0200

    Revert "[PC-25254](analytics) Add last year comparison in propilote kpis"

--- orchestration/dags/dependencies/propilote/sql/analytics/pilote_export.sql
+++ orchestration/dags/dependencies/propilote/sql/analytics/pilote_export.sql
@@ -25,14 +25,11 @@ WITH temp1 AS
               WHEN
                    INDICATOR = "taux_participation_eac_ecoles" THEN "IND-046"
           END AS identifiant_indic,
-          sum(numerator) as numerator,
-          sum(denominator) as denominator
+          numerator,
+          denominator
    FROM `{{ bigquery_analytics_dataset }}`.propilote_kpis
    LEFT JOIN `{{ bigquery_analytics_dataset }}`.region_department ON propilote_kpis.dimension_value = region_department.num_dep
-   WHERE dimension_name != 'ACAD' AND is_current_year
-   GROUP BY 1,2,3,4,5,6
-   
-   ),
+   WHERE dimension_name != 'ACAD' ),
      temp2 AS
   (SELECT identifiant_indic,
           CASE

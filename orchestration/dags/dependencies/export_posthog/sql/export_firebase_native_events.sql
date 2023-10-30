b327472b1808314224e032ee8e367c0a41dd4b5e
cdelabre <cyrille.delabre@passculture.app>
2023-10-30 08:40:24 +0100

    [HOTFIX](firebase) 10% for PH POC

--- orchestration/dags/dependencies/export_posthog/sql/export_firebase_native_events.sql
+++ orchestration/dags/dependencies/export_posthog/sql/export_firebase_native_events.sql
@@ -10,5 +10,6 @@ SELECT
     app_info.version as app_version,
     'native' as origin
 FROM `{{ bigquery_clean_dataset }}.firebase_events_{{ yyyymmdd(add_days(ds, params.days)) }}` 
-WHERE NOT REGEXP_CONTAINS(event_name, '^[a-z]+(_[a-z]+)*$')
+WHERE NOT REGEXP_CONTAINS(event_name, '^[a-z]+(_[a-z]+)*$') 
+AND  MOD(ABS(FARM_FINGERPRINT(user_pseudo_id)),10) = 0
 

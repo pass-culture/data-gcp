6aca6c0643f0e63b1b6dd2c973346e027ae232b5
cdelabre <cyrille.delabre@passculture.app>
2023-11-06 12:12:38 +0100

    (reco) feat: add columns name to cloudsql import tables

--- orchestration/dags/dependencies/export_cloudsql_tables_to_bigquery/sql/raw/past_recommended_offers.sql
+++ orchestration/dags/dependencies/export_cloudsql_tables_to_bigquery/sql/raw/past_recommended_offers.sql
@@ -1,1 +1,15 @@
-SELECT * FROM public.past_recommended_offers
+SELECT 
+
+id,
+userid,
+offerid,
+date,
+group_id,
+reco_origin,
+model_name,
+model_version,
+call_id,
+reco_filters,
+user_iris_id
+
+FROM public.past_recommended_offers

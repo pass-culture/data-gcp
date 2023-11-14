f7f3a42e13cdbb9984ded7c5e36d7e60a7678b8b
valoumiaou <valentin.busson@passculture.app>
2023-11-14 15:25:02 +0000

    remove uselsess print

--- jobs/playground_vm/utils/constants.py
+++ jobs/playground_vm/utils/constants.py
@@ -4,4 +4,3 @@ GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "passculture-data-ehp")
 ENV_SHORT_NAME = os.environ.get("ENV_SHORT_NAME", "dev")
 BIGQUERY_ANALYTICS_DATASET =  "analytics_prod" if ENV_SHORT_NAME == "prod" else "analytics_stg" if ENV_SHORT_NAME=="stg" else "analytics_dev"
 
-print(f"{GCP_PROJECT_ID}.{BIGQUERY_ANALYTICS_DATASET}.enriched_item_metadata")

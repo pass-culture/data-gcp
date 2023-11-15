fc3d64fde34571c2fe2f9f24bacd9dae7741ee9a
valoumiaou <valentin.busson@passculture.app>
2023-11-15 17:49:08 +0100

    run source folder

---
+++ orchestration/dags/data_gcp_dbt/models/analytics/downstream_adage_test.sql
@@ -0,0 +1,1 @@
+select * from {{ ref('adage_involved_institution') }}
--- orchestration/dags/jobs/dbt/dbt_run_sources.py
+++ orchestration/dags/jobs/dbt/dbt_run_sources.py
@@ -2,22 +2,14 @@ import datetime
 from airflow import DAG
 from airflow.models import Param
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.docker_operator import DockerOperator
-from airflow.operators.python import BranchPythonOperator, PythonOperator
+from airflow.operators.bash import BashOperator
 from common.alerts import task_fail_slack_alert
-from common.operators.gce import (
-    StartGCEOperator,
-    StopGCEOperator,
-    CloneRepositoryGCEOperator,
-    SSHGCEOperator,
-)
-
 from common.utils import (
     get_airflow_schedule,
 )
 
 from common import macros
-from common.config import ENV_SHORT_NAME, GCP_PROJECT_ID, DAG_FOLDER
+from common.config import GCP_PROJECT_ID, DAG_FOLDER
 
 default_dag_args = {
     "start_date": datetime.datetime(2020, 12, 1),
@@ -26,17 +18,11 @@ default_dag_args = {
     "project_id": GCP_PROJECT_ID,
 }
 
-GCE_INSTANCE = f"dbt-test-{ENV_SHORT_NAME}"
-dag_config = {
-    "GCP_PROJECT": GCP_PROJECT_ID,
-    "ENV_SHORT_NAME": ENV_SHORT_NAME,
-}
-
 
 with DAG(
-    "compile_dbt",
+    "dbt_source_jobs",
     default_args=default_dag_args,
-    description="Compile dbt test",
+    description="dbt test dag",
     on_failure_callback=None,
     schedule_interval=get_airflow_schedule("0 1 * * *"),
     catchup=False,
@@ -44,19 +30,27 @@ with DAG(
     user_defined_macros=macros.default,
     template_searchpath=DAG_FOLDER,
     params={
-        "branch": Param(
-            default="PC-25815-data-setup-du-poc-dbt",
+        "target": Param(
+            default="dev",
             type="string",
-        )
+        ),
+        "folder": Param(
+            default="clean",
+            type="string",
+        ),
+        "children": Param(
+            default="",
+            type="string",
+        ),
     },
+    
 ) as dag:
 
     start = DummyOperator(task_id="start")
-    fetch_code = CloneRepositoryGCEOperator(
-        task_id="fetch_code",
-        instance_name=GCE_INSTANCE,
-        command="{{ params.branch }}",
-        python_version="3.8",
+
+    dbt_run_op = BashOperator(
+        task_id='run_selective_dbt',  
+        bash_command="dbt run --models {{ params.folder }}.*{{ params.children }} --target {{ params.target }}"
     )
 
-start >> fetch_code 
+start >> dbt_run_op

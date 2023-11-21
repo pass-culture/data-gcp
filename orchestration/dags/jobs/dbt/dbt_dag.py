92182eb174aca6904b0100692197b2883e43e6c6
valoumiaou <valentin.busson@passculture.app>
2023-11-21 18:22:11 +0100

    remove empty tests from dbt dag

--- orchestration/dags/jobs/dbt/dbt_dag.py
+++ orchestration/dags/jobs/dbt/dbt_dag.py
@@ -30,61 +30,106 @@ def load_manifest():
 
     return data
 
-def make_dbt_task(node, dbt_verb,dag):
+# def make_dbt_task(node, dbt_verb,dag):
+#     """Returns an Airflow operator either run and test an individual model"""
+#     DBT_DIR = PATH_TO_DBT_PROJECT
+#     GLOBAL_CLI_FLAGS = "--no-write-json"
+#     model = node.split(".")[-1]
+
+#     if dbt_verb == "run":
+#         dbt_task = BashOperator(
+#             task_id=node,
+#             bash_command=f"""
+#             dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --target dev --models {model}
+#             """,
+#             dag=dag,
+#             cwd=PATH_TO_DBT_PROJECT 
+#         )
+
+#     elif dbt_verb == "test":
+#         node_test = node.replace("model", "test")
+#         dbt_task = BashOperator(
+#             task_id=node_test,
+#             bash_command=f"""
+#             dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --target dev --models {model}
+#             """,
+#             dag=dag,
+#             cwd=PATH_TO_DBT_PROJECT 
+#         )
+
+#     return dbt_task
+
+def make_dbt_task(node,dag,dbt_verb="run"):
     """Returns an Airflow operator either run and test an individual model"""
     DBT_DIR = PATH_TO_DBT_PROJECT
     GLOBAL_CLI_FLAGS = "--no-write-json"
-    model = node.split(".")[-1]
+    
+    prefix = node.split(".")[0]
+    model_name = node.split(".")[-1]
+    test_name = node.split(".")[-2]
 
+    node_name = model_name if prefix == "model" else test_name if prefix == "test" else node
+
+    # assert dbt_verb == "run", "dbt_verb other than run not implemented yet"
+    
     if dbt_verb == "run":
         dbt_task = BashOperator(
-            task_id=node,
+            task_id=node_name,
             bash_command=f"""
-            dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --target dev --models {model}
+            dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --target dev --models {node_name}
             """,
-            dag=dag,
-            cwd=PATH_TO_DBT_PROJECT 
+            cwd=PATH_TO_DBT_PROJECT, 
+            dag=dag
         )
-
-    elif dbt_verb == "test":
-        node_test = node.replace("model", "test")
-        dbt_task = BashOperator(
-            task_id=node_test,
-            bash_command=f"""
-            dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --target dev --models {model}
-            """,
-            dag=dag,
-            cwd=PATH_TO_DBT_PROJECT 
-        )
-
+    else:
+        print("dbt_verb other than run not implemented yet")
+        return None
     return dbt_task
+
 
 data = load_manifest()
 
 dbt_tasks = {}
 for node in data["nodes"].keys():
-    print(node)
-    if node.split(".")[0] == "model":
-        node_test = node.replace("model", "test")
-        try :
-            dbt_tasks[node] = make_dbt_task(node, "run",dag)
-        except DuplicateTaskIdFound:
-            pass
-        try :
-            dbt_tasks[node_test] = make_dbt_task(node, "test",dag)
-        except DuplicateTaskIdFound:
-            pass
+    if "elementary" not in node.split("."):
+        if node.split(".")[0] in ["model","test"]:
+            try :
+                dbt_tasks[node] = make_dbt_task(node,dag,dbt_verb="run")
+            except DuplicateTaskIdFound:
+                print(node)
+                # print(dbt_tasks[node])
+                pass
+
+# for node in data["nodes"].keys():
+#     if node.split(".")[0] == "model":
+
+#         # Set dependency to run tests on a model after model runs finishes
+#         # set condition if test not empty in fact test declared in schema are nodes
+#         # node_test = node.replace("model", "test")
+#         dbt_tasks[node] >> dbt_tasks[node_test]
+
+#         # Set all model -> model dependencies
+#         for upstream_node in data["nodes"][node]["depends_on"]["nodes"]:
+
+#             upstream_node_type = upstream_node.split(".")[0]
+#             if upstream_node_type == "model":
+#                 dbt_tasks[upstream_node] >> dbt_tasks[node]
 
 for node in data["nodes"].keys():
-    if node.split(".")[0] == "model":
+    # if node.split('.')[0] == 'test':
+    #     upstream_nodes = tuple(data["nodes"][node]['depends_on']['nodes'])
+    #     for unode in upstream_nodes:
+    #         print( unode,node)
+    #         dbt_tasks[unode] >> dbt_tasks[node]
+    if "elementary" not in node.split("."):
+        
+        if node.split(".")[0] == "model": 
 
-        # Set dependency to run tests on a model after model runs finishes
-        node_test = node.replace("model", "test")
-        dbt_tasks[node] >> dbt_tasks[node_test]
-
-        # Set all model -> model dependencies
-        for upstream_node in data["nodes"][node]["depends_on"]["nodes"]:
-
-            upstream_node_type = upstream_node.split(".")[0]
-            if upstream_node_type == "model":
-                dbt_tasks[upstream_node] >> dbt_tasks[node]
+            # Set all model -> model dependencies
+            for upstream_node in data["nodes"][node]["depends_on"]["nodes"]:
+            
+                upstream_node_type = upstream_node.split(".")[0]
+                if upstream_node_type == "model":
+                    dbt_tasks[upstream_node] >> dbt_tasks[node]
+                # from here build on critical tests dependancies
+                

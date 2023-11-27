fe39be92abd21ce2bac1eb937493c597be8c8b5c
valoumiaou <valentin.busson@passculture.app>
2023-11-27 10:13:04 +0100

    add compile , remove schedule

---
+++ orchestration/dags/jobs/dbt/dbt_compile.py
@@ -0,0 +1,94 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import datetime
+from airflow.utils.dates import timedelta
+from common.config import PATH_TO_DBT_PROJECT
+
+
+default_args = {
+    "depends_on_past": False,
+    "start_date": datetime(2020, 12, 23),
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG(
+    "dbt_dag",
+    default_args=default_args,
+    description="A dbt wrapper for airflow",
+    schedule_interval=timedelta(days=1),
+)
+compile_task = BashOperator(
+            task_id="c",
+            bash_command=f"""
+            dbt compile --target dev
+            """,
+            dag=dag,
+            cwd=PATH_TO_DBT_PROJECT,
+        )
+import datetime
+from airflow import DAG
+from airflow.models import Param
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash import BashOperator
+from common.alerts import task_fail_slack_alert
+from common.utils import (
+    get_airflow_schedule,
+)
+
+from common import macros
+from common.config import GCP_PROJECT_ID, DAG_FOLDER, PATH_TO_DBT_PROJECT
+
+
+# source {PATH_TO_DBT_VENV} &&
+# PATH_TO_DBT_VENV = "3.10.4/envs/dbt-venv"
+# PATH_TO_DBT_PROJECT ="dags/data_gcp_dbt"
+
+
+default_dag_args = {
+    "start_date": datetime.datetime(2020, 12, 1),
+    "on_failure_callback": task_fail_slack_alert,
+    "retries": 1,
+    "project_id": GCP_PROJECT_ID,
+}
+# dag_config = {
+#     "PATH_TO_DBT_VENV": None,
+#     "PATH_TO_DBT_PROJECT": "orchestration/dags/data_gcp_dbt"
+
+# }
+
+with DAG(
+    "dbt_compile",
+    default_args=default_dag_args,
+    description="compile dbt project",
+    on_failure_callback=None,
+    schedule_interval=get_airflow_schedule("0 1 * * *"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=240),
+    user_defined_macros=macros.default,
+    template_searchpath=DAG_FOLDER,
+    params={
+        "target": Param(
+            default="dev",
+            type="string",
+        ),
+        "folder": Param(
+            default="clean",
+            type="string",
+        ),
+        "children": Param(
+            default="",
+            type="string",
+        ),
+    },
+) as dag:
+
+    start = DummyOperator(task_id="start")
+
+    dbt_compile_op = BashOperator(
+        task_id="run_compile_dbt",
+        bash_command="dbt compile --target {{ params.target }}",
+        cwd=PATH_TO_DBT_PROJECT,
+    )
+
+start >> dbt_compile_op
--- orchestration/dags/jobs/dbt/dbt_dag.py
+++ orchestration/dags/jobs/dbt/dbt_dag.py
@@ -20,7 +20,7 @@ dag = DAG(
     "dbt_dag",
     default_args=default_args,
     description="A dbt wrapper for airflow",
-    schedule_interval=timedelta(days=1),
+    schedule_interval=None,
 )
 
 
@@ -28,11 +28,9 @@ def load_manifest():
     local_filepath = PATH_TO_DBT_PROJECT + "/target/manifest.json"
     with open(local_filepath) as f:
         data = json.load(f)
-
     return data
 
-
-def make_dbt_task(node, dbt_verb, dag):
+def make_dbt_task(node, dbt_verb,dag):
     """Returns an Airflow operator either run and test an individual model"""
     DBT_DIR = PATH_TO_DBT_PROJECT
     GLOBAL_CLI_FLAGS = "--no-write-json"
@@ -40,12 +38,12 @@ def make_dbt_task(node, dbt_verb, dag):
 
     if dbt_verb == "run":
         dbt_task = BashOperator(
-            task_id=node,
+            task_id=model if node.split(".")[0]=="model" else node.split(".")[0]+'.'+model,
             bash_command=f"""
             dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --target dev --models {model}
             """,
-            dag=dag,
-            cwd=PATH_TO_DBT_PROJECT,
+            cwd=PATH_TO_DBT_PROJECT, 
+            dag=dag
         )
 
     elif dbt_verb == "test":
@@ -55,8 +53,8 @@ def make_dbt_task(node, dbt_verb, dag):
             bash_command=f"""
             dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --target dev --models {model}
             """,
-            dag=dag,
             cwd=PATH_TO_DBT_PROJECT,
+            dag=dag
         )
 
     return dbt_task
@@ -65,29 +63,35 @@ def make_dbt_task(node, dbt_verb, dag):
 data = load_manifest()
 
 dbt_tasks = {}
+
 for node in data["nodes"].keys():
-    print(node)
-    if node.split(".")[0] == "model":
-        node_test = node.replace("model", "test")
-        try:
-            dbt_tasks[node] = make_dbt_task(node, "run", dag)
-        except DuplicateTaskIdFound:
-            pass
-        try:
-            dbt_tasks[node_test] = make_dbt_task(node, "test", dag)
-        except DuplicateTaskIdFound:
-            pass
+    if "elementary" not in node.split("."):
+        if node.split(".")[0] == "model":
+            node_test = node.replace("model", "test")
+            dbt_tasks[node] = make_dbt_task(node, "run",dag)
+            try :
+                dbt_tasks[node] = make_dbt_task(node, "run",dag)
+            except DuplicateTaskIdFound:
+                print(node)
+                # print(dbt_tasks[node])
+                pass
+            try :
+                dbt_tasks[node_test] = make_dbt_task(node, "test",dag)
+            except DuplicateTaskIdFound:
+                # print(node)
+                # print(dbt_tasks[node])
+                pass
 
 for node in data["nodes"].keys():
     if node.split(".")[0] == "model":
 
         # Set dependency to run tests on a model after model runs finishes
-        node_test = node.replace("model", "test")
-        dbt_tasks[node] >> dbt_tasks[node_test]
+        # node_test = node.replace("model", "test")
+        # dbt_tasks[node] >> dbt_tasks[node_test]
 
         # Set all model -> model dependencies
         for upstream_node in data["nodes"][node]["depends_on"]["nodes"]:
-
-            upstream_node_type = upstream_node.split(".")[0]
-            if upstream_node_type == "model":
-                dbt_tasks[upstream_node] >> dbt_tasks[node]
+            if "elementary" not in upstream_node.split("."):
+                upstream_node_type = upstream_node.split(".")[0]
+                if upstream_node_type == "model":
+                    dbt_tasks[upstream_node] >> dbt_tasks[node]

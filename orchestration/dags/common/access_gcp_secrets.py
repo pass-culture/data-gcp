39a440307c2ef416e8eaaca0e1e77fb96c1742f5
LucileRainteau <114910642+LucileRainteau@users.noreply.github.com>
2023-12-06 09:47:26 +0100

    (fix) fix acces gcp secrets function following composer upgrade (#2058)
    * (fix) fix acces gcp secrets function following composer upgrade
    
    * upgrade airflow requirement for ci

--- orchestration/dags/common/access_gcp_secrets.py
+++ orchestration/dags/common/access_gcp_secrets.py
@@ -7,7 +7,7 @@ def access_secret_data(project_id, secret_id, default=None):
     try:
         client = secretmanager.SecretManagerServiceClient()
         name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
-        response = client.access_secret_version(name)
+        response = client.access_secret_version(request={"name": name})
         return response.payload.data.decode("UTF-8")
     except (DefaultCredentialsError, PermissionDenied, NotFound) as e:
         return default

cb9330cc3cdd5f99f161e84204dc2a3c4896c494
LucileRainteau <114910642+LucileRainteau@users.noreply.github.com>
2023-12-18 16:27:12 +0100

    (PC-26259) create partitioned tables for reco tables (#2075)

--- orchestration/dags/dependencies/export_cloudsql_tables_to_bigquery/sql/raw/past_recommended_offers.sql
+++ orchestration/dags/dependencies/export_cloudsql_tables_to_bigquery/sql/raw/past_recommended_offers.sql
@@ -1,13 +1,5 @@
-SELECT 
-    id,
-    userid,
-    offerid,
-    date,
-    group_id,
-    reco_origin,
-    model_name,
-    model_version,
-    call_id,
-    reco_filters,
-    user_iris_id
-FROM public.past_recommended_offers
+SELECT
+    *,
+    DATE("{{ today() }}") as import_date
+FROM
+    `{{ bigquery_tmp_dataset }}.past_recommended_offers_{{ yyyymmdd(today()) }}`

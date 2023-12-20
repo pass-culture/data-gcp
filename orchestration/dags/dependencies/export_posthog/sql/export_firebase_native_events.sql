d850d9bbbc6b672c405ca687a47033629616d41b
Mathilde Vey <133665016+mathilde-vey@users.noreply.github.com>
2023-12-20 18:45:14 +0100

    [PC-26509] (analytics) Update pro and native posthog scripts with enriched data (#2104)

--- orchestration/dags/dependencies/export_posthog/sql/export_firebase_native_events.sql
+++ orchestration/dags/dependencies/export_posthog/sql/export_firebase_native_events.sql
@@ -1,4 +1,3 @@
-
 SELECT  
     event_date, 
     event_name,
@@ -6,10 +5,57 @@ SELECT
     user_id,
     user_pseudo_id,
     event_params,
+    STRUCT(
+        offer_name,
+        offer_category_id,
+        offer_subcategoryId,
+        venue.venue_name,
+        venue_type_label
+    ) as extra_params,
+    STRUCT(
+        user_current_deposit_type,
+        user_last_deposit_amount,
+        user_first_deposit_type,
+        user_deposit_initial_amount
+    ) as user_params,
     platform,
     app_info.version as app_version,
     'native' as origin
-FROM `{{ bigquery_clean_dataset }}.firebase_events_{{ yyyymmdd(add_days(ds, params.days)) }}` 
-WHERE (NOT REGEXP_CONTAINS(event_name, '^[a-z]+(_[a-z]+)*$') OR event_name = "screen_view")
+FROM `{{ bigquery_clean_dataset }}.firebase_events_{{ yyyymmdd(add_days(ds, params.days)) }}`
+LEFT JOIN `{{ bigquery_analytics_dataset }}.enriched_user_data` USING(user_id)
+LEFT JOIN `{{ bigquery_analytics_dataset }}.enriched_offer_data` offer
+ON offer.offer_id = (
+            select
+                event_params.value.string_value
+            from
+                unnest(event_params) event_params
+            where
+                event_params.key = 'offerId'
+        )
+LEFT JOIN `{{ bigquery_analytics_dataset }}.enriched_venue_data` venue
+ON venue.venue_id = (
+            select
+                event_params.value.string_value
+            from
+                unnest(event_params) event_params
+            where
+                event_params.key = 'venueId'
+        )
+WHERE (
+    (
+        NOT REGEXP_CONTAINS(event_name, '^[a-z]+(_[a-z]+)*$') 
+    )
+    OR
+    (
+        event_name = "screen_view" 
+        AND (
+            select
+                event_params.value.string_value
+            from
+                unnest(event_params) event_params
+            where
+                event_params.key = 'firebase_screen'
+        ) IN ('SignupForm','ProfilSignUp', 'SignupConfirmationEmailSent', 'OnboardingWelcome','OnboardingGeolocation', 'FirstTutorial','BeneficiaryRequestSent','UnderageAccountCreated','BeneficiaryAccountCreated','FirstTutorial2','FirstTutorial3','FirstTutorial4','HasSkippedTutorial' )
+    )
+)
 AND  MOD(ABS(FARM_FINGERPRINT(user_pseudo_id)),10) = 0
-

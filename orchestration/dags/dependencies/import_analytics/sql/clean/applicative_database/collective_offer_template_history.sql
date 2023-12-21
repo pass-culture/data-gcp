47162eb2f57538b4081e521669955846eb9773ea
« MathildeVey » <Mathilde.vey@passculture.app>
2023-12-21 10:56:38 +0100

    add fields in collective_offer_template_history

--- orchestration/dags/dependencies/import_analytics/sql/clean/applicative_database/collective_offer_template_history.sql
+++ orchestration/dags/dependencies/import_analytics/sql/clean/applicative_database/collective_offer_template_history.sql
@@ -1,3 +1,12 @@
+WITH add_domains AS(
+SELECT 
+    collective_offer_template_id as collective_offer_id
+    ,STRING_AGG(educational_domain_name) as educational_domains
+FROM `{{ bigquery_clean_dataset }}`.`applicative_database_collective_offer_template_domain`
+LEFT JOIN `{{ bigquery_clean_dataset }}`.`applicative_database_educational_domain` USING(educational_domain_id)
+GROUP BY 1
+)
+
 SELECT
     collective_offer_audio_disability_compliant,
     collective_offer_mental_disability_compliant,
@@ -20,6 +29,10 @@ SELECT
     collective_offer_contact_phone,
     collective_offer_offer_venue,
     collective_offer_last_validation_type,
+    collective_offer_image_id,
+    educational_domains,
+    collective_offer_venue_address_type,
     DATE_ADD(CURRENT_DATE(), INTERVAL -1 DAY) as partition_date
 FROM
-    `{{ bigquery_clean_dataset }}`.`applicative_database_collective_offer_template`
+    `{{ bigquery_clean_dataset }}`.`applicative_database_collective_offer_template` template
+LEFT JOIN add_domains ON template.collective_offer_id=add_domains.collective_offer_id

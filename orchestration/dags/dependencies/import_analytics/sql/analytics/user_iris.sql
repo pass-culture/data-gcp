9b3233f93737d95c2258398cdf5507f92a1f6c71
cdelabre <cyrille.delabre@passculture.app>
2023-12-27 10:28:35 +0100

    (analytics) fix user_iris table

--- orchestration/dags/dependencies/import_analytics/sql/analytics/user_iris.sql
+++ orchestration/dags/dependencies/import_analytics/sql/analytics/user_iris.sql
@@ -1,7 +1,7 @@
 -- Get the most frequented iris by users over the last 6 months
 
 WITH aggregated_reco as (SELECT
-  userid as user_id
+  user_id
   , call_id
   , date as reco_date
   , user_iris_id
@@ -10,7 +10,7 @@ FROM `{{ bigquery_clean_dataset }}.past_recommended_offers`
 WHERE user_iris_id is not null AND user_iris_id != ""
 AND DATE(date) >= DATE_ADD(current_date(), INTERVAL - 6 MONTH)
 GROUP BY
-  userid
+  user_id
   , call_id
   , date
   , user_iris_id

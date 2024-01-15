451313be7e1e7ff08b6fcbf7393f815af147514e
Jules-Arbelot <jules.arbelot@passculture.app>
2024-01-15 10:12:20 +0100

    correction domain raw tables

--- orchestration/dags/dependencies/import_analytics/sql/clean/collective_offer_domain_name.sql
+++ orchestration/dags/dependencies/import_analytics/sql/clean/collective_offer_domain_name.sql
@@ -2,5 +2,5 @@ SELECT
     cod.collective_offer_id,
     cod.educational_domain_id,
     ed.educational_domain_name
-FROM `{{ bigquery_clean_dataset }}`.`collective_offer_domain` cod
-LEFT JOIN `{{ bigquery_clean_dataset }}`.`educational_domain` ed ON cod.educational_domain_id = ed.educational_domain_id
+FROM `{{ bigquery_raw_dataset }}`.`applicative_database_collective_offer_domain` cod
+LEFT JOIN `{{ bigquery_raw_dataset }}`.`applicative_database_educational_domain` ed ON cod.educational_domain_id = ed.educational_domain_id

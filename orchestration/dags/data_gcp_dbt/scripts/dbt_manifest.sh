e9102859c0ae0427cfe4e99e22b6b01f5a403e29
LucileRainteau <lucile.rainteau@passculture.app>
2024-01-24 14:23:59 +0100

    (dbt) add env_short_name vars flag in cli commands

--- orchestration/dags/data_gcp_dbt/scripts/dbt_manifest.sh
+++ orchestration/dags/data_gcp_dbt/scripts/dbt_manifest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dbt ls --target $target --target-path $PATH_TO_DBT_TARGET --exclude fqn:* source:* exposure:* --select data_gcp_dbt --output json
+dbt ls --target $target --vars "{ENV_SHORT_NAME: $ENV_SHORT_NAME}" --target-path $PATH_TO_DBT_TARGET --exclude fqn:* source:* exposure:* --select data_gcp_dbt --output json

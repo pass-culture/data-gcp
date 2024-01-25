ef5ce1bd85935634ddd17572fce29c85e407b356
valoumiaou <valentin.busson@passculture.app>
2024-01-25 17:02:50 +0100

    update macro and set config to save migrated models in separate schema

--- orchestration/dags/data_gcp_dbt/macros/generate_schema_name.sql
+++ orchestration/dags/data_gcp_dbt/macros/generate_schema_name.sql
@@ -1,7 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none or target.name == "dev" -%}
+    {%- if custom_schema_name is none and target.name == "dev" -%}
 
         {{ default_schema }}
 

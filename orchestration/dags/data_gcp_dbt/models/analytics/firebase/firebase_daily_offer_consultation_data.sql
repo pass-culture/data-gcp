eae27b3afdf7af206d970e0bee8b1ad884951bbe
MatthieuRipollPassCulture <matthieu.ripoll+1@passculture.app>
2024-02-08 18:30:43 +0100

    wip fix

--- orchestration/dags/data_gcp_dbt/models/analytics/firebase/firebase_daily_offer_consultation_data.sql
+++ orchestration/dags/data_gcp_dbt/models/analytics/firebase/firebase_daily_offer_consultation_data.sql
@@ -1,3 +1,11 @@
+{{
+    config(
+        materialized = 'incremental',
+        incremental_strategy = 'insert_overwrite',
+        partition_by = {'field': 'event_date', 'data_type': 'date'},
+    )
+}}
+
 SELECT
   event_date,
   e.offer_id,
@@ -5,18 +13,18 @@ SELECT
   origin,
   COUNT(*) AS nb_daily_consult
 FROM
-  {{ SOURCE('analytics',
+  {{ source('analytics',
     'firebase_events') }} e
 LEFT JOIN
   {{ref('offer_item_ids')}} offer_item_ids
 ON
   offer_item_ids.offer_id = e.offer_id
 WHERE
-  event_name = 'ConsultOffer' {%
-IF
-  is_incremental() %} -- recalculate latest day's DATA + previous
+  event_name = 'ConsultOffer'
+   {% if is_incremental() %} -- recalculate latest day's DATA + previous
 WHERE
-  DATE(event_date) >= DATE_SUB(DATE(_dbt_max_partition), INTERVAL 1 day) {% endif %}
+  DATE(event_date) >= DATE_SUB(DATE(_dbt_max_partition), INTERVAL 1 day)
+{% endif %}
 GROUP BY
   1,
   2,

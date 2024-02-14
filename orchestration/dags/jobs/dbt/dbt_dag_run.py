2e64fceb107333b000fc52b8c429ff8eb318a51f
valoumiaou <valentin.busson@passculture.app>
2024-02-14 17:03:17 +0100

    typo

--- orchestration/dags/jobs/dbt/dbt_dag_run.py
+++ orchestration/dags/jobs/dbt/dbt_dag_run.py
@@ -191,7 +191,7 @@ with TaskGroup(group_id="data_transformation", dag=dag) as data_transfo:
                 env={
                      "GLOBAL_CLI_FLAGS": "{{ params.GLOBAL_CLI_FLAGS }}",
                      "target": "{{ params.target }}",
-                     "model": f"{model_data['model_alias']}",
+                     "model": f"{model_data['alias']}",
                      "full_ref_str": full_ref_str,
                      "PATH_TO_DBT_TARGET": PATH_TO_DBT_TARGET,
                      "EXCLUSION": " --exclude "

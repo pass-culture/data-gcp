b2b74fc46be24c8af9a9092b9207d29527bacbc6
valoumiaou <valentin.busson@passculture.app>
2024-02-14 17:05:02 +0100

    linter

--- orchestration/dags/jobs/dbt/dbt_dag_run.py
+++ orchestration/dags/jobs/dbt/dbt_dag_run.py
@@ -179,7 +179,7 @@ with TaskGroup(group_id="data_transformation", dag=dag) as data_transfo:
                 # create dependencies between tests and their attached model
                 if model_node in models_with_crit_test_dependencies:
                     model_op_dict[model_node] >> test_op_dict[model_node]
-                    
+
     for model_node in dbt_models:
         if "applicative" not in manifest["nodes"][model_node]["alias"]:
             full_ref_str = " --full-refresh" if not "{{ params.full_refresh }}" else ""
@@ -189,17 +189,15 @@ with TaskGroup(group_id="data_transformation", dag=dag) as data_transfo:
                 task_id=model_data["alias"],
                 bash_command=f"bash {PATH_TO_DBT_PROJECT}/scripts/dbt_run.sh ",
                 env={
-                     "GLOBAL_CLI_FLAGS": "{{ params.GLOBAL_CLI_FLAGS }}",
-                     "target": "{{ params.target }}",
-                     "model": f"{model_data['alias']}",
-                     "full_ref_str": full_ref_str,
-                     "PATH_TO_DBT_TARGET": PATH_TO_DBT_TARGET,
-                     "EXCLUSION": " --exclude "
-                     + " ".join(
-                         [f"tag:{item}" for item in EXCLUDED_TAGS]
-                     )
-                     if len(EXCLUDED_TAGS) > 0
-                     else "",
+                    "GLOBAL_CLI_FLAGS": "{{ params.GLOBAL_CLI_FLAGS }}",
+                    "target": "{{ params.target }}",
+                    "model": f"{model_data['alias']}",
+                    "full_ref_str": full_ref_str,
+                    "PATH_TO_DBT_TARGET": PATH_TO_DBT_TARGET,
+                    "EXCLUSION": " --exclude "
+                    + " ".join([f"tag:{item}" for item in EXCLUDED_TAGS])
+                    if len(EXCLUDED_TAGS) > 0
+                    else "",
                 },
                 append_env=True,
                 cwd=PATH_TO_DBT_PROJECT,
@@ -236,4 +234,3 @@ for test, parents in crit_test_parents.items():
             pass
 
 start >> branching >> [shunt, wait4init] >> join >> data_transfo
-

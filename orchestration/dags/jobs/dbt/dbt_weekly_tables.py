69a0f8391a876e9ba4020f0e3fe974e23c9e4d36
valoumiaou <valentin.busson@passculture.app>
2024-02-26 10:03:57 +0100

    change weekly and monthly schedule time to match with dbt run dag

--- orchestration/dags/jobs/dbt/dbt_weekly_tables.py
+++ orchestration/dags/jobs/dbt/dbt_weekly_tables.py
@@ -34,7 +34,7 @@ dag = DAG(
     dagrun_timeout=timedelta(minutes=60),
     catchup=False,
     description="run weekly aggregated models",
-    schedule_interval=get_airflow_schedule("0 3 * * 1"),  # run every monday at 3 AM
+    schedule_interval=get_airflow_schedule("0 1 * * 1"),  # run every monday at 1 AM
     params={
         "target": Param(
             default=ENV_SHORT_NAME,

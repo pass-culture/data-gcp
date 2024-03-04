dc801e1713e446ea5b14a5e994f4925bbf5fceb3
LucileRainteau <lucile.rainteau@passculture.app>
2024-03-04 12:22:40 +0100

    (discovery) add origin discovery

--- orchestration/dags/data_gcp_dbt/models/clean/user_item_consultation.sql
+++ orchestration/dags/data_gcp_dbt/models/clean/user_item_consultation.sql
@@ -10,11 +10,12 @@ SELECT
     user.user_id
     , event_date as consultation_date
     , array_agg(distinct offer_item_ids.item_id) as consulted_items
+    , array_agg(distinct firebase.origin) as consulted_origins
 FROM {{ ref('user_beneficiary') }} user
 JOIN {{ source('analytics', 'firebase_events') }} firebase
     ON user.user_id = firebase.user_id
     and event_name = 'ConsultOffer'
-    and event_date = date_sub('{{ ds() }}', INTERVAL 1 day)
+    and event_date >= date_sub('{{ ds() }}', INTERVAL 3 day)
 JOIN {{ ref('offer_item_ids') }} offer_item_ids -- retire les offres non reliées à un item_id 
     ON firebase.offer_id = offer_item_ids.offer_id
 GROUP BY 1, 2
--- orchestration/dags/data_gcp_dbt/models/clean/user_item_discovery.sql
+++ orchestration/dags/data_gcp_dbt/models/clean/user_item_discovery.sql
@@ -1,9 +1,10 @@
-{% set discovery_features = ['category_id', 'subcategory_id', 'new_consulted_item_id'] %}
+{% set discovery_features = ['category_id', 'subcategory_id', 'new_items'] %}
 
 WITH consultation as (
     SELECT 
         *
         , lag(consulted_items) over(partition by user_id order by consultation_date) as last_consulted_items
+        , lag(consulted_origins) over(partition by user_id order by consultation_date) as last_consulted_origins
     FROM {{ ref('user_item_consultation') }}
 ),
 new_consultation as (
@@ -15,22 +16,21 @@ new_consultation as (
             EXCEPT DISTINCT
             (SELECT * FROM consultation.last_consulted_items)
         ) AS new_consulted_items
+        , ARRAY(
+            SELECT * FROM consultation.consulted_origins
+            EXCEPT DISTINCT
+            (SELECT * FROM consultation.last_consulted_origins)
+        ) AS new_consulted_origins
     FROM consultation
 ),
-new_consultation_metadata as (
+new_consultation_item as (
     SELECT
         user_id
         , consultation_date
-        , new_items as new_consulted_item_id
+        , new_items
         , category_id
         , subcategory_id
-    FROM new_consultation, unnest(new_consulted_items) new_items
-    LEFT JOIN {{ ref('item_metadata') }} metadata
-    on new_items = metadata.item_id
-)
-SELECT 
-    *
-    , {% for feature in discovery_features %} 
+        , {% for feature in discovery_features %} 
         CASE
             WHEN consultation_date = min(consultation_date) over(partition by user_id, {{feature}})
             THEN 1
@@ -38,4 +38,45 @@ SELECT
         END as {{feature}}_discovery
       {% if not loop.last -%} , {%- endif %}
       {% endfor %}
-FROM new_consultation_metadata
+    FROM new_consultation, unnest(new_consulted_items) new_items
+    LEFT JOIN {{ ref('item_metadata') }} metadata
+    on new_items = metadata.item_id
+),
+new_consultation_origin as (
+    SELECT
+        user_id
+        , consultation_date
+        , new_origins as new_consulted_origin
+        , CASE
+            WHEN consultation_date = min(consultation_date) over(partition by user_id, new_origins)
+            THEN 1
+            ELSE 0
+        END as new_origins_discovery
+    FROM new_consultation, unnest(new_consulted_origins) as new_origins
+),
+
+agg_new_consultation_origin as (
+    SELECT 
+        user_id
+        , consultation_date
+        , sum(new_origins_discovery) AS discovery_origin
+    FROM new_consultation_origin
+    GROUP BY 1,2
+),
+
+agg_new_consultation_item as (
+        SELECT
+        user_id
+        , consultation_date  
+        , {% for feature in discovery_features %} 
+        sum({{feature}}_discovery) as discovery_{{feature}}
+      {% if not loop.last -%} , {%- endif %}
+      {% endfor %}
+    FROM new_consultation_item
+    GROUP BY 1, 2
+)
+
+SELECT *
+FROM agg_new_consultation_item
+FULL OUTER JOIN agg_new_consultation_origin
+USING (user_id, consultation_date)

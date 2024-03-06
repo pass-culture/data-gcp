280ea54a97490b3d86fceb762cff620c6fde8344
LucileRainteau <114910642+LucileRainteau@users.noreply.github.com>
2024-03-06 10:59:14 +0100

    Update orchestration/dags/data_gcp_dbt/models/analytics/enriched/enriched_collective_booking_data.sql
    Co-authored-by: carolinemestre-passculture <162314477+carolinemestre-passculture@users.noreply.github.com>

--- orchestration/dags/data_gcp_dbt/models/analytics/enriched/enriched_collective_booking_data.sql
+++ orchestration/dags/data_gcp_dbt/models/analytics/enriched/enriched_collective_booking_data.sql
@@ -62,4 +62,4 @@ FROM
     LEFT JOIN {{ source('analytics', 'region_department') }} AS venue_region_departement ON venue.venue_department_code = venue_region_departement.num_dep
     LEFT JOIN {{ source('analytics', 'region_department') }} AS school_region_departement ON eple.code_departement = school_region_departement.num_dep
     LEFT JOIN collective_booking_ranking_view ON collective_booking_ranking_view.collective_booking_id = collective_booking.collective_booking_id
-    LEFT JOIN {{ source('clean', 'subcategories') }} subcategories ON collective_offer.collective_offer_subcategory_id = subcategories.id;
+    LEFT JOIN {{ source('clean', 'subcategories') }} subcategories ON collective_offer.collective_offer_subcategory_id = subcategories.id

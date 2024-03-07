4816f208ca274fb552ea91f1735954af008c4b72
LucileRainteau <lucile.rainteau@passculture.app>
2024-03-07 17:35:51 +0100

    (reimbursement) fix join

--- orchestration/dags/data_gcp_dbt/models/clean/reimbursement_collective.sql
+++ orchestration/dags/data_gcp_dbt/models/clean/reimbursement_collective.sql
@@ -11,7 +11,8 @@ SELECT
     , invoice.invoice_id 
 FROM {{ source('raw', 'applicative_database_collective_booking') }} as collective_booking 
 JOIN {{ source('raw', 'applicative_database_collective_stock') }} as collective_stock
-    ON collective_stock.collective_stock_id = collective_booking.collective_stock_id 
+    ON collective_stock.collective_stock_id = collective_booking.collective_stock_id
+    AND collective_booking_status = 'REIMBURSED' 
 JOIN {{ source('raw', 'applicative_database_educational_institution') }} as institution
     ON  collective_booking.educational_institution_id = institution.educational_institution_id 
 JOIN {{ ref('venue') }} as venue
@@ -27,7 +28,7 @@ JOIN {{ source('raw', 'applicative_database_cashflow_pricing') }} cashflow_prici
 JOIN {{ source('raw', 'applicative_database_cashflow') }} cashflow 
     ON  cashflow_pricing.cashflowId = cashflow.id 
 JOIN {{ source('raw', 'applicative_database_cashflow_batch') }} cashflow_batch 
-    ON  cashflow.id = cashflow_batch.id 
+    ON  cashflow.batchId = cashflow_batch.id 
 JOIN {{ source('raw', 'applicative_database_invoice_cashflow') }} invoice_cashflow 
     ON  cashflow.id = invoice_cashflow.cashflow_id 
 JOIN {{ source('raw', 'applicative_database_invoice') }} invoice 

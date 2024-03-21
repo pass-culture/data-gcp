b78b9490722285aba386c2fc0aff5e32283f2aa4
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-03-21 17:13:43 +0100

    feat: update names + add incremental

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__collective_booking.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__collective_booking.sql
@@ -1,23 +1,23 @@
 SELECT
-    cb.collective_booking_id,
-    cb.collective_booking_creation_date,
-    cb.collective_booking_used_date,
+    collective_booking_id,
+    collective_booking_creation_date,
+    collective_booking_used_date,
     collective_stock_id,
-    cb.venue_id,
-    cb.offerer_id,
-    cb.collective_booking_cancellation_date,
-    cb.collective_booking_cancellation_limit_date,
-    cb.collective_booking_cancellation_reason,
-    cb.collective_booking_status,
-    cb.collective_booking_reimbursement_date,
-    cb.educational_institution_id,
-    cb.educational_year_id,
-    cb.collective_booking_confirmation_date,
-    cb.collective_booking_confirmation_limit_date,
-    cb.educational_redactor_id,
+    venue_id,
+    offerer_id,
+    collective_booking_cancellation_date,
+    collective_booking_cancellation_limit_date,
+    collective_booking_cancellation_reason,
+    collective_booking_status,
+    collective_booking_reimbursement_date,
+    educational_institution_id,
+    educational_year_id,
+    collective_booking_confirmation_date,
+    collective_booking_confirmation_limit_date,
+    educational_redactor_id,
     rank() OVER (
-            PARTITION BY cb.educational_institution_id
+            PARTITION BY educational_institution_id
             ORDER BY
-                cb.collective_booking_creation_date
+                collective_booking_creation_date
         ) AS collective_booking_rank
-FROM {{ source('raw', 'applicative_database_collective_booking') }} AS cb
+FROM {{ source('raw', 'applicative_database_collective_booking') }}

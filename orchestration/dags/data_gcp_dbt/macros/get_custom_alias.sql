26dca2a3bbe52685151d788a0e648fa62db46908
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-03-26 11:33:31 +0100

    feat: update macro for custom alias & generate schema

--- orchestration/dags/data_gcp_dbt/macros/get_custom_alias.sql
+++ orchestration/dags/data_gcp_dbt/macros/get_custom_alias.sql
@@ -4,6 +4,17 @@
 
         {{ custom_alias_name ~ node.name }}
 
+    {%- elif 'intermediate' in node.path -%}
+
+        {%- set model_name = node.name.split('__') -%}
+        {{ model_name[-1] | trim }}
+
+    {%- elif 'mart' in node.path -%}
+        {%- set model_name = node.name.split('__') -%}
+        {%- set model_name_parts = model_name[0].split('mrt_') -%}
+        {%- set model_name = model_name_parts[-1] | trim ~ "_" ~ model_name[-1] | trim -%}
+            {{ model_name }}
+
     {%- elif node.version -%}
 
         {{ return(node.name ~ "_v" ~ (node.version | replace(".", "_"))) }}

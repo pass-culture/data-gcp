684d8da93c0dbc04175aaf83d8cc1ec460467188
valentin <142802218+valoumiaou@users.noreply.github.com>
2024-03-28 17:08:38 +0100

    remove double run history tables (#2349)

--- orchestration/dags/data_gcp_dbt/models/clean/applicative_history/offerer_tag_category_mapping_history.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    offerer_tag_category_mapping_id,
-    offerer_tag_id,
-    offerer_tag_category_id,
-    DATE_ADD(CURRENT_DATE(), INTERVAL -1 DAY) as partition_date
-FROM
-    {{ source('raw', 'applicative_database_offerer_tag_category_mapping') }}
--- orchestration/dags/data_gcp_dbt/models/clean/applicative_history/offerer_tag_mapping_history.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    offerer_tag_mapping_id,
-    offerer_id,
-    tag_id,
-    DATE_ADD(CURRENT_DATE(), INTERVAL -1 DAY) as partition_date
-FROM
-    {{ source('raw', 'applicative_database_offerer_tag_mapping') }}

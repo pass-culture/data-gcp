805dd43dcd815c24df7896822450f62042b2c72a
carolinemestre-passculture <162314477+carolinemestre-passculture@users.noreply.github.com>
2024-03-29 11:24:57 +0100

    Update booking table name (#2354)
    * fix: update table name in booking history
    
    * fix: update table name in training_data_boookings

--- orchestration/dags/dependencies/training_data/sql/training_data_bookings.sql
+++ orchestration/dags/dependencies/training_data/sql/training_data_bookings.sql
@@ -15,7 +15,7 @@ SELECT
     enroffer.venue_id,
     enroffer.venue_name,
 from
-    `{{ bigquery_clean_dataset }}`.`booking` booking
+    `{{ bigquery_clean_dataset }}`.`applicative_database_booking` booking
     inner join `{{ bigquery_clean_dataset }}`.`applicative_database_stock` stock on booking.stock_id = stock.stock_id
     inner join `{{ bigquery_clean_dataset }}`.`applicative_database_offer` offer on stock.offer_id = offer.offer_id
     inner join `{{ bigquery_analytics_dataset }}`.`subcategories` subcategories on offer.offer_subcategoryId = subcategories.id

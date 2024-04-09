6a1bc1820dae261c840538dc9c6e0b2f63284490
valentin <142802218+valoumiaou@users.noreply.github.com>
2024-04-09 16:24:01 +0200

    fix: typo & aggregation logic of gtls in recommendable offers (#2385)
    * fix: typo & aggregation logic of gtls in recommendable offers

--- orchestration/dags/dependencies/import_analytics/sql/analytics/recommendable_offers_data.sql
+++ orchestration/dags/dependencies/import_analytics/sql/analytics/recommendable_offers_data.sql
@@ -43,10 +43,10 @@ get_recommendable_offers AS (
         offer.last_stock_price AS stock_price,
         offer.titelive_gtl_id AS gtl_id,
         gtl_mapping.gtl_type,
-        glt_mapping.gtl_label_level_1 as gtl_l1,
-        glt_mapping.gtl_label_level_2 as gtl_l2,
-        glt_mapping.gtl_label_level_3 as gtl_l3,
-        glt_mapping.gtl_label_level_4 as gtl_l4,
+        gtl_mapping.gtl_label_level_1 as gtl_l1,
+        gtl_mapping.gtl_label_level_2 as gtl_l2,
+        gtl_mapping.gtl_label_level_3 as gtl_l3,
+        gtl_mapping.gtl_label_level_4 as gtl_l4,
         MAX(item_counts.item_count) as item_count,
         MAX(COALESCE(booking_numbers.booking_number, 0)) AS booking_number,
         MAX(COALESCE(booking_numbers.booking_number_last_7_days, 0)) AS booking_number_last_7_days,
@@ -119,14 +119,14 @@ get_recommendable_offers AS (
         LEFT JOIN `{{ bigquery_raw_dataset }}`.sensitive_item_recommendation sensitive_offer on 
             offer.item_id = sensitive_offer.item_id
         LEFT JOIN `{{ bigquery_clean_dataset }}`.applicative_database_titelive_gtl gtl_mapping on 
-            offer.titelive_gtl_id = glt_mapping.gtl_id and gtl_mapping.gtl_type = enriched_item_metadata.offer_type_domain
+            offer.titelive_gtl_id = gtl_mapping.gtl_id and gtl_mapping.gtl_type = enriched_item_metadata.offer_type_domain
 
     WHERE
         offer.is_active = TRUE
         AND offer.offer_is_bookable = TRUE
         AND offerer.offerer_is_active = TRUE
         AND offer.offer_validation = 'APPROVED'
-    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19
 )
 SELECT  * 
 FROM get_recommendable_offers 

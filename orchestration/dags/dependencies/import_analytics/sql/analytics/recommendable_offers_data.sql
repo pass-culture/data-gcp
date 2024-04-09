af47f4d99526d45b12ba56f834d03a0519c28ea3
valoumiaou <valentin.busson@passculture.app>
2024-04-09 16:14:18 +0200

    fix: aggregation clause

--- orchestration/dags/dependencies/import_analytics/sql/analytics/recommendable_offers_data.sql
+++ orchestration/dags/dependencies/import_analytics/sql/analytics/recommendable_offers_data.sql
@@ -42,11 +42,11 @@ get_recommendable_offers AS (
         stock.stock_beginning_date AS stock_beginning_date,
         offer.last_stock_price AS stock_price,
         offer.titelive_gtl_id AS gtl_id,
-        ANY_VALUE(gtl_mapping.gtl_type) as gtl_type,
-        ANY_VALUE(gtl_mapping.gtl_label_level_1) as gtl_l1,
-        ANY_VALUE(gtl_mapping.gtl_label_level_2) as gtl_l2,
-        ANY_VALUE(gtl_mapping.gtl_label_level_3) as gtl_l3,
-        ANY_VALUE(gtl_mapping.gtl_label_level_4) as gtl_l4,
+        gtl_mapping.gtl_type,
+        gtl_mapping.gtl_label_level_1 as gtl_l1,
+        gtl_mapping.gtl_label_level_2 as gtl_l2,
+        gtl_mapping.gtl_label_level_3 as gtl_l3,
+        gtl_mapping.gtl_label_level_4 as gtl_l4,
         MAX(item_counts.item_count) as item_count,
         MAX(COALESCE(booking_numbers.booking_number, 0)) AS booking_number,
         MAX(COALESCE(booking_numbers.booking_number_last_7_days, 0)) AS booking_number_last_7_days,
@@ -126,7 +126,7 @@ get_recommendable_offers AS (
         AND offer.offer_is_bookable = TRUE
         AND offerer.offerer_is_active = TRUE
         AND offer.offer_validation = 'APPROVED'
-    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19
 )
 SELECT  * 
 FROM get_recommendable_offers 

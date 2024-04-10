061599d798b511b31802dba9146b5e7e1b6a4c15
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-04-10 14:51:04 +0200

    fix: override marketing attribution channel

---
+++ orchestration/dags/data_gcp_dbt/models/intermediate/firebase/native/int_firebase__native_attribution.sql
@@ -0,0 +1,38 @@
+WITH campaigns AS (
+  SELECT
+    user_pseudo_id,
+    traffic_campaign,
+    traffic_medium,
+    traffic_source,
+    MIN(event_timestamp) AS campaign_begins_at,
+    TIMESTAMP_ADD(MIN(event_timestamp), INTERVAL 1 DAY) AS original_campaign_ends_at
+FROM {{ ref("int_firebase__native_event_flattened") }}
+  WHERE
+    traffic_campaign IS NOT NULL AND traffic_source IS NOT NULL
+    AND event_date BETWEEN '2023-12-20' AND '2024-04-04'
+  GROUP BY
+    user_pseudo_id,
+    traffic_campaign,
+    traffic_medium,
+    traffic_source
+),
+campaigns_with_next_begin AS (
+  SELECT
+    *,
+    LEAD(campaign_begins_at) OVER (PARTITION BY user_pseudo_id ORDER BY campaign_begins_at) AS next_campaign_begins_at
+  FROM
+    campaigns
+)
+SELECT
+  user_pseudo_id,
+  traffic_campaign,
+  traffic_medium,
+  traffic_source,
+  campaign_begins_at,
+  IF(
+    next_campaign_begins_at IS NOT NULL AND next_campaign_begins_at < original_campaign_ends_at,
+    next_campaign_begins_at,
+    original_campaign_ends_at
+  ) AS campaign_ends_at
+FROM
+  campaigns_with_next_begin

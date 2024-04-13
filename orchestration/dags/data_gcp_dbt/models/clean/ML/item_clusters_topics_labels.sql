a462388b6c981337d01fc003cb4dc99ec52ba3ba
valentin <142802218+valoumiaou@users.noreply.github.com>
2024-04-13 12:07:16 +0200

    fix: various dags failures (#2403)

--- orchestration/dags/data_gcp_dbt/models/clean/ML/item_clusters_topics_labels.sql
+++ orchestration/dags/data_gcp_dbt/models/clean/ML/item_clusters_topics_labels.sql
@@ -1,6 +1,9 @@
 select 
     cluster.*,
-    topic.* except(semantic_cluster_id,semantic_category)
+    topic_labels.* except(semantic_cluster_id,semantic_category),
 from {{source('clean','item_clusters')}} as cluster
-left join {{ source('clean','item_topics_labels') }} as topic on cluster.semantic_cluster_id = topic.semantic_cluster_id
+left join {{ source('clean','item_topics') }} as topic 
+on cluster.item_id = topic.item_id 
+left join {{ source('clean','item_topics_labels') }} as topic_labels
+on topic.topic_id = topic_labels.topic_id
 

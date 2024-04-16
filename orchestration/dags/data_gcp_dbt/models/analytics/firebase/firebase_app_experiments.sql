7ba3e78e601ee36bef6e8431cddfa2e9930cfb3f
carolinemestre-passculture <162314477+carolinemestre-passculture@users.noreply.github.com>
2024-04-16 18:29:10 +0200

    (dbt) Switch firebase tables and delete legacy code (#2416)
    * feat: switch firebase tables and delete legacy code
    
    * feat: switch firebase tables and delete legacy code

--- orchestration/dags/data_gcp_dbt/models/analytics/firebase/firebase_app_experiments.sql
+++ orchestration/dags/data_gcp_dbt/models/analytics/firebase/firebase_app_experiments.sql
@@ -1,8 +1,10 @@
-SELECT 
-        user_pseudo_id, 
-        user_id, 
-        experiment_name,
-        experiment_value,
-FROM
-    {{ source('clean', 'firebase_app_experiments') }}
+SELECT DISTINCT
+        event_date,
+        user_pseudo_id,
+        user_id,
+        user_properties.key as experiment_name,
+        user_properties.value.string_value as experiment_value
+FROM {{ source('raw','firebase_events') }},
+UNNEST(user_properties) AS user_properties
+where user_properties.key like "%firebase_exp%"
 QUALIFY ROW_NUMBER() OVER (PARTITION BY user_pseudo_id,user_id,experiment_name ORDER BY event_date DESC) = 1

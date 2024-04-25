0128869dab53ebdb51ebca8abd3026f44e376e18
carolinemestre-passculture <162314477+carolinemestre-passculture@users.noreply.github.com>
2024-04-25 17:35:30 +0200

    feat: update routines target (#2462)

--- orchestration/dags/data_gcp_dbt/models/intermediate/firebase/pro/int_firebase__pro_event.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/firebase/pro/int_firebase__pro_event.sql
@@ -1,7 +1,7 @@
 {{ config(pre_hook="{{ create_dehumanize_id_function() }}") }}
 
 {% set target_name = target.name %}
-{% set target_schema = generate_schema_name('analytics_dbt_' ~ target_name) %}
+{% set target_schema = generate_schema_name('analytics_' ~ target_name) %}
 
 {{
     config(

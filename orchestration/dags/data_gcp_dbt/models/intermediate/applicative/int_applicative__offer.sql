f3f5fc18cdb1c324becb97d40b8a32a6e55bdb7b
cdelabre <cyrille.delabre@passculture.app>
2024-04-26 17:19:06 +0200

    [dbt](int_applicative__offer) move item logic to offer_item_ids during migration

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__offer.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__offer.sql
@@ -12,36 +12,6 @@ WITH stocks_grouped_by_offers AS (
             MAX(last_individual_booking_date) AS last_individual_booking_date
         FROM {{ ref('int_applicative__stock') }}
         GROUP BY offer_id
-),
-
-item_ids AS (
-    SELECT
-        offer_id,
-        CASE WHEN (
-            offer_subcategoryId = 'LIVRE_PAPIER'
-            AND isbn IS NOT NULL
-            AND isbn <> ''
-            ) THEN CONCAT('isbn-',isbn)
-        WHEN (offer_subcategoryId = 'SEANCE_CINE'
-            AND theater_movie_id IS NOT NULL
-            AND theater_movie_id <> ''
-            ) THEN CONCAT('movie_id-',theater_movie_id)
-        WHEN (offer_subcategoryId = 'SUPPORT_PHYSIQUE_MUSIQUE_CD'
-            AND ean IS NOT NULL
-            AND ean <> ''
-            ) THEN CONCAT('cd_id-',ean)
-        WHEN (offer_subcategoryId = 'SUPPORT_PHYSIQUE_MUSIQUE_VINYLE'
-            AND ean IS NOT NULL
-            AND ean <> ''
-            ) THEN CONCAT('vinyle_id-',ean)
-        ELSE CONCAT('offer-',offer_id) END AS item_id
-    FROM {{ ref('int_applicative__extract_offer') }}
-),
-
-linked_offers_deduped AS (
-    SELECT offer_id, item_linked_id
-    FROM {{ source('analytics','linked_offers') }}
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY offer_id ORDER BY rand()) = 1
 )
 
 SELECT
@@ -91,14 +61,9 @@ SELECT
     subcategories.is_digital_deposit digital_goods,
     subcategories.is_event as event,
     subcategories.category_id AS offer_category_id,
-    CASE WHEN lo.item_linked_id is not null THEN REGEXP_REPLACE(
-            lo.item_linked_id,
-            r'[^a-zA-Z0-9\-\_]',
-            '')
-    ELSE REGEXP_REPLACE(ii.item_id, r'[^a-zA-Z0-9\-\_]', '') END as item_id
+     item_id
 FROM {{ ref('int_applicative__extract_offer') }} AS o
-LEFT JOIN item_ids AS ii ON ii.offer_id = o.offer_id
-LEFT JOIN linked_offers_deduped AS lo ON lo.offer_id = o.offer_id
+LEFT JOIN {{ ref('offer_item_ids') }} AS ii on o.offer_id=ii.offer_id
 LEFT JOIN stocks_grouped_by_offers AS so ON so.offer_id = o.offer_id
 LEFT JOIN {{ source('clean','subcategories') }} AS subcategories ON o.offer_subcategoryId = subcategories.id
 WHERE o.offer_subcategoryid NOT IN ('ACTIVATION_THING', 'ACTIVATION_EVENT')

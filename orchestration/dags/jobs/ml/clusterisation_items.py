0e216a174a24fda122fb07fe15107bddd271279d
cdelabre <cyrille.delabre@passculture.app>
2024-04-26 08:58:09 +0200

    (clusters) :fix update default flag for clusters

--- orchestration/dags/jobs/ml/clusterisation_items.py
+++ orchestration/dags/jobs/ml/clusterisation_items.py
@@ -61,7 +61,7 @@ with DAG(
             default="default-config",
             type="string",
         ),
-        "cluster_prefix": Param(default=None, type=["null", "string"]),
+        "cluster_prefix": Param(default="default_", type=["null", "string"]),
     },
 ) as dag:
     start = DummyOperator(task_id="start", dag=dag)

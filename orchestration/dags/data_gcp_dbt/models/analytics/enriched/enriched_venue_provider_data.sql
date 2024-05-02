dcc0dd3cf0ebdc67fc20ee46c1c06695b4a75db6
« MathildeVey » <Mathilde.vey@passculture.app>
2024-05-02 10:30:04 +0200

    add fields about collective synchro and add creation_date fields

--- orchestration/dags/data_gcp_dbt/models/analytics/enriched/enriched_venue_provider_data.sql
+++ orchestration/dags/data_gcp_dbt/models/analytics/enriched/enriched_venue_provider_data.sql
@@ -9,8 +9,11 @@ SELECT
   , venue_is_permanent
   , venue_label
   , last_sync_date
+  , creation_date
   , count(distinct offer_id) as offer_sync_cnt
-  , min(offer_creation_date) AS first_offer_sync_date
+  , count(distinct collective_offer_id) as collective_offer_sync_cnt
+  , min(offer_creation_date) as first_offer_sync_date
+  , min(collective_offer_creation_date) as first_collective_offer_sync_date
 FROM {{ ref('provider') }} provider
 JOIN {{ ref('venue_provider') }} venue_provider
 USING(provider_id)
@@ -21,6 +24,9 @@ USING(venue_label_id)
 LEFT JOIN {{ ref('offer') }} offer
 ON venue.venue_id = offer.venue_id
 AND provider.provider_id = offer.offer_last_provider_id 
+LEFT JOIN {{ ref('collective_offer') }} collective_offer
+ON venue.venue_id = collective_offer.venue_id
+AND provider.provider_id = collective_offer.provider_id 
 GROUP BY 
   provider.provider_id
   , provider.provider_name 
@@ -32,3 +38,4 @@ GROUP BY
   , venue_is_permanent
   , venue_label
   , last_sync_date
+  , creation_date

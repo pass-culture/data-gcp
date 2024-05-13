58e60a3bd117d7530871eff3b29444852ab24450
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2024-05-13 17:27:57 +0200

    Update enriched_offerer_data.sql

--- orchestration/dags/data_gcp_dbt/models/analytics/enriched/enriched_offerer_data.sql
+++ orchestration/dags/data_gcp_dbt/models/analytics/enriched/enriched_offerer_data.sql
@@ -321,4 +321,4 @@ LEFT JOIN siren_reference_adage ON offerer.offerer_siren = siren_reference_adage
 WHERE
     offerer.offerer_validation_status='VALIDATED'
     AND offerer.offerer_is_active
-QUALIFY row_number() over(partition by siren_data.siren order by update_date desc) = 1
+QUALIFY row_number() over(partition by offerer.offerer_siren order by update_date desc) = 1

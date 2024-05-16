b7aa80eda34181354ebf0bf4c65d4cb80edcb92d
Lucille-Artgd <lucille.artigaud@passculture.app>
2024-05-16 17:20:48 +0200

    PC-29801 added tables to identify marseille en grand program

--- orchestration/dags/data_gcp_dbt/models/clean/applicative/educational_institution_program.sql
+++
@@ -1,3 +0,0 @@
-SELECT 
-    *
-FROM {{ source('raw', 'applicative_database_educational_institution_program') }}
--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__institution.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__institution.sql
@@ -2,5 +2,5 @@ SELECT
     ipa.institution_id
     ,ip.program_label AS institution_program_name
 FROM {{ source('raw','applicative_database_institution_program_association') }} AS ipa
-JOIN {{ source('raw','applicative_database_institution_program') }} AS ip
+INNER JOIN {{ source('raw','applicative_database_institution_program') }} AS ip
     ON ipa.program_id = ip.program_id

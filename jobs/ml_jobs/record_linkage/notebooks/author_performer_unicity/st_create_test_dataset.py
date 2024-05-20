a1e878cbb88dec3cb61dd69cbe5b64aabfa54a12
Laurent Montier <laurent.montier@passculture.app>
2024-05-20 11:19:45 +0200

    Add paging in streamlit app

--- jobs/ml_jobs/record_linkage/notebooks/author_performer_unicity/st_create_test_dataset.py
+++ jobs/ml_jobs/record_linkage/notebooks/author_performer_unicity/st_create_test_dataset.py
@@ -7,15 +7,21 @@ st.set_page_config(layout="wide")
 merged_df = pd.read_csv(
     "notebooks/author_performer_unicity/data/author_clustered.csv"
 ).sort_values(["cluster_name"])
-
-# %% Display the results
 cluster_names = ["NO CLUSTER"] + merged_df.cluster_name.unique().tolist()
+ITEM_PER_PAGE = 100
+st_selected_page = st.sidebar.selectbox(
+    "Page", options=[i for i in range(1, 2 + len(merged_df) // ITEM_PER_PAGE)], index=0
+)
+
+# %% Display data info
 st.write("Number of authors", len(merged_df))
 st.write("Number of clusters", len(cluster_names))
 data = {}
 with st.form(key="Assign Clusters"):
-    for row_index, row in merged_df.iterrows():
-        col1, col2 = st.columns(2)
+    for row_index, row in merged_df.iloc[
+        (st_selected_page - 1) * ITEM_PER_PAGE : st_selected_page * ITEM_PER_PAGE
+    ].iterrows():
+        col1, col2, _, _ = st.columns(4)
         with col1:
             st.write(row["author"])
         with col2:
@@ -34,5 +40,6 @@ if st_button is True:
     test_set_df = merged_df.assign(true_cluster_name=lambda df: df.index.map(data))
     st.dataframe(test_set_df)
     test_set_df.to_csv(
-        "notebooks/author_performer_unicity/data/test_set.csv", index=False
+        f"notebooks/author_performer_unicity/data/test_set_{st_selected_page}.csv",
+        index=False,
     )

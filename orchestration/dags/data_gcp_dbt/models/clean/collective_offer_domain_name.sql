9e453b242a617de664f437ab2ce922fc926694dc
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-05-21 11:05:25 +0200

    feat: add models to DBT

--- orchestration/dags/data_gcp_dbt/models/clean/collective_offer_domain_name.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-    cod.collective_offer_id,
-    cod.educational_domain_id,
-    ed.educational_domain_name
-FROM {{ source('raw', 'applicative_database_collective_offer_domain') }} cod
-LEFT JOIN {{ source('raw', 'applicative_database_educational_domain') }}ed ON cod.educational_domain_id = ed.educational_domain_id

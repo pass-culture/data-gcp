6e5dd9d22e5a058fc23daa39129fd53c9b4ad0df
Laurent Montier <laurent.montier@passculture.app>
2024-05-22 15:57:23 +0200

    Refacto the Streamlit

--- jobs/ml_jobs/record_linkage/notebooks/author_performer_unicity/st_explore_data.py
+++ jobs/ml_jobs/record_linkage/notebooks/author_performer_unicity/st_explore_data.py
@@ -16,6 +16,7 @@ CATEGORIES = author_df.offer_category_id.unique()
 PUNCTUATION = r"!|#|\$|\%|\&|\(|\)|\*|\+|\,|\/|\:|\;|\|\s-|\s-\s|-\s|\|"  # '<=>?@[\\]^_`{|}~|\s–\s'
 NUM_CHUNKS = 50
 SPARSE_FILTER_THRESHOLD = 0.2
+DTYPE_DISTANCE_MATRIX = np.uint8  # np.uint8, np.uint16, np.float32
 
 
 # %%
@@ -133,17 +134,16 @@ with st.form("compute clusters"):
     )
     authors_list = filtered_df.author.map(preprocessing).drop_duplicates().tolist()
     st.write("Number of authors", len(authors_list))
-    # Every form must have a submit button.
-    submitted = st.form_submit_button("Compute")
 
+    submitted = st.form_submit_button("Compute")
     if submitted is True:
 
         # Compute the parwise distance between the authors
         t0 = time.time()
-        dtype = np.uint8  # np.uint8, np.uint16, np.float32
-
         score_multiplier = (
-            255 if dtype == np.uint8 else 65535 if dtype == np.uint16 else 1
+            255
+            if DTYPE_DISTANCE_MATRIX == np.uint8
+            else 65535 if DTYPE_DISTANCE_MATRIX == np.uint16 else 1
         )
 
         # Loop over the chunks
@@ -156,22 +156,22 @@ with st.form("compute clusters"):
                 queries=authors_chunk,
                 choices=authors_list,
                 scorer=st_method,
-                workers=-1,  # -1 for all cores
                 score_multiplier=score_multiplier,
-                dtype=dtype,
+                dtype=DTYPE_DISTANCE_MATRIX,
+                workers=-1,  # -1 for all cores
             )
 
-            # Filter the distance matrix
+            # Create the Sparse Matrix
             distance_matrix[
                 distance_matrix > SPARSE_FILTER_THRESHOLD * score_multiplier
             ] = 0
             sparse_matrices.append(csr_matrix(distance_matrix))
 
         # Concatenate the sparse matrices
-        full_sparse_distance_matrix = vstack(blocks=sparse_matrices, format="csr")
+        complete_sparse_matrix = vstack(blocks=sparse_matrices, format="csr")
 
         st.write("Time to compute the matrix", time.time() - t0)
-        st.write("Memory used", full_sparse_distance_matrix.data.nbytes / 1024**2, "MB")
+        st.write("Memory used", complete_sparse_matrix.data.nbytes / 1024**2, "MB")
 
         # Perform clustering with DBSCAN
         t0 = time.time()
@@ -180,7 +180,7 @@ with st.form("compute clusters"):
             min_samples=2,
             metric="precomputed",
         )
-        clustering.fit(full_sparse_distance_matrix)
+        clustering.fit(complete_sparse_matrix)
         clusters = clustering.labels_
 
         clusters_df = (

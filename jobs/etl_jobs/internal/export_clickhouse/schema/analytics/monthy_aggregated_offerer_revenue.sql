fd0f1ccb99140dfefa7fdc12caad583307bf767f
valoumiaou <valentin.busson@passculture.app>
2024-05-24 09:33:27 +0200

    analytics queries in clickhouse to be called by stat page

---
+++ jobs/etl_jobs/internal/export_clickhouse/schema/analytics/monthy_aggregated_offerer_revenue.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW analytics.monthy_aggregated_offerer_revenue ON cluster default
+AS
+SELECT
+    date_trunc('MONTH',toDate(creation_date)) AS creation_month,
+    offerer_id,
+    sum(case when booking_status = 'USED' or booking_status ='REIMBURSED' then booking_amount else 0 end) AS revenue,
+    sum(booking_amount) AS forcasted_revenue
+FROM intermediate.bookings
+GROUP BY
+    offerer_id, creation_month
+order by offerer_id, creation_month DESC

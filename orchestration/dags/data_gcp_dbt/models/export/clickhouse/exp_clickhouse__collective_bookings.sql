c20a877d680eb06c3295fe89cd76454bd756cdd5
valoumiaou <valentin.busson@passculture.app>
2024-05-24 09:28:01 +0200

    create views in dbt to export clickhouse data

---
+++ orchestration/dags/data_gcp_dbt/models/export/clickhouse/exp_clickhouse__collective_bookings.sql
@@ -0,0 +1,14 @@
+SELECT
+    DATE("{{ ds }}") as update_date,
+    offerer_id,
+    collective_offer_id,
+    offer_id,
+    date(collective_booking_creation_date) as creation_date,
+    date(collective_booking_used_date) as used_date,
+    date(collective_booking_reimbursement_date) as reimbursement_date,
+    collective_booking_status,
+    educational_institution_id,
+    number_of_tickets,
+    booking_amount
+FROM {{ ref('enriched_collective_booking_data') }}
+WHERE not cast(collective_booking_is_cancelled as BOOL)

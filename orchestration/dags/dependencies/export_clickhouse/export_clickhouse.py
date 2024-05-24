ea505db9f59c6de1244571ce4b2f6ffd44c0a887
valoumiaou <valentin.busson@passculture.app>
2024-05-24 14:59:44 +0200

    remove old clickhouse dag

--- orchestration/dags/dependencies/export_clickhouse/export_clickhouse.py
+++ orchestration/dags/dependencies/export_clickhouse/export_clickhouse.py
@@ -1,26 +1,5 @@
 TABLES_CONFIGS = [
     {
-        "sql": "export_user_consultation",
-        "clickhouse_table_name": "user_consultation",
-        "clickhouse_dataset_name": "intermediate",
-        "mode": "incremental",
-    },
-    {
-        "sql": "export_offer_consultation",
-        "clickhouse_table_name": "offer_consultation",
-        "clickhouse_dataset_name": "intermediate",
-        "mode": "incremental",
-    },
-    {
-        "sql": "export_venue_offer_statistics",
-        "clickhouse_table_name": "venue_offer_statistics",
-        "clickhouse_dataset_name": "analytics",
-        "mode": "overwrite",
-    },
-]
-
-TABLES_CONFIGS2 = [
-    {
         "sql": "clickhouse__booking",
         "clickhouse_table_name": "booking",
         "clickhouse_dataset_name": "intermediate",

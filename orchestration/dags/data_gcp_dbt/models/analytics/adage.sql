d6923a1a55fb49163d716f28148ec66148501b49
Caroline Mestre <162314477+carolinemestre-passculture@users.noreply.github.com>
2024-05-27 11:02:04 +0200

    (dbt) Update analytics.adage (#2549)
    * feat: update analytics.adage
    
    * fix: update model reference
    
    * feat: update analytics.adage
    
    * feat: add raw variable
    
    * fix: remove adage_agreg_synchro subquery

---
+++ orchestration/dags/data_gcp_dbt/models/analytics/adage.sql
@@ -0,0 +1,38 @@
+WITH adage_agreg_synchro AS (
+SELECT siret
+FROM {{ source('raw', 'adage')}}
+where synchroPass = "1.0"
+)
+
+SELECT
+    id,
+    venueId,
+    siret,
+    statutId,
+    labelId,
+    typeId,
+    communeId,
+    libelle,
+    adresse,
+    siteWeb,
+    latitude,
+    longitude,
+    actif,
+    synchroPass,
+    dateModification,
+    statutLibelle,
+    labelLibelle,
+    typeIcone,
+    typeLibelle,
+    communeLibelle,
+    communeDepartement,
+    academieLibelle,
+    regionLibelle,
+    domaines,
+    domaineIds,
+    regionId,
+    academieId,
+    left(siret, 9) AS siren,
+    CASE WHEN siret in (select siret from adage_agreg_synchro) THEN TRUE ELSE FALSE END AS siret_synchro_adage,
+    CASE WHEN left(siret, 9) in (select siren from adage_agreg_synchro) THEN TRUE ELSE FALSE END AS siren_synchro_adage,
+FROM {{ source('raw','adage') }}

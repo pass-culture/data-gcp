c2a51303c78524c456013380931a59a2d5003192
Laurent Montier <laurent.montier@passculture.app>
2024-05-27 10:41:29 +0200

    Update link_artists dag

--- orchestration/dags/jobs/ml/link_artists.py
+++ orchestration/dags/jobs/ml/link_artists.py
@@ -13,12 +13,21 @@ from common.config import (
     GCP_PROJECT_ID,
 )
 from common.operators.biquery import BigQueryInsertJobOperator, bigquery_job_task
+from common.operators.gce import (
+    CloneRepositoryGCEOperator,
+    SSHGCEOperator,
+    StartGCEOperator,
+    StopGCEOperator,
+)
 from dependencies.ml.linkage.import_artists import PARAMS
 
 DEFAULT_REGION = "europe-west1"
-GCE_INSTANCE = f"link-offers-{ENV_SHORT_NAME}"
+GCE_INSTANCE = f"link-artists-{ENV_SHORT_NAME}"
 BASE_DIR = "data-gcp/jobs/ml_jobs/record_linkage"
 STORAGE_PATH = f"{DATA_GCS_BUCKET_NAME}/link_artists"
+INPUT_GCS_PATH = f"gs://{STORAGE_PATH}/artists_to_match.parquet"
+OUTPUT_GCS_PATH = f"gs://{STORAGE_PATH}/matched_artists.parquet"
+
 
 default_args = {
     "start_date": datetime(2024, 5, 1),
@@ -55,8 +64,7 @@ with DAG(
         dag, f"create_bq_table_{PARAMS['destination_table']}", PARAMS
     )
 
-    export_file_uri = f"gs://{STORAGE_PATH}/artists_to_match.parquet"
-    export_bq_to_gcs = BigQueryInsertJobOperator(
+    export_input_bq_to_gcs = BigQueryInsertJobOperator(
         task_id=f"{PARAMS['destination_table']}_to_bucket",
         configuration={
             "extract": {
@@ -66,11 +74,58 @@ with DAG(
                     "tableId": PARAMS["destination_table"],
                 },
                 "compression": None,
-                "destinationUris": export_file_uri,
+                "destinationUris": INPUT_GCS_PATH,
                 "destinationFormat": "PARQUET",
             }
         },
         dag=dag,
     )
 
-    (logging_task >> data_collect >> export_bq_to_gcs)
+    gce_instance_start = StartGCEOperator(
+        task_id="gce_start_task",
+        instance_name=GCE_INSTANCE,
+        instance_type="{{ params.instance_type }}",
+        labels={"job_type": "ml"},
+        preemptible=False,
+    )
+
+    fetch_code = CloneRepositoryGCEOperator(
+        task_id="fetch_code",
+        instance_name=GCE_INSTANCE,
+        python_version="3.10",
+        command="{{ params.branch }}",
+    )
+
+    install_dependencies = SSHGCEOperator(
+        task_id="install_dependencies",
+        instance_name=GCE_INSTANCE,
+        base_dir=BASE_DIR,
+        command="""pip install -r requirements.txt --user""",
+    )
+
+    record_linkage = SSHGCEOperator(
+        task_id="artist_linkage",
+        instance_name=GCE_INSTANCE,
+        base_dir=BASE_DIR,
+        command="""
+         python main.py \
+        --source-file-path {INPUT_GCS_PATH} \
+        --output-file-path {OUTPUT_GCS_PATH}"
+    )
+    """,
+    )
+
+    gce_instance_stop = StopGCEOperator(
+        task_id="gce_stop_task", instance_name=GCE_INSTANCE
+    )
+
+    (
+        logging_task
+        >> data_collect
+        >> export_input_bq_to_gcs
+        >> gce_instance_start
+        >> fetch_code
+        >> install_dependencies
+        >> record_linkage
+        >> gce_instance_stop
+    )

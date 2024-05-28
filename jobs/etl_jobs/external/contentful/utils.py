b114df2762bfcc21ab3ddde1dd3f77c0c1ce853d
cdelabre <cyrille.delabre@passculture.app>
2024-05-28 17:23:58 +0200

    [DE](contentful) feat: include draft modules

--- jobs/etl_jobs/external/contentful/utils.py
+++ jobs/etl_jobs/external/contentful/utils.py
@@ -18,7 +18,7 @@ def access_secret_data(project_id, secret_id, version_id=1, default=None):
         return default
 
 
-TOKEN = access_secret_data(GCP_PROJECT, "contentful-token")
+TOKEN = access_secret_data(GCP_PROJECT, "contentful-preview-token")
 SPACE_ID = access_secret_data(GCP_PROJECT, "contentful-space-id")
 
 ENTRIES_DTYPE = {

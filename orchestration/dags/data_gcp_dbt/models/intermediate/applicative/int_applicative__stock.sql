9e9dd743ad74f5eb79b411e4d4def40ea3b190ac
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-05-29 14:43:59 +0200

    feat: migrate enriched_stock_data

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__stock.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__stock.sql
@@ -1,9 +1,18 @@
+{% set target_name = target.name %}
+{% set target_schema = generate_schema_name('analytics_' ~ target_name) %}
+
+{{ config(
+    pre_hook="{{create_humanize_id_function()}}"
+) }}
+
 WITH bookings_grouped_by_stock AS (
 
     SELECT
         stock_id,
-        -- garder que un des deux entre total_bookings et total_individual_bookings
-        SUM(CASE WHEN booking_is_cancelled = False THEN booking_quantity END) AS total_bookings,
+        SUM(booking_quantity) AS total_bookings,
+        SUM(CASE WHEN booking_is_cancelled = False THEN booking_quantity END) AS total_non_cancelled_bookings,
+        SUM(CASE WHEN booking_is_cancelled = True THEN booking_quantity END) AS total_cancelled_bookings,
+        SUM(CASE WHEN booking_status = 'REIMBURSED' THEN booking_quantity END) AS total_paid_bookings,
         COUNT(booking_id) AS total_individual_bookings,
         COUNT(CASE WHEN booking_is_cancelled THEN booking_id END) AS total_cancelled_individual_bookings,
         COUNT(CASE WHEN NOT booking_is_cancelled THEN booking_id END) AS total_non_cancelled_individual_bookings,
@@ -19,11 +28,12 @@ WITH bookings_grouped_by_stock AS (
 
 SELECT
     s.stock_id,
+    {{target_schema}}.humanize_id(s.stock_id) AS stock_humanized_id,
     s.stock_id_at_providers,
     s.stock_modified_at_last_provider_date,
     DATE(s.stock_modified_date) AS stock_modified_date,
     s.stock_modified_date AS stock_modified_at,
-    s.stock_price,
+    COALESCE(s.stock_price, price_category.price) AS stock_price,
     s.stock_quantity,
     s.stock_booking_limit_date,
     s.stock_last_provider_id,
@@ -37,7 +47,10 @@ SELECT
     CASE WHEN s.stock_quantity IS NULL THEN NULL
         ELSE GREATEST( s.stock_quantity - COALESCE(bs.total_bookings, 0),0)
     END AS total_available_stock,
-    total_bookings,
+    bs.total_bookings,
+    bs.total_non_cancelled_bookings,
+    bs.total_cancelled_bookings,
+    bs.total_paid_bookings,
     bs.total_individual_bookings,
     bs.total_cancelled_individual_bookings,
     bs.total_non_cancelled_individual_bookings,
@@ -52,9 +65,13 @@ SELECT
     -- <> available_stock > 0 OR available_stock is null
     AND (GREATEST(s.stock_quantity - COALESCE(bs.total_bookings, 0),0) > 0 OR s.stock_quantity IS NULL)
     AND NOT s.stock_is_soft_deleted) THEN TRUE ELSE FALSE END AS is_bookable,
+    price_category.price_category_label_id,
+    price_category_label.label AS price_category_label,
     rank() OVER (PARTITION BY s.offer_id ORDER BY s.stock_creation_date DESC, s.stock_id DESC) AS stock_rk
 FROM {{ source('raw','applicative_database_stock') }} AS s
 LEFT JOIN bookings_grouped_by_stock AS bs ON bs.stock_id = s.stock_id
+LEFT JOIN {{ source('raw','applicative_database_price_category') }} AS price_category ON price_category.price_category_id = s.price_category_id
+LEFT JOIN {{ source('raw','applicative_database_price_category_label') }} AS price_category_label ON price_category.price_category_label_id = price_category_label.price_category_label_id
 WHERE TRUE
     {% if is_incremental() %}
     AND stock_modified_date BETWEEN date_sub(DATE("{{ ds() }}"), INTERVAL 1 DAY) and DATE("{{ ds() }}")

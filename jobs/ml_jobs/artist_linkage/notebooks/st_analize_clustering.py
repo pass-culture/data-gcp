99c51844a78b6bae2f2e85bc9a425b82120f876c
Laurent Montier <laurent.montier@passculture.app>
2024-05-29 07:48:38 +0200

    Add st_analize_clustering to analyze clustering

---
+++ jobs/ml_jobs/artist_linkage/notebooks/st_analize_clustering.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import streamlit as st
+
+## Streamlit Config
+st.set_page_config(layout="wide")
+
+
+## Load Data
+matched_artists_df = pd.read_parquet(
+    "/home/laurent_pass/Téléchargements/link_artists_matched_artists.parquet"
+)
+
+## Parameters
+st_artist_types = st.sidebar.multiselect(
+    "Artist type",
+    options=matched_artists_df.artist_type.unique(),
+    default=matched_artists_df.artist_type.unique(),
+)
+st_category = st.sidebar.multiselect(
+    "Categaroy",
+    options=matched_artists_df.offer_category_id.unique(),
+    default=matched_artists_df.offer_category_id.unique(),
+)
+st_search_filter = st.sidebar.text_input(
+    "Search Filter", value="", help="Search for artist name"
+)
+
+## Filter Dataframe
+filtered_df = (
+    matched_artists_df.loc[lambda df: df.artist_type.isin(st_artist_types)]
+    .loc[lambda df: df.offer_category_id.isin(st_category)]
+    .loc[
+        lambda df: (
+            df.artist_name.str.contains(st_search_filter, case=False)
+            if st_search_filter != ""
+            else df.index
+        )
+    ]
+)
+
+
+## Display
+st.sidebar.write(matched_artists_df.columns)
+st.dataframe(
+    filtered_df.sort_values("cluster_id"),
+    width=1500,
+    height=900,
+)

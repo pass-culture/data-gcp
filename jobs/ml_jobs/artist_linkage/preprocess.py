38473095c256cf1d2518133c71e04d1c2b9a9d98
Laurent Montier <laurent.montier@passculture.app>
2024-06-03 17:52:48 +0200

    Update preprocess.py

--- jobs/ml_jobs/artist_linkage/preprocess.py
+++ jobs/ml_jobs/artist_linkage/preprocess.py
@@ -1,69 +1,149 @@
+# %%
+import string
+
 import pandas as pd
 import rapidfuzz
 import typer
-
-from utils import read_parquet, upload_parquet
+from unidecode import unidecode
 
 app = typer.Typer()
 
 
-def remove_punctuation(input_df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Remove rows with artist names containing punctuation so that we get a clean dataset.
-    Args:
-        input_df: pd.DataFrame with a column 'artist_name'
-    Returns: pd.DataFrame
-    """
-    PUNCTUATION = r"!|#|\$|\%|\&|\(|\)|\*|\+|\,|\/|\:|\;|\|\s-|\s-\s|-\s|\|"
+### Parameters
+MIN_WORD_COUNT = 2
+MAX_WORD_COUNT = 5
+MIN_OFFER_COUNT = 100
+MIN_BOOKING_COUNT = 100
 
-    return (
-        input_df.loc[lambda df: ~df.artist_name.str.contains(PUNCTUATION)]
-        if len(input_df) > 0
-        else input_df
+
+### Cleaning
+
+
+def remove_leading_punctuation(artist_df: pd.DataFrame) -> pd.DataFrame:
+    return artist_df.assign(
+        artist_name=lambda df: df.artist_name.str.lstrip(
+            string.whitespace + string.punctuation
+        ).str.replace("\(.*\)", "", regex=True)
     )
 
 
-def format_artist_name(string: str) -> str:
-    return " ".join(sorted(rapidfuzz.utils.default_process(string).split()))
+def remove_parenthesis(artist_df: pd.DataFrame) -> pd.DataFrame:
+    return artist_df.assign(
+        artist_name=lambda df: df.artist_name.str.replace("\([.*]+\))", "")
+        .str.split("\(", regex=True)
+        .map(lambda ll: ll[0])
+    )
 
 
-def remove_writers_with_single_word_name(input_df: pd.DataFrame) -> pd.DataFrame:
-    writers_df = (
-        input_df.loc[lambda df: df.offer_category_id == "LIVRE"]
-        .assign(
-            preprocessed_artist_name=lambda df: df.preprocessed_artist_name.str.replace(
-                r"(\b[a-zA-Z]\b)", "", regex=True
-            )
-            .str.replace(r"\s+", " ", regex=True)
-            .str.strip()
+### Multi Artists
+
+
+def extract_first_artist_pattern(artist_df: pd.DataFrame):
+    pattern = ";|/|\+|\&"
+    return artist_df.assign(
+        first_artist_pattern=lambda df: df.artist_name.str.split(
+            pattern, regex=True
+        ).map(lambda artist_list: artist_list[0]),
+        is_multi_artists_pattern=lambda df: df.artist_name.str.contains(
+            pattern, regex=True
+        ),
+    )
+
+
+def extract_first_artist(artist_df: pd.DataFrame):
+    pattern = "^(?![\w\-']+,).*,.*|.*,.*,.*"
+    return artist_df.assign(
+        is_multi_artists=lambda df: (
+            df.first_artist_pattern.str.contains(pattern, regex=True)
+            & (~df.is_multi_artists_pattern)
+        ),
+        first_artist=lambda df: df.first_artist_pattern.str.split(",", regex=True)
+        .map(lambda artist_list: artist_list[0])
+        .where(df.is_multi_artists, df.first_artist_pattern),
+    )
+
+
+### Cleaning after extracting multi-authors
+
+
+def remove_single_characters(artist_df: pd.DataFrame) -> pd.DataFrame:
+    pattern = r"\b[a-zA-Z]\b(?!\.)"
+    return artist_df.assign(
+        first_artist=lambda df: df.first_artist.str.replace(pattern, "", regex=True)
+        .str.replace(r"\s+", " ", regex=True)
+        .str.strip()
+    )
+
+
+### Filtering
+
+
+def extract_artist_word_count(artist_df: pd.DataFrame) -> pd.DataFrame:
+    # count the number of words in the artist name that are longer than 2 characters (excluding punctuation / initials)
+    return artist_df.assign(
+        artist_word_count=lambda df: df.first_artist.str.replace(
+            r"[^\w\s]", "", regex=True
         )
-        .loc[
-            lambda df: (df.preprocessed_artist_name.apply(lambda x: len(x.split())) > 1)
-        ]
-        .drop_duplicates()
+        .str.split()
+        .map(lambda ll: len([x for x in ll if len(x) > 2])),
     )
-    return pd.concat(
-        [input_df.loc[lambda df: df.offer_category_id != "LIVRE"], writers_df]
+
+
+def filter_artists(artist_df: pd.DataFrame) -> bool:
+    pattern = "[\w\-\.]+\/[\w-]+|\+"  # pattern for multi artists separated by + or /
+
+    matching_patterns_indexes = artist_df.artist_name.str.contains(pattern, regex=True)
+    too_few_words_indexes = (artist_df.artist_word_count < MIN_WORD_COUNT) & (
+        (artist_df.offer_number < MIN_OFFER_COUNT)
+        | (artist_df.total_booking_count < MIN_BOOKING_COUNT)
     )
+    too_many_words_indexes = artist_df.artist_word_count > MAX_WORD_COUNT
+
+    should_be_filtered = (
+        matching_patterns_indexes | too_few_words_indexes | too_many_words_indexes
+    )
+
+    return artist_df.loc[~should_be_filtered]
+
+
+### Formatting
+
+
+def format_name(artist_df: pd.DataFrame) -> pd.DataFrame:
+    return artist_df.assign(
+        preprocessed_name=lambda df: df.first_artist.map(unidecode).map(
+            lambda s: " ".join(sorted(rapidfuzz.utils.default_process(s).split()))
+        )
+    )
+
+
+### Main
 
 
 @app.command()
 def main(
     source_file_path: str = typer.Option(), output_file_path: str = typer.Option()
 ) -> None:
-    # %%
     artists_to_match_df = read_parquet(source_file_path)
 
-    filtered_artists_to_match_df = (
-        artists_to_match_df.pipe(remove_punctuation)
-        .assign(
-            preprocessed_artist_name=lambda df: df.artist_name.map(format_artist_name)
-        )
-        .pipe(remove_writers_with_single_word_name)
+    cleaned_df = artists_to_match_df.pipe(remove_leading_punctuation).pipe(
+        remove_parenthesis
     )
 
+    single_artist_df = cleaned_df.pipe(extract_first_artist_pattern).pipe(
+        extract_first_artist
+    )
+
+    filtered_df = (
+        single_artist_df.pipe(extract_artist_word_count)
+        .pipe(remove_single_characters)
+        .pipe(filter_artists)
+    )
+
+    preprocessed_df = filtered_df.pipe(format_name)
+
     upload_parquet(
-        dataframe=filtered_artists_to_match_df,
+        dataframe=preprocessed_df,
         gcs_path=output_file_path,
     )
 

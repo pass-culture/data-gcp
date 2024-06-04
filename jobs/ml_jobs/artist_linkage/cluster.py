caf29d15633f37ff5c88d6d515176c97ee59e4ad
Laurent Montier <laurent.montier@passculture.app>
2024-06-04 15:13:50 +0200

    Refacto cluster_df

--- jobs/ml_jobs/artist_linkage/cluster.py
+++ jobs/ml_jobs/artist_linkage/cluster.py
@@ -1,5 +1,5 @@
 import time
-from typing import Generator, List
+from typing import Callable, Generator, List
 
 import numpy as np
 import pandas as pd
@@ -8,6 +8,8 @@ import typer
 from scipy.sparse import csr_matrix, vstack
 from sklearn.cluster import DBSCAN
 from tqdm import tqdm
+
+from utils.gcs_utils import upload_parquet
 
 app = typer.Typer()
 
@@ -21,7 +23,7 @@ CLUSTERING_THRESHOLD = 0.1
 RATIO_SYNCHRONISED_DATA_THRESHOLD = 0.8
 
 
-def get_score_multiplier(dtype_distance_matrix: np.dtype) -> int:
+def _get_score_multiplier(dtype_distance_matrix: np.dtype) -> int:
     """
     Get the score multiplier for the distance matrix. The score multiplier is the maximum value of the distance matrix.
     It can be useful in order to compress the distance matrix
@@ -36,35 +38,43 @@ def get_score_multiplier(dtype_distance_matrix: np.dtype) -> int:
         return 1
 
 
-def chunks(artist_list: List[str], num_chunks: int) -> Generator[List[str], None, None]:
+def _chunks(
+    artist_list: List[str], num_chunks: int
+) -> Generator[List[str], None, None]:
     """Yield successive n-sized chunks from artist_list."""
     for i in range(0, len(artist_list), num_chunks):
         yield artist_list[i : i + num_chunks]
 
 
-def compute_distance_matrix(artists_list: List[str], num_chunks: int):
+def _compute_distance_matrix(
+    artists_list: List[str],
+    num_chunks: int,
+    distance_metric: Callable,
+    dtype_distance_matrix: np.dtype,
+    sparse_filter_threshold: float,
+):
     if num_chunks <= 0:
         raise ValueError("num_chunks must be greater than 0")
 
     # Loop over the chunks
     sparse_matrices = []
     for artists_chunk in tqdm(
-        list(chunks(artists_list, len(artists_list) // num_chunks))
+        list(_chunks(artists_list, len(artists_list) // num_chunks))
     ):
         # Compute the distance matrix for the chunk
         distance_matrix = rapidfuzz.process.cdist(
             queries=artists_chunk,
             choices=artists_list,
-            scorer=DISTANCE_METRIC,
-            score_multiplier=get_score_multiplier(DTYPE_DISTANCE_MATRIX),
-            dtype=DTYPE_DISTANCE_MATRIX,
+            scorer=distance_metric,
+            score_multiplier=_get_score_multiplier(dtype_distance_matrix),
+            dtype=dtype_distance_matrix,
             workers=-1,  # -1 for all cores
         )
 
         # Create the Sparse Matrix
         distance_matrix[
             distance_matrix
-            > SPARSE_FILTER_THRESHOLD * get_score_multiplier(DTYPE_DISTANCE_MATRIX)
+            > sparse_filter_threshold * _get_score_multiplier(dtype_distance_matrix)
         ] = 0
         sparse_matrices.append(csr_matrix(distance_matrix))
 
@@ -76,96 +86,126 @@ def compute_distance_matrix(artists_list: List[str], num_chunks: int):
     return complete_sparse_matrix
 
 
+def cluster_with_distance_matrices(
+    group_artist_df: pd.DataFrame,
+    num_chunks: int,
+    clustering_threshold: float,
+    dtype_distance_matrix: np.dtype,
+    distance_metric: Callable,
+    sparse_filter_threshold: float,
+):
+    artists_list = group_artist_df.preprocessed_artist_name.drop_duplicates().tolist()
+    t0 = time.time()
+
+    complete_sparse_matrix = _compute_distance_matrix(
+        artists_list=artists_list,
+        num_chunks=num_chunks,
+        distance_metric=distance_metric,
+        dtype_distance_matrix=dtype_distance_matrix,
+        sparse_filter_threshold=sparse_filter_threshold,
+    )
+    print("Time to compute the ditance matrix", time.time() - t0)
+
+    # Perform clustering with DBSCAN
+    t0 = time.time()
+    clustering = DBSCAN(
+        eps=clustering_threshold * _get_score_multiplier(dtype_distance_matrix),
+        min_samples=2,
+        metric="precomputed",
+    )
+    clustering.fit(complete_sparse_matrix)
+    clusters = clustering.labels_
+    print("Time to compute the clustering", time.time() - t0)
+
+    return (
+        pd.DataFrame({"preprocessed_artist_name": artists_list})
+        .assign(
+            cluster=clusters,
+        )
+        .groupby("cluster")
+        .agg({"preprocessed_artist_name": set})
+    )
+
+
+def format_cluster_matrix(
+    cluster_df: pd.DataFrame, offer_category_id: str, artist_type: str
+) -> pd.DataFrame:
+    return cluster_df.assign(
+        num_artists=lambda df: df.preprocessed_artist_name.map(len),
+        offer_category_id=offer_category_id,
+        artist_type=artist_type,
+        group_cluster_id=lambda df: df.index,
+        cluster_id=lambda df: df.offer_category_id
+        + "_"
+        + df.artist_type
+        + "_"
+        + df.index.astype(str),
+    ).sort_values("num_artists", ascending=False)
+
+
 @app.command()
 def main(
     source_file_path: str = typer.Option(), output_file_path: str = typer.Option()
 ) -> None:
-    preprocessed_df = read_parquet(source_file_path)
+    preprocessed_df = pd.read_parquet(source_file_path).reset_index(drop=True)
 
     # Build the clusters
     clusters_df_list = []
     for group_name, group_df in preprocessed_df.groupby(
         ["offer_category_id", "artist_type"]
     ):
+        t0 = time.time()
+        print(
+            f"Matching artists names for group {group_name} containing {len(group_df.preprocessed_artist_name.unique())} artists"
+        )
+
         ratio_synchronised_data = group_df.is_synchronised.sum() / len(
             group_df.is_synchronised
         )
-
         if ratio_synchronised_data >= RATIO_SYNCHRONISED_DATA_THRESHOLD:
-            clusters_df_list.append(
+            clusters_by_group_df = (
                 group_df.loc[lambda df: df.is_synchronised]
                 .groupby("preprocessed_artist_name")
-                .apply(lambda g: set(g.artist_name))
-                .rename("artist")
+                .apply(lambda g: set(g.preprocessed_artist_name))
+                .rename("preprocessed_artist_name")
                 .to_frame()
-                .reset_index()
-                .assign(
-                    num_artists=lambda df: df.artist.map(len),
-                    offer_category_id=group_name[0],
-                    artist_type=group_name[1],
-                    group_cluster_id=lambda df: df.index,
-                    cluster_id=lambda df: df.offer_category_id
-                    + "_"
-                    + df.artist_type
-                    + "_"
-                    + df.index.astype(str),
-                )
-                .sort_values("num_artists", ascending=False)
+                .reset_index(drop=True)
             )
 
         else:
-            artists_list = group_df.preprocessed_artist_name.drop_duplicates().tolist()
+            clusters_by_group_df = cluster_with_distance_matrices(
+                group_artist_df=group_df,
+                num_chunks=NUM_CHUNKS,
+                clustering_threshold=CLUSTERING_THRESHOLD,
+                dtype_distance_matrix=DTYPE_DISTANCE_MATRIX,
+                distance_metric=DISTANCE_METRIC,
+                sparse_filter_threshold=SPARSE_FILTER_THRESHOLD,
+            )
 
-            t0 = time.time()
-            print(
-                f"Computing the distance for {group_name} containing {len(artists_list)} preprocessed artists"
+        clusters_df_list.append(
+            clusters_by_group_df.pipe(
+                format_cluster_matrix,
+                offer_category_id=group_name[0],
+                artist_type=group_name[1],
             )
-            complete_sparse_matrix = compute_distance_matrix(
-                artists_list=artists_list, num_chunks=NUM_CHUNKS
-            )
-            print("Time to compute the ditance matrix", time.time() - t0)
-
-            # Perform clustering with DBSCAN
-            t0 = time.time()
-            clustering = DBSCAN(
-                eps=CLUSTERING_THRESHOLD * get_score_multiplier(DTYPE_DISTANCE_MATRIX),
-                min_samples=2,
-                metric="precomputed",
-            )
-            clustering.fit(complete_sparse_matrix)
-            clusters = clustering.labels_
-
-            clusters_df_list.append(
-                pd.DataFrame({"artist": artists_list})
-                .assign(
-                    cluster=clusters,
-                )
-                .groupby("cluster")
-                .agg({"artist": set})
-                .assign(
-                    num_artists=lambda df: df.artist.map(len),
-                    offer_category_id=group_name[0],
-                    artist_type=group_name[1],
-                    group_cluster_id=lambda df: df.index,
-                    cluster_id=lambda df: df.offer_category_id
-                    + "_"
-                    + df.artist_type
-                    + "_"
-                    + df.index.astype(str),
-                )
-                .sort_values("num_artists", ascending=False)
-            )
-            print("Time to compute the clustering", time.time() - t0)
+        )
+        print("Time to compute the matching", time.time() - t0)
 
     clusters_df = pd.concat(clusters_df_list)
 
     merged_df = preprocessed_df.merge(
-        clusters_df.loc[lambda df: df.group_cluster_id != -1].explode("artist")[
-            ["artist", "offer_category_id", "artist_type", "cluster_id"]
+        clusters_df.loc[lambda df: df.group_cluster_id != -1].explode(
+            "preprocessed_artist_name"
+        )[
+            [
+                "preprocessed_artist_name",
+                "offer_category_id",
+                "artist_type",
+                "cluster_id",
+            ]
         ],
         how="left",
-        left_on=["preprocessed_artist_name", "offer_category_id", "artist_type"],
-        right_on=["artist", "offer_category_id", "artist_type"],
+        on=["preprocessed_artist_name", "offer_category_id", "artist_type"],
     )
 
     cluster_to_nickname_dict = (

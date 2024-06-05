a9a5cf90a4bd9b58afa86cc63cbc0f969075c20d
Laurent Montier <laurent.montier@passculture.app>
2024-06-05 10:10:27 +0200

    Document gcs_utils

--- jobs/ml_jobs/artist_linkage/utils/gcs_utils.py
+++ jobs/ml_jobs/artist_linkage/utils/gcs_utils.py
@@ -9,7 +9,18 @@ from pandas import DataFrame
 
 
 def _parse_gcs_path(gcs_path: str) -> Tuple[str, str]:
-    """Parse the GCS path into bucket and blob."""
+    """
+    Parse the GCS path into bucket and blob.
+
+    Args:
+        gcs_path (str): The GCS path to be parsed.
+
+    Returns:
+        Tuple[str, str]: A tuple containing the bucket name and blob name.
+
+    Raises:
+        ValueError: If the path does not start with 'gs://'.
+    """
     if not gcs_path.startswith("gs://"):
         raise ValueError("Path must start with 'gs://'")
     parsed_url = urlparse(gcs_path)
@@ -19,7 +30,16 @@ def _parse_gcs_path(gcs_path: str) -> Tuple[str, str]:
 
 
 def upload_parquet(dataframe: DataFrame, gcs_path: str) -> None:
-    """Upload a Pandas DataFrame as a Parquet file to the GCS path with a new filename."""
+    """
+    Uploads a Pandas DataFrame as a Parquet file to the GCS path with a new filename.
+
+    Args:
+        dataframe (DataFrame): The Pandas DataFrame to be uploaded.
+        gcs_path (str): The GCS path where the Parquet file will be uploaded.
+
+    Returns:
+        None
+    """
     bucket_name, blob_name = _parse_gcs_path(gcs_path)
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)

245f5ee5a9bc3c4138e35de21da0df6a794681f7
valoumiaou <valentin.busson@passculture.app>
2024-06-06 09:49:48 +0200

    add venue history snapshot

---
+++ orchestration/dags/data_gcp_dbt/snapshots/applicative_history/venue_history.sql
@@ -0,0 +1,28 @@
+{% snapshot booking_snapshot %}
+
+{{
+    config(
+      strategy='check',
+      unique_key='venue_id',
+      check_cols=['venue_siret', 'venue_is_permanent', 'venue_type_code', 'venue_label_id', 'banner_url', 'venue_description', 'venue_audioDisabilityCompliant', 'venue_mentalDisabilityCompliant', 'venue_motorDisabilityCompliant', 'venue_visualDisabilityCompliant', 'venue_withdrawal_details']
+    )
+}}
+
+select 
+  venue_id,
+  venue_siret,
+  venue_is_permanent,
+  venue_type_code,
+  venue_label_id,
+  banner_url,
+  venue_description,
+  venue_audioDisabilityCompliant,
+  venue_mentalDisabilityCompliant,
+  venue_motorDisabilityCompliant,
+  venue_visualDisabilityCompliant,
+  venue_withdrawal_details 
+from {{ ref('venue') }}
+
+{% endsnapshot %}
+
+

6f4644d23a54e21fdbe0ef2b5733670036681084
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-06-19 17:41:35 +0200

    feat: Add new columns int global_venue

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__collective_stock.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__collective_stock.sql
@@ -8,7 +8,8 @@ WITH collective_bookings_grouped_by_collective_stock AS (
         MAX(cb.collective_booking_creation_date) AS last_collective_booking_date,
         SUM(CASE WHEN cb.collective_booking_status != 'CANCELLED' THEN cs.collective_stock_price END) AS total_collective_theoretic_revenue,
         SUM(CASE WHEN cb.collective_booking_status IN ('USED','REIMBURSED')THEN cs.collective_stock_price END) AS total_collective_real_revenue,
-        SUM(CASE WHEN collective_booking_status IN ('USED','REIMBURSED') AND EXTRACT(YEAR FROM collective_booking_creation_date) = EXTRACT(YEAR FROM current_date) THEN cs.collective_stock_price ELSE NULL END) AS total_collective_current_year_real_revenue
+        SUM(CASE WHEN collective_booking_status IN ('USED','REIMBURSED') AND EXTRACT(YEAR FROM collective_booking_creation_date) = EXTRACT(YEAR FROM current_date) THEN cs.collective_stock_price END) AS total_collective_current_year_real_revenue,
+        COUNT(CASE WHEN is_current_educational_year AND cb.collective_booking_status != 'CANCELLED' THEN cb.collective_booking_id END) AS total_current_year_non_cancelled_collective_bookings
     FROM {{ ref('int_applicative__collective_booking') }} AS cb
     LEFT JOIN {{ source('raw','applicative_database_collective_stock') }} AS cs ON cb.collective_stock_id = cs.collective_stock_id
     GROUP BY collective_stock_id
@@ -32,10 +33,8 @@ SELECT cs.collective_stock_id,
     bcs.total_collective_theoretic_revenue,
     bcs.total_collective_real_revenue,
     bcs.total_collective_current_year_real_revenue,
-    CASE WHEN (DATE(cs.collective_stock_booking_limit_date_time) > CURRENT_DATE
-         OR cs.collective_stock_booking_limit_date_time IS NULL)
-    AND ( DATE( cs.collective_stock_beginning_date_time ) > CURRENT_DATE
-         OR cs.collective_stock_beginning_date_time IS NULL)
-    THEN TRUE ELSE FALSE END AS collective_stock_is_bookable
+    bcs.total_current_year_non_cancelled_collective_bookings,
+    (DATE(cs.collective_stock_booking_limit_date_time) > CURRENT_DATE OR cs.collective_stock_booking_limit_date_time IS NULL)
+    AND (DATE(cs.collective_stock_beginning_date_time) > CURRENT_DATE OR cs.collective_stock_beginning_date_time IS NULL) AS collective_stock_is_bookable
 FROM {{ source('raw','applicative_database_collective_stock') }} AS cs
 LEFT JOIN collective_bookings_grouped_by_collective_stock AS bcs ON bcs.collective_stock_id = cs.collective_stock_id

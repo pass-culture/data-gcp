3d318a15547bbeae26d30e4eb5e766d0c07bf025
valoumiaou <valentin.busson@passculture.app>
2024-06-21 14:19:09 +0200

    table export config

--- orchestration/dags/dependencies/export_clickhouse/export_clickhouse.py
+++ orchestration/dags/dependencies/export_clickhouse/export_clickhouse.py
@@ -1,18 +1,18 @@
 TABLES_CONFIGS = [
     {
-        "sql": "clickhouse__booking",
+        "sql": "clickhouse_booking",
         "clickhouse_table_name": "booking",
         "clickhouse_dataset_name": "intermediate",
         "mode": "overwrite",
     },
     {
-        "sql": "clickhouse__collective_booking",
+        "sql": "clickhouse_collective_booking",
         "clickhouse_table_name": "collective_booking",
         "clickhouse_dataset_name": "intermediate",
         "mode": "overwrite",
     },
     {
-        "sql": "clickhouse__native_event",
+        "sql": "clickhouse_native_event",
         "clickhouse_table_name": "native_event",
         "clickhouse_dataset_name": "intermediate",
         "mode": "incremental",

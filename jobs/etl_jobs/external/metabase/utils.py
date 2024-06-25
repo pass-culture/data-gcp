93258c05954fefecf9ce2d40e6d8be31271982cb
LucileRainteau <114910642+LucileRainteau@users.noreply.github.com>
2024-06-25 18:07:47 +0200

    (DE-171) add script to switch Metabase and create DAG (#2648)
    * (DE-171) add script to switch Metabase and create DAG
    
    * (DE-171) remove prod condition
    
    * (DE-171) fix dag parameters
    
    * (DE-171) cleaning code 1/2
    
    * cleaning code 2/2
    
    * (DE-171) update table name for metabase dependencies

---
+++ jobs/etl_jobs/external/metabase/utils.py
@@ -0,0 +1,61 @@
+import os
+import pandas as pd
+from google.auth.exceptions import DefaultCredentialsError
+from google.cloud import secretmanager
+
+
+PROJECT_NAME = os.environ.get("PROJECT_NAME")
+ENVIRONMENT_SHORT_NAME = os.environ.get("ENV_SHORT_NAME")
+ANALYTICS_DATASET = f"analytics_{ENVIRONMENT_SHORT_NAME}"
+CLEAN_DATASET = f"clean_{ENVIRONMENT_SHORT_NAME}"
+METABASE_API_USERNAME = "metabase-data-bot@passculture.app"
+
+
+def access_secret_data(project_id, secret_id, default=None):
+    try:
+        client = secretmanager.SecretManagerServiceClient()
+        name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
+        response = client.access_secret_version(request={"name": name})
+        return response.payload.data.decode("UTF-8")
+    except DefaultCredentialsError:
+        return default
+
+
+def get_dependant_cards(legacy_table_name, legacy_schema_name):
+    query = f"""
+        SELECT distinct card_id, card_type
+        FROM `{ENVIRONMENT_SHORT_NAME}.{CLEAN_DATASET}.dependencies_metabase` 
+        WHERE table_name = '{legacy_table_name}'
+        and lower(card_name) not like '%archive%'
+        and schema = '{legacy_schema_name}' 
+    """
+
+    metabase_activity_query = f"""
+        SELECT *
+        FROM `{ENVIRONMENT_SHORT_NAME}.{ANALYTICS_DATASET}.metabase_activity` 
+    """
+
+    dependant_cards = pd.read_gbq(query)
+    metabase_activity = pd.read_gbq(metabase_activity_query)
+    cards_with_traffic = dependant_cards.merge(
+        metabase_activity, on="card_id", how="left"
+    )
+
+    df = cards_with_traffic[
+        [
+            "card_id",
+            "card_type",
+            "card_name",
+            "card_collection_id",
+            "slug_reduced_level_2",
+            "total_users",
+            "total_views",
+            "nbr_dashboards",
+            "last_execution_date",
+        ]
+    ].sort_values("total_users", ascending=False)
+
+    native_cards = df.query('card_type == "native"')["card_id"].to_list()
+    query_cards = df.query('card_type == "query"')["card_id"].to_list()
+
+    return native_cards, query_cards

ab0694ca6b7dc8ef64e840c897788ebe9f456746
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-06-27 11:11:18 +0200

    feat: Update firebase_pro_visits column names

--- orchestration/dags/data_gcp_dbt/models/intermediate/firebase/pro/int_firebase__pro_visit.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/firebase/pro/int_firebase__pro_visit.sql
@@ -25,7 +25,7 @@ SELECT
     FROM {{ ref('int_firebase__pro_event') }}
     WHERE TRUE
         {% if is_incremental() %}
-        AND first_event_date BETWEEN date_sub(DATE("{{ ds() }}"), INTERVAL 3 DAY) and DATE("{{ ds() }}")
+        AND event_date BETWEEN date_sub(DATE("{{ ds() }}"), INTERVAL 3 DAY) and DATE("{{ ds() }}")
         {% endif %}
 
 )
@@ -42,62 +42,62 @@ SELECT
     DATE_DIFF(MAX(event_timestamp),MIN(event_timestamp),SECOND) AS visit_duration_seconds,
 
 -- count page view
-    COUNTIF(event_name = "page_view" AND page_name = "Espace acteurs culturels - pass Culture Pro") AS nb_view_home_page,
-    COUNTIF(event_name = "page_view" AND page_name = "Guichet - pass Culture Pro") AS nb_view_ticket_page,
-    COUNTIF(event_name = "page_view" AND page_name = "Offres individuelles - pass Culture Pro") AS nb_view_individual_offer_page,
-    COUNTIF(event_name = "page_view" AND page_name = "Offres collectives - pass Culture Pro") AS nb_collective_offer_page,
-    COUNTIF(event_name = "page_view" AND page_name = "Réservations individuelles - pass Culture Pro") AS nb_view_individual_booking_page,
-    COUNTIF(event_name = "page_view" AND page_name = "Réservations collectives - pass Culture Pro") AS nb_view_collective_booking_page,
-    COUNTIF(event_name = "page_view" AND page_name IN ("Remboursements - pass Culture Pro","Gestion financière - pass Culture Pro")) AS nb_view_financial_receipt_page,
-    COUNTIF(event_name = "page_view" AND page_name = "Détails - pass Culture Pro") AS nb_view_financial_details_page,
-    COUNTIF(event_name = "page_view" AND page_name = "Informations bancaires - pass Culture Pro") AS nb_view_banking_info_page,
-    COUNTIF(event_name = "page_view" AND page_name = "Statistiques - pass Culture Pro") AS nb_view_stat_page,
+    COUNTIF(event_name = "page_view" AND page_name = "Espace acteurs culturels - pass Culture Pro") AS total_home_page_views,
+    COUNTIF(event_name = "page_view" AND page_name = "Guichet - pass Culture Pro") AS total_ticket_page_views,
+    COUNTIF(event_name = "page_view" AND page_name = "Offres individuelles - pass Culture Pro") AS total_individual_offer_page_views,
+    COUNTIF(event_name = "page_view" AND page_name = "Offres collectives - pass Culture Pro") AS total_collective_offer_pages,
+    COUNTIF(event_name = "page_view" AND page_name = "Réservations individuelles - pass Culture Pro") AS total_individual_booking_page_views,
+    COUNTIF(event_name = "page_view" AND page_name = "Réservations collectives - pass Culture Pro") AS total_collective_booking_page_views,
+    COUNTIF(event_name = "page_view" AND page_name IN ("Remboursements - pass Culture Pro","Gestion financière - pass Culture Pro")) AS total_financial_receipt_page_views,
+    COUNTIF(event_name = "page_view" AND page_name = "Détails - pass Culture Pro") AS total_financial_details_page_views,
+    COUNTIF(event_name = "page_view" AND page_name = "Informations bancaires - pass Culture Pro") AS total_banking_info_page_views,
+    COUNTIF(event_name = "page_view" AND page_name = "Statistiques - pass Culture Pro") AS total_stat_page_views,
 
 -- count offer creation
 -- hub
-    COUNTIF(event_name = "page_view" AND page_name IN ("Selection du type d’offre - pass Culture Pro","Choix de la nature de l'offre - Créer une offre - pass Culture Pro")) AS nb_hub_for_offer_creation,
+    COUNTIF(event_name = "page_view" AND page_name IN ("Selection du type d’offre - pass Culture Pro","Choix de la nature de l'offre - Créer une offre - pass Culture Pro")) AS total_hub_for_offer_creation,
 -- indiv
-    COUNTIF(event_name = "page_view" AND page_name IN ("Création - Détail de l’offre - pass Culture Pro","Détails - Créer une offre individuelle - pass Culture Pro")) AS nb_start_individual_offer_creation,
-    COUNTIF(event_name = "page_view" AND page_name= "Confirmation - Offre individuelle publiée - pass Culture Pro") AS nb_confirmed_individual_offer_creation,
+    COUNTIF(event_name = "page_view" AND page_name IN ("Création - Détail de l’offre - pass Culture Pro","Détails - Créer une offre individuelle - pass Culture Pro")) AS total_started_individual_offers,
+    COUNTIF(event_name = "page_view" AND page_name= "Confirmation - Offre individuelle publiée - pass Culture Pro") AS total_confirmed_individual_offers,
 -- collectiv
-    COUNTIF(event_name = "page_view" AND page_location LIKE "%/offre/creation/collectif/vitrine%" ) AS nb_start_collective_offer_template_creation,
-    COUNTIF(event_name = "page_view" AND page_location LIKE "%collectif/vitrine/confirmation%") AS nb_confirmed_collective_offer_template_creation,
-    COUNTIF(event_name = "page_view" AND (page_location LIKE "%/offre/creation/collectif?%" OR (page_location LIKE "%offre/collectif%" AND page_location LIKE "%creation?%"))) AS nb_start_collective_offer_bookable_creation, 
-    COUNTIF(event_name = "page_view" AND page_location LIKE "%/collectif/confirmation%") AS nb_confirmed_collective_offer_bookable_creation, 
+    COUNTIF(event_name = "page_view" AND page_location LIKE "%/offre/creation/collectif/vitrine%" ) AS total_started_collective_offer_template,
+    COUNTIF(event_name = "page_view" AND page_location LIKE "%collectif/vitrine/confirmation%") AS total_confirmed_collective_offer_template,
+    COUNTIF(event_name = "page_view" AND (page_location LIKE "%/offre/creation/collectif?%" OR (page_location LIKE "%offre/collectif%" AND page_location LIKE "%creation?%"))) AS total_started_bookable_collective_offers,
+    COUNTIF(event_name = "page_view" AND page_location LIKE "%/collectif/confirmation%") AS total_confirmed_bookable_collective_offers,
 
 -- count offer edition 
 -- indiv
-    COUNTIF(event_name = "page_view" AND page_name = "Détails - Modifier une offre individuelle - pass Culture Pro") AS nb_start_individual_offer_details_edition,
-    COUNTIF(event_name = "page_view" AND page_name = "Récapitulatif - Modifier une offre individuelle - pass Culture Pro" AND origin LIKE "%/offre/individuelle%" AND origin LIKE "%edition/informations%") AS nb_confirmed_individual_offer_details_edition,
-    COUNTIF(event_name = "page_view" AND page_name = "Stocks et prix - Modifier une offre individuelle - pass Culture Pro") AS nb_start_individual_offer_stock_edition,
-    COUNTIF(event_name = "page_view" AND page_name = "Stocks et prix - Consulter une offre individuelle - pass Culture Pro" AND origin LIKE "%/offre/individuelle%" AND origin LIKE "%/stocks%") AS nb_confirmed_individual_offer_stock_edition,
+    COUNTIF(event_name = "page_view" AND page_name = "Détails - Modifier une offre individuelle - pass Culture Pro") AS total_detail_edition_started_individual_offers,
+    COUNTIF(event_name = "page_view" AND page_name = "Récapitulatif - Modifier une offre individuelle - pass Culture Pro" AND origin LIKE "%/offre/individuelle%" AND origin LIKE "%edition/informations%") AS total_detail_edition_confirmed_individual_offers,
+    COUNTIF(event_name = "page_view" AND page_name = "Stocks et prix - Modifier une offre individuelle - pass Culture Pro") AS total_stock_edition_started_individual_offers,
+    COUNTIF(event_name = "page_view" AND page_name = "Stocks et prix - Consulter une offre individuelle - pass Culture Pro" AND origin LIKE "%/offre/individuelle%" AND origin LIKE "%/stocks%") AS total_stock_edition_confirmed_individual_offers,
 -- collectiv
-    COUNTIF(event_name = "page_view" AND page_name = "Détails - Modifier une offre collective réservable - pass Culture Pro") AS nb_start_collective_offer_edition,
-    COUNTIF(event_name = "page_view" AND page_name = "Récapitulatif - Modifier une offre collective réservable - pass Culture Pro" AND origin LIKE "%collectif/edition%") AS nb_confirmed_collective_offer_edition,
+    COUNTIF(event_name = "page_view" AND page_name = "Détails - Modifier une offre collective réservable - pass Culture Pro") AS total_edition_started_collective_offers,
+    COUNTIF(event_name = "page_view" AND page_name = "Récapitulatif - Modifier une offre collective réservable - pass Culture Pro" AND origin LIKE "%collectif/edition%") AS total_confirmed_collective_offer_edition,
 
 -- count venue creation
-    COUNTIF(event_name IN ("hasClickedAddFirstVenueInOfferer","hasClickedAddVenueInOfferer","hasClickedCreateVenue")) AS nb_start_venue_creation,
-    COUNTIF(event_name="hasClickedSaveVenue" AND is_edition!="true") AS nb_confirmed_venue_creation, 
+    COUNTIF(event_name IN ("hasClickedAddFirstVenueInOfferer","hasClickedAddVenueInOfferer","hasClickedCreateVenue")) AS total_started_venue_creation, -- total_created_venue ?
+    COUNTIF(event_name = "hasClickedSaveVenue" AND is_edition != "true") AS total_confirmed_venue_creation, -- total confirmed_venue ?
 
 -- count venue edition
-    COUNTIF(event_name = "page_view" AND (page_name ="Modifier un lieu - pass Culture Pro" OR (page_name = "Modifier ma page partenaire - pass Culture Pro" AND origin LIKE "%lieux%") OR (page_name IN ("Gérer ma page sur l’application - pass Culture Pro","Gérer ma page sur ADAGE - pass Culture Pro") AND page_location LIKE "%edition"))) AS nb_start_venue_edition, 
-    COUNTIF(event_name="hasClickedSaveVenue" AND is_edition="true") AS nb_confirmed_venue_edition,  
-    COUNTIF(event_name="hasClickedAddImage") AS nb_clic_add_image,     
+    COUNTIF(event_name = "page_view" AND (page_name = "Modifier un lieu - pass Culture Pro" OR (page_name = "Modifier ma page partenaire - pass Culture Pro" AND origin LIKE "%lieux%") OR (page_name IN ("Gérer ma page sur l’application - pass Culture Pro","Gérer ma page sur ADAGE - pass Culture Pro") AND page_location LIKE "%edition"))) AS total_started_venue_edition,
+    COUNTIF(event_name = "hasClickedSaveVenue" AND is_edition = "true") AS total_confirmed_venue_edition,
+    COUNTIF(event_name = "hasClickedAddImage") AS total_add_image_clicks,
 
 -- count other CTA 
-    COUNTIF(event_name IN ("hasClickedAddBankAccount","hasClickedContinueToDS","hasClickedBankDetailsRecordFollowUp")) AS nb_clic_add_bank_account,  
-    COUNTIF(event_name IN ("hasClickedAddVenueToBankAccount","hasClickedSaveVenueToBankAccount","hasClickedChangeVenueToBankAccount")) AS nb_clic_add_venue_to_bank_account,    
-    COUNTIF(event_name="hasClickedInviteCollaborator") AS nb_clic_add_collaborator,  
-    COUNTIF(event_name="hasSentInvitation") AS nb_clic_send_invitation,   
-    COUNTIF(event_name = "page_view" AND page_name = "Profil - pass Culture Pro") AS nb_clic_modify_profil,  
-    COUNTIF(event_name="hasClickedPartnerBlockPreviewVenueLink") AS nb_clic_partner_preview,
-    COUNTIF(event_name="hasClickedPartnerBlockCopyVenueLink") AS nb_clic_copy_partner_link,
-    COUNTIF(event_name="hasClickedPartnerBlockDmsApplicationLink") AS nb_clic_adage_synchro,
-    COUNTIF(event_name="hasClickedPartnerBlockCollectiveHelpLink") AS nb_clic_collective_help,
-    COUNTIF(event_name="hasClickedHelpCenter") AS nb_clic_help_center,
-    COUNTIF(event_name="hasClickedBestPracticesAndStudies") AS nb_clic_best_practices,
-    COUNTIF(event_name="hasClickedConsultSupport") AS nb_clic_consult_support,
-    COUNTIF(event_name="hasClickedConsultCGU") AS nb_clic_consult_cgu,
+    COUNTIF(event_name IN ("hasClickedAddBankAccount","hasClickedContinueToDS","hasClickedBankDetailsRecordFollowUp")) AS total_add_bank_account_clicks,
+    COUNTIF(event_name IN ("hasClickedAddVenueToBankAccount","hasClickedSaveVenueToBankAccount","hasClickedChangeVenueToBankAccount")) AS total_add_venue_to_bank_account_clicks,
+    COUNTIF(event_name="hasClickedInviteCollaborator") AS total_add_collaborator_clicks,
+    COUNTIF(event_name="hasSentInvitation") AS total_send_invitation_clicks,
+    COUNTIF(event_name = "page_view" AND page_name = "Profil - pass Culture Pro") AS total_modify_profile_clicks,
+    COUNTIF(event_name= "hasClickedPartnerBlockPreviewVenueLink") AS total_partner_preview_clicks,
+    COUNTIF(event_name= "hasClickedPartnerBlockCopyVenueLink") AS total_copy_partner_link_clicks,
+    COUNTIF(event_name= "hasClickedPartnerBlockDmsApplicationLink") AS total_adage_synchro_clicks,
+    COUNTIF(event_name= "hasClickedPartnerBlockCollectiveHelpLink") AS total_collective_help_clicks,
+    COUNTIF(event_name= "hasClickedHelpCenter") AS total_help_center_clicks,
+    COUNTIF(event_name= "hasClickedBestPracticesAndStudies") AS total_best_practices_clicks,
+    COUNTIF(event_name= "hasClickedConsultSupport") AS total_consult_support_clicks,
+    COUNTIF(event_name= "hasClickedConsultCGU") AS total_consult_cgu_clicks,
 
 FROM filtered_events
 GROUP BY

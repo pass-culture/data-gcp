d103720c46aecda85ab3dfaf50fe5d359d5f429c
LucileRainteau <lucile.rainteau@passculture.app>
2024-06-27 10:49:06 +0200

    (DE-327) update qualtrics export for popularity index

--- orchestration/dags/dependencies/qualtrics/sql/clean/qualtrics_ir_jeunes.sql
+++ orchestration/dags/dependencies/qualtrics/sql/clean/qualtrics_ir_jeunes.sql
@@ -11,17 +11,22 @@ previous_export AS (
     SELECT 
         DISTINCT user_id 
     FROM  `{{ bigquery_clean_dataset }}.qualtrics_ir_jeunes`
-    WHERE calculation_month >= DATE_SUB(DATE("{{ current_month(ds) }}"), INTERVAL 1 MONTH)
+    WHERE calculation_month >= DATE_SUB(DATE("{{ current_month(ds) }}"), INTERVAL 6 MONTH)
 
 
 ),
+
+answers AS (
+    SELECT distinct user_id
+    FROM `{{ bigquery_analytics_dataset }}.qualtrics_answers` 
+),
+
 ir_export AS (
     SELECT
         user_data.user_id,
         user_data.user_current_deposit_type as deposit_type,
         user_data.user_civility,
         no_cancelled_booking,
-        user_data.user_department_code,
         user_data.user_region_name,
         actual_amount_spent,
         user_data.user_activity,
@@ -37,6 +42,7 @@ ir_export AS (
         LEFT JOIN `{{ bigquery_analytics_dataset }}.rural_city_type_data`  rural_city_type_data ON user_locations.city_code = rural_city_type_data.geo_code
         LEFT JOIN `{{ bigquery_raw_dataset }}.qualtrics_opt_out_users` opt_out on opt_out.ext_ref = user_data.user_id
         LEFT JOIN user_visits ON user_data.user_id = user_visits.user_id
+        LEFT JOIN answers ON user_data.user_id = answers.user_id
         WHERE 
             user_data.user_id is not null
         AND user_data.user_current_deposit_type in ("GRANT_15_17", "GRANT_18")
@@ -44,17 +50,16 @@ ir_export AS (
         AND user_data.user_is_active is true
         AND applicative_database_user.user_has_enabled_marketing_email is true
         AND opt_out.contact_id IS NULL
-    
+        AND answers.user_id IS NULL
 ),
 
 grant_15_17 as (
     SELECT 
         ir.*
-
     FROM ir_export ir
-    LEFT JOIN previous_export pe on pe.user_id = ir.user_id
-    
-    WHERE ir.deposit_type = "GRANT_15_17"
+    LEFT JOIN previous_export pe 
+    ON pe.user_id = ir.user_id
+    WHERE ir.deposit_type = "GRANT_15_7"
     AND pe.user_id IS NULL
     ORDER BY rand()
     LIMIT {{ params.volume }}
@@ -64,14 +69,13 @@ grant_18 as (
     SELECT 
         ir.*
     FROM ir_export ir
-    LEFT JOIN previous_export pe on pe.user_id = ir.user_id
+    LEFT JOIN previous_export pe 
+    ON pe.user_id = ir.user_id
     WHERE deposit_type = "GRANT_18"
     AND pe.user_id IS NULL
     ORDER BY rand()
     LIMIT {{ params.volume }}
 )
-
-
 
 SELECT  
     DATE("{{ current_month(ds) }}") as calculation_month,

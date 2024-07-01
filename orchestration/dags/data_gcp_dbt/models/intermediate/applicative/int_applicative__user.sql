26040a1d4212f5629b97a0432bb53f16f27c9df9
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-07-01 09:38:53 +0200

    feat: Add intermediate user iris mapping model

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__user.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__user.sql
@@ -6,10 +6,10 @@
 ) }}
 
 SELECT
-    user_id,
-    user_creation_date,
-    {{target_schema}}.humanize_id(user_id) AS user_humanized_id,
-    user_has_enabled_marketing_email,
+    u.user_id,
+    u.user_creation_date,
+    {{target_schema}}.humanize_id(u.user_id) AS user_humanized_id,
+    u.user_has_enabled_marketing_email,
     COALESCE(
     CASE
         WHEN u.user_postal_code = "97150" THEN "978"
@@ -23,24 +23,25 @@ SELECT
     ) AS user_department_code,
     u.user_postal_code,
     CASE
-        WHEN user_activity in ("Alternant", "Apprenti", "Volontaire") THEN "Apprenti, Alternant, Volontaire en service civique rémunéré"
-        WHEN user_activity in ("Inactif") THEN "Inactif (ni en emploi ni au chômage), En incapacité de travailler"
-        WHEN user_activity in ("Étudiant") THEN "Etudiant"
-        WHEN user_activity in ("Chômeur", "En recherche d'emploi ou chômeur") THEN "Chômeur, En recherche d'emploi"
-        ELSE user_activity
+        WHEN u.user_activity in ("Alternant", "Apprenti", "Volontaire") THEN "Apprenti, Alternant, Volontaire en service civique rémunéré"
+        WHEN u.user_activity in ("Inactif") THEN "Inactif (ni en emploi ni au chômage), En incapacité de travailler"
+        WHEN u.user_activity in ("Étudiant") THEN "Etudiant"
+        WHEN u.user_activity in ("Chômeur", "En recherche d'emploi ou chômeur") THEN "Chômeur, En recherche d'emploi"
+        ELSE u.user_activity
     END AS user_activity,
     CASE
-        WHEN user_civility in ("M", "M.") THEN "M."
-        WHEN user_civility = "Mme" THEN "Mme."
-        ELSE user_civility
+        WHEN u.user_civility in ("M", "M.") THEN "M."
+        WHEN u.user_civility = "Mme" THEN "Mme."
+        ELSE u.user_civility
     END AS user_civility,
-    user_school_type,
-    user_is_active,
-    user_age,
-    user_role,
-    user_birth_date,
-    user_cultural_survey_filled_date,
-    CASE WHEN user_role IN ("UNDERAGE_BENEFICIARY", "BENEFICIARY") THEN 1 ELSE 0 END AS is_beneficiary,
-    NULL AS user_iris_internal_id,
-    NULL AS user_region_name
+    u.user_school_type,
+    u.user_is_active,
+    u.user_age,
+    u.user_role,
+    u.user_birth_date,
+    u.user_cultural_survey_filled_date,
+    CASE WHEN u.user_role IN ("UNDERAGE_BENEFICIARY", "BENEFICIARY") THEN 1 ELSE 0 END AS is_beneficiary,
+    ui.user_iris_internal_id,
+    ui.user_region_name
 FROM {{ source("raw", "applicative_database_user") }} AS u
+LEFT JOIN {{ ref("int_api_gouv__user_address_location") }} AS ui ON ui.user_id = u.user_id

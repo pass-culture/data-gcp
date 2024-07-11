0eea1548d8cf7311f5fdfda0ffc1791b4c0d7991
valoumiaou <valentin.busson@passculture.app>
2024-07-11 15:46:20 +0200

    refacto macro

--- orchestration/dags/data_gcp_dbt/macros/custom_incremental_config.sql
+++ orchestration/dags/data_gcp_dbt/macros/custom_incremental_config.sql
@@ -5,26 +5,17 @@
     cluster_by=None
 ) %}
     {% if target.profile_name == 'CI' %}
-        {% set materialization = 'view' %}
+        {% set config_params = {'materialized': 'view'} %}
     {% else %}
-        {% set materialization = 'incremental' %}
-    {% endif %}
-
-    {% set config_params = {
-        'materialized': materialization,
-    } %}
-
-    {% if materialization == 'incremental' %}
-        {% do config_params.update({
+        {% set config_params = {
+            'materialized': 'incremental',
             'incremental_strategy': incremental_strategy,
             'partition_by': partition_by,
             'on_schema_change': on_schema_change
-        }) %}
+        } %}
+        {% if cluster_by is not none %}
+            {% do config_params.update({'cluster_by': cluster_by}) %}
+        {% endif %}
     {% endif %}
-
-    {% if cluster_by is not none %}
-        {% do config_params.update({'cluster_by': cluster_by}) %}
-    {% endif %}
-
     {{ return(config_params) }}
 {% endmacro %}

2a825aa5f9ad0feb14ca02cbda5b47eb99c928ca
dcuesta-pass <82374920+dcuesta-pass@users.noreply.github.com>
2024-07-11 17:27:11 +0200

    [PR-DE-386] feat: Add product_extra_data column to product.sql and up… (#2764)
    * [PR-DE-386] feat: Add product_extra_data column to product.sql and update item_group_by_product_id in offer_item_ids.sql
    
    * [PR-DE-386] feat: Add product_extra_data column to product.sql and update item_group_by_product_id in offer_item_ids.sql

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__extract_offer.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__extract_offer.sql
@@ -1,28 +1,37 @@
-WITH extracted_offers AS (
-SELECT *,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.author"), " ")) AS author,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.performer"), " ")) AS performer,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.musicType"), " ")) AS musicType,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.musicSubtype"), " ")) AS musicSubtype,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.stageDirector"), " ")) AS stageDirector,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.showType"), " ")) AS showType,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.showSubType"), " ")) AS showSubType,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.speaker"), " ")) AS speaker,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.rayon"), " ")) AS rayon,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.theater.allocine_movie_id"), " ")) AS theater_movie_id,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.theater.allocine_room_id"), " ")) AS theater_room_id,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.type"), " ")) AS movie_type,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.visa"), " ")) AS visa,
-LOWER(TRIM(JSON_EXTRACT_SCALAR(offer_extra_data, "$.releaseDate"), " ")) AS releaseDate,
-LOWER(TRIM(JSON_EXTRACT(offer_extra_data, "$.genres"), " ")) AS genres,
-LOWER(TRIM(JSON_EXTRACT(offer_extra_data, "$.companies"), " ")) AS companies,
-LOWER(TRIM(JSON_EXTRACT(offer_extra_data, "$.countries"), " ")) AS countries,
-LOWER(TRIM(JSON_EXTRACT(offer_extra_data, "$.cast"), " ")) AS casting,
-LOWER(TRIM(TRIM(JSON_EXTRACT(offer_extra_data, "$.isbn")," "),'"')) AS isbn,
-LOWER(TRIM(TRIM(JSON_EXTRACT(offer_extra_data, "$.ean"), " "),'"')) AS ean,
-LOWER(TRIM(TRIM(JSON_EXTRACT(offer_extra_data, "$.editeur")," "),'"')) AS book_editor,
-LOWER(TRIM(TRIM(JSON_EXTRACT(offer_extra_data, "$.gtl_id")," "),'"')) AS titelive_gtl_id
-FROM {{ source('raw', 'applicative_database_offer') }}
+WITH extra_data as (
+  SELECT 
+  offer.offer_id,
+  COALESCE(product.product_extra_data,offer.offer_extra_data) as extra_data
+  FROM {{ source('raw', 'applicative_database_offer') }} AS offer
+  LEFT JOIN {{ ref('int_applicative__product') }}  AS product on CAST(product.id as string)=offer.offer_product_id
+),
+extracted_offers AS (
+SELECT 
+offer.*,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.author"), " ")) AS author,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.performer"), " ")) AS performer,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.musicType"), " ")) AS musicType,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.musicSubtype"), " ")) AS musicSubtype,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.stageDirector"), " ")) AS stageDirector,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.showType"), " ")) AS showType,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.showSubType"), " ")) AS showSubType,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.speaker"), " ")) AS speaker,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.rayon"), " ")) AS rayon,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.theater.allocine_movie_id"), " ")) AS theater_movie_id,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.theater.allocine_room_id"), " ")) AS theater_room_id,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.type"), " ")) AS movie_type,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.visa"), " ")) AS visa,
+LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.releaseDate"), " ")) AS releaseDate,
+LOWER(TRIM(JSON_EXTRACT(extra_data, "$.genres"), " ")) AS genres,
+LOWER(TRIM(JSON_EXTRACT(extra_data, "$.companies"), " ")) AS companies,
+LOWER(TRIM(JSON_EXTRACT(extra_data, "$.countries"), " ")) AS countries,
+LOWER(TRIM(JSON_EXTRACT(extra_data, "$.cast"), " ")) AS casting,
+LOWER(TRIM(TRIM(JSON_EXTRACT(extra_data, "$.isbn")," "),'"')) AS isbn,
+LOWER(TRIM(TRIM(JSON_EXTRACT(extra_data, "$.ean"), " "),'"')) AS ean,
+LOWER(TRIM(TRIM(JSON_EXTRACT(extra_data, "$.editeur")," "),'"')) AS book_editor,
+LOWER(TRIM(TRIM(JSON_EXTRACT(extra_data, "$.gtl_id")," "),'"')) AS titelive_gtl_id
+FROM {{ source('raw', 'applicative_database_offer') }} offer
+LEFT JOIN extra_data AS ued on ued.offer_id=offer.offer_id
 )
 SELECT
     * EXCEPT(isbn, titelive_gtl_id),

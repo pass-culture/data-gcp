837ea6732289261040c704477fbd36dea0870443
Jules-Arbelot <115459312+Jules-Arbelot@users.noreply.github.com>
2024-07-11 16:48:27 +0200

    Update orchestration/dags/data_gcp_dbt/models/mart/native/mrt_native__daily_video_module_conversion.sql
    Co-authored-by: Caroline Mestre <162314477+carolinemestre-passculture@users.noreply.github.com>

--- orchestration/dags/data_gcp_dbt/models/mart/native/mrt_native__daily_video_module_conversion.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/native/mrt_native__daily_video_module_conversion.sql
@@ -17,7 +17,7 @@ SELECT
     , COUNT(DISTINCT CASE WHEN offers_consulted > 0 THEN unique_session_id ELSE NULL END) AS total_sessions_with_consult_offer
     , COUNT(DISTINCT CASE WHEN total_homes_consulted > 0 THEN unique_session_id ELSE NULL END) AS total_session_consult_home
     , COUNT(DISTINCT CASE WHEN total_videos_all_seen > 0 THEN unique_session_id ELSE NULL END) AS total_session_seen_all_video
-    , SUM(offers_consulted) AS total_offers_consulted
+    , SUM(offers_consulted) AS total_consulted_offers
     , SUM(total_video_seen_duration_seconds) AS total_video_seen_duration_seconds
     , COUNT(DISTINCT CASE WHEN pct_video_seen >= 0.25 THEN unique_session_id ELSE NULL END) AS total_seen_25_pct_video
     , COUNT(DISTINCT CASE WHEN pct_video_seen >= 0.5 THEN unique_session_id ELSE NULL END) AS total_seen_50_pct_video

eb94749cee0030596bc86c55010d6259c0362b19
valoumiaou <valentin.busson@passculture.app>
2024-07-11 09:36:20 +0200

    add booking snapshot

--- orchestration/dags/data_gcp_dbt/snapshots/applicative_history/booking_snapshot.sql
+++ orchestration/dags/data_gcp_dbt/snapshots/applicative_history/booking_snapshot.sql
@@ -1,17 +1,31 @@
-{% snapshot booking_snapshot %}
+{% snapshot booking_history %}
 
 {{
     config(
-      target_schema='snapshots_applicative',
       strategy='check',
       unique_key='booking_id',
-      check_cols=['booking_status']
+      check_cols=['booking_creation_date', 'stock_id', 'booking_quantity', 'user_id', 'booking_amount', 'booking_status', 'booking_is_cancelled', 'booking_is_used', 'booking_used_date', 'booking_cancellation_date', 'booking_cancellation_reason', 'booking_reimbursement_date']
     )
 }}
-    
 
-select * from {{ source('raw', 'applicative_database_booking') }}
+with bookings as (
+  select
+    booking_id,
+    DATE(booking_creation_date) AS booking_creation_date,
+    stock_id,
+    booking_quantity,
+    user_id,
+    booking_amount,
+    booking_status,
+    booking_is_cancelled,
+    booking_is_used,
+    booking_used_date,
+    booking_cancellation_date,
+    booking_cancellation_reason,
+    booking_reimbursement_date
+  from {{ source('raw', 'applicative_database_booking') }}
+)
+
+select * from bookings
 
 {% endsnapshot %}
-
-

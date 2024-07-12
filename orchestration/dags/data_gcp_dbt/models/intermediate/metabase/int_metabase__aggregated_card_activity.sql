fb6426be91621c1fe6ad809d656738779e283c10
cdelabre <cyrille.delabre@passculture.app>
2024-07-12 15:57:14 +0200

    (dbt) feat: improve cost queries

--- orchestration/dags/data_gcp_dbt/models/intermediate/metabase/int_metabase__aggregated_card_activity.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/metabase/int_metabase__aggregated_card_activity.sql
@@ -1,5 +1,19 @@
+WITH last_edition_card AS (
+    SELECT 
+        mr.timestamp,
+        mr.user_id, 
+        mu.email,
+        mr.model_id as card_id
+    FROM {{ source("raw", "metabase_revision") }} mr
+    LEFT JOIN {{ ref('int_metabase__user') }} as mu on mu.user_id = mr.user_id
+    WHERE model = "Card"
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY card_id ORDER BY timestamp DESC) = 1
+)
+
 SELECT 
-    card_id
+    eq.card_id
+    , lec.email as last_editor_email
+    , DATE(lec.timestamp) as last_edition_date
     , sum(running_time) as total_running_time
     , sum(result_rows) as total_result_rows
     , avg(running_time) as avg_running_time
@@ -9,6 +23,8 @@ SELECT
     , count(distinct dashboard_id) as nbr_dashboards
     , max(execution_date) as last_execution_date
     , sum(case when error is null then 0 else 1 end) as total_errors
-FROM  {{ ref('int_metabase__daily_query') }} as execution_query
-GROUP BY 
-    card_id
+FROM  {{ ref('int_metabase__daily_query') }} as eq
+LEFT JOIN last_edition_card as lec on lec.card_id = eq.card_id
+WHERE NOT cache_hit
+
+GROUP BY 1,2,3
--- orchestration/dags/data_gcp_dbt/models/mart/monitoring/mrt_monitoring__bigquery_cost.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/monitoring/mrt_monitoring__bigquery_cost.sql
@@ -6,16 +6,21 @@ SELECT
     WHEN mu.email is not null THEN "Metabase"
     WHEN gbc.user_email like "%matabase%" THEN "Metabase"
     WHEN gbc.user_email like "%composer%" THEN "Composer" 
-    ELSE "Bigquery" 
+    ELSE "Bigquery (adhoc)" 
     END 
   as origin,
   gbc.dataset_id,
   gbc.table_id,
   mc.card_name,
   mc.dashboard_name,
+  mc.card_id,  
+  mc.card_id_last_editor_email,
+  mc.card_id_last_edition_date,
+  mc.dashboard_id,
   sum(gbc.cost_usd) as cost_usd,
-  sum(gbc.total_bytes) / POW(10, 9) as total_gigabytes,
-  sum(gbc.total_bytes)  as total_bytes,
+  sum(gbc.cost_euro) as cost_euro, 
+  sum(gbc.total_bytes_billed) / POW(10, 9) as total_gigabytes,
+  sum(gbc.total_bytes_billed)  as total_bytes,
   sum(gbc.total_queries) as total_queries
 FROM {{ ref("int_gcp__bigquery_cost") }} gbc 
 LEFT JOIN {{ ref("int_metabase__user")}} mu on mu.user_id = gbc.metabase_user_id
@@ -25,4 +30,4 @@ LEFT JOIN {{ ref("mrt_monitoring__metabase_cost") }} mc on
   and mc.metabase_user_id = gbc.metabase_user_id
 
 WHERE NOT cache_hit
-GROUP BY 1,2,3,4,5,6,7,8
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12

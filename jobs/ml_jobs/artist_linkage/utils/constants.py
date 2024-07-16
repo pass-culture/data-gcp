4ef77c2a90228ad862d2571c86f199bb32c233c5
LaurentM Pass <laurent.montier@passculture.app>
2024-07-16 12:40:08 +0200

    (feat): Log metrics and artifacts in mlflow

--- jobs/ml_jobs/artist_linkage/utils/constants.py
+++ jobs/ml_jobs/artist_linkage/utils/constants.py
@@ -1,6 +1,10 @@
 import os
 
 GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "passculture-data-ehp")
+ENV_SHORT_NAME = os.environ.get("ENV_SHORT_NAME", "dev")
+
 MLFLOW_EHP_URI = "https://mlflow.staging.passculture.team/"
 MLFLOW_PROD_URI = "https://mlflow.passculture.team/"
 MLFLOW_SECRET_NAME = "mlflow_client_id"
+
+EXPERIMENT_BASE_NAME = "artist_linkage"
--- jobs/ml_jobs/artist_linkage/utils/mlflow.py
+++ jobs/ml_jobs/artist_linkage/utils/mlflow.py
@@ -1,9 +1,11 @@
 import os
+from typing import Literal
 
 import mlflow
 from google.auth.transport.requests import Request
 from google.cloud import secretmanager
 from google.oauth2 import id_token
+from mlflow.entities import Experiment
 
 from utils.constants import (
     GCP_PROJECT_ID,
@@ -20,13 +22,15 @@ def get_mlflow_client_id() -> str:
     return response.payload.data.decode("UTF-8")
 
 
-def connect_remote_mlflow(client_id, env="ehp"):
+def connect_remote_mlflow(
+    client_id: str, mlflow_env: Literal["ehp", "prod"] = "ehp"
+) -> None:
     os.environ["MLFLOW_TRACKING_TOKEN"] = id_token.fetch_id_token(Request(), client_id)
-    uri = MLFLOW_PROD_URI if env == "prod" else MLFLOW_EHP_URI
+    uri = MLFLOW_PROD_URI if mlflow_env == "prod" else MLFLOW_EHP_URI
     mlflow.set_tracking_uri(uri)
 
 
-def get_mlflow_experiment(experiment_name: str):
+def get_mlflow_experiment(experiment_name: str) -> Experiment:
     experiment = mlflow.get_experiment_by_name(experiment_name)
     if experiment is None:
         mlflow.create_experiment(name=experiment_name)

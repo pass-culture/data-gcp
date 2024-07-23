f5655ac5e8891ad105d0567ee6dc96260274673c
valoumiaou <valentin.busson@passculture.app>
2024-07-23 14:17:30 +0200

    add collective snapshots

---
+++ orchestration/dags/data_gcp_dbt/snapshots/applicative_history/collective_booking_history.sql
@@ -0,0 +1,30 @@
+{% snapshot collective_booking_history %}
+
+{{
+    config(
+      strategy='check',
+      unique_key='collective_booking_id',
+      check_cols=['collective_booking_creation_date', 'collective_booking_used_date', 'collective_stock_id', 'venue_id', 'offerer_id', 'collective_booking_cancellation_date', 'collective_booking_cancellation_limit_date', 'collective_booking_cancellation_reason', 'collective_booking_status', 'collective_booking_reimbursement_date', 'educational_institution_id', 'educational_year_id', 'collective_booking_confirmation_date', 'collective_booking_confirmation_limit_date', 'educational_redactor_id']
+    )
+}}
+
+SELECT
+	collective_booking_id,
+	collective_booking_creation_date,
+	collective_booking_used_date,
+	collective_stock_id,
+	venue_id,
+	offerer_id,
+	collective_booking_cancellation_date,
+	collective_booking_cancellation_limit_date,
+	collective_booking_cancellation_reason,
+	collective_booking_status,
+	collective_booking_reimbursement_date,
+	educational_institution_id,
+	educational_year_id,
+	collective_booking_confirmation_date,
+	collective_booking_confirmation_limit_date,
+	educational_redactor_id
+FROM {{ source('raw', 'applicative_database_collective_booking') }}
+
+{% endsnapshot %}
---
+++ orchestration/dags/data_gcp_dbt/snapshots/applicative_history/collective_offer_template_history.sql
@@ -0,0 +1,31 @@
+{% snapshot collective_offer_template_history %}
+
+{{
+    config(
+      strategy='timestamp',
+      unique_key='collective_offer_id',
+      updated_at='collective_offer_date_updated'
+    )
+}}
+with formated_offer_template as (
+    SELECT
+    	collective_offer_last_validation_date,
+    	collective_offer_validation,
+    	collective_offer_id,
+    	collective_offer_is_active,
+    	venue_id,
+    	collective_offer_name,
+    	collective_offer_creation_date,
+    	collective_offer_subcategory_id,
+    	cast(collective_offer_date_updated as timestamp) as collective_offer_date_updated,
+    	collective_offer_students,
+    	collective_offer_booking_email,
+    	collective_offer_offer_venue,
+    	collective_offer_last_validation_type,
+    	educational_domains,
+    	collective_offer_venue_address_type
+    FROM {{ source('raw','collective_offer_template') }}
+)
+select * from formated_offer_template
+
+{% endsnapshot %}

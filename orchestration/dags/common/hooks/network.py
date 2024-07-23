f3c8284cad4602bf07dc964315952bb9dc416351
cdelabre <cyrille.delabre@passculture.app>
2024-07-23 17:38:10 +0200

    [DE-260](clickhouse) :feat add gke network for ehp clusters logic

---
+++ orchestration/dags/common/hooks/network.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from common.config import (
+    GCE_SUBNETWORK_ID,
+    GCE_NETWORK_ID,
+    GCE_VPC_SHARED_HOST_SUBNETWORK_ID,
+    GCE_VPC_SHARED_HOST_NETWORK_ID,
+    ENV_SHORT_NAME,
+)
+
+
+@dataclass
+class DefaultVPCNetwork:
+    network_id: str = GCE_NETWORK_ID
+    subnetwork_id: str = GCE_SUBNETWORK_ID
+
+
+DEFAULT_GKE_NETWORK = [DefaultVPCNetwork()]
+
+# This is needed only for ehp environment.
+if ENV_SHORT_NAME != "prod":
+    DEFAULT_GKE_NETWORK.append(
+        DefaultVPCNetwork(
+            network_id=GCE_VPC_SHARED_HOST_NETWORK_ID,
+            subnetwork_id=GCE_VPC_SHARED_HOST_SUBNETWORK_ID,
+        )
+    )
+NETWORK_TYPE = {
+    "GKE": DEFAULT_GKE_NETWORK,
+    "GCE": [DefaultVPCNetwork()],
+}

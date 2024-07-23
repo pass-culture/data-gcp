22cfa12d0968abb544ee93ecaca4fceb18e53573
valoumiaou <valentin.busson@passculture.app>
2024-07-23 14:17:08 +0200

    remove useless formating for booking

--- orchestration/dags/data_gcp_dbt/snapshots/applicative_history/booking_history.sql
+++ orchestration/dags/data_gcp_dbt/snapshots/applicative_history/booking_history.sql
@@ -8,24 +8,22 @@
     )
 }}
 
-with bookings as (
-  select
-    booking_id,
-    DATE(booking_creation_date) AS booking_creation_date,
-    stock_id,
-    booking_quantity,
-    user_id,
-    booking_amount,
-    booking_status,
-    booking_is_cancelled,
-    booking_is_used,
-    booking_used_date,
-    booking_cancellation_date,
-    booking_cancellation_reason,
-    booking_reimbursement_date
-  from {{ source('raw', 'applicative_database_booking') }}
-)
 
-select * from bookings
+select
+  booking_id,
+  booking_creation_date,
+  stock_id,
+  booking_quantity,
+  user_id,
+  booking_amount,
+  booking_status,
+  booking_is_cancelled,
+  booking_is_used,
+  booking_used_date,
+  booking_cancellation_date,
+  booking_cancellation_reason,
+  booking_reimbursement_date
+from {{ source('raw', 'applicative_database_booking') }}
+
 
 {% endsnapshot %}

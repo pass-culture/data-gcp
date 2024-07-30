ebfdecaaa64bbc8985806cfcc867813ce4f0beee
valoumiaou <valentin.busson@passculture.app>
2024-07-30 15:51:40 +0200

    create custom macro materialization table such that CI materialization config is not overwritten

---
+++ orchestration/dags/data_gcp_dbt/macros/custom_table_config.sql
@@ -0,0 +1,11 @@
+{% macro custom_table_config() %}
+    {% if target.profile_name == 'CI' %}
+        {% set config_params = {'materialized': 'view'} %}
+    {% else %}
+        {% set config_params = {
+            'materialized': 'table'
+        } %}
+        {% endif %}
+    {% endif %}
+    {{ return(config_params) }}
+{% endmacro %}

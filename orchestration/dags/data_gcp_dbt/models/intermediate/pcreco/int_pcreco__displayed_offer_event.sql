885a74b500c3f16316d21ae47844d9011b49c485
cdelabre <cyrille.delabre@passculture.app>
2024-08-02 15:17:54 +0200

    (ranking) feat: add display order logic

--- orchestration/dags/data_gcp_dbt/models/intermediate/pcreco/int_pcreco__displayed_offer_event.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/pcreco/int_pcreco__displayed_offer_event.sql
@@ -31,15 +31,41 @@ with displayed as (
             "PlaylistVerticalScroll"
         )
     group by reco_call_id, event_date
+),
+
+interaction as (
+    select
+        reco_call_id,
+        event_date,
+        offer_id,
+        max(is_consult_offer) as is_consult_offer,
+        max(is_booking_confirmation) as is_booking_confirmation,
+        max(is_add_to_favorites) as is_add_to_favorites
+    from
+        {{ ref('int_firebase__native_event') }} fsoe
+    where
+        {% if is_incremental() %}
+            event_date between date_sub(date('{{ ds() }}'), interval 3 day) and date('{{ ds() }}') and
+        {% endif %}
+        reco_call_id is not null
+        and event_name in (
+            "ConsultOffer",
+            "BookingConfirmation"
+        )
+    group by reco_call_id, event_date, offer_id
 )
 
 select
     et.*,
     d.total_module_consult_offer,
     d.total_module_booking_confirmation,
-    d.total_module_add_to_favorites
+    d.total_module_add_to_favorites,
+    coalesce(i.is_consult_offer, 0) as is_consult_offer,
+    coalesce(i.is_booking_confirmation,  0) as is_booking_confirmation,
+    coalesce(i.is_add_to_favorites,  0) as is_add_to_favorites
 from {{ ref('int_pcreco__past_offer_context') }} et
-    inner join displayed d on d.event_date = et.event_date and d.reco_call_id = et.reco_call_id
+inner join displayed d on d.event_date = et.event_date and d.reco_call_id = et.reco_call_id
+left join interaction i on i.event_date = et.event_date and i.reco_call_id = et.reco_call_id and i.offer_id = et.offer_id
 {% if is_incremental() %}
     where et.event_date between date_sub(date('{{ ds() }}'), interval 3 day) and date('{{ ds() }}')
 {% endif %}

0fbc5119db7c48b753f139c77a4c278c2e9a2370
Jules-Arbelot <115459312+Jules-Arbelot@users.noreply.github.com>
2024-08-09 12:17:35 +0200

    feat: Update criterion tables and create mrt_global__offer_criterion

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__offer_criterion.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__offer_criterion.sql
@@ -1,8 +1,14 @@
 select
     adc.name as tag_name,
     adc.id as criterion_id,
+    adc.description,
+    adcc.criterion_category_label,
+    DATE(adc.startDateTime) as criterion_beginning_date,
+    DATE(adc.endDateTime) as criterion_ending_date,
     adoc.offerid as offer_id,
     ado.offer_name as offer_name
 from {{ source("raw", "applicative_database_criterion") }} adc
     left join {{ source("raw", "applicative_database_offer_criterion") }} adoc on adoc.criterionid = adc.id
     left join {{ source("raw", "applicative_database_offer") }} ado on ado.offer_id = adoc.offerid
+    left join {{ source("raw", "applicative_database_criterion_category_mapping") }} adccm on adccm.criterion_id = adc.id 
+    left join {{ source("raw", "applicative_database_criterion_category") }} adcc on adcc.criterion_category_id = adccm.criterion_category_id

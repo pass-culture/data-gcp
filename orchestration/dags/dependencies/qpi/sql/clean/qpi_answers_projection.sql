9d4aae798ff419ee1531e4d84d9b460bf8076441
cdelabre <cyrille.delabre@passculture.app>
2024-08-13 18:02:41 +0200

    [HOTFIX](qpi) fix: update query as seed format changed

--- orchestration/dags/dependencies/qpi/sql/clean/qpi_answers_projection.sql
+++ orchestration/dags/dependencies/qpi/sql/clean/qpi_answers_projection.sql
@@ -1,10 +1,10 @@
 with base as (
     SELECT
-        * 
+        *
     FROM (select * from `{{ bigquery_raw_dataset }}.qpi_answers_v4`) as qpi, qpi.answers as answers
 ),
 unnested_base as (
-    SELECT 
+    SELECT
         user_id
         , submitted_at
         , unnested AS answer_ids
@@ -12,39 +12,36 @@ unnested_base as (
     CROSS JOIN UNNEST(base.answer_ids) AS unnested
 ),
 user_subcat as (
-    select 
+    select
         b.user_id
         , submitted_at
-        , map.subcategories 
-    from unnested_base b 
+        , map.subcategories
+    from unnested_base b
     JOIN `{{ bigquery_seed_dataset }}.qpi_mapping` map
     ON b.answer_ids = map.answer_id
     WHERE b.answer_ids like 'PROJECTION_%'
     order by user_id
 ),
 clean as (
-    select 
+    select
         user_id
         , submitted_at
-        , unnested as subcategories
+        , unnested as subcategories.element
     from user_subcat
-    CROSS JOIN UNNEST(user_subcat.subcategories) AS unnested
-), 
+    CROSS JOIN UNNEST(user_subcat.subcategories.list) AS unnested
+),
 base_deduplicate as (
-    select 
+    select
         user_id
         , submitted_at
         , subcategories
         , ROW_NUMBER() OVER (PARTITION BY user_id, subcategories order by subcategories DESC) row_number
-    FROM clean 
+    FROM clean
     order by user_id
 )
-SELECT 
-    * except(row_number) 
-FROM base_deduplicate 
+SELECT
+    * except(row_number)
+FROM base_deduplicate
 WHERE row_number = 1
-AND subcategories is not null 
+AND subcategories is not null
 AND subcategories <> ""
-
-
-
--- orchestration/dags/dependencies/qpi/sql/clean/qpi_answers_v4_clean.sql
+++ orchestration/dags/dependencies/qpi/sql/clean/qpi_answers_v4_clean.sql
@@ -1,5 +1,5 @@
 with raw_answers as (
-    SELECT 
+    SELECT
         raw_answers.user_id
         , submitted_at
         , answers
@@ -9,12 +9,12 @@ with raw_answers as (
 
 base as (
     SELECT
-        * 
+        *
     FROM (select * from raw_answers) as qpi, qpi.answers as answers
 ),
 
 unnested_base as (
-    SELECT 
+    SELECT
         user_id
         , submitted_at
         , unnested AS answer_ids
@@ -23,39 +23,36 @@ unnested_base as (
 ),
 
 user_subcat as (
-    select 
+    select
         b.user_id
         , b.submitted_at
-        , map.subcategories 
-    from unnested_base b 
+        , map.subcategories
+    from unnested_base b
     JOIN `{{ bigquery_seed_dataset }}.qpi_mapping` map
     ON b.answer_ids = map.answer_id
     WHERE b.answer_ids NOT like 'PROJECTION_%'
     order by user_id
 ),
 clean as (
-    select 
+    select
         user_id
         , submitted_at
-        , unnested as subcategories
+        , unnested.element as subcategories
     from user_subcat
-    CROSS JOIN UNNEST(user_subcat.subcategories) AS unnested
-), 
+    CROSS JOIN UNNEST(user_subcat.subcategories.list) AS unnested
+),
 base_deduplicate as (
-    select 
+    select
         user_id
         , submitted_at
         , subcategories
         , ROW_NUMBER() OVER (PARTITION BY user_id, subcategories order by subcategories DESC) row_number
-    FROM clean 
+    FROM clean
     order by user_id
 )
-SELECT 
-    * except(row_number) 
-FROM base_deduplicate 
+SELECT
+    * except(row_number)
+FROM base_deduplicate
 WHERE row_number = 1
-AND subcategories is not null 
+AND subcategories is not null
 AND subcategories <> ""
-
-
-

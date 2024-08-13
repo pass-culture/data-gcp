6b49231faf82775fb5a7c6a891291fa997c97aab
LaurentM Pass <laurent.montier@passculture.app>
2024-08-13 18:11:45 +0200

    lint: lint manually _template

--- jobs/ml_jobs/_template/main.py
+++ jobs/ml_jobs/_template/main.py
@@ -1,6 +1,5 @@
 import typer
-
-from utils import read_parquet, upload_parquet
+from pandas import pd
 
 app = typer.Typer()
 
@@ -9,15 +8,12 @@ app = typer.Typer()
 def main(
     source_file_path: str = typer.Option(), output_file_path: str = typer.Option()
 ) -> None:
-    source_df = read_parquet(source_file_path)
+    source_df = pd.read_parquet(source_file_path)
 
     # Your code here
     output_df = source_df
 
-    upload_parquet(
-        dataframe=output_df,
-        gcs_path=output_file_path,
-    )
+    output_df.to_parquet(output_file_path, index=False)
 
 
 if __name__ == "__main__":

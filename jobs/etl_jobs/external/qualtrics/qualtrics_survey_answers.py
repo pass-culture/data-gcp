3a6af7e7d203bdbfc0a61c8c07b40d712cfddaaa
LaurentM Pass <laurent.montier@passculture.app>
2024-08-20 10:06:21 +0200

    fix: fix condition

--- jobs/etl_jobs/external/qualtrics/qualtrics_survey_answers.py
+++ jobs/etl_jobs/external/qualtrics/qualtrics_survey_answers.py
@@ -34,7 +34,7 @@ def import_survey_metadata(data_center, api_token):
     if response.json()["meta"]["httpStatus"] == "200 - OK":
         surveys = pd.DataFrame(response.json()["result"]["elements"])
         surveys.to_gbq(f"clean_{ENV_SHORT_NAME}.qualtrics_survey", if_exists="replace")
-        active_surveys = list(surveys[surveys.isActive is True].id)
+        active_surveys = surveys.loc[lambda df: df.isActive].id.tolist()
         return active_surveys
 
 

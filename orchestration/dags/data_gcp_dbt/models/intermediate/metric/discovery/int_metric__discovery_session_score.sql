b53b3fb64d166d6bf4211ef4533e1879816d6bc2
Jules-Arbelot <115459312+Jules-Arbelot@users.noreply.github.com>
2024-08-27 12:49:54 +0200

    fix

--- orchestration/dags/data_gcp_dbt/models/intermediate/metric/discovery/int_metric__discovery_session_score.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/metric/discovery/int_metric__discovery_session_score.sql
@@ -14,12 +14,12 @@ SELECT
     COUNT(CASE WHEN score.type = "item" THEN score.consultation_id END) AS item_discovery_score,
     COUNT(CASE WHEN score.type = "offer_category" THEN score.consultation_id END) AS offer_category_discovery_score,
     COUNT(CASE WHEN score.type = "offer_subcategory" THEN score.consultation_id END) AS offer_subcategory_discovery_score
-FROM int_metric_prod.discovery_score score
-LEFT JOIN int_firebase_prod.consultation consult on score.consultation_id = consult.consultation_id
+FROM {{ ref('int_metric__discovery_score')}} score
+LEFT JOIN {{ ref('int_firebase__native_consultation')}} consult on score.consultation_id = consult.consultation_id
 WHERE unique_session_id IS NOT NULL
 {% if is_incremental() %}
 AND consultation_date >= date_sub('{{ ds() }}', INTERVAL 3 day)
 {% endif %}
 GROUP BY consult.unique_session_id,
     consult.user_id,
-    consult.consultation_date,
+    consult.consultation_date

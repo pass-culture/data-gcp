4cfa481f96286aa43550680e4425ae8786fc82f6
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-08-30 11:19:55 +0200

    fix: Update config_enriched.py

--- orchestration/dags/dependencies/great_expectations/config_enriched.py
+++ orchestration/dags/dependencies/great_expectations/config_enriched.py
@@ -2,15 +2,6 @@ from common.config import BIGQUERY_ANALYTICS_DATASET
 from dependencies.great_expectations.utils import last_week, today, yesterday
 
 enriched_tables_test_config = {
-    "enriched_institution_data": {
-        "dataset_name": BIGQUERY_ANALYTICS_DATASET,
-        "date_field": "last_booking_date",
-        "freshness_check": {
-            "dev": [last_week, today],
-            "stg": [yesterday, today],
-            "prod": [yesterday, today],
-        },
-    },
     "enriched_suivi_dms_adage": {
         "dataset_name": BIGQUERY_ANALYTICS_DATASET,
         "date_field": "processed_at",

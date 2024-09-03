81a7b28749b96c02341e96b58ae83684457c7973
Caroline Mestre <162314477+carolinemestre-passculture@users.noreply.github.com>
2024-09-03 11:58:27 +0200

    Migrate offer_extracted_data and isbn_rayon_editor (#3076)
    * fix: Remove old models offer_extracted_data and isbn_rayon_editor
    
    * feat: Update offer_extracted_data model dependencies
    
    * feat: Update offer_extracted_data model dependencies

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__isbn_rayon_editor.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__isbn_rayon_editor.sql
@@ -3,11 +3,12 @@ with matching_isbn_with_rayon as (
         isbn,
         rayon,
         ROW_NUMBER() over (partition by isbn order by COUNT(distinct offer_id) desc) as rank_rayon
-    from {{ ref('offer_extracted_data') }}
+    from {{ ref('int_applicative__extract_offer') }}
     where offer_subcategoryid in ('LIVRE_PAPIER', 'LIVRE_NUMERIQUE', 'LIVRE_AUDIO_PHYSIQUE')
         and rayon is not NULL
         and isbn is not NULL
-    group by 1, 2
+    group by isbn,
+        rayon
     qualify ROW_NUMBER() over (partition by isbn order by COUNT(distinct offer_id) desc) = 1
 ),
 
@@ -16,11 +17,12 @@ matching_isbn_with_editor as (
         isbn,
         book_editor,
         ROW_NUMBER() over (partition by isbn order by COUNT(distinct offer_id) desc) as rank_editor
-    from {{ ref('offer_extracted_data') }}
+    from {{ ref('int_applicative__extract_offer') }}
     where offer_subcategoryid in ('LIVRE_PAPIER', 'LIVRE_NUMERIQUE', 'LIVRE_AUDIO_PHYSIQUE')
         and book_editor is not NULL
         and isbn is not NULL
-    group by 1, 2
+    group by isbn,
+        book_editor
     qualify ROW_NUMBER() over (partition by isbn order by COUNT(distinct offer_id) desc) = 1
 )
 

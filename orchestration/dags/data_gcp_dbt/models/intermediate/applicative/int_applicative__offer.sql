17b54c61d30fba1e0df60e6f134a09ba85e3100d
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-09-03 11:48:22 +0200

    feat: Update offer_extracted_data model dependencies

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__offer.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__offer.sql
@@ -153,7 +153,7 @@ from {{ ref("int_applicative__extract_offer") }} as o
     left join stocks_grouped_by_offers as so on so.offer_id = o.offer_id
     left join total_favorites as tf on tf.offerid = o.offer_id
     left join {{ source("clean","subcategories") }} as subcategories on o.offer_subcategoryid = subcategories.id
-    left join {{ ref("int_applicative__isbn_rayon_editor") }} as isbn_rayon_editor on o.isbn = int_applicative__isbn_rayon_editor.isbn
+    left join {{ ref("int_applicative__isbn_rayon_editor") }} as isbn_rayon_editor on o.isbn = isbn_rayon_editor.isbn
     left join
         {{ ref("int_applicative__mediation") }}
             as m

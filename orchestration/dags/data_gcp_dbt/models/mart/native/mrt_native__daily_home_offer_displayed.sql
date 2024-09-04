de8285401b01470083cb28b1f3b337160ba90c87
Jules-Arbelot <115459312+Jules-Arbelot@users.noreply.github.com>
2024-09-04 10:09:07 +0200

    fix: update bucket computation (#3075)
    Co-authored-by: carolinemestre-passculture <caroline.mestre@passculture.app>

--- orchestration/dags/data_gcp_dbt/models/mart/native/mrt_native__daily_home_offer_displayed.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/native/mrt_native__daily_home_offer_displayed.sql
@@ -2,7 +2,8 @@
     config(
         **custom_incremental_config(
         incremental_strategy = 'insert_overwrite',
-        partition_by = {'field': 'event_date', 'data_type': 'date'}
+        partition_by = {'field': 'event_date', 'data_type': 'date'},
+        on_schema_change = "sync_all_columns"
     )
 ) }}
 
@@ -33,12 +34,12 @@ SELECT
     offer_id,
     module_id,
     COUNT(*) as total_displays,
-    COUNT(CASE WHEN displayed_position < 4 THEN 1 ELSE 0 END) as total_position_0_3_diplays,
-    COUNT(CASE WHEN displayed_position <= 10 AND displayed_position >= 4 THEN 1 ELSE 0 END) AS total_position_4_10_displays,
-    COUNT(CASE WHEN displayed_position <= 20 AND displayed_position > 10 THEN 1 ELSE 0 END) AS total_position_11_20_displays,
-    COUNT(CASE WHEN displayed_position <= 30 AND displayed_position > 20 THEN 1 ELSE 0 END) AS total_position_21_30_displays,
-    COUNT(CASE WHEN displayed_position <= 40 AND displayed_position > 30 THEN 1 ELSE 0 END) AS total_position_31_40_displays,
-    COUNT(CASE WHEN displayed_position <= 50 AND displayed_position > 40 THEN 1 ELSE 0 END) AS total_position_41_50_displays,
+    SUM(CASE WHEN displayed_position < 4 THEN 1 ELSE 0 END) as total_position_0_3_diplays,
+    SUM(CASE WHEN displayed_position <= 10 AND displayed_position >= 4 THEN 1 ELSE 0 END) AS total_position_4_10_displays,
+    SUM(CASE WHEN displayed_position <= 20 AND displayed_position > 10 THEN 1 ELSE 0 END) AS total_position_11_20_displays,
+    SUM(CASE WHEN displayed_position <= 30 AND displayed_position > 20 THEN 1 ELSE 0 END) AS total_position_21_30_displays,
+    SUM(CASE WHEN displayed_position <= 40 AND displayed_position > 30 THEN 1 ELSE 0 END) AS total_position_31_40_displays,
+    SUM(CASE WHEN displayed_position <= 50 AND displayed_position > 40 THEN 1 ELSE 0 END) AS total_position_41_50_displays,
 FROM {{ ref('int_firebase__native_home_offer_displayed') }} as native 
 {% if is_incremental() %}
     WHERE date(event_date) = date_sub('{{ ds() }}', INTERVAL 3 day)
@@ -84,20 +85,20 @@ SELECT
     offers.offerer_id,
     offers.offerer_name,
     offers.venue_type_label,
-    display.total_displays,
-    display.total_position_0_3_diplays,
-    display.total_position_4_10_displays,
-    display.total_position_11_20_displays,
-    display.total_position_21_30_displays,
-    display.total_position_31_40_displays,
-    display.total_position_41_50_displays,
-    consult.total_consultations,
-    consult.total_position_0_3_consultations,
-    consult.total_position_4_10_consultations,
-    consult.total_position_11_20_consultations,
-    consult.total_position_21_30_consultations,
-    consult.total_position_31_40_consultations,
-    consult.total_position_41_50_consultations
+    coalesce(display.total_displays,0) as total_displays,
+    COALESCE(display.total_position_0_3_diplays,0) as total_position_0_3_diplays,
+    COALESCE(display.total_position_4_10_displays,0) as total_position_4_10_displays,
+    COALESCE(display.total_position_11_20_displays,0) as total_position_11_20_displays,
+    COALESCE(display.total_position_21_30_displays,0) as total_position_21_30_displays,
+    COALESCE(display.total_position_31_40_displays,0) as total_position_31_40_displays,
+    COALESCE(display.total_position_41_50_displays,0) as total_position_41_50_displays,
+    COALESCE(consult.total_consultations,0) as total_consultations,
+    COALESCE(consult.total_position_0_3_consultations,0) as total_position_0_3_consultations,
+    COALESCE(consult.total_position_4_10_consultations,0) as total_position_4_10_consultations,
+    COALESCE(consult.total_position_11_20_consultations,0) as total_position_11_20_consultations,
+    COALESCE(consult.total_position_21_30_consultations,0) as total_position_21_30_consultations,
+    COALESCE(consult.total_position_31_40_consultations,0) as total_position_31_40_consultations,
+    COALESCE(consult.total_position_41_50_consultations,0) as total_position_41_50_consultations
 FROM displays_by_position_bucket as display
 LEFT JOIN consultations_by_position_bucket as consult on display.offer_id = consult.offer_id AND display.event_date = consult.event_date AND display.module_id = consult.module_id
 LEFT JOIN {{ ref('mrt_global__offer') }} as offers ON consult.offer_id = offers.offer_id 

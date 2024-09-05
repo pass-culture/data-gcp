8102f461dcd45e92a6175d20b15bc5be56195281
Caroline Mestre <162314477+carolinemestre-passculture@users.noreply.github.com>
2024-09-05 15:38:03 +0200

    (dbt) Migrate collective_offer_domain_name (#3090)
    * feat: Migrate collective_offer_domain_name
    
    * feat: Create mrt_global__collective_offer_domain.sql
    
    * feat: Create mrt_global__collective_offer_domain.sql
    
    * feat: Create mrt_global__collective_offer_domain.sql

--- orchestration/dags/data_gcp_dbt/models/analytics/collective_offer_domain_name.sql
+++
@@ -1,6 +0,0 @@
-select
-    cod.collective_offer_id,
-    cod.educational_domain_id,
-    ed.educational_domain_name
-from {{ source('raw', 'applicative_database_collective_offer_domain') }} cod
-    left join {{ source('raw', 'applicative_database_educational_domain') }} ed on cod.educational_domain_id = ed.educational_domain_id

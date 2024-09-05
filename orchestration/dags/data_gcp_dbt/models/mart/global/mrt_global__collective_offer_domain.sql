946b8f51341de688ce2dcf7d73b823934c8041d7
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-09-05 12:43:02 +0200

    feat: Create mrt_global__collective_offer_domain.sql

--- orchestration/dags/data_gcp_dbt/models/mart/global/mrt_global__collective_offer_domain.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/global/mrt_global__collective_offer_domain.sql
@@ -2,4 +2,4 @@ select
     collective_offer_id,
     educational_domain_id,
     educational_domain_name
-from {{ ref('int_applicative__offer_domain') }} as cod
+from {{ ref('int_applicative__collective_offer_domain') }} as cod

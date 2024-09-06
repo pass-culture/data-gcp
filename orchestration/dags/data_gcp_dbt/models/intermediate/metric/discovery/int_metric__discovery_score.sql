e1dd65e50655fa2cf93f0a85d7407db6f3c9408a
Jules-Arbelot <115459312+Jules-Arbelot@users.noreply.github.com>
2024-09-06 15:32:01 +0200

    feat: update consult tables and create discovery mrt models

--- orchestration/dags/data_gcp_dbt/models/intermediate/metric/discovery/int_metric__discovery_score.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/metric/discovery/int_metric__discovery_score.sql
@@ -2,4 +2,4 @@ SELECT consultation_id,
     consulted_entity,
     type
 FROM {{ ref('int_metric__discovery_daily_consultation') }}
-QUALIFY ROW_NUMBER() OVER(PARTITION BY user_id, consulted_entity ORDER BY consultation_timestamp ASC) = 1
+QUALIFY ROW_NUMBER() OVER(PARTITION BY user_id, consulted_entity, type ORDER BY consultation_timestamp ASC) = 1

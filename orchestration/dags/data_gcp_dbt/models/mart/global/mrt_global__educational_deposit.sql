6b11e248d218a24eb8535f5564ad98e29adfa7c6
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-09-09 17:22:41 +0200

    feat: Create mrt_global__educational_deposit.sql

--- orchestration/dags/data_gcp_dbt/models/mart/global/mrt_global__educational_deposit.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/global/mrt_global__educational_deposit.sql
@@ -3,4 +3,4 @@ select ed.institution_id,
     ei.institution_department_code,
     ei.institution_academy_name
 from {{ ref('int_applicative__educational_deposit') }} as ed
-left join {{ ref('mrt_global__educational_institution') }} as ei on ed.educational_institution_id = ei.educational_institution_id
+left join {{ ref('mrt_global__educational_institution') }} as ei on ed.institution_id = ei.educational_institution_id

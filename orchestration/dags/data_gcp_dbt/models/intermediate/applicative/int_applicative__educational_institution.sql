fa27d06b6cebd02edf2ca6a9867b92e7db47acc5
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-09-09 17:15:25 +0200

    feat: Create mrt_global__educational_deposit.sql

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__educational_institution.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__educational_institution.sql
@@ -1,4 +1,16 @@
-with users_grouped_by_institution AS (
+WITH deposit_grouped_by_institution AS (
+SELECT institution_id,
+    MAX(CASE WHEN deposit_rank_asc = 1 THEN ministry END) AS ministry,
+    MAX(CASE WHEN deposit_rank_asc = 1 THEN deposit_creation_date END) AS first_deposit_creation_date,
+    MAX(CASE WHEN is_current_deposit THEN educational_deposit_amount END) AS current_deposit_amount,
+    MAX(CASE WHEN is_current_deposit THEN deposit_creation_date END) AS current_deposit_creation_date,
+    SUM(educational_deposit_amount) AS total_deposit_amount,
+    COUNT(*) AS total_deposits,
+FROM {{ ref('int_applicative__educational_deposit') }} AS ed
+GROUP BY institution_id
+),
+
+ users_grouped_by_institution AS (
 select REGEXP_EXTRACT(result_content, '"school_uai": \"(.*?)\",') AS institution_external_id,
     count(DISTINCT user_id) AS total_credited_beneficiaries
 from {{ ref('int_applicative__beneficiary_fraud_check') }}
@@ -25,9 +37,16 @@ select
         END,
         ei.institution_department_code
     ) as institution_department_code,
+    dgi.ministry,
+    dgi.first_deposit_creation_date,
+    dgi.current_deposit_amount,
+    dgi.current_deposit_creation_date,
+    dgi.total_deposit_amount,
+    dgi.total_deposits,
     institution_program.institution_program_name AS institution_program_name,
     ugi.total_credited_beneficiaries,
 from {{ source('raw', 'applicative_database_educational_institution') }} AS ei
 left join {{ ref('int_applicative__institution_program') }} AS institution_program
     on ei.educational_institution_id = institution_program.institution_id
+left join deposit_grouped_by_institution AS dgi ON dgi.institution_id = ei.educational_institution_id
 left join users_grouped_by_institution as ugi on ugi.institution_external_id = ei.institution_id

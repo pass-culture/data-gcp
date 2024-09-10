90986c275e7e7f1fdb1306140c034318d2199251
cdelabre <103637148+cdelabre@users.noreply.github.com>
2024-09-10 12:21:35 +0200

    feat: migrate logs views and table to dbt legacy (#3117)

---
+++ orchestration/dags/data_gcp_dbt/models/analytics/collective_logs.sql
@@ -0,0 +1,24 @@
+{{
+    config(
+        **custom_incremental_config(
+        incremental_strategy='insert_overwrite',
+        partition_by={'field': 'partition_date', 'data_type': 'date'},
+    )
+) }}
+
+select
+    DATE(timestamp) as partition_date,
+    timestamp,
+    jsonpayload.extra.path as path,
+    jsonpayload.extra.statuscode as status_code,
+    jsonpayload.extra.method as method,
+    jsonpayload.extra.sourceip as source_ip,
+    jsonpayload.extra.duration as duration
+from
+    {{ source("raw","stdout") }}
+where
+    1 = 1
+    {% if is_incremental() %}
+        and DATE(timestamp) = "{{ ds }}"
+    {% endif %}
+    and jsonpayload.extra.path like "/collective/%"

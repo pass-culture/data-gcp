4af83d7045c1fa601f59ce8fcfe7f0e956d6dc65
Mathilde Vey <133665016+mathilde-vey@users.noreply.github.com>
2024-09-11 16:13:33 +0200

    fix field name

--- orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__marketing_pro_live_show_email_churned_40_days_ago.sql
+++ orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__marketing_pro_live_show_email_churned_40_days_ago.sql
@@ -15,6 +15,6 @@ from {{ ref('mrt_global__venue') }} venue
 join {{ ref('mrt_global__offerer') }} offerer ON venue.venue_managing_offerer_id=offerer.offerer_id
     and DATE_DIFF(CURRENT_DATE, offerer.last_bookable_offer_date, day) = 40
 where venue_is_permanent
-    and venue_type = "Spectacle vivant"
+    and venue_type_label = "Spectacle vivant"
     and venue_booking_email is not NULL
     
--- orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__marketing_pro_live_show_email_last_booking_40_days_ago.sql
+++ orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__marketing_pro_live_show_email_last_booking_40_days_ago.sql
@@ -15,4 +15,4 @@ from {{ ref('mrt_global__venue') }}
 join {{ ref('mrt_global__offerer') }} offerer ON venue.venue_managing_offerer_id=offerer.offerer_id
     and DATE_DIFF(CURRENT_DATE, offerer.last_booking_date, day) = 40
 where venue_is_permanent
-    and venue_type = "Spectacle vivant"
+    and venue_type_label = "Spectacle vivant"

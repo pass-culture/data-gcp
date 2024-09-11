f818f92d3a42fc6578e90a3c10712210873ef335
Mathilde Vey <133665016+mathilde-vey@users.noreply.github.com>
2024-09-11 16:29:47 +0200

    mini fix

--- orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__marketing_pro_live_show_email_churned_40_days_ago.sql
+++ orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__marketing_pro_live_show_email_churned_40_days_ago.sql
@@ -13,7 +13,7 @@ select
     venue_booking_email
 from {{ ref('mrt_global__venue') }} venue
 join {{ ref('mrt_global__offerer') }} offerer ON venue.venue_managing_offerer_id=offerer.offerer_id
-    and DATE_DIFF(CURRENT_DATE, offerer.last_bookable_offer_date, day) = 40
+    and DATE_DIFF(CURRENT_DATE, offerer.last_bookable_offer_date, day) >= 40
 where venue_is_permanent
     and venue_type_label = "Spectacle vivant"
     and venue_booking_email is not NULL

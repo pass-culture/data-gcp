27faa41efc73f9fd2199e41c8b2c1050b143ef6e
Mathilde Vey <133665016+mathilde-vey@users.noreply.github.com>
2024-09-11 16:11:00 +0200

    Add 2 tables for live show venues

---
+++ orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__marketing_pro_live_show_email_churned_40_days_ago.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        **custom_incremental_config(
+        incremental_strategy = "insert_overwrite",
+        partition_by = {"field": "execution_date", "data_type": "date", "granularity" : "day"},
+        on_schema_change = "sync_all_columns",
+    )
+) }}
+
+select
+    DATE('{{ ds() }}') as execution_date,
+    venue_id,
+    venue_booking_email
+from {{ ref('mrt_global__venue') }} venue
+join {{ ref('mrt_global__offerer') }} offerer ON venue.venue_managing_offerer_id=offerer.offerer_id
+    and DATE_DIFF(CURRENT_DATE, offerer.last_bookable_offer_date, day) = 40
+where venue_is_permanent
+    and venue_type = "Spectacle vivant"
+    and venue_booking_email is not NULL
+    
---
+++ orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__marketing_pro_live_show_email_last_booking_40_days_ago.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        **custom_incremental_config(
+        incremental_strategy = "insert_overwrite",
+        partition_by = {"field": "execution_date", "data_type": "date", "granularity" : "day"},
+        on_schema_change = "sync_all_columns",
+    )
+) }}
+
+select
+    DATE('{{ ds() }}') as execution_date,
+    venue_id,
+    venue_booking_email
+from {{ ref('mrt_global__venue') }}
+join {{ ref('mrt_global__offerer') }} offerer ON venue.venue_managing_offerer_id=offerer.offerer_id
+    and DATE_DIFF(CURRENT_DATE, offerer.last_booking_date, day) = 40
+where venue_is_permanent
+    and venue_type = "Spectacle vivant"

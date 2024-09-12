448b5488e1c542bd9a138f3a6b9861177a0ca50a
carolinemestre-passculture <caroline.mestre@passculture.app>
2024-09-12 12:18:35 +0200

    fix: Update discovery features

--- orchestration/dags/data_gcp_dbt/macros/discovery_vars.sql
+++ orchestration/dags/data_gcp_dbt/macros/discovery_vars.sql
@@ -3,8 +3,8 @@
         set discovery_features = {
         "discovery_features" :
         var("discovery_features",
-        [ "category_id"
-        , "subcategory_id"
+        [ "offer_category_id"
+        , "offer_subcategory_id"
         , "new_items"
         ,])
 }

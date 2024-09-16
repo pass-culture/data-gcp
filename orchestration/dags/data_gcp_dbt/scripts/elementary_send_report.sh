b1119f6ec027ae623229d32368b69192207e726f
LucileRainteau <lucile.rainteau@passculture.app>
2024-09-16 14:32:20 +0200

    update .sh

--- orchestration/dags/data_gcp_dbt/scripts/elementary_send_report.sh
+++ orchestration/dags/data_gcp_dbt/scripts/elementary_send_report.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-edr send-report --project-dir . --target-path target/ --profiles-dir . --profile-target $ENV_SHORT_NAME --gcs-bucket-name $DATA_BUCKET_NAME --bucket-file-path $REPORT_FILE_PATH
+edr send-report --project-dir $PATH_TO_DBT_PROJECT --target-path $PATH_TO_DBT_TARGET --profiles-dir $PATH_TO_DBT_PROJECT --profile-target $ENV_SHORT_NAME --gcs-bucket-name $DATA_BUCKET_NAME --bucket-file-path $REPORT_FILE_PATH

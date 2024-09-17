89458a25878c5ce6b54a9388a99a640cba8eabea
valoumiaou <valentin.busson@passculture.app>
2024-09-17 17:04:37 +0200

    tune cron schedule according to airflow's ETA's

--- orchestration/dags/jobs/crons.py
+++ orchestration/dags/jobs/crons.py
@@ -1,16 +1,16 @@
 schedule_dict = {
     "import_applicative_database": "0 1 * * *",
     "import_intraday_firebase_data": "0 1 * * *",
-    "dbt_run_dag": "30 2 * * *",
-    "dbt_artifacts": "0 5 * * *",
-    "import_analytics_v7": "0 5 * * *",
+    "dbt_run_dag": "45 2 * * *",
     "clickhouse_exports": {
         "daily": {
-            "prod": "0 5 * * *",
-            "stg": "0 5 * * *",
-            "dev": "0 5 * * *",
+            "prod": "45 3 * * *",
+            "stg": "45 3 * * *",
+            "dev": "45 3 * * *",
         },
     },
+    "import_analytics_v7": "45 4 * * *",
+    "dbt_artifacts": "45 4 * * *",
     "dbt_weekly": "30 5 * * *",
     "dbt_monthly": "30 5 * * *",
 }

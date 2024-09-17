2a9646d96a23550a58ca8ae8278bc5109a10475d
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2024-09-17 19:03:54 +0200

    fix alias

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__extract_offer.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__extract_offer.sql
@@ -1,7 +1,7 @@
 with extra_data as (
     select
         offer.offer_id,
-        COALESCE(product.description, o.offer_description) AS offer_description,
+        COALESCE(product.description, offer.offer_description) AS offer_description,
         COALESCE(product.product_extra_data, offer.offer_extra_data) as extra_data
     from {{ source('raw', 'applicative_database_offer') }} as offer
         left join {{ ref('int_applicative__product') }} as product on CAST(product.id as string) = offer.offer_product_id
@@ -9,7 +9,7 @@ with extra_data as (
 
 extracted_offers as (
     select
-        offer.* EXCEPT(offer.offer_description),
+        offer.* EXCEPT(offer_description),
         ued.offer_description,
         LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.author"), " ")) as author,
         LOWER(TRIM(JSON_EXTRACT_SCALAR(extra_data, "$.performer"), " ")) as performer,

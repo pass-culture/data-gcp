c54d24b857b53adce8bc5a1bb2a0092861558371
valentin <142802218+valoumiaou@users.noreply.github.com>
2024-09-17 17:06:36 +0200

    Airflow/delayed waiting operator (#3151)
    * centralize dags crons
    
    * add delayed waiting operator with execution delta
    
    * switch waiting operators in dbt dag run
    
    * typo
    
    * reset waiting operator for internal waiting task ??
    
    * switch logical date to execution date and include cron schedule_dict
    
    * set crons for dbt and following dags
    
    * add optional lower date limit set default to execution date
    
    * switch waiting operator in dags with  cron dictionnary entries
    
    * tune cron schedule according to airflow's ETA's

---
+++ orchestration/dags/jobs/crons.py
@@ -0,0 +1,16 @@
+schedule_dict = {
+    "import_applicative_database": "0 1 * * *",
+    "import_intraday_firebase_data": "0 1 * * *",
+    "dbt_run_dag": "45 2 * * *",
+    "clickhouse_exports": {
+        "daily": {
+            "prod": "45 3 * * *",
+            "stg": "45 3 * * *",
+            "dev": "45 3 * * *",
+        },
+    },
+    "import_analytics_v7": "45 4 * * *",
+    "dbt_artifacts": "45 4 * * *",
+    "dbt_weekly": "30 5 * * *",
+    "dbt_monthly": "30 5 * * *",
+}

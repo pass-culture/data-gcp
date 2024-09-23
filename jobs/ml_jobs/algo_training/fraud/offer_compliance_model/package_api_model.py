0aea338df7b37c557b5c2e968fab309778875798
dcuesta-pass <daniel.cuesta@passculture.app>
2024-09-23 12:30:10 +0200

    [DE-581] refactor: Update imports for two_towers_model in package_api_model.py

--- jobs/ml_jobs/algo_training/fraud/offer_compliance_model/package_api_model.py
+++ jobs/ml_jobs/algo_training/fraud/offer_compliance_model/package_api_model.py
@@ -12,11 +12,11 @@ import typer
 from catboost import CatBoostClassifier, Pool
 from PIL import Image
 from sentence_transformers import SentenceTransformer
-from utils.constants import (
+
+from commons.constants import (
     ENV_SHORT_NAME,
     MODEL_DIR,
 )
-
 from commons.mlflow_tools import connect_remote_mlflow
 from fraud.offer_compliance_model.utils.constants import CONFIGS_PATH
 

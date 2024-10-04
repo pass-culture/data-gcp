b608f15f342ec4b851ab59922eaeccb9c47be02c
LucileRainteau <lucile.rainteau@passculture.app>
2024-10-04 15:39:19 +0200

    fix macro

--- orchestration/dags/data_gcp_dbt/macros/custom_snapshot_config.sql
+++ orchestration/dags/data_gcp_dbt/macros/custom_snapshot_config.sql
@@ -2,19 +2,15 @@
     strategy=None,
     unique_key=None,
     updated_at=None,
-    target_schema=generate_schema_name('source_' ~ target.name),
+    target_schema=generate_schema_name('raw_' ~ target.name),
     partition_by={'field': 'dbt_valid_to', 'data_type': 'timestamp'}
 ) %}
-    {% if target.profile_name == 'CI' %}
-        {% set config_params = {'materialized': 'view'} %}
-    {% else %}
-        {% set config_params = {
-            "strategy": strategy,
-            "unique_key": unique_key,
-            "updated_at": updated_at,
-            "target_schema": target_schema,
-            "partition_by": partition_by
-        } %}
-    {% endif %}
+    {% set config_params = {
+        "strategy": strategy,
+        "unique_key": unique_key,
+        "updated_at": updated_at,
+        "target_schema": target_schema,
+        "partition_by": partition_by
+    } %}
     {{ return(config_params) }}
 {% endmacro %}

85864974ae8d3e9722dcbdad531ac484da982ed5
LucileRainteau <lucile.rainteau@passculture.app>
2024-10-07 17:29:15 +0200

    (DE-74) bsr(qualtrics) deduplicate ir import

--- orchestration/dags/dependencies/qualtrics/sql/analytics/qualtrics_answers_ir_jeunes.sql
+++ orchestration/dags/dependencies/qualtrics/sql/analytics/qualtrics_answers_ir_jeunes.sql
@@ -1,18 +1,25 @@
-SELECT 
-  StartDate as start_date
-  , EndDate as end_date
-  , ResponseId as response_id
-  , ExternalReference as user_id
-  , user_type
-  , question
+with base as (
+select distinct
+  start_date
+  , end_date
+  , response_id
+  , user_id as venue_id
+  , case
+  when survey_id = 'SV_3IdnHqrnsuS17oy' then 'GRANT_15_17'
+  when survey_id = 'SV_cBV3xaZ92BoW5sW' then 'GRANT_18'
+  end as user_type
+  , CASE
+    WHEN question = "Q3 - Topics" THEN "Q3_topics"
+  end as question
   , question_id
   , question_str
-  , answer
-  , execution_date
-  , Q3_Topics as topics
-  , theoretical_amount_spent
-  , user_activity
-  , user_civility
-FROM `{{ bigquery_raw_dataset }}.qualtrics_answers_ir_survey_jeunes`
-QUALIFY row_number() over(partition by response_id order by execution_date desc) = 1
+  , REPLACE(extra_data, "nan", "'nan'") AS extra_data
+from `{{ bigquery_raw_dataset }}.qualtrics_answers`
+where survey_id in ("SV_3IdnHqrnsuS17oy", "SV_cBV3xaZ92BoW5sW")
+and question in ('Q1', 'Q2', 'Q3', 'Q3 - Topics'))
 
+select *
+, TRIM(JSON_EXTRACT(extra_data, '$.theoretical_amount_spent'), '"') as theoretical_amount_spent
+, TRIM(JSON_EXTRACT(extra_data, '$.user_activity'), '"') as user_activity
+, TRIM(JSON_EXTRACT(extra_data, '$.user_civility'), '"') as user_civility
+from base
--- orchestration/dags/dependencies/qualtrics/sql/analytics/qualtrics_answers_ir_pro.sql
+++ orchestration/dags/dependencies/qualtrics/sql/analytics/qualtrics_answers_ir_pro.sql
@@ -1,16 +1,26 @@
-SELECT 
-  StartDate as start_date
-  , EndDate as end_date
-  , ResponseId as response_id
-  , ExternalReference as venue_id
-  , user_type
-  , question
+with base as (
+select
+  start_date
+  , end_date
+  , response_id
+  , user_id as venue_id
+  , 'pro' as user_type
+  , CASE
+    WHEN question = "Q1 - Topics" THEN "Q1_topics"
+    WHEN question = "Q1 - Parent Topics" THEN "Q1_parent_topics"
+    WHEN question = "Q1" THEN "Q1"
+    WHEN question = "Q3" THEN "Q3"
+    WHEN question = "Q1_NPS_GROUP" THEN "Q1_nps_group"
+  end as question
   , question_id
   , question_str
   , answer
-  , execution_date
-  , Q1_Topics as topics
-  , anciennete_jours
-  , non_cancelled_bookings
-  , offers_created
-FROM `{{ bigquery_raw_dataset }}.qualtrics_answers_ir_survey_pro`
+  , replace(REPLACE(extra_data, "d'un", "dun"), "nan", "'nan'") AS extra_data
+from `{{ bigquery_raw_dataset }}.qualtrics_answers`
+where survey_id = "SV_eOOPuFjgZo1emR8")
+
+select *
+, TRIM(JSON_EXTRACT(extra_data, '$.anciennete_jours'), '"') as anciennete_jours
+, TRIM(JSON_EXTRACT(extra_data, '$.non_cancelled_bookings'), '"') as non_cancelled_bookings
+, TRIM(JSON_EXTRACT(extra_data, '$.offers_created'), '"') as offers_created
+from base

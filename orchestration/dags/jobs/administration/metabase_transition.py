2a7b98fa176d43fbed6a16cf2c120316b40f283a
valoumiaou <valentin.busson@passculture.app>
2024-10-08 07:53:31 +0200

    metabase transition -> uv, py3.10

--- orchestration/dags/jobs/administration/metabase_transition.py
+++ orchestration/dags/jobs/administration/metabase_transition.py
@@ -8,7 +8,7 @@ from common.config import (
     GCP_PROJECT_ID,
 )
 from common.operators.gce import (
-    CloneRepositoryGCEOperator,
+    InstallDependenciesOperator,
     SSHGCEOperator,
     StartGCEOperator,
     StopGCEOperator,
@@ -74,18 +74,12 @@ with DAG(
         instance_name=GCE_INSTANCE, task_id="gce_start_task"
     )
 
-    fetch_code = CloneRepositoryGCEOperator(
-        task_id="fetch_code",
+    fetch_install_code = InstallDependenciesOperator(
+        task_id="fetch_install_code",
         instance_name=GCE_INSTANCE,
-        command="{{ params.branch }}",
-        python_version="3.9",
-    )
-
-    install_dependencies = SSHGCEOperator(
-        task_id="install_dependencies",
-        instance_name=GCE_INSTANCE,
+        branch="{{ params.branch }}",
+        python_version="3.10",
         base_dir=BASE_PATH,
-        command="pip install -r requirements.txt --user",
         dag=dag,
         retries=2,
     )
@@ -95,6 +89,7 @@ with DAG(
         instance_name=GCE_INSTANCE,
         base_dir=BASE_PATH,
         environment=dag_config,
+        installer="uv",
         command="""
         python main.py \
         --metabase-card-type {{ params.metabase_card_type }} \
@@ -112,8 +107,7 @@ with DAG(
     (
         start
         >> gce_instance_start
-        >> fetch_code
-        >> install_dependencies
+        >> fetch_install_code
         >> switch_metabase_cards_op
         >> gce_instance_stop
     )

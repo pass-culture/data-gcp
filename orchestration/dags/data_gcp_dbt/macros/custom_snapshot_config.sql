881db1d76d350464f63e63c7ada4a6edf757407a
LucileRainteau <lucile.rainteau@passculture.app>
2024-10-10 11:33:42 +0200

    add snapshot check config

--- orchestration/dags/data_gcp_dbt/macros/custom_snapshot_config.sql
+++ orchestration/dags/data_gcp_dbt/macros/custom_snapshot_config.sql
@@ -3,14 +3,18 @@
     unique_key=None,
     updated_at=None,
     target_schema=generate_schema_name('raw_' ~ target.name),
-    partition_by={'field': 'dbt_valid_to', 'data_type': 'timestamp'}
+    partition_by={'field': 'dbt_valid_to', 'data_type': 'timestamp'},
+    tags=['source_snapshot'],
+    check_cols=None
 ) %}
     {% set config_params = {
         "strategy": strategy,
         "unique_key": unique_key,
         "updated_at": updated_at,
         "target_schema": target_schema,
-        "partition_by": partition_by
+        "partition_by": partition_by,
+        "tags": tags,
+        "check_cols": check_cols
     } %}
     {{ return(config_params) }}
 {% endmacro %}

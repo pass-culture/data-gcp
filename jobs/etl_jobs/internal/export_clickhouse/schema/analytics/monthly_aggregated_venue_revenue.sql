5c2e49cf6e3dada61e6e6d667d3ced48d148acd5
valoumiaou <valentin.busson@passculture.app>
2024-10-16 15:19:18 +0200

    typo

--- jobs/etl_jobs/internal/export_clickhouse/schema/analytics/monthly_aggregated_venue_revenue.sql
+++ jobs/etl_jobs/internal/export_clickhouse/schema/analytics/monthly_aggregated_venue_revenue.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE analytics.monthy_aggregated_venue_revenue ON cluster default
+CREATE OR REPLACE TABLE analytics.monthly_aggregated_venue_revenue ON cluster default
     ENGINE = SummingMergeTree()
     PARTITION BY creation_month
     ORDER BY (venue_id)
@@ -15,9 +15,9 @@ SELECT
     sum(coalesce(i.expected_revenue,0) + coalesce(c.expected_revenue,0)) as total_expected_revenue
 
 FROM
-    analytics.monthy_aggregated_venue_individual_revenue i
+    analytics.monthly_aggregated_venue_individual_revenue i
 LEFT JOIN
-    analytics.monthy_aggregated_venue_collective_revenue c
+    analytics.monthly_aggregated_venue_collective_revenue c
 ON i.creation_month = c.creation_month AND i.venue_id = c.venue_id
 GROUP BY
     1,2

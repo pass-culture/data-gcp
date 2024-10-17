2b8b0a51bf1ba40c2ffe1d8a7099145e77329c96
LaurentM Pass <laurent.montier@passculture.app>
2024-10-17 16:20:51 +0200

    lint: fix sql linting

--- orchestration/dags/data_gcp_dbt/models/mart/native/mrt_native__consultation.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/native/mrt_native__consultation.sql
@@ -22,62 +22,75 @@ with
         group by consultation_id
     ),
 
--- Next 4 subqueries : identify the micro-origin of each consultation that comes from a venue page or a similar offer page (origin of venue consultation and origin of inital offer consultation)
-consult_venue AS (
-    SELECT
-        user_id,
-        unique_session_id,
-        event_timestamp as venue_consultation_timestamp,
-        venue_id,
-        origin as consult_venue_origin
-    FROM {{ ref('int_firebase__native_event') }}
-    WHERE event_name = 'ConsultVenue'
-        AND unique_session_id IS NOT NULL
-        {% if is_incremental() %}
-        AND event_date = date_sub('{{ ds() }}', INTERVAL 3 day)
-        {% endif %}
-),
+    -- Next 4 subqueries : identify the micro-origin of each consultation that comes
+    -- from a venue page or a similar offer page (origin of venue consultation and
+    -- origin of inital offer consultation)
+    consult_venue as (
+        select
+            user_id,
+            unique_session_id,
+            event_timestamp as venue_consultation_timestamp,
+            venue_id,
+            origin as consult_venue_origin
+        from {{ ref("int_firebase__native_event") }}
+        where
+            event_name = 'ConsultVenue' and unique_session_id is not null
+            {% if is_incremental() %}
+                and event_date = date_sub('{{ ds() }}', interval 3 day)
+            {% endif %}
+    ),
 
-consult_offer AS (
-    SELECT
-        user_id,
-        unique_session_id,
-        consultation_timestamp,
-        consultation_id,
-        offer_id,
-        venue_id,
-        similar_offer_id,
-        origin as consult_offer_origin
-    FROM {{ ref('int_firebase__native_consultation') }}
-    WHERE 1=1
-        {% if is_incremental() %}
-        AND event_date = date_sub('{{ ds() }}', INTERVAL 3 day)
-        {% endif %}
-),
+    consult_offer as (
+        select
+            user_id,
+            unique_session_id,
+            consultation_timestamp,
+            consultation_id,
+            offer_id,
+            venue_id,
+            similar_offer_id,
+            origin as consult_offer_origin
+        from {{ ref("int_firebase__native_consultation") }}
+        where
+            1 = 1
+            {% if is_incremental() %}
+                and event_date = date_sub('{{ ds() }}', interval 3 day)
+            {% endif %}
+    ),
 
-consult_offer_through_venue AS (
-    SELECT
-        co.*,
-        cv.consult_venue_origin
-    FROM consult_offer co
-        LEFT JOIN consult_venue cv ON cv.unique_session_id = co.unique_session_id
-            AND cv.venue_id = co.venue_id
-            AND cv.venue_consultation_timestamp <= co.consultation_timestamp
-    WHERE co.consult_offer_origin = "venue"
-    QUALIFY row_number() over (partition by co.unique_session_id, co.venue_id, co.offer_id order by co.consultation_timestamp ASC) = 1 -- keep 1st offer consultation after venue consultation
-),
+    consult_offer_through_venue as (
+        select co.*, cv.consult_venue_origin
+        from consult_offer co
+        left join
+            consult_venue cv
+            on cv.unique_session_id = co.unique_session_id
+            and cv.venue_id = co.venue_id
+            and cv.venue_consultation_timestamp <= co.consultation_timestamp
+        where co.consult_offer_origin = "venue"
+        qualify
+            row_number() over (
+                partition by co.unique_session_id, co.venue_id, co.offer_id
+                order by co.consultation_timestamp asc
+            )
+            = 1  -- keep 1st offer consultation after venue consultation
+    ),
 
-consult_offer_through_similar_offer AS (
-    SELECT 
-        co1.*
-        ,co2.consult_offer_origin as consult_similar_offer_origin
-    FROM consult_offer co1 
-        LEFT JOIN consult_offer co2 ON co1.unique_session_id = co2.unique_session_id
-            AND co1.similar_offer_id=co2.offer_id
-            AND co2.consultation_timestamp <= co1.consultation_timestamp
-    WHERE co1.consult_offer_origin = "similar_offer"
-    QUALIFY row_number() over (partition by co1.unique_session_id, co1.venue_id, co1.offer_id order by co1.consultation_timestamp ASC) = 1 -- keep 1st similar offer consultation after offer consultation 
-)
+    consult_offer_through_similar_offer as (
+        select co1.*, co2.consult_offer_origin as consult_similar_offer_origin
+        from consult_offer co1
+        left join
+            consult_offer co2
+            on co1.unique_session_id = co2.unique_session_id
+            and co1.similar_offer_id = co2.offer_id
+            and co2.consultation_timestamp <= co1.consultation_timestamp
+        where co1.consult_offer_origin = "similar_offer"
+        qualify
+            row_number() over (
+                partition by co1.unique_session_id, co1.venue_id, co1.offer_id
+                order by co1.consultation_timestamp asc
+            )
+            = 1  -- keep 1st similar offer consultation after offer consultation
+    )
 
 select
     consult.consultation_id,
@@ -121,35 +134,66 @@ select
     ht.home_name,
     ht.home_audience,
     ht.user_lifecycle_home,
-    CASE WHEN consult.origin="similar_offer" AND consult.similar_offer_playlist_type = "sameCategorySimilarOffers" THEN "same_category_similar_offer"
-        WHEN consult.origin="similar_offer" AND consult.similar_offer_playlist_type = "otherCategoriesSimilarOffers" THEN "other_category_similar_offer"
-        ELSE consult.origin END AS consultation_macro_origin,
-    CASE WHEN ht.entry_id IS NOT NULL AND ht.home_type IS NOT NULL THEN CONCAT("home_",ht.home_type)
-        WHEN ht.entry_id IS NOT NULL AND ht.home_type IS NULL THEN "home_without_tag"
-        WHEN consult.origin="search" AND consult.search_query_input_is_generic IS TRUE THEN "generic_query_search"
-        WHEN consult.origin="search" AND consult.search_query_input_is_generic IS FALSE THEN "specific_query_search"
-        WHEN consult.origin="search" AND consult.query is NULL THEN "landing_search"
-        WHEN consult.origin="venue" AND ov.consult_venue_origin="offer" THEN "offer_venue"
-        WHEN consult.origin="venue" AND ov.consult_venue_origin="searchVenuePlaylist" THEN "search_venue_playlist"
-        WHEN consult.origin="venue" AND ov.consult_venue_origin="searchAutoComplete" THEN "search_venue_autocomplete"
-        WHEN consult.origin="venue" AND ov.consult_venue_origin="venueMap" THEN "venue_map"
-        WHEN consult.origin="venue" AND ov.consult_venue_origin IN ("home","venueList") THEN "home_venue_playlist"
-        WHEN consult.origin="venue" AND ov.consult_venue_origin="deeplink" THEN "venue_deeplink"
-        WHEN consult.origin="offer" AND consult.multi_venue_offer_id IS NOT NULL THEN "multi_venue_offer"
-        WHEN consult.origin="similar_offer" THEN CONCAT("similar_offer_",so.consult_similar_offer_origin)
-        ELSE consult.origin END as consultation_micro_origin
-FROM {{ ref('int_firebase__native_consultation')}} AS consult
-left join discoveries_by_consultation as dc on dc.consultation_id = consult.consultation_id
-left join {{ ref('int_global__offer')}} as offer on consult.offer_id = offer.offer_id
-left join {{ ref('int_global__user')}} as user on consult.user_id = user.user_id
-LEFT JOIN {{ ref('int_contentful__home_tag') }} AS ht ON ht.entry_id=consult.entry_id
-LEFT JOIN consult_offer_through_venue AS ov ON ov.consultation_id = consult.consultation_id AND consult.origin = "venue"
-LEFT JOIN consult_offer_through_similar_offer AS so ON so.consultation_id = consult.consultation_id AND consult.origin = "similar_offer"
-
+    case
+        when
+            consult.origin = "similar_offer"
+            and consult.similar_offer_playlist_type = "sameCategorySimilarOffers"
+        then "same_category_similar_offer"
+        when
+            consult.origin = "similar_offer"
+            and consult.similar_offer_playlist_type = "otherCategoriesSimilarOffers"
+        then "other_category_similar_offer"
+        else consult.origin
+    end as consultation_macro_origin,
+    case
+        when ht.entry_id is not null and ht.home_type is not null
+        then concat("home_", ht.home_type)
+        when ht.entry_id is not null and ht.home_type is null
+        then "home_without_tag"
+        when consult.origin = "search" and consult.search_query_input_is_generic is true
+        then "generic_query_search"
+        when
+            consult.origin = "search" and consult.search_query_input_is_generic is false
+        then "specific_query_search"
+        when consult.origin = "search" and consult.query is null
+        then "landing_search"
+        when consult.origin = "venue" and ov.consult_venue_origin = "offer"
+        then "offer_venue"
+        when
+            consult.origin = "venue" and ov.consult_venue_origin = "searchVenuePlaylist"
+        then "search_venue_playlist"
+        when consult.origin = "venue" and ov.consult_venue_origin = "searchAutoComplete"
+        then "search_venue_autocomplete"
+        when consult.origin = "venue" and ov.consult_venue_origin = "venueMap"
+        then "venue_map"
+        when
+            consult.origin = "venue"
+            and ov.consult_venue_origin in ("home", "venueList")
+        then "home_venue_playlist"
+        when consult.origin = "venue" and ov.consult_venue_origin = "deeplink"
+        then "venue_deeplink"
+        when consult.origin = "offer" and consult.multi_venue_offer_id is not null
+        then "multi_venue_offer"
+        when consult.origin = "similar_offer"
+        then concat("similar_offer_", so.consult_similar_offer_origin)
+        else consult.origin
+    end as consultation_micro_origin
+from {{ ref("int_firebase__native_consultation") }} as consult
+left join
+    discoveries_by_consultation as dc on dc.consultation_id = consult.consultation_id
+left join {{ ref("int_global__offer") }} as offer on consult.offer_id = offer.offer_id
+left join {{ ref("int_global__user") }} as user on consult.user_id = user.user_id
+left join {{ ref("int_contentful__home_tag") }} as ht on ht.entry_id = consult.entry_id
+left join
+    consult_offer_through_venue as ov
+    on ov.consultation_id = consult.consultation_id
+    and consult.origin = "venue"
+left join
+    consult_offer_through_similar_offer as so
+    on so.consultation_id = consult.consultation_id
+    and consult.origin = "similar_offer"
 
 {% if is_incremental() %}
-where consultation_date >= date_sub('{{ ds() }}', INTERVAL 3 day)
-{% else %}
-where consultation_date >= date_sub('{{ ds() }}', INTERVAL 1 year)
+    where consultation_date >= date_sub('{{ ds() }}', interval 3 day)
+{% else %} where consultation_date >= date_sub('{{ ds() }}', interval 1 year)
 {% endif %}
-

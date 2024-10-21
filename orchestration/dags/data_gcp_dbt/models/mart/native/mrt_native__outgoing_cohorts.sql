4978abbbf5f3b1f82f83dc09bfeb419995f9ca7c
Jules-Arbelot <115459312+Jules-Arbelot@users.noreply.github.com>
2024-10-21 16:20:47 +0200

    (DA-3315) feat(Analytics): Update outgoing_cohorts model (#3315)
    * Update outgoing_cohorts model
    
    * fix

--- orchestration/dags/data_gcp_dbt/models/mart/native/mrt_native__outgoing_cohorts.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/native/mrt_native__outgoing_cohorts.sql
@@ -32,61 +32,32 @@ with
             current_deposit_type = "GRANT_18"
             and last_deposit_expiration_date < date_trunc(current_date, month)
     ),
-    bookings_info as (
+    diversification_info as (
         select
             user.deposit_expiration_date,
             user.user_id,
-            sum(delta_diversification) as total_diversification,
-            sum(venue_id_diversification) as total_venue_id_diversification,
-            sum(
-                venue_type_label_diversification
-            ) as total_venue_type_label_diversification,
-            sum(category_diversification) as total_category_diversification
+            sum(delta_diversification) as total_diversification
         from users_expired_monthly user
         join
             {{ ref("diversification_booking") }} diversification_booking
             on user.user_id = diversification_booking.user_id
         group by deposit_expiration_date, user_id
     ),
-    weekly_active_user_compute as (
+    bookings_info as (
         select
-            deposit_expiration_date,
-            date_trunc(visits.first_event_date, week) connexion_week,
-            count(distinct visits.user_id) weekly_connected_users
-        from {{ ref("firebase_visits") }} visits
+            user.deposit_expiration_date,
+            user.user_id,
+            count(
+                distinct bookings.offer_category_id
+            ) as total_distinct_category_booked,
+            count(
+                distinct bookings.venue_type_label
+            ) as total_distinct_venue_type_booked,
+            count(distinct bookings.venue_id) as total_distinct_venue_booked
+        from users_expired_monthly user
         join
-            users_expired_monthly
-            on visits.user_id = users_expired_monthly.user_id
-            and date_sub(deposit_expiration_date, interval 2 year)
-            <= visits.first_event_date
-            and deposit_expiration_date >= visits.first_event_date
-        group by deposit_expiration_date, connexion_week
-    ),
-    weekly_active_user as (
-        select
-            deposit_expiration_date, avg(weekly_connected_users) as weekly_active_user
-        from weekly_active_user_compute
-        group by deposit_expiration_date
-    ),
-    monthly_active_user_compute as (
-        select
-            deposit_expiration_date,
-            date_trunc(first_event_date, month) connexion_month,
-            count(distinct visits.user_id) monthly_connected_users
-        from {{ ref("firebase_visits") }} visits
-        join
-            users_expired_monthly
-            on visits.user_id = users_expired_monthly.user_id
-            and date_sub(deposit_expiration_date, interval 2 year)
-            <= visits.first_event_date
-            and deposit_expiration_date >= visits.first_event_date
-        group by deposit_expiration_date, connexion_month
-    ),
-    monthly_active_user as (
-        select
-            deposit_expiration_date, avg(monthly_connected_users) as monthly_active_user
-        from monthly_active_user_compute
-        group by deposit_expiration_date
+            {{ ref("mrt_global__booking") }} bookings on bookings.user_id = user.user_id
+        group by deposit_expiration_date, user_id
     ),
     consultations as (
         select
@@ -120,48 +91,56 @@ select
     u.user_activity,
     u.user_civility,
     u.is_theme_subscribed,
-    count(distinct u.user_id) as total_users,
-    count(
-        distinct case when b.total_category_diversification >= 3 then u.user_id end
+    coalesce(count(distinct u.user_id), 0) as total_users,
+    coalesce(
+        count(
+            distinct case when b.total_distinct_category_booked >= 3 then u.user_id end
+        ),
+        0
     ) total_3_category_booked_users,
-    sum(u.total_actual_amount_spent) as total_amount_spent,
-    sum(
-        u.total_theoretical_digital_goods_amount_spent
+    coalesce(sum(u.total_actual_amount_spent), 0) as total_amount_spent,
+    coalesce(
+        sum(u.total_theoretical_digital_goods_amount_spent), 0
     ) as total_theoretical_digital_goods_amount_spent,
-    sum(
-        u.total_non_cancelled_individual_bookings
+    coalesce(
+        sum(u.total_non_cancelled_individual_bookings), 0
     ) as total_non_cancelled_individual_bookings,
-    sum(u.total_non_cancelled_duo_bookings) as total_non_cancelled_duo_bookings,
-    sum(u.total_free_bookings) as total_free_bookings,
-    sum(total_item_consulted) as total_item_consulted,
-    sum(total_venue_consulted) as total_venue_consulted,
-    avg(u.total_distinct_grant_18_booking_types) as avg_grant_18_subcategory_booked,
-    avg(
-        u.total_distinct_grant_15_17_booking_types
-    ) as avg_grant_15_17_subcategory_booked,
-    avg(total_venue_type_label_consulted) as avg_venue_type_label_consulted,
-    avg(
-        date_diff(u.first_individual_booking_date, u.first_deposit_creation_date, day)
-    ) as avg_day_between_deposit_and_first_booking,
-    avg(b.total_diversification) as avg_diversification_score,
-    avg(b.total_venue_id_diversification) as avg_venue_id_diversification_score,
-    avg(
-        b.total_venue_type_label_diversification
-    ) as avg_venue_type_label_diversification_score,
-    avg(b.total_category_diversification) as avg_category_diversification_score,
-    avg(weekly_active_user.weekly_active_user) as weekly_active_user,
-    avg(monthly_active_user.monthly_active_user) as monthly_active_user
+    coalesce(
+        sum(u.total_non_cancelled_duo_bookings), 0
+    ) as total_non_cancelled_duo_bookings,
+    coalesce(sum(u.total_free_bookings), 0) as total_free_bookings,
+    coalesce(sum(total_item_consulted), 0) as total_item_consulted,
+    coalesce(sum(total_venue_consulted), 0) as total_venue_consulted,
+    coalesce(
+        sum(u.total_distinct_grant_18_booking_types), 0
+    ) as total_grant_18_subcategory_booked,
+    coalesce(
+        sum(u.total_distinct_grant_15_17_booking_types), 0
+    ) as total_grant_15_17_subcategory_booked,
+    coalesce(
+        sum(total_venue_type_label_consulted), 0
+    ) as total_venue_type_label_consulted,
+    coalesce(
+        sum(
+            date_diff(
+                u.first_individual_booking_date, u.first_deposit_creation_date, day
+            )
+        ),
+        0
+    ) as total_day_between_deposit_and_first_booking,
+    coalesce(sum(d.total_diversification), 0) as total_diversification_score,
+    coalesce(sum(b.total_distinct_venue_booked), 0) as total_venue_id_booked,
+    coalesce(sum(b.total_distinct_venue_type_booked), 0) as total_venue_type_booked,
+    coalesce(sum(b.total_distinct_category_booked), 0) as total_category_booked
 from users_expired_monthly u
-left join
-    weekly_active_user
-    on weekly_active_user.deposit_expiration_date = u.deposit_expiration_date
-left join
-    monthly_active_user
-    on monthly_active_user.deposit_expiration_date = u.deposit_expiration_date
 left join
     consultations c
     on u.user_id = c.user_id
     and u.deposit_expiration_date = c.deposit_expiration_date
+left join
+    diversification_info d
+    on u.user_id = d.user_id
+    and u.deposit_expiration_date = d.deposit_expiration_date
 left join
     bookings_info b
     on u.user_id = b.user_id

4f09d258070673c550d2f0e324b0ed902cc0ba5a
valoumiaou <valentin.busson@passculture.app>
2024-10-22 15:42:49 +0200

    review: use airflow operator instead of custom vm job

--- orchestration/dags/jobs/export/export_applicative_snapshot.py
+++ orchestration/dags/jobs/export/export_applicative_snapshot.py
@@ -1,6 +1,6 @@
 import datetime
 
-from common import macros
+from common.alerts import task_fail_slack_alert
 from common.config import (
     BIGQUERY_INT_RAW_DATASET,
     DAG_FOLDER,
@@ -8,21 +8,18 @@ from common.config import (
     ENV_SHORT_NAME,
     GCP_PROJECT_ID,
 )
-from common.operators.gce import (
-    InstallDependenciesOperator,
-    SSHGCEOperator,
-    StartGCEOperator,
-    StopGCEOperator,
-)
 from common.utils import (
     delayed_waiting_operator,
     get_airflow_schedule,
     get_tables_config_dict,
 )
+from jobs.crons import schedule_dict
 
 from airflow import DAG
-from airflow.models import Param
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.google.cloud.transfers.bigquery_to_gcs import (
+    BigQueryToGCSOperator,
+)
 from airflow.utils.task_group import TaskGroup
 
 GCE_INSTANCE = f"historize-applicative-database-{ENV_SHORT_NAME}"
@@ -36,92 +33,51 @@ SNAPSHOT_TABLES = get_tables_config_dict(
     dbt_alias=True,
 )
 
-dag_config = {
-    "GCP_PROJECT": GCP_PROJECT_ID,
-    "ENV_SHORT_NAME": ENV_SHORT_NAME,
-}
-
 default_dag_args = {
     "start_date": datetime.datetime(2020, 12, 1),
     "retries": 6,
-    # "on_failure_callback": task_fail_slack_alert,
-    "on_failure_callback": None,
+    "on_failure_callback": task_fail_slack_alert,
     "retry_delay": datetime.timedelta(minutes=5),
     "project_id": GCP_PROJECT_ID,
 }
 
 
+dag_id = "historize_applicative_database"
 dag = DAG(
-    "historize_applicative_database",
+    dag_id,
     default_args=default_dag_args,
-    description="historize applicative database",
-    schedule_interval=get_airflow_schedule("0 1 * * *"),
-    catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=480),
-    user_defined_macros=macros.default,
-    template_searchpath=DAG_FOLDER,
-    tags=["VM"],
-    params={
-        "branch": Param(
-            default="production" if ENV_SHORT_NAME == "prod" else "master",
-            type="string",
-        ),
-    },
+    description="historize applicative database current state to gcs bucket",
+    schedule_interval=get_airflow_schedule(schedule_dict[dag_id]),
+    catchup=False,
 )
 
 start = DummyOperator(task_id="start", dag=dag)
 
-with TaskGroup(group_id="wait_transfo", dag=dag) as wait:
-    for table_name in SNAPSHOT_TABLES.keys():
+with TaskGroup(group_id="snapshots_to_gcs", dag=dag) as to_gcs:
+    for table_name, bq_config in SNAPSHOT_TABLES.items():
+        alias = bq_config["table_alias"]
+        dataset = bq_config["source_dataset"]
+        _now = datetime.datetime.now()
+        yyyymmdd = _now.strftime("%Y%m%d")
         waiting_task = delayed_waiting_operator(
             dag,
             external_dag_id="dbt_run_dag",
             external_task_id=f"data_transformation.{table_name}",
         )
-
-
-gce_instance_start = StartGCEOperator(
-    instance_name=GCE_INSTANCE,
-    task_id="gce_start_task",
-    dag=dag,
-    preemptible=False,
-)
-fetch_install_code = InstallDependenciesOperator(
-    task_id="fetch_install_code",
-    instance_name=GCE_INSTANCE,
-    branch="{{ params.branch }}",
-    installer="uv",
-    python_version="3.10",
-    base_dir=BASE_PATH,
-    dag=dag,
-)
-with TaskGroup(group_id="applicatives_to_gcs", dag=dag) as tables_to_gcs:
-    for table_name, bq_config in SNAPSHOT_TABLES.items():
-        alias = bq_config["table_alias"]
-        dataset = bq_config["source_dataset"]
-        table_to_gcs = SSHGCEOperator(
-            task_id=f"snapshot_to_gcs_{table_name}",
-            instance_name=GCE_INSTANCE,
-            base_dir=BASE_PATH,
-            environment=dag_config,
-            command=f"python main.py --table-name {alias} --table-dataset {dataset} --project-id {GCP_PROJECT_ID} --gcs-bucket {DATA_GCS_BUCKET_NAME}",
-            do_xcom_push=False,
-            installer="uv",
+        export_bigquery_to_gcs = BigQueryToGCSOperator(
+            task_id=f"export_bq_to_gcs_{table_name}",
+            source_project_dataset_table=f"{GCP_PROJECT_ID}.{dataset}.{alias}",
+            destination_cloud_storage_uris=[
+                f"gs://{DATA_GCS_BUCKET_NAME}/historization/applicative/{alias}/{yyyymmdd}/*.parquet"
+            ],
+            export_format="PARQUET",
+            field_delimiter=",",
+            print_header=True,
             dag=dag,
         )
+        waiting_task >> export_bigquery_to_gcs
 
+end = DummyOperator(task_id="end", dag=dag)
 
-gce_instance_stop = StopGCEOperator(
-    task_id="gce_stop_task",
-    instance_name=GCE_INSTANCE,
-    dag=dag,
-)
-
-(
-    start
-    >> wait
-    >> gce_instance_start
-    >> fetch_install_code
-    >> tables_to_gcs
-    >> gce_instance_stop
-)
+start >> to_gcs >> end

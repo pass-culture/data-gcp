1c2b957efcf214af1afd9405ce480213b6023b5a
valoumiaou <valentin.busson@passculture.app>
2024-10-22 12:25:22 +0200

    refactor dag historize applicative

--- orchestration/dags/jobs/export/export_applicative_snapshot.py
+++ orchestration/dags/jobs/export/export_applicative_snapshot.py
@@ -1,20 +1,23 @@
 import datetime
-from pathlib import Path
 
 from common import macros
 from common.config import (
+    BIGQUERY_INT_RAW_DATASET,
     DAG_FOLDER,
+    DATA_GCS_BUCKET_NAME,
     ENV_SHORT_NAME,
     GCP_PROJECT_ID,
-    PATH_TO_DBT_PROJECT,
 )
 from common.operators.gce import (
     InstallDependenciesOperator,
     SSHGCEOperator,
     StartGCEOperator,
-    StopGCEOperator,
 )
-from common.utils import delayed_waiting_operator, get_airflow_schedule
+from common.utils import (
+    delayed_waiting_operator,
+    get_airflow_schedule,
+    get_tables_config_dict,
+)
 
 from airflow import DAG
 from airflow.models import Param
@@ -23,6 +26,18 @@ from airflow.utils.task_group import TaskGroup
 
 GCE_INSTANCE = f"historize-applicative-database-{ENV_SHORT_NAME}"
 BASE_PATH = "data-gcp/jobs/etl_jobs/internal/export_applicative"
+
+DEV_BRANCH = "DE-577/migration-history-snapshot/gcs-historization-applicative"
+
+
+SNAPSHOT_MODELS_PATH = "data_gcp_dbt/models/intermediate/raw"
+
+
+SNAPSHOT_TABLES = get_tables_config_dict(
+    PATH=DAG_FOLDER + "/" + SNAPSHOT_MODELS_PATH,
+    BQ_DATASET=BIGQUERY_INT_RAW_DATASET,
+    is_source=True,
+)
 
 dag_config = {
     "GCP_PROJECT": GCP_PROJECT_ID,
@@ -38,9 +53,6 @@ default_dag_args = {
     "project_id": GCP_PROJECT_ID,
 }
 
-raw_directory = Path(PATH_TO_DBT_PROJECT + "models/intermediate/raw")
-sql_files = list(raw_directory.glob("*.sql"))
-SNAPSHOTS = {}
 
 dag = DAG(
     "historize_applicative_database",
@@ -54,7 +66,7 @@ dag = DAG(
     tags=["VM"],
     params={
         "branch": Param(
-            default="production" if ENV_SHORT_NAME == "prod" else "master",
+            default="production" if ENV_SHORT_NAME == "prod" else DEV_BRANCH,
             type="string",
         ),
     },
@@ -63,13 +75,13 @@ dag = DAG(
 start = DummyOperator(task_id="start", dag=dag)
 
 with TaskGroup(group_id="wait_transfo", dag=dag) as wait:
-    for sql_file in sql_files:
-        table_name = sql_file.stem
+    for table_name in SNAPSHOT_TABLES.keys():
         waiting_task = delayed_waiting_operator(
             dag,
             external_dag_id="dbt_run_dag",
             external_task_id=f"data_transformation.{table_name}",
         )
+
 
 gce_instance_start = StartGCEOperator(
     instance_name=GCE_INSTANCE,
@@ -86,26 +98,26 @@ fetch_install_code = InstallDependenciesOperator(
     dag=dag,
 )
 with TaskGroup(group_id="applicatives_to_gcs", dag=dag) as tables_to_gcs:
-    for sql_file in sql_files:
-        table_name = sql_file.stem
+    for table_name, bq_config in SNAPSHOT_TABLES.items():
         table_to_gcs = SSHGCEOperator(
-            task_id="snapshot_to_gcs",
+            task_id=f"snapshot_to_gcs_{table_name}",
             instance_name=GCE_INSTANCE,
             base_dir=BASE_PATH,
             environment=dag_config,
-            command=f"python main.py {table_name}",
+            command=f"python main.py {table_name} {bq_config['source_table']} {bq_config['source_dataset']} {GCP_PROJECT_ID} {DATA_GCS_BUCKET_NAME}",
             do_xcom_push=False,
             installer="uv",
             dag=dag,
         )
 
-gce_instance_stop = StopGCEOperator(
-    task_id="gce_stop_task",
-    instance_name=GCE_INSTANCE,
-    dag=dag,
-)
 
+# gce_instance_stop = StopGCEOperator(
+#     task_id="gce_stop_task",
+#     instance_name=GCE_INSTANCE,
+#     dag=dag,
+# )
 
+gce_instance_stop = DummyOperator(task_id="fake_gce_stop_task_for_dev", dag=dag)
 (
     start
     >> wait

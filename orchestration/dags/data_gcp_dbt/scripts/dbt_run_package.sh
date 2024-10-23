c98aae62f36f7d7cdef61b611568fc4ad7492525
valentin <142802218+valoumiaou@users.noreply.github.com>
2024-10-23 15:46:06 +0200

    (DE-624) fix(elementary): fix tmp artifacts (#3320)
    * BSR: remove useless scripts
    
    * tweak elementary report flags

--- orchestration/dags/data_gcp_dbt/scripts/dbt_run_package.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-dbt run --models package:$package_name --target $target --vars "{ENV_SHORT_NAME: $ENV_SHORT_NAME}" $full_ref_str --target-path $PATH_TO_DBT_TARGET $GLOBAL_CLI_FLAGS
-
--- orchestration/dags/jobs/dbt/dbt_artifacts.py
+++ orchestration/dags/jobs/dbt/dbt_artifacts.py
@@ -45,6 +45,7 @@ dag = DAG(
             type="string",
         ),
     },
+    tags=["DBT"],
 )
 
 start = DummyOperator(task_id="start", dag=dag)
@@ -54,7 +55,8 @@ wait_dbt_run = delayed_waiting_operator(dag=dag, external_dag_id="dbt_run_dag")
 compute_metrics_elementary = BashOperator(
     task_id="compute_metrics_elementary",
     bash_command="dbt run --no-write-json --target {{ params.target }} --select package:elementary "
-    + f"--target-path {PATH_TO_DBT_TARGET}",
+    + f"--target-path {PATH_TO_DBT_TARGET} --profiles-dir {PATH_TO_DBT_PROJECT} "
+    + """--vars "{{ "{" }}'ENV_SHORT_NAME':'{{ params.target }}'{{ "}" }}" """,
     cwd=PATH_TO_DBT_PROJECT,
     dag=dag,
 )
@@ -78,14 +80,15 @@ send_elementary_report = BashOperator(
     task_id="send_elementary_report",
     bash_command=f"bash {PATH_TO_DBT_PROJECT}/scripts/elementary_send_report.sh ",
     env={
-        "PATH_TO_DBT_TARGET": PATH_TO_DBT_TARGET,
         "PATH_TO_DBT_PROJECT": PATH_TO_DBT_PROJECT,
+        "PATH_TO_DBT_TARGET": PATH_TO_DBT_TARGET,
         "ENV_SHORT_NAME": ENV_SHORT_NAME,
         "DATA_BUCKET_NAME": DATA_GCS_BUCKET_NAME,
         "REPORT_FILE_PATH": "elementary_reports/{{ execution_date.year }}/elementary_report_{{ execution_date.strftime('%Y%m%d') }}.html",
         "SLACK_TOKEN": SLACK_TOKEN_ELEMENTARY,
         "CHANNEL_NAME": SLACK_CHANNEL,
         "ELEMENTARY_PYTHON_PATH": ELEMENTARY_PYTHON_PATH,
+        "GLOBAL_CLI_FLAGS": "{{ params.GLOBAL_CLI_FLAGS }}",
     },
     append_env=True,
     cwd=PATH_TO_DBT_PROJECT,
@@ -98,6 +101,7 @@ compile = BashOperator(
     env={
         "target": "{{ params.target }}",
         "PATH_TO_DBT_TARGET": PATH_TO_DBT_TARGET,
+        "ENV_SHORT_NAME": ENV_SHORT_NAME,
     },
     append_env=True,
     cwd=PATH_TO_DBT_PROJECT,

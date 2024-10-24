6915006d1c5e0886c8609e2a610f212117f7bac4
LucileRainteau <114910642+LucileRainteau@users.noreply.github.com>
2024-10-24 18:15:00 +0200

    (HF) fix(metabase): fix dependency table (#3339)
    * (DE) fix(metabase) fix dependency table
    
    * improve import schema

--- jobs/etl_jobs/external/metabase-archiving/dependencies.py
+++ jobs/etl_jobs/external/metabase-archiving/dependencies.py
@@ -119,12 +119,7 @@ def get_native_dependencies(cards_list, tables_df):
         sql_lines = card["dataset_query"]["native"]["query"].lower()
         sql_lines = sql_lines.replace("`", "")
         table_dependency = re.findall(regex, sql_lines)
-        table_dependency = list(set(table_dependency)) + list(set(table_dependency))
-        if len(table_dependency) == 1:
-            table = table_dependency[0]
-            match_schema = re.search(r"(from|join)\s+(\w+)", table)
-            if match_schema:
-                table_schema = match_schema.group(2)
+        table_dependency = list(set(table_dependency))
 
         dependency = dict()
         dependency["card_id"] = card_id
@@ -132,14 +127,21 @@ def get_native_dependencies(cards_list, tables_df):
         dependency["card_type"] = card_type
         dependency["card_owner"] = card_owner
         dependency["table_name"] = [table.split(".")[-1] for table in table_dependency]
-        dependency["table_schema"] = table_schema
+        table_schema_list = []
+        for dep in table_dependency:
+            match_schema = re.search(r"(from|join)\s+(\w+)", dep)
+            if match_schema:
+                table_schema = match_schema.group(2)
+                table_schema_list.append(table_schema)
+
+        dependency["table_schema"] = table_schema_list
 
         dependencies_native[i] = dependency
         i += 1
 
     dependencies_native_df = (
         pd.DataFrame.from_dict(dependencies_native, orient="index")
-        .explode("table_name")
+        .explode(["table_name", "table_schema"])
         .reset_index(drop=True)
         .merge(tables_df, how="left", on=["table_schema", "table_name"])
     )
@@ -168,7 +170,7 @@ def run():
         table_name=lambda _df: _df.table_name.astype(str),
         card_owner=lambda _df: _df.card_owner.astype(str),
         table_id=lambda _df: _df.table_id.astype(str),
-        schema=lambda _df: _df.schema.astype(str),
+        table_schema=lambda _df: _df.table_schema.astype(str),
     )
 
     dependencies_df.to_gbq(

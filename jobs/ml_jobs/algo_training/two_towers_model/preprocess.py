ed88ea551e51ec6982736f6be2ba50a0dbf9f70a
dcuesta-pass <daniel.cuesta@passculture.app>
2024-10-29 15:35:41 +0100

    [DE-749] Add timestamp discretization

--- jobs/ml_jobs/algo_training/two_towers_model/preprocess.py
+++ jobs/ml_jobs/algo_training/two_towers_model/preprocess.py
@@ -16,40 +16,6 @@ def get_features_by_type(feature_layers: dict, layer_types: list):
     ]
 
 
-def collect_prev_next(group):
-    import numpy as np
-
-    # Sort the group by 'event_date'
-    sub_df = group[["item_id", "event_date"]]
-    # Sort the group by 'event_date'
-    sub_df = sub_df.sort_values("event_date").reset_index(drop=True)
-
-    # Get list of booking_ids
-    booking_ids = sub_df["item_id"].values
-
-    # Create arrays to hold previous and next booking ids
-    previous_booking_ids = np.empty(len(booking_ids), dtype=object)
-    next_booking_ids = np.empty(len(booking_ids), dtype=object)
-
-    # Fill previous_booking_ids
-    for i in tqdm(
-        range(len(booking_ids)),
-        total=booking_ids.shape[0],
-        desc="Processing previous ids",
-        mininterval=30,
-    ):
-        previous_booking_ids[i] = booking_ids[max(i - 10, 0) : i].tolist()
-
-    # Fill next_booking_ids
-    next_booking_ids[:-1] = booking_ids[1:]
-    next_booking_ids[-1] = None
-
-    # Assign the previous and next booking ids to the group DataFrame
-    group["previous_item_id"] = previous_booking_ids
-    group["next_item_id"] = next_booking_ids
-    return group
-
-
 def preprocess(
     config_file_name: str = typer.Option(
         ...,
@@ -104,23 +70,10 @@ def preprocess(
         .fillna({col: 0 for col in integer_features})
         .astype({col: "int" for col in integer_features})
     )
-    # clean_data["timestamp"] = clean_data["event_date"].apply(pd.Timestamp)
+    logger.info("Converting 'event_date' to timestamp..")
     clean_data["timestamp"] = pd.to_datetime(clean_data["event_date"])
     clean_data["timestamp"] = clean_data["timestamp"].astype(np.int64) // 10**9
-    # booking_data=clean_data[clean_data['event_type'] == 'BOOKING']
-    # Convert 'event_date' from Unix timestamp to datetime for sorting
 
-    clean_data["event_date"] = pd.to_datetime(clean_data["event_date"])
-
-    # # Step 1: Sort the DataFrame by 'user_id' and 'event_date'
-    clean_data = clean_data.sort_values(by=["user_id", "event_date"]).reset_index(
-        drop=True
-    )
-    logger.info(f"Data shape after preprocessing: {clean_data.shape}")
-    logger.info("Collecting previous and next booking_ids..")
-    clean_data = (
-        clean_data.groupby("user_id").apply(collect_prev_next).reset_index(drop=True)
-    )
     clean_data = clean_data.sample(frac=1).reset_index(drop=True)
     ## Add sequential bookings_ids
     clean_data.to_parquet(f"{STORAGE_PATH}/{output_dataframe_file_name}/data.parquet")

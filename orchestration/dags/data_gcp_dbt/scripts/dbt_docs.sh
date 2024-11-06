0bca6c1f06f96ba30d3701988325e33649afccab
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2024-11-06 09:16:03 +0100

    wip

--- orchestration/dags/data_gcp_dbt/scripts/dbt_docs.sh
+++ orchestration/dags/data_gcp_dbt/scripts/dbt_docs.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# Check if manifest.json exists
-if [ -f "target/manifest.json" ]; then
-    # Run the Python script to hide dbt package documentation
-    python scripts/hide_macros.py
-    echo "dbt package visibility updated in manifest.json."
-else
-    echo "Error: manifest.json not found. Make sure dbt has successfully compiled."
-fi
+dbt docs generate --target='prod'
 
-dbt docs generate && dbt docs serve
+dbt docs serve

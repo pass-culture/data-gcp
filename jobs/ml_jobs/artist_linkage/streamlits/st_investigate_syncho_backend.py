7ee42543818604a8e53d47d139588b2e293b2d1f
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2024-11-06 18:21:09 +0100

    feat: Add artist creation for unlinked products

--- jobs/ml_jobs/artist_linkage/streamlits/st_investigate_syncho_backend.py
+++ jobs/ml_jobs/artist_linkage/streamlits/st_investigate_syncho_backend.py
@@ -12,17 +12,24 @@ DEFAULT_EXPANDER_STATE = False
 MERGE_COLUMNS = ["product_id", "artist_type"]
 st.set_page_config(layout="wide")
 
+ARTIST_NAME_TO_FILTER = {
+    "multi-artistes",
+    "xxx",
+    "compilation",
+    "tbc",
+}
+
 
 @st.cache_data
 def load_artist_data() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     artist_df = pd.read_parquet(
-        "/home/laurent_pass/Projects/data-gcp/jobs/ml_jobs/artist_linkage/streamlits/data/artist_linkage_stg_artist_table.parquet"
+        "streamlits/data/artist_linkage_stg_artist_table.parquet"
     )
     alias_df = pd.read_parquet(
-        "/home/laurent_pass/Projects/data-gcp/jobs/ml_jobs/artist_linkage/streamlits/data/artist_linkage_stg_artist_alias_table.parquet"
+        "streamlits/data/artist_linkage_stg_artist_alias_table.parquet"
     )
     product_artist_link_df = pd.read_parquet(
-        "/home/laurent_pass/Projects/data-gcp/jobs/ml_jobs/artist_linkage/streamlits/data/artist_linkage_stg_product_artist_link_table.parquet"
+        "streamlits/data/artist_linkage_stg_product_artist_link_table.parquet"
     ).astype({"product_id": int})
 
     # TODO: remove that in the tables
@@ -41,6 +48,7 @@ def load_products() -> pd.DataFrame:
         .rename(columns={"offer_product_id": "product_id"})
         .astype({"product_id": int})
         .dropna(subset=["artist_name"])
+        .loc[lambda df: df.artist_name != ""]
         .drop_duplicates(
             subset=["product_id", "artist_type"]
         )  # To remove offers with the same product_id and artist_type
@@ -84,8 +92,10 @@ def preprocess_before_matching(df: pd.DataFrame) -> pd.DataFrame:
                 "offer_category_id",
                 "artist_name",
                 "artist_name_to_match",
+                "artist_id",
             ],
         )
+        .assign(artist_name_to_match=lambda df: df.artist_name_to_match.str.strip())
     )
 
 
@@ -158,11 +168,59 @@ st.divider()
 st.subheader("Preprocess names")
 
 ### Params
-preproc_products_to_link_df = preprocess_before_matching(products_to_link_df)
-preproc_artist_alias_df = preprocess_before_matching(
-    alias_df.rename(columns={"artist_alias_name": "artist_name"})
+preproc_products_to_link_df = preprocess_before_matching(products_to_link_df).loc[
+    lambda df: ~df.artist_name_to_match.isin(ARTIST_NAME_TO_FILTER)
+]
+preproc_artist_alias_df = (
+    preprocess_before_matching(
+        alias_df.rename(columns={"artist_alias_name": "artist_name"})
+    )
+    .drop(columns=["artist_name"])
+    .drop_duplicates()
 )
-with st.expander("Preprocessed names", expanded=DEFAULT_EXPANDER_STATE):
+with st.expander("Preprocessed products to link", expanded=DEFAULT_EXPANDER_STATE):
     st.write(preproc_products_to_link_df)
 with st.expander("Preprocessed aliases", expanded=DEFAULT_EXPANDER_STATE):
     st.write(preproc_artist_alias_df)
+
+# %% Match names
+st.subheader("Match names")
+matched_df = preproc_products_to_link_df.merge(
+    preproc_artist_alias_df,
+    how="left",
+    on=["artist_name_to_match", "artist_type", "offer_category_id"],
+)
+linked_products_df = matched_df.loc[lambda df: df.artist_id.notna()]
+unlinked_products_df = matched_df.loc[lambda df: df.artist_id.isna()]
+
+with st.expander(f"Matched names ({len(matched_df)})", expanded=DEFAULT_EXPANDER_STATE):
+    st.write(matched_df)
+with st.expander(
+    f"Linked products ({len(linked_products_df)})", expanded=DEFAULT_EXPANDER_STATE
+):
+    st.write(linked_products_df)
+with st.expander(
+    f"Products to create artists ({len(unlinked_products_df)})",
+    expanded=DEFAULT_EXPANDER_STATE,
+):
+    st.write(unlinked_products_df.sort_values(by=["artist_name_to_match"]))
+
+
+# %% Create artists for unlinked products
+st.subheader("Create artists for unlinked products")
+count_df = (
+    unlinked_products_df.groupby(
+        ["artist_name_to_match", "artist_type"], as_index=False
+    )
+    .agg({"product_id": "count", "artist_name_to_match": "first"})
+    .rename(columns={"product_id": "number_of_products"})
+)
+with st.expander(
+    "Number of artists to create",
+    expanded=DEFAULT_EXPANDER_STATE,
+):
+    cols = st.columns(2)
+    with cols[0]:
+        st.write(count_df.sort_values(by=["number_of_products"], ascending=False))
+    with cols[1]:
+        st.write(count_df.number_of_products.value_counts())

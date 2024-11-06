4e717fac9be273d3c1267394058de074037c044d
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2024-11-06 14:35:00 +0100

    refactor: small refacto

--- jobs/ml_jobs/artist_linkage/streamlits/st_investigate_syncho_backend.py
+++ jobs/ml_jobs/artist_linkage/streamlits/st_investigate_syncho_backend.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 
 
 @st.cache_data
-def load_artist_data() -> pd.DataFrame:
+def load_artist_data() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     artist_df = pd.read_parquet(
         "/home/laurent_pass/Projects/data-gcp/jobs/ml_jobs/artist_linkage/streamlits/data/artist_linkage_stg_artist_table.parquet"
     )
@@ -14,7 +14,7 @@ def load_artist_data() -> pd.DataFrame:
     )
     product_artist_link_df = pd.read_parquet(
         "/home/laurent_pass/Projects/data-gcp/jobs/ml_jobs/artist_linkage/streamlits/data/artist_linkage_stg_product_artist_link_table.parquet"
-    )
+    ).astype({"product_id": int})
 
     # TODO: remove that in the tables
     artist_df = artist_df.dropna(subset=["id"])
@@ -25,17 +25,50 @@ def load_artist_data() -> pd.DataFrame:
 
 @st.cache_data
 def load_products() -> pd.DataFrame:
-    return pd.read_csv(
-        "/home/laurent_pass/Téléchargements/artist_linkage_stg_tmp_artist_product.csv"
+    return (
+        pd.read_parquet(
+            "/home/laurent_pass/Téléchargements/artist_linkage_stg_tmp_artist_product.parquet"
+        )
+        .astype({"offer_product_id": int})
+        .dropna(subset=["artist_name"])
     )
-    # return pd.read_parquet(
-    #     "/home/laurent_pass/Téléchargements/artist_linkage_prod_tmp_artist_product.parquet"
-    # ).sample(frac=0.05)
 
+
+@st.cache_data
+def remove_data(product_artist_link_df):
+    return product_artist_link_df.sample(frac=0.95)
+
+
+def count_matched_products(products_df, product_artist_link_df):
+    actual_product_ids = (
+        products_df.loc[:, ["offer_product_id", "artist_type"]].reset_index(drop=True)
+    ).astype({"offer_product_id": int, "artist_type": str})
+    linked_product_ids = product_artist_link_df.loc[
+        :, ["product_id", "artist_type"]
+    ].astype({"product_id": int, "artist_type": str})
+    return actual_product_ids.merge(
+        linked_product_ids,
+        how="outer",
+        left_on=["offer_product_id", "artist_type"],
+        right_on=["product_id", "artist_type"],
+        indicator=True,
+    ).replace(
+        {
+            "_merge": {
+                "left_only": "Not matched with artists",
+                "right_only": "Removed products",
+                "both": "Matched with artists",
+            }
+        }
+    )
+
+
+#################################################################################
 
 # %% Load DATA
-artist_df, alias_df, product_artist_link_df = load_artist_data()
-products_df = load_products()
+st.subheader("Load Data")
+artist_df, alias_df, raw_product_artist_link_df = load_artist_data()
+raw_products_df = load_products()
 
 
 # %% Show Dataframes
@@ -46,10 +79,28 @@ with st.expander("Artist Alias Table", expanded=True):
     st.dataframe(alias_df)
 
 with st.expander("Product Artist Link Table", expanded=True):
-    st.dataframe(product_artist_link_df)
+    st.dataframe(raw_product_artist_link_df.sort_values(by=["product_id"]))
 
 with st.expander("Product Table", expanded=True):
-    st.dataframe(products_df)
+    st.dataframe(raw_products_df.sort_values(by=["offer_product_id"]))
 st.divider()
 
+# %% Check previous matching
+st.subheader("Check previous matching")
+merged_df = count_matched_products(raw_products_df, raw_product_artist_link_df)
+st.dataframe(merged_df)
+st.write(merged_df._merge.value_counts().reset_index())
+st.divider()
+
+# %% Remove data to simulate real life
+products_df = raw_products_df.pipe(remove_data)
+product_artist_link_df = raw_product_artist_link_df.pipe(remove_data)
+st.dataframe(products_df)
+st.dataframe(product_artist_link_df)
+
+merged_df = count_matched_products(products_df, product_artist_link_df)
+st.dataframe(merged_df)
+st.write(merged_df._merge.value_counts().reset_index())
+
 # %% Do the Magic
+st.subheader("Split the Dataframe into the different options")

d33a6270cc8db74367f0d17d9e3a8bd4b2ffc57f
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2024-11-08 11:45:17 +0100

    (DE-778) test(firebase): generic test missing partitions + test firebase events (#3409)
    * check_partition_count macro
    
    * macro: check missing partition date if wrong count
    
    * articulate the two macros in a generic test
    
    * test firebase events at begining of transformation
    
    * Review: fix logic
    
    * order missing dates for readability
    
    * Exclude generic test from CI as this test Tables while CI only supports Views
    
    * refacto test with information schema query
    
    * test pro events & remove where_condition

---
+++ orchestration/dags/data_gcp_dbt/tests/generic/generic_has_no_missing_partitions.sql
@@ -0,0 +1,34 @@
+{% test generic_has_no_missing_partitions(model, column_name, where_condition=null) %}
+    -- Exclude from CI as this test Tables while CI only supports Views
+    {{ config(enabled=target.profile_name != "CI") }}
+
+    {% set missing_partitions = get_missing_date_partitions(
+        model, date_column_name=column_name, where_condition=where_condition
+    ) %}
+
+    {% if missing_partitions | length > 0 %}
+        with
+            missing_dates as (
+                select missing_partition_date, existing_partition_date
+                from
+                    unnest(
+                        [
+                            {% for partition in missing_partitions %}
+                                struct(
+                                    "{{ partition['missing_partition_date'] }}"
+                                    as missing_partition_date,
+                                    "{{ partition['existing_partition_date'] }}"
+                                    as existing_partition_date
+                                )
+                                {% if not loop.last %}, {% endif %}
+                            {% endfor %}
+                        ]
+                    )
+            )
+        select *
+        from missing_dates
+        order by missing_partition_date desc
+    {% else %} select {{ column_name }} from {{ model }} where true = false
+    {% endif %}
+
+{% endtest %}

40d107e6b0ede6cac6c0a59891f3b17b352a0c39
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2024-11-08 09:52:44 +0100

    refacto test with information schema query

--- orchestration/dags/data_gcp_dbt/macros/test_utils/get_missing_date_partitions.sql
+++ orchestration/dags/data_gcp_dbt/macros/test_utils/get_missing_date_partitions.sql
@@ -1,56 +1,73 @@
 {% macro get_missing_date_partitions(model, date_column_name, where_condition=null) %}
-    {% set has_missing_partitions = check_partition_count(
+    {% set has_missing_partitions = check_date_partition_count(
         model,
-        partition_column_name=date_column_name,
+        date_column_name,
         where_condition=where_condition,
     ) %}
 
     {% if has_missing_partitions and execute %}
 
+        {% set database_name = target.project %}
+        {% set schema_name = model.schema %}
+        {% set table_name = model.identifier %}
+
+        {% if where_condition %}
+            {% set adjusted_where_condition = where_condition.replace(
+                date_column_name, "partition_date"
+            ) %}
+        {% else %} {% set adjusted_where_condition = None %}
+        {% endif %}
+
         {% set query %}
-            with date_span as (
-                select
-                    min({{ date_column_name }}) as min_date,
-                    max({{ date_column_name }}) as max_date
-                from {{ model }}
+            WITH partitions AS (
+                SELECT
+                    SAFE.PARSE_DATE('%Y%m%d', partition_id) AS partition_date
+                FROM `{{ database_name }}.{{ schema_name }}.INFORMATION_SCHEMA.PARTITIONS`
+                WHERE table_name = '{{ table_name }}'
+                  AND SAFE.PARSE_DATE('%Y%m%d', partition_id) IS NOT NULL
+            ),
+            date_span AS (
+                SELECT
+                    MIN(partition_date) AS min_date,
+                    MAX(partition_date) AS max_date
+                FROM partitions
                 {% if where_condition %}
-                where {{ where_condition }}
+                    where {{ adjusted_where_condition }}
                 {% endif %}
             ),
-            all_dates as (
-                select
-                    date as date
-                from
+            all_dates AS (
+                SELECT
+                    date AS date
+                FROM
                     date_span,
-                    unnest(generate_date_array(date_span.min_date, date_span.max_date, INTERVAL 1 day)) as date
+                    UNNEST(GENERATE_DATE_ARRAY(date_span.min_date, date_span.max_date, INTERVAL 1 DAY)) AS date
+                WHERE date_span.min_date IS NOT NULL AND date_span.max_date IS NOT NULL
             ),
-            existing_partitions as (
-                select distinct {{ date_column_name }} as date
-                from {{ model }}
-                {% if where_condition %}
-                where {{ where_condition }}
-                {% endif %}
+            existing_partitions AS (
+                SELECT DISTINCT partition_date AS date
+                FROM partitions
             ),
-            unmatched_partitions as (
-                select
-                    all_dates.date as missing_partition_date,
-                    existing_partitions.date as existing_partition_date
-                from
+            missing_partitions AS (
+                SELECT
+                    all_dates.date AS missing_partition_date
+                FROM
                     all_dates
-                full outer join
+                LEFT JOIN
                     existing_partitions
-                on all_dates.date = existing_partitions.date
-                where
-                    existing_partitions.date is null or all_dates.date is null
+                ON all_dates.date = existing_partitions.date
+                WHERE
+                    existing_partitions.date IS NULL
             )
-            select missing_partition_date, existing_partition_date from unmatched_partitions
+            SELECT missing_partition_date FROM missing_partitions
         {% endset %}
 
         {% set result = run_query(query) %}
         {% set output = [] %}
 
         {% if result is not none and result.rows | length > 0 %}
-            {% set output = result.rows %}
+            {% for row in result.rows %}
+                {% do output.append(row["missing_partition_date"]) %}
+            {% endfor %}
         {% endif %}
 
         {{ return(output) }}

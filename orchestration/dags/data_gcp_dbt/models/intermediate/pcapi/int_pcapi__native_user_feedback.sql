c29bb169cda367ea9a2959f6809ab9c185f4351b
tscheurer-pass <timothee.scheurer@passculture.app>
2024-11-08 16:03:47 +0100

    (DA-3785) feat(Analytics): Add int_pcapi__native_user_feedback model (#3367)

---
+++ orchestration/dags/data_gcp_dbt/models/intermediate/pcapi/int_pcapi__native_user_feedback.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        **custom_incremental_config(
+            incremental_strategy="insert_overwrite",
+            partition_by={
+                "field": "user_feedback_creation_date",
+                "data_type": "date",
+                "granularity": "day",
+            },
+            on_schema_change="sync_all_columns",
+        )
+    )
+}}
+
+select
+    partition_date as user_feedback_creation_date,
+    user_id,
+    user_age,
+    total_bookings,
+    user_first_deposit_activation_date,
+    user_status,
+    user_feedback_message
+from {{ ref("int_pcapi__log") }}
+where
+    technical_message_id = 'user_feedback'
+    {% if is_incremental() %}
+        and partition_date
+        between date_sub(date("{{ ds() }}"), interval 1 day) and date("{{ ds() }}")
+    {% else %} and partition_date >= "2024-08-01"  -- feature deployment date
+    {% endif %}

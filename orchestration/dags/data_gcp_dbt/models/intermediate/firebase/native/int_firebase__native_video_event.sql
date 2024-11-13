c2392523c1a69a41a0a2b13b6b493e066bb72a90
Julpass <jules.arbelot@passculture.app>
2024-11-13 18:21:59 +0100

    refacto native_video_events

--- orchestration/dags/data_gcp_dbt/models/intermediate/firebase/native/int_firebase__native_video_event.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/firebase/native/int_firebase__native_video_event.sql
@@ -12,28 +12,56 @@
     )
 }}
 
+with
+    all_seen as (
+        select unique_session_id, user_id, video_id
+        from int_firebase_prod.native_event ne
+        join analytics_prod.contentful_entries ce on ne.module_id = ce.id
+        where
+            ne.event_date
+            between date_sub(date(current_date), interval 1 day) and date(current_date)
+            and ne.event_name = "HasSeenAllVideo"
+            and ce.content_type in ("videoCarousel")
+            {% if is_incremental() %}
+                and event_date
+                between date_sub(date("{{ ds() }}"), interval 1 day) and date(
+                    "{{ ds() }}"
+                )
+            {% endif %}
+    ),
+
 select
-    unique_session_id,
-    user_id,
-    event_timestamp,
-    event_date,
-    app_version,
-    event_name,
-    module_id,
-    video_id,
-    entry_id,
+    ne.unique_session_id,
+    ne.user_id,
+    ne.event_timestamp,
+    ne.event_date,
+    ne.app_version,
+    ne.event_name,
+    ne.module_id,
+    ne.video_id,
+    ne.entry_id,
     ne.offer_id,
     coalesce(
-        cast(video_seen_duration_seconds as float64), 0
-    ) total_video_seen_duration_seconds,
-    coalesce(cast(video_duration_seconds as float64), 0) video_duration_seconds
-from {{ ref("int_firebase__native_event") }} ne
+        case
+            when all_seen.video_id is not null and ne.event_name = 'VideoPaused'
+            then cast(video_duration_seconds as float64)
+            else cast(video_seen_duration_seconds as float64)
+        end,
+        0
+    ) as total_video_seen_duration_seconds,
+    coalesce(cast(video_duration_seconds as float64), 0) as video_duration_seconds
+from int_firebase_prod.native_event ne
 inner join
-    {{ ref("int_contentful__entry") }} ce
+    analytics_prod.contentful_entries ce
     on ne.module_id = ce.id
     and ce.content_type in ('video', 'videoCarousel', 'videoCarouselItem')
+left join
+    all_seen
+    on ne.unique_session_id = all_seen.unique_session_id
+    and ne.user_id = all_seen.user_id
+    and ne.video_id = all_seen.video_id
 where
-    event_name in (
+    ne.event_name in (
         'ConsultVideo',
         'HasSeenAllVideo',
         'HasDismissedModal',

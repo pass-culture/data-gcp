f15a4f1bf21ab8168204650104f48b4c2cc0b483
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2024-11-14 11:16:50 +0100

    cast back to datetime after snapshot

--- orchestration/dags/data_gcp_dbt/models/intermediate/raw/int_raw__offer.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/raw/int_raw__offer.sql
@@ -7,6 +7,8 @@
     )
 }}
 
-select *
+select
+    * except (offer_updated_date),
+    cast(offer_updated_date as datetime) as offer_updated_date
 from {{ ref("snapshot_raw__offer") }}
 where {{ var("snapshot_filter") }}

e7f9eb925873f3d05d5167aab8e95b1e19dced83
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2024-11-26 10:30:17 +0100

    docs: Update dbt doc

--- orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/artist/ml_linkage_artist__artist_alias.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/artist/ml_linkage_artist__artist_alias.sql
@@ -1,4 +1,4 @@
 select distinct
-    artist_id, offer_category_id, artist_type, artist_name as artist_alias_name
+    artist_id, offer_category_id, artist_type, artist_name as artist_offer_name
 from {{ source("ml_preproc", "linked_artists") }}
 where artist_id is not null

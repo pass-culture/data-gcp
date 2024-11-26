4776adafa45433528ff1a96762c7adaf40115016
LucileRainteau <lucile.rainteau@passculture.app>
2024-11-26 12:30:41 +0100

    add stock_lite model

--- orchestration/dags/data_gcp_dbt/snapshots/raw_applicative/snapshot_raw__stock.sql
+++ orchestration/dags/data_gcp_dbt/snapshots/raw_applicative/snapshot_raw__stock.sql
@@ -6,36 +6,12 @@
                 strategy="timestamp",
                 unique_key="stock_id",
                 updated_at="stock_modified_date",
-                invalidate_hard_delete=true,
+                invalidate_hard_delete=false,
             )
         )
     }}
 
-    select
-        * except (stock_modified_date),
-        cast(stock_modified_date as timestamp) as stock_modified_date
-    from
-        external_query(
-            "{{ env_var('APPLICATIVE_EXTERNAL_CONNECTION_ID') }}",
-            '''
-    SELECT
-        CAST("idAtProviders" AS varchar(255)) AS stock_id_at_providers
-        , "dateModifiedAtLastProvider" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS stock_modified_at_last_provider_date
-        , CAST("id" AS varchar(255)) AS stock_id
-        , "dateModified" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS stock_modified_date
-        , "price" AS stock_price
-        , "quantity" AS stock_quantity
-        , "bookingLimitDatetime" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS stock_booking_limit_date
-        , CAST("lastProviderId" AS varchar(255)) AS stock_last_provider_id
-        , CAST("offerId" AS varchar(255)) AS offer_id
-        , "isSoftDeleted" AS stock_is_soft_deleted
-        , "beginningDatetime" AS stock_beginning_date
-        , "dateCreated" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS stock_creation_date
-        , "fieldsUpdated" AS stock_fields_updated
-        , CAST("priceCategoryId" AS varchar(255)) AS price_category_id
-        , BTRIM(array_to_string("features", \',\'), \'{\') AS stock_features
-    FROM public.stock
-        '''
-        )
+    select *
+    from {{ ref("raw_applicative__stock_lite") }}
 
 {% endsnapshot %}

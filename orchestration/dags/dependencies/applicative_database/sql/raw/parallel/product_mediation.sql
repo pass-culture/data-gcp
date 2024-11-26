dbc32d34a60954b853cf4f0820e969f8ed18ba48
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2024-11-26 14:30:37 +0100

    import product mediation

---
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/product_mediation.sql
@@ -0,0 +1,8 @@
+SELECT
+ "id" as id
+ ,"dateModifiedAtLastProvider" as dateModifiedAtLastProvider
+ ,"productId" as productId
+ ,"imageType" as imageType
+ ,"lastProviderId" as lastProviderId
+ ,"UUID" AS uuid
+FROM public.product_mediation

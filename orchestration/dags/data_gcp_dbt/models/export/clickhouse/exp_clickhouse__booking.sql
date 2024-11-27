203b21561e231c4afd49ace6e526359c0fd7ee4b
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2024-11-27 12:53:10 +0100

    export event scheduled date (stock beginning) and reimbursement date

--- orchestration/dags/data_gcp_dbt/models/export/clickhouse/exp_clickhouse__booking.sql
+++ orchestration/dags/data_gcp_dbt/models/export/clickhouse/exp_clickhouse__booking.sql
@@ -4,6 +4,8 @@ select
     offer_id,
     date(booking_creation_date) as creation_date,
     date(booking_used_date) as used_date,
+    date(booking_reimbursement_date) as reimbursement_date,
+    date(stock_beginning_date) as stock_beginning_date,
     booking_status,
     deposit_type,
     booking_quantity,

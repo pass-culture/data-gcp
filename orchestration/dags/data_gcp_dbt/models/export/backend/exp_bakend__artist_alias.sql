e0fe200c4bd3b1cafb874d9502446270d0238db8
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2024-12-02 14:51:40 +0100

    refactor: Remove unnecessary columns for backend

--- orchestration/dags/data_gcp_dbt/models/export/backend/exp_bakend__artist_alias.sql
+++ orchestration/dags/data_gcp_dbt/models/export/backend/exp_bakend__artist_alias.sql
@@ -1,9 +1,8 @@
 select distinct
     artist_id,
+    artist_offer_name,
     artist_cluster_id,
     artist_wiki_id,
     offer_category_id,
     artist_type,
-    artist_name as artist_offer_name
 from {{ ref("ml_linkage__artist_alias") }}
-where artist_id is not null

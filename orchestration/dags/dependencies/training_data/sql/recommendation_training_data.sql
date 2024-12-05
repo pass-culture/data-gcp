57207529bb7d9b8320f844cc78bc8fa00867b045
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2024-12-05 14:23:27 +0100

    WIP: Two Tower Migration

--- orchestration/dags/dependencies/training_data/sql/recommendation_training_data.sql
+++ orchestration/dags/dependencies/training_data/sql/recommendation_training_data.sql
@@ -5,7 +5,7 @@ from
             *,
             row_number() over (partition by user_id order by event_date) as row_index,
             count(*) over (partition by user_id) as total
-        from `{{ bigquery_raw_dataset }}`.`training_data_{{ params.input_type }}`
+        from `{{ bigquery_ml_reco_dataset }}`.`training_data_{{ params.input_type }}`
         where
             event_date
             >= date_sub(date("{{ ds }}"), interval {{ params.event_day_number }} day)

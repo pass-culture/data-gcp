a933abcc7a356b19c01229b6f69e26081ad82ada
lmontier-pass <172769973+lmontier-pass@users.noreply.github.com>
2024-12-10 15:23:17 +0100

    (DE-904) refactor(dbt): Migrate last training data queries to dbt (#3582)
    * refactor: Migrate compliance_offers
    
    * refactor: Update compliance dag
    
    * refactor: Use uv in compliance training DAG
    
    * fix: fix naming
    
    * refactor: Update retrieval_vector_semantic_build DAG
    
    * refactor: Move offer_categorization_offer to dbt
    
    * refactor: Update algo_training_offer_categorization
    
    * fix: Fix query and dag
    
    * refactor: Rename dbt models with more explicit names
    
    * clean: Remove useless join

--- orchestration/dags/jobs/ml/algo_training_offer_compliance_model.py
+++ orchestration/dags/jobs/ml/algo_training_offer_compliance_model.py
@@ -4,29 +4,28 @@ from datetime import datetime, timedelta
 from common import macros
 from common.alerts import task_fail_slack_alert
 from common.config import (
-    BIGQUERY_TMP_DATASET,
+    BIGQUERY_ML_COMPLIANCE_DATASET,
     DAG_FOLDER,
     ENV_SHORT_NAME,
+    GCE_UV_INSTALLER,
     GCP_PROJECT_ID,
     MLFLOW_BUCKET_NAME,
     MLFLOW_URL,
     SLACK_CONN_PASSWORD,
 )
 from common.operators.gce import (
-    CloneRepositoryGCEOperator,
+    InstallDependenciesOperator,
     SSHGCEOperator,
     StartGCEOperator,
     StopGCEOperator,
 )
 from common.utils import get_airflow_schedule
 from dependencies.ml.utils import create_algo_training_slack_block
-from jobs.ml.constants import IMPORT_TRAINING_SQL_PATH
 
 from airflow import DAG
 from airflow.models import Param
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.providers.google.cloud.operators.bigquery import (
-    BigQueryExecuteQueryOperator,
     BigQueryInsertJobOperator,
 )
 from airflow.providers.http.operators.http import HttpOperator
@@ -96,36 +95,22 @@ with DAG(
 ) as dag:
     start = DummyOperator(task_id="start", dag=dag)
 
-    import_tables = {}
-    for table in [
-        "compliance_offers",
-    ]:
-        import_tables[table] = BigQueryExecuteQueryOperator(
-            task_id=f"import_{table}",
-            sql=(IMPORT_TRAINING_SQL_PATH / f"{table}.sql").as_posix(),
-            write_disposition="WRITE_TRUNCATE",
-            use_legacy_sql=False,
-            destination_dataset_table=f"{BIGQUERY_TMP_DATASET}.{DATE}_{table}_raw_data",
-            dag=dag,
-        )
-
-        store_data = {}
-        store_data["raw"] = BigQueryInsertJobOperator(
-            task_id="store_raw_data",
-            configuration={
-                "extract": {
-                    "sourceTable": {
-                        "projectId": GCP_PROJECT_ID,
-                        "datasetId": BIGQUERY_TMP_DATASET,
-                        "tableId": f"{DATE}_{table}_raw_data",
-                    },
-                    "compression": None,
-                    "destinationUris": f"{dag_config['STORAGE_PATH']}/{table}_raw_data/data-*.parquet",
-                    "destinationFormat": "PARQUET",
-                }
-            },
-            dag=dag,
-        )
+    import_offer_as_parquet = BigQueryInsertJobOperator(
+        task_id="import_offer_as_parquet",
+        configuration={
+            "extract": {
+                "sourceTable": {
+                    "projectId": GCP_PROJECT_ID,
+                    "datasetId": BIGQUERY_ML_COMPLIANCE_DATASET,
+                    "tableId": "training_data_offer",
+                },
+                "compression": None,
+                "destinationUris": f"{dag_config['STORAGE_PATH']}/compliance_raw_data/data-*.parquet",
+                "destinationFormat": "PARQUET",
+            }
+        },
+        dag=dag,
+    )
 
     gce_instance_start = StartGCEOperator(
         task_id="gce_start_task",
@@ -135,20 +120,13 @@ with DAG(
         labels={"job_type": "ml"},
     )
 
-    fetch_code = CloneRepositoryGCEOperator(
-        task_id="fetch_code",
+    fetch_install_code = InstallDependenciesOperator(
+        task_id="fetch_install_code",
         instance_name="{{ params.instance_name }}",
+        branch="{{ params.branch }}",
         python_version="3.10",
-        command="{{ params.branch }}",
-        retries=2,
-    )
-
-    install_dependencies = SSHGCEOperator(
-        task_id="install_dependencies",
-        instance_name="{{ params.instance_name }}",
         base_dir=dag_config["BASE_DIR"],
-        command="pip install -r requirements.txt --user",
-        dag=dag,
+        installer=GCE_UV_INSTALLER,
         retries=2,
     )
 
@@ -157,9 +135,10 @@ with DAG(
         instance_name="{{ params.instance_name }}",
         base_dir=dag_config["BASE_DIR"],
         environment=dag_config,
+        installer=GCE_UV_INSTALLER,
         command=f"mkdir -p img && PYTHONPATH=. python {dag_config['MODEL_DIR']}/preprocess.py "
         "--config-file-name {{ params.config_file_name }} "
-        "--input-dataframe-file-name compliance_offers_raw_data "
+        "--input-dataframe-file-name compliance_raw_data "
         "--output-dataframe-file-name compliance_clean_data ",
         dag=dag,
     )
@@ -169,6 +148,7 @@ with DAG(
         instance_name="{{ params.instance_name }}",
         base_dir=dag_config["BASE_DIR"],
         environment=dag_config,
+        installer=GCE_UV_INSTALLER,
         command=f"PYTHONPATH=. python {dag_config['MODEL_DIR']}/split_data.py "
         f"--clean-table-name compliance_clean_data "
         "--training-table-name compliance_training_data "
@@ -181,6 +161,7 @@ with DAG(
         instance_name="{{ params.instance_name }}",
         base_dir=dag_config["BASE_DIR"],
         environment=dag_config,
+        installer=GCE_UV_INSTALLER,
         command=f"PYTHONPATH=. python {dag_config['MODEL_DIR']}/train.py "
         "--model-name {{ params.model_name }} "
         "--config-file-name {{ params.config_file_name }} "
@@ -194,6 +175,7 @@ with DAG(
         instance_name="{{ params.instance_name }}",
         base_dir=dag_config["BASE_DIR"],
         environment=dag_config,
+        installer=GCE_UV_INSTALLER,
         command=f"PYTHONPATH=. python {dag_config['MODEL_DIR']}/evaluate.py "
         "--model-name {{ params.model_name }} "
         "--config-file-name {{ params.config_file_name }} "
@@ -207,6 +189,7 @@ with DAG(
         instance_name="{{ params.instance_name }}",
         base_dir=dag_config["BASE_DIR"],
         environment=dag_config,
+        installer=GCE_UV_INSTALLER,
         command=f"PYTHONPATH=. python {dag_config['MODEL_DIR']}/package_api_model.py "
         "--model-name {{ params.model_name }} "
         "--config-file-name {{ params.config_file_name }} ",
@@ -234,11 +217,9 @@ with DAG(
 
     (
         start
-        >> import_tables["compliance_offers"]
-        >> store_data["raw"]
+        >> import_offer_as_parquet
         >> gce_instance_start
-        >> fetch_code
-        >> install_dependencies
+        >> fetch_install_code
         >> preprocess
         >> split_data
         >> train

d31657a9593f6da9507bedf60b850e0b09b96b2c
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2024-12-10 11:21:01 +0100

    WIP

--- jobs/ml_jobs/retrieval_vector/app/logger.py
+++ jobs/ml_jobs/retrieval_vector/app/logger.py
@@ -1,22 +1,28 @@
 import logging
-import sys
 
-from pythonjsonlogger import jsonlogger
+import google.cloud.logging
 
 
 def get_logger() -> logging.Logger:
-    """Configure and return the logger."""
-    logger = logging.getLogger(__name__)
-    stdout = logging.StreamHandler(stream=sys.stdout)
-    formatter = jsonlogger.JsonFormatter(
-        "%(name)s %(asctime)s %(levelname)s %(filename)s %(lineno)s %(process)d %(message)s",
-        rename_fields={"levelname": "severity", "asctime": "timestamp"},
-    )
-    stdout.setFormatter(formatter)
-    logger.addHandler(stdout)
+    """Configure and return the logger with automatic Google Cloud Logging setup."""
+    # Instantiates a Cloud Logging client
+    client = google.cloud.logging.Client()
+
+    # Automatically configures Cloud Logging with the standard Python logging module
+    client.setup_logging()
+
+    # Get the root logger
+    logger = logging.getLogger()
+
+    # Set the logging level if needed (default is INFO)
     logger.setLevel(logging.INFO)
 
     return logger
 
 
+# Get the logger instance
 logger = get_logger()
+
+# Example usage
+logger.info("This is an INFO log.")
+logger.error("This is an ERROR log.")

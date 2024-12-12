2aefd1a3e45f42f70c0dcbbd53dec0fa785b8d95
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2024-12-12 16:07:36 +0100

    DBG

--- orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__artist.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__artist.sql
@@ -9,6 +9,7 @@ select
     image_page_url as wikidata_image_page_url,
     image_author as wikidata_image_author,
     image_license as wikidata_image_license,
-    image_license_url as wikidata_image_license_url,
+    image_license_url as wikidata_image_license_url
+
 from {{ source("ml_preproc", "artist_linked") }}
 where is_cluster_representative = true

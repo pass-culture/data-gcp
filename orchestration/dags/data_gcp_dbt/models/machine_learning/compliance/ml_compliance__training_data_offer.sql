8d63aba78157e6e3de5b3bd2d6671e8771fe3660
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2024-12-12 15:46:51 +0100

    DEBG

--- orchestration/dags/data_gcp_dbt/models/machine_learning/compliance/ml_compliance__training_data_offer.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/compliance/ml_compliance__training_data_offer.sql
@@ -1,22 +1,22 @@
 with
     base as (
         select
-             offer.offer_id,
+            offer.offer_id,
+            offer.offer_validation,
+            offer.offer_subcategoryid as offer_subcategory_id,
+            extract_offer.rayon,
+            macro_rayons.macro_rayon,
             case
-                when (offer.offer_name is null or offer.offer_name= 'NaN')
-                then "None"
+                when (offer.offer_name is null or offer.offer_name = 'NaN')
+                then 'None'
                 else safe_cast(offer.offer_name as string)
             end as offer_name,
             case
                 when
                     (offer.offer_description is null or offer.offer_description = 'NaN')
-                then "None"
+                then 'None'
                 else safe_cast(offer.offer_description as string)
-            end AS offer_description,
-            offer.offer_validation,
-            offer.offer_subcategoryid as offer_subcategory_id,
-            extract_offer.rayon,
-            macro_rayons.macro_rayon as macro_rayon,
+            end as offer_description,
             case
                 when stock.stock_price is null
                 then 0
@@ -33,16 +33,16 @@ with
                 then false
                 else true
             end as is_rule_up_to_date
-        from {{ ref("int_raw__offer") }} offer
+        from {{ ref("int_raw__offer") }} as offer
         left join
-            {{ source("raw", "applicative_database_stock") }} stock
-            on stock.offer_id = offer.offer_id  -- TODO:update join with offer_extra_data
+            {{ source("raw", "applicative_database_stock") }} as stock
+            on offer.offer_id = stock.offer_id
         left join
-            {{ ref("int_applicative__extract_offer") }} extract_offer
-            on extract_offer.offer_id = offer.offer_id
+            {{ ref("int_applicative__extract_offer") }} as extract_offer
+            on offer.offer_id = extract_offer.offer_id
         left join
-            {{ source("raw", "subcategories") }} subcategories
-            on subcategories.id = offer.offer_subcategoryid
+            {{ source("raw", "subcategories") }} as subcategories
+            on offer.offer_subcategoryid = subcategories.id
         left join
             {{ source("seed", "macro_rayons") }} as macro_rayons
             on extract_offer.rayon = macro_rayons.rayon
@@ -60,22 +60,19 @@ with
             subcategories.id,
             offer.offer_creation_date,
             extract_offer.rayon,
-            macro_rayon,
-            stock_price
+            macro_rayons.macro_rayon,
+            stock.stock_price
     )
-select
-    base.* except (is_rule_up_to_date),
-    item_embedding.semantic_content_embedding as semantic_content_embedding,
-    item_embedding.image_embedding as image_embedding
+
+select base.*, item_embedding.semantic_content_embedding, item_embedding.image_embedding
 from base
 left join
     {{ ref("int_applicative__offer_item_id") }} as offer_item_id
     on base.offer_id = offer_item_id.offer_id
-
-join
-    {{ ref("ml_feat__item_embedding") }} item_embedding
+inner join
+    {{ ref("ml_feat__item_embedding") }} as item_embedding
     on offer_item_id.item_id = item_embedding.item_id
-where is_rule_up_to_date
+where base.is_rule_up_to_date
 qualify
     row_number() over (
         partition by item_embedding.item_id order by item_embedding.extraction_date desc

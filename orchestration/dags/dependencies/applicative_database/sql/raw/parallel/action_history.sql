6cd0611abebd10d193304f774fe62d5ae0f07089
mripoll-pass <91483773+mripoll-pass@users.noreply.github.com>
2024-12-12 14:30:06 +0100

    add status update in action history as columns (#3591)
    Co-authored-by: MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>

--- orchestration/dags/dependencies/applicative_database/sql/raw/parallel/action_history.sql
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/action_history.sql
@@ -23,5 +23,9 @@ SELECT
     , CAST(ah."offererId" AS VARCHAR(255)) AS offerer_id
     , CAST(ah."venueId" AS VARCHAR(255)) AS venue_id
     , ah."comment" AS comment
+    ,REPLACE(CAST("jsonData" -> \'modified_info\' -> \'postalCode\' -> \'old_info\' AS VARCHAR(255)), \'"\', \'\') AS old_postal_code
+    ,REPLACE(CAST("jsonData" -> \'modified_info\' -> \'postalCode\' -> \'new_info\' AS VARCHAR(255)), \'"\', \'\') AS new_postal_code
+    ,REPLACE(CAST("jsonData" -> \'modified_info\' -> \'activity\' -> \'old_info\' AS VARCHAR(255)), \'"\', \'\') AS old_activity
+    ,REPLACE(CAST("jsonData" -> \'modified_info\' -> \'activity\' -> \'new_info\' AS VARCHAR(255)), \'"\', \'\') AS new_activity
 FROM public.action_history ah
 LEFT JOIN public.user u on ah."authorUserId" = u.id

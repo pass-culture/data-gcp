f8080d0d393cc3af889be6b600dcccdd1631569f
dcuesta-pass <82374920+dcuesta-pass@users.noreply.github.com>
2024-12-16 18:33:08 +0100

    (BSR) fix(file_handler): handle None URLs and improve image loading error handling (#3614)

--- jobs/ml_jobs/embeddings/utils/__init__.py
+++ jobs/ml_jobs/embeddings/utils/__init__.py
@@ -5,4 +5,7 @@ def get_filename_from_url(url):
     """
     Extract the filename from a URL.
     """
-    return urlparse(url).path.split("/")[-1]
+    if url is None:
+        return None
+    else:
+        return urlparse(url).path.split("/")[-1]

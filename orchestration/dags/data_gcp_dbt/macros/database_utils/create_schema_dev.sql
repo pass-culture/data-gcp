d3b648549cfe5035174136a4b3bfadf9cc9662c3
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2024-12-17 12:05:48 +0100

    (BSR) feat(dbt): fix edge case invalid user name

--- orchestration/dags/data_gcp_dbt/macros/database_utils/create_schema_dev.sql
+++ orchestration/dags/data_gcp_dbt/macros/database_utils/create_schema_dev.sql
@@ -1,8 +1,18 @@
-{% macro create_schema_dev
-() %}
+{% macro create_schema_dev() %}
+    {% set raw_user_name = env_var(
+        "PERSONAL_DBT_USER", env_var("USER", "anonymous_user")
+    ) %}
+
+    {# filter forbidden schema names #}
+    {% set user_name = dbt_utils.regex_replace(raw_user_name, "^[^A-Za-z_]+", "") %}
+    {% set user_name = user_name | replace("-", "_") %}
+    {% set user_name = dbt_utils.regex_replace(user_name, "[^A-Za-z0-9_]", "") %}
+    {% if user_name == "" %} {% set user_name = "anonymous_user" %} {% endif %}
+
+    {# Create and alter schema only if target is local #}
     {% if target.name == "local" %}
-        CREATE SCHEMA IF NOT EXISTS `{{ target.project }}.tmp_{{ env_var('USER', 'anonymous_user') }}_dev`;
-        ALTER SCHEMA `{{ target.project }}.tmp_{{ env_var('USER', 'anonymous_user') }}_dev`
+        CREATE SCHEMA IF NOT EXISTS `{{ target.project }}.tmp_{{ user_name }}_dev`;
+        ALTER SCHEMA `{{ target.project }}.tmp_{{ user_name }}_dev`
         SET OPTIONS( default_table_expiration_days = 2 );
     {% endif %}
 {% endmacro %}

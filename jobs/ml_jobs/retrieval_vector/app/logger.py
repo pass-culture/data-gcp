9f9f2b40a08a910e2176ce3a8540e27c156f4cbe
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2024-12-17 14:36:53 +0100

    WIP: debug

--- jobs/ml_jobs/retrieval_vector/app/logger.py
+++ jobs/ml_jobs/retrieval_vector/app/logger.py
@@ -1,22 +1,36 @@
 import logging
-import sys
 
-from pythonjsonlogger import jsonlogger
+import google.cloud.logging
+from google.cloud.logging.handlers import CloudLoggingHandler
+
+# from pythonjsonlogger import jsonlogger
+# def get_logger() -> logging.Logger:
+#     """Configure and return the logger."""
+#     logger = logging.getLogger(__name__)
+#     stdout = logging.StreamHandler(stream=sys.stdout)
+#     formatter = jsonlogger.JsonFormatter(
+#         "%(name)s %(asctime)s %(levelname)s %(filename)s %(lineno)s %(process)d %(message)s",
+#         rename_fields={"levelname": "severity", "asctime": "timestamp"},
+#     )
+#     stdout.setFormatter(formatter)
+#     logger.addHandler(stdout)
+#     logger.setLevel(logging.INFO)
+#     return logger
+# logger = get_logger()
 
 
 def get_logger() -> logging.Logger:
-    """Configure and return the logger."""
+    client = google.cloud.logging.Client()
+    handler = CloudLoggingHandler(client)
     logger = logging.getLogger(__name__)
-    stdout = logging.StreamHandler(stream=sys.stdout)
-    formatter = jsonlogger.JsonFormatter(
-        "%(name)s %(asctime)s %(levelname)s %(filename)s %(lineno)s %(process)d %(message)s",
-        rename_fields={"levelname": "severity", "asctime": "timestamp"},
-    )
-    stdout.setFormatter(formatter)
-    logger.addHandler(stdout)
+    logger.addHandler(handler)
     logger.setLevel(logging.INFO)
-
     return logger
 
 
 logger = get_logger()
+logger.info("Info Log")
+logger.warning("warning Log")
+logger.debug("debug Log")
+logger.error("error Log")
+logger.critical("critical Log")

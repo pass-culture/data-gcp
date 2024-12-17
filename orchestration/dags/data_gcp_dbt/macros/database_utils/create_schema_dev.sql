12e60bbe1dcf521aa3b6f467f649d1a8644f365d
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2024-12-17 13:51:35 +0100

    fix macro

--- orchestration/dags/data_gcp_dbt/macros/database_utils/create_schema_dev.sql
+++ orchestration/dags/data_gcp_dbt/macros/database_utils/create_schema_dev.sql
@@ -1,15 +1,9 @@
 {% macro create_schema_dev() %}
-    {% set raw_user_name = env_var(
-        "PERSONAL_DBT_USER", env_var("USER", "anonymous_user")
+    -- If your username is incomptatible with BigQuery dataset naming constraints:
+    -- https://cloud.google.com/bigquery/docs/datasets?hl=en#dataset-naming
+    {% set user_name = env_var("PERSONAL_DBT_USER") or env_var(
+        "USER", "anonymous_user"
     ) %}
-
-    {# filter forbidden schema names #}
-    {% set user_name = dbt_utils.regex_replace(raw_user_name, "^[^A-Za-z_]+", "") %}
-    {% set user_name = user_name | replace("-", "_") %}
-    {% set user_name = dbt_utils.regex_replace(user_name, "[^A-Za-z0-9_]", "") %}
-    {% if user_name == "" %} {% set user_name = "anonymous_user" %} {% endif %}
-
-    {# Create and alter schema only if target is local #}
     {% if target.name == "local" %}
         CREATE SCHEMA IF NOT EXISTS `{{ target.project }}.tmp_{{ user_name }}_dev`;
         ALTER SCHEMA `{{ target.project }}.tmp_{{ user_name }}_dev`

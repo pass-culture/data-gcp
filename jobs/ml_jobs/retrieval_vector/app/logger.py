cf3e0a9181d00a4fc1065886e5b34e9f5b72330c
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2024-12-17 17:17:23 +0100

    WIP3

--- jobs/ml_jobs/retrieval_vector/app/logger.py
+++ jobs/ml_jobs/retrieval_vector/app/logger.py
@@ -4,11 +4,19 @@ import sys
 from pythonjsonlogger import jsonlogger
 
 
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def process_log_record(self, log_record):
+        # Ensure 'severity' is at the top level
+        if "severity" not in log_record and "levelname" in log_record:
+            log_record["severity"] = log_record["levelname"]
+        return super().process_log_record(log_record)
+
+
 def get_logger() -> logging.Logger:
     """Configure and return the logger."""
     logger = logging.getLogger(__name__)
     stdout = logging.StreamHandler(stream=sys.stdout)
-    formatter = jsonlogger.JsonFormatter(
+    formatter = CustomJsonFormatter(
         "%(name)s %(asctime)s %(levelname)s %(filename)s %(lineno)s %(process)d %(message)s",
         rename_fields={"levelname": "severity", "asctime": "timestamp"},
     )
@@ -19,8 +27,9 @@ def get_logger() -> logging.Logger:
 
 
 logger = get_logger()
+
 logger.info("Info Log")
-logger.warning("warning Log")
-logger.debug("debug Log")
-logger.error("error Log")
-logger.critical("critical Log")
+logger.warning("Warning Log")
+logger.debug("Debug Log")
+logger.error("Error Log")
+logger.critical("Critical Log")

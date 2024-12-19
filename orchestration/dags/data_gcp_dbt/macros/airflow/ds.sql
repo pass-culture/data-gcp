7e2b06afaa16ced1300440088e1fa6dc1f0348b3
LucileRainteau <lucile.rainteau@passculture.app>
2024-12-19 13:42:06 +0100

    (DE-932) feat(dbt): add selectors in dbt cli

--- orchestration/dags/data_gcp_dbt/macros/airflow/ds.sql
+++ orchestration/dags/data_gcp_dbt/macros/airflow/ds.sql
@@ -1,4 +1,8 @@
 {%- macro ds() -%}
-    {%- set execution_date = dbt_airflow_macros.execution_date() -%}
-    {{- execution_date.strftime("%Y-%m-%d") -}}
+    {%- set execution_date = var('execution_date', dbt_airflow_macros.execution_date()) -%}
+    {% if execution_date is string %}
+        {{- execution_date -}}
+    {% else %}
+        {{- execution_date.strftime("%Y-%m-%d") -}}
+    {% endif %}
 {%- endmacro -%}

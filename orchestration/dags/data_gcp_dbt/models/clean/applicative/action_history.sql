50ed7abe3bbf52bbe12a0bbee419dffcbc9f998c
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2024-12-23 09:43:55 +0100

    add json data extract to clean model

--- orchestration/dags/data_gcp_dbt/models/clean/applicative/action_history.sql
+++ orchestration/dags/data_gcp_dbt/models/clean/applicative/action_history.sql
@@ -1,7 +1,39 @@
 select
-    *
-    ,REPLACE(CAST(JSON_EXTRACT_SCALAR(action_history_json_data, '$.modified_info.postalCode.old_info') AS STRING), '"', '') AS old_postal_code
-    ,REPLACE(CAST(JSON_EXTRACT_SCALAR(action_history_json_data, '$.modified_info.postalCode.new_info') AS STRING), '"', '') AS new_postal_code
-    ,REPLACE(CAST(JSON_EXTRACT_SCALAR(action_history_json_data, '$.modified_info.activity.old_info') AS STRING), '"', '') AS old_activity
-    ,REPLACE(CAST(JSON_EXTRACT_SCALAR(action_history_json_data, '$.modified_info.activity.new_info') AS STRING), '"', '') AS new_activity
+    *,
+    replace(
+        cast(
+            json_extract_scalar(
+                action_history_json_data, '$.modified_info.postalCode.old_info'
+            ) as string
+        ),
+        '"',
+        ''
+    ) as old_postal_code,
+    replace(
+        cast(
+            json_extract_scalar(
+                action_history_json_data, '$.modified_info.postalCode.new_info'
+            ) as string
+        ),
+        '"',
+        ''
+    ) as new_postal_code,
+    replace(
+        cast(
+            json_extract_scalar(
+                action_history_json_data, '$.modified_info.activity.old_info'
+            ) as string
+        ),
+        '"',
+        ''
+    ) as old_activity,
+    replace(
+        cast(
+            json_extract_scalar(
+                action_history_json_data, '$.modified_info.activity.new_info'
+            ) as string
+        ),
+        '"',
+        ''
+    ) as new_activity
 from {{ source("raw", "applicative_database_action_history") }}

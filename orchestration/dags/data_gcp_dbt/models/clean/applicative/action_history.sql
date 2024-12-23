a4b0ad2cbf8bf206a7441fdc60c552ae7175a6bb
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2024-12-23 09:43:03 +0100

    add json data extract to clean model

--- orchestration/dags/data_gcp_dbt/models/clean/applicative/action_history.sql
+++ orchestration/dags/data_gcp_dbt/models/clean/applicative/action_history.sql
@@ -1,1 +1,7 @@
-select * from {{ source("raw", "applicative_database_action_history") }}
+select
+    *
+    ,REPLACE(CAST(JSON_EXTRACT_SCALAR(action_history_json_data, '$.modified_info.postalCode.old_info') AS STRING), '"', '') AS old_postal_code
+    ,REPLACE(CAST(JSON_EXTRACT_SCALAR(action_history_json_data, '$.modified_info.postalCode.new_info') AS STRING), '"', '') AS new_postal_code
+    ,REPLACE(CAST(JSON_EXTRACT_SCALAR(action_history_json_data, '$.modified_info.activity.old_info') AS STRING), '"', '') AS old_activity
+    ,REPLACE(CAST(JSON_EXTRACT_SCALAR(action_history_json_data, '$.modified_info.activity.new_info') AS STRING), '"', '') AS new_activity
+from {{ source("raw", "applicative_database_action_history") }}

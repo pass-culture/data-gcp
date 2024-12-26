dd57edbd8c7261222efa63f17aeaa65d3e1dc913
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2024-12-26 15:21:32 +0100

    (BSR) feat(dbt): fix edge case invalid user name (#3618)
    * (BSR) feat(dbt): fix edge case invalid user name
    
    * fix macro
    
    * review

--- orchestration/dags/data_gcp_dbt/macros/database_utils/create_schema_dev.sql
+++ orchestration/dags/data_gcp_dbt/macros/database_utils/create_schema_dev.sql
@@ -1,8 +1,12 @@
-{% macro create_schema_dev
-() %}
+{% macro create_schema_dev() %}
+    -- If your username is incomptatible with BigQuery dataset naming constraints:
+    -- https://cloud.google.com/bigquery/docs/datasets?hl=en#dataset-naming
+    {% set user_name = env_var("PERSONAL_DBT_USER", false) or env_var(
+        "USER", "anonymous_user"
+    ) %}
     {% if target.name == "local" %}
-        CREATE SCHEMA IF NOT EXISTS `{{ target.project }}.tmp_{{ env_var('USER', 'anonymous_user') }}_dev`;
-        ALTER SCHEMA `{{ target.project }}.tmp_{{ env_var('USER', 'anonymous_user') }}_dev`
+        CREATE SCHEMA IF NOT EXISTS `{{ target.project }}.tmp_{{ user_name }}_dev`;
+        ALTER SCHEMA `{{ target.project }}.tmp_{{ user_name }}_dev`
         SET OPTIONS( default_table_expiration_days = 2 );
     {% endif %}
 {% endmacro %}

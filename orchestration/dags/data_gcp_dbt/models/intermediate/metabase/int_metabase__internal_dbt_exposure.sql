2bfbb21d5c9a453a80f88b2dfa1ebb4639fd8523
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2024-12-30 18:19:05 +0100

    feat: add exposures to job + airflow job

--- orchestration/dags/data_gcp_dbt/models/intermediate/metabase/int_metabase__internal_dbt_exposure.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/metabase/int_metabase__internal_dbt_exposure.sql
@@ -1,25 +1,23 @@
-WITH collection_status AS (
-  SELECT
-    collection_id,
-    collection_name
-  FROM {{ source("raw", "metabase_collection") }}
-  WHERE
-    -- the collection is not located in Temporary Archive directory.
-    concat(location, collection_id) NOT LIKE '/610%'
-    -- the collection is not located in Metabase's native archive.
-    AND archived = false
-    -- the collections are public (i.e., remove personal collections)
-    AND personal_owner_id IS null
-)
+with
+    collection_status as (
+        select collection_id, collection_name
+        from {{ source("raw", "metabase_collection") }}
+        where
+            -- the collection is not located in Temporary Archive directory.
+            concat(location, collection_id) not like '/610%'
+            -- the collection is not located in Metabase's native archive.
+            and archived = false
+            -- the collections are public (i.e., remove personal collections)
+            and personal_owner_id is null
+    )
 
-
-SELECT
-    collection_id AS metabase_collection_id,
-    collection_name AS metabase_collection_name,
-    count(DISTINCT card_id) AS total_metabase_cards,
-    count(DISTINCT metabase_user_id) AS total_metabase_users,
-    count(*) AS total_metabase_queries
-    FROM {{ ref("int_metabase__daily_query") }} AS dq
-    INNER JOIN collection_status AS cs ON dq.card_collection_id = cs.collection_id
-    WHERE date(execution_date) >= date_sub(current_date, INTERVAL 90 DAY )
-GROUP BY 1,2
+select
+    collection_id as metabase_collection_id,
+    collection_name as metabase_collection_name,
+    count(distinct card_id) as total_metabase_cards,
+    count(distinct metabase_user_id) as total_metabase_users,
+    count(*) as total_metabase_queries
+from {{ ref("int_metabase__daily_query") }} as dq
+inner join collection_status as cs on dq.card_collection_id = cs.collection_id
+where date(execution_date) >= date_sub(current_date, interval 90 day)
+group by 1, 2

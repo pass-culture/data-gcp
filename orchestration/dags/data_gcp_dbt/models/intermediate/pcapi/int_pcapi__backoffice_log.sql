1197e9b5fe66fd599498566a1976507e9d76ffac
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2025-01-02 11:24:29 +0100

    add new logs to backoffice model

--- orchestration/dags/data_gcp_dbt/models/intermediate/pcapi/int_pcapi__backoffice_log.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/pcapi/int_pcapi__backoffice_log.sql
@@ -20,27 +20,33 @@ with
             search_nb_results,
             card_clicked_rank,
             case
-                when message like 'HTTP request%' then REGEXP_REPLACE(REGEXP_REPLACE(message, r'HTTP request at /', ''), r'/\d+', '')
+                when message like 'HTTP request%'
+                then
+                    regexp_replace(
+                        regexp_replace(message, r'HTTP request at /', ''), r'/\d+', ''
+                    )
                 else message
-                end as message,
-            COALESCE(search_sub_type, LOWER(search_pro_type)) as search_protype,
+            end as message,
+            coalesce(search_sub_type, lower(search_pro_type)) as search_protype,
             case
                 when search_query like '%@%' then 'xxx@xxx.com' else search_query
             end as search_query
 
         from {{ ref("int_pcapi__log") }}
         where
-            log_timestamp >= DATE_SUB(CURRENT_TIMESTAMP(), interval 365 day)
+            log_timestamp >= date_sub(current_timestamp(), interval 365 day)
 
             {% if is_incremental() %}
-                and DATE(log_timestamp) >= DATE_SUB(DATE('{{ ds() }}'), interval 2 day)
-                and DATE(log_timestamp) <= DATE('{{ ds() }}')
+                and date(log_timestamp) >= date_sub(date('{{ ds() }}'), interval 2 day)
+                and date(log_timestamp) <= date('{{ ds() }}')
             {% endif %}
             and (
-            (analytics_source = 'backoffice')
-            or (k8s_pod_role = 'backoffice'
-            and message not like 'HTTP request at /(health/api)%'
-            and message not like 'HTTP request at /static%')
+                (analytics_source = 'backoffice')
+                or (
+                    k8s_pod_role = 'backoffice'
+                    and message not like 'HTTP request at /(health/api)%'
+                    and message not like 'HTTP request at /static%'
+                )
             )
     ),
 
@@ -48,28 +54,28 @@ with
         select
             *,
             rnk - session_sum as session_num,
-            MIN(log_timestamp) over (
+            min(log_timestamp) over (
                 partition by user_id, rnk - session_sum
             ) as session_start
         from
             (
                 select
                     *,
-                    SUM(same_session) over (
+                    sum(same_session) over (
                         partition by user_id order by log_timestamp
                     ) as session_sum,
-                    ROW_NUMBER() over (
+                    row_number() over (
                         partition by user_id order by log_timestamp
                     ) as rnk
                 from
                     (
                         select
                             *,
-                            COALESCE(
-                                CAST(
-                                    DATE_DIFF(
+                            coalesce(
+                                cast(
+                                    date_diff(
                                         log_timestamp,
-                                        LAG(log_timestamp, 1) over (
+                                        lag(log_timestamp, 1) over (
                                             partition by user_id order by log_timestamp
                                         ),
                                         minute
@@ -85,8 +91,8 @@ with
 
 select
     * except (session_num, session_start, rnk, same_session, session_sum),
-    TO_HEX(
-        MD5(CONCAT(CAST(session_start as string), user_id, session_num))
+    to_hex(
+        md5(concat(cast(session_start as string), user_id, session_num))
     ) as session_id
 from generate_session
-{% if is_incremental() %} where partition_date = DATE('{{ ds() }}') {% endif %}
+{% if is_incremental() %} where partition_date = date('{{ ds() }}') {% endif %}

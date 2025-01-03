8d879b2ebd4e66b85481b1937639234911d445fd
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-01-03 13:00:30 +0100

    clean: Refacto logging module

--- jobs/ml_jobs/retrieval_vector/app/logger.py
+++ jobs/ml_jobs/retrieval_vector/app/logger.py
@@ -3,6 +3,9 @@ import sys
 
 from pythonjsonlogger import jsonlogger
 
+STDERR_LOG_LEVEL = logging.WARNING
+STDOUT_LOG_LEVEL = logging.DEBUG
+
 
 class CustomLogFilter(logging.Filter):
     """Filter logs based on severity level."""
@@ -14,11 +17,22 @@ class CustomLogFilter(logging.Filter):
         return record.levelno < self.max_level
 
 
-def setup_logging():
-    """Configure logging for the entire application, including Flask, Werkzeug, and Hypercorn."""
+def setup_logging(stdout_log_level: int, stderr_log_level: int) -> None:
+    """
+    Configure logging for the entire application, including Hypercorn.
+        - We use this kind of logging because vertexAI online prediction logging only allow ERROR and INFO logs.
+        - All logs writtent to stdout are mapped to INFO level.
+        - All logs written to stderr are mapped to ERROR level.
+        Ref : see https://cloud.google.com/vertex-ai/docs/predictions/online-prediction-logging?hl=en
+
+    Args:
+        stdout_log_level: The log level for stdout logs.
+        stderr_log_level: The log level for stderr.
+    """
+
     # Root logger
     root_logger = logging.getLogger()
-    root_logger.setLevel(logging.DEBUG)
+    root_logger.setLevel(stdout_log_level)
 
     # Formatter
     formatter = jsonlogger.JsonFormatter(
@@ -28,12 +42,12 @@ def setup_logging():
 
     # Handlers
     stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setLevel(logging.DEBUG)
-    stdout_handler.addFilter(CustomLogFilter(logging.WARNING))
+    stdout_handler.setLevel(stdout_log_level)
+    stdout_handler.addFilter(CustomLogFilter(stderr_log_level))
     stdout_handler.setFormatter(formatter)
 
     stderr_handler = logging.StreamHandler(sys.stderr)
-    stderr_handler.setLevel(logging.WARNING)
+    stderr_handler.setLevel(STDERR_LOG_LEVEL)
     stderr_handler.setFormatter(formatter)
 
     # Apply handlers to root logger
@@ -42,11 +56,4 @@ def setup_logging():
     root_logger.addHandler(stderr_handler)
 
 
-setup_logging()
-
-# Example logs
-logger = logging.getLogger("api_logger")
-logger.debug("TEST: Debug message")
-logger.info("TEST: Info message")
-logger.warning("TEST: Warning message")
-logger.error("TEST: Error message")
+setup_logging(stdout_log_level=STDOUT_LOG_LEVEL, stderr_log_level=STDERR_LOG_LEVEL)

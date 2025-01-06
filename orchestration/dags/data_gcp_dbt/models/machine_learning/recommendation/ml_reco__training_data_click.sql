e7e202e439987d0f0467a8d47d2550007571b526
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-01-06 14:30:51 +0100

    chore: Uniformize table names

--- orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__training_data_click.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__training_data_click.sql
@@ -1,23 +1,23 @@
 with
     events as (
         select
-            native_event.user_id,
-            native_event.offer_id,
-            native_event.event_date,
-            extract(hour from native_event.event_timestamp) as event_hour,
-            extract(dayofweek from native_event.event_timestamp) as event_day,
-            extract(month from native_event.event_timestamp) as event_month
-        from {{ ref("int_firebase__native_event") }} as native_event
+            native_events.user_id,
+            native_events.offer_id,
+            native_events.event_date,
+            extract(hour from native_events.event_timestamp) as event_hour,
+            extract(dayofweek from native_events.event_timestamp) as event_day,
+            extract(month from native_events.event_timestamp) as event_month
+        from {{ ref("int_firebase__native_event") }} as native_events
         where
-            native_event.event_name = "ConsultOffer"
-            and native_event.event_date
+            native_events.event_name = "ConsultOffer"
+            and native_events.event_date
             >= date_sub(date("{{ ds() }}"), interval 6 month)
-            and native_event.user_id is not null
-            and native_event.offer_id is not null
-            and native_event.offer_id != "NaN"
+            and native_events.user_id is not null
+            and native_events.offer_id is not null
+            and native_events.offer_id != "NaN"
     ),
 
-    click as (
+    clicks as (
         select
             events.event_date,
             events.user_id,
@@ -34,34 +34,34 @@ with
     )
 
 select distinct
-    click.event_date,
-    click.user_id,
-    click.item_id,
-    click.event_hour,
-    click.event_day,
-    click.event_month,
-    user_feature.consult_offer,
-    user_feature.booking_cnt,
-    user_feature.user_theoretical_remaining_credit,
-    user_feature.has_added_offer_to_favorites,
-    user_feature.user_queries,
-    user_feature.qpi_subcategory_ids,
-    item_feature.offer_subcategory_id,
-    item_feature.offer_category_id,
-    item_feature.item_image_embedding,
-    item_feature.item_semantic_content_hybrid_embedding,
-    item_feature.item_names,
-    item_feature.item_descriptions,
-    item_feature.item_rayons,
-    item_feature.item_author,
-    item_feature.item_performer,
-    item_feature.item_mean_stock_price,
-    item_feature.item_booking_cnt,
-    item_feature.item_favourite_cnt
-from click
+    clicks.event_date,
+    clicks.user_id,
+    clicks.item_id,
+    clicks.event_hour,
+    clicks.event_day,
+    clicks.event_month,
+    user_features.consult_offer,
+    user_features.booking_cnt,
+    user_features.user_theoretical_remaining_credit,
+    user_features.has_added_offer_to_favorites,
+    user_features.user_queries,
+    user_features.qpi_subcategory_ids,
+    item_features.offer_subcategory_id,
+    item_features.offer_category_id,
+    item_features.item_image_embedding,
+    item_features.item_semantic_content_hybrid_embedding,
+    item_features.item_names,
+    item_features.item_descriptions,
+    item_features.item_rayons,
+    item_features.item_author,
+    item_features.item_performer,
+    item_features.item_mean_stock_price,
+    item_features.item_booking_cnt,
+    item_features.item_favourite_cnt
+from clicks
 inner join
-    {{ ref("ml_reco__training_data_user_feature") }} as user_feature
-    on click.user_id = user_feature.user_id
+    {{ ref("ml_reco__training_data_user_feature") }} as user_features
+    on clicks.user_id = user_features.user_id
 inner join
-    {{ ref("ml_reco__training_data_item_feature") }} as item_feature
-    on click.item_id = item_feature.item_id
+    {{ ref("ml_reco__training_data_item_feature") }} as item_features
+    on clicks.item_id = item_features.item_id

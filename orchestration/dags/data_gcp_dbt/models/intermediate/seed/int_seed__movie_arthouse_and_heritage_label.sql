b9c8ab5e605276a7733a71ad0119848b42ee27d6
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2025-01-08 19:04:19 +0100

    fix raw source and column aliases

--- orchestration/dags/data_gcp_dbt/models/intermediate/seed/int_seed__movie_arthouse_and_heritage_label.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/seed/int_seed__movie_arthouse_and_heritage_label.sql
@@ -4,6 +4,6 @@ select
     movie_distributor,
     cast(movie_release_date as date) as movie_release_date,
     safe_cast(safe_cast(movie_visa as float64) as string) as movie_visa
-from {{ source("seed", "gsheet_movie_arthouse_and_heritage_label") }}
+from {{ source("raw", "gsheet_movie_arthouse_and_heritage_label") }}
 where (movie_visa is not null and movie_visa != 'nan')
 qualify row_number() over (partition by movie_visa order by movie_status) = 1

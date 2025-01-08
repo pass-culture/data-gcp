a90be2396f2cd7a50392d80252d5768b910c8057
MatthieuRipollPassCulture <91483773+MatthieuRipollPassCulture@users.noreply.github.com>
2025-01-08 17:28:16 +0100

    replace safe cast

--- orchestration/dags/data_gcp_dbt/models/intermediate/seed/int_seed__movie_arthouse_and_heritage_label.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/seed/int_seed__movie_arthouse_and_heritage_label.sql
@@ -1,8 +1,9 @@
 select
     movie_status as movie_label,
-    split(movie_visa, '.')[offset(0)] as movie_visa,
     movie_title,
     movie_distributor,
-    cast(movie_release_date as date) as movie_release_date
+    cast(movie_release_date as date) as movie_release_date,
+    safe_cast(safe_cast(movie_visa as float64) as string) as movie_visa
 from {{ source("seed", "gsheet_movie_arthouse_and_heritage_label") }}
+where (movie_visa is not null and movie_visa != 'nan')
 qualify row_number() over (partition by movie_visa order by movie_status) = 1

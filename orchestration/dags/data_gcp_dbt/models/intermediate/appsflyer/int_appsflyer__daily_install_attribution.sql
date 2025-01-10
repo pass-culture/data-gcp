626aeec1810667db60fa8f20e979abfba86d7ec7
Timothée Scheurer <timothee.scheurer@passculture.app>
2025-01-10 11:39:23 +0100

    review update models

---
+++ orchestration/dags/data_gcp_dbt/models/intermediate/appsflyer/int_appsflyer__daily_install_attribution.sql
@@ -0,0 +1,100 @@
+{{
+    config(
+        **custom_incremental_config(
+            incremental_strategy="insert_overwrite",
+            partition_by={"field": "app_install_date", "data_type": "date"},
+            on_schema_change="sync_all_columns",
+        )
+    )
+}}
+
+
+
+-- Aggregates user events, picking the latest version per app_install_date and acquisition_days_post_attribution
+select
+            app_id,
+            media_source as acquisition_media_source,
+            campaign as acquisition_campaign,
+            adset as acquisition_adset,
+            ad as acquisition_ad,
+            version as acquisition_version,
+            cast(days_post_attribution as INT64) as acquisition_days_post_attribution,
+            cast(conversion_date as DATE) as app_install_date,
+            sum(
+                if(
+                    event_name = 'af_complete_registration',
+                    cast(unique_users as INT64),
+                    0
+                )
+            ) as registration,
+            sum(
+                if(
+                    event_name = 'af_complete_beneficiary',
+                    cast(unique_users as INT64),
+                    0
+                )
+            ) as beneficiary,
+            sum(
+                if(
+                    event_name = 'af_complete_beneficiary_18',
+                    cast(unique_users as INT64),
+                    0
+                )
+            ) as beneficiary_18,
+            sum(
+                if(
+                    event_name = 'af_complete_beneficiary_underage',
+                    cast(unique_users as INT64),
+                    0
+                )
+            ) as beneficiary_underage,
+            sum(
+                if(
+                    event_name = 'af_complete_beneficiary_17',
+                    cast(unique_users as INT64),
+                    0
+                )
+            ) as beneficiary_17,
+            sum(
+                if(
+                    event_name = 'af_complete_beneficiary_16',
+                    cast(unique_users as INT64),
+                    0
+                )
+            ) as beneficiary_16,
+            sum(
+                if(
+                    event_name = 'af_complete_beneficiary_15',
+                    cast(unique_users as INT64),
+                    0
+                )
+            ) as beneficiary_15
+        from {{ source("appsflyer_import", "cohort_unified_timezone_versioned") }}
+        where cast(days_post_attribution as INT64) < 14
+        {% if is_incremental() %}
+            and
+                cast(conversion_date as date)
+                between date_sub(date("{{ ds() }}"), interval 14 day) and date("{{ ds() }}")
+        {% endif %}
+        group by
+            app_install_date,
+            app_id,
+            acquisition_media_source,
+            acquisition_campaign,
+            acquisition_adset,
+            acquisition_ad,
+            acquisition_days_post_attribution,
+            acquisition_version
+        qualify
+            row_number() over (
+                partition by
+                    app_install_date,
+                    app_id,
+                    acquisition_media_source,
+                    acquisition_campaign,
+                    acquisition_adset,
+                    acquisition_ad,
+                    acquisition_days_post_attribution
+                order by acquisition_version desc
+            )
+            = 1
---
+++ orchestration/dags/data_gcp_dbt/models/mart/marketing/mrt_marketing__daily_install_acquisition_campaign.sql
@@ -0,0 +1,62 @@
+{{
+    config(
+        **custom_incremental_config(
+            incremental_strategy="insert_overwrite",
+            partition_by={"field": "app_install_date", "data_type": "date"},
+            on_schema_change="sync_all_columns",
+        )
+    )
+}}
+
+
+with
+
+-- Consolidates user events by app_install_date
+daily_attribution_summary as (
+select
+    app_install_date,
+    app_id,
+    acquisition_media_source,
+    acquisition_campaign,
+    acquisition_adset,
+    acquisition_ad,
+    sum(registration) as total_registrations,
+    sum(beneficiary) as total_beneficiaries,
+    sum(beneficiary_underage) as total_beneficiaries_underage,
+    sum(beneficiary_18) as total_beneficiaries_18,
+    sum(beneficiary_17) as total_beneficiaries_17,
+    sum(beneficiary_16) as total_beneficiaries_16,
+    sum(beneficiary_15) as total_beneficiaries_15
+from {{ ref("int_appsflyer__daily_install_attribution") }}
+{% if is_incremental() %}
+    where
+        app_install_date
+        between date_sub(date("{{ ds() }}"), interval 14 day) and date("{{ ds() }}")
+{% endif %}
+group by app_install_date, app_id, acquisition_media_source, acquisition_campaign, acquisition_adset, acquisition_ad)
+
+select
+    daily_install_cost.app_install_date,
+    daily_install_cost.app_os,
+    daily_install_cost.acquisition_media_source,
+    daily_install_cost.acquisition_campaign,
+    daily_install_cost.acquisition_adset,
+    daily_install_cost.acquisition_ad,
+    daily_install_cost.total_costs,
+    daily_install_cost.total_installs,
+    daily_attribution_summary.total_registrations,
+    daily_attribution_summary.total_beneficiaries,
+    daily_attribution_summary.total_beneficiaries_underage,
+    daily_attribution_summary.total_beneficiaries_18,
+    daily_attribution_summary.total_beneficiaries_17,
+    daily_attribution_summary.total_beneficiaries_16,
+    daily_attribution_summary.total_beneficiaries_15
+from {{ ref("int_appsflyer__daily_install_cost") }} as daily_install_cost
+inner join
+    daily_attribution_summary
+    on daily_install_cost.app_install_date = daily_attribution_summary.app_install_date
+    and daily_install_cost.app_id = daily_attribution_summary.app_id
+    and daily_install_cost.acquisition_media_source = daily_attribution_summary.acquisition_media_source
+    and daily_install_cost.acquisition_campaign = daily_attribution_summary.acquisition_campaign
+    and daily_install_cost.acquisition_adset = daily_attribution_summary.acquisition_adset
+    and daily_install_cost.acquisition_ad = daily_attribution_summary.acquisition_ad

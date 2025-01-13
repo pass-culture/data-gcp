fe8f12f43a485d8ad26828492ea77d3f194d8efc
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-01-13 11:19:30 +0100

    lint

--- orchestration/dags/data_gcp_dbt/tests/generic/not_null_multiple_columns.sql
+++ orchestration/dags/data_gcp_dbt/tests/generic/not_null_multiple_columns.sql
@@ -1,11 +1,11 @@
 {% test not_null_multiple_columns(model, columns) %}
 
-    {{
-        config(severity='ERROR')
-    }}
-
     {% if not columns %}
-        {{ exceptions.raise_compiler_error("The 'columns' argument is required for the not_null_multiple_columns test.") }}
+        {{
+            exceptions.raise_compiler_error(
+                "The 'columns' argument is required for the not_null_multiple_columns test."
+            )
+        }}
     {% endif %}
 
     {% set conditions = [] %}
@@ -13,12 +13,14 @@
         {% do conditions.append(column ~ " IS NULL") %}
     {% endfor %}
 
-    with violations as (SELECT
-        COUNT(*) AS num_violations
-    FROM {{ model }}
-    WHERE {{ conditions | join(' OR ') }}
-    )
-    select * from violations
+    with
+        violations as (
+            select count(*) as num_violations
+            from {{ model }}
+            where {{ conditions | join(" OR ") }}
+        )
+    select *
+    from violations
     where num_violations > 0
 
 {% endtest %}

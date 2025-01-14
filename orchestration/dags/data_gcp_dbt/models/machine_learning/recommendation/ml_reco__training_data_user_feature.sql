ec167a8eb88b2736db73284829b6a7aa32c8570f
lmontier-pass <172769973+lmontier-pass@users.noreply.github.com>
2025-01-14 17:10:52 +0100

    (BSR) fix(query): Fix user theoritical credit null (#3709)
    * fix: Remove users without last_depositf_amount in user stats
    
    * lint: lint queries

--- orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__training_data_user_feature.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__training_data_user_feature.sql
@@ -9,25 +9,26 @@ with
         where subcategories <> 'none'
         group by user_id
     )
+
 select
     firebase_agg.user_id,
     firebase_agg.consult_offer,
     stats_reco.booking_cnt,
+    stats_reco.has_added_offer_to_favorites,
+    uqpi.qpi_subcategory_ids,
     round(
         stats_reco.user_theoretical_remaining_credit, 0
     ) as user_theoretical_remaining_credit,
-    stats_reco.has_added_offer_to_favorites,
-    string_agg(distinct firebase.query, " ") as user_queries,
-    uqpi.qpi_subcategory_ids as qpi_subcategory_ids
+    string_agg(distinct firebase.query, ' ') as user_queries
 
-from {{ ref("firebase_aggregated_users") }} firebase_agg
-join
-    {{ ref("ml_reco__user_statistics") }} stats_reco
-    on stats_reco.user_id = firebase_agg.user_id
-join
-    {{ ref("int_firebase__native_event") }} firebase
-    on firebase.user_id = firebase_agg.user_id
-left join user_qpi uqpi on uqpi.user_id = firebase_agg.user_id
+from {{ ref("firebase_aggregated_users") }} as firebase_agg
+inner join
+    {{ ref("ml_reco__user_statistics") }} as stats_reco
+    on firebase_agg.user_id = stats_reco.user_id
+inner join
+    {{ ref("int_firebase__native_event") }} as firebase
+    on firebase_agg.user_id = firebase.user_id
+left join user_qpi as uqpi on firebase_agg.user_id = uqpi.user_id
 where firebase.event_date >= date_sub(current_date, interval 6 month)
 group by
     firebase_agg.user_id,

8b0af8cdce6add2c00799ebc2d4d4d89b2e0e0b5
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-01-15 16:30:04 +0100

    (BSR) feat(clickhouse): compress native_event

--- jobs/etl_jobs/internal/export_clickhouse/schema/intermediate/native_event.sql
+++ jobs/etl_jobs/internal/export_clickhouse/schema/intermediate/native_event.sql
@@ -1,17 +1,16 @@
 CREATE TABLE IF NOT EXISTS intermediate.native_event ON CLUSTER default
 (
-    partition_date Date,
-    event_name String,
-    event_timestamp Nullable(DateTime),
-    offer_id Nullable(String),
-    user_id Nullable(String),
-    unique_session_id Nullable(String),
-    venue_id Nullable(String),
-    origin Nullable(String),
-    is_consult_offer UInt8,
-    is_consult_venue UInt8,
-    is_add_to_favorites UInt8
-
+    partition_date Date CODEC(ZSTD),
+    event_name String CODEC(ZSTD),
+    event_timestamp Nullable(DateTime) CODEC(Delta, ZSTD),
+    offer_id Nullable(String) CODEC(ZSTD),
+    user_id Nullable(String) CODEC(ZSTD),
+    unique_session_id Nullable(String) CODEC(ZSTD),
+    venue_id Nullable(String) CODEC(ZSTD),
+    origin Nullable(String) CODEC(ZSTD),
+    is_consult_venue UInt8 CODEC(LZ4),
+    is_consult_offer UInt8 CODEC(LZ4),
+    is_add_to_favorites UInt8 CODEC(LZ4)
 )
 ENGINE = MergeTree
 PARTITION BY partition_date

10ede78e9e0d8568d9efe02ab7a1799b2d7d8731
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-01-15 09:46:52 +0100

    (DE-807) feat(dbt): add lookback filter to cardinality equality test and set bookings test lookback to 6 months (#3712)

--- orchestration/dags/data_gcp_dbt/tests/generic/cardinality_equality.sql
+++ orchestration/dags/data_gcp_dbt/tests/generic/cardinality_equality.sql
@@ -1,16 +1,47 @@
-{% test cardinality_equality(model, column_name, to, field) %}
+{% test cardinality_equality(
+    model,
+    column_name,
+    to,
+    field,
+    lookback_month=None,
+    lookback_fields=["field_model", "field_to"]
+) %}
+
+    {%- set date_filter -%}
+        {% if lookback_month is not none %}
+            and {{ lookback_fields[0] }} >= date_sub(date("{{ ds() }}"), interval {{ lookback_month }} month)
+        {% else %}
+
+        {% endif %}
+    {%- endset -%}
+    {%- set date_filter_to -%}
+        {% if lookback_month is not none %}
+            and {{ lookback_fields[1] }} >= date_sub(date("{{ ds() }}"), interval {{ lookback_month }} month)
+        {% else %}
+        {% endif %}
+    {%- endset -%}
 
     with
+        filtered_model as (
+            select {{ column_name }} from {{ model }} where true {{ date_filter }}
+        ),
+
+        filtered_to as (
+            select {{ field }} from {{ to }} where true {{ date_filter_to }}
+        ),
+
         table as (
             select
-                (select count(distinct {{ field }}) from {{ to }}) as num_rows_to,
+                (select count(distinct {{ field }}) from filtered_to) as num_rows_to,
                 (
-                    select count(distinct {{ column_name }}) from {{ model }}
+                    select count(distinct {{ column_name }}) from filtered_model
                 ) as num_rows_table
         ),
 
-        ratio_table as (select num_rows_table / num_rows_to * 100 as ratio from table)
-    select 100 - ratio as ratio
+        ratio_table as (select num_rows_table / num_rows_to as ratio from table)
+
+    select 100 * (1 - ratio) as matching_percentage
     from ratio_table
-    where 100 - ratio > {{ var("not_null_anomaly_threshold_alert_percentage") }}
+    where ratio > {{ var("not_null_anomaly_threshold_alert_percentage") }}
+
 {% endtest %}

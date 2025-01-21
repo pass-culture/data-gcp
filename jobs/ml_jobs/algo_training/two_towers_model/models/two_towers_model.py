e9badb1481f5b41ed9052bf9bf1586085df41999
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-01-21 12:24:54 +0100

    WIP: Set from_logits to false

--- jobs/ml_jobs/algo_training/two_towers_model/models/two_towers_model.py
+++ jobs/ml_jobs/algo_training/two_towers_model/models/two_towers_model.py
@@ -61,7 +61,7 @@ class TwoTowersModel(tfrs.models.Model):
     def set_task(self, item_dataset=None):
         self.task = tfrs.tasks.Retrieval(
             loss=tf.keras.losses.CategoricalCrossentropy(
-                from_logits=True,
+                from_logits=False,
                 reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE,
             ),
             metrics=self.get_metrics(item_dataset) if item_dataset else None,

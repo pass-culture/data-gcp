3e256ab95fab87c3d417c1af1542be9b5477ae2d
Lucille <Lucile@MacBook-Air-de-Lucille.local>
2025-01-21 10:52:40 +0100

    fix pro_event to get non nested events

--- orchestration/dags/data_gcp_dbt/models/intermediate/firebase/pro/int_firebase__pro_event.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/firebase/pro/int_firebase__pro_event.sql
@@ -13,82 +13,39 @@
     )
 }}
 
-with
-    pro_event_raw_data as (
-        select
-            event_name,
-            user_pseudo_id,
-            case
-                when regexp_contains(user_id, r"\D")
-                then {{ target_schema }}.dehumanize_id(user_id)
-                else user_id
-            end as user_id,
-            platform,
-            event_date,
-            event_timestamp,
-            ga_session_number as session_number,
-            ga_session_id as session_id,
-            concat(user_pseudo_id, '-', ga_session_id) as unique_session_id,
-            origin,
-            destination,
-            traffic_campaign,
-            traffic_medium,
-            traffic_source,
-            category as user_device_category,
-            operating_system as user_device_operating_system,
-            operating_system_version as user_device_operating_system_version,
-            browser as user_web_browser,
-            browser_version as user_web_browser_version,
-            coalesce(
-                cast(offerer_id as string), cast(offererid as string)
-            ) as offerer_id,
-            coalesce(venue_id, cast(venueid as string)) as venue_id,
-            page_location,
-            page_title as page_name,
-            regexp_extract(
-                page_location, r"""passculture\.pro\/(.*)$""", 1
-            ) as url_first_path,
-            regexp_extract(
-                page_location, r"""passculture\.pro\/(.*?)[\/.*?\/.*?\/|\?]""", 1
-            ) as url_path_type,
-            regexp_extract(
-                page_location, r"""passculture\.pro\/.*?\/.*?\/(.*?)\/|\?""", 1
-            ) as url_path_details,
-            regexp_extract(
-                page_location, r"""passculture\.pro\/(.*?)\?""", 1
-            ) as url_path_before_params,
-            regexp_extract_all(
-                page_location, r'(?:\?|&)(?:([^=]+)=(?:[^&]*))'
-            ) as url_params_key,
-            regexp_extract_all(
-                page_location, r'(?:\?|&)(?:(?:[^=]+)=([^&]*))'
-            ) as url_params_value,
-            regexp_replace(
-                regexp_replace(page_location, "[A-Z \\d]+[\\?\\/\\&]?", ""),
-                "https://passculture.pro/",
-                ""
-            ) as url_path_agg,
-            page_referrer,
-            page_number,
-            coalesce(double_offer_id, offerid) as offer_id,
-            offertype as offer_type,
-            saved as has_saved_query,
-            hasonly6eand5estudents as has_opened_wrong_student_modal,
-            isedition as is_edition,
-            isdraft as is_draft,
-            filled,
-            filledwitherrors as filled_with_errors,
-            categoriejuridiqueunitelegale as onboarding_selected_legal_category,
-            format as download_format,
-            bookingstatus as download_booking_status,
-            buttontype as download_button_type,
-            filetype as download_file_type,
-            filescount as download_files_cnt,
-            subcategoryid as offer_subcategory_id,
-            choosensuggestedsubcategory as suggested_offer_subcategory_selected,
-            status as offer_status,
-            selected_offers
-        from {{ ref("int_firebase__pro_event_flattened") }}
+WITH pro_event_raw_data AS (
+  SELECT
+      event_name,
+      user_pseudo_id,
+      user_id,
+      platform,
+      event_date,
+      event_timestamp,
+      ga_session_number AS session_number,
+      ga_session_id AS session_id,
+      CONCAT(user_pseudo_id, '-', ga_session_id) AS unique_session_id,
+      origin,
+      destination,
+      traffic_campaign,
+      traffic_medium,
+      traffic_source,
+      category AS user_device_category,
+      operating_system AS user_device_operating_system,
+      operating_system_version AS user_device_operating_system_version,
+      browser AS user_web_browser,
+      browser_version AS user_web_browser_version,
+      COALESCE(CAST(offerer_id AS STRING), CAST(offererid AS STRING)) AS offerer_id,
+      COALESCE(venue_id, CAST(venueid AS STRING)) AS venue_id,
+      page_location,
+      page_title AS page_name,
+      REGEXP_EXTRACT(page_location, r"passculture\.pro\/([^\/\?]+)", 1) AS url_path_type,
+      REGEXP_EXTRACT(page_location, r"passculture\.pro\/[^\/]+\/([^\/\?]+)", 1) AS url_path_details,
+      JSON_EXTRACT_ARRAY(selected_offers) AS selected_offers_array,
+      ARRAY_LENGTH(JSON_EXTRACT_ARRAY(selected_offers)) > 1 AS multiple_selection,
+      COALESCE(CAST(double_offer_id AS STRING), CAST(offerid AS STRING)) AS offer_id,
+      status AS offer_status,
+      offerType AS offer_type
+  from {{ ref("int_firebase__pro_event_flattened") }}
         {% if is_incremental() %}
             where
                 event_date
@@ -96,41 +53,94 @@ with
                     "{{ ds() }}"
                 )
         {% endif %}
-    ),
 
-    clean_url_path_and_multiple_selection as (
-        select
-            * except (
-                url_first_path,
-                url_path_type,
-                url_path_details,
-                url_path_before_params,
-                selected_offers
-            ),
-            case
-                when url_path_details is null
-                then replace(coalesce(url_path_before_params, url_first_path), "/", "-")
-                when url_path_details is not null
-                then concat(url_path_type, "-", url_path_details)
-                else url_path_type
-            end as url_path_extract,
-            json_extract_array(selected_offers) as selected_offers_array,
-            array_length(json_extract_array(selected_offers)) > 1 as multiple_selection
-        from pro_event_raw_data
-    ),
+),
 
-    final as (
-        select
-            * except (selected_offers_array, offer_id, offer_status),
-            coalesce(
-                json_extract_scalar(item, "$.offerId"), cast(offer_id as string)
-            ) as offer_id,
-            coalesce(
-                json_extract_scalar(item, "$.offerStatus"), offer_status
-            ) as offer_status,
-        from
-            clean_url_path_and_multiple_selection, unnest(selected_offers_array) as item
-    )
+unnested_events AS (
+  SELECT
+      *,
+      JSON_EXTRACT_SCALAR(item, "$.offerId") AS offer_id_from_array,
+      JSON_EXTRACT_SCALAR(item, "$.offerStatus") AS offer_status_from_array,
+      multiple_selection AS is_multiple_selection
+  FROM
+      pro_event_raw_data, UNNEST(selected_offers_array) AS item
+),
 
-select * except (item)
-from final
+combined_events AS (
+  SELECT
+      event_name,
+      user_pseudo_id,
+      user_id,
+      platform,
+      event_date,
+      event_timestamp,
+      session_number,
+      session_id,
+      unique_session_id,
+      origin,
+      destination,
+      traffic_campaign,
+      traffic_medium,
+      traffic_source,
+      user_device_category,
+      user_device_operating_system,
+      user_device_operating_system_version,
+      user_web_browser,
+      user_web_browser_version,
+      offerer_id,
+      venue_id,
+      page_location,
+      page_name,
+      url_path_type,
+      url_path_details,
+      is_multiple_selection AS multiple_selection, -- Utilisation du champ correct
+      COALESCE(offer_id_from_array, offer_id) AS offer_id,
+      COALESCE(offer_status_from_array, offer_status) AS offer_status,
+      CAST(offer_type AS STRING) AS offer_type
+  FROM
+      unnested_events
+
+  UNION ALL
+
+  SELECT
+      event_name,
+      user_pseudo_id,
+      user_id,
+      platform,
+      event_date,
+      event_timestamp,
+      session_number,
+      session_id,
+      unique_session_id,
+      origin,
+      destination,
+      traffic_campaign,
+      traffic_medium,
+      traffic_source,
+      user_device_category,
+      user_device_operating_system,
+      user_device_operating_system_version,
+      user_web_browser,
+      user_web_browser_version,
+      offerer_id,
+      venue_id,
+      page_location,
+      page_name,
+      url_path_type,
+      url_path_details,
+      FALSE AS multiple_selection,
+      CAST(offer_id AS STRING) AS offer_id,
+      CAST(offer_status AS STRING) AS offer_status,
+      CAST(offer_type AS STRING) AS offer_type
+  FROM
+      pro_event_raw_data
+  WHERE
+      selected_offers_array IS NULL
+)
+
+SELECT DISTINCT
+    *
+FROM
+    combined_events
+
+

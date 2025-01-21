bddfd6e9c6df100e30766121194c86a1256ac851
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-01-21 08:59:12 +0100

    WIPP: v2

--- orchestration/dags/airflow_monitorng_v2.py
+++ orchestration/dags/airflow_monitorng_v2.py
@@ -1,53 +1,87 @@
 import datetime
+import json
+
+from common.config import (
+    SLACK_CONN_PASSWORD,
+)
+from sqlalchemy import func
 
 from airflow import DAG
-from airflow.models import DagBag
+from airflow.models import DagRun
 from airflow.operators.python import PythonOperator
+from airflow.providers.http.operators.http import HttpOperator
 from airflow.utils.dates import days_ago
+from airflow.utils.session import create_session
 
 # # Slack webhook URL (store in Airflow Variables or Connections for security)
 # SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/your/webhook/url"
 
 
 def generate_dag_status_report():
-    dag_bag = DagBag()
-    dag_statuses = {"complete": 0, "running": 0, "error": 0}
+    dag_statuses = {"complete": 0, "running": 0, "error": 0, "dags_in_error": []}
 
-    for dag_id in dag_bag.dag_ids:
-        dag = dag_bag.get_dag(dag_id)
-        latest_run = dag.get_latest_execution_date()
+    with create_session() as session:
+        # Query the last DAG runs and their states
+        # Subquery to get the latest run for each dag_id
+        subquery = (
+            session.query(
+                DagRun.dag_id,
+                func.max(DagRun.execution_date).label("latest_execution_date"),
+            )
+            .group_by(DagRun.dag_id)
+            .subquery()
+        )
 
-        if latest_run:
-            session = dag_bag.get_session()
-            dag_run = dag.get_last_dagrun(session=session)
+        # Join the subquery to get the states of the latest runs
+        latest_runs = (
+            session.query(DagRun.dag_id, DagRun.state)
+            .join(
+                subquery,
+                (DagRun.dag_id == subquery.c.dag_id)
+                & (DagRun.execution_date == subquery.c.latest_execution_date),
+            )
+            .all()
+        )
 
-            if dag_run and dag_run.state:
-                state = dag_run.state
-                if state == "success":
-                    dag_statuses["complete"] += 1
-                elif state == "running":
-                    dag_statuses["running"] += 1
-                elif state in ["failed", "up_for_retry"]:
-                    dag_statuses["error"] += 1
+        for dag_id, state in latest_runs:
+            if state == "success":
+                dag_statuses["complete"] += 1
+            elif state == "running":
+                dag_statuses["running"] += 1
+            elif state in ["failed", "up_for_retry"]:
+                dag_statuses["error"] += 1
+                dag_statuses["dags_in_error"].append(dag_id)
 
-    return dag_statuses
+    message = {
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": (
+                        f"*Cluster Summary for {datetime.date.today()}*:\n"
+                        f"- {dag_statuses['complete']} DAGs complete\n"
+                        f"- {dag_statuses['running']} DAGs running\n"
+                        f"- {dag_statuses['error']} DAGs in error\n"
+                        f"- Dags in error: {', '.join(dag_statuses['dags_in_error'])}"
+                    ),
+                },
+            }
+        ]
+    }
+    return json.dumps(message)
 
 
-def format_slack_message(**context):
-    statuses = context["ti"].xcom_pull(task_ids="generate_report")
-    message = (
-        f"*Cluster Summary for {datetime.date.today()}*:\n"
-        f"- {statuses['complete']} DAGs complete\n"
-        f"- {statuses['running']} DAGs running\n"
-        f"- {statuses['error']} DAGs in error"
-    )
-    return message
-
-
-def simple_print_task(**context):
-    # Retrieve the formatted Slack message from XCom
-    message = context["ti"].xcom_pull(task_ids="format_slack_message")
-    print(f"Slack Message: {message}")
+# def format_slack_message(**context):
+#     statuses = context["ti"].xcom_pull(task_ids="generate_report")
+#     message = (
+#         f"*Cluster Summary for {datetime.date.today()}*:\n"
+#         f"- {statuses['complete']} DAGs complete\n"
+#         f"- {statuses['running']} DAGs running\n"
+#         f"- {statuses['error']} DAGs in error\n"
+#         f"- Dags in error: {', '.join(statuses['dags_in_error'])}"
+#     )
+#     return message
 
 
 with DAG(
@@ -60,6 +94,7 @@ with DAG(
     schedule_interval="0 8 * * *",
     start_date=days_ago(1),
     catchup=False,
+    tags=["DEBUG_ON_DEV", "@Laurent"],
 ) as dag:
     generate_report = PythonOperator(
         task_id="generate_report",
@@ -73,10 +108,13 @@ with DAG(
     #     message="{{ task_instance.xcom_pull(task_ids='generate_report') | format_slack_message }}",
     # )
 
-    send_report = PythonOperator(
-        task_id="simple_print",
-        python_callable=simple_print_task,
-        provide_context=True,
+    send_slack = HttpOperator(
+        task_id="send_slack_notif_success",
+        method="POST",
+        http_conn_id="http_slack_default",
+        endpoint=f"{SLACK_CONN_PASSWORD}",
+        data="{{ task_instance.xcom_pull(task_ids='generate_report') }}",
+        headers={"Content-Type": "application/json"},
     )
 
-    generate_report >> send_report
+    generate_report >> send_slack

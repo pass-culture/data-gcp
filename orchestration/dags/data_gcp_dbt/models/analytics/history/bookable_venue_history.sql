6873a90f0890e88041d2676f16ef28191cdcff47
Julpass <jules.arbelot@passculture.app>
2025-01-23 16:42:53 +0100

    update

--- orchestration/dags/data_gcp_dbt/models/analytics/history/bookable_venue_history.sql
+++ orchestration/dags/data_gcp_dbt/models/analytics/history/bookable_venue_history.sql
@@ -1,68 +1,85 @@
 {{
     config(
         **custom_incremental_config(
-        incremental_strategy = 'insert_overwrite',
-        partition_by = {'field': 'partition_date', 'data_type': 'date'},
+            incremental_strategy="insert_overwrite",
+            partition_by={"field": "partition_date", "data_type": "date"},
+        )
     )
-) }}
+}}
 
 
-with all_bookable_data as (
-    select
-        o.venue_id,
-        v.venue_managing_offerer_id as offerer_id,
-        DATE('{{ ds() }}') AS partition_date,
-        "individual" as offer_type,
-        COUNT(CASE
-              WHEN DATE_SUB('{{ ds() }}', interval 1 day)  >= DATE(dbt_valid_from)
-                   AND (dbt_valid_to IS NULL OR DATE_SUB('{{ ds() }}', interval 1 day) <= DATE(dbt_valid_to))
-              THEN offer_id
-              ELSE NULL
-         END) AS total_bookable_offers
-    from {{ ref('snapshot__bookable_offer') }}
+with
+    all_bookable_data as (
+        select
+            o.venue_id,
+            v.venue_managing_offerer_id as offerer_id,
+            date('{{ ds() }}') as partition_date,
+            'individual' as offer_type,
+            count(
+                case
+                    when
+                        date_sub('{{ ds() }}', interval 1 day) >= date(s.dbt_valid_from)
+                        and (
+                            s.dbt_valid_to is null
+                            or date_sub('{{ ds() }}', interval 1 day)
+                            <= date(s.dbt_valid_to)
+                        )
+                    then s.offer_id
+                end
+            ) as total_bookable_offers
+        from {{ ref("snapshot__bookable_offer") }} as s
         inner join {{ ref("int_applicative__offer") }} as o using (offer_id)
-        inner join {{ source('raw','applicative_database_venue')  }} as v using (venue_id)
-    group by venue_id,
-        offerer_id,
-        partition_date,
-        offer_type
-    union all
-    select
-        o.venue_id,
-        venue_managing_offerer_id as offerer_id,
-        DATE('{{ ds() }}') AS partition_date,
-        "collective" as offer_type,
-        COUNT(CASE
-              WHEN DATE_SUB('{{ ds() }}', interval 1 day) >= DATE(dbt_valid_from)
-                   AND (dbt_valid_to IS NULL OR DATE_SUB('{{ ds() }}', interval 1 day) <= DATE(dbt_valid_to))
-              THEN collective_offer_id
-              ELSE NULL
-         END) AS total_bookable_offers
-    from {{ ref('snapshot__bookable_collective_offer') }}
-        inner join {{ source('raw','applicative_database_collective_offer') }} as o using (collective_offer_id)
-        inner join {{ source('raw','applicative_database_venue')  }} as v using (venue_id)
-    group by venue_id,
-        offerer_id,
-        partition_date,
-        offer_type
-),
+        inner join
+            {{ source("raw", "applicative_database_venue") }} as v using (venue_id)
+        group by o.venue_id, v.venue_managing_offerer_id, date('{{ ds() }}'), 'individual'
+        union all
+        select
+            o.venue_id,
+            v.venue_managing_offerer_id as offerer_id,
+            date('{{ ds() }}') as partition_date,
+            'collective' as offer_type,
+            count(
+                case
+                    when
+                        date_sub('{{ ds() }}', interval 1 day) >= date(sb.dbt_valid_from)
+                        and (
+                            sb.dbt_valid_to is null
+                            or date_sub('{{ ds() }}', interval 1 day)
+                            <= date(sb.dbt_valid_to)
+                        )
+                    then sb.collective_offer_id
+                end
+            ) as total_bookable_offers
+        from {{ ref("snapshot__bookable_collective_offer") }} as sb
+        inner join
+            {{ source("raw", "applicative_database_collective_offer") }} as o using (
+                collective_offer_id
+            )
+        inner join
+            {{ source("raw", "applicative_database_venue") }} as v using (venue_id)
+        group by o.venue_id, v.venue_managing_offerer_id, date('{{ ds() }}'), 'collective'
+    ),
 
-pivoted_data as (
-    select
-        venue_id,
-        offerer_id,
-        partition_date,
-        individual as total_individual_bookable_offers,
-        collective as total_collective_bookable_offers
-    from all_bookable_data
-        pivot (SUM(total_bookable_offers) for offer_type in ('individual', 'collective'))
-)
+    pivoted_data as (
+        select
+            venue_id,
+            offerer_id,
+            partition_date,
+            individual as total_individual_bookable_offers,
+            collective as total_collective_bookable_offers
+        from
+            all_bookable_data pivot (
+                sum(total_bookable_offers) for offer_type
+                in ('individual', 'collective')
+            )
+    )
 
 select
     venue_id,
     offerer_id,
     partition_date,
-    COALESCE(total_individual_bookable_offers, 0) as total_individual_bookable_offers,
-    COALESCE(total_collective_bookable_offers, 0) as total_collective_bookable_offers,
-    COALESCE(total_individual_bookable_offers, 0) + COALESCE(total_collective_bookable_offers, 0) as total_bookable_offers
+    coalesce(total_individual_bookable_offers, 0) as total_individual_bookable_offers,
+    coalesce(total_collective_bookable_offers, 0) as total_collective_bookable_offers,
+    coalesce(total_individual_bookable_offers, 0)
+    + coalesce(total_collective_bookable_offers, 0) as total_bookable_offers
 from pivoted_data

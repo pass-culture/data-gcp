8729b3fd04d8de8bb7e5092904bcfb6b2179ba0d
Mathilde Vey <133665016+mvey-pass@users.noreply.github.com>
2025-01-24 16:19:35 +0100

    Create int_firebase__pro_experiment

---
+++ orchestration/dags/data_gcp_dbt/models/intermediate/firebase/pro/int_firebase__pro_experiment.sql
@@ -0,0 +1,31 @@
+{{
+    config(
+        **custom_incremental_config(
+            incremental_strategy="insert_overwrite",
+            partition_by={"field": "event_date", "data_type": "date"},
+            on_schema_change="sync_all_columns"
+        )
+    )
+}}
+
+SELECT DISTINCT
+    event_date,
+    user_pseudo_id,
+    user_id,
+    (
+        SELECT value.int_value 
+        FROM UNNEST(user_properties) 
+        WHERE key = 'offerer_id'
+    ) AS offerer_id,
+    event_params.key AS experiment_name,
+    CAST(event_params.value.string_value AS BOOLEAN) AS experiment_value
+FROM 
+    {{ source("raw", "firebase_pro_events") }}
+WHERE
+    (event_params.key = "PRO_DIDACTIC_ONBOARDING_AB_TEST" 
+     OR event_params.key LIKE "PRO_EXPERIMENT%")
+    AND event_params.value.string_value IS NOT NULL
+    {% if is_incremental() %}
+        AND event_date BETWEEN DATE_SUB(DATE("{{ ds() }}"), INTERVAL 1 DAY) AND DATE("{{ ds() }}")
+    {% endif %}
+

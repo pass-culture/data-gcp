8e3f74e35c66a27962fe81996dfd6ccaf25b304c
Mathilde Vey <133665016+mvey-pass@users.noreply.github.com>
2025-01-24 16:48:59 +0100

    fix

--- orchestration/dags/data_gcp_dbt/models/intermediate/firebase/pro/int_firebase__pro_experiment.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/firebase/pro/int_firebase__pro_experiment.sql
@@ -13,19 +13,23 @@ SELECT DISTINCT
     user_pseudo_id,
     user_id,
     (
-        SELECT value.int_value 
-        FROM UNNEST(user_properties) 
-        WHERE key = 'offerer_id'
+        SELECT value.int_value
+        FROM UNNEST(user_properties) AS up
+        WHERE up.key = 'offerer_id'
     ) AS offerer_id,
     event_params.key AS experiment_name,
     CAST(event_params.value.string_value AS BOOLEAN) AS experiment_value
 FROM 
-    {{ source("raw", "firebase_pro_events") }}
+    {{ source("raw", "firebase_pro_events") }},
+    UNNEST(event_params) AS event_params
 WHERE
-    (event_params.key = "PRO_DIDACTIC_ONBOARDING_AB_TEST" 
-     OR event_params.key LIKE "PRO_EXPERIMENT%")
+    (
+        event_params.key = "PRO_DIDACTIC_ONBOARDING_AB_TEST"
+        OR event_params.key LIKE "PRO_EXPERIMENT%"
+    )
     AND event_params.value.string_value IS NOT NULL
     {% if is_incremental() %}
-        AND event_date BETWEEN DATE_SUB(DATE("{{ ds() }}"), INTERVAL 1 DAY) AND DATE("{{ ds() }}")
+        AND event_date BETWEEN DATE_SUB(DATE("{{ ds() }}"), INTERVAL 1 DAY)
+        AND DATE("{{ ds() }}")
     {% endif %}
 

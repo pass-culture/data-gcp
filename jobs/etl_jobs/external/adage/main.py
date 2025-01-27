0f71d5509d9a0a37c33952076ee3b27f9e818087
cibrahim-pass <188350754+cibrahim-pass@users.noreply.github.com>
2025-01-27 15:57:46 +0100

    refactored main of import adage for robustness and clarity

--- jobs/etl_jobs/external/adage/main.py
+++ jobs/etl_jobs/external/adage/main.py
@@ -1,3 +1,5 @@
+import logging
+
 from scripts.import_adage import (
     get_adage_stats,
     import_adage,
@@ -5,11 +7,15 @@ from scripts.import_adage import (
 
 
 def run():
-    import_adage()
+    try:
+        import_adage()
+        get_adage_stats()
+        return "Success"
+    except Exception:
+        return "Failed"
 
-    get_adage_stats()
 
-    return "Success"
-
-
-run()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    result = run()
+    logging.info(f"Import adage task {result}")

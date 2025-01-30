be75c5cf51fcc1ab6f07de2f9dfa87f242e64e38
Julpass <jules.arbelot@passculture.app>
2025-01-30 14:52:03 +0100

    fix linter

--- orchestration/dags/data_gcp_dbt/models/intermediate/firebase/pro/int_firebase__pro_experiment.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/firebase/pro/int_firebase__pro_experiment.sql
@@ -12,26 +12,25 @@ SELECT DISTINCT
     event_date,
     user_pseudo_id,
     user_id,
+    event_params.key AS experiment_name,
     (
         SELECT value.int_value
         FROM UNNEST(user_properties) AS up
         WHERE up.key = 'offerer_id'
     ) AS offerer_id,
-    event_params.key AS experiment_name,
     SAFE_CAST(event_params.value.string_value AS BOOLEAN) AS experiment_value
-FROM 
+FROM
     {{ source("raw", "firebase_pro_events") }},
     UNNEST(event_params) AS event_params
 WHERE
     (
-        event_params.key = "PRO_DIDACTIC_ONBOARDING_AB_TEST"
-        OR event_params.key LIKE "PRO_EXPERIMENT%"
+        event_params.key = 'PRO_DIDACTIC_ONBOARDING_AB_TEST'
+        OR event_params.key LIKE 'PRO_EXPERIMENT%'
     )
-    AND event_params.value.string_value IS NOT NULL
+    AND event_params.value.string_value IS NOT null
     {% if is_incremental() %}
         AND event_date BETWEEN DATE_SUB(DATE("{{ ds() }}"), INTERVAL 1 DAY)
         AND DATE("{{ ds() }}")
-    {% else %} 
+    {% else %}
         AND event_date >= DATE_SUB(DATE("{{ ds() }}"), INTERVAL 6 MONTH)
     {% endif %}
-

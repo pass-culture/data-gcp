00a64b1b37fc6af9319ef3f312d47ba0849197b1
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-02-03 17:10:13 +0100

    (DE-1077) feat(seed): add & update qpv_population seed (#3771)

--- jobs/etl_jobs/internal/gcs_seed/tables_config.py
+++ jobs/etl_jobs/internal/gcs_seed/tables_config.py
@@ -213,6 +213,9 @@ REF_TABLES = {
     "2024_insee_qpv": {
         "file_type": "avro",
     },
+    "2024_insee_qpv_population": {
+        "file_type": "parquet",
+    },
     "qpv_eligible_pop": {
         "file_type": "avro",
     },

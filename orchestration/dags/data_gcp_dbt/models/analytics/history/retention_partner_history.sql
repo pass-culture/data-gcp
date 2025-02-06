5228202961f7edec348f98a47a24257c62f06879
Mathilde Vey <133665016+mvey-pass@users.noreply.github.com>
2025-02-06 15:07:28 +0100

    fix linter

--- orchestration/dags/data_gcp_dbt/models/analytics/history/retention_partner_history.sql
+++ orchestration/dags/data_gcp_dbt/models/analytics/history/retention_partner_history.sql
@@ -18,20 +18,20 @@ with
     ),
 
     scheduled_offers as (
-        select 
+        select
             partner_id,
-            date_add(date('2024-04-30'), interval offset day) as day,  
+            date_add(date('2024-04-30'), interval offset day) as day,
             coalesce(count(distinct offer_id), 0) as individual_scheduled_offers
-    from {{ ref("int_global__offer") }}
+        from {{ ref("int_global__offer") }}
         cross join
             unnest(generate_array(0, date_diff(current_date(), '2024-04-30', day))) as
         offset
         where
-            offer_publication_date is not null -- Seules les offres programmées dans le futur ont ce champs comblé
+            offer_publication_date is not null  -- Seules les offres programmées dans le futur ont ce champs comblé
+            and date_add(date('2024-04-30'), interval offset day) >= offer_creation_date  -- Les jours depuis la création de l'offre
             and date_add(date('2024-04-30'), interval offset day)
-            >= offer_creation_date  -- Les jours depuis la création de l'offre
-            and date_add(date('2024-04-30'), interval offset day) <= offer_publication_date  -- Les jours avant la publication de l'offre
-        group by 1,2
+            <= offer_publication_date  -- Les jours avant la publication de l'offre
+        group by 1, 2
     ),
 
     all_days_and_bookability as (
@@ -53,7 +53,7 @@ with
             = all_activated_partners_and_days_since_activation.partner_id
             and bookable_partner_history.partition_date
             = all_activated_partners_and_days_since_activation.day
-        left join 
+        left join
             scheduled_offers
             on scheduled_offers.partner_id
             = all_activated_partners_and_days_since_activation.partner_id
@@ -75,12 +75,21 @@ select
         order by day
         rows between unbounded preceding and current row
     ) as last_bookable_date,
-    max(case when total_bookable_offers != 0 or individual_scheduled_offers != 0 then day end) over (
+    max(
+        case
+            when total_bookable_offers != 0 or individual_scheduled_offers != 0 then day
+        end
+    ) over (
         partition by partner_id
         order by day
         rows between unbounded preceding and current row
     ) as last_retention_date,
-    max(case when individual_bookable_offers != 0 or individual_scheduled_offers != 0 then day end) over (
+    max(
+        case
+            when individual_bookable_offers != 0 or individual_scheduled_offers != 0
+            then day
+        end
+    ) over (
         partition by partner_id
         order by day
         rows between unbounded preceding and current row
@@ -101,7 +110,12 @@ select
     ) as days_since_last_bookable_date,
     date_diff(
         day,
-        max(case when total_bookable_offers != 0 or individual_scheduled_offers != 0 then day end) over (
+        max(
+            case
+                when total_bookable_offers != 0 or individual_scheduled_offers != 0
+                then day
+            end
+        ) over (
             partition by partner_id
             order by day
             rows between unbounded preceding and current row
@@ -110,7 +124,12 @@ select
     ) as days_since_last_retention_date,
     date_diff(
         day,
-        max(case when individual_bookable_offers != 0 or individual_scheduled_offers != 0 then day end) over (
+        max(
+            case
+                when individual_bookable_offers != 0 or individual_scheduled_offers != 0
+                then day
+            end
+        ) over (
             partition by partner_id
             order by day
             rows between unbounded preceding and current row
@@ -126,4 +145,4 @@ select
         ),
         day
     ) as days_since_last_collective_retention_date
-from all_days_and_bookability 
+from all_days_and_bookability

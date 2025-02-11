cb12d74f1e024553c5d2501cb22ff652cfc720ab
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-02-11 16:02:37 +0100

    tests(dbt): critical data quality test

--- orchestration/dags/data_gcp_dbt/tests/generic/is_valid_booking_intermediary_amout.sql
+++ orchestration/dags/data_gcp_dbt/tests/generic/is_valid_booking_intermediary_amout.sql
@@ -1,24 +1,17 @@
-{% test is_valid_booking_intermediary_amout(model, column_name,max_amout = 300) %}
+{% test is_valid_booking_intermediary_amout(model, column_name, max_amout=300) %}
 
-with validation as (
+    with
+        validation as (select {{ column_name }} as amount_field from {{ model }}),
 
-    select
-        {{ column_name }} as amount_field
+        errors as (
 
-    from {{ model }}
+            select amount_field
+            from validation
+            where amount_field is null or amount_field > {{ max_amout }}
 
-),
+        )
 
-errors as (
-
-    select
-        amount_field
-    from validation
-    where amount_field is Null or amount_field > {{ max_amout }}
-
-)
-
-select *
-from errors
+    select *
+    from errors
 
 {% endtest %}

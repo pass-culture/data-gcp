bda9df25b7d15ce73ccb9f97f1fe15a154093438
Mathilde Vey <133665016+mvey-pass@users.noreply.github.com>
2025-02-12 14:33:58 +0100

    update timespan

--- orchestration/dags/dependencies/applicative_database/sql/raw/parallel/headline_offer.sql
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/headline_offer.sql
@@ -2,6 +2,6 @@ SELECT
      CAST("id" AS varchar(255)) as headline_offer_id
     , CAST("offerId" AS varchar(255)) as offer_id
     , CAST("venueId" AS varchar(255)) as venue_id
-    , CAST(TRIM(BOTH \'[") \' FROM SPLIT_PART("timespan" :: text, \',\',1)) AS timestamp) AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS headline_beginning_time
-    , CAST(NULLIF(TRIM(BOTH \'[") \' FROM SPLIT_PART("timespan" :: text, \',\',2)),\'\') AS timestamp) AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS headline_ending_time
+    , TIMESTAMP(REGEXP_REPLACE(SPLIT(timespan, ',')[SAFE_OFFSET(0)], r'[\[\]"]', '')) AS headline_beginning_time
+    , TIMESTAMP(NULLIF(REGEXP_REPLACE(SPLIT(timespan, ',')[SAFE_OFFSET(1)], r'[\[\]"]', ''), '')) AS headline_ending_time
 FROM public.headline_offer

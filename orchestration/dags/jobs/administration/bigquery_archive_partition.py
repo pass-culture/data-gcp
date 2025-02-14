fac52d514089808bd37e50649b6c3451d9e70200
Lucile <lucile@MacBook-Air-de-Lucile.local>
2025-02-14 10:20:50 +0100

    fix bigquery dag json

--- orchestration/dags/jobs/administration/bigquery_archive_partition.py
+++ orchestration/dags/jobs/administration/bigquery_archive_partition.py
@@ -88,7 +88,7 @@ for table, config in TABLES.items():
         instance_name="{{ params.instance_name }}",
         base_dir=BASE_PATH,
         environment=dag_config,
-        command=f'python main.py --table {table} --config "{config}" ',
+        command=f"python main.py --table {table} --config '{config}' ",
         do_xcom_push=True,
     )
     tasks.append(export_old_partitions_to_gcs)

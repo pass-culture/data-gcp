7578d8b9c21705c8eafe5fdd6db36b79655d3857
Mathilde Vey <133665016+mvey-pass@users.noreply.github.com>
2025-02-18 10:58:20 +0100

    fix

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__headline_offer.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__headline_offer.sql
@@ -4,7 +4,7 @@ select
     venue_id,
     safe.parse_timestamp(
         '%Y-%m-%d %H:%M:%E6S', regexp_extract(timespan, r'"([^"]+)"')
-    ) as headline_beginning_time,,
+    ) as headline_beginning_time,
     safe.parse_timestamp(
         '%Y-%m-%d %H:%M:%E6S', regexp_extract(timespan, r',"([^"]+)"')
     ) as headline_ending_time

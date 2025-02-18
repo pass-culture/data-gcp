db4a90116b77e98f1e5dd9421cc5c8b00224a8d7
Mathilde Vey <133665016+mvey-pass@users.noreply.github.com>
2025-02-18 10:54:43 +0100

    fix

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__headline_offer.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__headline_offer.sql
@@ -1,7 +1,11 @@
-SELECT 
-    id as headline_offer_id
-    , offer_id 
-    , venue_id 
-    , CAST(TRIM(BOTH \'[") \' FROM SPLIT_PART("timespan" :: text, \',\',1)) AS timestamp) AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS headline_beginning_time
-    , CAST(NULLIF(TRIM(BOTH \'[") \' FROM SPLIT_PART("timespan" :: text, \',\',2)),\'\') AS timestamp) AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS headline_ending_time
-FROM {{ source("raw", "applicative_database_headline_offer") }}
+select
+    id as headline_offer_id,
+    offer_id,
+    venue_id,
+    safe.parse_timestamp(
+        '%Y-%m-%d %H:%M:%E6S', regexp_extract(timespan, r'"([^"]+)"')
+    ) as headline_beginning_time,,
+    safe.parse_timestamp(
+        '%Y-%m-%d %H:%M:%E6S', regexp_extract(timespan, r',"([^"]+)"')
+    ) as headline_ending_time
+from {{ source("raw", "applicative_database_headline_offer") }}

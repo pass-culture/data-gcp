c166bf8c1c5e9ac2a89c8a90ae474c8e75e91b7e
Timothée Scheurer <timothee.scheurer@passculture.app>
2025-02-19 15:00:49 +0100

    add achievement table

---
+++ orchestration/dags/data_gcp_dbt/models/raw/applicative/raw_applicative__achievement.sql
@@ -0,0 +1,16 @@
+select
+    *
+from
+    external_query(
+        "{{ env_var('APPLICATIVE_EXTERNAL_CONNECTION_ID') }}",
+        """
+SELECT
+    CAST("id" AS varchar(255)) AS id,
+    CAST("userId" AS varchar(255)) AS user_id,
+    CAST("bookingId" AS varchar(255)) AS booking_id,
+    CAST("name" AS varchar(255)) AS name,
+    "unlockedDate" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS unlocked_date,
+    "seenDate" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS seen_date
+    FROM public.achievement
+"""
+    )

5d49ed0ba9f732514dbfcded33b7db6929f6f8b2
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-02-20 19:27:02 +0100

    feat: keep name instead of id

--- orchestration/dags/data_gcp_dbt/models/export/ministry/exp_ministry__offer.sql
+++ orchestration/dags/data_gcp_dbt/models/export/ministry/exp_ministry__offer.sql
@@ -1,3 +1,8 @@
-select offer_id, offer_name, offer_category_id, offer_created_at, is_active
+select
+    offer_id,
+    offer_name,
+    offer_category_id as offer_category_name,
+    offer_created_at,
+    is_active
 from {{ ref("mrt_global__offer") }}
 where is_active

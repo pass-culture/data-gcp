de5343c41b811affd4771904cb0f052804531a07
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-02-20 17:24:32 +0100

    wip

--- orchestration/dags/data_gcp_dbt/models/export/ministry/exp_ministry__offer.sql
+++ orchestration/dags/data_gcp_dbt/models/export/ministry/exp_ministry__offer.sql
@@ -1,8 +1,3 @@
-select
-    offer_id,
-    offer_name,
-    offer_category_id,
-    offer_created_at,
-    is_active
+select offer_id, offer_name, offer_category_id, offer_created_at, is_active
 from {{ ref("mrt_global__offer") }}
 where is_active

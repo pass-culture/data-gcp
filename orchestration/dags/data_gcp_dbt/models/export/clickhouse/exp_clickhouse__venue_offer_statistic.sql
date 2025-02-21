b20055bd7c72fd5ef14ca129a707cfd1799545ee
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-02-21 14:28:51 +0100

    feat: add venue offer stat queries

--- orchestration/dags/data_gcp_dbt/models/export/clickhouse/exp_clickhouse__venue_offer_statistic.sql
+++ orchestration/dags/data_gcp_dbt/models/export/clickhouse/exp_clickhouse__venue_offer_statistic.sql
@@ -1,34 +1,82 @@
-WITH collective_offer_status AS (
-    SELECT
-        venue_id,
-        COUNT(DISTINCT CASE WHEN collective_offer_is_active = true AND collective_offer_validation = 'APPROVED' THEN collective_offer_id END) AS total_active_collective_offers,
-        COUNT(DISTINCT CASE WHEN collective_offer_is_active = true AND collective_offer_validation = 'PENDING' THEN collective_offer_id END) AS total_pending_collective_offers,
-        COUNT(DISTINCT CASE WHEN collective_offer_is_active = false AND collective_offer_validation != 'REJECTED' THEN collective_offer_id END) AS total_inactive_non_rejected_collective_offers
-    FROM {{ ref('int_applicative__collective_offer') }}
-    GROUP BY venue_id
-),
+with
+    collective_offer_status as (
+        select
+            venue_id,
+            count(
+                distinct case
+                    when
+                        collective_offer_is_active = true
+                        and collective_offer_validation = 'APPROVED'
+                    then collective_offer_id
+                end
+            ) as total_active_collective_offers,
+            count(
+                distinct case
+                    when
+                        collective_offer_is_active = true
+                        and collective_offer_validation = 'PENDING'
+                    then collective_offer_id
+                end
+            ) as total_pending_collective_offers,
+            count(
+                distinct case
+                    when
+                        collective_offer_is_active = false
+                        and collective_offer_validation != 'REJECTED'
+                    then collective_offer_id
+                end
+            ) as total_inactive_non_rejected_collective_offers
+        from {{ ref("int_applicative__collective_offer") }}
+        group by venue_id
+    ),
 
-individual_offer_status AS (
-    SELECT
-        o.venue_id,
-        COUNT(DISTINCT CASE WHEN o.is_active = true AND o.offer_validation = 'APPROVED' AND (s.booking_limit_datetime IS null OR s.booking_limit_datetime > CURRENT_TIMESTAMP()) THEN o.offer_id END) AS total_active_offers,
-        COUNT(DISTINCT CASE WHEN o.is_active = true AND o.offer_validation = 'PENDING' THEN o.offer_id END) AS total_pending_offers,
-        COUNT(DISTINCT CASE WHEN o.is_active = false AND o.offer_validation != 'REJECTED' THEN o.offer_id END) AS total_inactive_non_rejected_offers
-    FROM {{ ref('int_applicative__offer') }} AS o
-    LEFT JOIN {{ ref('int_applicative__stock') }} AS s
-        ON o.offer_id = s.offer_id
-        AND s.is_soft_deleted = false
-    GROUP BY o.venue_id
-)
+    individual_offer_status as (
+        select
+            o.venue_id,
+            count(
+                distinct case
+                    when
+                        o.is_active = true
+                        and o.offer_validation = 'APPROVED'
+                        and (
+                            s.booking_limit_datetime is null
+                            or s.booking_limit_datetime > current_timestamp()
+                        )
+                    then o.offer_id
+                end
+            ) as total_active_offers,
+            count(
+                distinct case
+                    when o.is_active = true and o.offer_validation = 'PENDING'
+                    then o.offer_id
+                end
+            ) as total_pending_offers,
+            count(
+                distinct case
+                    when o.is_active = false and o.offer_validation != 'REJECTED'
+                    then o.offer_id
+                end
+            ) as total_inactive_non_rejected_offers
+        from {{ ref("int_applicative__offer") }} as o
+        left join
+            {{ ref("int_applicative__stock") }} as s
+            on o.offer_id = s.offer_id
+            and s.is_soft_deleted = false
+        group by o.venue_id
+    )
 
-SELECT
+select
     v.venue_id,
-    COALESCE(ios.total_active_offers, 0) AS total_active_offers,
-    COALESCE(ios.total_pending_offers, 0) AS total_pending_offers,
-    COALESCE(ios.total_inactive_non_rejected_offers, 0) AS total_inactive_non_rejected_offers,
-    COALESCE(cos.total_active_collective_offers, 0) AS total_active_collective_offers,
-    COALESCE(cos.total_pending_collective_offers, 0) AS total_pending_collective_offers,
-    COALESCE(cos.total_inactive_non_rejected_collective_offers, 0) AS total_inactive_non_rejected_collective_offers
-FROM {{ ref('int_applicative__venue') }} AS v
-LEFT JOIN individual_offer_status AS ios ON v.venue_id = ios.venue_id
-LEFT JOIN collective_offer_status AS cos ON v.venue_id = cos.venue_id
+    coalesce(ios.total_active_offers, 0) as total_active_offers,
+    coalesce(ios.total_pending_offers, 0) as total_pending_offers,
+    coalesce(
+        ios.total_inactive_non_rejected_offers, 0
+    ) as total_inactive_non_rejected_offers,
+    coalesce(cos.total_active_collective_offers, 0) as total_active_collective_offers,
+    coalesce(cos.total_pending_collective_offers, 0) as total_pending_collective_offers,
+    coalesce(
+        cos.total_inactive_non_rejected_collective_offers, 0
+    ) as total_inactive_non_rejected_collective_offers
+from {{ ref("int_applicative__venue") }} as v
+left join individual_offer_status as ios on v.venue_id = ios.venue_id
+left join collective_offer_status as cos on v.venue_id = cos.venue_id

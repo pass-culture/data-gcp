36c82966b1b09be70205334bfa4ec5651c8f6257
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-02-21 17:35:41 +0100

    (DE-1225) feat(etl_jobs): add new address job

--- orchestration/dags/data_gcp_dbt/models/intermediate/api_gouv/address/int_api_gouv__user_address_candidate_queue.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/api_gouv/address/int_api_gouv__user_address_candidate_queue.sql
@@ -1,55 +1,49 @@
-WITH last_status_update AS (
-    SELECT
-        user_id,
-        MAX(datecreated) AS last_modified_at
-    FROM {{ source("raw", "applicative_database_beneficiary_fraud_check") }}
-    WHERE
-        type = 'PROFILE_COMPLETION'
-        AND status = 'OK'
-    GROUP BY user_id
-),
+with
+    last_status_update as (
+        select user_id, max(datecreated) as last_modified_at
+        from {{ source("raw", "applicative_database_beneficiary_fraud_check") }}
+        where type = 'PROFILE_COMPLETION' and status = 'OK'
+        group by user_id
+    ),
 
-user_location_update AS (
-    SELECT
-        user_id,
-        MAX(date_updated) AS last_calculation_at
-    FROM {{ source("raw", "user_locations") }}
-    GROUP BY user_id
-),
+    user_location_update as (
+        select user_id, max(date_updated) as last_calculation_at
+        from {{ source("raw", "user_locations") }}
+        group by user_id
+    ),
 
-user_candidates AS (
-    SELECT
-        adu.user_id,
-        adu.user_creation_date AS user_creation_at,
-        GREATEST(adu.user_creation_date, lsu.last_modified_at) AS user_last_modified_at,
-        TRIM(
-            CONCAT(
-                REPLACE(REPLACE(adu.user_address, '\\r', ''), '\\n', ''), ' ',
-                adu.user_postal_code, ' ',
-                adu.user_city
-            )
-        ) AS user_full_address
-    FROM {{ source("raw", "applicative_database_user") }} AS adu
-    LEFT JOIN last_status_update AS lsu
-        ON adu.user_id = lsu.user_id
-    WHERE
-        COALESCE(adu.user_address, '') <> ''
-        AND adu.user_postal_code IS NOT null
-        AND adu.user_city IS NOT null
-)
+    user_candidates as (
+        select
+            adu.user_id,
+            adu.user_creation_date as user_creation_at,
+            greatest(
+                adu.user_creation_date, lsu.last_modified_at
+            ) as user_last_modified_at,
+            trim(
+                concat(
+                    replace(replace(adu.user_address, '\\r', ''), '\\n', ''),
+                    ' ',
+                    adu.user_postal_code,
+                    ' ',
+                    adu.user_city
+                )
+            ) as user_full_address
+        from {{ source("raw", "applicative_database_user") }} as adu
+        left join last_status_update as lsu on adu.user_id = lsu.user_id
+        where
+            coalesce(adu.user_address, '') <> ''
+            and adu.user_postal_code is not null
+            and adu.user_city is not null
+    )
 
-
-SELECT
+select
     uc.user_id,
     uc.user_full_address,
     uc.user_creation_at,
-    ulu.last_calculation_at AS user_address_last_calculation_at
+    ulu.last_calculation_at as user_address_last_calculation_at
 
-FROM user_candidates AS uc
-LEFT JOIN user_location_update AS ulu ON uc.user_id = ulu.user_id
-WHERE
--- no location update or location update is older than last user update
-    (
-        ulu.user_id IS null
-        OR ulu.last_calculation_at < uc.user_last_modified_at
-    )
+from user_candidates as uc
+left join user_location_update as ulu on uc.user_id = ulu.user_id
+where
+    -- no location update or location update is older than last user update
+    (ulu.user_id is null or ulu.last_calculation_at < uc.user_last_modified_at)

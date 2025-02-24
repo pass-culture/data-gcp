f8593df0266d6ad24bb3c974e8fe8c09d171e50e
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-02-24 16:34:51 +0100

    (DE-1225) feat(etl_jobs): add new address job (#3843)

---
+++ jobs/etl_jobs/external/api_gouv/utils/__init__.py
@@ -0,0 +1,30 @@
+import os
+
+import pandas as pd
+from google.cloud import bigquery
+
+GCP_PROJECT = os.environ["GCP_PROJECT_ID"]
+
+
+def chunks(input_list: list, size: int):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(input_list), size):
+        yield input_list[i : i + size]
+
+
+def save(df: pd.DataFrame, dataset_id: str, table_name: str):
+    bigquery_client = bigquery.Client()
+    table_id = f"{GCP_PROJECT}.{dataset_id}.{table_name}"
+    job_config = bigquery.LoadJobConfig(
+        write_disposition="WRITE_APPEND",
+        schema_update_options=[
+            bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION,
+        ],
+    )
+    job = bigquery_client.load_table_from_dataframe(df, table_id, job_config=job_config)
+    job.result()
+
+
+def query(dataset_id: str, table_name: str, limit: int = 1000):
+    query_string = f"SELECT user_id, user_full_address FROM {dataset_id}.{table_name} ORDER BY user_creation_at DESC LIMIT {limit} "
+    return bigquery.Client().query(query_string).to_dataframe()
---
+++ orchestration/dags/data_gcp_dbt/models/intermediate/api_gouv/address/int_api_gouv__user_address_candidate_queue.sql
@@ -0,0 +1,49 @@
+with
+    last_status_update as (
+        select user_id, max(datecreated) as last_modified_at
+        from {{ source("raw", "applicative_database_beneficiary_fraud_check") }}
+        where type = 'PROFILE_COMPLETION' and status = 'OK'
+        group by user_id
+    ),
+
+    user_location_update as (
+        select user_id, datetime(max(updated_at)) as last_calculation_at
+        from {{ source("raw", "user_address") }}
+        group by user_id
+    ),
+
+    user_candidates as (
+        select
+            adu.user_id,
+            adu.user_creation_date as user_creation_at,
+            greatest(
+                adu.user_creation_date, lsu.last_modified_at
+            ) as user_last_modified_at,
+            trim(
+                concat(
+                    replace(replace(adu.user_address, '\\r', ''), '\\n', ''),
+                    ' ',
+                    adu.user_postal_code,
+                    ' ',
+                    adu.user_city
+                )
+            ) as user_full_address
+        from {{ source("raw", "applicative_database_user") }} as adu
+        left join last_status_update as lsu on adu.user_id = lsu.user_id
+        where
+            coalesce(adu.user_address, '') <> ''
+            and adu.user_postal_code is not null
+            and adu.user_city is not null
+    )
+
+select
+    uc.user_id,
+    uc.user_full_address,
+    uc.user_creation_at,
+    ulu.last_calculation_at as user_address_last_calculation_at
+
+from user_candidates as uc
+left join user_location_update as ulu on uc.user_id = ulu.user_id
+where
+    -- no location update or location update is older than last user update
+    (ulu.user_id is null or ulu.last_calculation_at < uc.user_last_modified_at)

ba141e47afe639de60740fcfbfff7df71acb6b34
Timothée Scheurer <timothee.scheurer@passculture.app>
2025-02-26 08:58:59 +0100

    add recredit fields to int_global models

--- orchestration/dags/data_gcp_dbt/models/intermediate/global/int_global__user.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/global/int_global__user.sql
@@ -101,17 +101,24 @@ select
     dgu.last_recredit_date,
     dgu.total_recredit,
     dgu.total_recredit_amount,
-    coalesce(u.user_activity = "Chômeur, En recherche d'emploi", false) as user_is_unemployed,
-    coalesce(u.user_activity
-            in ("Collégien", "Etudiant", "Lycéen", "Apprenti", "Alternant"), false) as user_is_in_education,
-    coalesce((
-                (ui.qpv_name is not null)
-                or (
-                    u.user_activity
-                    not in ("Collégien", "Etudiant", "Lycéen", "Apprenti", "Alternant")
-                )
-                or (ui.user_macro_density_label = "rural")
-            ), false) as user_is_priority_public,
+    coalesce(
+        u.user_activity = "Chômeur, En recherche d'emploi", false
+    ) as user_is_unemployed,
+    coalesce(
+        u.user_activity in ("Collégien", "Etudiant", "Lycéen", "Apprenti", "Alternant"),
+        false
+    ) as user_is_in_education,
+    coalesce(
+        (
+            (ui.qpv_name is not null)
+            or (
+                u.user_activity
+                not in ("Collégien", "Etudiant", "Lycéen", "Apprenti", "Alternant")
+            )
+            or (ui.user_macro_density_label = "rural")
+        ),
+        false
+    ) as user_is_priority_public,
     coalesce(
         dgu.total_non_cancelled_individual_bookings, 0
     ) as total_non_cancelled_individual_bookings,
@@ -133,12 +140,15 @@ select
     date_diff(
         dgu.first_individual_booking_date, dgu.first_deposit_creation_date, day
     ) as days_between_activation_date_and_first_booking_paid,
-    coalesce((
-                timestamp(dgu.last_deposit_expiration_date) >= current_timestamp()
-                and coalesce(dgu.total_deposit_actual_amount_spent, 0)
-                < dgu.last_deposit_amount
-            )
-            and u.user_is_active, false) as user_is_current_beneficiary
+    coalesce(
+        (
+            timestamp(dgu.last_deposit_expiration_date) >= current_timestamp()
+            and coalesce(dgu.total_deposit_actual_amount_spent, 0)
+            < dgu.last_deposit_amount
+        )
+        and u.user_is_active,
+        false
+    ) as user_is_current_beneficiary
 from {{ ref("int_applicative__user") }} as u
 left join
     {{ ref("int_applicative__action_history") }} as ah

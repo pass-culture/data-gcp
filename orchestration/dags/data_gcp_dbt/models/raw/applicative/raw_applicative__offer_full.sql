0ee4b9abde0a09baf9535e0edafcc0c2a4a4564b
Lucile <lucile@MacBook-Air-de-Lucile.local>
2025-02-27 12:13:32 +0100

    add monthly condition

--- orchestration/dags/data_gcp_dbt/models/raw/applicative/raw_applicative__offer_full.sql
+++ orchestration/dags/data_gcp_dbt/models/raw/applicative/raw_applicative__offer_full.sql
@@ -1,5 +1,13 @@
-select
-    * except (offer_updated_date, offer_fields_updated),
-    timestamp(offer_updated_date) as offer_updated_date,
-    to_hex(md5(to_json_string(offer))) as custom_scd_id
-from {{ source("raw", "applicative_database_offer_legacy") }} as offer
+{% if is_first_day_of_month() == "TRUE" %}
+
+    select
+        * except (offer_updated_date),
+        timestamp(offer_updated_date) as offer_updated_date,
+        to_hex(md5(to_json_string(offer))) as custom_scd_id
+    from {{ source("raw", "applicative_database_offer_legacy") }} as offer
+
+{% else %}
+
+select 1 as dummy_column
+
+{% endif %}

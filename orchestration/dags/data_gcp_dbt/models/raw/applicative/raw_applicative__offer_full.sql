8fb055d348b15ecaf74f7b3109d825637c81a1ca
Lucile <lucile@MacBook-Air-de-Lucile.local>
2025-02-27 12:05:43 +0100

    (DE-1249) feat(snapshot): add custom strategy snapshot offer

---
+++ orchestration/dags/data_gcp_dbt/models/raw/applicative/raw_applicative__offer_full.sql
@@ -0,0 +1,6 @@
+select
+    * except (offer_updated_date, offer_fields_updated),
+    timestamp(offer_updated_date) as offer_updated_date,
+    to_hex(md5(to_json_string(offer))) as custom_scd_id
+from
+    {{ source("raw", "applicative_database_offer_legacy") }} as offer
--- orchestration/dags/data_gcp_dbt/snapshots/raw_applicative/snapshot_raw__offer.sql
+++ orchestration/dags/data_gcp_dbt/snapshots/raw_applicative/snapshot_raw__offer.sql
@@ -1,17 +1,36 @@
 {% snapshot snapshot_raw__offer %}
 
+{% if is_first_day_of_month() == "TRUE" %}
+
     {{
         config(
             **custom_snapshot_config(
-                strategy="timestamp",
                 unique_key="offer_id",
-                updated_at="offer_updated_date",
-                invalidate_hard_delete=False,
+                strategy="check",
+                check_cols=["custom_scd_id"],
             )
         )
     }}
 
     select *
-    from {{ ref("raw_applicative__offer_lite") }}
+    from {{ ref("raw_applicative__offer_full") }}
+
+{% else %}
+
+        {{
+            config(
+                **custom_snapshot_config(
+                    strategy="timestamp",
+                    unique_key="offer_id",
+                    updated_at="offer_updated_date",
+                    invalidate_hard_delete=False,
+                )
+            )
+        }}
+
+        select *
+        from {{ ref("raw_applicative__offer_lite") }}
+
+{% endif %}
 
 {% endsnapshot %}

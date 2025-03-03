ff03d6197226137409415b2edb9c326320a209bc
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-03-03 15:03:09 +0100

    lint: lint sql

--- orchestration/dags/data_gcp_dbt/models/intermediate/firebase/native/int_firebase__native_home_offer_displayed.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/firebase/native/int_firebase__native_home_offer_displayed.sql
@@ -9,23 +9,22 @@
     )
 }}
 
-
 select
     native_event.event_date,
     offer_id_split as offer_id,
-    module_id,
-    entry_id,
-    user_id,
-    unique_session_id,
-    position + 1 as displayed_position
+    native_event.module_id,
+    native_event.entry_id,
+    native_event.user_id,
+    native_event.unique_session_id,
+    position + 1 as displayed_position  -- noqa: RF01
 from
     {{ ref("int_firebase__native_event") }} as native_event,
-    unnest(displayed_offers) as offer_id_split
+    unnest(native_event.displayed_offers) as offer_id_split
 with
 offset as position
 where
     native_event.event_name = "ModuleDisplayedOnHomePage"
     {% if is_incremental() %}
-        and date(event_date) = date_sub('{{ ds() }}', interval 3 day)
-    {% else %} and date(event_date) >= "2024-06-13"
+        and date(native_event.event_date) = date_sub("{{ ds() }}", interval 3 day)
+    {% else %} and date(native_event.event_date) >= "2024-06-13"
     {% endif %}

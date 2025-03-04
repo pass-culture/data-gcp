81305f37c1a327a7518bb15a1d08ef1556b667e4
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-03-04 15:35:57 +0100

    WIP: Add ml_ranking__training_data.sql

---
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/ranking/ml_ranking__training_data.sql
@@ -0,0 +1,30 @@
+select
+    offer_displayed.event_date,
+    offer_displayed.user_id,
+    offer_displayed.unique_session_id,
+    offer_displayed.module_id,
+    home_module.module_name,
+    home_module.module_type,
+    offer_displayed.offer_id,
+    offer_displayed.displayed_position,
+    home_module.offer_id as offer_id_clicked,
+    home_module.booking_id,
+    home_module.click_type,
+    home_module.reco_call_id,
+    home_module.user_location_type,
+    home_module.module_clicked_timestamp,
+    home_module.consult_offer_timestamp
+from
+    {{ ref("int_firebase__native_home_offer_displayed") }} as offer_displayed
+left join
+    {{ ref("int_firebase__native_daily_user_home_module") }} as home_module
+    on offer_displayed.module_id = home_module.module_id
+    and offer_displayed.user_id = home_module.user_id
+    and offer_displayed.unique_session_id = home_module.unique_session_id
+where
+    offer_displayed.event_date between DATE_SUB(DATE("{{ ds() }}"), interval 3 day) and DATE("{{ ds() }}")
+    and home_module.module_displayed_date between DATE_SUB(DATE("{{ ds() }}"), interval 3 day) and DATE("{{ ds() }}")
+order by
+    offer_displayed.unique_session_id,
+    offer_displayed.module_id,
+    offer_displayed.displayed_position

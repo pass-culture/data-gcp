3aab4c9f32b883a9605a9b38bcf6fba8f212cccf
Lucille <78435671+lartigaud-pass@users.noreply.github.com>
2025-03-04 16:06:40 +0100

    fix sql

--- orchestration/dags/data_gcp_dbt/models/intermediate/pcapi/int_pcapi__log.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/pcapi/int_pcapi__log.sql
@@ -100,7 +100,7 @@ select
         jsonpayload.extra.filtervalues.departments, ','
     ) as department_filter,
     array_to_string(jsonpayload.extra.filtervalues.academies, ',') as academy_filter,
-    jsonPayload.extra.filtervalues.geolocradius as geoloc_radius_filter,
+    jsonpayload.extra.filtervalues.geolocradius as geoloc_radius_filter,
     array_to_string(
         array(
             select cast(value as string)

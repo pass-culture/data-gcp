1eb6a40adf2c9900ae93cf71c0a719c6010f24d4
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-03-05 10:38:44 +0100

    WIP

--- orchestration/dags/data_gcp_dbt/models/intermediate/firebase/native/int_firebase__native_home_offer_displayed.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/firebase/native/int_firebase__native_home_offer_displayed.sql
@@ -11,21 +11,22 @@
 
 select
     native_event.event_date,
-    offer_id_split as offer_id,
+    offer_id,
     native_event.module_id,
     native_event.entry_id,
     native_event.user_id,
     native_event.unique_session_id,
+    native_event.event_timestamp,
     position + 1 as displayed_position  -- noqa: RF01
 from
     {{ ref("int_firebase__native_event") }} as native_event,
-    unnest(native_event.displayed_offers) as offer_id_split
+    UNNEST(native_event.displayed_offers) as offer_id
 with
 offset as position
 where
     native_event.event_name = "ModuleDisplayedOnHomePage"
     {% if is_incremental() %}
-        and date(native_event.event_date)
-        between date_sub("{{ ds() }}", interval 3 day) and date("{{ ds() }}")
+        and DATE(native_event.event_date)
+        between DATE_SUB("{{ ds() }}", interval 3 day) and DATE("{{ ds() }}")
     {% else %} and date(native_event.event_date) >= "2024-06-13"
     {% endif %}
--- orchestration/dags/data_gcp_dbt/models/machine_learning/ranking/ml_ranking__training_data.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/ranking/ml_ranking__training_data.sql
@@ -1,31 +1,44 @@
+with deduplicated_home_offer_displayed as (
+    select distinct
+    event_date,
+    user_id,
+    unique_session_id,
+    module_id,
+    offer_id,
+    displayed_position,
+    TIMESTAMP_TRUNC(event_timestamp, minute) as event_timestamp_min
+from {{ ref("int_firebase__native_home_offer_displayed") }}
+)
+
 select
-    offer_displayed.event_date,
-    offer_displayed.user_id,
-    offer_displayed.unique_session_id,
-    offer_displayed.module_id,
+    deduplicated_home_offer_displayed.event_date,
+    deduplicated_home_offer_displayed.user_id,
+    deduplicated_home_offer_displayed.unique_session_id,
+    deduplicated_home_offer_displayed.module_id,
     home_module.module_name,
     home_module.module_type,
-    offer_displayed.offer_id,
-    offer_displayed.displayed_position,
+    deduplicated_home_offer_displayed.offer_id,
+    deduplicated_home_offer_displayed.displayed_position,
     home_module.offer_id as offer_id_clicked,
     home_module.booking_id,
     home_module.click_type,
     home_module.reco_call_id,
     home_module.user_location_type,
     home_module.module_clicked_timestamp,
-    home_module.consult_offer_timestamp
-from {{ ref("int_firebase__native_home_offer_displayed") }} as offer_displayed
+    home_module.consult_offer_timestamp,
+    deduplicated_home_offer_displayed.event_timestamp_min as module_displayed_timestamp
+from deduplicated_home_offer_displayed
 left join
     {{ ref("int_firebase__native_daily_user_home_module") }} as home_module
-    on offer_displayed.module_id = home_module.module_id
-    and offer_displayed.user_id = home_module.user_id
-    and offer_displayed.unique_session_id = home_module.unique_session_id
+    on deduplicated_home_offer_displayed.module_id = home_module.module_id
+    and deduplicated_home_offer_displayed.user_id = home_module.user_id
+    and deduplicated_home_offer_displayed.unique_session_id = home_module.unique_session_id
 where
-    offer_displayed.event_date
-    between date_sub(date("{{ ds() }}"), interval 3 day) and date("{{ ds() }}")
+    deduplicated_home_offer_displayed.event_date
+    between DATE_SUB(DATE("{{ ds() }}"), interval 3 day) and DATE("{{ ds() }}")
     and home_module.module_displayed_date
-    between date_sub(date("{{ ds() }}"), interval 3 day) and date("{{ ds() }}")
+    between DATE_SUB(DATE("{{ ds() }}"), interval 3 day) and DATE("{{ ds() }}")
 order by
-    offer_displayed.unique_session_id,
-    offer_displayed.module_id,
-    offer_displayed.displayed_position
+    deduplicated_home_offer_displayed.unique_session_id,
+    deduplicated_home_offer_displayed.module_id,
+    deduplicated_home_offer_displayed.displayed_position

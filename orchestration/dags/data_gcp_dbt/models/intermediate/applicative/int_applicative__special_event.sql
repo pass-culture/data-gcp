b5330d2b07405bfa3f0c3cd98e054398426a4af7
Julpass <jules.arbelot@passculture.app>
2025-03-05 15:16:38 +0100

    formatting

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__special_event.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__special_event.sql
@@ -8,7 +8,9 @@ select
     ser.special_event_response_status,
     date(se.special_event_creation_date) as special_event_creation_date,
     date(se.special_event_date) as special_event_date,
-    date(ser.special_event_response_submitted_date) as special_event_response_submitted_date
+    date(
+        ser.special_event_response_submitted_date
+    ) as special_event_response_submitted_date
 from {{ ref("raw_applicative__special_event_response") }} as ser
 left join
     {{ ref("raw_applicative__special_event") }} as se on ser.event_id = se.event_id

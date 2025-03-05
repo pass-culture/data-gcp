16d189e7eedac09b8ff8175c26cdd3644cdad702
lrainteau-pass <114910642+lrainteau-pass@users.noreply.github.com>
2025-03-05 13:48:22 +0100

    (DE-1283) fix(clickhouse): fix yearly expected revenue (#3852)
    * (DE-1283) fix(clickhouse): fix yearly expected revenue
    
    * add year aggregation for expected revenue
    
    ---------
    
    Co-authored-by: Lucile <lucile@MacBook-Air-de-Lucile.local>

--- jobs/etl_jobs/internal/export_clickhouse/schema/analytics/yearly_aggregated_venue_collective_revenue.sql
+++ jobs/etl_jobs/internal/export_clickhouse/schema/analytics/yearly_aggregated_venue_collective_revenue.sql
@@ -4,19 +4,48 @@ CREATE OR REPLACE TABLE analytics.yearly_aggregated_venue_collective_revenue ON 
     ORDER BY (venue_id)
     SETTINGS storage_policy='gcs_main'
 AS
+WITH effective_revenue as (
+    SELECT
+        toStartOfYear(coalesce(toDate(used_date), toDate(creation_date))) AS year,
+        cast(venue_id as String) as venue_id,
+        sum(
+            case
+                when collective_booking_status = 'USED'
+                or collective_booking_status = 'REIMBURSED' then booking_amount
+                else 0
+            end
+        ) AS collective_revenue,
+    FROM
+        intermediate.collective_booking
+    WHERE
+        venue_id IS NOT NULL
+    GROUP BY 1,2
+)
+, expected_revenue as (
+    SELECT
+        toStartOfYear(toDate(now())) AS year,
+        cast(venue_id as String) as venue_id,
+        sum(
+            case
+                when collective_booking_status = 'CONFIRMED'
+                then booking_amount
+                else 0
+            end
+        ) AS collective_expected_revenue
+    FROM
+        intermediate.collective_booking
+    WHERE
+        venue_id IS NOT NULL
+    GROUP BY 1, 2
+),
 SELECT
-    toStartOfYear(coalesce(toDate(used_date), toDate(creation_date))) AS year,
-    cast(venue_id as String) as venue_id,
-    sum(
-        case
-            when collective_booking_status = 'USED'
-            or collective_booking_status = 'REIMBURSED' then booking_amount
-            else 0
-        end
-    ) AS revenue,
-    sum(booking_amount) AS expected_revenue
-FROM
-    intermediate.collective_booking
-WHERE
-    venue_id IS NOT NULL
-GROUP BY 1,2
+    year,
+    venue_id,
+    0 as individual_revenue,
+    0 as individual_expected_revenue,
+    collective_revenue,
+    collective_revenue + collective_expected_revenue as collective_expected_revenue
+FROM effective_revenue
+LEFT JOIN expected_revenue
+ON effective_revenue.venue_id = expected_revenue.venue_id
+AND effective_revenue.year = expected_revenue.year
--- jobs/etl_jobs/internal/export_clickhouse/schema/analytics/yearly_aggregated_venue_individual_revenue.sql
+++ jobs/etl_jobs/internal/export_clickhouse/schema/analytics/yearly_aggregated_venue_individual_revenue.sql
@@ -4,7 +4,8 @@ CREATE OR REPLACE TABLE analytics.yearly_aggregated_venue_individual_revenue ON 
     ORDER BY (venue_id)
     SETTINGS storage_policy='gcs_main'
 AS
-SELECT
+WITH effective_revenue as (
+    SELECT
     toStartOfYear(coalesce(toDate(used_date), toDate(creation_date))) AS year,
     cast(venue_id as String) as venue_id,
     sum(
@@ -13,10 +14,40 @@ SELECT
             or booking_status = 'REIMBURSED' then booking_amount*booking_quantity
             else 0
         end
-    ) AS revenue,
-    sum(booking_amount*booking_quantity) AS expected_revenue
-FROM
-    intermediate.booking
-WHERE
-    venue_id IS NOT NULL
-GROUP BY 1,2
+    ) AS individual_revenue
+    FROM
+        intermediate.booking
+    WHERE
+        venue_id IS NOT NULL
+    GROUP BY 1,2
+)
+
+, expected_revenue as (
+    SELECT
+        toStartOfYear(toDate(now())) AS year,
+        cast(venue_id as String) as venue_id,
+        sum(
+            case
+                when booking_status = 'CONFIRMED'
+                then booking_amount*booking_quantity
+                else 0
+            end
+        ) AS individual_expected_revenue
+    FROM
+        intermediate.booking
+    WHERE
+        venue_id IS NOT NULL
+    GROUP BY 1, 2
+)
+
+SELECT
+    year,
+    venue_id,
+    individual_revenue,
+    individual_revenue + individual_expected_revenue AS individual_expected_revenue,
+    0 as collective_revenue,
+    0 as collective_expected_revenue
+FROM effective_revenue
+LEFT JOIN expected_revenue
+ON effective_revenue.venue_id = expected_revenue.venue_id
+AND effective_revenue.year = expected_revenue.year

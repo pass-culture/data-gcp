a01a02fe9f8588b319c5c39098b30544cc2fd3a7
lricard-pass <191850846+lricard-pass@users.noreply.github.com>
2025-03-11 11:53:35 +0100

    feat(dbt): Enhance analytics installation process
    - Add gcloud authentication step
    - Update gcloud components
    - Include additional setup instructions for DBT environment
    - Migrate dbt model int_firebase__native_booking

---
+++ orchestration/dags/data_gcp_dbt/models/intermediate/firebase/native/int_firebase__native_booking.sql
@@ -0,0 +1,33 @@
+{{
+    config(
+        **custom_incremental_config(
+            incremental_strategy="insert_overwrite",
+            partition_by={"field": "booking_date", "data_type": "date"},
+            on_schema_change="sync_all_columns",
+            require_partition_filter=true,
+        )
+    )
+}}
+
+select
+    user_id,
+    user_pseudo_id,
+    session_id,
+    unique_session_id,
+    offer_id,
+    booking_id,
+    event_date as booking_date,
+    event_timestamp as booking_timestamp,
+    platform,
+    user_location_type,
+    app_version
+from {{ ref("int_firebase__native_event") }}
+where
+    event_name = 'BookingConfirmation'
+    {% if is_incremental() %}
+        and date(event_date) >= date_sub('{{ ds() }}', interval 3 day)
+    {% else %}
+        and date(event_date)
+        >= date_sub('{{ ds() }}', interval {{ var('full_refresh_lookback') }})
+    {% endif %}
+qualify row_number() over (partition by booking_id order by event_timestamp) = 1

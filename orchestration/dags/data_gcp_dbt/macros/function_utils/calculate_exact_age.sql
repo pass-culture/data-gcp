8178676c958be021b78728911755adca86600109
Timothée Scheurer <timothee.scheurer@passculture.app>
2025-03-11 16:24:38 +0100

    fix typo review

--- orchestration/dags/data_gcp_dbt/macros/function_utils/calculate_exact_age.sql
+++ orchestration/dags/data_gcp_dbt/macros/function_utils/calculate_exact_age.sql
@@ -1,4 +1,4 @@
-{% macro calculate_exact_age(reference_date, birth_date, r) %}
+{% macro calculate_exact_age(reference_date, birth_date) %}
     date_diff({{ reference_date }}, {{ birth_date }}, year) - if(
         extract(month from {{ birth_date }}) * 100 + extract(day from {{ birth_date }})
         > extract(month from {{ reference_date }}) * 100

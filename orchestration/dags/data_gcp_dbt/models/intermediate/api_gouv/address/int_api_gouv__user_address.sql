fb0c708634ed960a9bacb90eee738077db38cb5e
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-03-11 18:24:47 +0100

    (DE-1225) feat(dbt): update user-addresses new job

--- orchestration/dags/data_gcp_dbt/models/intermediate/api_gouv/address/int_api_gouv__user_address.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/api_gouv/address/int_api_gouv__user_address.sql
@@ -4,8 +4,8 @@
 select
     ul.user_id,
     ul.result_postcode as user_postal_code,
-    cast(ul.longitude as float) as user_longitude,
-    cast(ul.latitude as float) as user_latitude,
+    SAFE_CAST(ul.longitude as FLOAT64) as user_longitude,
+    SAFE_CAST(ul.latitude as FLOAT64) as user_latitude,
     ul.result_citycode as user_city_code,
     ul.result_city as user_city,
     ul.user_full_address as user_raw_address,

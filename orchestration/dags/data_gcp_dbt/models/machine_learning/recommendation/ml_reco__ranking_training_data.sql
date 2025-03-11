69c175f0b5a065a00cf7149eae4d86c3422474b8
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-03-11 16:53:22 +0100

    feat: Add join on int_global__offer

--- orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__ranking_training_data.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__ranking_training_data.sql
@@ -1,5 +1,5 @@
 with
-    home_interactions as (
+    home_displays as (
         select distinct
             offer_displayed.event_date,
             offer_displayed.user_id,
@@ -33,7 +33,44 @@ with
             between date_sub(date("{{ ds() }}"), interval 3 day) and date("{{ ds() }}")
             and home_module.module_displayed_date
             between date_sub(date("{{ ds() }}"), interval 3 day) and date("{{ ds() }}")
+    ),
+
+home_interactions as (
+        select
+            event_date,
+            user_id,
+            unique_session_id,
+            module_id,
+            module_name,
+            context,
+            offer_id,
+            displayed_position,
+            click_type,
+            reco_call_id,
+            user_location_type,
+            module_clicked_timestamp,
+            consult_offer_timestamp,
+            module_displayed_timestamp,
+            is_consulted,
+            is_added_to_favorite,
+            is_booked
+        from home_displays
+        where
+            is_consulted is not null
+            or is_added_to_favorite is not null
+            or is_booked is not null
+    ),
+
+offers as (
+        select
+            offer_id,
+            offer_category_id,
+            offer_subcategory_id,
+            max(offer_url is null) as is_geolocated,
+            date_diff(current_date(), offers.offer_creation_date, day) as offer_creation_delta_in_days
+        from {{ ref("int_global__offer") }}
     )
+
 
 select
     home_interactions.event_date,
@@ -53,6 +90,13 @@ select
     home_interactions.is_consulted as consult,
     home_interactions.is_added_to_favorite as favorite,
     home_interactions.is_booked as booking,
+    offers.offer_category_id,
+    offers.offer_subcategory_id,
+    offers.is_geolocated,
+    offers.offer_creation_delta_in_days,
     format_date("%A", home_interactions.event_date) as day_of_week,
     extract(hour from home_interactions.event_date) as hour_of_day
+
 from home_interactions
+left join offers
+    on home_interactions.offer_id = offers.offer_id

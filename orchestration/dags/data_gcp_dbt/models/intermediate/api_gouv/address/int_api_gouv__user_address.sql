017e3e39cf973362f7a2d0aa66cb2863357a56bf
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-03-11 18:12:04 +0100

    (DE-1225) feat(dbt): update user-addresses new job

--- orchestration/dags/data_gcp_dbt/models/intermediate/api_gouv/address/int_api_gouv__user_address.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/api_gouv/address/int_api_gouv__user_address.sql
@@ -10,19 +10,24 @@ select
     ul.result_city as user_city,
     ul.user_full_address as user_raw_address,
     ul.result_type as user_geo_position_quality,
-    COALESCE(
+    coalesce(
         case
-            when ul.result_postcode = '97150' then '978'
-            when LEFT(ul.result_postcode, 2) in ('97', '98') then LEFT(ul.result_postcode, 3)
-            when LEFT(ul.result_postcode, 3) in ('200', '201', '209', '205') then '2A'
-            when LEFT(ul.result_postcode, 3) in ('202', '206') then '2B'
-            else LEFT(ul.result_postcode, 2)
+            when ul.result_postcode = '97150'
+            then '978'
+            when left(ul.result_postcode, 2) in ('97', '98')
+            then left(ul.result_postcode, 3)
+            when left(ul.result_postcode, 3) in ('200', '201', '209', '205')
+            then '2A'
+            when left(ul.result_postcode, 3) in ('202', '206')
+            then '2B'
+            else left(ul.result_postcode, 2)
         end,
         adu.user_department_code
     ) as user_department_code,
-    TIMESTAMP(ul.updated_at) as updated_at,
-    DATE(ul.updated_at) as updated_date
-  from {{ source("raw", "user_address") }} as ul
-  inner join  {{ source("raw", "applicative_database_user") }}   as adu on ul.user_id = adu.user_id
-  where ul.result_status = 'ok'
-qualify ROW_NUMBER() over (partition by ul.user_id order by ul.updated_at desc) = 1
+    timestamp(ul.updated_at) as updated_at,
+    date(ul.updated_at) as updated_date
+from {{ source("raw", "user_address") }} as ul
+inner join
+    {{ source("raw", "applicative_database_user") }} as adu on ul.user_id = adu.user_id
+where ul.result_status = 'ok'
+qualify row_number() over (partition by ul.user_id order by ul.updated_at desc) = 1

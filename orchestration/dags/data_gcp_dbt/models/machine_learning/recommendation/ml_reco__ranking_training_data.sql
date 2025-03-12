1cd46c766991d561968b9a09997bf61310c1d62c
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-03-12 11:17:24 +0100

    feat: Add item embeddings

--- orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__ranking_training_data.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__ranking_training_data.sql
@@ -36,7 +36,7 @@ with
             between date_sub(date("{{ ds() }}"), interval 3 day) and date("{{ ds() }}")
     ),
 
-home_interactions as (
+    home_interactions as (
         select
             event_date,
             user_id,
@@ -60,22 +60,35 @@ home_interactions as (
             day_of_week,
             hour_of_day
         from home_displays
-        where
-            is_consulted is true
-            or is_added_to_favorite is true
-            or is_booked is true
+        where is_consulted is true or is_added_to_favorite is true or is_booked is true
     ),
 
-offers as (
-            select
-                offer_id,
-                offer_category_id,
-                offer_subcategory_id,
-                offer_url is null as is_geolocated,
-                date_diff(current_date(), offer_creation_date, day) as offer_created_delta_in_days
-            from {{ ref("int_global__offer") }}
-        )
+    offers as (
+        select
+            offer_id,
+            item_id,
+            offer_category_id,
+            offer_subcategory_id,
+            offer_url is null as is_geolocated,
+            date_diff(
+                current_date(), offer_creation_date, day
+            ) as offer_created_delta_in_days
+        from {{ ref("int_global__offer") }}
+    ),
 
+    item_embedding as (
+        select
+            item_id,
+            item_embedding
+        from {{ ref("ml_feat__two_tower_last_item_embedding") }}
+    ),
+
+    user_embedding as (
+        select
+            user_id,
+            user_embedding
+        from {{ ref("ml_feat__two_tower_last_user_embedding") }}
+    )
 
 select
     home_interactions.event_date,
@@ -97,11 +110,16 @@ select
     home_interactions.is_booked,
     home_interactions.day_of_week,
     home_interactions.hour_of_day,
+    offers.item_id,
     offers.offer_category_id,
     offers.offer_subcategory_id,
     offers.is_geolocated,
-    offers.offer_created_delta_in_days
+    offers.offer_created_delta_in_days,
+    item_embedding.item_embedding,
+    user_embedding.user_embedding
 
 from home_interactions
-left join offers
-    on home_interactions.offer_id = offers.offer_id
+left join offers on home_interactions.offer_id = offers.offer_id
+left join item_embedding on offers.item_id = item_embedding.item_id
+left join user_embedding on home_interactions.user_id = user_embedding.user_id
+order by event_date, user_id, unique_session_id, module_id, displayed_position

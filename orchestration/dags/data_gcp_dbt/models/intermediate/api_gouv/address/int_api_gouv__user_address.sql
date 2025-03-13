a9ea7114725406180b9d4d296dddff929ed374ec
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-03-13 14:16:58 +0100

    (DE-1225) feat(dbt): update address

--- orchestration/dags/data_gcp_dbt/models/intermediate/api_gouv/address/int_api_gouv__user_address.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/api_gouv/address/int_api_gouv__user_address.sql
@@ -1,33 +1,72 @@
 {{ config(**custom_table_config()) }}
 
 
+with user_postal_code_geocode as (
+    select
+        adu.user_id,
+        adu.user_creation_at,
+        adu.user_department_code,
+        safe_cast(pc.centroid_latitude as float64) as user_latitude,
+        safe_cast(pc.centroid_longitude as float64) as user_longitude,
+        pc.postal_code as user_postal_code,
+        if(pc.centroid_latitude is not null and pc.centroid_longitude is not null, "municipality", "unknown") as geocode_type
+    from {{ source("raw", "applicative_database_user") }} as adu
+    left join {{ source("seed", "2025_insee_postal_code") }} as pc on adu.user_postal_code = pc.postal_code
+    qualify row_number() over (partition by adu.user_id order by adu.user_creation_at desc) = 1
+),
+
+user_address_geocode as (
+    select
+        ul.user_id,
+        ul.result_postcode as user_postal_code,
+        safe_cast(ul.longitude as float64) as user_longitude,
+        safe_cast(ul.latitude as float64) as user_latitude,
+        ul.result_citycode as user_city_code,
+        ul.result_city as user_city,
+        ul.user_full_address as user_raw_address,
+        ul.result_type as geocode_type
+    from {{ source("raw", "user_address") }} as ul
+    where ul.result_status = 'ok'
+    qualify row_number() over (partition by ul.user_id order by ul.updated_at desc) = 1
+),
+
+
+address_combined as (
 select
-    ul.user_id,
-    ul.result_postcode as user_postal_code,
-    safe_cast(ul.longitude as float64) as user_longitude,
-    safe_cast(ul.latitude as float64) as user_latitude,
-    ul.result_citycode as user_city_code,
-    ul.result_city as user_city,
-    ul.user_full_address as user_raw_address,
-    ul.result_type as user_geo_position_quality,
+    upcg.user_id,
+    upcg.user_creation_at,
+    upcg.user_department_code,
+    coalesce(uag.geocode_type, upcg.geocode_type) as geocode_type,
+    coalesce(uag.user_postal_code, upcg.user_postal_code) as user_postal_code,
+    coalesce(uag.user_longitude, upcg.user_longitude) as user_longitude,
+    coalesce(uag.user_latitude, upcg.user_latitude) as user_latitude,
+    timestamp(uag.updated_at) as user_address_geocode_updated_at,
+from user_postal_code_geocode as upcg
+left join user_address_geocode as uag on upcg.user_id = uag.user_id
+
+)
+
+
+select
+    user_id,
+    user_creation_at,
+    geocode_type,
+    user_postal_code,
     coalesce(
         case
-            when ul.result_postcode = '97150'
+            when user_postal_code = '97150'
             then '978'
-            when left(ul.result_postcode, 2) in ('97', '98')
-            then left(ul.result_postcode, 3)
-            when left(ul.result_postcode, 3) in ('200', '201', '209', '205')
+            when left(user_postal_code, 2) in ('97', '98')
+            then left(  , 3)
+            when left(result_postcode, 3) in ('200', '201', '209', '205')
             then '2A'
-            when left(ul.result_postcode, 3) in ('202', '206')
+            when left(result_postcode, 3) in ('202', '206')
             then '2B'
-            else left(ul.result_postcode, 2)
+            else left(result_postcode, 2)
         end,
-        adu.user_department_code
+        user_department_code
     ) as user_department_code,
-    timestamp(ul.updated_at) as updated_at,
-    date(ul.updated_at) as updated_date
-from {{ source("raw", "user_address") }} as ul
-inner join
-    {{ source("raw", "applicative_database_user") }} as adu on ul.user_id = adu.user_id
-where ul.result_status = 'ok'
-qualify row_number() over (partition by ul.user_id order by ul.updated_at desc) = 1
+    user_longitude,
+    user_latitude,
+    user_address_geocode_updated_at
+from address_combined
--- orchestration/dags/data_gcp_dbt/models/intermediate/api_gouv/address/int_api_gouv__user_address_candidate_queue.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/api_gouv/address/int_api_gouv__user_address_candidate_queue.sql
@@ -1,49 +1,94 @@
-with
-    last_status_update as (
-        select user_id, max(datecreated) as last_modified_at
-        from {{ source("raw", "applicative_database_beneficiary_fraud_check") }}
-        where type = 'PROFILE_COMPLETION' and status = 'OK'
-        group by user_id
-    ),
+with profile_completion_check as (
+  select
+      user_id,
+      timestamp(datecreated) as user_last_modified_at,
+      json_extract(result_content , '$.city') as user_city,
+      json_extract(result_content , '$.address') as user_address,
+      json_extract(result_content , '$.postal_code') as user_postal_code
+  from {{ source("raw", "applicative_database_beneficiary_fraud_check") }}
+  where type = 'PROFILE_COMPLETION' and status = 'OK'
+),
 
-    user_location_update as (
-        select user_id, datetime(max(updated_at)) as last_calculation_at
-        from {{ source("raw", "user_address") }}
-        group by user_id
-    ),
-
-    user_candidates as (
-        select
-            adu.user_id,
-            adu.user_creation_date as user_creation_at,
-            greatest(
-                adu.user_creation_date, lsu.last_modified_at
-            ) as user_last_modified_at,
-            trim(
-                concat(
-                    replace(replace(adu.user_address, '\\r', ''), '\\n', ''),
-                    ' ',
-                    adu.user_postal_code,
-                    ' ',
-                    adu.user_city
-                )
-            ) as user_full_address
-        from {{ source("raw", "applicative_database_user") }} as adu
-        left join last_status_update as lsu on adu.user_id = lsu.user_id
-        where
-            coalesce(adu.user_address, '') <> ''
-            and adu.user_postal_code is not null
-            and adu.user_city is not null
+profile_completion as (
+  select
+    user_last_modified_at,
+    user_id,
+    trim(
+        concat(
+            replace(replace(adu.user_address, '\\r', ''), '\\n', ''),
+            ' ',
+            adu.user_postal_code,
+            ' ',
+            adu.user_city
+        )
     )
+    as user_full_address
+  from profile_completion_check as adu
+  where
+    -- beneficiary update should have all fields
+    coalesce(adu.user_address, '') <> ''
+    and adu.user_postal_code is not null
+),
+
+applicative_database_user as (
+    select
+        adu.user_id,
+        timestamp(adu.user_creation_date) as user_last_modified_at,
+        trim(
+            concat(
+                replace(replace(adu.user_address, '\\r', ''), '\\n', ''),
+                ' ',
+                adu.user_postal_code,
+                ' ',
+                adu.user_city
+            )
+        )
+        as user_full_address
+    from {{ source("raw", "applicative_database_user") }} as adu
+    where
+    -- beneficiary update should have all fields
+    coalesce(adu.user_address, '') <> ''
+    and adu.user_postal_code is not null
+),
+
+-- at least a postal code is present in the data
+all_users as (
+  select
+    user_id,
+    user_last_modified_at,
+    user_full_address
+  from applicative_database_user
+  union all
+  select
+    user_id,
+    user_last_modified_at,
+    user_full_address
+  from profile_completion
+),
+
+user_candidates as (
+  select
+    user_id,
+    user_last_modified_at,
+    user_full_address
+  from all_users
+  qualify row_number() over( partition by user_id order by user_last_modified_at desc ) = 1
+),
+
+user_location_update as (
+    select user_id, timestamp(max(updated_at)) as last_calculation_at
+    from {{ source("raw", "user_address") }}
+    group by user_id
+)
 
 select
     uc.user_id,
     uc.user_full_address,
-    uc.user_creation_at,
-    ulu.last_calculation_at as user_address_last_calculation_at
+    uc.user_last_modified_at as user_address_last_calculation_at
 
 from user_candidates as uc
 left join user_location_update as ulu on uc.user_id = ulu.user_id
+inner join {{ source("raw", "applicative_database_user") }} as u on uc.user_id = u.user_id
 where
     -- no location update or location update is older than last user update
     (ulu.user_id is null or ulu.last_calculation_at < uc.user_last_modified_at)

a015e0cf7ba9d11081492a61d8b4c873d3db1566
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-03-13 08:28:49 +0100

    feat: Add user_centroid

--- orchestration/dags/data_gcp_dbt/models/machine_learning/feature/ml_feat__user_features.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/feature/ml_feat__user_features.sql
@@ -12,11 +12,25 @@ with user_iris_data as (
         row_number() over(partition by user_id order by event_date desc) as row_num
     from {{ ref('int_pcreco__past_offer_context') }}
     where user_context.user_iris_id is not null and event_date >= date_sub(date('{{ ds() }}'), interval 60 day)
-)
+),
 
-select
+last_user_iris_data as (
+    select
     user_id,
     user_iris_id as last_known_user_iris_id,
     event_date as last_known_user_iris_date
 from user_iris_data
 where row_num = 1
+)
+
+select
+    last_user_iris_data.user_id,
+    last_user_iris_data.last_known_user_iris_id,
+    last_user_iris_data.last_known_user_iris_date,
+    iris.centroid as last_known_user_centroid,
+    st_x(iris.centroid) as last_known_user_centroid_x,
+    st_y(iris.centroid) as last_known_user_centroid_y
+from last_user_iris_data
+left join {{ ref('int_seed__iris_france') }} as iris
+on last_user_iris_data.last_known_user_iris_id = iris.id
+order by last_user_iris_data.centroid

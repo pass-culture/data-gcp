f25b09014bdbaac74a746d83a8c5e570889e36a2
Julpass <jules.arbelot@passculture.app>
2025-03-13 11:59:42 +0100

    one more dependencies removed

--- orchestration/dags/data_gcp_dbt/models/export/clickhouse/exp_clickhouse__collective_booking.sql
+++ orchestration/dags/data_gcp_dbt/models/export/clickhouse/exp_clickhouse__collective_booking.sql
@@ -2,13 +2,12 @@ select
     venue_id,
     offerer_id,
     collective_offer_id,
-    offer_id,
-    date(collective_booking_creation_date) as creation_date,
-    date(collective_booking_used_date) as used_date,
-    date(collective_booking_reimbursement_date) as reimbursement_date,
     collective_booking_status,
     educational_institution_id,
     collective_stock_number_of_tickets as number_of_tickets,
-    booking_amount
+    booking_amount,
+    date(collective_booking_creation_date) as creation_date,
+    date(collective_booking_used_date) as used_date,
+    date(collective_booking_reimbursement_date) as reimbursement_date
 from {{ ref("mrt_global__collective_booking") }}
 where offerer_id is not null and collective_booking_status != 'CANCELLED'

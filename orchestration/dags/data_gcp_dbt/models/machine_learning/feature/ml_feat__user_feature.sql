4452640ce5553906bf87aefb15e8e7b2f3f49124
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-03-17 13:44:42 +0100

    feat: Add favorite user_date

--- orchestration/dags/data_gcp_dbt/models/machine_learning/feature/ml_feat__user_feature.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/feature/ml_feat__user_feature.sql
@@ -11,6 +11,11 @@ with
         where not booking_is_cancelled
     ),
 
+    favorites_raw as (
+        select user_id, favorite_creation_date as event_date
+        from {{ ref("int_applicative__favorite") }}
+    ),
+
     daily_positions as (
         select
             user_id,
@@ -46,20 +51,42 @@ with
         group by user_id, event_date
     ),
 
+    favorites_by_date as (
+        select user_id, event_date, count(*) as daily_favorites_count
+        from favorites_raw
+        group by user_id, event_date
+    ),
+
     cumulative_bookings as (
         select
-            ud.user_id,
-            ud.event_date,
+            user_days.user_id,
+            user_days.event_date,
             sum(
                 case
-                    when bd.event_date <= ud.event_date
-                    then bd.daily_bookings_count
+                    when bookings_by_date.event_date <= user_days.event_date
+                    then bookings_by_date.daily_bookings_count
                     else 0
                 end
             ) as user_bookings_count
-        from user_days as ud
-        left join bookings_by_date as bd on ud.user_id = bd.user_id
-        group by ud.user_id, ud.event_date
+        from user_days
+        left join bookings_by_date on user_days.user_id = bookings_by_date.user_id
+        group by user_days.user_id, user_days.event_date
+    ),
+
+    cumulative_favorites as (
+        select
+            user_days.user_id,
+            user_days.event_date,
+            sum(
+                case
+                    when favorites_by_date.event_date <= user_days.event_date
+                    then favorites_by_date.daily_favorites_count
+                    else 0
+                end
+            ) as user_favorites_count
+        from user_days
+        left join favorites_by_date on user_days.user_id = favorites_by_date.user_id
+        group by user_days.user_id, user_days.event_date
     )
 
 select
@@ -69,10 +96,15 @@ select
     daily_positions.user_centroid,
     daily_positions.user_centroid_x,
     daily_positions.user_centroid_y,
-    cumulative_bookings.user_bookings_count
+    cumulative_bookings.user_bookings_count,
+    cumulative_favorites.user_favorites_count
 from cumulative_bookings
 left join
     daily_positions
     on cumulative_bookings.user_id = daily_positions.user_id
     and cumulative_bookings.event_date = daily_positions.event_date
+left join
+    cumulative_favorites
+    on daily_positions.user_id = cumulative_favorites.user_id
+    and daily_positions.event_date = cumulative_favorites.event_date
 order by daily_positions.user_id, daily_positions.event_date

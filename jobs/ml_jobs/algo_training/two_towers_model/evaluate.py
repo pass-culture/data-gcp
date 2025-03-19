75dfb658ca9d48ce53e98931815cb5a5955e0641
cibrahim-pass <188350754+cibrahim-pass@users.noreply.github.com>
2025-03-19 16:59:48 +0100

    (DS) refactor(metrics): refactored Two Tower evaluation code  (#3880)
    * refactored evaluation code to use recommenders lib
    
    * added dummy models
    
    * added recommenders to reqs and removed recmetrics
    
    * feat: Compile requirements
    
    * feat: resolved PR comments
    
    * feat: resolved PR comments
    
    * upgraded pandas-gbq, pyarrow and added fastparquet
    
    * typo in import commons
    
    * removed unnecessary param in dag task
    
    * solved dependencies
    
    * define GCP_PROJECT_ID according to env
    
    * fix metrics name to comply with mlflow reqs
    
    * comment on coverage and novelty + added extra default k to compute
    
    * added unittest to compute_metrics function
    
    * update README.md
    
    * resolved PR comments
    
    ---------
    
    Co-authored-by: LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>

--- jobs/ml_jobs/algo_training/two_towers_model/evaluate.py
+++ jobs/ml_jobs/algo_training/two_towers_model/evaluate.py
@@ -25,14 +25,25 @@ from two_towers_model.utils.evaluate import evaluate, save_pca_representation
 def main(
     experiment_name: str = typer.Option(None, help="Name of the experiment on MLflow"),
     model_name: str = typer.Option(MODEL_NAME, help="Name of the model to evaluate"),
-    training_dataset_name: str = typer.Option(
+    train_dataset_name: str = typer.Option(
         "recommendation_training_data", help="Name of the training dataset in storage"
     ),
     test_dataset_name: str = typer.Option(
         "recommendation_test_data", help="Name of the test dataset in storage"
     ),
-    config_file_name: str = typer.Option(
-        "config_file_name", help="List of input features for prediction"
+    list_k: list[int] = typer.Option(
+        [10, 50, 100, 250, 1000],
+        help="List of k values (top-k cutoff) for metrics evaluation.",
+    ),
+    all_users: bool = typer.Option(
+        False, help="Whether to evaluate for all users or not"
+    ),
+    dummy: bool = typer.Option(
+        False, help="Whether to evaluate metrics on dummy models or not"
+    ),
+    quantile_threshold: float = typer.Option(
+        0.99,
+        help="Threshold to consider top X% most popular items (0-1 range) in recommend popular dummy model",
     ),
 ):
     logger.info("-------EVALUATE START------- ")
@@ -65,12 +76,16 @@ def main(
     )
 
     metrics = evaluate(
-        loaded_model,
-        STORAGE_PATH,
-        training_dataset_name,
-        test_dataset_name,
-        config_file_name,
+        model=loaded_model,
+        storage_path=STORAGE_PATH,
+        train_dataset_name=train_dataset_name,
+        test_dataset_name=test_dataset_name,
+        list_k=list_k,
+        all_users=all_users,
+        dummy=dummy,
+        quantile_threshold=quantile_threshold,
     )
+
     # Export the PCA representations of the item embeddings
     pca_plots_path = f"{MODEL_DIR}/pca_plots/"
     os.makedirs(pca_plots_path, exist_ok=True)

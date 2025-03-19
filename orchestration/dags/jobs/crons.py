f63328ab10580d481049ef8ff31476bfd8d1b211
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-03-19 18:05:20 +0100

    (DE-1362) feat(ml): update cloudsql export to vm (#3898)

--- orchestration/dags/jobs/crons.py
+++ orchestration/dags/jobs/crons.py
@@ -5,22 +5,20 @@ SCHEDULE_DICT = {
         "stg": "0 12 * * 3",
     },
     "bigquery_export_old_partitions": "0 7 1 * *",
-    "clickhouse_exports": {
-        "daily": {
-            "prod": "0 6 * * *",
-            "stg": "45 4 * * *",
-            "dev": "45 4 * * *",
-        },
+    "export_clickhouse_daily": {
+        "prod": "0 6 * * *",
+        "stg": "45 4 * * *",
+        "dev": "45 4 * * *",
     },
     "dbt_artifacts": "0 5 * * *",
     "dbt_monthly": "30 4 1 * *",
     "dbt_run_dag": "45 2 * * *",
     "dbt_weekly": "0 4 * * 1",
     "historize_applicative_database": "15 3 * * *",
-    "import_analytics_v7": "45 4 * * *",
     "import_applicative_database": "0 1 * * *",
     "import_intraday_firebase_data": "0 1 * * *",
     "link_items": {"prod": "0 20 * * 3", "stg": "0 6 * * 3", "dev": "0 6 * * 3"},
+    "sync_bq_to_cloudsql_recommendation_tables": "00 5 * * *",
 }
 
 

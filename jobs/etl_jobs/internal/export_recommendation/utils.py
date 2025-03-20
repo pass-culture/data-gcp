77dde91f7297533a9bbd1e060e671fcc89e39d57
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-03-20 16:18:29 +0100

    (HF) feat(api): update secret record

--- jobs/etl_jobs/internal/export_recommendation/utils.py
+++ jobs/etl_jobs/internal/export_recommendation/utils.py
@@ -4,11 +4,16 @@ from google.auth.exceptions import DefaultCredentialsError
 from google.cloud import secretmanager
 
 ENV_SHORT_NAME = os.environ.get("ENV_SHORT_NAME")
+ENV_LONG_NAME = {
+    "prod": "production",
+    "stg": "stg",
+    "dev": "dev",
+}
 PROJECT_NAME = os.environ.get("GCP_PROJECT_ID")
 BIGQUERY_ML_RECOMMENDATION_DATASET = f"ml_reco_{ENV_SHORT_NAME}"
 BIGQUERY_ML_RETRIEVAL_DATASET = f"ml_retrieval_{ENV_SHORT_NAME}"
 BIGQUERY_SEED_DATASET = f"seed_{ENV_SHORT_NAME}"
-RECOMMENDATION_SQL_INSTANCE = f"cloudsql-recommendation-{ENV_SHORT_NAME}-ew1"
+RECOMMENDATION_SQL_INSTANCE = f"cloudsql-recommendation-{ENV_LONG_NAME}-ew1"
 REGION = "europe-west1"
 
 

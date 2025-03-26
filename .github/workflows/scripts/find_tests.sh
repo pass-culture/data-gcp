c2e585f383bbb1210488040dd40f65829c0f2649
lmontier-pass <172769973+lmontier-pass@users.noreply.github.com>
2025-03-26 19:45:06 +0100

    (BSR) fix(cd): Fix CD (#3961)
    * fix find_tests.sh

--- .github/workflows/scripts/find_tests.sh
+++ .github/workflows/scripts/find_tests.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
-# Define a list of folders to ignore
-ignored_folders=("_template")
+# Initialize flag for escaping
+no_escape=false
 
-# Build the find exclusion patterns
-find_exclusions=""
-for ignored in "${ignored_folders[@]}"; do
-  find_exclusions="$find_exclusions -not -path \"*$ignored*\""
+# Parse arguments
+for arg in "$@"; do
+  if [ "$arg" == "--no-escape" ]; then
+    no_escape=true
+  fi
 done
 
-# Find directories named "tests" and get their parent directories, excluding ignored folders
-eval "find jobs -type d -name \"tests\" $find_exclusions -exec dirname {} \;" > /tmp/list.txt
+# Define a list of folders to ignore
+ignored_folders=("jobs/ml_jobs/_template")
+
+# Build the find command
+find_cmd="find jobs -type d"
+for ignored in "${ignored_folders[@]}"; do
+  find_cmd="$find_cmd -not \( -path \"$ignored\" -prune \)"
+done
+find_cmd="$find_cmd -name tests -exec dirname {} \;"
+
+# Execute the command
+echo Find command: $find_cmd
 
 # Initialize an array to hold the folder names
+eval "$find_cmd" > /tmp/list.txt
 folders=()
-
 # Read the list of directories from the file and add them to the array
 while IFS= read -r folder; do
-  folders+=(\\\"${folder}\\\")
+  if [ "$no_escape" = true ]; then
+    folders+=("'${folder}'")
+  else
+    folders+=(\\\"${folder}\\\")
+  fi
 done < /tmp/list.txt
 
 # Create a JSON array from the folder names

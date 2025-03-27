982ecf950579118450e48b05970aea789b532e1f
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-03-27 11:00:20 +0100

    merge master

---
+++ orchestration/dags/data_gcp_dbt/models/export/ministry/exp_ministry__offerer_address.sql
@@ -0,0 +1,10 @@
+select
+    offerer_address_id,
+    offerer_address_label,
+    address_id,
+    offerer_id,
+    address_street,
+    address_postal_code,
+    address_city,
+    address_department_code
+from {{ ref("mrt_global__offerer_address") }}

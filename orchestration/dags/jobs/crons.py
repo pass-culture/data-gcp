1aa4962468eb9224975b2a5d02e04b14c4a93fb6
dcuesta-pass <daniel.cuesta@passculture.app>
2025-03-27 09:56:08 +0100

    feat(dbt): Add documentation and schema for int_applicative__offer_item_id model

--- orchestration/dags/jobs/crons.py
+++ orchestration/dags/jobs/crons.py
@@ -4,23 +4,23 @@ SCHEDULE_DICT = {
         "dev": "0 12 * * 2",
         "stg": "0 12 * * 3",
     },
+    "artist_linkage": "0 12 * * 0",
     "bigquery_export_old_partitions": "0 7 1 * *",
-    "clickhouse_exports": {
-        "daily": {
-            "prod": "0 6 * * *",
-            "stg": "45 4 * * *",
-            "dev": "45 4 * * *",
-        },
+    "export_clickhouse_daily": {
+        "prod": "0 6 * * *",
+        "stg": "45 4 * * *",
+        "dev": "45 4 * * *",
     },
-    "dbt_artifacts": "0 5 * * *",
-    "dbt_monthly": "30 4 1 * *",
+    "dbt_artifacts": "0 6 * * *",
+    "dbt_monthly": "30 10 1 * *",
     "dbt_run_dag": "45 2 * * *",
-    "dbt_weekly": "0 4 * * 1",
+    "dbt_weekly": "0 10 * * 1",
+    "embedding_reduction_item": "0 12 * * 0",
     "historize_applicative_database": "15 3 * * *",
-    "import_analytics_v7": "45 4 * * *",
     "import_applicative_database": "0 1 * * *",
     "import_intraday_firebase_data": "0 1 * * *",
     "link_items": {"prod": "0 20 * * 3", "stg": "0 6 * * 3", "dev": "0 6 * * 3"},
+    "sync_bq_to_cloudsql_recommendation_tables": "00 6 * * *",
 }
 
 

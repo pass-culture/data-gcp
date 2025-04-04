e5401b1cdd4cef3abc518ae821a500d390dcdd75
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-04-04 15:50:46 +0200

    (BSR) chore(cleaning): remove deprecated table user_pro_flags

--- orchestration/dags/data_gcp_dbt/scripts/elementary_send_report_k8s.sh
+++ orchestration/dags/data_gcp_dbt/scripts/elementary_send_report_k8s.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
+set -xe
 
-set -e
 
-edr send-report --project-dir $PATH_TO_DBT_PROJECT --target-path $PATH_TO_DBT_TARGET --profiles-dir $PATH_TO_DBT_PROJECT --profile-target $ENV_SHORT_NAME --gcs-bucket-name $DATA_BUCKET_NAME --bucket-file-path $REPORT_FILE_PATH --slack-token $SLACK_TOKEN --slack-channel-name $CHANNEL_NAME --env $ENV_SHORT_NAME $GLOBAL_CLI_FLAGS --disable html_attachment --days-back 1
+# Run the report
+edr send-report \
+    --project-dir "$PATH_TO_DBT_PROJECT" \
+    --target-path "$DBT_TARGET_DIR" \
+    --profiles-dir "$PATH_TO_DBT_PROJECT" \
+    --profile-target "$ENV_SHORT_NAME" \
+    --gcs-bucket-name "$DATA_BUCKET_NAME" \
+    --bucket-file-path "$REPORT_FILE_PATH" \
+    --slack-token "$SLACK_TOKEN" \
+    --slack-channel-name "$CHANNEL_NAME" \
+    --env "$ENV_SHORT_NAME" \
+    $GLOBAL_CLI_FLAGS \
+    --disable html_attachment \
+    --days-back 1
 
-# Capture the exit code and propagate it
+# Capture exit code before cleanup
 exit_code=$?
+
+# Cleanup
+rm -rf "$DBT_TARGET_DIR" "$DBT_PACKAGES_DIR"
+rm -f "$PATH_TO_DBT_PROJECT/dbt_packages"
+
+# Final exit
 if [ $exit_code -ne 0 ]; then
     echo "dbt command failed with exit code $exit_code."
-    exit $exit_code  # Ensure the script exits with dbt's exit code
+    exit $exit_code
 fi

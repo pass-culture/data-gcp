6ca5ccd0bbff6208da976a95b96e1bc5d42dcffb
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-04-15 16:57:17 +0200

    WIP feat: Evaluate on unique session id instead of user id

--- jobs/ml_jobs/ranking_endpoint/src/evaluation.py
+++ jobs/ml_jobs/ranking_endpoint/src/evaluation.py
@@ -3,35 +3,39 @@ from recommenders.evaluation.python_evaluation import ndcg_at_k
 
 
 def compute_ndcg_at_k(predictions: pd.DataFrame, k_list: list) -> dict:
-    predictions_by_user_df = predictions.loc[
-        :, ["user_id", "item_id", "target_class", "score"]
-    ].drop_duplicates(subset=["user_id", "item_id"])
+    predictions_by_session_df = predictions.loc[
+        :, ["unique_session_id", "item_id", "target_class", "score"]
+    ].drop_duplicates(subset=["unique_session_id", "item_id"])
 
     rating_true_df = (
-        predictions_by_user_df[
+        predictions_by_session_df[
             [
-                "user_id",
+                "unique_session_id",
                 "item_id",
                 "target_class",
             ]
         ]
         .copy()
         .rename(
-            columns={"target_class": "rating", "user_id": "userID", "item_id": "itemID"}
+            columns={
+                "target_class": "rating",
+                "unique_session_id": "userID",
+                "item_id": "itemID",
+            }
         )
         .assign(prediction=-1)
     )
 
     rating_pred_df = (
-        predictions_by_user_df.assign(prediction=lambda df: df.score)[
+        predictions_by_session_df.assign(prediction=lambda df: df.score)[
             [
-                "user_id",
+                "unique_session_id",
                 "item_id",
                 "prediction",
             ]
         ]
         .copy()
-        .rename(columns={"user_id": "userID", "item_id": "itemID"})
+        .rename(columns={"unique_session_id": "userID", "item_id": "itemID"})
         .assign(rating=-1)
     )
 

3258e44a9326d72bc6f248d57722556121c910de
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-04-22 16:33:18 +0200

    feat: fix postal_code geocode

--- orchestration/dags/data_gcp_dbt/models/intermediate/seed/int_seed__geo_postal_code.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/seed/int_seed__geo_postal_code.sql
@@ -1,8 +1,8 @@
 {{ config(**custom_table_config()) }}
 
-SELECT 
+select
     postal_code,
     avg(safe_cast(centroid_latitude as float64)) as postal_approx_centroid_latitude,
     avg(safe_cast(centroid_longitude as float64)) as postal_approx_centroid_longitude
-FROM {{ source("seed", "2025_insee_code") }}
-GROUP BY postal_code
+from {{ source("seed", "2025_insee_code") }}
+group by postal_code

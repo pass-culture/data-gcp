b3ff631a3dbe6dddc46f6bd23d9aab638b79f492
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-04-30 17:09:51 +0200

    feat: cleanup userBirthDate

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__user.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__user.sql
@@ -3,6 +3,7 @@
 
 {{ config(pre_hook="{{create_humanize_id_function()}}") }}
 
+with int_applicative__user as (
 select
     u.user_id,
     u.user_creation_date,
@@ -64,3 +65,26 @@ select
     u.user_subscribed_themes is not null as is_theme_subscribed
 from {{ source("raw", "applicative_database_user") }} u
 where u.user_role in ("UNDERAGE_BENEFICIARY", "BENEFICIARY")
+)
+
+select 
+    user_id,
+    user_creation_date,
+    user_humanized_id,
+    user_has_enabled_marketing_email,
+    user_activity,
+    user_civility,
+    user_birth_date,
+    {{ calculate_exact_age(CURRENT_DATE, user_birth_date) }} as user_age,
+    user_school_type,
+    user_is_active,
+    user_role,
+    user_last_connection_date,
+    user_is_email_validated,
+    user_has_seen_pro_tutorials,
+    user_phone_validation_status,
+    user_has_validated_email,
+    user_has_enabled_marketing_push,
+    user_subscribed_themes,
+    is_theme_subscribed,
+from int_applicative__user

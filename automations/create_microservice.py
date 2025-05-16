75249330553fdd29280bed58ffa4b2316451c549
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-05-16 10:50:39 +0200

    (BSR) fix(miscellaneous): typo & deprecated variables (#4135)
    * fix: typo
    
    * migration: artifacts bucket deferal

--- automations/create_microservice.py
+++ automations/create_microservice.py
@@ -36,7 +36,7 @@ def append_to_makefile(service_name):
     )
 
     service_name_kebab = service_name.replace("_", "-")
-    new_line = f"\tMICROSERVICE_PATH=jobs/ml_jobs/{service_name} PHYTON_VENV_VERSION=3.10.4 VENV_NAME=data-gcp-{service_name_kebab} REQUIREMENTS_NAME=requirements.txt RECREATE_VENV=$(CLEAN_INSTALL) make install_microservice\n"
+    new_line = f"\tMICROSERVICE_PATH=jobs/ml_jobs/{service_name} PYTHON_VENV_VERSION=3.10.4 VENV_NAME=data-gcp-{service_name_kebab} REQUIREMENTS_NAME=requirements.txt RECREATE_VENV=$(CLEAN_INSTALL) make install_microservice\n"
 
     lines.insert(end_install_index, new_line)
 

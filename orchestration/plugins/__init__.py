eb2880ff768ec19db4821f50dcdf281538dac2a4
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-05-19 14:10:06 +0200

    wip

--- orchestration/plugins/__init__.py
+++ orchestration/plugins/__init__.py
@@ -1,19 +1,34 @@
-from dbt_docs import DBTDocs
 from flask import Blueprint
 
 from airflow.plugins_manager import AirflowPlugin
 
-bp = Blueprint("plugin", __name__, template_folder="templates", static_folder="static")
 
-v_appbuilder_view = DBTDocs()
-v_appbuilder_package = {
-    "name": "Docs",
-    "category": "DBT",
-    "view": v_appbuilder_view,
-}
+def init_plugin():
+    try:
+        from dbt_docs import DBTDocs
+
+        bp = Blueprint(
+            "plugin", __name__, template_folder="templates", static_folder="static"
+        )
+
+        v_appbuilder_view = DBTDocs()
+        v_appbuilder_package = {
+            "name": "Docs",
+            "category": "DBT",
+            "view": v_appbuilder_view,
+        }
+
+        return bp, v_appbuilder_package
+        # Defining the plugin class
+
+    except Exception as e:
+        print(e)
+        return None, None
 
 
-# Defining the plugin class
+bp, v_appbuilder_package = init_plugin()
+
+
 class AirflowCustomPlugin(AirflowPlugin):
     name = "AirflowCustomPlugin"
     flask_blueprints = [bp]

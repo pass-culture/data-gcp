5d0fef97d20231c6f191b1ca954236b42ea899dc
cibrahim-pass <188350754+cibrahim-pass@users.noreply.github.com>
2025-05-21 16:33:09 +0200

    added cloud ops agent to hooks to monitor RAM usage

--- orchestration/dags/common/hooks/image.py
+++ orchestration/dags/common/hooks/image.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class CPUImage:
     source_image: str = "projects/deeplearning-platform-release/global/images/tf-ent-2-14-cpu-v20240922-py310"
@@ -10,6 +11,8 @@ class CPUImage:
         sudo bash add-google-cloud-ops-agent-repo.sh --also-install
         """
     startup_script_wait_time: int = 30
+
+
 @dataclass
 class TFGPUImage:
     source_image: str = "projects/deeplearning-platform-release/global/images/tf-ent-2-14-cu118-v20240922-py310"
@@ -21,6 +24,8 @@ class TFGPUImage:
         sudo bash add-google-cloud-ops-agent-repo.sh --also-install
     """
     startup_script_wait_time: int = 180
+
+
 MACHINE_TYPE = {
     "cpu": CPUImage,
     "gpu": TFGPUImage,

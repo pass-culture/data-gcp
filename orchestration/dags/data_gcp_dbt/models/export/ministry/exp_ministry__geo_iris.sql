a0daf1c6a8c9140e069c8d905dd134c3f6dd7fb7
cdelabre-pass <103637148+cdelabre-pass@users.noreply.github.com>
2025-06-02 16:04:14 +0200

    (HF) feat(export): fix prefix on encrypt transfer

--- orchestration/dags/data_gcp_dbt/models/export/ministry/exp_ministry__geo_iris.sql
+++ orchestration/dags/data_gcp_dbt/models/export/ministry/exp_ministry__geo_iris.sql
@@ -8,6 +8,6 @@ select
     sub_district_code,
     department_code,
     department_name,
-    ST_ASTEXT(iris_centroid) as iris_centroid,
-    ST_ASTEXT(iris_shape) as iris_shape
+    st_astext(iris_centroid) as iris_centroid,
+    st_astext(iris_shape) as iris_shape
 from {{ ref("int_seed__geo_iris") }}

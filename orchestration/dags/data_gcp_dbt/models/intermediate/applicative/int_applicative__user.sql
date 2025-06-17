375dae33fba5778fb1bef7f20802cf5830699c0e
Timothée Scheurer <timothee.scheurer@passculture.app>
2025-06-17 10:36:45 +0200

    add 15_16 users

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__user.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__user.sql
@@ -8,7 +8,7 @@ with
         select
             u.user_id,
             u.user_creation_date,
-            {{ target_schema }}.humanize_id(u.user_id) as user_humanized_id,
+            {{ target_schema }}.humanize_id(u.user_id) as user_humanized_id, --noqa: PRS
             u.user_has_enabled_marketing_email,
             case
                 when u.user_activity in ("Alternant")
@@ -68,8 +68,8 @@ with
             u.user_subscribed_themes,
             u.user_subscribed_themes is not null as is_theme_subscribed,
             current_date as reference_date
-        from {{ source("raw", "applicative_database_user") }} u
-        where u.user_role in ("UNDERAGE_BENEFICIARY", "BENEFICIARY")
+        from {{ source("raw", "applicative_database_user") }} as u
+        where u.user_role in ("UNDERAGE_BENEFICIARY", "BENEFICIARY", "FREE_BENEFICIARY")
     )
 
 select
@@ -91,5 +91,5 @@ select
     user_has_validated_email,
     user_has_enabled_marketing_push,
     user_subscribed_themes,
-    is_theme_subscribed,
+    is_theme_subscribed
 from int_applicative__user

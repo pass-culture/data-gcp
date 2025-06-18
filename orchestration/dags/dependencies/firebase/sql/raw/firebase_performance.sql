4f93bcbf0f260bc4de4099f259a83ed6f36ea9f5
Lucile <lucile.rainteau@passculture.app>
2025-06-18 09:38:47 +0200

    fix formatting

--- orchestration/dags/dependencies/firebase/sql/raw/firebase_performance.sql
+++ orchestration/dags/dependencies/firebase/sql/raw/firebase_performance.sql
@@ -1,3 +1,3 @@
-select *,  TIMESTAMP_TRUNC(event_timestamp, DAY) as event_date
+select *, timestamp_trunc(event_timestamp, day) as event_date
 from `{{ params.gcp_project_env }}.app_passculture_{{ params.suffix }}`
-WHERE TIMESTAMP_TRUNC(event_timestamp, DAY) = TIMESTAMP("{{ ds }}")
+where timestamp_trunc(event_timestamp, day) = timestamp("{{ ds }}")

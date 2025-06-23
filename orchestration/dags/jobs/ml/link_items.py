af34ce5341325badc18239a0a8ab9402e1683edd
dcuesta-pass <daniel.cuesta@passculture.app>
2025-06-23 17:35:58 +0200

    fix(link_items): replace MLFLOW_BUCKET_NAME with ML_BUCKET_TEMP in GCS to BigQuery operators

--- orchestration/dags/jobs/ml/link_items.py
+++ orchestration/dags/jobs/ml/link_items.py
@@ -10,9 +10,6 @@ from common.config import (
     DAG_TAGS,
     ENV_SHORT_NAME,
     GCP_PROJECT_ID,
-    MLFLOW_BUCKET_NAME,
-    ML_BUCKET_OUTPUT,
-    ML_BUCKET_PROCESSING,
     ML_BUCKET_TEMP,
 )
 from common.operators.gce import (
@@ -362,7 +359,7 @@ with DAG(
         load_linked_product_into_bq = GCSToBigQueryOperator(
             project_id=GCP_PROJECT_ID,
             task_id="load_linked_product_into_bq",
-            bucket=MLFLOW_BUCKET_NAME,
+            bucket=ML_BUCKET_TEMP,
             source_objects=f"""{
                 build_path(
                     DAG_CONFIG["BASE_PATHS"]["GCS_FOLDER"],
@@ -466,7 +463,7 @@ with DAG(
         load_linked_offer_into_bq = GCSToBigQueryOperator(
             project_id=GCP_PROJECT_ID,
             task_id="load_linked_offer_into_bq",
-            bucket=MLFLOW_BUCKET_NAME,
+            bucket=ML_BUCKET_TEMP,
             source_objects=f"""{
                 build_path(
                     DAG_CONFIG["BASE_PATHS"]["GCS_FOLDER"],

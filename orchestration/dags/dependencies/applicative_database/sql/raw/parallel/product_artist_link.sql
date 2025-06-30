356e72d5400921c41efa732d22708dd42faaae7c
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-06-30 16:50:35 +0200

    feat: Add product_artist_link import sql query

---
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/product_artist_link.sql
@@ -0,0 +1,8 @@
+SELECT
+    "id" as product_artist_link_id
+    ,"product_id" as offer_product_id
+    ,"artist_id"
+    ,"artist_type"
+    ,"date_created"
+    ,"date_modified"
+FROM public.artist_product_link

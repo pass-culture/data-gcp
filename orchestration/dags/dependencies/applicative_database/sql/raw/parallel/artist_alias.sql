ce45e66b501b7da3115d99ec7b5360a200ae6287
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-06-30 16:47:16 +0200

    feat: Add import artist_alias sql query

---
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/artist_alias.sql
@@ -0,0 +1,11 @@
+SELECT
+    "id" as artist_alias_id
+    ,"artist_id"
+    ,"artist_alias_name" as artist_offer_name
+    ,"artist_cluster_id"
+    ,"artist_wiki_data_id" as artist_wiki_id
+    ,"offer_category_id"
+    ,"artist_type"
+    ,"date_created"
+    ,"date_modified"
+FROM public.artist_alias

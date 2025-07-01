073f894634bdb425d19ba51c4dd90c74d9ff01f1
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-07-01 10:57:43 +0200

    fix: Fix typo and needless as

--- orchestration/dags/dependencies/applicative_database/sql/raw/parallel/artist.sql
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/artist.sql
@@ -3,9 +3,9 @@ SELECT
     ,"name" as artist_name
     ,"description" as artist_description
     ,"image" as wikidata_image_file_url
-    ,"image_license" as woikidata_image_license
+    ,"image_license" as wikidata_image_license
     ,"image_license_url" as wikidata_image_license_url
     ,"image_author" as wikidata_image_author
-    ,"date_created" as date_created
-    ,"date_modified" as date_modified
+    ,"date_created"
+    ,"date_modified"
 FROM public.artist

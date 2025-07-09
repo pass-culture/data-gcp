7b121fdfd54fc9587ca89de9bcd69e84d1f16dc3
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-07-09 11:23:33 +0200

    fix: offer snapshot dependencies

--- orchestration/dags/data_gcp_dbt/snapshots/raw_applicative/snapshot_raw__offer.sql
+++ orchestration/dags/data_gcp_dbt/snapshots/raw_applicative/snapshot_raw__offer.sql
@@ -1,5 +1,3 @@
---depends_on: {{ ref('raw_applicative__offer_full') }}
---depends_on: {{ ref('raw_applicative__offer_lite') }}
 {% snapshot snapshot_raw__offer %}
 
     {% if is_first_day_of_month() == "TRUE" %}
@@ -48,6 +46,45 @@
             offerer_address_id,
             custom_scd_id
         from {{ ref("raw_applicative__offer_full") }}
+        union all -- dummy select to enforce snapshot dependency
+        select
+            offer_id_at_providers,
+            offer_modified_at_last_provider_date,
+            offer_id,
+            offer_creation_date,
+            offer_product_id,
+            venue_id,
+            offer_last_provider_id,
+            booking_email,
+            offer_is_active,
+            offer_name,
+            offer_description,
+            offer_url,
+            offer_is_national,
+            offer_extra_data,
+            offer_ean,
+            offer_is_duo,
+            offer_fields_updated,
+            offer_withdrawal_details,
+            offer_audio_disability_compliant,
+            offer_mental_disability_compliant,
+            offer_motor_disability_compliant,
+            offer_visual_disability_compliant,
+            offer_external_ticket_office_url,
+            offer_validation,
+            offer_last_validation_type,
+            offer_subcategoryid,
+            offer_updated_date,
+            offer_withdrawal_type,
+            offer_withdrawal_delay,
+            booking_contact,
+            offerer_address_id,
+            custom_scd_id,
+            offer_finalization_date,
+            offer_publication_date,
+            scheduled_offer_bookability_date
+        from {{ ref("raw_applicative__offer_lite") }}
+        limit 0
 
     {% else %}
 
@@ -99,7 +136,42 @@
             offer_publication_date,
             scheduled_offer_bookability_date
         from {{ ref("raw_applicative__offer_lite") }}
-
+        union all -- dummy select to enforce snapshot dependency
+        select
+            offer_id_at_providers,
+            offer_modified_at_last_provider_date,
+            offer_id,
+            offer_creation_date,
+            offer_product_id,
+            venue_id,
+            offer_last_provider_id,
+            booking_email,
+            offer_is_active,
+            offer_name,
+            offer_description,
+            offer_url,
+            offer_is_national,
+            offer_extra_data,
+            offer_ean,
+            offer_is_duo,
+            offer_fields_updated,
+            offer_withdrawal_details,
+            offer_audio_disability_compliant,
+            offer_mental_disability_compliant,
+            offer_motor_disability_compliant,
+            offer_visual_disability_compliant,
+            offer_external_ticket_office_url,
+            offer_validation,
+            offer_last_validation_type,
+            offer_subcategoryid,
+            offer_updated_date,
+            offer_withdrawal_type,
+            offer_withdrawal_delay,
+            booking_contact,
+            offerer_address_id,
+            custom_scd_id
+        from {{ ref("raw_applicative__offer_full") }}
+        limit 0
     {% endif %}
 
 {% endsnapshot %}

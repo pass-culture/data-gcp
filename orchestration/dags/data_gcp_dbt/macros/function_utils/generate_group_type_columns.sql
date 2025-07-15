5eea1c52b38c46b08bad5eb5f61d0addb5fe54d9
Julpass <jules.arbelot@passculture.app>
2025-07-15 10:37:41 +0200

    fmt

--- orchestration/dags/data_gcp_dbt/macros/function_utils/generate_group_type_columns.sql
+++ orchestration/dags/data_gcp_dbt/macros/function_utils/generate_group_type_columns.sql
@@ -1,8 +1,6 @@
 {% macro generate_group_type_columns(params) %}
     {{ params.group_type }} as dimension_name,
-    {% if params.group_type == "NAT" %}
-        'NAT'
-    {% else %}
-        {{ params.group_type_name }}
+    {% if params.group_type == "NAT" %} 'NAT'
+    {% else %} {{ params.group_type_name }}
     {% endif %} as dimension_value
 {% endmacro %}

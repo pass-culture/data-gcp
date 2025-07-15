5fdf0de6882df9ac7792f99890aa50b9adf7ad63
Julpass <jules.arbelot@passculture.app>
2025-07-15 18:24:14 +0200

    update model and incremental config

--- orchestration/dags/data_gcp_dbt/models/mart/propilote/PPG/ephemerals_kpis/mrt_propilote__ppg_beneficiary.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/propilote/PPG/ephemerals_kpis/mrt_propilote__ppg_beneficiary.sql
@@ -1,4 +1,12 @@
-{{ config(materialized="table") }}
+{{
+    config(
+        **custom_incremental_config(
+            incremental_strategy="insert_overwrite",
+            partition_by={"field": "execution_date", "data_type": "date"},
+            on_schema_change="append_new_columns",
+        )
+    )
+}}
 
 {% set dimensions = [
     {"name": "NAT", "value_expr": "'NAT'"},
@@ -11,10 +19,13 @@ with
     last_day_of_month as (
         select
             date_trunc(deposit_active_date, month) as execution_date,
-            date("{{ ds }}") as update_date,
+            date("{{ ds() }}") as update_date,
             max(deposit_active_date) as last_active_date
-        from ref("mrt_native__daily_user_deposit")
+        from {{ ref("mrt_native__daily_user_deposit") }}
         where deposit_active_date > date('2021-01-01')
+            {% if is_incremental() %}
+            and date_trunc(deposit_active_date, month) = date_trunc(date("{{ ds() }}"), month)
+            {% endif %}
         group by date_trunc(deposit_active_date, month)
     ),
 
@@ -24,33 +35,29 @@ with
             uua.user_id,
             uua.deposit_amount,
             coalesce(sum(booking_intermediary_amount), 0) as amount_spent,
-            case
-                when uua.deposit_type = "GRANT_17_18" and uua.user_age <= 17
-                then "GRANT_15_17"
-                when uua.deposit_type = "GRANT_17_18" and uua.user_age >= 18
-                then "GRANT_18"
-                else uua.deposit_type
-            end as deposit_type
-        from ref("mrt_native__daily_user_deposit") uua
+        from {{ ref("mrt_native__daily_user_deposit") }} uua
         left join
-            ref("mrt_global__booking") ebd
+            {{ ref("mrt_global__booking") }} ebd
             on ebd.deposit_id = uua.deposit_id
             and uua.deposit_active_date = date(booking_used_date)
             and booking_is_used
         where deposit_active_date > date('2021-01-01')
-        group by deposit_active_date, user_id, deposit_type, deposit_amount
+        group by deposit_active_date, user_id, deposit_amount
     ),
+
 
     user_cumulative_amount_spent as (
         select
             deposit_active_date,
             user_id,
-            deposit_type,
             deposit_amount as initial_deposit_amount,
             sum(amount_spent) over (
-                partition by user_id, deposit_type order by deposit_active_date asc
+                partition by user_id order by deposit_active_date asc
             ) as cumulative_amount_spent,
         from user_amount_spent_per_day
+        {% if is_incremental() %}
+            where deposit_active_date = date_trunc(date("{{ ds() }}"), month)
+        {% endif %}
     ),
 
     aggregated_active_beneficiary as (
@@ -64,7 +71,6 @@ with
                 update_date,
                 '{{ dim.name }}' as dimension_name,
                 {{ dim.value_expr }} as dimension_value,
-                deposit_type as user_type,
                 'beneficiaire_actuel' as indicator,
                 count(distinct uua.user_id) as numerator,
                 1 as denominator
@@ -76,18 +82,20 @@ with
                 {{ ref("region_department") }} rd
                 on eud.user_department_code = rd.num_dep
             where cumulative_amount_spent < initial_deposit_amount
+            {% if is_incremental() %}
+                and execution_date = date_trunc(date("{{ ds() }}"), month)
+            {% endif %}
             group by
                 execution_date,
                 update_date,
-                dim.name,
-                dim.value_expr,
-                deposit_type,
+                dimension_name,
+                dimension_value,
                 indicator
         {% endfor %}
 
     ),
 
-    aggregated_total_beneficiairy as (
+    aggregated_total_beneficiary as (
         {% for dim in dimensions %}
             {% if not loop.first %}
                 union all
@@ -97,23 +105,24 @@ with
                 update_date,
                 '{{ dim.name }}' as dimension_name,
                 {{ dim.value_expr }} as dimension_value,
-                cast(null as string) as user_type,
                 "beneficiaire_total" as indicator,
                 count(distinct eud.user_id) as numerator,
                 1 as denominator
             from last_day_of_month ldm
             inner join
-                ref("mrt_global__user") eud
+                {{ ref("mrt_global__user") }} eud
                 on date(eud.first_deposit_creation_date) <= date(ldm.last_active_date)
             left join
-                ref("region_department") as rd on eud.user_department_code = rd.num_dep
+                {{ ref("region_department") }} as rd on eud.user_department_code = rd.num_dep
+            {% if is_incremental() %}
+                where execution_date = date_trunc(date("{{ ds() }}"), month)
+            {% endif %}
             group by
                 execution_date,
                 update_date,
-                dim.name,
-                dim.value_expr,
-                user_type,
-                indiator
+                dimension_name,
+                dimension_value,
+                indicator
         {% endfor %}
     )
 
@@ -121,4 +130,4 @@ select *
 from aggregated_active_beneficiary
 union all
 select *
-from aggregated_total_beneficiairy
+from aggregated_total_beneficiary

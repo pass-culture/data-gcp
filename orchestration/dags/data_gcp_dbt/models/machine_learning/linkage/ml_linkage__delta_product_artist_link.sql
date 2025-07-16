b241545e575602c748251b8dabc1ef2de8e6b974
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-07-16 15:23:37 +0200

    feat: Add export delta tables

--- orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__delta_product_artist_link.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__delta_product_artist_link.sql
@@ -1,9 +1,4 @@
 {{ config(**custom_table_config(materialized="view")) }}
 
-select distinct
-    offer_product_id,
-    artist_id,
-    artist_type,
-    action,
-    comment
+select distinct offer_product_id, artist_id, artist_type, action, comment
 from {{ source("ml_preproc", "delta_product_artist_link") }}

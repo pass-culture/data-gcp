3fb1cea357203ea01f84cdfc66e89eaa5191d8a0
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-07-18 14:26:29 +0200

    (DE) feat(clickhouse): export venue offer statistics to clickhouse (#4284)
    * (DE) feat(clickhouse): export venue offer statistics to clickhouse
    
    * single partition table
    
    * feat: update overwrite -> handle unpartitionned table

---
+++ jobs/etl_jobs/internal/export_clickhouse/schema/tmp/intermediate_venue_offer_statistic.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS {{ dataset }}.{{ tmp_table_name }} ON cluster default
+    ENGINE = MergeTree
+    PARTITION BY tuple()
+    ORDER BY (venue_id)
+    SETTINGS storage_policy='gcs_main'
+
+AS
+    SELECT
+        '{{ date }}' as update_date,
+        cast(venue_id as String) as venue_id,
+        cast(total_active_offers as UInt64) as total_active_offers,
+        cast(total_pending_offers as UInt64) as total_pending_offers,
+        cast(total_inactive_non_rejected_offers as UInt64) as total_inactive_non_rejected_offers,
+        cast(total_active_collective_offers as UInt64) as total_active_collective_offers,
+        cast(total_pending_collective_offers as UInt64) as total_pending_collective_offers,
+        cast(total_inactive_non_rejected_collective_offers as UInt64) as total_inactive_non_rejected_collective_offers
+    FROM s3(
+        gcs_credentials,
+        url='{{ bucket_path }}'
+)

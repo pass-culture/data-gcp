0fef5dcb76b2d39f85877a9c93fdc9c3b716df76
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-07-22 00:33:51 +0200

    fix: newletter global stats header

--- jobs/etl_jobs/connectors/brevo.py
+++ jobs/etl_jobs/connectors/brevo.py
@@ -11,8 +11,8 @@ class BrevoConnector:
     """
     Brevo API wrapper using SyncHttpClient for interacting with Brevo endpoints.
     """
-    BASE_URL = "https://api.brevo.com/v3"
 
+    BASE_URL = "https://api.brevo.com/v3"
 
     def __init__(self, api_key: str, client: Optional[SyncHttpClient] = None):
         self.api_key = api_key
@@ -20,12 +20,30 @@ class BrevoConnector:
         self.headers = {
             "api-key": self.api_key,
             "Content-Type": "application/json",
-            "Accept": "application/json"
+            "Accept": "application/json",
         }
 
-    def get_email_campaigns(self, status: str = "sent", limit: int = 50, offset: int = 0):
+    def get_email_campaigns(
+        self,
+        status: str = "sent",
+        limit: int = 50,
+        start_date: Optional[str] = None,
+        end_date: Optional[str] = None,
+        offset: int = 0,
+    ):
         url = f"{self.BASE_URL}/emailCampaigns"
-        params = {"status": status, "limit": limit, "offset": offset}
+        params = {
+            "status": status,
+            "limit": limit,
+            "offset": offset,
+            "statistics": "globalStats",
+        }
+        if start_date:
+            params["startDate"] = start_date
+
+        if end_date:
+            params["endDate"] = end_date
+
         return self.client.request("GET", url, headers=self.headers, params=params)
 
     def get_smtp_templates(self, active_only: bool = True, offset: int = 0):
@@ -33,14 +51,21 @@ class BrevoConnector:
         params = {"templateStatus": str(active_only).lower(), "offset": offset}
         return self.client.request("GET", url, headers=self.headers, params=params)
 
-    def get_email_event_report(self, template_id: int, event: str, start_date: str, end_date: str, offset: int = 0):
+    def get_email_event_report(
+        self,
+        template_id: int,
+        event: str,
+        start_date: str,
+        end_date: str,
+        offset: int = 0,
+    ):
         url = f"{self.BASE_URL}/smtp/statistics/events"
         params = {
             "templateId": template_id,
             "event": event,
             "startDate": start_date,
             "endDate": end_date,
-            "offset": offset
+            "offset": offset,
         }
         return self.client.request("GET", url, headers=self.headers, params=params)
 
@@ -49,6 +74,7 @@ class AsyncBrevoConnector:
     """
     Brevo API wrapper using AsyncHttpClient for non-blocking API interactions.
     """
+
     BASE_URL = "https://api.brevo.com/v3"
 
     def __init__(self, api_key: str, client: Optional[AsyncHttpClient] = None):
@@ -57,26 +83,41 @@ class AsyncBrevoConnector:
         self.headers = {
             "api-key": self.api_key,
             "Content-Type": "application/json",
-            "Accept": "application/json"
+            "Accept": "application/json",
         }
 
-    async def get_email_campaigns(self, status: str = "sent", limit: int = 50, offset: int = 0):
+    async def get_email_campaigns(
+        self, status: str = "sent", limit: int = 50, offset: int = 0
+    ):
         url = f"{self.BASE_URL}/emailCampaigns"
         params = {"status": status, "limit": limit, "offset": offset}
-        return await self.client.request("GET", url, headers=self.headers, params=params)
+        return await self.client.request(
+            "GET", url, headers=self.headers, params=params
+        )
 
     async def get_smtp_templates(self, active_only: bool = True, offset: int = 0):
         url = f"{self.BASE_URL}/smtp/templates"
         params = {"templateStatus": str(active_only).lower(), "offset": offset}
-        return await self.client.request("GET", url, headers=self.headers, params=params)
+        return await self.client.request(
+            "GET", url, headers=self.headers, params=params
+        )
 
-    async def get_email_event_report(self, template_id: int, event: str, start_date: str, end_date: str, offset: int = 0):
+    async def get_email_event_report(
+        self,
+        template_id: int,
+        event: str,
+        start_date: str,
+        end_date: str,
+        offset: int = 0,
+    ):
         url = f"{self.BASE_URL}/smtp/statistics/events"
         params = {
             "templateId": template_id,
             "event": event,
             "startDate": start_date,
             "endDate": end_date,
-            "offset": offset
+            "offset": offset,
         }
-        return await self.client.request("GET", url, headers=self.headers, params=params)
+        return await self.client.request(
+            "GET", url, headers=self.headers, params=params
+        )
--- jobs/etl_jobs/jobs/brevo/main.py
+++ jobs/etl_jobs/jobs/brevo/main.py
@@ -1,22 +1,21 @@
-from datetime import datetime, timedelta, timezone
-import typer
-import pandas as pd
-from google.cloud import bigquery
 import logging
+from datetime import datetime, timedelta, timezone
 
+import pandas as pd
+import typer
 from connectors.brevo import BrevoConnector
+from google.cloud import bigquery
 from http_custom.clients import SyncHttpClient
 from jobs.brevo.utils import (
-    GCP_PROJECT,
     BIGQUERY_RAW_DATASET,
     BIGQUERY_TMP_DATASET,
+    ENV_SHORT_NAME,
+    GCP_PROJECT,
+    SyncBrevoHeaderRateLimiter,
     access_secret_data,
     campaigns_histo_schema,
     transactional_histo_schema,
-    ENV_SHORT_NAME,
 )
-
-from jobs.brevo.utils import SyncBrevoHeaderRateLimiter
 
 # Logging setup
 logging.basicConfig(level=logging.INFO)
@@ -25,9 +24,12 @@ logger = logging.getLogger(__name__)
 app = typer.Typer()
 
 TRANSACTIONAL_TABLE_NAME = "sendinblue_transactional_detailed"
-UPDATE_WINDOW = 7 if ENV_SHORT_NAME == "prod" else 14
+UPDATE_WINDOW = 31 if ENV_SHORT_NAME == "prod" else 500
 
-def save_df_to_bq(df: pd.DataFrame, table_id: str, schema: dict, partition_field: str = "update_date"):
+
+def save_df_to_bq(
+    df: pd.DataFrame, table_id: str, schema: dict, partition_field: str = "update_date"
+):
     logger.info(f"Saving dataframe to BigQuery: {table_id}")
     client = bigquery.Client()
     job_config = bigquery.LoadJobConfig(
@@ -37,7 +39,8 @@ def save_df_to_bq(df: pd.DataFrame, table_id: str, schema: dict, partition_field
             field=partition_field,
         ),
         schema=[
-            bigquery.SchemaField(name, field_type) for name, field_type in schema.items()
+            bigquery.SchemaField(name, field_type)
+            for name, field_type in schema.items()
         ],
         schema_update_options=[
             bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION,
@@ -48,24 +51,83 @@ def save_df_to_bq(df: pd.DataFrame, table_id: str, schema: dict, partition_field
     logger.info("Upload to BigQuery completed.")
 
 
+def save_to_historical(
+    df: pd.DataFrame,
+    schema: dict,
+    project: str,
+    dataset: str,
+    table_name: str,
+    end_date: datetime,
+):
+    """
+    Upload the DataFrame to BigQuery using partitioned table naming conventions.
+
+    Args:
+        df (pd.DataFrame): DataFrame à écrire.
+        schema (dict): Schéma BigQuery, ex: {"field": "STRING"}.
+        project (str): Nom du projet GCP.
+        dataset (str): Dataset BigQuery.
+        table_name (str): Nom de base de la table.
+        end_date (datetime): Date utilisée pour le partitionnement.
+    """
+    client = bigquery.Client()
+    yyyymmdd = end_date.strftime("%Y%m%d")
+    table_id = f"{project}.{dataset}.{table_name}_histo${yyyymmdd}"
+
+    job_config = bigquery.LoadJobConfig(
+        write_disposition="WRITE_TRUNCATE",
+        time_partitioning=bigquery.TimePartitioning(
+            type_=bigquery.TimePartitioningType.DAY, field="update_date"
+        ),
+        schema=[
+            bigquery.SchemaField(name, field_type)
+            for name, field_type in schema.items()
+        ],
+        schema_update_options=[
+            bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION,
+        ],
+    )
+
+    logger.info(f"Saving {len(df)} rows to {table_id}...")
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+    job.result()
+    logger.info("Upload completed.")
+
+
 @app.command()
 def run(
-    target: str = typer.Option(..., help="Nom de la tache (newsletter ou transactional)"),
+    target: str = typer.Option(
+        ..., help="Nom de la tache (newsletter ou transactional)"
+    ),
     audience: str = typer.Option(..., help="Nom de l'audience (native ou pro)"),
     start_date: str = typer.Option(None, help="Date de début d'import (YYYY-MM-DD)"),
     end_date: str = typer.Option(None, help="Date de fin d'import (YYYY-MM-DD)"),
 ):
     today = datetime.now(tz=timezone.utc)
-    start_dt = datetime.strptime(start_date, "%Y-%m-%d") if start_date else (today - timedelta(days=UPDATE_WINDOW))
+    start_dt = (
+        datetime.strptime(start_date, "%Y-%m-%d")
+        if start_date
+        else (today - timedelta(days=UPDATE_WINDOW))
+    )
+    start_dt_newletter = datetime.strptime(
+        today - timedelta(days=UPDATE_WINDOW), "%Y-%m-%d"
+    )
     end_dt = datetime.strptime(end_date, "%Y-%m-%d") if end_date else today
+    end_dt_newsletter = datetime.strptime(today, "%Y-%m-%d")
 
-    logger.info(f"Running Brevo ETL with target={target}, audience={audience}, start={start_dt}, end={end_dt}")
+    logger.info(
+        f"Running Brevo ETL with target={target}, audience={audience}, start={start_dt}, end={end_dt}"
+    )
 
     if audience == "native":
-        api_key = access_secret_data(GCP_PROJECT, f"sendinblue-api-key-{ENV_SHORT_NAME}")
+        api_key = access_secret_data(
+            GCP_PROJECT, f"sendinblue-api-key-{ENV_SHORT_NAME}"
+        )
         table_name = "sendinblue_newsletters"
     elif audience == "pro":
-        api_key = access_secret_data(GCP_PROJECT, f"sendinblue-pro-api-key-{ENV_SHORT_NAME}")
+        api_key = access_secret_data(
+            GCP_PROJECT, f"sendinblue-pro-api-key-{ENV_SHORT_NAME}"
+        )
         table_name = "sendinblue_pro_newsletters"
     else:
         typer.echo("Invalid audience. Must be one of native/pro.")
@@ -77,7 +139,9 @@ def run(
 
     if target == "newsletter":
         logger.info("Fetching email campaigns...")
-        resp = connector.get_email_campaigns()
+        resp = connector.get_email_campaigns(
+            start_date=start_dt_newletter, end_date=end_dt_newsletter
+        )
         if not resp:
             typer.echo("No campaigns fetched.")
             return
@@ -85,25 +149,43 @@ def run(
         campaigns = resp.json().get("campaigns", [])
         logger.info(f"Fetched {len(campaigns)} campaigns")
 
-        df = pd.DataFrame([
-            {
-                "campaign_id": c.get("id"),
-                "campaign_utm": c.get("tag"),
-                "campaign_name": c.get("name"),
-                "campaign_sent_date": c.get("sentDate"),
-                "share_link": c.get("shareLink"),
-                "audience_size": c.get("statistics", {}).get("globalStats", {}).get("delivered"),
-                "unsubscriptions": c.get("statistics", {}).get("globalStats", {}).get("unsubscriptions"),
-                "open_number": c.get("statistics", {}).get("globalStats", {}).get("uniqueViews"),
-                "update_date": pd.to_datetime("today"),
-            }
-            for c in campaigns
-        ])
+        df = pd.DataFrame(
+            [
+                {
+                    "campaign_id": c.get("id"),
+                    "campaign_utm": c.get("tag"),
+                    "campaign_name": c.get("name"),
+                    "campaign_sent_date": c.get("sentDate"),
+                    "share_link": c.get("shareLink"),
+                    "audience_size": c.get("statistics", {})
+                    .get("globalStats", {})
+                    .get("delivered"),
+                    "unsubscriptions": c.get("statistics", {})
+                    .get("globalStats", {})
+                    .get("unsubscriptions"),
+                    "open_number": c.get("statistics", {})
+                    .get("globalStats", {})
+                    .get("uniqueViews"),
+                    "update_date": pd.to_datetime("today"),
+                }
+                for c in campaigns
+            ]
+        )
         df["campaign_target"] = audience
 
-        partition_date = end_dt.strftime("%Y%m%d")
-        table_id = f"{GCP_PROJECT}.{BIGQUERY_RAW_DATASET}.{table_name}_histo${partition_date}"
-        save_df_to_bq(df, table_id, campaigns_histo_schema)
+        # partition_date = end_dt.strftime("%Y%m%d")
+        # table_id = (
+        #     f"{GCP_PROJECT}.{BIGQUERY_RAW_DATASET}.{table_name}_histo${partition_date}"
+        # )
+        # save_df_to_bq(df, table_id, campaigns_histo_schema)
+        save_to_historical(
+            df,
+            campaigns_histo_schema,
+            GCP_PROJECT,
+            BIGQUERY_RAW_DATASET,
+            table_name,
+            end_dt,
+        )
 
     elif target == "transactional":
         logger.info("Fetching active SMTP templates...")
@@ -123,13 +205,15 @@ def run(
             for event_type in ["delivered", "opened", "unsubscribed"]:
                 offset = 0
                 while True:
-                    logger.info(f"Fetching events: template={template_id}, event={event_type}, offset={offset}")
+                    logger.info(
+                        f"Fetching events: template={template_id}, event={event_type}, offset={offset}"
+                    )
                     events_resp = connector.get_email_event_report(
                         template_id=template_id,
                         event=event_type,
                         start_date=start_dt.strftime("%Y-%m-%d"),
                         end_date=end_dt.strftime("%Y-%m-%d"),
-                        offset=offset
+                        offset=offset,
                     )
                     if not events_resp:
                         logger.warning("No response received. Skipping.")
@@ -141,13 +225,15 @@ def run(
                         break
 
                     for event in events:
-                        all_events.append({
-                            "template": template_id,
-                            "tag": tag,
-                            "email": event.get("email"),
-                            "event": event.get("event"),
-                            "event_date": pd.to_datetime(event.get("date")).date(),
-                        })
+                        all_events.append(
+                            {
+                                "template": template_id,
+                                "tag": tag,
+                                "email": event.get("email"),
+                                "event": event.get("event"),
+                                "event_date": pd.to_datetime(event.get("date")).date(),
+                            }
+                        )
 
                     if len(events) < 2500:
                         break
@@ -160,7 +246,9 @@ def run(
         df = pd.DataFrame(all_events)
         logger.info(f"Collected {len(df)} events")
 
-        df_kpis = df.groupby(["tag", "template", "email", "event", "event_date"]).agg({"event_date": "count"})
+        df_kpis = df.groupby(["tag", "template", "email", "event", "event_date"]).agg(
+            {"event_date": "count"}
+        )
         df_kpis.columns = ["count"]
         df_kpis.reset_index(inplace=True)
 
@@ -177,17 +265,22 @@ def run(
             if col not in df_pivot:
                 df_pivot[col] = 0
 
-        df_pivot.rename(columns={
-            "delivered": "delivered_count",
-            "opened": "opened_count",
-            "unsubscribed": "unsubscribed_count"
-        }, inplace=True)
+        df_pivot.rename(
+            columns={
+                "delivered": "delivered_count",
+                "opened": "opened_count",
+                "unsubscribed": "unsubscribed_count",
+            },
+            inplace=True,
+        )
 
         df_pivot["target"] = audience
 
         yyyymmdd = end_dt.strftime("%Y%m%d")
         table_id = f"{GCP_PROJECT}.{BIGQUERY_TMP_DATASET}.{yyyymmdd}_{TRANSACTIONAL_TABLE_NAME}_histo"
-        save_df_to_bq(df_pivot, table_id, transactional_histo_schema, partition_field="event_date")
+        save_df_to_bq(
+            df_pivot, table_id, transactional_histo_schema, partition_field="event_date"
+        )
 
     else:
         typer.echo("Invalid target. Must be one of transactional/newsletter.")

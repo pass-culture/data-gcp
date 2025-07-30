e7cc940bd2c6a085b75400b841dd8d632c9dc64f
dcuesta-pass <daniel.cuesta@passculture.app>
2025-07-30 10:27:11 +0200

    refactor: Simplify SQL query structure and improve readability

--- jobs/ml_jobs/reco_integration_test/sql/user_data.sql
+++ jobs/ml_jobs/reco_integration_test/sql/user_data.sql
@@ -1,72 +1,97 @@
 -- get_user_data.sql
-WITH user_sample AS (
-    SELECT DISTINCT fe.user_id
-    FROM `passculture-data-prod.analytics_prod.native_event` fe
-    WHERE event_date > DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH)
-    AND event_name = 'ConsultOffer'
-    AND user_id IS NOT NULL
-    LIMIT 1000
-),
-users_clics AS (
-    SELECT us.user_id, CAST(fe.offer_id AS STRING) AS offer_id
-    FROM user_sample us
-    JOIN `passculture-data-prod.analytics_prod.native_event` fe
-        ON fe.user_id = us.user_id
-    WHERE event_date > DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH)
-    AND event_name = 'ConsultOffer'
-),
-base_booking AS (
-    SELECT eud.user_id, CAST(ebd.offer_id AS STRING) AS offer_id
-    FROM (SELECT DISTINCT user_id FROM user_sample) eud
-    JOIN `passculture-data-prod.analytics_prod.global_booking` ebd
-        ON ebd.user_id = eud.user_id
-),
-base_interaction AS (
-    SELECT *, "booking" AS event_name
-    FROM base_booking
-    UNION ALL
-    SELECT *, "clics" AS event_name
-    FROM users_clics
-),
-enriched_interaction AS (
-    SELECT
-        bi.user_id, eom.offer_subcategory_id,
-        eom.offer_type_label, eom.offer_sub_type_label,
-        event_name, COUNT(*) AS subcat_count
-    FROM base_interaction bi
-    JOIN `passculture-data-prod.analytics_prod.global_offer_metadata` eom
-        ON eom.offer_id = bi.offer_id
-    WHERE eom.offer_subcategory_id IN ("LIVRE_PAPIER", "SUPPORT_PHYSIQUE_MUSIQUE_CD", "SEANCE_CINE")
-    GROUP BY bi.user_id, offer_subcategory_id, eom.offer_type_label, eom.offer_sub_type_label, event_name
-),
-user_subcategory_count AS (
-    SELECT user_id, offer_subcategory_id, SUM(subcat_count) AS total_count
-    FROM enriched_interaction
-    GROUP BY user_id, offer_subcategory_id
-),
-user_total_count AS (
-    SELECT user_id, SUM(total_count) AS total_bookings
-    FROM user_subcategory_count
-    GROUP BY user_id
-),
-user_top_subcategory AS (
-    SELECT
-        us.user_id, us.offer_subcategory_id, us.total_count,
-        ut.total_bookings, (us.total_count / ut.total_bookings) AS subcategory_ratio
-    FROM user_subcategory_count us
-    JOIN user_total_count ut ON us.user_id = ut.user_id
-    WHERE (us.total_count / ut.total_bookings) >= 0.5
-    AND us.offer_subcategory_id IN ("LIVRE_PAPIER", "SUPPORT_PHYSIQUE_MUSIQUE_CD", "SEANCE_CINE")
-),
-ranked_users AS (
-    SELECT
-        uts.user_id, uts.offer_subcategory_id, uts.total_count,
-        uts.subcategory_ratio,
-        ROW_NUMBER() OVER (PARTITION BY uts.offer_subcategory_id ORDER BY uts.total_count DESC) AS rank
-    FROM user_top_subcategory uts
-)
-SELECT
-    ru.user_id, ru.offer_subcategory_id, ru.total_count, ru.subcategory_ratio
-FROM ranked_users ru
-WHERE ru.rank <= 1000
-ORDER BY ru.offer_subcategory_id, ru.total_count DESC;
+with
+    user_sample as (
+        select distinct fe.user_id
+        from `passculture-data-prod.analytics_prod.native_event` fe
+        where
+            event_date > date_sub(current_date(), interval 2 month)
+            and event_name = 'ConsultOffer'
+            and user_id is not null
+        limit 1000
+    ),
+    users_clics as (
+        select us.user_id, cast(fe.offer_id as string) as offer_id
+        from user_sample us
+        join
+            `passculture-data-prod.analytics_prod.native_event` fe
+            on fe.user_id = us.user_id
+        where
+            event_date > date_sub(current_date(), interval 2 month)
+            and event_name = 'ConsultOffer'
+    ),
+    base_booking as (
+        select eud.user_id, cast(ebd.offer_id as string) as offer_id
+        from (select distinct user_id from user_sample) eud
+        join
+            `passculture-data-prod.analytics_prod.global_booking` ebd
+            on ebd.user_id = eud.user_id
+    ),
+    base_interaction as (
+        select *, "booking" as event_name
+        from base_booking
+        union all
+        select *, "clics" as event_name
+        from users_clics
+    ),
+    enriched_interaction as (
+        select
+            bi.user_id,
+            eom.offer_subcategory_id,
+            eom.offer_type_label,
+            eom.offer_sub_type_label,
+            event_name,
+            count(*) as subcat_count
+        from base_interaction bi
+        join
+            `passculture-data-prod.analytics_prod.global_offer_metadata` eom
+            on eom.offer_id = bi.offer_id
+        where
+            eom.offer_subcategory_id
+            in ("LIVRE_PAPIER", "SUPPORT_PHYSIQUE_MUSIQUE_CD", "SEANCE_CINE")
+        group by
+            bi.user_id,
+            offer_subcategory_id,
+            eom.offer_type_label,
+            eom.offer_sub_type_label,
+            event_name
+    ),
+    user_subcategory_count as (
+        select user_id, offer_subcategory_id, sum(subcat_count) as total_count
+        from enriched_interaction
+        group by user_id, offer_subcategory_id
+    ),
+    user_total_count as (
+        select user_id, sum(total_count) as total_bookings
+        from user_subcategory_count
+        group by user_id
+    ),
+    user_top_subcategory as (
+        select
+            us.user_id,
+            us.offer_subcategory_id,
+            us.total_count,
+            ut.total_bookings,
+            (us.total_count / ut.total_bookings) as subcategory_ratio
+        from user_subcategory_count us
+        join user_total_count ut on us.user_id = ut.user_id
+        where
+            (us.total_count / ut.total_bookings) >= 0.5
+            and us.offer_subcategory_id
+            in ("LIVRE_PAPIER", "SUPPORT_PHYSIQUE_MUSIQUE_CD", "SEANCE_CINE")
+    ),
+    ranked_users as (
+        select
+            uts.user_id,
+            uts.offer_subcategory_id,
+            uts.total_count,
+            uts.subcategory_ratio,
+            row_number() over (
+                partition by uts.offer_subcategory_id order by uts.total_count desc
+            ) as rank
+        from user_top_subcategory uts
+    )
+select ru.user_id, ru.offer_subcategory_id, ru.total_count, ru.subcategory_ratio
+from ranked_users ru
+where ru.rank <= 1000
+order by ru.offer_subcategory_id, ru.total_count desc
+;

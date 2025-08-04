3ce689ff2e40736d81dc8be49f5f599776fb4edf
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-08-04 17:10:54 +0200

    (HF) fix(dbt): inject scheduled date in SQL from airflow context python

--- orchestration/dags/data_gcp_dbt/macros/airflow/is_first_day_of_month.sql
+++ orchestration/dags/data_gcp_dbt/macros/airflow/is_first_day_of_month.sql
@@ -1,10 +1,10 @@
 {%- macro is_first_day_of_month() -%}
-    {%- set execution_date = var(
-        "execution_date", dbt_airflow_macros.execution_date()
+    {%- set scheduled_date = var(
+        "scheduled_date", dbt_airflow_macros.execution_date()
     ) -%}
-    {{ print("execution_date: " ~ execution_date) }}
-    {%- if execution_date is string -%} {%- set formatted_date = execution_date -%}
-    {%- else -%} {%- set formatted_date = execution_date.strftime("%Y-%m-%d") -%}
+    {{ print("scheduled_date: " ~ scheduled_date) }}
+    {%- if scheduled_date is string -%} {%- set formatted_date = scheduled_date -%}
+    {%- else -%} {%- set formatted_date = scheduled_date.strftime("%Y-%m-%d") -%}
     {%- endif -%}
     {%- if formatted_date[-2:] == "01" -%}
         {{ print("Macro is_first_day_of_month evalutate to TRUE") }}

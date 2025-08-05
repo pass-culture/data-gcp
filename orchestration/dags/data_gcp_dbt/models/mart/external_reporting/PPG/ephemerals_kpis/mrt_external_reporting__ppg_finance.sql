020beddf8bae224b0d86ac7e2cba6c22222e7186
Julpass <jules.arbelot@passculture.app>
2025-08-05 15:56:33 +0200

    sqlfmt

--- orchestration/dags/data_gcp_dbt/models/mart/external_reporting/PPG/ephemerals_kpis/mrt_external_reporting__ppg_finance.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/external_reporting/PPG/ephemerals_kpis/mrt_external_reporting__ppg_finance.sql
@@ -27,8 +27,14 @@
     {"name": "total_reservations", "value_expr": "total_bookings"},
     {"name": "total_quantites", "value_expr": "total_quantities"},
     {"name": "total_ca", "value_expr": "total_revenue_amount"},
-    {"name": "total_montant_rembourse", "value_expr": "total_reimbursed_amount"},
-    {"name": "total_montant_contribution", "value_expr": "total_contribution_amount"},
+    {
+        "name": "total_montant_rembourse",
+        "value_expr": "total_reimbursed_amount",
+    },
+    {
+        "name": "total_montant_contribution",
+        "value_expr": "total_contribution_amount",
+    },
 ] %}
 
 
@@ -40,37 +46,35 @@
         {% if not loop.first %}
             union all
         {% endif %}
-    select
-        date_trunc(date(booking_used_date), month) as execution_date,
-        date("{{ ds() }}") as update_date,
-        '{{ dim.name }}' as dimension_name,
-        {{ dim.value_expr }} as dimension_value,
-        '{{ kpi.name }}' as kpi_name,
-        sum({{ kpi.value_expr }}) as numerator,
-        1 as denominator,
-        safe_divide(
-            sum({{ kpi.value_expr }}), 1
-        ) as kpi
-    from {{ ref("mrt_finance__reimbursement") }}
-    where
-        1=1
-        {% if is_incremental() %}
-            and date_trunc(booking_used_date, month)
-            = date_trunc(date("{{ ds() }}"), month)
-        {% endif %}
-    group by execution_date, update_date, dimension_name, dimension_value, kpi_name
+        select
+            date_trunc(date(booking_used_date), month) as execution_date,
+            date("{{ ds() }}") as update_date,
+            '{{ dim.name }}' as dimension_name,
+            {{ dim.value_expr }} as dimension_value,
+            '{{ kpi.name }}' as kpi_name,
+            sum({{ kpi.value_expr }}) as numerator,
+            1 as denominator,
+            safe_divide(sum({{ kpi.value_expr }}), 1) as kpi
+        from {{ ref("mrt_finance__reimbursement") }}
+        where
+            1 = 1
+            {% if is_incremental() %}
+                and date_trunc(booking_used_date, month)
+                = date_trunc(date("{{ ds() }}"), month)
+            {% endif %}
+        group by execution_date, update_date, dimension_name, dimension_value, kpi_name
     {% endfor %}
 {% endfor %}
 {% for category in categories %}
-        union all
+    union all
     {% for dim in dimensions %}
         {% if not loop.first %}
             union all
         {% endif %}
         {% for kpi in kpis %}
-        {% if not loop.first %}
-            union all
-        {% endif %}
+            {% if not loop.first %}
+                union all
+            {% endif %}
             select
                 date_trunc(date(booking_used_date), month) as execution_date,
                 date("{{ ds() }}") as update_date,
@@ -79,17 +83,16 @@
                 '{{ kpi.name }}_{{ category.value_expr }}' as kpi_name,
                 sum({{ kpi.value_expr }}) as numerator,
                 1 as denominator,
-                safe_divide(
-                    sum({{ kpi.value_expr }}), 1
-                ) as kpi
+                safe_divide(sum({{ kpi.value_expr }}), 1) as kpi
             from {{ ref("mrt_finance__reimbursement") }}
             where
                 offer_category_id = '{{ category.name }}'
-            {% if is_incremental() %}
-                and date_trunc(booking_used_date, month)
-                = date_trunc(date("{{ ds() }}"), month)
-            {% endif %}
-            group by execution_date, update_date, dimension_name, dimension_value, kpi_name
+                {% if is_incremental() %}
+                    and date_trunc(booking_used_date, month)
+                    = date_trunc(date("{{ ds() }}"), month)
+                {% endif %}
+            group by
+                execution_date, update_date, dimension_name, dimension_value, kpi_name
         {% endfor %}
     {% endfor %}
 {% endfor %}

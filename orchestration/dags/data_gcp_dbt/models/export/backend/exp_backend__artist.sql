1ea0c0e53cf42fe7d399554df1fdfc63cea02742
lmontier-pass <172769973+lmontier-pass@users.noreply.github.com>
2025-08-11 12:43:14 +0200

    (BSR) fix(artist): Fix artist tables loading into dbt (#4369)
    * fix: Fix dbt artist tables
    
    * fix: Fix dbt artist_alias tables
    
    * fix: Fix dbt product_artist_link tables

--- orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__artist.sql
+++ orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__artist.sql
@@ -2,8 +2,9 @@ select
     artist_id,
     artist_name,
     artist_description,
+    wikidata_id,
     wikidata_image_file_url,
     wikidata_image_author,
     wikidata_image_license,
-    wikidata_image_license_url,
+    wikidata_image_license_url
 from {{ ref("ml_linkage__artist") }}

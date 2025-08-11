bb59eb0395c586c726fe80d055674f0c0e03e90e
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-08-11 12:32:41 +0200

    fix: Fix dbt artist_alias tables

--- orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__artist_alias.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__artist_alias.sql
@@ -2,10 +2,10 @@
 
 select distinct
     artist_id,
-    cluster_id as artist_cluster_id,
     wiki_id as artist_wiki_id,
     offer_category_id,
     artist_type,
-    artist_name as artist_offer_name
-from {{ source("ml_preproc", "artist_linked") }}
+    artist_name as artist_offer_name,
+    artist_name_to_match
+from {{ source("ml_preproc", "artist_alias") }}
 where artist_id is not null

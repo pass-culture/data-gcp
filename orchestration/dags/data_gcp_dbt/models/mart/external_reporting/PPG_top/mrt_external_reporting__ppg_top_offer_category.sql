1c4807cb3b5548d35eeafe96869950ad39c67cbb
Julpass <jules.arbelot@passculture.app>
2025-08-11 16:28:15 +0200

    sqlfmt

--- orchestration/dags/data_gcp_dbt/models/mart/external_reporting/PPG_top/mrt_external_reporting__ppg_top_offer_category.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/external_reporting/PPG_top/mrt_external_reporting__ppg_top_offer_category.sql
@@ -18,39 +18,49 @@
     {% if not loop.first %}
         union all
     {% endif %}
-SELECT
-    date_trunc(date(booking_used_date), MONTH) AS execution_date,
-    date("{{ ds() }}") as update_date,
-    '{{ dim.name }}' as dimension_name,
-    {{ dim.value_expr }} as dimension_value,
-    item_id,
-    offer_category_id,
-    offer_subcategory_id,
-    ANY_VALUE(offer_name) AS offer_name,
-    SUM(booking_intermediary_amount) AS total_booking_amount,
-    SUM(booking_quantity) AS total_booking_quantity,
-    ROW_NUMBER() OVER(PARTITION BY offer_category_id ORDER BY SUM(booking_intermediary_amount) DESC) as total_booking_amount_ranked
-FROM {{ ref('mrt_global__booking') }}
-WHERE booking_is_used
-{% if is_incremental() %}
-    AND date_trunc(date(booking_used_date), MONTH) = date_trunc(date("{{ ds() }}"), month)
-{% endif %}
-GROUP BY
-    execution_date,
-    update_date,
-    dimension_name,
-    dimension_value,
-    item_id,
-    offer_category_id,
-    offer_subcategory_id
-ORDER BY
-    execution_date,
-    dimension_name,
-    dimension_value,
-    offer_category_id,
-    total_booking_amount_ranked,
-    total_booking_amount,
-    total_booking_quantity
+    select
+        date_trunc(date(booking_used_date), month) as execution_date,
+        date("{{ ds() }}") as update_date,
+        '{{ dim.name }}' as dimension_name,
+        {{ dim.value_expr }} as dimension_value,
+        item_id,
+        offer_category_id,
+        offer_subcategory_id,
+        any_value(offer_name) as offer_name,
+        sum(booking_intermediary_amount) as total_booking_amount,
+        sum(booking_quantity) as total_booking_quantity,
+        row_number() over (
+            partition by offer_category_id
+            order by sum(booking_intermediary_amount) desc
+        ) as total_booking_amount_ranked
+    from {{ ref("mrt_global__booking") }}
+    where
+        booking_is_used
+        {% if is_incremental() %}
+            and date_trunc(date(booking_used_date), month)
+            = date_trunc(date("{{ ds() }}"), month)
+        {% endif %}
+    group by
+        execution_date,
+        update_date,
+        dimension_name,
+        dimension_value,
+        item_id,
+        offer_category_id,
+        offer_subcategory_id
+    order by
+        execution_date,
+        dimension_name,
+        dimension_value,
+        offer_category_id,
+        total_booking_amount_ranked,
+        total_booking_amount,
+        total_booking_quantity
 
-QUALIFY ROW_NUMBER() OVER(PARTITION BY offer_category_id ORDER BY SUM(booking_intermediary_amount) DESC) <= 50
+    qualify
+        row_number() over (
+            partition by offer_category_id
+            order by sum(booking_intermediary_amount) desc
+        )
+        <= 50
 {% endfor %}

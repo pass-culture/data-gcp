05ff38030c41f51facbb33ef0f8c7939ada63a52
Julpass <jules.arbelot@passculture.app>
2025-08-12 14:02:04 +0200

    sqlfmt

--- orchestration/dags/data_gcp_dbt/models/mart/external_reporting/PPG_top/mrt_external_reporting__ppg_top_offer_category.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/external_reporting/PPG_top/mrt_external_reporting__ppg_top_offer_category.sql
@@ -15,74 +15,74 @@
 
 {% for dim in dimensions %}
     {% if loop.first %}
-    with
+        with
     {% endif %}
-    cte_{{ dim.name }} as (
+        cte_{{ dim.name }} as (
+            select
+                date_trunc(date(booking_used_date), month) as execution_date,
+                date("{{ ds() }}") as update_date,
+                '{{ dim.name }}' as dimension_name,
+                {{ dim.value_expr }} as dimension_value,
+                item_id,
+                offer_category_id,
+                offer_subcategory_id,
+                any_value(offer_name) as offer_name,
+                sum(booking_intermediary_amount) as total_booking_amount,
+                sum(booking_quantity) as total_booking_quantity,
+                row_number() over (
+                    partition by offer_category_id
+                    order by sum(booking_intermediary_amount) desc
+                ) as total_booking_amount_ranked
+            from {{ ref("mrt_global__booking") }}
+            where
+                booking_is_used
+                {% if is_incremental() %}
+                    and date_trunc(date(booking_used_date), month)
+                    = date_trunc(date("{{ ds() }}"), month)
+                {% endif %}
+            group by
+                execution_date,
+                update_date,
+                dimension_name,
+                dimension_value,
+                item_id,
+                offer_category_id,
+                total_booking_amount_ranked
+
+            qualify
+                row_number() over (
+                    partition by offer_category_id
+                    order by sum(booking_intermediary_amount) desc
+                )
+                <= 50
+
+            order by
+                execution_date,
+                dimension_name,
+                dimension_value,
+                offer_category_id,
+                total_booking_amount_ranked,
+                total_booking_amount,
+                total_booking_quantity
+        )
+        {% if not loop.last %}, {% endif %}
+{% endfor %}
+
+{% for dim in dimensions %}
+    {% if not loop.first %}
+        union all
+    {% endif %}
     select
-        date_trunc(date(booking_used_date), month) as execution_date,
-        date("{{ ds() }}") as update_date,
-        '{{ dim.name }}' as dimension_name,
-        {{ dim.value_expr }} as dimension_value,
-        item_id,
-        offer_category_id,
-        offer_subcategory_id,
-        any_value(offer_name) as offer_name,
-        sum(booking_intermediary_amount) as total_booking_amount,
-        sum(booking_quantity) as total_booking_quantity,
-        row_number() over (
-            partition by offer_category_id
-            order by sum(booking_intermediary_amount) desc
-        ) as total_booking_amount_ranked
-    from {{ ref("mrt_global__booking") }}
-    where
-        booking_is_used
-        {% if is_incremental() %}
-            and date_trunc(date(booking_used_date), month)
-            = date_trunc(date("{{ ds() }}"), month)
-        {% endif %}
-    group by
         execution_date,
         update_date,
         dimension_name,
         dimension_value,
         item_id,
         offer_category_id,
-        total_booking_amount_ranked
-
-    qualify
-        row_number() over (
-            partition by offer_category_id
-            order by sum(booking_intermediary_amount) desc
-        )
-        <= 50
-
-    order by
-        execution_date,
-        dimension_name,
-        dimension_value,
-        offer_category_id,
-        total_booking_amount_ranked,
+        offer_subcategory_id,
+        offer_name,
         total_booking_amount,
-        total_booking_quantity
-    ){% if not loop.last %} , {% endif %}
+        total_booking_quantity,
+        total_booking_amount_ranked
+    from cte_{{ dim.name }}
 {% endfor %}
-
-{% for dim in dimensions %}
-{% if not loop.first %}
-    union all
-{% endif %}
-select 
-    execution_date,
-    update_date,
-    dimension_name,
-    dimension_value,
-    item_id,
-    offer_category_id,
-    offer_subcategory_id,
-    offer_name,
-    total_booking_amount,
-    total_booking_quantity,
-    total_booking_amount_ranked
-from cte_{{ dim.name }}
-{% endfor %}
-

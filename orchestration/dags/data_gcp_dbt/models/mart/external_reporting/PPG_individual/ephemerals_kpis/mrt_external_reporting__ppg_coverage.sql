8b12850a5a29f5709f6b0ba56313e6f4eb1ef7f9
Julpass <jules.arbelot@passculture.app>
2025-08-14 15:18:58 +0200

    add coverage 15 and 16

--- orchestration/dags/data_gcp_dbt/models/mart/external_reporting/PPG_individual/ephemerals_kpis/mrt_external_reporting__ppg_coverage.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/external_reporting/PPG_individual/ephemerals_kpis/mrt_external_reporting__ppg_coverage.sql
@@ -59,4 +59,46 @@
             = date_trunc(date_sub(date("{{ ds() }}"), interval 1 month), month)
         {% endif %}
     group by partition_month, update_date, dimension_name, dimension_value, kpi_name
+    union all
+    select
+        date_trunc(population_snapshot_month, month) as partition_month,
+        date("{{ ds() }}") as update_date,
+        '{{ dim.name }}' as dimension_name,
+        {{ dim.value_expr }} as dimension_value,
+        "taux_couverture_16" as kpi_name,
+        sum(total_users_last_12_months) as numerator,
+        sum(total_population_last_12_months) as denominator,
+        safe_divide(
+            sum(total_users_last_12_months), sum(total_population_last_12_months)
+        ) as kpi
+    from {{ ref("mrt_native__monthly_beneficiary_coverage") }}
+    where
+        population_decimal_age = "17"
+        and population_snapshot_month <= date_trunc(current_date, month)
+        {% if is_incremental() %}
+            and date_trunc(population_snapshot_month, month)
+            = date_trunc(date_sub(date("{{ ds() }}"), interval 1 month), month)
+        {% endif %}
+    group by partition_month, update_date, dimension_name, dimension_value, kpi_name
+    union all
+    select
+        date_trunc(population_snapshot_month, month) as partition_month,
+        date("{{ ds() }}") as update_date,
+        '{{ dim.name }}' as dimension_name,
+        {{ dim.value_expr }} as dimension_value,
+        "taux_couverture_15" as kpi_name,
+        sum(total_users_last_12_months) as numerator,
+        sum(total_population_last_12_months) as denominator,
+        safe_divide(
+            sum(total_users_last_12_months), sum(total_population_last_12_months)
+        ) as kpi
+    from {{ ref("mrt_native__monthly_beneficiary_coverage") }}
+    where
+        population_decimal_age = "16"
+        and population_snapshot_month <= date_trunc(current_date, month)
+        {% if is_incremental() %}
+            and date_trunc(population_snapshot_month, month)
+            = date_trunc(date_sub(date("{{ ds() }}"), interval 1 month), month)
+        {% endif %}
+    group by partition_month, update_date, dimension_name, dimension_value, kpi_name
 {% endfor %}

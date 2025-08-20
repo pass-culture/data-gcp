c7e802c7a5a90a7574c74dae312913b89310f42b
Julpass <jules.arbelot@passculture.app>
2025-08-20 12:49:29 +0200

    sqlfmt

--- orchestration/dags/data_gcp_dbt/models/mart/external_reporting/collective/ephemerals_kpis/mrt_external_reporting__eac_coverage.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/external_reporting/collective/ephemerals_kpis/mrt_external_reporting__eac_coverage.sql
@@ -15,8 +15,16 @@
 ] %}
 
 {% set objects = [
-    {"name": "jeunes", "attribute": "involved_students", "table_name": "adage_involved_student"},
-    {"name": "eple", "attribute": "institutions", "table_name": "adage_involved_institution"},
+    {
+        "name": "jeunes",
+        "attribute": "involved_students",
+        "table_name": "adage_involved_student",
+    },
+    {
+        "name": "eple",
+        "attribute": "institutions",
+        "table_name": "adage_involved_institution",
+    },
 ] %}
 
 with
@@ -34,40 +42,45 @@ with
                     = date_trunc(date_sub(date("{{ ds() }}"), interval 1 month), month)
                 {% endif %}
             group by 1
-        ){% if not loop.last %},{% endif %}
-        {% endfor %}
+        )
+        {% if not loop.last %},{% endif %}
+    {% endfor %}
 
 {% for obj in objects %}
-{% if not loop.first %}
-        union all
-    {% endif %}
-{% for dim in dimensions %}
     {% if not loop.first %}
         union all
     {% endif %}
-    select
-        date_trunc(involved.date, month) as partition_month,
-        date("{{ ds() }}") as update_date,
-        '{{ dim.name }}' as dimension_name,
-        {{ dim.value_expr }} as dimension_value,
-        "taux_participation_eac_{{ obj.name }}" as kpi_name,
-        sum(involved.{{ obj.attribute }}) as numerator,
-        sum(involved.total_{{ obj.attribute }}) as denominator,
-        safe_divide(sum(involved.{{ obj.attribute }}), sum(involved.total_{{ obj.attribute }})) as kpi
-    from {{ ref(obj.table_name) }} as involved
-    -- take only last day for each month.
-    inner join
-        last_day_{{ obj.name }}
-        on involved.date = last_day_{{ obj.name }}.last_date
-        and date_trunc(involved.date, month) = last_day_{{ obj.name }}.partition_month
-        and last_day_{{ obj.name }}.last_adage_id = cast(involved.adage_id as int)
-    left join
-        {{ source("seed", "region_department") }} as rd
-        on involved.department_code = rd.num_dep
-    where
-        1 = 1 and {% if "{{ dim.name }}" == "NAT" %} involved.department_code = '-1'
-        {% else %} not involved.department_code = "-1"
+    {% for dim in dimensions %}
+        {% if not loop.first %}
+            union all
         {% endif %}
-    group by partition_month, update_date, dimension_name, dimension_value, kpi_name
-{% endfor %}
+        select
+            date_trunc(involved.date, month) as partition_month,
+            date("{{ ds() }}") as update_date,
+            '{{ dim.name }}' as dimension_name,
+            {{ dim.value_expr }} as dimension_value,
+            "taux_participation_eac_{{ obj.name }}" as kpi_name,
+            sum(involved.{{ obj.attribute }}) as numerator,
+            sum(involved.total_{{ obj.attribute }}) as denominator,
+            safe_divide(
+                sum(involved.{{ obj.attribute }}),
+                sum(involved.total_{{ obj.attribute }})
+            ) as kpi
+        from {{ ref(obj.table_name) }} as involved
+        -- take only last day for each month.
+        inner join
+            last_day_{{ obj.name }}
+            on involved.date = last_day_{{ obj.name }}.last_date
+            and date_trunc(involved.date, month)
+            = last_day_{{ obj.name }}.partition_month
+            and last_day_{{ obj.name }}.last_adage_id = cast(involved.adage_id as int)
+        left join
+            {{ source("seed", "region_department") }} as rd
+            on involved.department_code = rd.num_dep
+        where
+            1 = 1 and {% if "{{ dim.name }}" == "NAT" %} involved.department_code = '-1'
+            {% else %} not involved.department_code = "-1"
+            {% endif %}
+        group by partition_month, update_date, dimension_name, dimension_value, kpi_name
+    {% endfor %}
 {% endfor %}

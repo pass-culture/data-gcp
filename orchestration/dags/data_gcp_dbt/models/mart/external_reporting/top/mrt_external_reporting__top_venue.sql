f9cc33cb197df0eab349a1096b08621df7778fd7
Julpass <jules.arbelot@passculture.app>
2025-08-27 10:25:57 +0200

    fix mrt_external_reporting_top models

--- orchestration/dags/data_gcp_dbt/models/mart/external_reporting/top/mrt_external_reporting__top_venue.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/external_reporting/top/mrt_external_reporting__top_venue.sql
@@ -58,6 +58,9 @@ with
                 offerer_name,
                 sum(total_venue_booking_amount) as total_venue_booking_amount,
                 row_number() over (
+                    {% if not dim.name == 'NAT' %}
+                    partition by {{ dim.value_expr }}
+                    {% endif %}
                     order by sum(total_venue_booking_amount) desc
                 ) as total_venue_booking_amount_ranked
             from base_aggregation
@@ -69,7 +72,11 @@ with
                 venue_id,
                 venue_name,
                 offerer_name
-            qualify row_number() over (order by total_venue_booking_amount desc) <= 50
+            qualify row_number() over (
+                {% if not dim.name == 'NAT' %}
+                    partition by {{ dim.value_expr }}
+                {% endif %}
+                order by total_venue_booking_amount desc) <= 50
         {% endfor %}
     )
 

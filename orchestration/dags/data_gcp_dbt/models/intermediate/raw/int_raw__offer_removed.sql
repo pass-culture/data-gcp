0c8112a7b2464234661f3662cf1e9847d05898fd
Lucile <lucile.rainteau@passculture.app>
2025-09-03 17:32:15 +0200

    fmt

--- orchestration/dags/data_gcp_dbt/models/intermediate/raw/int_raw__offer_removed.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/raw/int_raw__offer_removed.sql
@@ -13,5 +13,6 @@ select
     * except (offer_updated_date),
     cast(offer_updated_date as datetime) as offer_updated_date
 from {{ ref("snapshot_raw__offer") }}
-qualify row_number() over (partition by offer_id order by dbt_valid_from desc) = 1
-and dbt_valid_to is not null
+qualify
+    row_number() over (partition by offer_id order by dbt_valid_from desc) = 1
+    and dbt_valid_to is not null

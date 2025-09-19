53725bdab70362e4616f812999f31e193a3f4f3e
Julpass <jules.arbelot@passculture.app>
2025-09-19 14:37:40 +0200

    fix epn

--- orchestration/dags/data_gcp_dbt/models/mart/external_reporting/individual/ephemerals_kpis/mrt_external_reporting__cultural_partner.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/external_reporting/individual/ephemerals_kpis/mrt_external_reporting__cultural_partner.sql
@@ -141,10 +141,7 @@ with
             date_trunc(date(offerer.offerer_creation_date), month) as partition_month,
             count(distinct offerer.offerer_id) as epn_created
         from {{ ref("mrt_global__offerer") }} as offerer
-        inner join
-            {{ ref("mrt_global__offerer_tag") }} as tag
-            on offerer.offerer_id = tag.offerer_id
-        where tag.tag_name in ('part-epn', 'ecosysteme-epn')
+        where offerer_is_epn
         group by partition_month, partner_region_name, partner_department_name
     ),
 

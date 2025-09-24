62286b813a4cf94c99b595289bb4053cee41c1c2
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2025-09-24 15:48:13 +0200

    refactor: Use mrt_global__booking in user_booked_item

--- orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__user_booked_item.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__user_booked_item.sql
@@ -1,7 +1,4 @@
-select distinct b.user_id, o.item_id
-from {{ ref("int_applicative__booking") }} as b
--- TODO add join in int_applicative__booking
-inner join {{ ref("int_applicative__stock") }} as s on b.stock_id = s.stock_id
-inner join {{ ref("int_applicative__offer_item_id") }} as o on s.offer_id = o.offer_id
-inner join {{ ref("int_applicative__user") }} as eud on b.user_id = eud.user_id
-where booking_is_cancelled = false
+select distinct user_id, item_id
+from {{ ref("mrt_global__booking") }}
+where booking_is_cancelled = false and stock_id is not null and user_id is not null
+and offer_id is not null

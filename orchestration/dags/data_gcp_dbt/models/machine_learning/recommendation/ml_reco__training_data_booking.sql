a0bab883a47f3e1f0df3a1d8cb9700400924aab8
Timothée Scheurer <timothee.scheurer@passculture.app>
2025-09-29 09:17:13 +0200

    create int_global__user_beneficiary

--- orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__training_data_booking.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__training_data_booking.sql
@@ -1,9 +1,13 @@
 -- noqa: disable=all
+-- noqa: disable=all
 select
     booking.user_id,
     cast(user.user_age as int64) as user_age,
     "BOOKING" as event_type,
     booking.booking_creation_date as event_date,
+    offer.item_id,
+    offer.offer_subcategory_id,
+    offer.offer_category_id,
     offer.item_id,
     offer.offer_subcategory_id,
     offer.offer_category_id,
@@ -17,6 +21,5 @@ select
     extract(month from booking.booking_created_at) as event_month
 from {{ ref("int_global__booking") }} as booking
 inner join {{ ref("int_global__offer") }} as offer on booking.offer_id = offer.offer_id
-inner join
-    {{ ref("int_global__user_beneficiary") }} as user on booking.user_id = user.user_id
+inner join {{ ref("int_global__user_beneficiary") }} as user on booking.user_id = user.user_id
 where booking.booking_creation_date >= date_sub(date("{{ ds() }}"), interval 6 month)
--- orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__training_data_favorite.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__training_data_favorite.sql
@@ -1,4 +1,5 @@
 -- noqa: disable=all
+-- noqa: disable=all
 {{ config(**custom_table_config(materialized="view")) }}
 
 with
@@ -17,7 +18,9 @@ with
             and user_id is not null
             and offer_id is not null
             and offer_id != "NaN"
+            and offer_id != "NaN"
     )
+
 
 select
     events.user_id,
@@ -30,12 +33,15 @@ select
     offer.item_id,
     offer.offer_subcategory_id,
     offer.offer_category_id,
+    offer.item_id,
+    offer.offer_subcategory_id,
+    offer.offer_category_id,
     offer.genres,
     offer.rayon,
     offer.type,
     offer.venue_id,
     offer.venue_name
+    offer.venue_name
 from events
 inner join {{ ref("int_global__offer") }} as offer on events.offer_id = offer.offer_id
-inner join
-    {{ ref("int_global__user_beneficiary") }} as user on events.user_id = user.user_id
+inner join {{ ref("int_global__user_beneficiary") }} as user on events.user_id = user.user_id

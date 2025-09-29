2cbfcfbe505efec06670b3639cd7047837af8575
Timothée Scheurer <timothee.scheurer@passculture.app>
2025-09-29 11:01:27 +0200

    create mrt_global__user_beneficiary

--- orchestration/dags/data_gcp_dbt/models/intermediate/brevo/int_brevo__transactional_native.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/brevo/int_brevo__transactional_native.sql
@@ -36,7 +36,8 @@ with
             ) as total_favorites
         from {{ ref("int_firebase__native_event") }} as firebase
         left join
-            {{ ref("mrt_global__user_beneficiary") }} as user on firebase.user_id = user.user_id
+            {{ ref("mrt_global__user_beneficiary") }} as user
+            on firebase.user_id = user.user_id
         where
             traffic_campaign is not null
             and lower(traffic_medium) like '%email%'

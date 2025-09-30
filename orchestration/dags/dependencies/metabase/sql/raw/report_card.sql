2ae0fd1d4186a2829ef7894859eaca580a46e0fe
jarbelot-pass <115459312+jarbelot-pass@users.noreply.github.com>
2025-09-30 10:05:44 +0200

    add fields to metabase daily query model (#4492)

--- orchestration/dags/dependencies/metabase/sql/raw/report_card.sql
+++ orchestration/dags/dependencies/metabase/sql/raw/report_card.sql
@@ -6,5 +6,6 @@ select
     description as card_description,
     creator_id as card_creator_id,
     collection_id as card_collection_id
-
+    query_type,
+    dataset_query
 from public.report_card

724abfffa2b5073dd583fe2c7bb213c568f9c424
Lucile <lucile.rainteau@passculture.app>
2025-10-01 12:09:12 +0200

    sql fmt

--- orchestration/dags/data_gcp_dbt/models/intermediate/raw/int_raw__offer.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/raw/int_raw__offer.sql
@@ -11,9 +11,7 @@ select
     * except (offer_updated_date, offer_is_active),
     cast(offer_updated_date as datetime) as offer_updated_date,
     case
-        when dbt_valid_to is null
-        then offer_is_active
-        else false
+        when dbt_valid_to is null then offer_is_active else false
     end as offer_is_active
 from {{ ref("snapshot_raw__offer") }}
 qualify row_number() over (partition by offer_id order by dbt_valid_from desc) = 1

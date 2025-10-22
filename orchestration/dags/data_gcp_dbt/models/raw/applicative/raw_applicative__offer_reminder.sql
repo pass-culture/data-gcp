52cca2ecab3b86305df7f1cb7178486d9b11e603
Timothée Scheurer <timothee.scheurer@passculture.app>
2025-10-22 09:10:09 +0200

    add raw_applicative model

--- orchestration/dags/data_gcp_dbt/models/raw/applicative/raw_applicative__offer_reminder.sql
+++ orchestration/dags/data_gcp_dbt/models/raw/applicative/raw_applicative__offer_reminder.sql
@@ -1,12 +1,7 @@
-select *
-from
-    external_query(
-        "{{ env_var('APPLICATIVE_EXTERNAL_CONNECTION_ID') }}",
-        """
+select * from external_query("{{ env_var('APPLICATIVE_EXTERNAL_CONNECTION_ID') }}", """
 SELECT
     CAST("id" AS varchar(255)) AS offer_reminder_id,
     CAST("userId" AS varchar(255)) AS user_id,
     CAST("offerId" AS varchar(255)) AS offer_id
     FROM public.offer_reminder
-"""
-    )
+""")

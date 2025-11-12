3cd88c1b6c26aa29b4dfef29238c15d640fb1f33
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-11-12 17:49:12 +0100

    lint

--- orchestration/dags/data_gcp_dbt/models/intermediate/cultural_survey/int_cultural_survey__qpi_internal_survey_v2.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/cultural_survey/int_cultural_survey__qpi_internal_survey_v2.sql
@@ -1,29 +1,29 @@
-with exploded_answers as (
-    select
-        a.culturalsurvey_id,
-        cast(a.user_id as string) as user_id,
-        a.form_id,
-        a.submitted_at,
-        a.landed_at,
-        a.platform,
-        ans.question_id,
-        ans.choices as choice_array
-    from {{ source('gcs_seeds', 'qpi_answers_v2') }} as a,
-    unnest(a.answers) as ans
-),
+with
+    exploded_answers as (
+        select
+            a.culturalsurvey_id,
+            cast(a.user_id as string) as user_id,
+            a.form_id,
+            a.submitted_at,
+            a.landed_at,
+            a.platform,
+            ans.question_id,
+            ans.choices as choice_array
+        from {{ source("gcs_seeds", "qpi_answers_v2") }} as a, unnest(a.answers) as ans
+    ),
 
-aggregated_answers as (
-    select
-        culturalsurvey_id,
-        user_id,
-        form_id,
-        submitted_at,
-        landed_at,
-        platform,
-        question_id,
-        choice_array as all_answers
-    from exploded_answers
-)
+    aggregated_answers as (
+        select
+            culturalsurvey_id,
+            user_id,
+            form_id,
+            submitted_at,
+            landed_at,
+            platform,
+            question_id,
+            choice_array as all_answers
+        from exploded_answers
+    )
 
 select
     aa.user_id,
@@ -36,9 +36,9 @@ select
     q.question_text,
     aa.all_answers
 from aggregated_answers as aa
-left join {{ ref('int_seed__qpi_questions_v2') }} as q
-    on aa.question_id = q.question_id
-left join {{ ref('int_applicative__user') }} as u
+left join {{ ref("int_seed__qpi_questions_v2") }} as q on aa.question_id = q.question_id
+left join
+    {{ ref("int_applicative__user") }} as u
     on aa.culturalsurvey_id = u.user_cultural_survey_id
 where u.user_has_filled_cultural_survey
 order by aa.user_id, aa.question_id

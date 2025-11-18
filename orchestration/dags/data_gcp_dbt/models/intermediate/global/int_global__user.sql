251955ea38058079f62558267c527ae0710a4c86
Timothée Scheurer <timothee.scheurer@passculture.app>
2025-11-18 10:36:18 +0100

    add int_global model

---
+++ orchestration/dags/data_gcp_dbt/models/intermediate/global/int_global__user.sql
@@ -0,0 +1,26 @@
+select
+    user_id,
+    user_creation_date,
+    user_humanized_id,
+    user_has_enabled_marketing_email,
+    user_activity,
+    user_civility,
+    user_birth_date,
+    user_age,
+    user_school_type,
+    user_is_active,
+    user_role,
+    user_last_connection_date,
+    user_is_email_validated,
+    user_has_seen_pro_tutorials,
+    user_phone_validation_status,
+    user_has_validated_email,
+    user_has_enabled_marketing_push,
+    user_subscribed_themes,
+    is_theme_subscribed,
+      case
+    when user_role is null and user_age in (15, 16) then '15-16'
+    when user_role is null and (user_age >= 17 or user_age <= 14) then 'general_public'
+    else lower(user_role)
+  end as user_category
+from {{ ref("int_applicative__user") }}

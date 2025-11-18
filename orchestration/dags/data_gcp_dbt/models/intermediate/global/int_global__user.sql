28fa0395777a10e8a078806f60b8e689808ed24e
tscheurer-pass <timothee.scheurer@passculture.app>
2025-11-18 16:37:00 +0100

    (DE-2333) feat(dbt): Create new user models (#4616)

---
+++ orchestration/dags/data_gcp_dbt/models/intermediate/global/int_global__user.sql
@@ -0,0 +1,26 @@
+select
+    user_id,
+    user_creation_date,
+    user_has_enabled_marketing_email,
+    user_activity,
+    user_civility,
+    user_birth_date,
+    user_age,
+    user_school_type,
+    user_is_active,
+    user_role,
+    user_is_email_validated,
+    user_phone_validation_status,
+    user_has_validated_email,
+    user_has_enabled_marketing_push,
+    user_subscribed_themes,
+    is_theme_subscribed,
+    case
+        when user_role is null and user_age in (15, 16)
+        then '15-16'
+        when user_role is null and (user_age >= 17 or user_age <= 14)
+        then 'general_public'
+        when user_role is not null
+        then 'beneficiary'
+    end as user_category
+from {{ ref("int_applicative__user") }}

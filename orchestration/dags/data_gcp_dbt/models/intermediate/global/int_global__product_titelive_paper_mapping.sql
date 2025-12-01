7d306536a216a065f3205bad784ec645ee743414
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-12-01 17:32:36 +0100

    propagate downstream

--- orchestration/dags/data_gcp_dbt/models/intermediate/global/int_global__product_titelive_paper_mapping.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/global/int_global__product_titelive_paper_mapping.sql
@@ -10,6 +10,7 @@ with
     titelive_details as (
         select ean, title, authors, contributor, language_iso, series, series_id
         from {{ ref("int_raw__product_titelive_details") }}
+        where product_type = 'paper'
     )
 
 select

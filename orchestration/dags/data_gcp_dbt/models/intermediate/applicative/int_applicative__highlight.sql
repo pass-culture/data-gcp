29fd829d5bda371c512411fa16a558d9db07ec59
Mathilde Vey <133665016+mvey-pass@users.noreply.github.com>
2025-12-02 19:22:31 +0100

    Add highlight tables

---
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__highlight.sql
@@ -0,0 +1,28 @@
+select 
+    hr.id 
+    ,hr.offer_id 
+    ,hr.highlight_id
+    ,hr.mediation_uuid
+    ,h.highlight_name
+    ,h.highlight_description
+    ,safe.parse_date(
+        '%Y-%m-%d',
+        regexp_extract(hr.highlight_datespan, r'\[([^,]+)')
+    ) as highlight_beginning_date
+    ,safe.parse_date(
+        '%Y-%m-%d',
+        regexp_extract(hr.highlight_datespan, r',([^)]+)')
+    ) as highlight_ending_date
+    ,safe.parse_date(
+        '%Y-%m-%d',
+        regexp_extract(hr.availability_datespan, r'\[([^,]+)')
+    ) as highlight_availability_beginning_date
+    ,safe.parse_date(
+        '%Y-%m-%d',
+        regexp_extract(hr.availability_datespan, r',([^)]+)')
+    ) as highlight_availability_ending_date
+    ,hr.communication_date as highlight_communication_date
+from {{ source("raw", "applicative_database_highlight_request") }} hr
+left join
+    {{ source("raw", "applicative_database_highlight") }} h
+    on hr.id=h.id
---
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/highlight.sql
@@ -0,0 +1,5 @@
+SELECT 
+    CAST("id" AS varchar(255)) as id 
+    , CAST("name" AS varchar(255)) as highlight_name
+    , CAST("description" as varchar(255)) as highlight_description
+FROM public.highlight

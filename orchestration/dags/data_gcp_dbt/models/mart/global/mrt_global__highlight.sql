1b47e8f1651bf1a80b1e560c1d4a96d05b2dac10
Mathilde Vey <133665016+mvey-pass@users.noreply.github.com>
2025-12-02 19:22:50 +0100

    Add highlight tables

--- orchestration/dags/data_gcp_dbt/models/mart/global/mrt_global__highlight.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/global/mrt_global__highlight.sql
@@ -1,12 +1,12 @@
 select
-    offer_id 
-    ,highlight_id
-    ,mediation_uuid
-    ,highlight_name
-    ,highlight_description
-    ,highlight_beginning_date
-    ,highlight_ending_date
-    ,highlight_beginning_date
-    ,highlight_availability_ending_date
-    ,highlight_availability_communication_date
+    offer_id,
+    highlight_id,
+    mediation_uuid,
+    highlight_name,
+    highlight_description,
+    highlight_beginning_date,
+    highlight_ending_date,
+    highlight_beginning_date,
+    highlight_availability_ending_date,
+    highlight_availability_communication_date
 from {{ ref("int_applicative__highlight") }}
--- orchestration/dags/dependencies/applicative_database/sql/raw/parallel/highlight_request.sql
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/highlight_request.sql
@@ -1,10 +1,9 @@
-SELECT 
-    CAST("id" AS varchar(255)) as id 
-    , CAST("offerId" AS varchar(255)) as offer_id 
+SELECT
+    CAST("id" AS varchar(255)) as id
+    , CAST("offerId" AS varchar(255)) as offer_id
     , CAST("highlightId" as varchar(255)) as highlight_id
     , CAST("mediationUuid" as varchar(255)) as mediation_uuid
     , CAST("highlightDatespan" as varchar(255)) as highlight_datespan
     , CAST("availabilityDatespan" as varchar(255)) as availability_datespan
     , "communicationDate" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS communication_date
 FROM public.highlight_request
-

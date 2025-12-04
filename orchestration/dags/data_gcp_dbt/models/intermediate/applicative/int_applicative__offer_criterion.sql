3ff70f409477fbc95b5e3633b2f28729e75d275e
Mathilde Vey <133665016+mvey-pass@users.noreply.github.com>
2025-12-04 11:25:55 +0100

    fix

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__offer_criterion.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__offer_criterion.sql
@@ -2,20 +2,20 @@ select
     adc.name as tag_name,
     adc.id as criterion_id,
     adc.description,
-    adc.highlight_id,
+    adc.highlightid as highlight_id,
     adcc.criterion_category_label,
-    date(adc.startdatetime) as criterion_beginning_date,
-    date(adc.enddatetime) as criterion_ending_date,
     adoc.offerid as offer_id,
-    ado.offer_name as offer_name
-from {{ source("raw", "applicative_database_criterion") }} adc
+    ado.offer_name,
+    date(adc.startdatetime) as criterion_beginning_date,
+    date(adc.enddatetime) as criterion_ending_date
+from {{ source("raw", "applicative_database_criterion") }} as adc
 left join
-    {{ source("raw", "applicative_database_offer_criterion") }} adoc
-    on adoc.criterionid = adc.id
-left join {{ ref("int_raw__offer") }} ado on ado.offer_id = adoc.offerid
+    {{ source("raw", "applicative_database_offer_criterion") }} as adoc
+    on adc.id = adoc.criterionid
+left join {{ ref("int_raw__offer") }} as ado on adoc.offerid = ado.offer_id
 left join
-    {{ source("raw", "applicative_database_criterion_category_mapping") }} adccm
-    on adccm.criterion_id = adc.id
+    {{ source("raw", "applicative_database_criterion_category_mapping") }} as adccm
+    on adc.id = adccm.criterion_id
 left join
-    {{ source("raw", "applicative_database_criterion_category") }} adcc
-    on adcc.criterion_category_id = adccm.criterion_category_id
+    {{ source("raw", "applicative_database_criterion_category") }} as adcc
+    on adccm.criterion_category_id = adcc.criterion_category_id

7aa534c04aac99c6cd9c250b120c5a358a42787c
cibrahim-pass <188350754+cibrahim-pass@users.noreply.github.com>
2025-12-08 15:15:44 +0100

    file formatted

--- orchestration/dags/data_gcp_dbt/models/machine_learning/finance/ml_finance__weekly_pricing.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/finance/ml_finance__weekly_pricing.sql
@@ -1,9 +1,5 @@
-select
-    TIMESTAMP_TRUNC(valuedate, week) as semaine,
-    SUM(-amount / 100) as total_pricing
+select timestamp_trunc(valuedate, week) as semaine, sum(- amount / 100) as total_pricing
 from {{ ref("int_finance__pricing") }}
 where status like "invoiced" and bookingid is not null
-group by
-    semaine
-order by
-    semaine
+group by semaine
+order by semaine

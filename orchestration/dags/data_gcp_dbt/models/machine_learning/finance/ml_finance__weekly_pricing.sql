7d5676e24e37daaee26a04b14716f5c0c4277976
cibrahim-pass <188350754+cibrahim-pass@users.noreply.github.com>
2025-12-08 14:03:02 +0100

    corrected lintering

--- orchestration/dags/data_gcp_dbt/models/machine_learning/finance/ml_finance__weekly_pricing.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/finance/ml_finance__weekly_pricing.sql
@@ -1,10 +1,9 @@
 select
     TIMESTAMP_TRUNC(valuedate, week) as semaine,
     SUM(-amount / 100) as total_pricing
-from `passculture-data-prod.raw_prod.applicative_database_pricing`
+from {{ ref("int_finance__pricing") }}
 where status like "invoiced" and bookingid is not null
 group by
     semaine
 order by
     semaine
-;

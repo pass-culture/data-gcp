c86b2d88bda8151f36e624e58df1942747fe8ad6
Lucile <lucile.rainteau@passculture.app>
2025-12-11 10:23:22 +0100

    test a fix2

--- jobs/etl_jobs/external/metabase-archiving/dependencies.py
+++ jobs/etl_jobs/external/metabase-archiving/dependencies.py
@@ -38,26 +38,24 @@ def get_query_dependencies(card_list, tables_df):
         card_id = card["id"]
         card_owner = card["creator"]["email"]
         card_name = card["name"]
-        card_type = card["dataset_query"]["type"]
-        query_attributes_keys = card["dataset_query"]["query"].keys()
+        card_type = card["query_type"]
+        query_attributes_keys = card["legacy_query"]["query"].keys()
         table_dependency = []
 
         if "source-table" in query_attributes_keys:
-            source_table_id = card["dataset_query"]["query"]["source-table"]
+            source_table_id = card["legacy_query"]["query"]["source-table"]
             if "joins" in query_attributes_keys:
-                for join in card["dataset_query"]["query"]["joins"]:
+                for join in card["legacy_query"]["query"]["joins"]:
                     table_dependency.append(join["source-table"])
             table_dependency.append(source_table_id)
 
         elif (
             "source-query" in query_attributes_keys
-            and "source-table" in card["dataset_query"]["query"]["source-query"].keys()
+            and "source-table" in card["legacy_query"]["query"]["source-query"].keys()
         ):
-            source_table = card["dataset_query"]["query"]["source-query"][
-                "source-table"
-            ]
+            source_table = card["legacy_query"]["query"]["source-query"]["source-table"]
             if "joins" in query_attributes_keys:
-                for join in card["dataset_query"]["query"]["joins"]:
+                for join in card["legacy_query"]["query"]["joins"]:
                     table_dependency.append(join["source-table"])
             table_dependency.append(source_table)
 
@@ -105,9 +103,9 @@ def get_native_dependencies(cards_list, tables_df):
         card_id = card["id"]
         card_name = card["name"]
         card_owner = card["creator"]["email"]
-        card_type = card["dataset_query"]["type"]
+        card_type = card["query_type"]
 
-        sql_lines = card["dataset_query"]["native"]["query"].lower()
+        sql_lines = card["legacy_query"]["native"]["query"].lower()
         sql_lines = sql_lines.replace("`", "")
         table_dependency = re.findall(regex, sql_lines)
         table_dependency = list(set(table_dependency))

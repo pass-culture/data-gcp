8a9af05a3e3ffb511e3ed0470e572269e078ed06
Lucile <lucile.rainteau@passculture.app>
2025-12-11 10:14:07 +0100

    add temp logs

--- jobs/etl_jobs/external/metabase-archiving/dependencies.py
+++ jobs/etl_jobs/external/metabase-archiving/dependencies.py
@@ -19,6 +19,9 @@ def get_card_lists(metabase):
     native_cards = []
     other_cards = []
     for card in cards:
+        print(card.keys())
+        print(card["dataset_query"].keys())
+        break
         if card["dataset_query"]["type"] == "native":
             native_cards.append(card)
         else:

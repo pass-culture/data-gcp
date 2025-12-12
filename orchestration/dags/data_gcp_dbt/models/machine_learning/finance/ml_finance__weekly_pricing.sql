f669975a65ea610738bb3648007db1797910606c
cibrahim-pass <188350754+cibrahim-pass@users.noreply.github.com>
2025-12-12 15:55:07 +0100

    new daily pricing model

--- orchestration/dags/data_gcp_dbt/models/machine_learning/finance/ml_finance__weekly_pricing.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/finance/ml_finance__weekly_pricing.sql
@@ -1,6 +1,6 @@
 select
-    timestamp_trunc(valuedate, week) as pricing_week,
-    sum(- amount / 100) as total_pricing
+    DATE(TIMESTAMP_TRUNC(valuedate, isoweek)) as pricing_week,
+    SUM(- amount / 100) as total_pricing
 from {{ ref("int_finance__pricing") }}
 where status like "invoiced" and bookingid is not null
 group by pricing_week

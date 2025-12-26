6b2eefb592b9eac0e2258e6fef5d726d9e3e88f5
cibrahim-pass <188350754+cibrahim-pass@users.noreply.github.com>
2025-12-26 16:10:58 +0100

    init finance

---
+++ jobs/ml_jobs/finance/src/constants.py
@@ -0,0 +1,17 @@
+import os
+
+# GCP project and Environment
+GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "passculture-data-prod")
+ENV_SHORT_NAME = os.environ.get("ENV_SHORT_NAME", "prod")
+
+## BigQuery constants
+FINANCE_DATASET = f"ml_finance_{ENV_SHORT_NAME}"
+TABLE_WEEKLY_PRICING = "weekly_pricing"
+TABLE_DAILY_PRICING = "daily_pricing"
+
+
+## Training constants between all models
+IS_start_date = "2022-01-01"  # in sample start date
+OOS_start_date = "2025-09-01"  # out of sample start date (aka backtest set)
+OOS_end_date = "2025-12-01"  # out of sample end date
+PREDICTION_FULL_HORIZON = "2026-12-31"  # Generate predictions up to this date

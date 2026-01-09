da144e657c5ac24df4643112ad62c1b33c5fb91f
Lucille <78435671+lartigaud-pass@users.noreply.github.com>
2026-01-09 16:31:32 +0100

    fix dates in educational_deposit

--- orchestration/dags/dependencies/applicative_database/sql/raw/parallel/educational_deposit.sql
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/educational_deposit.sql
@@ -5,6 +5,6 @@ SELECT
     , amount AS educational_deposit_amount
     , "dateCreated" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS educational_deposit_creation_date
     , CAST("ministry" AS TEXT) AS ministry
-    ,CAST(TRIM(BOTH \'[") \' FROM SPLIT_PART("period" :: text, \',\',1)) AS timestamp) AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS educational_deposit_beginning_date
-    ,CAST(TRIM(BOTH \'[") \' FROM SPLIT_PART("period" :: text, \',\',2)) AS timestamp) AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS educational_deposit_expiration_date
+    , LOWER(period)::date + INTERVAL '1 day' AS educational_deposit_beginning_date
+    , UPPER(period)::date + INTERVAL '1 day' AS educational_deposit_expiration_date
 FROM educational_deposit

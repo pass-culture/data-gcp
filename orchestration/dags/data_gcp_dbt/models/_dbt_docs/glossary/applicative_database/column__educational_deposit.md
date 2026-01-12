74d471b08861c9eb4d6a94e099bfa329f56e9ae9
Lucille <78435671+lartigaud-pass@users.noreply.github.com>
2026-01-12 17:59:52 +0100

    updated educational_deposit model

--- orchestration/dags/data_gcp_dbt/models/_dbt_docs/glossary/applicative_database/column__educational_deposit.md
+++ orchestration/dags/data_gcp_dbt/models/_dbt_docs/glossary/applicative_database/column__educational_deposit.md
@@ -1,2 +1,9 @@
 {% docs column__educational_year_id %} Id of the scholar year. {% enddocs %}
 {% docs column__educational_deposit_amount %} Amount received by the educational institution for this scholar year, in euros. {% enddocs %}
+{% docs column__educational_deposit_id %} Id of the educational deposit. {% enddocs %}
+{% docs column__scholar_year %} Scholar year of the deposit. {% enddocs %}
+{% docs column__calendar_year %} Calendar year of the deposit. {% enddocs %}
+{% docs column__educational_deposit_period %} Period associated to the deposit : sept-dec, janv-aout, all year. {% enddocs %}
+{% docs column__is_current_scholar_year %} Whether or not the deposit is associated to the current scholar year. {% enddocs %}
+{% docs column__is_current_calendar_year %} Whether or not the deposit is associated to the current calendar year. {% enddocs %}
+{% docs column__is_current_deposit %} Whether or not the deposit is associated to the current period. {% enddocs %}
--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__educational_deposit.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__educational_deposit.sql
@@ -1,19 +1,40 @@
 select
-    ed.educational_institution_id as institution_id,
-    ed.educational_deposit_creation_date as deposit_creation_date,
+    ed.educational_deposit_id,
+    ed.educational_institution_id,
+    ed.educational_deposit_creation_date,
     ed.educational_deposit_amount,
-    ey.educational_year_beginning_date as educational_year_beginning_date,
-    ey.educational_year_expiration_date as educational_year_expiration_date,
+    ey.educational_year_beginning_date,
+    ey.educational_year_expiration_date,
+    ed.educational_deposit_beginning_date,
+    ed.educational_deposit_expiration_date,
+    ey.scholar_year,
+    ey.educational_year_id,
     ed.ministry,
+    extract(year from ed.educational_deposit_beginning_date) as calendar_year,
     case
-        when
-            (
-                cast(ey.educational_year_beginning_date as date) <= current_date
-                and cast(ey.educational_year_expiration_date as date) >= current_date
-            )
-        then true
-        else false
-    end as is_current_deposit,
+        (
+            extract(month from ed.educational_deposit_beginning_date),
+            extract(month from ed.educational_deposit_expiration_date)
+        )
+        when (9, 12)
+        then 'sept-dec'
+        when (1, 8)
+        then 'janv-aout'
+        else 'all year'
+    end as educational_deposit_period,
+    coalesce(
+        (
+            cast(ey.educational_year_beginning_date as date) <= current_date
+            and cast(ey.educational_year_expiration_date as date) >= current_date
+        )
+        and (
+            extract(month from ed.educational_deposit_beginning_date)
+            <= extract(month from current_date)
+            and extract(month from ed.educational_deposit_expiration_date)
+            > extract(month from current_date)
+        ),
+        false
+    ) as is_current_deposit,
     rank() over (
         partition by ed.educational_institution_id
         order by ed.educational_deposit_creation_date, ed.educational_deposit_id
@@ -23,7 +44,18 @@ select
         order by
             ed.educational_deposit_creation_date desc, ed.educational_deposit_id desc
     ) as deposit_rank_desc,
-    ey.educational_year_id
+    coalesce(
+        (
+            cast(ey.educational_year_beginning_date as date) <= current_date
+            and cast(ey.educational_year_expiration_date as date) >= current_date
+        ),
+        false
+    ) as is_current_scholar_year,
+    coalesce(
+        extract(year from ed.educational_deposit_beginning_date)
+        = extract(year from current_date),
+        false
+    ) as is_current_calendar_year
 
 from {{ source("raw", "applicative_database_educational_deposit") }} as ed
 left join

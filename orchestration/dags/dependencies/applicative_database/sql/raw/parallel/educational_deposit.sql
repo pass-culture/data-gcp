beae84a2d974f886a1aa21376d902226263b87fe
Lucille <78435671+lartigaud-pass@users.noreply.github.com>
2026-01-12 18:11:39 +0100

    fix educational_deposit model

--- orchestration/dags/dependencies/applicative_database/sql/raw/parallel/educational_deposit.sql
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/educational_deposit.sql
@@ -3,7 +3,7 @@ SELECT
     , CAST("educationalInstitutionId" AS varchar(255)) AS educational_institution_id
     , CAST("educationalYearId" AS varchar(255)) AS educational_year_id
     , amount AS educational_deposit_amount
-    , "dateCreated" AT TIME ZONE \'UTC\' AT TIME ZONE \'Europe/Paris\' AS educational_deposit_creation_date
+    , "dateCreated" AS educational_deposit_creation_date
     , CAST("ministry" AS TEXT) AS ministry
     , LOWER(period)::date + INTERVAL '1 day' AS educational_deposit_beginning_date
     , UPPER(period)::date AS educational_deposit_expiration_date

809ce55b77f38bdddfc692881db46aeaed3abeec
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2026-01-26 16:18:33 +0100

    fix: Fix embed_offer_names script

--- jobs/ml_jobs/artist_linkage/cli/embed_offer_names_on_namesakes.py
+++ jobs/ml_jobs/artist_linkage/cli/embed_offer_names_on_namesakes.py
@@ -6,7 +6,7 @@ import pandas as pd
 import typer
 from sentence_transformers import SentenceTransformer
 
-from src.constants import OFFER_NAME_KEY
+from src.constants import OFFER_NAME_KEY, PRODUCT_ID_KEY
 
 ENCODER_NAME = "google/embeddinggemma-300m"
 PROMPT_NAME = "STS"
@@ -16,15 +16,6 @@ RANDOM_SEED = 42
 
 logging.basicConfig(level=logging.INFO)
 app = typer.Typer()
-
-# uv run cli/deduplicate_artists.py
-#  --applicative-artist-filepath gs://data-bucket-ml-temp-stg/artist_linkage_stg/20260126/applicative_database_artist.parquet
-#  --applicative-product-artist-link-filepath gs://data-bucket-ml-temp-stg/artist_linkage_stg/20260126/applicative_database_product_artist_link.parquet
-#  --product-stats-filepath gs://data-bucket-ml-temp-stg/artist_linkage_stg/20260126/ml_metadata_product_stats.parquet
-#  --artist-score-filepath gs://data-bucket-ml-temp-stg/artist_linkage_stg/20260126/ml_metadata_artist_score.parquet
-#  --output-delta-artist-file-path gs://data-bucket-ml-temp-stg/artist_linkage_stg/20260126/05_delta_artist_with_biography.parquet
-#  --output-delta-artist-alias-file-path gs://data-bucket-ml-temp-stg/artist_linkage_stg/20260126/delta_artist_alias.parquet
-#  --output-delta-product-artist-link-filepath gs://data-bucket-ml-temp-stg/artist_linkage_stg/20260126/delta_product_artist_link.parquet
 
 
 def preprocess_offer_name(offer_name: str) -> str:
@@ -51,13 +42,16 @@ def get_namesakes(artist_with_stats_df: pd.DataFrame) -> pd.DataFrame:
 def main(
     applicative_product_artist_link_filepath: str = typer.Option(),
     artist_score_filepath: str = typer.Option(),
-    namesake_artist_filepath: str = typer.Option(),
-    product_embeddings_filepath: str = typer.Option(),
+    product_stats_filepath: str = typer.Option(),
+    output_product_embeddings_filepath: str = typer.Option(),
 ) -> None:
     # 1. Load raw data
     artist_df = pd.read_parquet(artist_score_filepath)
     applicative_product_artist_link_df = pd.read_parquet(
         applicative_product_artist_link_filepath
+    )
+    product_stats_df = pd.read_parquet(product_stats_filepath).dropna(
+        subset=[OFFER_NAME_KEY]
     )
 
     # 2. Preprocess data
@@ -68,6 +62,7 @@ def main(
                 namesake_artist_df.explode("artist_id_list").artist_id_list
             )
         ]
+        .merge(product_stats_df, on=PRODUCT_ID_KEY, how="inner")
         .assign(
             preprocessed_offer_name=lambda df: df[OFFER_NAME_KEY].map(
                 preprocess_offer_name
@@ -77,12 +72,10 @@ def main(
     )
 
     # 3. Encode offer names
-    encoder = SentenceTransformer(
-        "sentence-transformers/all-MiniLM-L6-v2", token=os.environ.get("HF_TOKEN")
-    )
+    encoder = SentenceTransformer(ENCODER_NAME, token=os.environ.get("HF_TOKEN"))
     embedding_array = encoder.encode(
         products_of_namesake_artists_df.drop_duplicates()
-        .dropna(subset=["offer_name"])
+        .dropna(subset=[OFFER_NAME_KEY])
         .offer_name.tolist(),
         prompt_name=PROMPT_NAME,
         batch_size=BATCH_SIZE,
@@ -93,10 +86,9 @@ def main(
     products_of_namesake_artists_df.assign(
         embeddings=lambda df: list(embedding_array)
     ).to_parquet(
-        product_embeddings_filepath,
+        output_product_embeddings_filepath,
         index=False,
     )
-    namesake_artist_df.to_parquet(namesake_artist_filepath, index=False)
 
 
 if __name__ == "__main__":

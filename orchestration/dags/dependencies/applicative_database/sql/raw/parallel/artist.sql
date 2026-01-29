32ccd1a0e86f949f36b14f398c87e47770333ffa
lmontier-pass <172769973+lmontier-pass@users.noreply.github.com>
2026-01-29 11:04:27 +0100

    (DE-3249) refactor(artist dag): Rename wiki_id and description keys (#4798)
    * refactor: Rename columns in sparql queries
    
    * refactor: Rename columns for wikidata_id and description
    
    * refactor: Update ml_linkage__delta_artist.sql
    
    * refactor: Rename wikimedia columns
    
    * refactor: Update delta_artist sql
    
    * feat: Import artist scores as well as mediation_uuid in applicative artist
    
    * fix: Fix delta_artist_alias sql

--- orchestration/dags/dependencies/applicative_database/sql/raw/parallel/artist.sql
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/artist.sql
@@ -3,12 +3,15 @@ SELECT
     ,"name" as artist_name
     ,"description" as artist_description
     ,"biography" as artist_biography
+    ,"mediation_uuid" as artist_mediation_uuid
     ,"wikidata_id"
     ,"wikipedia_url"
     ,"image" as wikidata_image_file_url
     ,"image_license" as wikidata_image_license
     ,"image_license_url" as wikidata_image_license_url
     ,"image_author" as wikidata_image_author
+    ,"app_search_score" as artist_app_search_score
+    ,"pro_search_score" as artist_pro_search_score
     ,"date_created"
     ,"date_modified"
 FROM public.artist

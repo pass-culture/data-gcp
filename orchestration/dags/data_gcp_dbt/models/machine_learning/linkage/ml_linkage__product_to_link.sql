9f78dccca035651a24a9a416242f710a0ee439dc
dcuesta-pass <daniel.cuesta@passculture.app>
2026-02-03 15:37:47 +0100

    feat(artist linkage): update job to handle multi-artist scenarios in product linking

--- orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__product_to_link.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__product_to_link.sql
@@ -23,6 +23,23 @@ with
         group by offer_product_id, performer, offer_category_id
     ),
 
+    product_actor as (
+        select
+            gof.offer_product_id,
+            gof.offer_category_id,
+            "actor" as artist_type,
+            trim(casting_names, '""') as artist_name,
+            sum(coalesce(gof.total_individual_bookings, 0)) as total_booking_count
+        from
+            {{ ref("mrt_global__offer") }} as gof,
+            unnest(split(trim(gof.casting, "[]"), ",")) as casting_names
+        where
+            gof.offer_product_id != ""
+            and gof.casting is not null
+            and gof.casting != "[]"
+        group by gof.offer_product_id, casting_names, gof.offer_category_id
+    ),
+
     product_artist as (
         select
             offer_product_id,
@@ -39,6 +56,15 @@ with
             artist_type,
             total_booking_count
         from product_performer
+        where artist_name is not null and artist_name != ""
+        union all
+        select
+            offer_product_id,
+            artist_name,
+            offer_category_id,
+            artist_type,
+            total_booking_count
+        from product_actor
         where artist_name is not null and artist_name != ""
     )
 

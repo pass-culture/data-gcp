8e5ada155559c236e097b5545c542a68183d86d6
Mathilde Vey <133665016+mvey-pass@users.noreply.github.com>
2026-02-25 12:23:15 +0100

    add venue domains

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__venue_domain.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__venue_domain.sql
@@ -1,8 +1,4 @@
-select
-    edv.educational_domain_venue_id,
-    edv.educational_domain_id,
-    edv.venue_id,
-    ed.educational_domain_name
+select edv.educational_domain_id, edv.venue_id, ed.educational_domain_name
 from {{ source("raw", "applicative_database_educational_domain_venue") }} edv
 left join
     {{ source("raw", "applicative_database_educational_domain") }} as ed
--- orchestration/dags/data_gcp_dbt/models/mart/global/mrt_global__venue_domain.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/global/mrt_global__venue_domain.sql
@@ -1,6 +1,2 @@
-select
-    educational_domain_venue_id,
-    educational_domain_id,
-    venue_id,
-    educational_domain_name
+select educational_domain_id, venue_id, educational_domain_name
 from {{ ref("int_applicative__venue_domain") }}

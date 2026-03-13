a8c4580b8680fec9b325cc98a886a230ede32132
cibrahim-pass <188350754+cibrahim-pass@users.noreply.github.com>
2026-03-13 17:15:02 +0100

    (DS) feat(embedding): added DBT model for final embedding table with documentation (#4935)
    * added DBT model for final embedding table with documentation
    
    * resolve copilot comments

---
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/feature/ml_feat__item_embedding_refactor.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        **custom_incremental_config(
+            incremental_strategy="merge",
+            partition_by=None,
+            unique_key="item_id",
+            on_schema_change="append_new_columns",
+        )
+    )
+}}
+
+select
+    ie.item_id,
+    ie.content_hash,
+    array(
+        select e.element from unnest(ie.semantic_content_sts.list) as e
+    ) as semantic_content_sts,
+    array(
+        select e.element from unnest(ie.semantic_content_clustering.list) as e
+    ) as semantic_content_clustering
+from {{ source("ml_feat", "item_embedding_tmp") }} as ie
+inner join {{ ref("ml_input__item_metadata") }} as im on ie.item_id = im.item_id

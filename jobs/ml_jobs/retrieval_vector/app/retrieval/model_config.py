f18a162acc5b3de7277a1fbf1ab2c65f8d7e1460
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2026-03-20 07:20:20 +0100

    fix: Fix ModelConfig Reading

--- jobs/ml_jobs/retrieval_vector/app/retrieval/model_config.py
+++ jobs/ml_jobs/retrieval_vector/app/retrieval/model_config.py
@@ -1,5 +1,6 @@
 import json
 from dataclasses import dataclass
+from enum import StrEnum
 from typing import Optional
 
 from app.retrieval.client import DefaultClient
@@ -7,21 +8,30 @@ from app.retrieval.metadata_graph_client import MetadataGraphClient
 from app.retrieval.reco_client import RecoClient
 
 
+class ModelType(StrEnum):
+    RECOMMENDATION = "recommendation"
+    METADATA_GRAPH = "metadata_graph"
+
+
 @dataclass
 class ModelConfig:
-    type: str
+    type: ModelType
     default_token: Optional[str] = None
 
 
 def load_model() -> DefaultClient:
     """Load the model based on the configuration file."""
+
+    # Load model configuration
     with open("./metadata/model_type.json", "r") as file:
         desc = json.load(file)
-        config = ModelConfig(**desc)
+    config = ModelConfig(
+        type=ModelType(desc["type"]), default_token=desc.get("default_token")
+    )
 
-        if config.type == "recommendation":
-            return RecoClient(default_token=config.default_token)
-        elif config.type == "metadata_graph":
-            return MetadataGraphClient(default_token=config.default_token)
-        else:
-            raise ValueError("Invalid model type")
+    if config.type == ModelType.RECOMMENDATION:
+        return RecoClient(default_token=config.default_token)
+    elif config.type == ModelType.METADATA_GRAPH:
+        return MetadataGraphClient(default_token=config.default_token)
+    else:
+        raise ValueError("Invalid model type")

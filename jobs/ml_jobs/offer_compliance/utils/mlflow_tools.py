d210eaf69a0d09c3914c2b17f6ff00c9762f9d7b
cibrahim-pass <188350754+cibrahim-pass@users.noreply.github.com>
2026-03-24 15:24:00 +0100

    Revert "Revert "(BSR) chore(jwt): migrate mlflow connection to JWT token (#49…"
    This reverts commit eb10f36964b29b16105b5b49e295568a5b910d02.

--- jobs/ml_jobs/offer_compliance/utils/mlflow_tools.py
+++ jobs/ml_jobs/offer_compliance/utils/mlflow_tools.py
@@ -1,22 +1,89 @@
+import datetime
 import json
 import os
 
+import google.auth
 import mlflow
-from google.auth.transport.requests import Request
-from google.oauth2 import service_account
+from google.cloud import iam_credentials_v1
 
-from constants import MLFLOW_SECRET_NAME, MLFLOW_URI, SA_ACCOUNT
-from utils.secrets_utils import get_secret
+from constants import ENV_SHORT_NAME
+
+SA_ACCOUNT = (
+    f"algo-training-{ENV_SHORT_NAME}@passculture-data-ehp.iam.gserviceaccount.com"
+)
+MODELS_RESULTS_TABLE_NAME = "mlflow_training_results"
+MLFLOW_RUN_ID_FILENAME = "mlflow_run_id"
+MLFLOW_URI = (
+    "https://mlflow.passculture.team/"
+    if ENV_SHORT_NAME == "prod"
+    else "https://mlflow.staging.passculture.team/"
+)
+
+
+def generate_jwt_payload(service_account_email: str, resource_url: str) -> str:
+    """Generates JWT payload for service account.
+
+    Creates a properly formatted JWT payload with standard claims (iss, sub,
+    aud, iat, exp) needed for IAP authentication.
+
+    Args:
+        service_account_email (str): Specifies the service account that the
+        JWT is created for.
+        resource_url (str): Specifies the scope of the JWT, the URL that the
+        JWT will be allowed to access.
+
+    Returns:
+        str: JSON string containing the JWT payload with properly formatted
+        claims.
+    """
+    # Create current time and expiration time (1 hour later) in UTC
+    iat = datetime.datetime.now(tz=datetime.UTC)
+    exp = iat + datetime.timedelta(seconds=3600)
+
+    payload = {
+        "iss": service_account_email,
+        "sub": service_account_email,
+        "aud": resource_url,
+        "iat": int(iat.timestamp()),
+        "exp": int(exp.timestamp()),
+    }
+
+    return json.dumps(payload)
+
+
+def sign_jwt(target_sa: str, resource_url: str) -> str:
+    """Signs JWT payload using ADC and IAM credentials API.
+
+    Uses Google Cloud's IAM Credentials API to sign a JWT. This requires the
+    caller to have iap.webServiceVersions.accessViaIap permission on the
+    target service account.
+
+    Args:
+        target_sa (str): Service Account JWT is being created for.
+            iap.webServiceVersions.accessViaIap permission is required.
+        resource_url (str): Audience of the JWT and scope of the JWT token.
+            This is the URL of the IAP-secured application.
+
+    Returns:
+        str: A signed JWT that can be used to access IAP-secured applications.
+            Use in Authorization header as: 'Bearer <signed_jwt>'
+    """
+    source_credentials, _ = google.auth.default()
+    iam_client = iam_credentials_v1.IAMCredentialsClient(credentials=source_credentials)
+    name = iam_client.service_account_path("-", target_sa)
+    payload = generate_jwt_payload(target_sa, resource_url)
+    response = iam_client.sign_jwt(name=name, payload=payload)
+
+    return response.signed_jwt
 
 
 def connect_remote_mlflow() -> None:
-    service_account_dict = json.loads(get_secret(SA_ACCOUNT))
-    mlflow_client_audience = get_secret(MLFLOW_SECRET_NAME)
-
-    id_token_credentials = service_account.IDTokenCredentials.from_service_account_info(
-        service_account_dict, target_audience=mlflow_client_audience
+    """Connect to remote MLflow tracking server using signed JWT for authentication."""
+    signed_jwt = sign_jwt(
+        target_sa=SA_ACCOUNT,
+        resource_url=MLFLOW_URI
+        + "*",  # add * wildcard to get a token that works for all MLflow API endpoints
+        # under the base URI
     )
-    id_token_credentials.refresh(Request())
-
-    os.environ["MLFLOW_TRACKING_TOKEN"] = id_token_credentials.token
+    os.environ["MLFLOW_TRACKING_TOKEN"] = signed_jwt
     mlflow.set_tracking_uri(MLFLOW_URI)

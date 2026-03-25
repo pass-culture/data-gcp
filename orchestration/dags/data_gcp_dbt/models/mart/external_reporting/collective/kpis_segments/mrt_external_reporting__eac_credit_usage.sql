8014a5324bf244b21b02ea37891d848009b9dd62
Julpass <jules.arbelot@passculture.app>
2026-03-25 16:13:59 +0100

    fix typo

--- orchestration/dags/data_gcp_dbt/models/mart/external_reporting/collective/kpis_segments/mrt_external_reporting__eac_credit_usage.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/external_reporting/collective/kpis_segments/mrt_external_reporting__eac_credit_usage.sql
@@ -11,33 +11,28 @@
 {% set dimensions = [
     {
         "name": "NAT",
-        "value_expr": "bc.dimension_nat",
-        "value_filter": "bc.dimension_nat",
-        "budget_col": "dimension_value",
+        "value_expr": "dimension_nat",
+        "value_filter": "dimension_nat",
     },
     {
         "name": "REG",
-        "value_expr": "bc.institution_region_name",
-        "value_filter": "bc.venue_region_name",
-        "budget_col": "dimension_value",
+        "value_expr": "institution_region_name",
+        "value_filter": "venue_region_name",
     },
     {
         "name": "ACAD",
-        "value_expr": "bc.institution_academy_name",
-        "value_filter": "bc.venue_academy_name",
-        "budget_col": "dimension_value",
+        "value_expr": "institution_academy_name",
+        "value_filter": "venue_academy_name",
     },
     {
         "name": "COM",
-        "value_expr": "bc.institution_city_code",
-        "value_filter": "bc.venue_city_code",
-        "budget_col": "dimension_value",
+        "value_expr": "institution_city_code",
+        "value_filter": "venue_city_code",
     },
     {
         "name": "EPCI",
-        "value_expr": "bc.institution_epci_code",
-        "value_filter": "bc.venue_epci_code",
-        "budget_col": "dimension_value",
+        "value_expr": "institution_epci_code",
+        "value_filter": "venue_epci_code",
     },
 ] %}
 
@@ -47,12 +42,12 @@ with
             select
                 '{{ dim.name }}' as dimension_name,
                 {% if dim.name == "NAT" %} 'NAT'
-                {% else %} {{ dim.value_expr.replace("bc.", "") }}
+                {% else %} {{ dim.value_expr }}
                 {% endif %} as dimension_value,
                 scholar_year,
                 sum(total_scholar_year_deposit) as total_budget_deposit
             from {{ ref("mrt_collective__eple_aggregated") }}
-            group by 1, 2, 3
+            group by dimension_name, dimension_value, scholar_year
             {% if not loop.last %}
                 union all
             {% endif %}
@@ -91,7 +86,18 @@ with
     ),
 
     complete_grid as (
-        select ad.*, am.partition_month
+        select
+            ad.scholar_year,
+            ad.dimension_nat,
+            ad.institution_region_name,
+            ad.institution_academy_name,
+            ad.institution_city_code,
+            ad.institution_epci_code,
+            ad.venue_region_name,
+            ad.venue_academy_name,
+            ad.venue_city_code,
+            ad.venue_epci_code,
+            am.partition_month
         from all_dimensions as ad
         inner join all_months as am on ad.scholar_year = am.scholar_year
     ),
@@ -112,12 +118,32 @@ with
             count(distinct collective_booking_id) as total_collective_bookings
         from {{ ref("mrt_global__collective_booking") }}
         where collective_booking_status = 'REIMBURSED'
-        group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        group by
+            scholar_year,
+            institution_region_name,
+            institution_academy_name,
+            institution_city_code,
+            institution_epci_code,
+            venue_region_name,
+            venue_academy_name,
+            venue_city_code,
+            venue_epci_code,
+            partition_month
     ),
 
     complete_data as (
         select
-            cg.*,
+            cg.scholar_year,
+            cg.dimension_nat,
+            cg.institution_region_name,
+            cg.institution_academy_name,
+            cg.institution_city_code,
+            cg.institution_epci_code,
+            cg.venue_region_name,
+            cg.venue_academy_name,
+            cg.venue_city_code,
+            cg.venue_epci_code,
+            cg.partition_month,
             coalesce(
                 md.total_amount_spent_reimbursed, 0
             ) as total_amount_spent_reimbursed,
@@ -140,7 +166,19 @@ with
 
     booking_cumul_amount as (
         select
-            *,
+            scholar_year,
+            dimension_nat,
+            institution_region_name,
+            institution_academy_name,
+            institution_city_code,
+            institution_epci_code,
+            venue_region_name,
+            venue_academy_name,
+            venue_city_code,
+            venue_epci_code,
+            partition_month,
+            total_amount_spent_reimbursed,
+            total_collective_bookings,
             sum(total_amount_spent_reimbursed) over (
                 partition by
                     scholar_year,
@@ -176,27 +214,27 @@ with
         {% for dim in dimensions %}
             select
                 '{{ dim.name }}' as dimension_name,
-                {{ dim.value_expr }} as dimension_value,
-                scholar_year,
-                partition_month,
-                sum(cumulative_amount_spent) as sum_cum_spent,
-                sum(cumulative_bookings) as sum_cum_bookings,
+                bc.{{ dim.value_expr }} as dimension_value,
+                bc.scholar_year,
+                bc.partition_month,
+                sum(bc.cumulative_amount_spent) as sum_cum_spent,
+                sum(bc.cumulative_bookings) as sum_cum_bookings,
                 sum(
                     case
-                        when {{ dim.value_filter }} = {{ dim.value_expr }}
-                        then cumulative_amount_spent
+                        when bc.{{ dim.value_filter }} = bc.{{ dim.value_expr }}
+                        then bc.cumulative_amount_spent
                         else 0
                     end
                 ) as sum_cum_spent_local,
                 sum(
                     case
-                        when {{ dim.value_filter }} = {{ dim.value_expr }}
-                        then cumulative_bookings
+                        when bc.{{ dim.value_filter }} = bc.{{ dim.value_expr }}
+                        then bc.cumulative_bookings
                         else 0
                     end
                 ) as sum_cum_bookings_local
             from booking_cumul_amount as bc
-            group by 1, 2, 3, 4
+            group by dimension_name, dimension_value, scholar_year, partition_month
             {% if not loop.last %}
                 union all
             {% endif %}
@@ -275,6 +313,14 @@ with
         {% endfor %}
     )
 
-select *
+select
+    partition_month,
+    updated_at,
+    dimension_name,
+    dimension_value,
+    kpi_name,
+    numerator,
+    denominator,
+    kpi
 from aggregation_by_scholar_year
 where partition_month is not null and dimension_value is not null

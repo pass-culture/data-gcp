f60d818f4c9f63325520320a4444549134fb4024
Julien <201599413+jmontagnat-pass@users.noreply.github.com>
2026-03-26 17:12:28 +0100

    (DE-3605) feat: create offer-quality applicative_database source

---
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/offer_quality.sql
@@ -0,0 +1,6 @@
+SELECT
+    CAST("id" AS varchar(255)) as offer_quality_id
+    ,cast("offerId" as VARCHAR(255)) as offer_id
+    ,"completionScore" as completion_score
+    ,"updatedAt" as updated_at
+FROM public.offer_quality

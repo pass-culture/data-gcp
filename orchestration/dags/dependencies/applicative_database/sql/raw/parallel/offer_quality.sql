25574a5337cca1a5ba91e2cc3c3995404060e0b5
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2026-03-26 17:58:21 +0100

    Update orchestration/dags/dependencies/applicative_database/sql/raw/parallel/offer_quality.sql

--- orchestration/dags/dependencies/applicative_database/sql/raw/parallel/offer_quality.sql
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/offer_quality.sql
@@ -1,5 +1,5 @@
 SELECT
-    CAST("id" AS varchar(255)) as offer_quality_id
+    cast("id" AS varchar(255)) as offer_quality_id
     ,cast("offerId" as VARCHAR(255)) as offer_id
     ,"completionScore" as completion_score
     ,"updatedAt" as updated_at

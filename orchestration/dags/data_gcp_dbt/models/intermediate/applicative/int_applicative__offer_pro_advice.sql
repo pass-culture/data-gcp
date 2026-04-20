25414a526ddd37f9bb3205c9eb9df1863d7b57de
Mathilde Vey <133665016+mvey-pass@users.noreply.github.com>
2026-04-20 14:08:52 +0200

    fix

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__offer_pro_advice.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__offer_pro_advice.sql
@@ -1,2 +1,2 @@
 select id as advice_id, offer_id, venue_id, advice_content, advice_author, update_date
-from {{ source("raw", "applicative_database_advice") }}
+from {{ source("raw", "applicative_database_pro_advice") }}

58b26fc8b1f1d0d1442d09d35bc0054c6703c395
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2026-05-05 18:10:30 +0200

    Update orchestration/dags/jobs/import/import_batch.py

--- orchestration/dags/jobs/import/import_batch.py
+++ orchestration/dags/jobs/import/import_batch.py
@@ -86,7 +86,7 @@ with DAG(
         instance_name=GCE_INSTANCE,
         base_dir=BASE_PATH,
         command=f"""
-        uv run python main.py {GCP_PROJECT_ID} {ENV_SHORT_NAME} android
+        uv run main.py {GCP_PROJECT_ID} {ENV_SHORT_NAME} android
         """,
         retries=2,
     )

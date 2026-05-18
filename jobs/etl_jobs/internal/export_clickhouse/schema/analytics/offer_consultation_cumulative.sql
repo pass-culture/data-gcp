53e38292472c04d5cfe22e57faf7a7813bfe8c62
Lucile <lucile.rainteau@passculture.app>
2026-05-18 10:18:34 +0200

    (DE-3797) feat(clickhouse): add cumulative consultation table

---
+++ jobs/etl_jobs/internal/export_clickhouse/schema/analytics/offer_consultation_cumulative.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE TABLE analytics.offer_consultation_cumulative ON CLUSTER default
+ENGINE = ReplacingMergeTree()
+PARTITION BY tuple()
+ORDER BY (IFNULL(offer_id, 'unknown_offer_id'), partition_date)
+SETTINGS storage_policy = 'gcs_main'
+AS
+WITH offer_consultations AS (
+    SELECT
+        partition_date,
+        offer_id,
+        sum(is_consult_offer) as consultation_cnt
+    FROM intermediate.native_event
+    WHERE event_name = 'ConsultOffer'
+      AND offer_id IS NOT NULL
+      AND partition_date >= today() - INTERVAL 6 MONTH
+    GROUP BY partition_date, offer_id
+)
+SELECT
+    partition_date,
+    offer_id,
+    sum(consultation_cnt) OVER (ORDER BY partition_date ASC) AS consultation_cumulative_cnt
+FROM offer_consultations

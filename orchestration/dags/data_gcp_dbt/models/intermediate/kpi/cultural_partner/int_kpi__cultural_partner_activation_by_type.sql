463d34bd997d49bb9077d5870e51af9fc0fa26a6
Julpass <jules.arbelot@passculture.app>
2026-06-05 12:43:58 +0200

    adapt cultural models

--- orchestration/dags/data_gcp_dbt/models/intermediate/kpi/cultural_partner/int_kpi__cultural_partner_activation_by_type.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/kpi/cultural_partner/int_kpi__cultural_partner_activation_by_type.sql
@@ -1,91 +1,49 @@
 {% set partner_types = get_partner_types() %}
 
 with
-    all_activated_partners_and_days as (
-        -- Pour chaque venue_id, une ligne par jour depuis la 1ère offre publiée
+    monthly_partner_activity as (
         select
             gcp.venue_id,
-            gcp.first_individual_offer_creation_date,
-            gcp.first_collective_offer_creation_date,
-            date_add(date('2022-01-01'), interval offset day) as partition_day
+            date_trunc(
+                date_add(date('2022-01-01'), interval offset month), month
+            ) as partition_month
         from {{ ref("mrt_global__cultural_partner") }} as gcp
         cross join
-            unnest(generate_array(0, date_diff(current_date(), '2022-01-01', day))) as
+            unnest(generate_array(0, date_diff(current_date(), '2022-01-01', month))) as
         offset
         where
-            (gcp.first_individual_offer_creation_date is not null)
-            or (gcp.first_collective_offer_creation_date is not null)
+            gcp.first_individual_offer_creation_date is not null
+            or gcp.first_collective_offer_creation_date is not null
     ),
 
-    all_days_with_bookability as (
+    historical_max_dates as (
         select
-            apd.venue_id,
-            apd.first_individual_offer_creation_date,
-            apd.first_collective_offer_creation_date,
-            apd.partition_day,
-            coalesce(
-                bvh.total_individual_bookable_offers, 0
-            ) as total_indiv_bookable_offers,
-            coalesce(
-                bvh.total_collective_bookable_offers, 0
-            ) as total_collective_bookable_offers
-        from all_activated_partners_and_days as apd
+            m.venue_id,
+            m.partition_month,
+            max(
+                case
+                    when h.total_individual_bookable_offers > 0 then h.partition_date
+                end
+            ) as last_indiv_date,
+            max(
+                case
+                    when h.total_collective_bookable_offers > 0 then h.partition_date
+                end
+            ) as last_collective_date
+        from monthly_partner_activity as m
         left join
-            {{ ref("int_history__bookable_venue") }} as bvh
-            on apd.venue_id = bvh.venue_id
-            and apd.partition_day = bvh.partition_date
-    ),
-
-    bookable_dates as (
-        select
-            venue_id,
-            first_individual_offer_creation_date,
-            first_collective_offer_creation_date,
-            partition_day,
-            date_diff(
-                partition_day,
-                coalesce(
-                    max(
-                        case
-                            when total_indiv_bookable_offers != 0 then partition_day
-                        end
-                    ) over (
-                        partition by venue_id
-                        order by partition_day
-                        rows between unbounded preceding and current row
-                    ),
-                    first_individual_offer_creation_date
-                ),
-                day
-            ) as days_since_last_indiv_bookable_date,
-            date_diff(
-                partition_day,
-                coalesce(
-                    max(
-                        case
-                            when total_collective_bookable_offers != 0
-                            then partition_day
-                        end
-                    ) over (
-                        partition by venue_id
-                        order by partition_day
-                        rows between unbounded preceding and current row
-                    ),
-                    first_collective_offer_creation_date
-                ),
-                day
-            ) as days_since_last_collective_bookable_date
-        from all_days_with_bookability
+            {{ ref("int_history__bookable_venue") }} as h
+            on m.venue_id = h.venue_id
+            and h.partition_date <= last_day(m.partition_month)
+        group by m.venue_id, m.partition_month
     ),
 
     partner_details as (
         select
             bd.venue_id,
-            bd.partition_day,
-            bd.first_individual_offer_creation_date,
-            bd.first_collective_offer_creation_date,
+            bd.partition_month,
             gcp.partner_region_name,
-            rd.region_code as partner_region_code,
+            gcp.partner_region_code,
             gcp.partner_department_name,
             gcp.partner_department_code,
             gcp.partner_epci_code,
@@ -93,13 +51,17 @@ with
             gcp.partner_type,
             gcp.offerer_id,
             gvt.venue_tag_name,
-            coalesce(
-                bd.days_since_last_indiv_bookable_date, -9999
+            date_diff(
+                least(current_date(), last_day(bd.partition_month)),
+                bd.last_indiv_date,
+                day
             ) as days_since_last_indiv_bookable_date,
-            coalesce(
-                bd.days_since_last_collective_bookable_date, -9999
+            date_diff(
+                least(current_date(), last_day(bd.partition_month)),
+                bd.last_collective_date,
+                day
             ) as days_since_last_collective_bookable_date
-        from bookable_dates as bd
+        from historical_max_dates as bd
         inner join
             {{ ref("mrt_global__cultural_partner") }} as gcp
             on bd.venue_id = gcp.venue_id
@@ -110,15 +72,12 @@ with
             = 'Comptage partenaire label et appellation du MC'
         inner join
             {{ ref("mrt_global__offerer") }} as gof on gcp.offerer_id = gof.offerer_id
-        left join
-            {{ ref("region_department") }} as rd
-            on gcp.partner_department_code = rd.num_dep
     ),
 
     union_partner_types as (
         {% for partner_type in partner_types %}
             select
-                partition_day,
+                partition_month,
                 partner_region_name,
                 partner_region_code,
                 partner_department_name,
@@ -147,7 +106,7 @@ with
             partner_epci_code,
             partner_city_code,
             partner_type,
-            date_trunc(partition_day, month) as partition_month,
+            partition_month,
             count(
                 distinct case
                     when days_since_last_indiv_bookable_date <= 365 then venue_id
@@ -218,7 +177,7 @@ with
             ) as total_cumulative_partners_dual_part
         from union_partner_types
         group by
-            date_trunc(partition_day, month),
+            partition_month,
             partner_region_name,
             partner_region_code,
             partner_department_name,
--- orchestration/dags/data_gcp_dbt/models/mart/global/mrt_global__cultural_partner.sql
+++ orchestration/dags/data_gcp_dbt/models/mart/global/mrt_global__cultural_partner.sql
@@ -13,6 +13,7 @@ select
     v.venue_name as partner_name,
     v.venue_academy_name as partner_academy_name,
     v.venue_region_name as partner_region_name,
+    v.venue_region_code as partner_region_code,
     v.venue_department_code as partner_department_code,
     v.venue_department_name as partner_department_name,
     v.venue_epci as partner_epci,
@@ -53,11 +54,11 @@ select
     v.total_collective_real_revenue,
     v.total_real_revenue,
     case when v.venue_is_open_to_public then "ERP" else "non ERP" end as partner_status
-from {{ ref("int_global__venue") }} v
+from {{ ref("int_global__venue") }} as v
 left join
     {{ source("seed", "agg_partner_cultural_sector") }}
     on v.venue_type_label = agg_partner_cultural_sector.partner_type
 where
     (v.venue_is_open_to_public or v.venue_siret is not null or v.venue_is_permanent)
-    and offerer_validation_status = 'VALIDATED'
+    and offerer_validation_status = "VALIDATED"
     and offerer_is_active

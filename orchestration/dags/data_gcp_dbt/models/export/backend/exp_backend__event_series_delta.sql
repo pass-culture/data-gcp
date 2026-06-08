04b07ad6176f8d4c2dda71eeb74ec1bd6c713938
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2026-06-08 09:54:07 +0200

    feat: Add event_series_mediation_uuid

--- orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__event_series_delta.sql
+++ orchestration/dags/data_gcp_dbt/models/export/backend/exp_backend__event_series_delta.sql
@@ -3,6 +3,7 @@ select
     event_series_name,
     event_series_description,
     event_series_image_url,
+    event_series_mediation_uuid,
     action,
     comment
 from {{ ref("ml_linkage__delta_event_series") }}
--- orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__delta_event_series.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__delta_event_series.sql
@@ -4,5 +4,8 @@ select
     event_description as event_series_description,
     event_image_url as event_series_image_url,
     action,
-    comment
+    comment,
+    regexp_extract(
+        event_image_url, r'/mediations/([^/]+)'
+    ) as event_series_mediation_uuid
 from {{ source("ml_preproc", "delta_event_series") }}

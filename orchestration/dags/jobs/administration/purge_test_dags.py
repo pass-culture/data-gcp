423af1b81a22a68e17562f4fdecc3607604979b1
Valentin Busson <142802218+vbusson-pass@users.noreply.github.com>
2026-06-10 19:14:57 +0200

    feat(devxp): dag schelude test dags purges

---
+++ orchestration/dags/jobs/administration/purge_test_dags.py
@@ -0,0 +1,85 @@
+import datetime
+import fnmatch
+import logging
+
+from airflow import DAG
+from airflow.models.param import Param
+from airflow.operators.python import PythonOperator
+from common.callback import on_failure_base_callback
+from common.config import DAG_TAGS, ENV_SHORT_NAME, GCP_PROJECT_ID, GCS_AIRFLOW_BUCKET
+from common.utils import get_airflow_schedule
+from google.cloud import storage
+
+from jobs.crons import SCHEDULE_DICT
+
+DAG_NAME = "purge_test_dags"
+
+TEST_DAG_MAX_AGE_DAYS = 14
+
+default_dag_args = {
+    "start_date": datetime.datetime(2026, 6, 1),
+    "retries": 1,
+    "retry_delay": datetime.timedelta(minutes=5),
+    "project_id": GCP_PROJECT_ID,
+    "on_failure_callback": on_failure_base_callback,
+}
+
+schedule = SCHEDULE_DICT.get(DAG_NAME, {})
+if isinstance(schedule, dict):
+    schedule = schedule.get(ENV_SHORT_NAME)
+else:
+    schedule = None
+
+
+def _purge_test_dags(**context) -> None:
+    force_purge_all: bool = context["params"]["force_purge_all"]
+
+    client = storage.Client()
+    bucket = client.bucket(GCS_AIRFLOW_BUCKET)
+    cutoff = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(
+        days=TEST_DAG_MAX_AGE_DAYS
+    )
+
+    if force_purge_all:
+        logging.info("force_purge_all=True: deleting all test DAGs regardless of age.")
+    else:
+        logging.info(
+            "Deleting test DAGs last updated before %s (%d-day threshold).",
+            cutoff.isoformat(),
+            TEST_DAG_MAX_AGE_DAYS,
+        )
+
+    deleted = []
+    for blob in client.list_blobs(bucket, prefix="dags/jobs/"):
+        if not fnmatch.fnmatch(blob.name, "*_test_*.py"):
+            continue
+        if blob.name.endswith(DAG_NAME):
+            continue
+        if force_purge_all or blob.updated < cutoff:
+            logging.info("Deleting %s (last updated %s)", blob.name, blob.updated)
+            blob.delete()
+            deleted.append(blob.name)
+
+    logging.info("Purged %d test DAG(s).", len(deleted))
+
+
+with DAG(
+    DAG_NAME,
+    default_args=default_dag_args,
+    description=f"Delete test DAGs from GCS older than {TEST_DAG_MAX_AGE_DAYS} days (or all when force_purge_all=True)",
+    schedule_interval=get_airflow_schedule(schedule),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=15),
+    params={
+        "force_purge_all": Param(
+            default=False,
+            type="boolean",
+            description="When True, delete all test DAGs regardless of age. Intended for manual triggers only.",
+        ),
+    },
+    tags=[DAG_TAGS.DE.value],
+) as dag:
+    purge = PythonOperator(
+        task_id="purge_test_dags",
+        python_callable=_purge_test_dags,
+    )

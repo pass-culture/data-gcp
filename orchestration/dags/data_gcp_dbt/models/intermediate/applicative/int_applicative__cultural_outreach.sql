5d07024ec1e0b84bd2d71d00cb93e1af8d12fdc0
Quitterie Cadiou <quitterie.cadiou@passculture.app>
2026-06-10 14:08:46 +0200

    fix: run precommit

--- orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__cultural_outreach.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/applicative/int_applicative__cultural_outreach.sql
@@ -1,7 +1,7 @@
-SELECT
+select
     cultural_outreach_id,
     offer_id,
     cultural_outreach_claimed_at,
     cultural_outreach_status,
-    lower(cultural_outreach_status) = 'qualified' AS offer_has_mediation
-FROM {{ ref("raw_applicative__cultural_outreach") }}
+    lower(cultural_outreach_status) = 'qualified' as offer_has_mediation
+from {{ ref("raw_applicative__cultural_outreach") }}

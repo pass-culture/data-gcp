6364869371bd10f316de8ad2646867a98886357d
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2026-06-16 16:08:16 +0200

    WIP refactor: use the fix that follows directly the branching in offer_metadata

--- orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__available_offer.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__available_offer.sql
@@ -8,14 +8,14 @@ with
         from {{ ref("mrt_global__offer") }} as o
         inner join
             {{ ref("int_applicative__product") }} as p on o.offer_product_id = p.id
-        where p.is_mediation = 1
+        where p.is_mediation = 1 and o.mediation_humanized_id is null
         union distinct
         select o.offer_id
         from {{ ref("mrt_global__offer") }} as o
         inner join
             {{ ref("int_applicative__product_mediation") }} as pm
             on o.offer_product_id = pm.product_id
-        where pm.uuid is not null
+        where pm.uuid is not null and o.mediation_humanized_id is null
     ),
 
     get_recommendable_offers as (

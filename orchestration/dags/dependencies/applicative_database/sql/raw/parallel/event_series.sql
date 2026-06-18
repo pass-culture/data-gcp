51af54b04cb3a3c985c071bf09ea254aea5a3a57
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2026-06-18 11:17:51 +0200

    feat: Import event_series and event_series_offer_link in applicative

---
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/event_series.sql
@@ -0,0 +1,8 @@
+SELECT
+    "id" as event_series_id
+    ,"name" as event_series_name
+    ,"description" as event_series_description
+    ,"mediationUuid" as event_series_mediation_uuid
+    ,"dateCreated" as date_created
+    ,"dateModified" as date_modified
+FROM public.event_series
---
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/event_series_offer_link.sql
@@ -0,0 +1,7 @@
+SELECT
+    "id" as event_series_offer_link_id
+    ,"eventSeriesId" as event_series_id
+    ,"offerId" as offer_id
+    ,"dateCreated" as date_created
+    ,"dateModified" as date_modified
+FROM public.event_series_offer_link

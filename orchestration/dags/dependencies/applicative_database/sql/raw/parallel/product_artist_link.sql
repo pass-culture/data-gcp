fb80e1114f9dd2f66f6298e50569ee4b1b7750d8
Taner Durmus <289626098+tdurmus-pass@users.noreply.github.com>
2026-06-19 09:16:52 +0200

    fix artist queries

--- orchestration/dags/dependencies/applicative_database/sql/raw/parallel/product_artist_link.sql
+++ orchestration/dags/dependencies/applicative_database/sql/raw/parallel/product_artist_link.sql
@@ -3,6 +3,6 @@ SELECT
     ,"product_id" as offer_product_id
     ,"artist_id"
     ,"artist_type"
-    ,"date_created" AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Paris'
-    ,"date_modified" AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Paris'
+    ,"date_created" AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Paris' as date_created,
+    ,"date_modified" AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Paris' as date_modified,
 FROM public.artist_product_link

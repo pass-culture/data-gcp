8d29591a85a53a9344bf6c264b834502654229e4
dcuesta-pass <82374920+dcuesta-pass@users.noreply.github.com>
2026-06-25 15:17:15 +0200

    (BSR) feat(reco): remove inactive users (#5408)
    * (BSR) feat(reco): remove inactive users
    
    * fix is_active attribute and remaining credit
    
    * add filter on negative remaining credit

--- orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__user_statistics.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/recommendation/ml_reco__user_statistics.sql
@@ -7,9 +7,20 @@ with
             eu.first_deposit_amount as user_deposit_initial_amount,
             eu.last_deposit_amount as user_last_deposit_amount,
             eu.total_theoretical_remaining_credit,
-            eu.total_non_cancelled_individual_bookings as booking_cnt
+            eu.total_deposit_amount,
+            eu.total_actual_amount_spent,
+            eu.total_non_cancelled_individual_bookings as booking_cnt,
+            coalesce(
+                eu.total_deposit_amount - eu.total_actual_amount_spent,
+                eu.last_deposit_amount
+            ) as user_theoretical_remaining_credit
+
         from {{ ref("mrt_global__user_beneficiary") }} as eu
-        where eu.last_deposit_amount is not null  -- Bad quality data (4 rows in stg and prod)
+        where
+            eu.last_deposit_amount is not null  -- Bad quality data (4 rows in stg and prod)
+            and eu.user_is_active  -- is compliant to CGU
+            and eu.user_is_current_beneficiary
+            and eu.total_deposit_amount - eu.total_actual_amount_spent > 0
     )
 
 select
@@ -17,13 +28,10 @@ select
     selected_users.user_deposit_creation_date,
     selected_users.user_birth_date,
     selected_users.user_deposit_initial_amount,
+    selected_users.user_theoretical_remaining_credit,
     selected_users.booking_cnt,
     au.consult_offer,
-    au.has_added_offer_to_favorites,
-    coalesce(
-        selected_users.total_theoretical_remaining_credit,
-        selected_users.user_last_deposit_amount
-    ) as user_theoretical_remaining_credit
+    au.has_added_offer_to_favorites
 from selected_users
 left join
     {{ ref("firebase_aggregated_users") }} as au on selected_users.user_id = au.user_id

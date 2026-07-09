8f5d8a5a34ff763893112ed432855324bd13be9d
lmontier-pass <172769973+lmontier-pass@users.noreply.github.com>
2026-07-09 15:55:41 +0200

    (DE-4252) feat(events): Add mode preingestion checks (#5444)
    * feat: Add mode preingestion checks
    
    * refactor: Add few more checks + reorder checks
    
    * fix: Fix delta for empty
    
    * fix: Fix dbt whith empty tables

--- orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__delta_event_series.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__delta_event_series.sql
@@ -1,19 +1,32 @@
+with
+    casted_delta_event_series as (
+        select
+            cast(event_id as string) as event_series_id,
+            cast(event_description as string) as event_series_description,
+            cast(event_image_url as string) as event_series_image_url,
+            cast(action as string) as action,  -- noqa: disable=RF04
+            cast(comment as string) as comment,
+            cast(event_name as string) as event_name
+        from {{ source("ml_preproc", "delta_event_series") }}
+    )
+
 select
-    delta_event_series.event_id as event_series_id,
-    delta_event_series.event_description as event_series_description,
-    delta_event_series.event_image_url as event_series_image_url,
-    delta_event_series.action,
-    delta_event_series.comment,
+    casted_delta_event_series.event_series_id,
+    casted_delta_event_series.event_series_description,
+    casted_delta_event_series.event_series_image_url,
+    casted_delta_event_series.action,
+    casted_delta_event_series.comment,
     case
-        when delta_event_series.action = 'remove'
+        when casted_delta_event_series.action = 'remove'
         then applicative_database_event_series.event_series_name
-        else delta_event_series.event_name
+        else casted_delta_event_series.event_name
     end as event_series_name,
     regexp_extract(
-        delta_event_series.event_image_url, r'/mediations/([^/]+)'
+        casted_delta_event_series.event_series_image_url, r'/mediations/([^/]+)'
     ) as event_series_mediation_uuid
-from {{ source("ml_preproc", "delta_event_series") }} as delta_event_series
+from casted_delta_event_series
 left join
     {{ source("raw", "applicative_database_event_series") }}
     as applicative_database_event_series
-    on delta_event_series.event_id = applicative_database_event_series.event_series_id
+    on casted_delta_event_series.event_series_id
+    = applicative_database_event_series.event_series_id
--- orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__delta_event_series_offer_link.sql
+++ orchestration/dags/data_gcp_dbt/models/machine_learning/linkage/ml_linkage__delta_event_series_offer_link.sql
@@ -1,2 +1,6 @@
-select event_id as event_series_id, offer_id, action, comment
+select
+    cast(event_id as string) as event_series_id,
+    cast(offer_id as string) as offer_id,
+    cast(action as string) as action,  -- noqa: disable=RF04
+    cast(comment as string) as comment
 from {{ source("ml_preproc", "delta_event_series_offer_link") }}

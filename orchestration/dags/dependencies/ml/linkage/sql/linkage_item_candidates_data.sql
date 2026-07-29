7763488e51daa9a6bf6d9da377d0c1814c627e4e
cibrahim-pass <188350754+cibrahim-pass@users.noreply.github.com>
2026-07-29 17:43:52 +0200

    optimize SQL query and use 128-dim table

--- orchestration/dags/dependencies/ml/linkage/sql/linkage_item_candidates_data.sql
+++ orchestration/dags/dependencies/ml/linkage/sql/linkage_item_candidates_data.sql
@@ -1,30 +1,30 @@
 with
-    import_embeddings as (
-        select
-            ie.item_id,
-            array(
-                select val
-                from unnest(ie.semantic_content) as val
-                with
-                offset pos
-                where pos < 128
-            ) as embedding
-        from `{{ bigquery_ml_feat_dataset }}.item_embedding_refactor` ie
-        inner join
-            `{{ bigquery_analytics_dataset }}.global_offer` go
-            on go.item_id = ie.item_id
-        where go.offer_product_id is null
-    ),
     offers as (
         select
+            go.item_id as raw_item_id,
+            case
+                when go.item_id like 'link-%'
+                then concat('offer-', go.offer_id)
+                else go.item_id
+            end as item_id,
             go.offer_id,
-            go.item_id,
             go.offer_name,
             go.offer_description,
             go.performer,
             go.offer_subcategory_id
         from `{{ bigquery_analytics_dataset }}.global_offer` go
         where go.offer_product_id is null
+        qualify
+            row_number() over (
+                partition by
+                    case
+                        when go.item_id like 'link-%'
+                        then concat('offer-', go.offer_id)
+                        else go.item_id
+                    end
+                order by go.performer desc
+            )
+            = 1
     ),
     bookings as (
         select bo.offer_id, sum(bo.booking_quantity) as booking_count
@@ -36,22 +36,18 @@ with
     ),
     candidates as (
         select
-            case
-                when o.item_id like 'link-%'
-                then concat('offer-', o.offer_id)
-                else o.item_id
-            end as item_id,
-            import_embeddings.embedding,
+            o.item_id,
+            ie.semantic_content_128 as embedding,
             o.offer_name,
             o.offer_description,
             o.performer,
             o.offer_subcategory_id,
             b.booking_count
         from offers o
-        inner join import_embeddings on import_embeddings.item_id = o.item_id
+        inner join
+            `{{ bigquery_ml_feat_dataset }}.item_embedding_refactor_128` ie
+            on ie.item_id = o.raw_item_id
         left join bookings b on b.offer_id = o.offer_id
     )
 select *
 from candidates
-qualify row_number() over (partition by item_id order by performer desc) = 1
-order by rand()

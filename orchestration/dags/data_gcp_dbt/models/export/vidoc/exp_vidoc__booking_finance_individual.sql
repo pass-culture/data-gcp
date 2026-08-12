66b79681588680a14e6c0732c66383a76a60784a
Taner Durmus <289626098+tdurmus-pass@users.noreply.github.com>
2026-08-12 10:30:25 +0200

    statistic secret

--- orchestration/dags/data_gcp_dbt/models/export/vidoc/exp_vidoc__booking_finance_individual.sql
+++ orchestration/dags/data_gcp_dbt/models/export/vidoc/exp_vidoc__booking_finance_individual.sql
@@ -7,16 +7,16 @@ select
     venue_city_code,
     offerer_is_epn,
     offer_category_id,
-    is_statistical_secret,
-    case when not is_statistical_secret then total_bookings end as total_bookings,
-    case when not is_statistical_secret then total_quantities end as total_quantities,
+    is_statistic_secret,
+    case when not is_statistic_secret then total_bookings end as total_bookings,
+    case when not is_statistic_secret then total_quantities end as total_quantities,
     case
-        when not is_statistical_secret then total_revenue_amount
+        when not is_statistic_secret then total_revenue_amount
     end as total_revenue_amount,
     case
-        when not is_statistical_secret then total_reimbursed_amount
+        when not is_statistic_secret then total_reimbursed_amount
     end as total_reimbursed_amount,
     case
-        when not is_statistical_secret then total_contribution_amount
+        when not is_statistic_secret then total_contribution_amount
     end as total_contribution_amount
 from {{ ref("metrics_booking__finance_individual") }}

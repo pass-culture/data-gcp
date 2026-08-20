34b99c99ba2c3639201ba4e1229ec8898300e7ce
Timothée Scheurer <timothee.scheurer@passculture.app>
2026-08-20 15:20:38 +0200

    create models by type

--- orchestration/dags/data_gcp_dbt/models/metrics/cultural_partner/metrics_cultural_partner__activation_by_type.sql
+++ orchestration/dags/data_gcp_dbt/models/metrics/cultural_partner/metrics_cultural_partner__activation_by_type.sql
@@ -1,20 +1,38 @@
 select
-    partition_month,
-    partner_region_name,
-    partner_region_code,
-    partner_department_name,
-    partner_department_code,
-    partner_epci_code,
-    partner_city_code,
-    partner_type,
-    total_active_partners_individual,
-    total_active_partners_collective,
-    total_active_partners_global,
-    total_active_partners_dual_part,
-    total_cumulative_activated_partners_individual,
-    total_cumulative_activated_partners_collective,
-    total_cumulative_activated_partners_global,
-    total_cumulative_activated_partners_individual_only,
-    total_cumulative_activated_partners_collective_only,
-    total_cumulative_activated_partners_dual_part
-from {{ ref("int_kpi__cultural_partner_activation_by_type") }}
+    coalesce(m.partition_month, s.partition_month) as partition_month,
+    coalesce(m.partner_region_name, s.partner_region_name) as partner_region_name,
+    coalesce(m.partner_region_code, s.partner_region_code) as partner_region_code,
+    coalesce(
+        m.partner_department_name, s.partner_department_name
+    ) as partner_department_name,
+    coalesce(
+        m.partner_department_code, s.partner_department_code
+    ) as partner_department_code,
+    coalesce(m.partner_epci_code, s.partner_epci_code) as partner_epci_code,
+    coalesce(m.partner_city_code, s.partner_city_code) as partner_city_code,
+    coalesce(m.partner_type, s.partner_type) as partner_type,
+    m.total_active_partners_individual,
+    m.total_active_partners_collective,
+    m.total_active_partners_global,
+    m.total_active_partners_dual_part,
+    m.total_cumulative_activated_partners_individual,
+    m.total_cumulative_activated_partners_collective,
+    m.total_cumulative_activated_partners_global,
+    m.total_cumulative_activated_partners_individual_only,
+    m.total_cumulative_activated_partners_collective_only,
+    m.total_cumulative_activated_partners_dual_part,
+    s.total_offerers_created_by_cohort,
+    s.total_activated_offerer_consultation_or_adage_30d_by_cohort,
+    s.total_activated_offerer_offer_or_adage_30d_by_cohort,
+    s.total_activated_offerer_global_30d_by_cohort,
+    s.total_activated_offerer_individual_30d_by_cohort,
+    s.total_activated_offerer_collective_30d_by_cohort
+from {{ ref("int_kpi__cultural_partner_activation_by_type") }} as m
+full outer join
+    {{ ref("int_kpi__cultural_partner_activation_cohorts_by_type") }} as s
+    on m.partition_month = s.partition_month
+    and m.partner_city_code = s.partner_city_code
+    and m.partner_epci_code = s.partner_epci_code
+    and m.partner_department_code = s.partner_department_code
+    and m.partner_region_code = s.partner_region_code
+    and m.partner_type = s.partner_type

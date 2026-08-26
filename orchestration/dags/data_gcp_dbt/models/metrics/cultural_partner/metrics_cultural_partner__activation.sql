5c006a4a5e8cf9600e96808c9fc6bca9d7d420cd
Timothée Scheurer <timothee.scheurer@passculture.app>
2026-08-26 10:48:15 +0200

    add tests

--- orchestration/dags/data_gcp_dbt/models/metrics/cultural_partner/metrics_cultural_partner__activation.sql
+++ orchestration/dags/data_gcp_dbt/models/metrics/cultural_partner/metrics_cultural_partner__activation.sql
@@ -1,15 +1,11 @@
 select
-    coalesce(m.partition_month, s.partition_month) as partition_month,
-    coalesce(m.partner_region_name, s.partner_region_name) as partner_region_name,
-    coalesce(m.partner_region_code, s.partner_region_code) as partner_region_code,
-    coalesce(
-        m.partner_department_name, s.partner_department_name
-    ) as partner_department_name,
-    coalesce(
-        m.partner_department_code, s.partner_department_code
-    ) as partner_department_code,
-    coalesce(m.partner_epci_code, s.partner_epci_code) as partner_epci_code,
-    coalesce(m.partner_city_code, s.partner_city_code) as partner_city_code,
+    m.partition_month,
+    m.partner_region_name,
+    m.partner_region_code,
+    m.partner_department_name,
+    m.partner_department_code,
+    m.partner_epci_code,
+    m.partner_city_code,
     m.total_active_partners_individual,
     m.total_active_partners_collective,
     m.total_active_partners_global,
@@ -20,14 +16,24 @@ select
     m.total_cumulative_activated_partners_individual_only,
     m.total_cumulative_activated_partners_collective_only,
     m.total_cumulative_activated_partners_dual_part,
-    s.total_offerers_created_by_cohort,
-    s.total_activated_offerer_consultation_or_adage_30d_by_cohort,
-    s.total_activated_offerer_offer_or_adage_30d_by_cohort,
-    s.total_activated_offerer_global_30d_by_cohort,
-    s.total_activated_offerer_individual_30d_by_cohort,
-    s.total_activated_offerer_collective_30d_by_cohort
+    coalesce(s.total_offerers_created_by_cohort, 0) as total_offerers_created_by_cohort,
+    coalesce(
+        s.total_activated_offerer_consultation_or_adage_30d_by_cohort, 0
+    ) as total_activated_offerer_consultation_or_adage_30d_by_cohort,
+    coalesce(
+        s.total_activated_offerer_offer_or_adage_30d_by_cohort, 0
+    ) as total_activated_offerer_offer_or_adage_30d_by_cohort,
+    coalesce(
+        s.total_activated_offerer_global_30d_by_cohort, 0
+    ) as total_activated_offerer_global_30d_by_cohort,
+    coalesce(
+        s.total_activated_offerer_individual_30d_by_cohort, 0
+    ) as total_activated_offerer_individual_30d_by_cohort,
+    coalesce(
+        s.total_activated_offerer_collective_30d_by_cohort, 0
+    ) as total_activated_offerer_collective_30d_by_cohort
 from {{ ref("int_kpi__cultural_partner_activation") }} as m
-full outer join
+left join
     {{ ref("int_kpi__cultural_partner_activation_cohorts") }} as s
     on m.partition_month = s.partition_month
     and m.partner_city_code = s.partner_city_code

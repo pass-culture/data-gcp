04f52146eb92af865f1bded309a7f568610195f8
Julien <201599413+jmontagnat-pass@users.noreply.github.com>
2026-09-11 11:44:32 +0200

    (DE-4376) feat(sync_recommendation): offer metadata as a view

--- orchestration/dags/data_gcp_dbt/models/export/reco_cloudsql/exp_reco_cloudsql__offer_metadata.sql
+++ orchestration/dags/data_gcp_dbt/models/export/reco_cloudsql/exp_reco_cloudsql__offer_metadata.sql
@@ -1,1 +1,4 @@
-select offer_id, search_group_name from {{ ref("mrt_global__offer_metadata") }}
+{{ config(materialized="view") }}
+
+select offer_id, search_group_name
+from {{ ref("mrt_global__offer_metadata") }}

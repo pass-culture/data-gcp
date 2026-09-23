c879f955ccc5b4e28c1cac021f77e1e104ee6e07
tdurmus-pass <289626098+tdurmus-pass@users.noreply.github.com>
2026-09-23 17:34:53 +0200

    (DE-4362) fix(dbt): clusterize logs query (#5615)
    * clusterize logs query
    
    * Modify log_timestamp
    
    * Modify query to remove date

--- orchestration/dags/data_gcp_dbt/models/intermediate/pcapi/int_pcapi__native_log.sql
+++ orchestration/dags/data_gcp_dbt/models/intermediate/pcapi/int_pcapi__native_log.sql
@@ -3,6 +3,7 @@
         **custom_incremental_config(
             incremental_strategy="insert_overwrite",
             partition_by={"field": "partition_date", "data_type": "date"},
+            cluster_by=["user_id", "source_ip"],
             on_schema_change="append_new_columns",
             require_partition_filter=true,
         )
@@ -28,15 +29,6 @@ select
     cookies_consent_mandatory,
     cookies_consent_accepted,
     cookies_consent_refused,
-    case
-        url_path
-        when "/native/v1/me"
-        then "app_native"
-        when "/beneficiaries/current"
-        then "webapp"
-        when "/users/current"
-        then "pro"
-    end as source,
     newly_subscribed_themes,
     newly_subscribed_email,
     newly_subscribed_push,
@@ -45,11 +37,19 @@ select
     currently_subscribed_marketing_email,
     newly_unsubscribed_themes,
     newly_unsubscribed_email,
-    newly_unsubscribed_push
+    newly_unsubscribed_push,
+    case
+        url_path
+        when "/native/v1/me"
+        then "app_native"
+        when "/beneficiaries/current"
+        then "webapp"
+        when "/users/current"
+        then "pro"
+    end as `source`
 from {{ ref("int_pcapi__log") }}
 where
-    log_timestamp >= date_sub(current_timestamp(), interval 365 day)
-    and url_path in ("/users/current", "/native/v1/me", "/native/v1/signin")
+    url_path in ("/users/current", "/native/v1/me", "/native/v1/signin")
     {% if is_incremental() %}
         and date(log_timestamp)
         between date_sub(date("{{ ds() }}"), interval 2 day) and date("{{ ds() }}")
